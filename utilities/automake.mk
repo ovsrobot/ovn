2d127f3261c10e75a034e822313a42436783defe
Aliasgar Ginwala <aginwala@ebay.com>
2019-08-21 10:07:21 -0700

    Containerize components
    1. Containerize ovn central components
    2. Containerize ovn host
    3. Update documentation about building/running ovn in containers.
    
    Signed-off-by: Aliasgar Ginwala <aginwala@ebay.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- utilities/automake.mk
+++ utilities/automake.mk
@@ -29,6 +29,14 @@ EXTRA_DIST += \
     utilities/ovn-detrace.in \
     utilities/ovndb-servers.ocf \
     utilities/checkpatch.py
+    utilities/docker/Makefile \
+    utilities/docker/start-ovn \
+    utilities/docker/create_ovn_dbs.sh \
+    utilities/docker/ovn_default_nb_port \
+    utilities/docker/ovn_default_sb_port \
+    utilities/docker/ovn_default_northd_host \
+    utilities/docker/debian/Dockerfile \
+    utilities/docker/debian/build.sh
 
 CLEANFILES += \
     utilities/ovn-ctl.8 \
