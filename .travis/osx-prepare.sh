54e9e807d0fa9cdc7c9ef4e5475e7e3dfd83de92
Numan Siddique <numans@ovn.org>
2019-11-08 16:12:48 +0530

    travis: Fix CI failure for osx builds
    The below failure is seen
    
    ****
    checking for Python 3 (version 3.4 or later)... /usr/local/bin/python3
    checking where Python six library is available... configure: error: Missing Python six library.
    The command "./.travis/${TRAVIS_OS_NAME}-build.sh $OPTS" exited with 1.
    ****
    
    This patch fixes it.
    
    Signed-off-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- .travis/osx-prepare.sh
+++ .travis/osx-prepare.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -ev
-pip2 install --user six
-pip2 install --user --upgrade docutils
+pip3 install --user six
+pip3 install --user --upgrade docutils
 
-brew update || true
-brew uninstall libtool && brew install libtool || true
