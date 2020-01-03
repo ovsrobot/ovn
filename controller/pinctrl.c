ovsrobot/ovn

commit 5a12a940f63a5909baee6d00ce9e1696a2e7c96c

Add DNSSL support to OVN
Introduce the possibility to specify a DNSSL option to Router
Advertisement packets. DNS Search list can be specified using
'dnssl' tag in the ipv6_ra_configs column of logical router
port table

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/pinctrl.c
@@ -2194,6 +2194,7 @@ struct ipv6_ra_config {
     struct lport_addresses prefixes;
     struct in6_addr rdnss;
     bool has_rdnss;
+    struct ds dnssl;
 };
 
 struct ipv6_ra_state {
@@ -2215,6 +2216,7 @@ ipv6_ra_config_delete(struct ipv6_ra_config *config)
 {
     if (config) {
         destroy_lport_addresses(&config->prefixes);
+        ds_destroy(&config->dnssl);
         free(config);
     }
 }
@@ -2253,6 +2255,7 @@ ipv6_ra_update_config(const struct sbrec_port_binding *pb)
             nd_ra_min_interval_default(config->max_interval));
     config->mtu = smap_get_int(&pb->options, "ipv6_ra_mtu", ND_MTU_DEFAULT);
     config->la_flags = IPV6_ND_RA_OPT_PREFIX_ON_LINK;
+    ds_init(&config->dnssl);
 
     const char *address_mode = smap_get(&pb->options, "ipv6_ra_address_mode");
     if (!address_mode) {
@@ -2297,6 +2300,11 @@ ipv6_ra_update_config(const struct sbrec_port_binding *pb)
         goto fail;
     }
     config->has_rdnss = !!rdnss;
+
+    const char *dnssl = smap_get(&pb->options, "ipv6_ra_dnssl");
+    if (dnssl) {
+        ds_put_buffer(&config->dnssl, dnssl, strlen(dnssl));
+    }
 
     return config;
 
@@ -2354,6 +2362,57 @@ packet_put_ra_rdnss_opt(struct dp_packet *b, uint8_t num,
                                                       prev_l4_size + len * 8));
 }
 
+static void
+packet_put_ra_dnssl_opt(struct dp_packet *b, ovs_be32 lifetime,
+                        char *dnssl_list)
+{
+    size_t prev_l4_size = dp_packet_l4_size(b);
+    size_t size = sizeof(struct ovs_nd_dnssl);
+    struct ip6_hdr *nh = dp_packet_l3(b);
+    char *t0, *r0, dnssl[255] = {};
+    int i = 0;
+
+    /* Multiple DNS Search List must be 'comma' separated
+     * (e.g. "a.b.c, d.e.f"). Domain names must be encoded
+     * as described in Section 3.1 of RFC1035.
+     * (e.g if dns list is a.b.c,www.ovn.org, it will be encoded as:
+     * 01 61 01 62 01 63 00 03 77 77 77 03 6f 76 63 03 6f 72 67 00
+     */
+    for (t0 = strtok_r(dnssl_list, ",", &r0); t0;
+         t0 = strtok_r(NULL, ",", &r0)) {
+        char *t1, *r1;
+
+        size += strlen(t0) + 2;
+        if (size > sizeof(dnssl)) {
+            return;
+        }
+
+        for (t1 = strtok_r(t0, ".", &r1); t1;
+             t1 = strtok_r(NULL, ".", &r1)) {
+            dnssl[i++] = strlen(t1);
+            memcpy(&dnssl[i], t1, strlen(t1));
+            i += strlen(t1);
+        }
+        dnssl[i++] = 0;
+    }
+    size = ROUND_UP(size, 8);
+    nh->ip6_plen = htons(prev_l4_size + size);
+
+    struct ovs_nd_dnssl *nd_dnssl = dp_packet_put_uninit(b, sizeof *nd_dnssl);
+    nd_dnssl->type = ND_OPT_DNSSL;
+    nd_dnssl->len = size / 8;
+    nd_dnssl->reserved = 0;
+    put_16aligned_be32(&nd_dnssl->lifetime, lifetime);
+
+    dp_packet_put(b, dnssl, size - sizeof *nd_dnssl);
+
+    struct ovs_ra_msg *ra = dp_packet_l4(b);
+    ra->icmph.icmp6_cksum = 0;
+    uint32_t icmp_csum = packet_csum_pseudoheader6(dp_packet_l3(b));
+    ra->icmph.icmp6_cksum = csum_finish(csum_continue(icmp_csum, ra,
+                                                      prev_l4_size + size));
+}
+
 /* Called with in the pinctrl_handler thread context. */
 static long long int
 ipv6_ra_send(struct rconn *swconn, struct ipv6_ra_state *ra)
@@ -2381,6 +2440,10 @@ ipv6_ra_send(struct rconn *swconn, struct ipv6_ra_state *ra)
     if (ra->config->has_rdnss) {
         packet_put_ra_rdnss_opt(&packet, 1, htonl(0xffffffff),
                                 &ra->config->rdnss);
+    }
+    if (ra->config->dnssl.length) {
+        packet_put_ra_dnssl_opt(&packet, htonl(0xffffffff),
+                                ra->config->dnssl.string);
     }
 
     uint64_t ofpacts_stub[4096 / 8];

lib/ovn-l7.h
@@ -231,6 +231,17 @@ struct nd_rdnss_opt {
 };
 BUILD_ASSERT_DECL(ND_RDNSS_OPT_LEN == sizeof(struct nd_rdnss_opt));
 
+/* DNSSL option RFC 6106 */
+#define ND_OPT_DNSSL        31
+#define ND_DNSSL_OPT_LEN    8
+struct ovs_nd_dnssl {
+    u_int8_t type;  /* ND_OPT_DNSSL */
+    u_int8_t len;   /* >= 2 */
+    ovs_be16 reserved;
+    ovs_16aligned_be32 lifetime;
+};
+BUILD_ASSERT_DECL(ND_DNSSL_OPT_LEN == sizeof(struct ovs_nd_dnssl));
+
 #define DHCPV6_DUID_LL      3
 #define DHCPV6_HW_TYPE_ETH  1
 

northd/ovn-northd.c
@@ -6489,6 +6489,10 @@ copy_ra_to_sb(struct ovn_port *op, const char *address_mode)
     if (rdnss) {
         smap_add(&options, "ipv6_ra_rdnss", rdnss);
     }
+    const char *dnssl = smap_get(&op->nbrp->ipv6_ra_configs, "dnssl");
+    if (dnssl) {
+        smap_add(&options, "ipv6_ra_dnssl", dnssl);
+    }
 
     smap_add(&options, "ipv6_ra_src_eth", op->lrp_networks.ea_s);
 

ovn-nb.xml
@@ -1890,6 +1890,11 @@
         IPv6 address of RDNSS server announced in RA packets. At the moment
         OVN supports just one RDNSS server.
       </column>
+
+      <column name="ipv6_ra_configs" key="dnssl">
+        DNS Search List announced in RA packets. Multiple DNS Search List
+        must be 'comma' separated (e.g. "a.b.c, d.e.f")
+      </column>
     </group>
 
     <group title="Options">

tests/ovn.at
@@ -11408,14 +11408,15 @@ construct_expected_ra() {
     local mtu=$1
     local ra_mo=$2
     local rdnss=$3
-    local ra_prefix_la=$4
+    local dnssl=$4
+    local ra_prefix_la=$5
 
     local slla=0101${src_mac}
     local mtu_opt=""
     if test $mtu != 0; then
         mtu_opt=05010000${mtu}
     fi
-    shift 4
+    shift 5
 
     local prefix=""
     while [[ $# -gt 0 ]] ; do
@@ -11429,8 +11430,12 @@ construct_expected_ra() {
     if test $rdnss != 0; then
         rdnss_opt=19030000ffffffff${rdnss}
     fi
+    local dnssl_opt=""
+    if test $dnssl != 0; then
+        dnssl_opt=1f030000ffffffff${dnssl}
+    fi
 
-    local ra=ff${ra_mo}ffff0000000000000000${slla}${mtu_opt}${prefix}${rdnss_opt}
+    local ra=ff${ra_mo}ffff0000000000000000${slla}${mtu_opt}${prefix}${rdnss_opt}${dnssl_opt}
     local icmp=8600XXXX${ra}
 
     local ip_len=$(expr ${#icmp} / 2)
@@ -11465,28 +11470,33 @@ ra_test() {
 }
 
 # Baseline test with no MTU
-ra_test 0 00 0 c0 40 aef00000000000000000000000000000
+ra_test 0 00 0 0 c0 40 aef00000000000000000000000000000
 
 # Now make sure an MTU option makes it
 ovn-nbctl --wait=hv set Logical_Router_Port ro-sw ipv6_ra_configs:mtu=1500
-ra_test 000005dc 00 0 c0 40 aef00000000000000000000000000000
+ra_test 000005dc 00 0 0 c0 40 aef00000000000000000000000000000
 
 # Now test for multiple network prefixes
 ovn-nbctl --wait=hv set Logical_Router_port ro-sw networks='aef0\:\:1/64 fd0f\:\:1/48'
-ra_test 000005dc 00 0 c0 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
+ra_test 000005dc 00 0 0 c0 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
 
 # Now test for RDNSS
 ovn-nbctl --wait=hv set Logical_Router_port ro-sw ipv6_ra_configs:rdnss='aef0::11'
 dns_addr=aef00000000000000000000000000011
-ra_test 000005dc 00 $dns_addr c0 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
+ra_test 000005dc 00 $dns_addr 0 c0 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
 
-# Test a different address mode now
+# Now test for DNSSL
+ovn-nbctl --wait=hv set Logical_Router_port ro-sw ipv6_ra_configs:dnssl="aa.bb.cc"
+dnssl=02616102626202636300000000000000
+ra_test 000005dc 00 $dns_addr $dnssl c0 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
+
+## Test a different address mode now
 ovn-nbctl --wait=hv set Logical_Router_Port ro-sw ipv6_ra_configs:address_mode=dhcpv6_stateful
-ra_test 000005dc 80 $dns_addr 80 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
+ra_test 000005dc 80 $dns_addr $dnssl 80 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
 
 # And the other address mode
 ovn-nbctl --wait=hv set Logical_Router_Port ro-sw ipv6_ra_configs:address_mode=dhcpv6_stateless
-ra_test 000005dc 40 $dns_addr c0 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
+ra_test 000005dc 40 $dns_addr $dnssl c0 40 aef00000000000000000000000000000 30 fd0f0000000000000000000000000000
 
 OVN_CLEANUP([hv1],[hv2])
 AT_CLEANUP

commit 28b1105026cabb861dd98e3a227c80a3d0d3d51f

DNSSL: copy dnssl string in order to avoid truncated value
ipv6_ra_send can run 2 times in a row before prepare_ipv6_ras updates
the dnss list. Clone the dnss dynamic string before running
packet_put_ra_dnssl_opt in order to avoid sending truncated dnssl list
on the wire.
Moreover move ip6_hdr definition just before accessing it because the
packet can be reallocated if the data area is not big enough for packet
content

Fixes: 5a12a940f ("Add DNSSL support to OVN")
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/pinctrl.c
@@ -2417,7 +2417,6 @@ packet_put_ra_dnssl_opt(struct dp_packet *b, ovs_be32 lifetime,
 {
     size_t prev_l4_size = dp_packet_l4_size(b);
     size_t size = sizeof(struct ovs_nd_dnssl);
-    struct ip6_hdr *nh = dp_packet_l3(b);
     char *t0, *r0 = NULL, dnssl[255] = {};
     int i = 0;
 
@@ -2445,6 +2444,8 @@ packet_put_ra_dnssl_opt(struct dp_packet *b, ovs_be32 lifetime,
         dnssl[i++] = 0;
     }
     size = ROUND_UP(size, 8);
+
+    struct ip6_hdr *nh = dp_packet_l3(b);
     nh->ip6_plen = htons(prev_l4_size + size);
 
     struct ovs_nd_dnssl *nd_dnssl = dp_packet_put_uninit(b, sizeof *nd_dnssl);
@@ -2566,8 +2567,11 @@ ipv6_ra_send(struct rconn *swconn, struct ipv6_ra_state *ra)
                                 &ra->config->rdnss);
     }
     if (ra->config->dnssl.length) {
-        packet_put_ra_dnssl_opt(&packet, htonl(0xffffffff),
-                                ra->config->dnssl.string);
+        struct ds dnssl;
+
+        ds_clone(&dnssl, &ra->config->dnssl);
+        packet_put_ra_dnssl_opt(&packet, htonl(0xffffffff), ds_cstr(&dnssl));
+        ds_destroy(&dnssl);
     }
     if (ra->config->route_info.length) {
         packet_put_ra_route_info_opt(&packet, htonl(0xffffffff),