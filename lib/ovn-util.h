e490d6bd4aa503aefaee4ad8bf4e41058ac49b22
Han Zhou <hzhou@ovn.org>
2020-01-27 18:55:31 -0800

    ovn-ic: Transit switch controller.
    Processing transit switches and sync between IC-NB, IC-SB and NB.
    
    Signed-off-by: Han Zhou <hzhou@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ovn-util.h
+++ lib/ovn-util.h
@@ -98,6 +98,7 @@ uint32_t ovn_logical_flow_hash(const struct uuid *logical_datapath,
 struct hmap;
 void ovn_destroy_tnlids(struct hmap *tnlids);
 void ovn_add_tnlid(struct hmap *set, uint32_t tnlid);
+bool ovn_tnlid_in_use(const struct hmap *set, uint32_t tnlid);
 uint32_t ovn_allocate_tnlid(struct hmap *set, const char *name, uint32_t min,
                             uint32_t max, uint32_t *hint);
 #endif
