ovsrobot/ovn

commit 8fcbcca9649f6787f2a8fc646aeca8c181c8d50e

docs: update various OVS references to OVN
Submitted-at: https://github.com/ovn-org/ovn/pull/40
Signed-off-by: Dan Williams <dcbw@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

Documentation/conf.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Open vSwitch documentation build configuration file, created by
+# Open Virtual Network (OVN) documentation build configuration file, created by
 # sphinx-quickstart on Fri Sep 30 09:57:36 2016.
 #
 # This file is execfile()d with the current directory set to its
@@ -47,9 +47,9 @@ source_suffix = '.rst'
 master_doc = 'contents'
 
 # General information about the project.
-project = u'OVN'
-copyright = u'2020, The OVN Development Community'
-author = u'The OVN Development Community'
+project = u'Open Virtual Network (OVN)'
+copyright = u'2020, The Open Virtual Network (OVN) Development Community'
+author = u'The Open Virtual Network (OVN) Development Community'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -65,7 +65,7 @@ with open(filename, 'rU') as f:
             release = line.split(',')[1].strip(string.whitespace + '[]')
             break
 if release is None:
-    sys.stderr.write('%s: failed to determine Open vSwitch version\n'
+    sys.stderr.write('%s: failed to determine Open Virtual Network (OVN) version\n'
                      % filename)
     sys.exit(1)
 

ovn-architecture.7.xml
@@ -1730,7 +1730,7 @@
 
   <p>
     For more information on L3 gateway high availability, please refer to
-    http://docs.openvswitch.org/en/latest/topics/high-availability.
+    http://docs.ovn.org/en/latest/topics/high-availability.
   </p>
 
   <h3>Restrictions of Distributed Gateway Ports</h3>

rhel/ovn-fedora.spec.in
@@ -28,7 +28,7 @@
 Name: ovn
 Summary: Open Virtual Network support
 Group: System Environment/Daemons
-URL: http://www.openvswitch.org/
+URL: http://www.ovn.org/
 Version: @VERSION@
 Obsoletes: openvswitch-ovn-common < %{?epoch:%{epoch}:}%{version}-%{release}
 Provides: openvswitch-ovn-common = %{?epoch:%{epoch}:}%{version}-%{release}

commit ac3afe5ac56b93f10ee1a26aeb9dc2c447065bd5

conf.py: Fix flake8 error in build.
In commit 8fcbcca9649f it made the line > 79, which causes build error.

Fixes: 8fcbcca9649f ("docs: update various OVS references to OVN")
Cc: Dan Williams <dcbw@redhat.com>
Signed-off-by: Han Zhou <hzhou@ovn.org>
Acked-by: Ben Pfaff <blp@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

Documentation/conf.py
@@ -65,8 +65,8 @@ with open(filename, 'rU') as f:
             release = line.split(',')[1].strip(string.whitespace + '[]')
             break
 if release is None:
-    sys.stderr.write('%s: failed to determine Open Virtual Network (OVN) version\n'
-                     % filename)
+    sys.stderr.write('%s: failed to determine Open Virtual Network (OVN) '
+                     'version\n' % filename)
     sys.exit(1)
 
 # The short X.Y version.