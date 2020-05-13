ovsrobot/ovn

commit c0bf32d72f8b893bbe3cb64912b0fd259d71555f

Manage ARP process locally in a DVR scenario
OVN currently performs L2 address resolution and IP buffering on the
gw node. If the system relies on FIPs, OVN will re-inject the buffered
IP packets on the gw node, while following packets will go though
the localnet port on the compute node resulting in a ToR switch
misconfiguration. This patch addresses the issue managing ARP
and IP buffering locally if FIPs are configured on the node

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/ovn-northd.8.xml
@@ -2407,11 +2407,44 @@ output;
 
       <li>
         <p>
+          For distributed logical routers where one of the logical router ports
+          specifies a <code>redirect-chassis</code>, a priority-400 logical
+          flow for each <code>dnat_and_snat</code> NAT rules configured.
+          These flows will allow to properly forward traffic to the external
+          connections if available and avoid sending it through the tunnel.
+          Assuming the following NAT rule has been configured:
+        </p>
+
+        <pre>
+external_ip = <var>A</var>;
+external_mac = <var>B</var>;
+logical_ip = <var>C</var>;
+        </pre>
+
+        <p>
+          the following action will be applied:
+        </p>
+
+        <pre>
+ip.ttl--;
+reg0 = <var>ip.dst</var>;
+reg1 = <var>A</var>;
+eth.src = <var>B</var>;
+outport = <var>router-port</var>;
+next;
+        </pre>
+
+      </li>
+
+      <li>
+        <p>
           IPv4 routing table.  For each route to IPv4 network <var>N</var> with
           netmask <var>M</var>, on router port <var>P</var> with IP address
           <var>A</var> and Ethernet
           address <var>E</var>, a logical flow with match <code>ip4.dst ==
-          <var>N</var>/<var>M</var></code>, whose priority is the number of
+          <var>N</var>/<var>M</var></code>, whose priority is <code>400</code>
+          + the number of 1-bits in <var>M</var> if the router port is not a
+          distributed gateway port, else the priority is the number of
           1-bits in <var>M</var>, has the following actions:
         </p>
 
@@ -2810,7 +2843,7 @@ icmp4 {
 
       <li>
         For each NAT rule in the OVN Northbound database that can
-        be handled in a distributed manner, a priority-100 logical
+        be handled in a distributed manner, a priority-200 logical
         flow with match <code>ip4.src == <var>B</var> &amp;&amp;
         outport == <var>GW</var></code>, where <var>GW</var> is
         the logical router distributed gateway port, with actions

northd/ovn-northd.c
@@ -6972,6 +6972,8 @@ build_routing_policy_flow(struct hmap *lflows, struct ovn_datapath *od,
     ds_destroy(&actions);
 }
 
+/* default logical flow prioriry for distributed routes */
+#define DROUTE_PRIO 400
 struct parsed_route {
     struct ovs_list list_node;
     struct v46_ip prefix;
@@ -7360,6 +7362,40 @@ build_ecmp_route_flow(struct hmap *lflows, struct ovn_datapath *od,
 }
 
 static void
+add_distributed_routes(struct hmap *lflows, struct ovn_datapath *od)
+{
+    struct ds actions = DS_EMPTY_INITIALIZER;
+    struct ds match = DS_EMPTY_INITIALIZER;
+
+    for (size_t i = 0; i < od->nbr->n_nat; i++) {
+        const struct nbrec_nat *nat = od->nbr->nat[i];
+
+        if (strcmp(nat->type, "dnat_and_snat") ||
+            !nat->external_mac) {
+            continue;
+        }
+
+        bool is_ipv4 = strchr(nat->logical_ip, '.') ? true : false;
+        ds_put_format(&match, "ip%s.src == %s && is_chassis_resident(\"%s\")",
+                      is_ipv4 ? "4" : "6", nat->logical_ip,
+                      nat->logical_port);
+        char *prefix = is_ipv4 ? "" : "xx";
+        ds_put_format(&actions, "outport = %s; eth.src = %s; "
+                      "%sreg0 = ip%s.dst; %sreg1 = %s; next;",
+                      od->l3dgw_port->json_key, nat->external_mac,
+                      prefix, is_ipv4 ? "4" : "6",
+                      prefix, nat->external_ip);
+        ovn_lflow_add(lflows, od, S_ROUTER_IN_IP_ROUTING, DROUTE_PRIO,
+                      ds_cstr(&match), ds_cstr(&actions));
+        ds_clear(&match);
+        ds_clear(&actions);
+    }
+
+    ds_destroy(&actions);
+    ds_destroy(&match);
+}
+
+static void
 add_route(struct hmap *lflows, const struct ovn_port *op,
           const char *lrp_addr_s, const char *network_s, int plen,
           const char *gateway, bool is_src_route,
@@ -7380,6 +7416,12 @@ add_route(struct hmap *lflows, const struct ovn_port *op,
     }
     build_route_match(op_inport, network_s, plen, is_src_route, is_ipv4,
                       &match, &priority);
+    /* traffic for internal IPs of logical switch ports must be sent to
+     * the gw controller through the overlay tunnels
+     */
+    if (op->nbrp && !op->nbrp->n_gateway_chassis) {
+        priority += DROUTE_PRIO;
+    }
 
     struct ds actions = DS_EMPTY_INITIALIZER;
     ds_put_format(&actions, "ip.ttl--; "REG_ECMP_GROUP_ID" = 0; %sreg0 = ",
@@ -8948,7 +8990,7 @@ build_lrouter_flows(struct hmap *datapaths, struct hmap *ports,
                               nat->logical_ip,
                               od->l3dgw_port->json_key);
                 ovn_lflow_add_with_hint(lflows, od, S_ROUTER_IN_GW_REDIRECT,
-                                        100, ds_cstr(&match), "next;",
+                                        200, ds_cstr(&match), "next;",
                                         &nat->header_);
             }
 
@@ -9227,6 +9269,15 @@ build_lrouter_flows(struct hmap *datapaths, struct hmap *ports,
 
         ovn_lflow_add(lflows, od, S_ROUTER_IN_ND_RA_OPTIONS, 0, "1", "next;");
         ovn_lflow_add(lflows, od, S_ROUTER_IN_ND_RA_RESPONSE, 0, "1", "next;");
+    }
+
+    /* Logical router ingress table IP_ROUTING - IP routing for distributed
+     * logical router
+     */
+    HMAP_FOR_EACH (od, key_node, datapaths) {
+        if (od->nbr && od->l3dgw_port) {
+            add_distributed_routes(lflows, od);
+        }
     }
 
     /* Logical router ingress table IP_ROUTING & IP_ROUTING_ECMP: IP Routing.

tests/ovn.at
@@ -9503,6 +9503,20 @@ AT_CHECK([as hv3 ovs-vsctl set Open_vSwitch . external-ids:ovn-bridge-mappings=p
 OVS_WAIT_UNTIL([test 1 = `as hv3 ovs-vsctl show | \
 grep "Port patch-br-int-to-ln_port" | wc -l`])
 
+AT_CHECK([test 1 = `ovn-sbctl dump-flows lr0 | grep lr_in_ip_routing | \
+grep "ip4.src == 10.0.0.3 && is_chassis_resident(\"foo1\")" -c`])
+AT_CHECK([test 1 = `ovn-sbctl dump-flows lr0 | grep lr_in_ip_routing | \
+grep "ip4.src == 10.0.0.4 && is_chassis_resident(\"foo2\")" -c`])
+
+key=`ovn-sbctl --bare --columns tunnel_key list datapath_Binding lr0`
+# Check that the OVS flows appear for the dnat_and_snat entries in
+# lr_in_ip_routing table.
+OVS_WAIT_UNTIL([test 1 = `as hv3 ovs-ofctl dump-flows br-int table=17 | \
+grep "priority=400,ip,metadata=0x$key,nw_src=10.0.0.3" -c`])
+
+OVS_WAIT_UNTIL([test 1 = `as hv3 ovs-ofctl dump-flows br-int table=17 | \
+grep "priority=400,ip,metadata=0x$key,nw_src=10.0.0.4" -c`])
+
 # Re-add nat-addresses option
 ovn-nbctl lsp-set-options lrp0-rp router-port=lrp0 nat-addresses="router"
 

commit fb72b0fa86674ef25a6dbc804749e73e132d74d7

ovn-northd: Increase ECMP route priority with 400.
In commit c0bf32d the priorities of the regular routes were increased by
400, but ECMP routes were not updated. This patch fixes it. Since
for ECMP routes the outport is unknown (there can be many different
outports) the condition check of whether the outport is distributed
gateway port is skipped.

Fixes: c0bf32d ("Manage ARP process locally in a DVR scenario")
Cc: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Han Zhou <hzhou@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/ovn-northd.8.xml
@@ -2601,7 +2601,8 @@ next;
           ids <var>MID1</var>, <var>MID2</var>, ..., a logical flow with match
           in CIDR notation <code>ip4.dst == <var>N</var>/<var>M</var></code>,
           or <code>ip6.dst == <var>N</var>/<var>M</var></code>, whose priority
-          is the integer value of <var>M</var>, has the following actions:
+          is <code>400</code> + the integer value of <var>M</var>, has the
+          following actions:
         </p>
 
         <pre>

northd/ovn-northd.c
@@ -7337,7 +7337,8 @@ build_route_prefix_s(const struct v46_ip *prefix, unsigned int plen)
 }
 
 static void
-build_route_match(const struct ovn_port *op_inport, const char *network_s,
+build_route_match(const struct ovn_port *op_inport,
+                  const struct ovn_port *op_outport, const char *network_s,
                   int plen, bool is_src_route, bool is_ipv4, struct ds *match,
                   uint16_t *priority)
 {
@@ -7350,6 +7351,13 @@ build_route_match(const struct ovn_port *op_inport, const char *network_s,
     } else {
         dir = "dst";
         *priority = (plen * 2) + 1;
+    }
+    /* traffic for internal IPs of logical switch ports must be sent to
+     * the gw controller through the overlay tunnels
+     */
+    if (!op_outport ||
+        (op_outport->nbrp && !op_outport->nbrp->n_gateway_chassis)) {
+        priority += DROUTE_PRIO;
     }
 
     if (op_inport) {
@@ -7434,8 +7442,8 @@ build_ecmp_route_flow(struct hmap *lflows, struct ovn_datapath *od,
     uint16_t priority;
 
     char *prefix_s = build_route_prefix_s(&eg->prefix, eg->plen);
-    build_route_match(NULL, prefix_s, eg->plen, eg->is_src_route, is_ipv4,
-                      &match, &priority);
+    build_route_match(NULL, NULL, prefix_s, eg->plen, eg->is_src_route,
+                      is_ipv4, &match, &priority);
     free(prefix_s);
 
     struct ds actions = DS_EMPTY_INITIALIZER;
@@ -7548,14 +7556,8 @@ add_route(struct hmap *lflows, const struct ovn_port *op,
             op_inport = op;
         }
     }
-    build_route_match(op_inport, network_s, plen, is_src_route, is_ipv4,
+    build_route_match(op_inport, op, network_s, plen, is_src_route, is_ipv4,
                       &match, &priority);
-    /* traffic for internal IPs of logical switch ports must be sent to
-     * the gw controller through the overlay tunnels
-     */
-    if (op->nbrp && !op->nbrp->n_gateway_chassis) {
-        priority += DROUTE_PRIO;
-    }
 
     struct ds actions = DS_EMPTY_INITIALIZER;
     ds_put_format(&actions, "ip.ttl--; "REG_ECMP_GROUP_ID" = 0; %sreg0 = ",