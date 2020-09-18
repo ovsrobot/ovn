eb7afb028a658a9f642ad538ee9f049f589508b7
Ihar Hrachyshka <ihrachys@redhat.com>
2020-09-18 13:12:49 -0400

    Allow to run multiple controllers on the same machine
    User stories:
    1) NFV: an admin wants to run two separate instances of OVN controller
       using the same database but configuring ports on different bridges.
       Some of these bridges may use DPDK while others may not.
    
    2) Parallel OVN instances: an admin wants to run two separate
       instances of OVN controller using different databases. The
       instances are completely independent and serve different consumers.
       For example, the same machine runs both OpenStack and OpenShift
       stacks, each running its own separate OVN stack.
    
    To serve these use cases, several features should be added to
    ovn-controller:
    
    - use different database configuration for multiple controllers;
    - customize chassis name used by controller.
    
    =====
    
    For each of the following database configuration options, their
    extended chassis specific counterparts are introduced:
    
    external_ids:hostname
    external_ids:ovn-bridge
    external_ids:ovn-bridge-datapath-type
    external_ids:ovn-bridge-mappings
    external_ids:ovn-chassis-mac-mappings
    external_ids:ovn-cms-options
    external_ids:ovn-encap-csum
    external_ids:ovn-encap-ip
    external_ids:ovn-encap-type
    external_ids:ovn-is-interconn
    external_ids:ovn-monitor-all
    external_ids:ovn-openflow-probe-interval
    external_ids:ovn-remote
    external_ids:ovn-remote-probe-interval
    
    For example,
    
    external_ids:ovn-bridge -> external_ids:ovn-bridge-<chassis-name>=
    external_ids:ovn-encap-ip -> external_ids:ovn-encap-ip-<chassis-name>=
    external_ids:ovn-remote -> external_ids:ovn-remote-<chassis-name>=
    
    Priority wise, <chassis-name> specific options take precedence.
    
    =====
    
    For system-id,
    
    You can now pass intended chassis name via CLI argument:
    
      $ ovn-controller ... -n <chassis_name>
    
    Alternatively, you can configure a chassis name by putting it into the
    ${ovs_sysconfdir}/ovn/system-id-override file before running the
    controller.
    
    The latter option may be more useful in container environment where
    the same image may be reused for multiple controller instances, where
    ovs_sysconfigdir/ovn/system-id-override is a volume mounted into this
    generic image. The override file is read once on startup. If you want
    to apply a new chassis name to a controller instance, restart it to
    reread the file.
    
    Priority wise, this is the order in which different means to configure
    the chassis name are used:
    
    - ovn-controller ... -n <chassis_name> CLI argument.
    - ${ovs_sysconfdir}/ovn/system-id-override file;
    - external_ids:system-id= ovsdb option;
    
    =====
    
    Concurrent chassis running on the same host may inadvertantly remove
    patch ports that belong to their peer chassis. To avoid that, a new
    external-ids:ovn-is-concurrent config option is introduced. It will
    avoid removing "unknown" patch ports and tunnel endpoints.
    
    =====
    
    Note: this patch assumes that each chassis has its own unique IP.
    Future work may consider adding support to specify custom port numbers
    for tunneling that would allow to reuse the same IP address for
    multiple chassis running on the same host. This work is out of scope
    for this patch.
    
    Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/chassis.c
+++ controller/chassis.c
@@ -125,9 +125,10 @@ chassis_register_ovs_idl(struct ovsdb_idl *ovs_idl)
 }
 
 static const char *
-get_hostname(const struct smap *ext_ids)
+get_hostname(const struct smap *ext_ids, const char *chassis_id)
 {
-    const char *hostname = smap_get_def(ext_ids, "hostname", "");
+    const char *hostname = get_chassis_external_id_value(
+        ext_ids, chassis_id, "hostname", "");
 
     if (strlen(hostname) == 0) {
         static char hostname_[HOST_NAME_MAX + 1];
@@ -143,39 +144,45 @@ get_hostname(const struct smap *ext_ids)
 }
 
 static const char *
-get_bridge_mappings(const struct smap *ext_ids)
+get_bridge_mappings(const struct smap *ext_ids, const char *chassis_id)
 {
-    return smap_get_def(ext_ids, "ovn-bridge-mappings", "");
+    return get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-bridge-mappings", "");
 }
 
 const char *
-get_chassis_mac_mappings(const struct smap *ext_ids)
+get_chassis_mac_mappings(const struct smap *ext_ids, const char *chassis_id)
 {
-    return smap_get_def(ext_ids, "ovn-chassis-mac-mappings", "");
+    return get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-chassis-mac-mappings", "");
 }
 
 static const char *
-get_cms_options(const struct smap *ext_ids)
+get_cms_options(const struct smap *ext_ids, const char *chassis_id)
 {
-    return smap_get_def(ext_ids, "ovn-cms-options", "");
+    return get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-cms-options", "");
 }
 
 static const char *
-get_monitor_all(const struct smap *ext_ids)
+get_monitor_all(const struct smap *ext_ids, const char *chassis_id)
 {
-    return smap_get_def(ext_ids, "ovn-monitor-all", "false");
+    return get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-monitor-all", "false");
 }
 
 static const char *
-get_enable_lflow_cache(const struct smap *ext_ids)
+get_enable_lflow_cache(const struct smap *ext_ids, const char *chassis_id)
 {
-    return smap_get_def(ext_ids, "ovn-enable-lflow-cache", "true");
+    return get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-enable-lflow-cache", "true");
 }
 
 static const char *
-get_encap_csum(const struct smap *ext_ids)
+get_encap_csum(const struct smap *ext_ids, const char *chassis_id)
 {
-    return smap_get_def(ext_ids, "ovn-encap-csum", "true");
+    return get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-encap-csum", "true");
 }
 
 static const char *
@@ -189,9 +196,10 @@ get_datapath_type(const struct ovsrec_bridge *br_int)
 }
 
 static bool
-get_is_interconn(const struct smap *ext_ids)
+get_is_interconn(const struct smap *ext_ids, const char *chassis_id)
 {
-    return smap_get_bool(ext_ids, "ovn-is-interconn", false);
+    return get_chassis_external_id_value_bool(
+        ext_ids, chassis_id, "ovn-is-interconn", false);
 }
 
 static void
@@ -278,22 +286,27 @@ chassis_parse_ovs_config(const struct ovsrec_open_vswitch_table *ovs_table,
         return false;
     }
 
-    const char *encap_type = smap_get(&cfg->external_ids, "ovn-encap-type");
-    const char *encap_ips = smap_get(&cfg->external_ids, "ovn-encap-ip");
+    const char *chassis_id = get_ovs_chassis_id(cfg);
+    const struct smap *ext_ids = &cfg->external_ids;
+
+    const char *encap_type = get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-encap-type", NULL);
+    const char *encap_ips = get_chassis_external_id_value(
+        ext_ids, chassis_id, "ovn-encap-ip", NULL);
     if (!encap_type || !encap_ips) {
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
         VLOG_INFO_RL(&rl, "Need to specify an encap type and ip");
         return false;
     }
 
-    ovs_cfg->hostname = get_hostname(&cfg->external_ids);
-    ovs_cfg->bridge_mappings = get_bridge_mappings(&cfg->external_ids);
+    ovs_cfg->hostname = get_hostname(ext_ids, chassis_id);
+    ovs_cfg->bridge_mappings = get_bridge_mappings(ext_ids, chassis_id);
     ovs_cfg->datapath_type = get_datapath_type(br_int);
-    ovs_cfg->encap_csum = get_encap_csum(&cfg->external_ids);
-    ovs_cfg->cms_options = get_cms_options(&cfg->external_ids);
-    ovs_cfg->monitor_all = get_monitor_all(&cfg->external_ids);
-    ovs_cfg->chassis_macs = get_chassis_mac_mappings(&cfg->external_ids);
-    ovs_cfg->enable_lflow_cache = get_enable_lflow_cache(&cfg->external_ids);
+    ovs_cfg->encap_csum = get_encap_csum(ext_ids, chassis_id);
+    ovs_cfg->cms_options = get_cms_options(ext_ids, chassis_id);
+    ovs_cfg->monitor_all = get_monitor_all(ext_ids, chassis_id);
+    ovs_cfg->chassis_macs = get_chassis_mac_mappings(ext_ids, chassis_id);
+    ovs_cfg->enable_lflow_cache = get_enable_lflow_cache(ext_ids, chassis_id);
 
     if (!chassis_parse_ovs_encap_type(encap_type, &ovs_cfg->encap_type_set)) {
         return false;
@@ -311,7 +324,7 @@ chassis_parse_ovs_config(const struct ovsrec_open_vswitch_table *ovs_table,
         sset_destroy(&ovs_cfg->encap_ip_set);
     }
 
-    ovs_cfg->is_interconn = get_is_interconn(&cfg->external_ids);
+    ovs_cfg->is_interconn = get_is_interconn(ext_ids, chassis_id);
 
     return true;
 }
@@ -348,7 +361,7 @@ chassis_other_config_changed(const char *bridge_mappings,
                              const struct sbrec_chassis *chassis_rec)
 {
     const char *chassis_bridge_mappings =
-        get_bridge_mappings(&chassis_rec->other_config);
+        get_bridge_mappings(&chassis_rec->other_config, NULL);
 
     if (strcmp(bridge_mappings, chassis_bridge_mappings)) {
         return true;
@@ -362,28 +375,28 @@ chassis_other_config_changed(const char *bridge_mappings,
     }
 
     const char *chassis_cms_options =
-        get_cms_options(&chassis_rec->other_config);
+        get_cms_options(&chassis_rec->other_config, NULL);
 
     if (strcmp(cms_options, chassis_cms_options)) {
         return true;
     }
 
     const char *chassis_monitor_all =
-        get_monitor_all(&chassis_rec->other_config);
+        get_monitor_all(&chassis_rec->other_config, NULL);
 
     if (strcmp(monitor_all, chassis_monitor_all)) {
         return true;
     }
 
     const char *chassis_enable_lflow_cache =
-        get_enable_lflow_cache(&chassis_rec->other_config);
+        get_enable_lflow_cache(&chassis_rec->other_config, NULL);
 
     if (strcmp(enable_lflow_cache, chassis_enable_lflow_cache)) {
         return true;
     }
 
     const char *chassis_mac_mappings =
-        get_chassis_mac_mappings(&chassis_rec->other_config);
+        get_chassis_mac_mappings(&chassis_rec->other_config, NULL);
     if (strcmp(chassis_macs, chassis_mac_mappings)) {
         return true;
     }
@@ -791,7 +804,7 @@ chassis_get_mac(const struct sbrec_chassis *chassis_rec,
                 struct eth_addr *chassis_mac)
 {
     const char *tokens
-        = get_chassis_mac_mappings(&chassis_rec->other_config);
+        = get_chassis_mac_mappings(&chassis_rec->other_config, NULL);
     if (!tokens[0]) {
        return false;
     }
