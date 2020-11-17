da5ca272c12e4db5bdbf9a143d4ba03140b927c3
Numan Siddique <numans@ovn.org>
2020-11-17 17:27:09 +0530

    Provide the option to pin ovn-controller and ovn-northd to a specific version.
    OVN recommends updating/upgrading ovn-controllers first and then
    ovn-northd and OVN DB ovsdb-servers.  This is to ensure that any
    new functionality specified by the database or logical flows created
    by ovn-northd is understood by ovn-controller.
    
    However certain deployments may upgrade ovn-northd services first and
    then ovn-controllers.  In a large scal deployment, this can result in
    downtime during upgrades as old ovn-controllers may not understand
    new logical flows or new actions added by ovn-northd.
    
    Even with upgrading ovn-controllers first, can result in ovn-controllers
    rejecting some of the logical flows if an existing OVN action is
    changed.  One such example is ct_commit action which recently was updated
    to take new arguments.
    
    To avoid such downtimes during upgrades, this patch adds the
    functionality of pinning ovn-controller and ovn-northd to a specific
    version. An internal OVN version is generated and this version is stored
    by ovn-northd in the Southbound SB_Global table's
    options:northd_internal_version.
    
    When ovn-controller notices that the internal version has changed, it
    stops handling the database changes - both Southbound and OVS. All the
    existing OF flows are preserved.  When ovn-controller is upgraded to the
    same version as ovn-northd services, it will process the database
    changes.
    
    This feature is made optional and disabled by default. Any CMS can
    enable it by configuring the OVS local database with the option -
    ovn-pin-to-northd=false.
    
    Signed-off-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -2136,6 +2136,37 @@ struct ovn_controller_exit_args {
     bool *restart;
 };
 
+static bool
+pin_to_northd(struct ovsdb_idl *ovs_idl)
+{
+    const struct ovsrec_open_vswitch *cfg = ovsrec_open_vswitch_first(ovs_idl);
+    return !cfg ? false : smap_get_bool(&cfg->external_ids,
+                                        "ovn-pin-to-northd", false);
+}
+
+/* Returns false if the northd internal version and ovn-controller internal
+ * version doesn't match.
+ */
+static bool
+check_northd_version(const struct sbrec_sb_global *sb, const char *my_version)
+{
+    if (!sb) {
+        return false;
+    }
+
+    const char *northd_version =
+        smap_get_def(&sb->options, "northd_internal_version", "");
+
+    bool mismatch = strcmp(northd_version, my_version);
+    if (mismatch) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
+        VLOG_WARN_RL(&rl, "controller version - %s mismatch with northd "
+                     "version - %s", my_version, northd_version);
+    }
+
+    return mismatch;
+}
+
 int
 main(int argc, char *argv[])
 {
@@ -2428,10 +2459,14 @@ main(int argc, char *argv[])
         .enable_lflow_cache = true
     };
 
+    char *ovn_internal_version = ovn_get_internal_version();
+    VLOG_INFO("OVN internal version is : [%s]", ovn_internal_version);
+
     /* Main loop. */
     exiting = false;
     restart = false;
     bool sb_monitor_all = false;
+    bool version_mismatch_with_northd = false;
     while (!exiting) {
         /* If we're paused just run the unixctl server and skip most of the
          * processing loop.
@@ -2441,8 +2476,6 @@ main(int argc, char *argv[])
             unixctl_server_wait(unixctl);
             goto loop_done;
         }
-
-        engine_init_run();
 
         struct ovsdb_idl_txn *ovs_idl_txn = ovsdb_idl_loop_run(&ovs_idl_loop);
         unsigned int new_ovs_cond_seqno
@@ -2473,6 +2506,22 @@ main(int argc, char *argv[])
             ovnsb_cond_seqno = new_ovnsb_cond_seqno;
         }
 
+        bool version_mismatched = false;
+        if (pin_to_northd(ovs_idl_loop.idl)) {
+            version_mismatched = check_northd_version(
+                sbrec_sb_global_first(ovnsb_idl_loop.idl),
+                ovn_internal_version);
+        } else {
+            version_mismatched = false;
+        }
+
+        if (version_mismatch_with_northd != version_mismatched) {
+            version_mismatch_with_northd = version_mismatched;
+            engine_set_force_recompute(true);
+        }
+
+        engine_init_run();
+
         struct engine_context eng_ctx = {
             .ovs_idl_txn = ovs_idl_txn,
             .ovnsb_idl_txn = ovnsb_idl_txn,
@@ -2481,7 +2530,8 @@ main(int argc, char *argv[])
 
         engine_set_context(&eng_ctx);
 
-        if (ovsdb_idl_has_ever_connected(ovnsb_idl_loop.idl)) {
+        if (ovsdb_idl_has_ever_connected(ovnsb_idl_loop.idl) &&
+            !version_mismatch_with_northd) {
             /* Contains the transport zones that this Chassis belongs to */
             struct sset transport_zones = SSET_INITIALIZER(&transport_zones);
             sset_from_delimited_string(&transport_zones,
@@ -2770,6 +2820,7 @@ loop_done:
         }
     }
 
+    free(ovn_internal_version);
     unixctl_server_destroy(unixctl);
     lflow_destroy();
     ofctrl_destroy();
@@ -2822,6 +2873,7 @@ parse_options(int argc, char *argv[])
 
         case 'V':
             ovs_print_version(OFP15_VERSION, OFP15_VERSION);
+            printf("SB DB Schema %s\n", sbrec_get_db_version());
             exit(EXIT_SUCCESS);
 
         VLOG_OPTION_HANDLERS
