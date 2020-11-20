ovsrobot/ovn

commit 5820502a5507481652ed9b3edcadb43b6dacef3d

ovn-nbctl.c: Fix lr-policy-del command
This change will check the existence of lr-policy uuid.
If not, it will print "uuid is not found".

Fixes: 1b030874c32("ovn-nbctl.c: Add an optional way to delete router policy by uuid")

Author: Tao YunXiang <taoyunxiang@cmss.chinamobile.com>
Signed-off-by: Tao YunXiang <taoyunxiang@cmss.chinamobile.com>
CC: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

utilities/ovn-nbctl.c
@@ -3647,6 +3647,11 @@ nbctl_lr_policy_del(struct ctl_context *ctx)
                     new_policies[n_policies++] = lr->policies[i];
                 }
             }
+            if (n_policies == lr->n_policies) {
+                ctl_error(ctx, "Logical router policy uuid is not found.");
+                return;
+            }
+
     /* If match is not specified, delete all routing policies with the
      * specified priority. */
         } else {

commit 9ce446fe5ac712ea585cce43ca097dda3f7b933f

ovn-nbctl: Fix leak of array of new policies.
CC: Tao YunXiang <taoyunxiang@cmss.chinamobile.com>
Fixes: 5820502a5507 ("ovn-nbctl.c: Fix lr-policy-del command")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/ovn-nbctl.c
@@ -3758,6 +3758,7 @@ nbctl_lr_policy_del(struct ctl_context *ctx)
                 if (!shash_find(&ctx->options, "--if-exists")) {
                     ctl_error(ctx, "Logical router policy uuid is not found.");
                 }
+                free(new_policies);
                 return;
             }
 