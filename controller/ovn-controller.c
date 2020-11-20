ovsrobot/ovn

commit 252e1642fb594f414b8faa0e49dc91e6b5f50e4a

ovn-controller: pending_ct_zones should be destroyed
pending_ct_zones in ovn-controller main should be destroyed when exit.

Signed-off-by: xu rong <xu.rong@zte.com.cn>
Acked-by: Miguel Angel Ajo <majopela@redhat.com>
Signed-off-by: Russell Bryant <russell@ovn.org>

controller/ovn-controller.c
@@ -843,6 +843,7 @@ main(int argc, char *argv[])
     pinctrl_destroy();
 
     simap_destroy(&ct_zones);
+    shash_destroy(&pending_ct_zones);
 
     bitmap_free(group_table.group_ids);
     hmap_destroy(&group_table.desired_groups);

commit 9c8765747cd091d54f30ea6a58ad23261f77bf51

ovn-controller: Fix leak of pending ct zones.
shash contains pointers to the data that should be freed.

  Direct leak of 32 byte(s) in 2 object(s) allocated from:
    #0 0x52100f in malloc (/controller/ovn-controller+0x52100f)
    #1 0x752436 in xmalloc /lib/util.c:138:15
    #2 0x5a2f0b in add_pending_ct_zone_entry /controller/ovn-controller.c:548:45
    #3 0x5a2d1d in update_ct_zones /controller/ovn-controller.c:668:9
    #4 0x59d8c6 in en_ct_zones_run /controller/ovn-controller.c:1495:5
    #5 0x5dade4 in engine_run /lib/inc-proc-eng.c:377:9
    #6 0x59adf4 in main /controller/ovn-controller.c
    #7 0x7f0799ef41a2 in __libc_start_main (/lib64/libc.so.6+0x271a2)

CC: xu rong <xu.rong@zte.com.cn>
Fixes: 252e1642fb59 ("ovn-controller: pending_ct_zones should be destroyed")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/ovn-controller.c
@@ -1512,7 +1512,7 @@ en_ct_zones_cleanup(void *data)
     struct ed_type_ct_zones *ct_zones_data = data;
 
     simap_destroy(&ct_zones_data->current);
-    shash_destroy(&ct_zones_data->pending);
+    shash_destroy_free_data(&ct_zones_data->pending);
 }
 
 static void