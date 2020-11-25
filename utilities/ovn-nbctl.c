ovsrobot/ovn

commit 8fb54e16378c1b130d2e0e84bea6e0b80521cae3

ovn-nbctl: Cleanup allocated memory to keep valgrind happy.
Fixes: 58287c369bed ("ovn-nbctl: Initial support for daemon mode.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

utilities/ovn-nbctl.c
@@ -155,13 +155,11 @@ main(int argc, char *argv[])
      * depends on the command line.  So, for now we transform the command line
      * into a parsed form, and figure out what to do with it later.
      */
-    char *args = process_escape_args(argv);
     struct ovs_cmdl_parsed_option *parsed_options;
     size_t n_parsed_options;
     char *error_s = ovs_cmdl_parse_all(argc, argv, get_all_options(),
                                        &parsed_options, &n_parsed_options);
     if (error_s) {
-        free(args);
         ctl_fatal("%s", error_s);
     }
 
@@ -188,7 +186,6 @@ main(int argc, char *argv[])
     bool daemon_mode = false;
     if (get_detach()) {
         if (argc != optind) {
-            free(args);
             ctl_fatal("non-option arguments not supported with --detach "
                       "(use --help for help)");
         }
@@ -211,9 +208,10 @@ main(int argc, char *argv[])
         error = ctl_parse_commands(argc - optind, argv + optind,
                                    &local_options, &commands, &n_commands);
         if (error) {
-            free(args);
             ctl_fatal("%s", error);
         }
+
+        char *args = process_escape_args(argv);
         VLOG(ctl_might_write_to_db(commands, n_commands) ? VLL_INFO : VLL_DBG,
              "Called as %s", args);
 
@@ -221,15 +219,13 @@ main(int argc, char *argv[])
 
         error = run_prerequisites(commands, n_commands, idl);
         if (error) {
-            free(args);
-            ctl_fatal("%s", error);
+            goto cleanup;
         }
 
         error = main_loop(args, commands, n_commands, idl, NULL);
-        if (error) {
-            free(args);
-            ctl_fatal("%s", error);
-        }
+
+cleanup:
+        free(args);
 
         struct ctl_command *c;
         for (c = commands; c < &commands[n_commands]; c++) {
@@ -239,12 +235,14 @@ main(int argc, char *argv[])
             shash_destroy_free_data(&c->options);
         }
         free(commands);
+        if (error) {
+            ctl_fatal("%s", error);
+        }
     }
 
     ovsdb_idl_destroy(idl);
     idl = the_idl = NULL;
 
-    free(args);
     exit(EXIT_SUCCESS);
 }
 

commit 590133be786f503cdd0c8eeb8a0fdf7ffc9959b8

ovn-nbctl: Fix double-free of parsed commands on error path.
Reported-by: Daniel Alvarez <dalvarez@redhat.com>
Fixes: 8fb54e16378c ("ovn-nbctl: Cleanup allocated memory to keep valgrind happy.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/ovn-nbctl.c
@@ -6430,12 +6430,6 @@ out_error:
     the_idl_txn = NULL;
 
     ovsdb_symbol_table_destroy(symtab);
-    for (c = commands; c < &commands[n_commands]; c++) {
-        ds_destroy(&c->output);
-        table_destroy(c->table);
-        free(c->table);
-    }
-
     return error;
 }
 
@@ -6851,17 +6845,18 @@ server_cmd_run(struct unixctl_conn *conn, int argc, const char **argv_,
         } else {
             ds_put_cstr(&output, ds_cstr_ro(&c->output));
         }
-
-        ds_destroy(&c->output);
-        table_destroy(c->table);
-        free(c->table);
     }
     unixctl_command_reply(conn, ds_cstr_ro(&output));
     ds_destroy(&output);
 
 out:
     free(error);
-    for (struct ctl_command *c = commands; c < &commands[n_commands]; c++) {
+
+    struct ctl_command *c;
+    for (c = commands; c < &commands[n_commands]; c++) {
+        ds_destroy(&c->output);
+        table_destroy(c->table);
+        free(c->table);
         shash_destroy_free_data(&c->options);
     }
     free(commands);

tests/ovn-nbctl.at
@@ -1976,6 +1976,18 @@ AT_CHECK([ovn-nbctl list forwarding_group], [0], [])
 
 ])
 
+dnl ---------------------------------------------------------------------
+
+OVN_NBCTL_TEST([ovn_nbctl_negative], [basic negative tests], [
+AT_CHECK([ovn-nbctl --id=@ls create logical_switch name=foo -- \
+          set logical_switch foo1 name=bar],
+         [1], [], [dnl
+ovn-nbctl: no row "foo1" in table Logical_Switch
+])
+])
+
+dnl ---------------------------------------------------------------------
+
 AT_SETUP([ovn-nbctl - daemon retry connection])
 OVN_NBCTL_TEST_START daemon
 AT_CHECK([kill `cat ovsdb-server.pid`])