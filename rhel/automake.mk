f2a698135cf711017c17007571ccbf8733656604
Vladislav Odintsov <odivlad@gmail.com>
2021-01-27 23:28:17 +0530

    rhel: Add systemd-unit for ovsdb-server-ic
    New ovsdb-server-ic.service systemd-unit manages OVN_IC_Northbound and
    OVN_IC_Southbound OVSDB server processes.
    
    Signed-off-by: Vladislav Odintsov <odivlad@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- rhel/automake.mk
+++ rhel/automake.mk
@@ -15,6 +15,7 @@ EXTRA_DIST += \
 	rhel/usr_lib_systemd_system_ovn-controller-vtep.service \
 	rhel/usr_lib_systemd_system_ovn-ic.service \
 	rhel/usr_lib_systemd_system_ovn-northd.service \
+	rhel/usr_lib_systemd_system_ovsdb-server-ic.service \
 	rhel/usr_lib_firewalld_services_ovn-central-firewall-service.xml \
 	rhel/usr_lib_firewalld_services_ovn-host-firewall-service.xml \
 	rhel/usr_share_ovn_scripts_systemd_sysconfig.template
