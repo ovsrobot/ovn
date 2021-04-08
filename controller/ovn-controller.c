ovsrobot/ovn

commit 4b946b366c4ca4157ad24d17598ac67b96503074

controller: Add support for Logical Datapath Groups.
ovn-controller will receive updates from Logical_DP_Group table
and process logical flows accordingly.  Feature is fully backward
compatible since old 'logical_datapath' column kept as is.
It will also be used by nothd to not create datapath groups if there
is ony one datapath in it.

Unfortunately, almost every part of the ovn-controller depends on
fact that there is 1:1 relation between logical flows and logical
datapaths, starting from the logical flow handling and all the way
to deep internals of expression parsing and I-P engine.
So, instead of re-writing everything we're taking a "safe" approach
and just re-factoring a bit to add new 'datapath' arguments to
functions and call them in a loop for all datapaths in a datapath
group.  This might have some performance impact in case datapath
groups are actually used by nothd and there are many datapaths that
are local to this ovn-controller.  However, this imapct might be
compensated by lower number of logical flows in general.
There should be no performance penalty if datapath groups are not
used by northd.

Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Dumitru Ceara <dceara@redhat.com>

controller/ovn-controller.c
@@ -157,6 +157,7 @@ update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
      * the connected logical routers and logical switches. */
     struct ovsdb_idl_condition pb = OVSDB_IDL_CONDITION_INIT(&pb);
     struct ovsdb_idl_condition lf = OVSDB_IDL_CONDITION_INIT(&lf);
+    struct ovsdb_idl_condition ldpg = OVSDB_IDL_CONDITION_INIT(&ldpg);
     struct ovsdb_idl_condition mb = OVSDB_IDL_CONDITION_INIT(&mb);
     struct ovsdb_idl_condition mg = OVSDB_IDL_CONDITION_INIT(&mg);
     struct ovsdb_idl_condition dns = OVSDB_IDL_CONDITION_INIT(&dns);
@@ -168,6 +169,7 @@ update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
     if (monitor_all) {
         ovsdb_idl_condition_add_clause_true(&pb);
         ovsdb_idl_condition_add_clause_true(&lf);
+        ovsdb_idl_condition_add_clause_true(&ldpg);
         ovsdb_idl_condition_add_clause_true(&mb);
         ovsdb_idl_condition_add_clause_true(&mg);
         ovsdb_idl_condition_add_clause_true(&dns);
@@ -231,11 +233,31 @@ update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
             sbrec_port_binding_add_clause_datapath(&pb, OVSDB_F_EQ, uuid);
             sbrec_logical_flow_add_clause_logical_datapath(&lf, OVSDB_F_EQ,
                                                            uuid);
+            sbrec_logical_dp_group_add_clause_datapaths(
+                &ldpg, OVSDB_F_INCLUDES, &uuid, 1);
             sbrec_mac_binding_add_clause_datapath(&mb, OVSDB_F_EQ, uuid);
             sbrec_multicast_group_add_clause_datapath(&mg, OVSDB_F_EQ, uuid);
             sbrec_dns_add_clause_datapaths(&dns, OVSDB_F_INCLUDES, &uuid, 1);
             sbrec_ip_multicast_add_clause_datapath(&ip_mcast, OVSDB_F_EQ,
                                                    uuid);
+        }
+
+        /* Updating conditions to receive logical flows that references
+         * datapath groups containing local datapaths. */
+        const struct sbrec_logical_dp_group *group;
+        SBREC_LOGICAL_DP_GROUP_FOR_EACH (group, ovnsb_idl) {
+            struct uuid *uuid = CONST_CAST(struct uuid *,
+                                           &group->header_.uuid);
+            size_t i;
+
+            for (i = 0; i < group->n_datapaths; i++) {
+                if (get_local_datapath(local_datapaths,
+                                       group->datapaths[i]->tunnel_key)) {
+                    sbrec_logical_flow_add_clause_logical_dp_group(
+                        &lf, OVSDB_F_EQ, uuid);
+                    break;
+                }
+            }
         }
     }
 
@@ -243,6 +265,7 @@ out:;
     unsigned int cond_seqnos[] = {
         sbrec_port_binding_set_condition(ovnsb_idl, &pb),
         sbrec_logical_flow_set_condition(ovnsb_idl, &lf),
+        sbrec_logical_dp_group_set_condition(ovnsb_idl, &ldpg),
         sbrec_mac_binding_set_condition(ovnsb_idl, &mb),
         sbrec_multicast_group_set_condition(ovnsb_idl, &mg),
         sbrec_dns_set_condition(ovnsb_idl, &dns),
@@ -259,6 +282,7 @@ out:;
 
     ovsdb_idl_condition_destroy(&pb);
     ovsdb_idl_condition_destroy(&lf);
+    ovsdb_idl_condition_destroy(&ldpg);
     ovsdb_idl_condition_destroy(&mb);
     ovsdb_idl_condition_destroy(&mg);
     ovsdb_idl_condition_destroy(&dns);
@@ -921,6 +945,7 @@ ctrl_register_ovs_idl(struct ovsdb_idl *ovs_idl)
     SB_NODE(port_group, "port_group") \
     SB_NODE(multicast_group, "multicast_group") \
     SB_NODE(datapath_binding, "datapath_binding") \
+    SB_NODE(logical_dp_group, "logical_dp_group") \
     SB_NODE(port_binding, "port_binding") \
     SB_NODE(mac_binding, "mac_binding") \
     SB_NODE(logical_flow, "logical_flow") \
@@ -1804,6 +1829,11 @@ static void init_lflow_ctx(struct engine_node *node,
                 engine_get_input("SB_logical_flow", node),
                 "logical_datapath");
 
+    struct ovsdb_idl_index *sbrec_logical_flow_by_dp_group =
+        engine_ovsdb_node_get_index(
+                engine_get_input("SB_logical_flow", node),
+                "logical_dp_group");
+
     struct ovsdb_idl_index *sbrec_mc_group_by_name_dp =
         engine_ovsdb_node_get_index(
                 engine_get_input("SB_multicast_group", node),
@@ -1824,6 +1854,10 @@ static void init_lflow_ctx(struct engine_node *node,
     struct sbrec_logical_flow_table *logical_flow_table =
         (struct sbrec_logical_flow_table *)EN_OVSDB_GET(
             engine_get_input("SB_logical_flow", node));
+
+    struct sbrec_logical_dp_group_table *logical_dp_group_table =
+        (struct sbrec_logical_dp_group_table *)EN_OVSDB_GET(
+            engine_get_input("SB_logical_dp_group", node));
 
     struct sbrec_multicast_group_table *multicast_group_table =
         (struct sbrec_multicast_group_table *)EN_OVSDB_GET(
@@ -1857,11 +1891,14 @@ static void init_lflow_ctx(struct engine_node *node,
         sbrec_mc_group_by_name_dp;
     l_ctx_in->sbrec_logical_flow_by_logical_datapath =
         sbrec_logical_flow_by_dp;
+    l_ctx_in->sbrec_logical_flow_by_logical_dp_group =
+        sbrec_logical_flow_by_dp_group;
     l_ctx_in->sbrec_port_binding_by_name = sbrec_port_binding_by_name;
     l_ctx_in->dhcp_options_table  = dhcp_table;
     l_ctx_in->dhcpv6_options_table = dhcpv6_table;
     l_ctx_in->mac_binding_table = mac_binding_table;
     l_ctx_in->logical_flow_table = logical_flow_table;
+    l_ctx_in->logical_dp_group_table = logical_dp_group_table;
     l_ctx_in->mc_group_table = multicast_group_table;
     l_ctx_in->chassis = chassis;
     l_ctx_in->lb_table = lb_table;
@@ -2405,6 +2442,9 @@ main(int argc, char *argv[])
     struct ovsdb_idl_index *sbrec_logical_flow_by_logical_datapath
         = ovsdb_idl_index_create1(ovnsb_idl_loop.idl,
                                   &sbrec_logical_flow_col_logical_datapath);
+    struct ovsdb_idl_index *sbrec_logical_flow_by_logical_dp_group
+        = ovsdb_idl_index_create1(ovnsb_idl_loop.idl,
+                                  &sbrec_logical_flow_col_logical_dp_group);
     struct ovsdb_idl_index *sbrec_port_binding_by_name
         = ovsdb_idl_index_create1(ovnsb_idl_loop.idl,
                                   &sbrec_port_binding_col_logical_port);
@@ -2538,6 +2578,12 @@ main(int argc, char *argv[])
                      flow_output_sb_mac_binding_handler);
     engine_add_input(&en_flow_output, &en_sb_logical_flow,
                      flow_output_sb_logical_flow_handler);
+    /* Using a noop handler since we don't really need any data from datapath
+     * groups or a full recompute.  Update of a datapath group will put
+     * logical flow into the tracked list, so the logical flow handler will
+     * process all changes. */
+    engine_add_input(&en_flow_output, &en_sb_logical_dp_group,
+                     engine_noop_handler);
     engine_add_input(&en_flow_output, &en_sb_dhcp_options, NULL);
     engine_add_input(&en_flow_output, &en_sb_dhcpv6_options, NULL);
     engine_add_input(&en_flow_output, &en_sb_dns, NULL);
@@ -2581,6 +2627,8 @@ main(int argc, char *argv[])
                                 sbrec_multicast_group_by_name_datapath);
     engine_ovsdb_node_add_index(&en_sb_logical_flow, "logical_datapath",
                                 sbrec_logical_flow_by_logical_datapath);
+    engine_ovsdb_node_add_index(&en_sb_logical_flow, "logical_dp_group",
+                                sbrec_logical_flow_by_logical_dp_group);
     engine_ovsdb_node_add_index(&en_sb_port_binding, "name",
                                 sbrec_port_binding_by_name);
     engine_ovsdb_node_add_index(&en_sb_port_binding, "key",
@@ -2820,7 +2868,10 @@ main(int argc, char *argv[])
                                     br_int, chassis,
                                     &runtime_data->local_datapaths,
                                     &runtime_data->active_tunnels);
-                        if (engine_node_changed(&en_runtime_data)) {
+                        /* Updating monitor conditions if runtime data or
+                         * logical datapath goups changed. */
+                        if (engine_node_changed(&en_runtime_data)
+                            || engine_node_changed(&en_sb_logical_dp_group)) {
                             ovnsb_expected_cond_seqno =
                                 update_sb_monitors(
                                     ovnsb_idl_loop.idl, chassis,

controller/lflow.c
@@ -667,6 +667,7 @@ update_conj_id_ofs(uint32_t *conj_id_ofs, uint32_t n_conjs)
 
 static void
 add_matches_to_flow_table(const struct sbrec_logical_flow *lflow,
+                          const struct sbrec_datapath_binding *dp,
                           struct hmap *matches, size_t conj_id_ofs,
                           uint8_t ptable, uint8_t output_ptable,
                           struct ofpbuf *ovnacts,
@@ -677,7 +678,7 @@ add_matches_to_flow_table(const struct sbrec_logical_flow *lflow,
         .sbrec_multicast_group_by_name_datapath
             = l_ctx_in->sbrec_multicast_group_by_name_datapath,
         .sbrec_port_binding_by_name = l_ctx_in->sbrec_port_binding_by_name,
-        .dp = lflow->logical_datapath
+        .dp = dp,
     };
 
     /* Encode OVN logical actions into OpenFlow. */
@@ -687,7 +688,7 @@ add_matches_to_flow_table(const struct sbrec_logical_flow *lflow,
         .lookup_port = lookup_port_cb,
         .tunnel_ofport = tunnel_ofport_cb,
         .aux = &aux,
-        .is_switch = datapath_is_switch(lflow->logical_datapath),
+        .is_switch = datapath_is_switch(dp),
         .group_table = l_ctx_out->group_table,
         .meter_table = l_ctx_out->meter_table,
         .lflow_uuid = lflow->header_.uuid,
@@ -706,17 +707,16 @@ add_matches_to_flow_table(const struct sbrec_logical_flow *lflow,
 
     struct expr_match *m;
     HMAP_FOR_EACH (m, hmap_node, matches) {
-        match_set_metadata(&m->match,
-                           htonll(lflow->logical_datapath->tunnel_key));
+        match_set_metadata(&m->match, htonll(dp->tunnel_key));
         if (m->match.wc.masks.conj_id) {
             m->match.flow.conj_id += conj_id_ofs;
         }
-        if (datapath_is_switch(lflow->logical_datapath)) {
+        if (datapath_is_switch(dp)) {
             unsigned int reg_index
                 = (ingress ? MFF_LOG_INPORT : MFF_LOG_OUTPORT) - MFF_REG0;
             int64_t port_id = m->match.flow.regs[reg_index];
             if (port_id) {
-                int64_t dp_id = lflow->logical_datapath->tunnel_key;
+                int64_t dp_id = dp->tunnel_key;
                 char buf[16];
                 get_unique_lport_key(dp_id, port_id, buf, sizeof(buf));
                 lflow_resource_add(l_ctx_out->lfrr, REF_TYPE_PORTBINDING, buf,
@@ -765,6 +765,7 @@ add_matches_to_flow_table(const struct sbrec_logical_flow *lflow,
  */
 static struct expr *
 convert_match_to_expr(const struct sbrec_logical_flow *lflow,
+                      const struct sbrec_datapath_binding *dp,
                       struct expr *prereqs,
                       const struct shash *addr_sets,
                       const struct shash *port_groups,
@@ -777,8 +778,7 @@ convert_match_to_expr(const struct sbrec_logical_flow *lflow,
 
     struct expr *e = expr_parse_string(lflow->match, &symtab, addr_sets,
                                        port_groups, &addr_sets_ref,
-                                       &port_groups_ref,
-                                       lflow->logical_datapath->tunnel_key,
+                                       &port_groups_ref, dp->tunnel_key,
                                        &error);
     const char *addr_set_name;
     SSET_FOR_EACH (addr_set_name, &addr_sets_ref) {
@@ -816,23 +816,18 @@ convert_match_to_expr(const struct sbrec_logical_flow *lflow,
 }
 
 static bool
-consider_logical_flow(const struct sbrec_logical_flow *lflow,
-                      struct hmap *dhcp_opts, struct hmap *dhcpv6_opts,
-                      struct hmap *nd_ra_opts,
-                      struct controller_event_options *controller_event_opts,
-                      struct lflow_ctx_in *l_ctx_in,
-                      struct lflow_ctx_out *l_ctx_out)
+consider_logical_flow__(const struct sbrec_logical_flow *lflow,
+                        const struct sbrec_datapath_binding *dp,
+                        struct hmap *dhcp_opts, struct hmap *dhcpv6_opts,
+                        struct hmap *nd_ra_opts,
+                        struct controller_event_options *controller_event_opts,
+                        struct lflow_ctx_in *l_ctx_in,
+                        struct lflow_ctx_out *l_ctx_out)
 {
     /* Determine translation of logical table IDs to physical table IDs. */
     bool ingress = !strcmp(lflow->pipeline, "ingress");
 
-    const struct sbrec_datapath_binding *ldp = lflow->logical_datapath;
-    if (!ldp) {
-        VLOG_DBG("lflow "UUID_FMT" has no datapath binding, skip",
-                 UUID_ARGS(&lflow->header_.uuid));
-        return true;
-    }
-    if (!get_local_datapath(l_ctx_in->local_datapaths, ldp->tunnel_key)) {
+    if (!get_local_datapath(l_ctx_in->local_datapaths, dp->tunnel_key)) {
         VLOG_DBG("lflow "UUID_FMT" is not for local datapath, skip",
                  UUID_ARGS(&lflow->header_.uuid));
         return true;
@@ -881,7 +876,7 @@ consider_logical_flow(const struct sbrec_logical_flow *lflow,
         .sbrec_multicast_group_by_name_datapath
             = l_ctx_in->sbrec_multicast_group_by_name_datapath,
         .sbrec_port_binding_by_name = l_ctx_in->sbrec_port_binding_by_name,
-        .dp = lflow->logical_datapath
+        .dp = dp,
     };
     struct condition_aux cond_aux = {
         .sbrec_port_binding_by_name = l_ctx_in->sbrec_port_binding_by_name,
@@ -894,7 +889,7 @@ consider_logical_flow(const struct sbrec_logical_flow *lflow,
     struct expr *expr = NULL;
     if (!l_ctx_out->lflow_cache_map) {
         /* Caching is disabled. */
-        expr = convert_match_to_expr(lflow, prereqs, l_ctx_in->addr_sets,
+        expr = convert_match_to_expr(lflow, dp, prereqs, l_ctx_in->addr_sets,
                                      l_ctx_in->port_groups, l_ctx_out->lfrr,
                                      NULL);
         if (!expr) {
@@ -920,7 +915,7 @@ consider_logical_flow(const struct sbrec_logical_flow *lflow,
             return true;
         }
 
-        add_matches_to_flow_table(lflow, &matches, *l_ctx_out->conj_id_ofs,
+        add_matches_to_flow_table(lflow, dp, &matches, *l_ctx_out->conj_id_ofs,
                                   ptable, output_ptable, &ovnacts, ingress,
                                   l_ctx_in, l_ctx_out);
 
@@ -937,7 +932,7 @@ consider_logical_flow(const struct sbrec_logical_flow *lflow,
     if (lc && lc->type == LCACHE_T_MATCHES) {
         /* 'matches' is cached. No need to do expr parsing.
          * Add matches to flow table and return. */
-        add_matches_to_flow_table(lflow, lc->expr_matches, lc->conj_id_ofs,
+        add_matches_to_flow_table(lflow, dp, lc->expr_matches, lc->conj_id_ofs,
                                   ptable, output_ptable, &ovnacts, ingress,
                                   l_ctx_in, l_ctx_out);
         ovnacts_free(ovnacts.data, ovnacts.size);
@@ -957,7 +952,7 @@ consider_logical_flow(const struct sbrec_logical_flow *lflow,
 
     bool pg_addr_set_ref = false;
     if (!expr) {
-        expr = convert_match_to_expr(lflow, prereqs, l_ctx_in->addr_sets,
+        expr = convert_match_to_expr(lflow, dp, prereqs, l_ctx_in->addr_sets,
                                      l_ctx_in->port_groups, l_ctx_out->lfrr,
                                      &pg_addr_set_ref);
         if (!expr) {
@@ -1015,7 +1010,7 @@ consider_logical_flow(const struct sbrec_logical_flow *lflow,
     }
 
     /* Encode OVN logical actions into OpenFlow. */
-    add_matches_to_flow_table(lflow, matches, lc->conj_id_ofs,
+    add_matches_to_flow_table(lflow, dp, matches, lc->conj_id_ofs,
                               ptable, output_ptable, &ovnacts, ingress,
                               l_ctx_in, l_ctx_out);
     ovnacts_free(ovnacts.data, ovnacts.size);
@@ -1035,6 +1030,42 @@ consider_logical_flow(const struct sbrec_logical_flow *lflow,
     }
 
     return true;
+}
+
+static bool
+consider_logical_flow(const struct sbrec_logical_flow *lflow,
+                      struct hmap *dhcp_opts, struct hmap *dhcpv6_opts,
+                      struct hmap *nd_ra_opts,
+                      struct controller_event_options *controller_event_opts,
+                      struct lflow_ctx_in *l_ctx_in,
+                      struct lflow_ctx_out *l_ctx_out)
+{
+    const struct sbrec_logical_dp_group *dp_group = lflow->logical_dp_group;
+    const struct sbrec_datapath_binding *dp = lflow->logical_datapath;
+    bool ret = true;
+
+    if (!dp_group && !dp) {
+        VLOG_DBG("lflow "UUID_FMT" has no datapath binding, skip",
+                 UUID_ARGS(&lflow->header_.uuid));
+        return true;
+    }
+    ovs_assert(!dp_group || !dp);
+
+    if (dp && !consider_logical_flow__(lflow, dp,
+                                       dhcp_opts, dhcpv6_opts, nd_ra_opts,
+                                       controller_event_opts,
+                                       l_ctx_in, l_ctx_out)) {
+        ret = false;
+    }
+    for (size_t i = 0; dp_group && i < dp_group->n_datapaths; i++) {
+        if (!consider_logical_flow__(lflow, dp_group->datapaths[i],
+                                     dhcp_opts,  dhcpv6_opts, nd_ra_opts,
+                                     controller_event_opts,
+                                     l_ctx_in, l_ctx_out)) {
+            ret = false;
+        }
+    }
+    return ret;
 }
 
 static void
@@ -1432,14 +1463,46 @@ lflow_add_flows_for_datapath(const struct sbrec_datapath_binding *dp,
     const struct sbrec_logical_flow *lflow;
     SBREC_LOGICAL_FLOW_FOR_EACH_EQUAL (
         lflow, lf_row, l_ctx_in->sbrec_logical_flow_by_logical_datapath) {
-        if (!consider_logical_flow(lflow, &dhcp_opts, &dhcpv6_opts,
-                                   &nd_ra_opts, &controller_event_opts,
-                                   l_ctx_in, l_ctx_out)) {
+        if (!consider_logical_flow__(lflow, dp, &dhcp_opts, &dhcpv6_opts,
+                                     &nd_ra_opts, &controller_event_opts,
+                                     l_ctx_in, l_ctx_out)) {
             handled = false;
             l_ctx_out->conj_id_overflow = true;
-            break;
+            goto lflow_processing_end;
         }
     }
+    sbrec_logical_flow_index_destroy_row(lf_row);
+
+    lf_row = sbrec_logical_flow_index_init_row(
+        l_ctx_in->sbrec_logical_flow_by_logical_dp_group);
+    /* There are far fewer datapath groups than logical flows. */
+    const struct sbrec_logical_dp_group *ldpg;
+    SBREC_LOGICAL_DP_GROUP_TABLE_FOR_EACH (ldpg,
+                                           l_ctx_in->logical_dp_group_table) {
+        bool found = false;
+        for (size_t i = 0; i < ldpg->n_datapaths; i++) {
+            if (ldpg->datapaths[i] == dp) {
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            continue;
+        }
+
+        sbrec_logical_flow_index_set_logical_dp_group(lf_row, ldpg);
+        SBREC_LOGICAL_FLOW_FOR_EACH_EQUAL (
+            lflow, lf_row, l_ctx_in->sbrec_logical_flow_by_logical_dp_group) {
+            if (!consider_logical_flow__(lflow, dp, &dhcp_opts, &dhcpv6_opts,
+                                         &nd_ra_opts, &controller_event_opts,
+                                         l_ctx_in, l_ctx_out)) {
+                handled = false;
+                l_ctx_out->conj_id_overflow = true;
+                goto lflow_processing_end;
+            }
+        }
+    }
+lflow_processing_end:
     sbrec_logical_flow_index_destroy_row(lf_row);
 
     dhcp_opts_destroy(&dhcp_opts);

controller/lflow.h
@@ -127,12 +127,14 @@ void lflow_resource_clear(struct lflow_resource_ref *);
 struct lflow_ctx_in {
     struct ovsdb_idl_index *sbrec_multicast_group_by_name_datapath;
     struct ovsdb_idl_index *sbrec_logical_flow_by_logical_datapath;
+    struct ovsdb_idl_index *sbrec_logical_flow_by_logical_dp_group;
     struct ovsdb_idl_index *sbrec_port_binding_by_name;
     const struct sbrec_dhcp_options_table *dhcp_options_table;
     const struct sbrec_dhcpv6_options_table *dhcpv6_options_table;
     const struct sbrec_datapath_binding_table *dp_binding_table;
     const struct sbrec_mac_binding_table *mac_binding_table;
     const struct sbrec_logical_flow_table *logical_flow_table;
+    const struct sbrec_logical_dp_group_table *logical_dp_group_table;
     const struct sbrec_multicast_group_table *mc_group_table;
     const struct sbrec_chassis *chassis;
     const struct sbrec_load_balancer_table *lb_table;

commit a3c3dfdaea2ea5dde07e17198f3bd30d4df1c33a

controller: Monitor all logical flows that refer to datapath groups.
Considering two logical datapaths (DP1 and DP2) and a logical flow that
is shared between the two, ovn-northd will create the following SB
records:

Datapaths: DP1, DP2
Datapath_Group: DPG1 {dps=[DP1, DP2]}
Logical_Flow: LF {dp_group=DPG1}

If an ovn-controller is conditionally monitoring DP1 and DP2 its
monitor conditions look like this:

Datapath_Group table when:
 - Datapath_Group.datapaths in {DP1._uuid, DP2._uuid}
Logical_Flow table when:
 - Logical_Flow.logical_datapath in {DP1._uuid, DP2._uuid}
 - Logical_Flow.logical_dp_group in {DPG1._uuid}

If at this point a new logical datapath DP3 is created (e.g., a LS is
added) and the logical flow LF is shared with this third datapath, then
ovn-northd populates the SB with the following contents, replacing
old DPG1 with DPG1-new:

Datapaths: DP1, DP2, DP3
Datapath_Group: DPG1-new {dps=[DP1, DP2, DP3]}
Logical_Flow: LF {dp_group=DPG1-new}

At this point, the SB ovsdb-server will send the following updates to
ovn-controller, to match the current monitor condition it requested:

Datapaths:
- "insert" DP3

Datapath_Group:
- "delete" DPG1
- "insert" DPG1-new {dps=[DP1, DP2, DP3]}

Logical_Flow:
- "delete" LF

The logical flow is deleted from the client's view of the database
because the monitor condition for Logical_Flow records doesn't include
DPG1-new._uuid.

Now ovn-controller will:
- delete all openflows corresponding to LF, including the ones generated
  for datapaths DP1 and DP2.
- compute new set of monitor conditions and send the monitor_cond_change
  request to the SB:

Datapath_Group table when Datapath_Group.uuid in {DPG1-new._uuid}
Logical_Flow table when:
 - Logical_Flow.logical_datapath in {DP1._uuid, DP2._uuid, DP3._uuid}
 - Logical_Flow.logical_dp_group in {DPG1-new._uuid}

Upon processing this new set of conditions, the SB sends the following
update:
Logical_Flow:
- "insert" LF <--- now LF.logical_dp_group matches the condition.

Finally, ovn-controller will add all openflows that correspond to LF, on
all datapaths, DP1, DP2, DP3.

There is therefore a window in which traffic on DP1 and DP2 might be
dropped because openflows corresponding to LF1 on DP1 and DP2 have been
removed but not yet reinstalled.

To avoid this we now unconditionally monitor all logical flows that
refer to datapath groups.

Fixes: 4b946b366c4c ("controller: Add support for Logical Datapath Groups.")

controller/ovn-controller.c
@@ -259,23 +259,15 @@ update_sb_monitors(struct ovsdb_idl *ovnsb_idl,
                                                    uuid);
         }
 
-        /* Updating conditions to receive logical flows that references
-         * datapath groups containing local datapaths. */
-        const struct sbrec_logical_dp_group *group;
-        SBREC_LOGICAL_DP_GROUP_FOR_EACH (group, ovnsb_idl) {
-            struct uuid *uuid = CONST_CAST(struct uuid *,
-                                           &group->header_.uuid);
-            size_t i;
-
-            for (i = 0; i < group->n_datapaths; i++) {
-                if (get_local_datapath(local_datapaths,
-                                       group->datapaths[i]->tunnel_key)) {
-                    sbrec_logical_flow_add_clause_logical_dp_group(
-                        &lf, OVSDB_F_EQ, uuid);
-                    break;
-                }
-            }
-        }
+        /* Datapath groups are immutable, which means a new group record is
+         * created when a datapath is added to a group.  The logical flows
+         * referencing a datapath group are also updated in such cases but the
+         * new group UUID is not known by ovn-controller until the SB update
+         * is received.  To avoid unnecessarily removing and adding lflows
+         * that reference datapath groups, set the monitor condition to always
+         * request all of them.
+         */
+        sbrec_logical_flow_add_clause_logical_dp_group(&lf, OVSDB_F_NE, NULL);
     }
 
 out:;