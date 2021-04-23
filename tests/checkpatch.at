ovsrobot/ovn

commit 0e77b3bcbfe26286c27bb426a474deace44dd8a4

ovn-northd-ddlog: New implementation of ovn-northd based on ddlog.
This implementation is incremental, meaning that it only recalculates
what is needed for the southbound database when northbound changes
occur.  It is expected to scale better than the C implementation,
for large deployments.  (This may take testing and tuning to be
effective.)

There are three tests that I'm having mysterious trouble getting
to work with DDlog.  For now, I've marked the testsuite to skip
them unless RUN_ANYWAY=yes is set in the environment.

Signed-off-by: Leonid Ryzhyk <lryzhyk@vmware.com>
Co-authored-by: Justin Pettit <jpettit@ovn.org>
Signed-off-by: Justin Pettit <jpettit@ovn.org>
Co-authored-by: Ben Pfaff <blp@ovn.org>
Signed-off-by: Ben Pfaff <blp@ovn.org>
Acked-by: Numan Siddique <numans@ovn.org>

utilities/checkpatch.py
@@ -184,7 +184,7 @@ skip_signoff_check = False
 #
 # Python isn't checked as flake8 performs these checks during build.
 line_length_blacklist = re.compile(
-    r'\.(am|at|etc|in|m4|mk|patch|py)$|debian/rules')
+    r'\.(am|at|etc|in|m4|mk|patch|py|dl)|$|debian/rules')
 
 # Don't enforce a requirement that leading whitespace be all spaces on
 # files that include these characters in their name, since these kinds

Documentation/automake.mk
@@ -20,12 +20,14 @@ DOC_SOURCE = \
 	Documentation/tutorials/ovn-ipsec.rst \
 	Documentation/tutorials/ovn-rbac.rst \
 	Documentation/tutorials/ovn-interconnection.rst \
+	Documentation/tutorials/ddlog-new-feature.rst \
 	Documentation/topics/index.rst \
 	Documentation/topics/testing.rst \
 	Documentation/topics/high-availability.rst \
 	Documentation/topics/integration.rst \
 	Documentation/topics/ovn-news-2.8.rst \
 	Documentation/topics/role-based-access-control.rst \
+	Documentation/topics/debugging-ddlog.rst \
 	Documentation/howto/index.rst \
 	Documentation/howto/docker.rst \
 	Documentation/howto/firewalld.rst \

acinclude.m4
@@ -42,6 +42,49 @@ AC_DEFUN([OVS_ENABLE_WERROR],
    fi
    AC_SUBST([SPARSE_WERROR])])
 
+dnl OVS_CHECK_DDLOG
+dnl
+dnl Configure ddlog source tree
+AC_DEFUN([OVS_CHECK_DDLOG], [
+  AC_ARG_WITH([ddlog],
+              [AC_HELP_STRING([--with-ddlog=.../differential-datalog/lib],
+                              [Enables DDlog by pointing to its library dir])],
+              [DDLOGLIBDIR=$withval], [DDLOGLIBDIR=no])
+
+  AC_MSG_CHECKING([for DDlog library directory])
+  if test "$DDLOGLIBDIR" != no; then
+    if test ! -d "$DDLOGLIBDIR"; then
+      AC_MSG_ERROR([ddlog library dir "$DDLOGLIBDIR" doesn't exist])
+    elif test ! -f "$DDLOGLIBDIR"/ddlog_std.dl; then
+      AC_MSG_ERROR([ddlog library dir "$DDLOGLIBDIR" lacks ddlog_std.dl])
+    fi
+
+    AC_ARG_VAR([DDLOG])
+    AC_CHECK_PROGS([DDLOG], [ddlog], [none])
+    if test X"$DDLOG" = X"none"; then
+        AC_MSG_ERROR([ddlog is required to build with DDlog])
+    fi
+
+    AC_ARG_VAR([CARGO])
+    AC_CHECK_PROGS([CARGO], [cargo], [none])
+    if test X"$CARGO" = X"none"; then
+        AC_MSG_ERROR([cargo is required to build with DDlog])
+    fi
+
+    AC_ARG_VAR([RUSTC])
+    AC_CHECK_PROGS([RUSTC], [rustc], [none])
+    if test X"$RUSTC" = X"none"; then
+        AC_MSG_ERROR([rustc is required to build with DDlog])
+    fi
+
+    AC_SUBST([DDLOGLIBDIR])
+    AC_DEFINE([DDLOG], [1], [Build OVN daemons with ddlog.])
+  fi
+  AC_MSG_RESULT([$DDLOGLIBDIR])
+
+  AM_CONDITIONAL([DDLOG], [test "$DDLOGLIBDIR" != no])
+])
+
 dnl Checks for net/if_dl.h.
 dnl
 dnl (We use this as a proxy for checking whether we're building on FreeBSD

build-aux/automake.mk
@@ -6,6 +6,7 @@ EXTRA_DIST += \
 	build-aux/dpdkstrip.py \
 	build-aux/generate-dhparams-c \
 	build-aux/initial-tab-whitelist \
+	build-aux/libtool-deps \
 	build-aux/sodepends.py \
 	build-aux/soexpand.py \
 	build-aux/text2c \

build-aux/libtool-deps
@@ -1,0 +1,35 @@
+#! /bin/sh
+
+if test $# != 1; then
+    echo "$0: exactly one argument required (use --help for help)" >&2
+    exit 1
+elif test $1 = "--help"; then
+    cat <<EOF
+$0: extracts recursive library dependencies from libtool .la files
+usage: $0 LIBRARY
+where LIBRARY is a libtool .la file
+
+Prints the -l options required by the specified libtool .la library.
+EOF
+    exit 0
+fi
+
+deps=
+parse_dependencies() {
+    dependency_libs=none
+    . "$1" || exit 1
+    if test "$dependency_libs" = none; then
+        echo "$1: not a libtool .la file (use --help for help)" >&2
+        exit 1
+    fi
+
+    for dep in $dependency_libs; do
+        case $dep in
+            *.la) parse_dependencies "$dep" ;;
+            *) deps="$deps $dep" ;;
+        esac
+    done
+}
+
+parse_dependencies "$1"
+echo $deps

configure.ac
@@ -132,6 +132,7 @@ OVS_LIBTOOL_VERSIONS
 OVS_CHECK_CXX
 AX_FUNC_POSIX_MEMALIGN
 OVN_CHECK_UNBOUND
+OVS_CHECK_DDLOG_FAST_BUILD
 
 OVS_CHECK_INCLUDE_NEXT([stdio.h string.h])
 AC_CONFIG_FILES([lib/libovn.sym])
@@ -168,11 +169,15 @@ OVS_CONDITIONAL_CC_OPTION([-Wno-unused-parameter], [HAVE_WNO_UNUSED_PARAMETER])
 OVS_ENABLE_WERROR
 OVS_ENABLE_SPARSE
 
+OVS_CHECK_DDLOG
 OVS_CHECK_PRAGMA_MESSAGE
 OVN_CHECK_OVS
 OVS_CTAGS_IDENTIFIERS
 AC_SUBST([OVS_CFLAGS])
 AC_SUBST([OVS_LDFLAGS])
+AC_SUBST([DDLOG_EXTRA_FLAGS])
+AC_SUBST([DDLOG_EXTRA_RUSTFLAGS])
+AC_SUBST([DDLOG_NORTHD_LIB_ONLY])
 
 AC_SUBST([ovs_srcdir], ['${OVSDIR}'])
 AC_SUBST([ovs_builddir], ['${OVSBUILDDIR}'])

m4/ovn.m4
@@ -576,3 +576,19 @@ AC_DEFUN([OVN_CHECK_UNBOUND],
    fi
    AM_CONDITIONAL([HAVE_UNBOUND], [test "$HAVE_UNBOUND" = yes])
    AC_SUBST([HAVE_UNBOUND])])
+
+dnl Checks for --enable-ddlog-fast-build and updates DDLOG_EXTRA_RUSTFLAGS.
+AC_DEFUN([OVS_CHECK_DDLOG_FAST_BUILD],
+  [AC_ARG_ENABLE(
+     [ddlog_fast_build],
+     [AC_HELP_STRING([--enable-ddlog-fast-build],
+                     [Build ddlog programs faster, but generate slower code])],
+     [case "${enableval}" in
+        (yes) ddlog_fast_build=true ;;
+        (no)  ddlog_fast_build=false ;;
+        (*) AC_MSG_ERROR([bad value ${enableval} for --enable-ddlog-fast-build]) ;;
+      esac],
+     [ddlog_fast_build=false])
+   if $ddlog_fast_build; then
+      DDLOG_EXTRA_RUSTFLAGS="-C opt-level=z"
+   fi])

northd/.gitignore
@@ -1,2 +1,6 @@
 /ovn-northd
+/ovn-northd-ddlog
 /ovn-northd.8
+/OVN_Northbound.dl
+/OVN_Southbound.dl
+/ovn_northd_ddlog/

commit 3e2053b72751f82be32fc9186350ee3c3928c0e9

checkpatch: Re-enforce line length checks.
This check was removed by accident, re-add it.  Also add a test for it.

Fixes: 0e77b3bcbfe2 ("ovn-northd-ddlog: New implementation of ovn-northd based on ddlog.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/checkpatch.py
@@ -184,7 +184,7 @@ skip_signoff_check = False
 #
 # Python isn't checked as flake8 performs these checks during build.
 line_length_blacklist = re.compile(
-    r'\.(am|at|etc|in|m4|mk|patch|py|dl)|$|debian/rules')
+    r'\.(am|at|etc|in|m4|mk|patch|py|dl)$|debian/rules')
 
 # Don't enforce a requirement that leading whitespace be all spaces on
 # files that include these characters in their name, since these kinds

tests/checkpatch.at
@@ -329,3 +329,15 @@ try_checkpatch \
 "
 
 AT_CLEANUP
+
+AT_SETUP([checkpatch - line too long])
+try_checkpatch \
+   "COMMON_PATCH_HEADER
+    +/* This is a very long line.................................................. */
+    " \
+    "WARNING: Line is 80 characters long (recommended limit is 79)
+#8 FILE: A.c:1:
+/* This is a very long line.................................................. */
+"
+
+AT_CLEANUP