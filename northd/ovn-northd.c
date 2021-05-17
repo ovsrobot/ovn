ovsrobot/ovn

commit 3187b9fef124e038e474270a2728fe94bdca8eef

ovn-northd: introduce new allow-stateless ACL verb
For allow-stateless ACLs, bypass connection tracking by avoiding
setting ct hints for matching traffic. Avoid sending all traffic to ct
when a stateful ACL is present.

===

Reusing an existing 'allow' verb for stateless matching would have its
drawbacks, specifically, when it comes to backwards incompatibility of
the new behavior with existing environments. When using "allow" ACLs
in mixed allow/allow-related environment, we still commit "allow"
traffic to conntrack. This unnecessarily hits performance when mixed
ACL action types were used for the same datapath. This is why we
introduce a new action verb to describe stateless behavior.

Another complexity to consider is the fact that with stateless
matching, one would not be able to rely on 'related' magic that
guarantees that reply traffic is passed through. Instead, the user
would have to accurately define matching rules both for request and
reply directions of a protocol session. Specifically, when allowing
ICMP for a specific peer host, one has to define 'allow-stateless'
rules that would match against ip.dst for request direction and ip.src
for reply direction. Other protocols and scenarios will require their
own fine grained matching approaches implemented by the user.

===

For performance measurements, qperf was used. Tests were executed on two
setups:

1) ovn-fake-multinode virtual environment;
2) Supermicro SYS-5039MS-H8TRF 3-node RH-OSP 16.x cluster with 2 compute
   nodes, NIC: "Intel Corporation Ethernet Controller XXV710 for 25GbE
   SFP28 (rev 02)", using fake_nova_driver to avoid qemu overhead.

1) ovn-fake-multinode:

Performance measured between two virtual nodes, two ports
that belong to different LSs connected via router. Using qperf,
performance was measured for UDP, TCP, SCTP protocols (using
<proto>_lat and <proto>_bw tests). The qperf version used:
0.4.9-16.fc31.x86_64.  Each test scenario was executed five times and
averages compared.

northd/ovn-northd.c
@@ -4975,7 +4975,52 @@ skip_port_from_conntrack(struct ovn_datapath *od, struct ovn_port *op,
 }
 
 static void
-build_pre_acls(struct ovn_datapath *od, struct hmap *lflows)
+build_stateless_filter(struct ovn_datapath *od,
+                       const struct nbrec_acl *acl,
+                       struct hmap *lflows)
+{
+    if (!strcmp(acl->direction, "from-lport")) {
+        ovn_lflow_add_with_hint(lflows, od, S_SWITCH_IN_PRE_ACL,
+                                acl->priority + OVN_ACL_PRI_OFFSET,
+                                acl->match,
+                                "next;",
+                                &acl->header_);
+    } else {
+        ovn_lflow_add_with_hint(lflows, od, S_SWITCH_OUT_PRE_ACL,
+                                acl->priority + OVN_ACL_PRI_OFFSET,
+                                acl->match,
+                                "next;",
+                                &acl->header_);
+    }
+}
+
+static void
+build_stateless_filters(struct ovn_datapath *od, struct hmap *port_groups,
+                        struct hmap *lflows)
+{
+    for (size_t i = 0; i < od->nbs->n_acls; i++) {
+        const struct nbrec_acl *acl = od->nbs->acls[i];
+        if (!strcmp(acl->action, "allow-stateless")) {
+            build_stateless_filter(od, acl, lflows);
+        }
+    }
+
+    struct ovn_port_group *pg;
+    HMAP_FOR_EACH (pg, key_node, port_groups) {
+        if (ovn_port_group_ls_find(pg, &od->nbs->header_.uuid)) {
+            for (size_t i = 0; i < pg->nb_pg->n_acls; i++) {
+                const struct nbrec_acl *acl = pg->nb_pg->acls[i];
+                if (!strcmp(acl->action, "allow-stateless")) {
+                    build_stateless_filter(od, acl, lflows);
+                }
+            }
+        }
+    }
+}
+
+static void
+build_pre_acls(struct ovn_datapath *od, struct hmap *port_groups,
+               struct hmap *lflows)
 {
     /* Ingress and Egress Pre-ACL Table (Priority 0): Packets are
      * allowed by default. */
@@ -4988,9 +5033,9 @@ build_pre_acls(struct ovn_datapath *od, struct hmap *lflows)
     ovn_lflow_add(lflows, od, S_SWITCH_OUT_PRE_ACL, 110,
                   "eth.src == $svc_monitor_mac", "next;");
 
-    /* If there are any stateful ACL rules in this datapath, we must
-     * send all IP packets through the conntrack action, which handles
-     * defragmentation, in order to match L4 headers. */
+    /* If there are any stateful ACL rules in this datapath, we may
+     * send IP packets for some (allow) filters through the conntrack action,
+     * which handles defragmentation, in order to match L4 headers. */
     if (od->has_stateful_acl) {
         for (size_t i = 0; i < od->n_router_ports; i++) {
             skip_port_from_conntrack(od, od->router_ports[i],
@@ -5002,6 +5047,8 @@ build_pre_acls(struct ovn_datapath *od, struct hmap *lflows)
                                      S_SWITCH_IN_PRE_ACL, S_SWITCH_OUT_PRE_ACL,
                                      110, lflows);
         }
+
+        build_stateless_filters(od, port_groups, lflows);
 
         /* Ingress and Egress Pre-ACL Table (Priority 110).
          *
@@ -5406,7 +5453,8 @@ build_acl_log(struct ds *actions, const struct nbrec_acl *acl,
     } else if (!strcmp(acl->action, "reject")) {
         ds_put_cstr(actions, "verdict=reject, ");
     } else if (!strcmp(acl->action, "allow")
-        || !strcmp(acl->action, "allow-related")) {
+        || !strcmp(acl->action, "allow-related")
+        || !strcmp(acl->action, "allow-stateless")) {
         ds_put_cstr(actions, "verdict=allow, ");
     }
 
@@ -5465,7 +5513,16 @@ consider_acl(struct hmap *lflows, struct ovn_datapath *od,
     bool ingress = !strcmp(acl->direction, "from-lport") ? true :false;
     enum ovn_stage stage = ingress ? S_SWITCH_IN_ACL : S_SWITCH_OUT_ACL;
 
-    if (!strcmp(acl->action, "allow")
+    if (!strcmp(acl->action, "allow-stateless")) {
+        struct ds actions = DS_EMPTY_INITIALIZER;
+        build_acl_log(&actions, acl, meter_groups);
+        ds_put_cstr(&actions, "next;");
+        ovn_lflow_add_with_hint(lflows, od, stage,
+                                acl->priority + OVN_ACL_PRI_OFFSET,
+                                acl->match, ds_cstr(&actions),
+                                &acl->header_);
+        ds_destroy(&actions);
+    } else if (!strcmp(acl->action, "allow")
         || !strcmp(acl->action, "allow-related")) {
         /* If there are any stateful flows, we must even commit "allow"
          * actions.  This is because, while the initiater's
@@ -6795,7 +6852,7 @@ build_lswitch_lflows_pre_acl_and_acl(struct ovn_datapath *od,
         od->has_stateful_acl = ls_has_stateful_acl(od);
         od->has_lb_vip = ls_has_lb_vip(od);
 
-        build_pre_acls(od, lflows);
+        build_pre_acls(od, port_groups, lflows);
         build_pre_lb(od, lflows, meter_groups, lbs);
         build_pre_stateful(od, lflows);
         build_acl_hints(od, lflows);

northd/ovn_northd.dl
@@ -1822,6 +1822,27 @@ for (&Switch(.ls =ls)) {
          .external_ids     = map_empty())
 }
 
+for (&SwitchACL(.sw = sw@&Switch{.ls = ls}, .acl = &acl, .has_fair_meter = fair_meter)) {
+    if (sw.has_stateful_acl) {
+        if (acl.action == "allow-stateless") {
+            if (acl.direction == "from-lport") {
+                Flow(.logical_datapath = ls._uuid,
+                     .stage            = s_SWITCH_IN_PRE_ACL(),
+                     .priority         = acl.priority + oVN_ACL_PRI_OFFSET(),
+                     .__match          = acl.__match,
+                     .actions          = "next;",
+                     .external_ids     = stage_hint(acl._uuid))
+            } else {
+                Flow(.logical_datapath = ls._uuid,
+                     .stage            = s_SWITCH_OUT_PRE_ACL(),
+                     .priority         = acl.priority + oVN_ACL_PRI_OFFSET(),
+                     .__match          = acl.__match,
+                     .actions          = "next;",
+                     .external_ids     = stage_hint(acl._uuid))
+            }
+        }
+    }
+}
 
 /* If there are any stateful ACL rules in this datapath, we must
  * send all IP packets through the conntrack action, which handles
@@ -2211,6 +2232,9 @@ function build_acl_log(acl: nb::ACL, fair_meter: bool): string =
             "allow-related" -> {
                 strs.push("verdict=allow")
             },
+            "allow-stateless" -> {
+                strs.push("verdict=allow")
+            },
             _ -> ()
         };
         match (acl.meter) {
@@ -2595,6 +2619,13 @@ for (&SwitchACL(.sw = sw@&Switch{.ls = ls}, .acl = &acl, .has_fair_meter = fair_
                  .actions          = "${acl_log}next;",
                  .external_ids     = stage_hint)
         }
+    } else if (acl.action == "allow-stateless") {
+        Flow(.logical_datapath = ls._uuid,
+             .stage            = stage,
+             .priority         = acl.priority + oVN_ACL_PRI_OFFSET(),
+             .__match          = acl.__match,
+             .actions          = "${acl_log}next;",
+             .external_ids     = stage_hint)
     } else if (acl.action == "drop" or acl.action == "reject") {
         /* The implementation of "drop" differs if stateful ACLs are in
          * use for this datapath.  In that case, the actions differ

commit a5a59594d59adc70672973fa6694749f28b81887

Honor ACL direction when omitting ct for stateless
While we *should not* circumvent conntrack when a stateful ACL of higher
priority is present on the switch, we should do so only when
allow-stateless and allow-stateful directions are the same, otherwise we
should still skip conntrack for allow-stateless ACLs.

Fixes: 3187b9fef1 ("ovn-northd: introduce new allow-stateless ACL verb")

Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/lswitch.dl
@@ -135,16 +135,39 @@ LogicalSwitchStatelessACL(ls, acl) :-
     LogicalSwitchACL(ls, acl),
     nb::ACL(._uuid = acl, .action = "allow-stateless").
 
+relation LogicalSwitchStatelessFromACL(ls: uuid, acl: uuid)
+
+LogicalSwitchStatelessFromACL(ls, acl) :-
+    LogicalSwitchStatelessACL(ls, acl),
+    nb::ACL(._uuid = acl, .direction = "from-lport").
+
 // "Pitfalls of projections" in ddlog-new-feature.rst explains why this
 // is an output relation:
-output relation LogicalSwitchHasStatelessACL(ls: uuid, has_stateless_acl: bool)
+output relation LogicalSwitchHasStatelessFromACL(ls: uuid, has_stateless_from_acl: bool)
 
-LogicalSwitchHasStatelessACL(ls, true) :-
-    LogicalSwitchStatelessACL(ls, _).
+LogicalSwitchHasStatelessFromACL(ls, true) :-
+    LogicalSwitchStatelessFromACL(ls, _).
 
-LogicalSwitchHasStatelessACL(ls, false) :-
+LogicalSwitchHasStatelessFromACL(ls, false) :-
     nb::Logical_Switch(._uuid = ls),
-    not LogicalSwitchStatelessACL(ls, _).
+    not LogicalSwitchStatelessFromACL(ls, _).
+
+relation LogicalSwitchStatelessToACL(ls: uuid, acl: uuid)
+
+LogicalSwitchStatelessToACL(ls, acl) :-
+    LogicalSwitchStatelessACL(ls, acl),
+    nb::ACL(._uuid = acl, .direction = "to-lport").
+
+// "Pitfalls of projections" in ddlog-new-feature.rst explains why this
+// is an output relation:
+output relation LogicalSwitchHasStatelessToACL(ls: uuid, has_stateless_to_acl: bool)
+
+LogicalSwitchHasStatelessToACL(ls, true) :-
+    LogicalSwitchStatelessToACL(ls, _).
+
+LogicalSwitchHasStatelessToACL(ls, false) :-
+    nb::Logical_Switch(._uuid = ls),
+    not LogicalSwitchStatelessToACL(ls, _).
 
 // "Pitfalls of projections" in ddlog-new-feature.rst explains why this
 // is an output relation:
@@ -223,18 +246,19 @@ LogicalSwitchHasNonRouterPort(ls, false) :-
 /* Switch relation collects all attributes of a logical switch */
 
 relation &Switch(
-    ls:                nb::Logical_Switch,
-    has_stateful_acl:  bool,
-    has_stateless_acl: bool,
-    has_acls:          bool,
-    has_lb_vip:        bool,
-    has_dns_records:   bool,
-    has_unknown_ports: bool,
-    localnet_ports:    Vec<(uuid, string)>,  // UUID and name of each localnet port.
-    subnet:            Option<(in_addr/*subnet*/, in_addr/*mask*/, bit<32>/*start_ipv4*/, bit<32>/*total_ipv4s*/)>,
-    ipv6_prefix:       Option<in6_addr>,
-    mcast_cfg:         Ref<McastSwitchCfg>,
-    is_vlan_transparent: bool,
+    ls:                     nb::Logical_Switch,
+    has_stateful_acl:       bool,
+    has_stateless_from_acl: bool,
+    has_stateless_to_acl:   bool,
+    has_acls:               bool,
+    has_lb_vip:             bool,
+    has_dns_records:        bool,
+    has_unknown_ports:      bool,
+    localnet_ports:         Vec<(uuid, string)>,  // UUID and name of each localnet port.
+    subnet:                 Option<(in_addr/*subnet*/, in_addr/*mask*/, bit<32>/*start_ipv4*/, bit<32>/*total_ipv4s*/)>,
+    ipv6_prefix:            Option<in6_addr>,
+    mcast_cfg:              Ref<McastSwitchCfg>,
+    is_vlan_transparent:    bool,
 
     /* Does this switch have at least one port with type != "router"? */
     has_non_router_port: bool
@@ -251,22 +275,24 @@ function ipv6_parse_prefix(s: string): Option<in6_addr> {
     }
 }
 
-&Switch(.ls                = ls,
-        .has_stateful_acl  = has_stateful_acl,
-        .has_stateless_acl = has_stateless_acl,
-        .has_acls          = has_acls,
-        .has_lb_vip        = has_lb_vip,
-        .has_dns_records   = has_dns_records,
-        .has_unknown_ports = has_unknown_ports,
-        .localnet_ports    = localnet_ports,
-        .subnet            = subnet,
-        .ipv6_prefix       = ipv6_prefix,
-        .mcast_cfg         = mcast_cfg,
-        .has_non_router_port = has_non_router_port,
-        .is_vlan_transparent = is_vlan_transparent) :-
+&Switch(.ls                     = ls,
+        .has_stateful_acl       = has_stateful_acl,
+        .has_stateless_from_acl = has_stateless_from_acl,
+        .has_stateless_to_acl   = has_stateless_to_acl,
+        .has_acls               = has_acls,
+        .has_lb_vip             = has_lb_vip,
+        .has_dns_records        = has_dns_records,
+        .has_unknown_ports      = has_unknown_ports,
+        .localnet_ports         = localnet_ports,
+        .subnet                 = subnet,
+        .ipv6_prefix            = ipv6_prefix,
+        .mcast_cfg              = mcast_cfg,
+        .has_non_router_port    = has_non_router_port,
+        .is_vlan_transparent    = is_vlan_transparent) :-
     nb::Logical_Switch[ls],
     LogicalSwitchHasStatefulACL(ls._uuid, has_stateful_acl),
-    LogicalSwitchHasStatelessACL(ls._uuid, has_stateless_acl),
+    LogicalSwitchHasStatelessFromACL(ls._uuid, has_stateless_from_acl),
+    LogicalSwitchHasStatelessToACL(ls._uuid, has_stateless_to_acl),
     LogicalSwitchHasACLs(ls._uuid, has_acls),
     LogicalSwitchHasLBVIP(ls._uuid, has_lb_vip),
     LogicalSwitchHasDNSRecords(ls._uuid, has_dns_records),

northd/ovn-northd.c
@@ -627,6 +627,8 @@ struct ovn_datapath {
     uint32_t port_key_hint;
 
     bool has_stateful_acl;
+    bool has_stateless_from;
+    bool has_stateless_to;
     bool has_lb_vip;
     bool has_unknown;
     bool has_acls;
@@ -4759,19 +4761,46 @@ ovn_ls_port_group_destroy(struct hmap *nb_pgs)
     hmap_destroy(nb_pgs);
 }
 
+static bool
+ls_get_acl_flags_for_acl(struct ovn_datapath *od,
+                         const struct nbrec_acl *acl)
+{
+    if (!strcmp(acl->action, "allow-related")) {
+        od->has_stateful_acl = true;
+        if (od->has_stateless_to && od->has_stateless_from) {
+            return true;
+        }
+    }
+    if (!strcmp(acl->action, "allow-stateless")) {
+        if (!strcmp(acl->direction, "from-lport")) {
+            od->has_stateless_from = true;
+            if (od->has_stateful_acl && od->has_stateless_to) {
+                return true;
+            }
+        } else {
+            od->has_stateless_to = true;
+            if (od->has_stateful_acl && od->has_stateless_from) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 static void
 ls_get_acl_flags(struct ovn_datapath *od)
 {
     od->has_acls = false;
     od->has_stateful_acl = false;
+    od->has_stateless_from = false;
+    od->has_stateless_to = false;
 
     if (od->nbs->n_acls) {
         od->has_acls = true;
 
         for (size_t i = 0; i < od->nbs->n_acls; i++) {
             struct nbrec_acl *acl = od->nbs->acls[i];
-            if (!strcmp(acl->action, "allow-related")) {
-                od->has_stateful_acl = true;
+            if (ls_get_acl_flags_for_acl(od, acl)) {
                 return;
             }
         }
@@ -4784,8 +4813,7 @@ ls_get_acl_flags(struct ovn_datapath *od)
 
             for (size_t i = 0; i < ls_pg->nb_pg->n_acls; i++) {
                 struct nbrec_acl *acl = ls_pg->nb_pg->acls[i];
-                if (!strcmp(acl->action, "allow-related")) {
-                    od->has_stateful_acl = true;
+                if (ls_get_acl_flags_for_acl(od, acl)) {
                     return;
                 }
             }
@@ -4984,17 +5012,20 @@ skip_port_from_conntrack(struct ovn_datapath *od, struct ovn_port *op,
 }
 
 static bool
-apply_to_each_acl_of_action(struct ovn_datapath *od,
-                            const struct hmap *port_groups,
-                            struct hmap *lflows, const char *action,
-                            void (*func)(struct ovn_datapath *,
-                                         const struct nbrec_acl *,
-                                         struct hmap *))
+apply_to_each_acl_of_action_and_direction(
+        struct ovn_datapath *od,
+        const struct hmap *port_groups,
+        struct hmap *lflows,
+        const char *action, const char *direction,
+        void (*func)(struct ovn_datapath *,
+                     const struct nbrec_acl *,
+                     struct hmap *))
 {
     bool applied = false;
     for (size_t i = 0; i < od->nbs->n_acls; i++) {
         const struct nbrec_acl *acl = od->nbs->acls[i];
-        if (!strcmp(acl->action, action)) {
+        if (!strcmp(acl->action, action) &&
+                (!direction || !strcmp(acl->direction, direction))) {
             func(od, acl, lflows);
             applied = true;
         }
@@ -5005,7 +5036,8 @@ apply_to_each_acl_of_action(struct ovn_datapath *od,
         if (ovn_port_group_ls_find(pg, &od->nbs->header_.uuid)) {
             for (size_t i = 0; i < pg->nb_pg->n_acls; i++) {
                 const struct nbrec_acl *acl = pg->nb_pg->acls[i];
-                if (!strcmp(acl->action, action)) {
+                if (!strcmp(acl->action, action) &&
+                        (!direction || !strcmp(acl->direction, direction))) {
                     func(od, acl, lflows);
                     applied = true;
                 }
@@ -5040,8 +5072,9 @@ build_stateless_filters(struct ovn_datapath *od,
                         const struct hmap *port_groups,
                         struct hmap *lflows)
 {
-    return apply_to_each_acl_of_action(
-        od, port_groups, lflows, "allow-stateless", build_stateless_filter);
+    return apply_to_each_acl_of_action_and_direction(
+        od, port_groups, lflows, "allow-stateless", NULL,
+        build_stateless_filter);
 }
 
 static void
@@ -5069,16 +5102,32 @@ build_stateful_override_filter(struct ovn_datapath *od,
 }
 
 static void
+build_stateful_override_filters_for_direction(struct ovn_datapath *od,
+                                              const struct hmap *port_groups,
+                                              struct hmap *lflows,
+                                              const char *direction)
+{
+    apply_to_each_acl_of_action_and_direction(
+        od, port_groups, lflows, "allow-related", direction,
+        build_stateful_override_filter);
+    apply_to_each_acl_of_action_and_direction(
+        od, port_groups, lflows, "allow", direction,
+        build_stateful_override_filter);
+}
+
+static void
 build_stateful_override_filters(struct ovn_datapath *od,
                                 const struct hmap *port_groups,
                                 struct hmap *lflows)
 {
-    apply_to_each_acl_of_action(
-        od, port_groups, lflows, "allow-related",
-        build_stateful_override_filter);
-    apply_to_each_acl_of_action(
-        od, port_groups, lflows, "allow",
-        build_stateful_override_filter);
+    if (od->has_stateless_from) {
+        build_stateful_override_filters_for_direction(
+            od, port_groups, lflows, "from-lport");
+    }
+    if (od->has_stateless_to) {
+        build_stateful_override_filters_for_direction(
+            od, port_groups, lflows, "to-lport");
+    }
 }
 
 static void

northd/ovn_northd.dl
@@ -1845,23 +1845,21 @@ for (&SwitchACL(.sw = sw@&Switch{.ls = ls}, .acl = &acl, .has_fair_meter = _)) {
 }
 
 for (&SwitchACL(.sw = sw@&Switch{.ls = ls}, .acl = &acl, .has_fair_meter = _)) {
-    if (sw.has_stateless_acl) {
-        if ((sw.has_stateful_acl and acl.action == "allow") or acl.action == "allow-related") {
-            if (acl.direction == "from-lport") {
-                Flow(.logical_datapath = ls._uuid,
-                     .stage            = s_SWITCH_IN_PRE_ACL(),
-                     .priority         = acl.priority + oVN_ACL_PRI_OFFSET(),
-                     .__match          = "ip && ${acl.__match}",
-                     .actions          = "${rEGBIT_CONNTRACK_DEFRAG()} = 1; next;",
-                     .external_ids     = stage_hint(acl._uuid))
-            } else {
-                Flow(.logical_datapath = ls._uuid,
-                     .stage            = s_SWITCH_OUT_PRE_ACL(),
-                     .priority         = acl.priority + oVN_ACL_PRI_OFFSET(),
-                     .__match          = "ip && ${acl.__match}",
-                     .actions          = "${rEGBIT_CONNTRACK_DEFRAG()} = 1; next;",
-                     .external_ids     = stage_hint(acl._uuid))
-            }
+    if ((sw.has_stateful_acl and acl.action == "allow") or acl.action == "allow-related") {
+        if (sw.has_stateless_from_acl and acl.direction == "from-lport") {
+            Flow(.logical_datapath = ls._uuid,
+                 .stage            = s_SWITCH_IN_PRE_ACL(),
+                 .priority         = acl.priority + oVN_ACL_PRI_OFFSET(),
+                 .__match          = "ip && ${acl.__match}",
+                 .actions          = "${rEGBIT_CONNTRACK_DEFRAG()} = 1; next;",
+                 .external_ids     = stage_hint(acl._uuid))
+        } else if (sw.has_stateless_to_acl) {
+            Flow(.logical_datapath = ls._uuid,
+                 .stage            = s_SWITCH_OUT_PRE_ACL(),
+                 .priority         = acl.priority + oVN_ACL_PRI_OFFSET(),
+                 .__match          = "ip && ${acl.__match}",
+                 .actions          = "${rEGBIT_CONNTRACK_DEFRAG()} = 1; next;",
+                 .external_ids     = stage_hint(acl._uuid))
         }
     }
 }

tests/ovn-northd.at
@@ -2683,6 +2683,78 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn -- ACL priority: allow-stateless vs allow-related of different direction])
+ovn_start
+
+# Create two switches to validate direction. We can't use the same switch for
+# both ports, otherwise both in and out pipelines are triggered.
+ovn-nbctl lr-add r0
+for i in $(seq 1 2); do
+    check ovn-nbctl --wait=sb ls-add sw$i
+
+    check ovn-nbctl --wait=sb lrp-add r0 r0-sw$i f0:00:00:00:00:0$i 192.168.$i.1/24
+    check ovn-nbctl --wait=sb lsp-add sw$i sw$i-r0
+    check ovn-nbctl --wait=sb lsp-set-type sw$i-r0 router
+    check ovn-nbctl --wait=sb lsp-set-options sw$i-r0 router-port=r0-sw$i
+    check ovn-nbctl --wait=sb lsp-set-addresses sw$i-r0 router
+
+    check ovn-nbctl --wait=sb lsp-add sw$i lsp$i
+    check ovn-nbctl --wait=sb lsp-set-addresses lsp$i "fe:00:00:00:00:0$i 192.168.$i.100/24"
+done
+
+ovn-nbctl acl-add sw1 from-lport 3 tcp allow-stateless
+ovn-nbctl acl-add sw1 to-lport 4 tcp allow-related
+ovn-nbctl --wait=sb sync
+
+flow_eth='eth.src == fe:00:00:00:00:01 && eth.dst == f0:00:00:00:00:01'
+flow_ip='ip.ttl==64 && ip4.src == 192.168.1.100 && ip4.dst == 192.168.2.100'
+flow_tcp='tcp && tcp.dst == 80'
+flow_udp='udp && udp.dst == 80'
+lsp1_inport=$(fetch_column Port_Binding tunnel_key logical_port=lsp1)
+
+# TCP packets should not go to conntrack because allow-related direction is different.
+flow="inport == \"lsp1\" && ${flow_eth} && ${flow_ip} && ${flow_tcp}"
+AT_CHECK_UNQUOTED([ovn-trace --minimal sw1 "${flow}"], [0], [dnl
+# tcp,reg14=0x2,vlan_tci=0x0000,dl_src=fe:00:00:00:00:01,dl_dst=f0:00:00:00:00:01,nw_src=192.168.1.100,nw_dst=192.168.2.100,nw_tos=0,nw_ecn=0,nw_ttl=64,tp_src=0,tp_dst=80,tcp_flags=0
+ip.ttl--;
+eth.src = f0:00:00:00:00:02;
+eth.dst = fe:00:00:00:00:02;
+output("lsp2");
+])
+
+# Add allow-related with the same direction.
+ovn-nbctl acl-add sw1 from-lport 4 tcp allow-related
+ovn-nbctl --wait=sb sync
+
+# TCP packets should now go to conntrack.
+AT_CHECK_UNQUOTED([ovn-trace --ct new --ct new --minimal sw1 "${flow}"], [0], [dnl
+# tcp,reg14=0x2,vlan_tci=0x0000,dl_src=fe:00:00:00:00:01,dl_dst=f0:00:00:00:00:01,nw_src=192.168.1.100,nw_dst=192.168.2.100,nw_tos=0,nw_ecn=0,nw_ttl=64,tp_src=0,tp_dst=80,tcp_flags=0
+ct_next(ct_state=new|trk) {
+    ip.ttl--;
+    eth.src = f0:00:00:00:00:02;
+    eth.dst = fe:00:00:00:00:02;
+    output("lsp2");
+};
+])
+
+# Reduce priority for allow-related rule of the same direction.
+ovn-nbctl acl-del sw1 from-lport 4 tcp
+ovn-nbctl acl-add sw1 from-lport 2 tcp allow-related
+ovn-nbctl --wait=sb sync
+
+# TCP packets should no longer go to conntrack
+AT_CHECK_UNQUOTED([ovn-trace --ct new --ct new --minimal sw1 "${flow}"], [0], [dnl
+# tcp,reg14=0x2,vlan_tci=0x0000,dl_src=fe:00:00:00:00:01,dl_dst=f0:00:00:00:00:01,nw_src=192.168.1.100,nw_dst=192.168.2.100,nw_tos=0,nw_ecn=0,nw_ttl=64,tp_src=0,tp_dst=80,tcp_flags=0
+ip.ttl--;
+eth.src = f0:00:00:00:00:02;
+eth.dst = fe:00:00:00:00:02;
+output("lsp2");
+])
+
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD([
 AT_SETUP([ovn -- ACL priority: allow-stateless vs allow-related])
 ovn_start
 