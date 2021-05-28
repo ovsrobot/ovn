1c2c7db33224a63fc756ff16e5e33330ad514cc0
Vladislav Odintsov <odivlad@gmail.com>
2021-05-28 14:31:52 +0300

    northd: Add support for DHCP Option 12 (Hostname)
    DHCP Option Hostname is a per-Logical_Switch_Port property, configured in
    Logical_Switch_Port's options:hostname field. It is used if DHCPv4 is
    enabled for this LSP.
    
    Signed-off-by: Vladislav Odintsov <odivlad@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/ovn-northd.c
+++ northd/ovn-northd.c
@@ -4579,6 +4579,14 @@ build_dhcpv4_action(struct ovn_port *op, ovs_be32 offer_ip,
                   REGBIT_DHCP_OPTS_RESULT" = put_dhcp_opts(offerip = "
                   IP_FMT", ", IP_ARGS(offer_ip));
 
+    /* hostname is a per Logical_Switch_Port dhcp option,
+     * so try to get it from ovn_port and put it to options_action
+     * if it exists. */
+    const char *hostname = smap_get(&op->nbsp->options, "hostname");
+    if (hostname) {
+        ds_put_format(options_action, "%s = %s, ", "hostname", hostname);
+    }
+
     /* We're not using SMAP_FOR_EACH because we want a consistent order of the
      * options on different architectures (big or little endian, SSE4.2) */
     const struct smap_node **sorted_opts = smap_sort(&dhcpv4_options);
@@ -13566,6 +13574,7 @@ static struct gen_opts_map supported_dhcp_opts[] = {
     DHCP_OPT_BOOTFILE,
     DHCP_OPT_PATH_PREFIX,
     DHCP_OPT_TFTP_SERVER_ADDRESS,
+    DHCP_OPT_HOSTNAME,
     DHCP_OPT_DOMAIN_NAME,
     DHCP_OPT_ARP_CACHE_TIMEOUT,
     DHCP_OPT_TCP_KEEPALIVE_INTERVAL,
