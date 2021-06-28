ovsrobot/ovn

commit 37ad427cfb78a9e59d7e7ef249b2f2b3ac68c65a

northd: Port dummy-numa cli option from OVS
Signed-off-by: Fabrizio D'Angelo <fdangelo@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/ovn-northd-ddlog.c
@@ -29,6 +29,7 @@
 #include "memory.h"
 #include "openvswitch/hmap.h"
 #include "openvswitch/json.h"
+#include "openvswitch/ovs-numa.h"
 #include "openvswitch/poll-loop.h"
 #include "openvswitch/vlog.h"
 #include "ovsdb-cs.h"
@@ -1080,7 +1081,8 @@ parse_options(int argc OVS_UNUSED, char *argv[] OVS_UNUSED,
         VLOG_OPTION_ENUMS,
         SSL_OPTION_ENUMS,
         OPT_DRY_RUN,
-        OPT_DDLOG_RECORD
+        OPT_DDLOG_RECORD,
+        OPT_DUMMY_NUMA,
     };
     static const struct option long_options[] = {
         {"ovnsb-db", required_argument, NULL, 'd'},
@@ -1094,6 +1096,7 @@ parse_options(int argc OVS_UNUSED, char *argv[] OVS_UNUSED,
         OVN_DAEMON_LONG_OPTIONS,
         VLOG_LONG_OPTIONS,
         STREAM_SSL_LONG_OPTIONS,
+        {"dummy-numa", required_argument, NULL, OPT_DUMMY_NUMA},
         {NULL, 0, NULL, 0},
     };
     char *short_options = ovs_cmdl_long_options_to_short_options(long_options);
@@ -1148,6 +1151,10 @@ parse_options(int argc OVS_UNUSED, char *argv[] OVS_UNUSED,
 
         case OPT_DRY_RUN:
             *pause = true;
+            break;
+
+        case OPT_DUMMY_NUMA:
+            ovs_numa_set_dummy(optarg);
             break;
 
         case OPT_DDLOG_RECORD:

northd/ovn-northd.c
@@ -39,6 +39,7 @@
 #include "lib/ovn-util.h"
 #include "lib/lb.h"
 #include "memory.h"
+#include "ovs-numa.h"
 #include "lib/ovn-parallel-hmap.h"
 #include "ovn/actions.h"
 #include "ovn/features.h"
@@ -14022,6 +14023,7 @@ parse_options(int argc OVS_UNUSED, char *argv[] OVS_UNUSED,
         VLOG_OPTION_ENUMS,
         SSL_OPTION_ENUMS,
         OPT_DRY_RUN,
+        OPT_DUMMY_NUMA,
     };
     static const struct option long_options[] = {
         {"ovnsb-db", required_argument, NULL, 'd'},
@@ -14031,6 +14033,7 @@ parse_options(int argc OVS_UNUSED, char *argv[] OVS_UNUSED,
         {"options", no_argument, NULL, 'o'},
         {"version", no_argument, NULL, 'V'},
         {"dry-run", no_argument, NULL, OPT_DRY_RUN},
+        {"dummy-numa", required_argument, NULL, OPT_DUMMY_NUMA},
         OVN_DAEMON_LONG_OPTIONS,
         VLOG_LONG_OPTIONS,
         STREAM_SSL_LONG_OPTIONS,
@@ -14085,6 +14088,10 @@ parse_options(int argc OVS_UNUSED, char *argv[] OVS_UNUSED,
         case 'V':
             ovn_print_version(0, 0);
             exit(EXIT_SUCCESS);
+
+        case OPT_DUMMY_NUMA:
+            ovs_numa_set_dummy(optarg);
+            break;
 
         case OPT_DRY_RUN:
             *paused = true;

commit 9471025f1e8fe8eb795ca0d82acafb4fb7788926

ovn-northd-ddlog: Fix build errror due to wrong ovs-numa.h path.
Fixes: 37ad427cfb78 ("northd: Port dummy-numa cli option from OVS")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/ovn-northd-ddlog.c
@@ -29,9 +29,9 @@
 #include "memory.h"
 #include "openvswitch/hmap.h"
 #include "openvswitch/json.h"
-#include "openvswitch/ovs-numa.h"
 #include "openvswitch/poll-loop.h"
 #include "openvswitch/vlog.h"
+#include "ovs-numa.h"
 #include "ovsdb-cs.h"
 #include "ovsdb-data.h"
 #include "ovsdb-error.h"