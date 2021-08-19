ovsrobot/ovn

commit be3a60f8e6a3a01e08ae6fe760279dc4f274562a

ovn-nbctl: Deal with nb_cfg overflows.
Reported-at: https://bugzilla.redhat.com/id=1873455
Reported-by: Ying Xu <yinxu@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

utilities/ovn-nbctl.c
@@ -6081,6 +6081,11 @@ do_nbctl(const char *args, struct ctl_command *commands, size_t n_commands,
         nb = nbrec_nb_global_insert(txn);
     }
 
+    /* Deal with potential overflows. */
+    if (nb->nb_cfg == LLONG_MAX) {
+        nbrec_nb_global_set_nb_cfg(nb, 0);
+    }
+
     if (wait_type != NBCTL_WAIT_NONE) {
         ovsdb_idl_txn_increment(txn, &nb->header_, &nbrec_nb_global_col_nb_cfg,
                                 force_wait);

commit 17fda9202fe832b6aa8a513f67b237ba1a155cb2

ovn-nbctl: Monitor nb_cfg to detect and handle potential overflows
ovn-nbctl sync command will increase nb_cfg value each time it is executed
with a specific wait_type, this increment will be handled without testing
the current nb_cfg value because it is not monitored and that could lead
to an overflow issue if nb_cfg == LLONG_MAX.

To avoid such potential overflow cases we must monitor the real value
of nb_cfg each time sync is executed and if there is any overflow issue
it will be handled by function nbctl_pre_execute later on the execution.

Fixes: be3a60f8e6a3 ("ovn-nbctl: Deal with nb_cfg overflows.")
Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=1979774
Signed-off-by: Mohammad Heib <mheib@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/ovn-nbctl.c
@@ -830,6 +830,8 @@ static void
 nbctl_pre_sync(struct ctl_context *base OVS_UNUSED)
 {
     force_wait = true;
+    /* Monitor nb_cfg to detect and handle potential overflows. */
+    ovsdb_idl_add_column(base->idl, &nbrec_nb_global_col_nb_cfg);
 }
 
 static void