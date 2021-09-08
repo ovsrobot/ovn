ovsrobot/ovn

commit b07f1bc3d068e231e40840b0d977bd57158987fd

Add VXLAN support for non-VTEP datapath bindings
Because of limited space in VXLAN VNI to pass over all three of -
datapath id, ingress port, egress port - the implementation ignores
ingress; and splits the remaining 24 bits of VNI into two chunks, 12
bits each - one for datapath and one for egress port.

This also bumps priority for ramp switch flows to 110 to prioritize
them over regular non-ramp VXLAN flows.

Limitations: because ingress port is not passed, ACLs that rely on it
won't work with VXLAN; reduced number of networks and ports per
network (max 4096 for both).

NB consumers may use NB_Global options:max_tunid to determine maximum
capacity for logical switches supported by the setup.

Renamed MLF_RCV_FROM_VXLAN_BIT into MLF_RCV_FROM_RAMP_BIT to reflect
the new use case.

Added test scenarios that ping through VXLAN tunnel between two
hypervisors added. Also max_tunid is validated.

Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/ovn-northd.c
@@ -64,6 +64,7 @@ struct northd_context {
     struct ovsdb_idl *ovnsb_idl;
     struct ovsdb_idl_txn *ovnnb_txn;
     struct ovsdb_idl_txn *ovnsb_txn;
+    struct ovsdb_idl_index *sbrec_chassis_by_name;
     struct ovsdb_idl_index *sbrec_ha_chassis_grp_by_name;
     struct ovsdb_idl_index *sbrec_mcast_group_by_name_dp;
     struct ovsdb_idl_index *sbrec_ip_mcast_by_dp;
@@ -1189,12 +1190,36 @@ join_datapaths(struct northd_context *ctx, struct hmap *datapaths,
     }
 }
 
+static bool
+is_vxlan_mode(struct ovsdb_idl *ovnsb_idl)
+{
+    const struct sbrec_chassis *chassis;
+    SBREC_CHASSIS_FOR_EACH (chassis, ovnsb_idl) {
+        for (int i = 0; i < chassis->n_encaps; i++) {
+            if (!strcmp(chassis->encaps[i]->type, "vxlan")) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
 static uint32_t
-ovn_datapath_allocate_key(struct hmap *dp_tnlids)
+get_ovn_max_dp_key_local(struct northd_context *ctx)
+{
+    if (is_vxlan_mode(ctx->ovnsb_idl)) {
+        /* OVN_MAX_DP_GLOBAL_NUM doesn't apply for vxlan mode. */
+        return OVN_MAX_DP_VXLAN_KEY;
+    }
+    return OVN_MAX_DP_KEY - OVN_MAX_DP_GLOBAL_NUM;
+}
+
+static uint32_t
+ovn_datapath_allocate_key(struct northd_context *ctx, struct hmap *dp_tnlids)
 {
     static uint32_t hint;
     return ovn_allocate_tnlid(dp_tnlids, "datapath", OVN_MIN_DP_KEY_LOCAL,
-                              OVN_MAX_DP_KEY_LOCAL, &hint);
+                              get_ovn_max_dp_key_local(ctx), &hint);
 }
 
 /* Updates the southbound Datapath_Binding table so that it contains the
@@ -1237,7 +1262,7 @@ build_datapaths(struct northd_context *ctx, struct hmap *datapaths,
             }
         }
         if (!tunnel_key) {
-            tunnel_key = ovn_datapath_allocate_key(&dp_tnlids);
+            tunnel_key = ovn_datapath_allocate_key(ctx, &dp_tnlids);
             if (!tunnel_key) {
                 break;
             }
@@ -12110,32 +12135,34 @@ ovnnb_db_run(struct northd_context *ctx,
         }
     }
 
-    if (!mac_addr_prefix || !monitor_mac) {
-        struct smap options;
-        smap_clone(&options, &nb->options);
+    struct smap options;
+    smap_clone(&options, &nb->options);
 
-        if (!mac_addr_prefix) {
-            eth_addr_random(&mac_prefix);
-            memset(&mac_prefix.ea[3], 0, 3);
+    if (!mac_addr_prefix) {
+        eth_addr_random(&mac_prefix);
+        memset(&mac_prefix.ea[3], 0, 3);
 
-            smap_add_format(&options, "mac_prefix",
-                            "%02"PRIx8":%02"PRIx8":%02"PRIx8,
-                            mac_prefix.ea[0], mac_prefix.ea[1],
-                            mac_prefix.ea[2]);
-        }
-
-        if (!monitor_mac) {
-            eth_addr_random(&svc_monitor_mac_ea);
-            snprintf(svc_monitor_mac, sizeof svc_monitor_mac,
-                     ETH_ADDR_FMT, ETH_ADDR_ARGS(svc_monitor_mac_ea));
-            smap_replace(&options, "svc_monitor_mac", svc_monitor_mac);
-        }
-
-        nbrec_nb_global_verify_options(nb);
-        nbrec_nb_global_set_options(nb, &options);
-
-        smap_destroy(&options);
+        smap_add_format(&options, "mac_prefix",
+                        "%02"PRIx8":%02"PRIx8":%02"PRIx8,
+                        mac_prefix.ea[0], mac_prefix.ea[1],
+                        mac_prefix.ea[2]);
     }
+
+    if (!monitor_mac) {
+        eth_addr_random(&svc_monitor_mac_ea);
+        snprintf(svc_monitor_mac, sizeof svc_monitor_mac,
+                 ETH_ADDR_FMT, ETH_ADDR_ARGS(svc_monitor_mac_ea));
+        smap_replace(&options, "svc_monitor_mac", svc_monitor_mac);
+    }
+
+    char *max_tunid = xasprintf("%d", get_ovn_max_dp_key_local(ctx));
+    smap_replace(&options, "max_tunid", max_tunid);
+    free(max_tunid);
+
+    nbrec_nb_global_verify_options(nb);
+    nbrec_nb_global_set_options(nb, &options);
+
+    smap_destroy(&options);
 
     /* Update the probe interval. */
     northd_probe_interval_nb = get_probe_interval(ovnnb_db, nb);
@@ -13009,6 +13036,10 @@ main(int argc, char *argv[])
     ovsdb_idl_add_table(ovnsb_idl_loop.idl, &sbrec_table_chassis);
     ovsdb_idl_add_column(ovnsb_idl_loop.idl, &sbrec_chassis_col_name);
     ovsdb_idl_add_column(ovnsb_idl_loop.idl, &sbrec_chassis_col_other_config);
+    ovsdb_idl_add_column(ovnsb_idl_loop.idl, &sbrec_chassis_col_encaps);
+
+    ovsdb_idl_add_table(ovnsb_idl_loop.idl, &sbrec_table_encap);
+    ovsdb_idl_add_column(ovnsb_idl_loop.idl, &sbrec_encap_col_type);
 
     ovsdb_idl_add_table(ovnsb_idl_loop.idl, &sbrec_table_chassis_private);
     ovsdb_idl_add_column(ovnsb_idl_loop.idl,
@@ -13122,6 +13153,7 @@ main(int argc, char *argv[])
                 .ovnnb_txn = ovsdb_idl_loop_run(&ovnnb_idl_loop),
                 .ovnsb_idl = ovnsb_idl_loop.idl,
                 .ovnsb_txn = ovsdb_idl_loop_run(&ovnsb_idl_loop),
+                .sbrec_chassis_by_name = sbrec_chassis_by_name,
                 .sbrec_ha_chassis_grp_by_name = sbrec_ha_chassis_grp_by_name,
                 .sbrec_mcast_group_by_name_dp = sbrec_mcast_group_by_name_dp,
                 .sbrec_ip_mcast_by_dp = sbrec_ip_mcast_by_dp,

controller/physical.c
@@ -180,7 +180,8 @@ static void
 put_encapsulation(enum mf_field_id mff_ovn_geneve,
                   const struct chassis_tunnel *tun,
                   const struct sbrec_datapath_binding *datapath,
-                  uint16_t outport, struct ofpbuf *ofpacts)
+                  uint16_t outport, bool is_ramp_switch,
+                  struct ofpbuf *ofpacts)
 {
     if (tun->type == GENEVE) {
         put_load(datapath->tunnel_key, MFF_TUN_ID, 0, 24, ofpacts);
@@ -191,7 +192,12 @@ put_encapsulation(enum mf_field_id mff_ovn_geneve,
                  MFF_TUN_ID, 0, 64, ofpacts);
         put_move(MFF_LOG_INPORT, 0, MFF_TUN_ID, 40, 15, ofpacts);
     } else if (tun->type == VXLAN) {
-        put_load(datapath->tunnel_key, MFF_TUN_ID, 0, 24, ofpacts);
+        uint64_t vni = datapath->tunnel_key;
+        if (!is_ramp_switch) {
+            /* Only some bits are used for regular tunnels. */
+            vni |= (uint64_t) outport << 12;
+        }
+        put_load(vni, MFF_TUN_ID, 0, 24, ofpacts);
     } else {
         OVS_NOT_REACHED();
     }
@@ -323,8 +329,9 @@ put_remote_port_redirect_overlay(const struct
         if (!rem_tun) {
             return;
         }
-        put_encapsulation(mff_ovn_geneve, tun, binding->datapath,
-                          port_key, ofpacts_p);
+        put_encapsulation(mff_ovn_geneve, tun, binding->datapath, port_key,
+                          !strcmp(binding->type, "vtep"),
+                          ofpacts_p);
         /* Output to tunnel. */
         ofpact_put_OUTPUT(ofpacts_p)->port = rem_tun->ofport;
     } else {
@@ -360,8 +367,9 @@ put_remote_port_redirect_overlay(const struct
             return;
         }
 
-        put_encapsulation(mff_ovn_geneve, tun, binding->datapath,
-                          port_key, ofpacts_p);
+        put_encapsulation(mff_ovn_geneve, tun, binding->datapath, port_key,
+                          !strcmp(binding->type, "vtep"),
+                          ofpacts_p);
 
         /* Output to tunnels with active/backup */
         struct ofpact_bundle *bundle = ofpact_put_BUNDLE(ofpacts_p);
@@ -1370,7 +1378,7 @@ consider_mc_group(enum mf_field_id mff_ovn_geneve,
 
             if (!prev || tun->type != prev->type) {
                 put_encapsulation(mff_ovn_geneve, tun, mc->datapath,
-                                  mc->tunnel_key, &remote_ofpacts);
+                                  mc->tunnel_key, true, &remote_ofpacts);
                 prev = tun;
             }
             ofpact_put_OUTPUT(&remote_ofpacts)->port = tun->ofport;
@@ -1615,11 +1623,12 @@ physical_run(struct physical_ctx *p_ctx,
      * Process packets that arrive from a remote hypervisor (by matching
      * on tunnel in_port). */
 
-    /* Add flows for Geneve and STT encapsulations.  These
-     * encapsulations have metadata about the ingress and egress logical
-     * ports.  We set MFF_LOG_DATAPATH, MFF_LOG_INPORT, and
-     * MFF_LOG_OUTPORT from the tunnel key data, then resubmit to table
-     * 33 to handle packets to the local hypervisor. */
+    /* Add flows for Geneve, STT and VXLAN encapsulations.  Geneve and STT
+     * encapsulations have metadata about the ingress and egress logical ports.
+     * VXLAN encapsulations have metadata about the egress logical port only.
+     * We set MFF_LOG_DATAPATH, MFF_LOG_INPORT, and MFF_LOG_OUTPORT from the
+     * tunnel key data where possible, then resubmit to table 33 to handle
+     * packets to the local hypervisor. */
     HMAP_FOR_EACH (tun, hmap_node, &tunnels) {
         struct match match = MATCH_CATCHALL_INITIALIZER;
         match_set_in_port(&match, tun->ofport);
@@ -1636,8 +1645,10 @@ physical_run(struct physical_ctx *p_ctx,
             put_move(MFF_TUN_ID, 24, MFF_LOG_OUTPORT,  0, 16, &ofpacts);
             put_move(MFF_TUN_ID,  0, MFF_LOG_DATAPATH, 0, 24, &ofpacts);
         } else if (tun->type == VXLAN) {
-            /* We'll handle VXLAN later. */
-            continue;
+            /* Add flows for non-VTEP tunnels. Split VNI into two 12-bit
+             * sections and use them for datapath and outport IDs. */
+            put_move(MFF_TUN_ID, 12, MFF_LOG_OUTPORT,  0, 12, &ofpacts);
+            put_move(MFF_TUN_ID, 0, MFF_LOG_DATAPATH, 0, 12, &ofpacts);
         } else {
             OVS_NOT_REACHED();
         }
@@ -1648,11 +1659,7 @@ physical_run(struct physical_ctx *p_ctx,
                         &ofpacts, hc_uuid);
     }
 
-    /* Add flows for VXLAN encapsulations.  Due to the limited amount of
-     * metadata, we only support VXLAN for connections to gateways.  The
-     * VNI is used to populate MFF_LOG_DATAPATH.  The gateway's logical
-     * port is set to MFF_LOG_INPORT.  Then the packet is resubmitted to
-     * table 16 to determine the logical egress port. */
+    /* Handle ramp switch encapsulations. */
     HMAP_FOR_EACH (tun, hmap_node, &tunnels) {
         if (tun->type != VXLAN) {
             continue;
@@ -1660,7 +1667,9 @@ physical_run(struct physical_ctx *p_ctx,
 
         SBREC_PORT_BINDING_TABLE_FOR_EACH (binding,
                                            p_ctx->port_binding_table) {
-            struct match match = MATCH_CATCHALL_INITIALIZER;
+            if (strcmp(binding->type, "vtep")) {
+                continue;
+            }
 
             if (!binding->chassis ||
                 !encaps_tunnel_id_match(tun->chassis_id,
@@ -1668,18 +1677,24 @@ physical_run(struct physical_ctx *p_ctx,
                 continue;
             }
 
+            struct match match = MATCH_CATCHALL_INITIALIZER;
             match_set_in_port(&match, tun->ofport);
+            ofpbuf_clear(&ofpacts);
+
+            /* Add flows for ramp switches.  The VNI is used to populate
+             * MFF_LOG_DATAPATH.  The gateway's logical port is set to
+             * MFF_LOG_INPORT.  Then the packet is resubmitted to table 8
+             * to determine the logical egress port. */
             match_set_tun_id(&match, htonll(binding->datapath->tunnel_key));
 
-            ofpbuf_clear(&ofpacts);
             put_move(MFF_TUN_ID, 0,  MFF_LOG_DATAPATH, 0, 24, &ofpacts);
             put_load(binding->tunnel_key, MFF_LOG_INPORT, 0, 15, &ofpacts);
-            /* For packets received from a vxlan tunnel, set a flag to that
+            /* For packets received from a ramp tunnel, set a flag to that
              * effect. */
-            put_load(1, MFF_LOG_FLAGS, MLF_RCV_FROM_VXLAN_BIT, 1, &ofpacts);
+            put_load(1, MFF_LOG_FLAGS, MLF_RCV_FROM_RAMP_BIT, 1, &ofpacts);
             put_resubmit(OFTABLE_LOG_INGRESS_PIPELINE, &ofpacts);
 
-            ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 100,
+            ofctrl_add_flow(flow_table, OFTABLE_PHY_TO_LOG, 110,
                             binding->header_.uuid.parts[0],
                             &match, &ofpacts, hc_uuid);
         }
@@ -1696,7 +1711,7 @@ physical_run(struct physical_ctx *p_ctx,
     struct match match;
     match_init_catchall(&match);
     match_set_reg_masked(&match, MFF_LOG_FLAGS - MFF_REG0,
-                         MLF_RCV_FROM_VXLAN, MLF_RCV_FROM_VXLAN);
+                         MLF_RCV_FROM_RAMP, MLF_RCV_FROM_RAMP);
 
     /* Resubmit to table 33. */
     ofpbuf_clear(&ofpacts);

include/ovn/logical-fields.h
@@ -51,7 +51,7 @@ void ovn_init_symtab(struct shash *symtab);
 /* MFF_LOG_FLAGS_REG bit assignments */
 enum mff_log_flags_bits {
     MLF_ALLOW_LOOPBACK_BIT = 0,
-    MLF_RCV_FROM_VXLAN_BIT = 1,
+    MLF_RCV_FROM_RAMP_BIT = 1,
     MLF_FORCE_SNAT_FOR_DNAT_BIT = 2,
     MLF_FORCE_SNAT_FOR_LB_BIT = 3,
     MLF_LOCAL_ONLY_BIT = 4,
@@ -64,11 +64,11 @@ enum mff_log_flags {
     /* Allow outputting back to inport. */
     MLF_ALLOW_LOOPBACK = (1 << MLF_ALLOW_LOOPBACK_BIT),
 
-    /* Indicate that a packet was received from a VXLAN tunnel to
-     * compensate for the lack of egress port information available in
-     * VXLAN encapsulation.  Egress port information is available for
-     * Geneve and STT tunnel types. */
-    MLF_RCV_FROM_VXLAN = (1 << MLF_RCV_FROM_VXLAN_BIT),
+    /* Indicate that a packet was received from a ramp switch to compensate for
+     * the lack of egress port information available in ramp switch
+     * encapsulation.  Egress port information is available for Geneve, STT and
+     * regular VXLAN tunnel types. */
+    MLF_RCV_FROM_RAMP = (1 << MLF_RCV_FROM_RAMP_BIT),
 
     /* Indicate that a packet needs a force SNAT in the gateway router when
      * DNAT has taken place. */

lib/ovn-util.h
@@ -107,6 +107,10 @@ void ovn_conn_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
 #define OVN_MAX_DP_KEY_LOCAL (OVN_MAX_DP_KEY - OVN_MAX_DP_GLOBAL_NUM)
 #define OVN_MIN_DP_KEY_GLOBAL (OVN_MAX_DP_KEY_LOCAL + 1)
 #define OVN_MAX_DP_KEY_GLOBAL OVN_MAX_DP_KEY
+
+#define OVN_MAX_DP_VXLAN_KEY ((1u << 12) - 1)
+#define OVN_MAX_DP_VXLAN_KEY_LOCAL (OVN_MAX_DP_KEY - OVN_MAX_DP_GLOBAL_NUM)
+
 struct hmap;
 void ovn_destroy_tnlids(struct hmap *tnlids);
 void ovn_add_tnlid(struct hmap *set, uint32_t tnlid);

ovn-architecture.7.xml
@@ -1163,11 +1163,14 @@
 
       <p>
         Geneve and STT tunnels pass this field as part of the tunnel key.
-        Although VXLAN tunnels do not explicitly carry a logical input port,
-        OVN only uses VXLAN to communicate with gateways that from OVN's
+        Ramp switch VXLAN tunnels do not explicitly carry a logical input port,
+        but since they are used to communicate with gateways that from OVN's
         perspective consist of only a single logical port, so that OVN can set
         the logical input port field to this one on ingress to the OVN logical
-        pipeline.
+        pipeline. As for regular VXLAN tunnels, they don't carry input port
+        field at all. This puts additional limitations on cluster
+        capabilities that are described in
+        <code>Tunnel Encapsulations</code> section.
       </p>
     </dd>
 
@@ -1183,15 +1186,15 @@
       </p>
 
       <p>
-        Geneve and STT tunnels pass this field as part of the tunnel key.
-        VXLAN tunnels do not transmit the logical output port field.
-        Since VXLAN tunnels do not carry a logical output port field in
-        the tunnel key, when a packet is received from VXLAN tunnel by
-        an OVN hypervisor, the packet is resubmitted to table 8 to
-        determine the output port(s);  when the packet reaches table 32,
+        Geneve, STT and regular VXLAN tunnels pass this field as part of the
+        tunnel key. Ramp switch VXLAN tunnels do not transmit the logical
+        output port field, and since they do not carry a logical output port
+        field in the tunnel key, when a packet is received from ramp switch
+        VXLAN tunnel by an OVN hypervisor, the packet is resubmitted to table 8
+        to determine the output port(s); when the packet reaches table 32,
         these packets are resubmitted to table 33 for local delivery by
-        checking a MLF_RCV_FROM_VXLAN flag, which is set when the packet
-        arrives from a VXLAN tunnel.
+        checking a MLF_RCV_FROM_RAMP flag, which is set when the packet
+        arrives from a ramp tunnel.
       </p>
     </dd>
 
@@ -1263,15 +1266,16 @@
       </p>
 
       <p>
-        Table 0 also processes packets that arrive from other chassis.  It
+        Table 0 also processes packets that arrive from other chassis. It
         distinguishes them from other packets by ingress port, which is a
-        tunnel.  As with packets just entering the OVN pipeline, the actions
-        annotate these packets with logical datapath and logical ingress port
-        metadata.  In addition, the actions set the logical output port field,
+        tunnel. As with packets just entering the OVN pipeline, the actions
+        annotate these packets with logical datapath metadata. For tunnel types
+        that support it, they are also annotated with logical ingress port
+        metadata. In addition, the actions set the logical output port field,
         which is available because in OVN tunneling occurs after the logical
-        output port is known.  These three pieces of information are obtained
+        output port is known. These pieces of information are obtained
         from the tunnel encapsulation metadata (see <code>Tunnel
-        Encapsulations</code> for encoding details).  Then the actions resubmit
+        Encapsulations</code> for encoding details). Then the actions resubmit
         to table 33 to enter the logical egress pipeline.
       </p>
     </li>
@@ -1430,12 +1434,12 @@
 
       <ul>
         <li>
-          A higher-priority rule to match packets received from VXLAN tunnels,
-          based on flag MLF_RCV_FROM_VXLAN, and resubmit these packets to table
-          33 for local delivery.  Packets received from VXLAN tunnels reach
-          here because of a lack of logical output port field in the tunnel key
-          and thus these packets needed to be submitted to table 8 to
-          determine the output port.
+          A higher-priority rule to match packets received from ramp switch
+          tunnels, based on flag MLF_RCV_FROM_RAMP, and resubmit these packets
+          to table 33 for local delivery.  Packets received from ramp switch
+          tunnels reach here because of a lack of logical output port field in
+          the tunnel key and thus these packets needed to be submitted to table
+          8 to determine the output port.
         </li>
         <li>
           A higher-priority rule to match packets received from ports of type
@@ -2352,6 +2356,11 @@
     communication between the AZs are established.
   </p>
 
+  <p>
+    When VXLAN tunneling is enabled in an OVN cluster, due to the limited
+    range available for VNIs, Interconnection feature is not supported.
+  </p>
+
   <h3>A day in the life of a packet crossing AZs</h3>
   <ol>
     <li>
@@ -2658,9 +2667,9 @@
   <h2>Tunnel Encapsulations</h2>
 
   <p>
-    OVN annotates logical network packets that it sends from one hypervisor to
-    another with the following three pieces of metadata, which are encoded in
-    an encapsulation-specific fashion:
+    In general, OVN annotates logical network packets that it sends from one
+    hypervisor to another with the following three pieces of metadata, which
+    are encoded in an encapsulation-specific fashion:
   </p>
 
   <ul>
@@ -2686,16 +2695,60 @@
   </ul>
 
   <p>
-    For hypervisor-to-hypervisor traffic, OVN supports only Geneve and STT
-    encapsulations, for the following reasons:
+      When VXLAN is enabled on any hypervisor in a cluster, datapath and egress
+      port identifier ranges are reduced to 12-bits. This is done because only
+      STT and Geneve provide the large space for metadata (over 32 bits per
+      packet). To accommodate for VXLAN, 24 bits available are split as
+      follows:
   </p>
 
   <ul>
     <li>
-      Only STT and Geneve support the large amounts of metadata (over 32 bits
-      per packet) that OVN uses (as described above).
+      12-bit logical datapath identifier, derived from the
+      <code>tunnel_key</code> column in the OVN Southbound
+      <code>Datapath_Binding</code> table.
     </li>
 
+    <li>
+      12-bit logical egress port identifier.  IDs 0 through 32767 have the same
+      meaning as for logical ingress ports.  IDs 32768 through 65535,
+      inclusive, may be assigned to logical multicast groups (see the
+      <code>tunnel_key</code> column in the OVN Southbound
+      <code>Multicast_Group</code> table).
+    </li>
+
+    <li>
+      No logical ingress port identifier.
+    </li>
+  </ul>
+
+  <p>
+      The limited space available for metadata when VXLAN tunnels are enabled
+      in a cluster put the following functional limitations onto features
+      available to users:
+  </p>
+
+  <ul>
+    <li>
+      The maximum number of networks is reduced to 4096.
+    </li>
+    <li>
+      The maximum number of ports per network is reduced to 4096. (Including
+      multicast group ports.)
+    </li>
+    <li>
+      ACLs matching against logical ingress port identifiers are not supported.
+    </li>
+    <li>
+      OVN interconnection feature is not supported.
+    </li>
+  </ul>
+
+  <p>
+      In addition to functional limitations described above, the following
+      should be considered before enabling it in your cluster:
+  </p>
+  <ul>
     <li>
       STT and Geneve use randomized UDP or TCP source ports that allows
       efficient distribution among multiple paths in environments that use ECMP

ovn-nb.xml
@@ -267,6 +267,16 @@
         OVN tunnels will be encrypted with IPsec.
       </column>
     </group>
+
+    <group title="Read-only Options">
+      <column name="options" key="max_tunid">
+        <p>
+          The maximum supported tunnel ID. Depends on types of encapsulation
+          enabled in the cluster.
+        </p>
+      </column>
+    </group>
+
   </table>
 
   <table name="Logical_Switch" title="L2 logical switch">

tests/ovn-macros.at
@@ -215,7 +215,7 @@ net_attach () {
 
 # ovn_az_attach AZ NETWORK BRIDGE IP [MASKLEN]
 ovn_az_attach() {
-    local az=$1 net=$2 bridge=$3 ip=$4 masklen=${5-24}
+    local az=$1 net=$2 bridge=$3 ip=$4 masklen=${5-24} encap=${6-geneve,vxlan}
     net_attach $net $bridge || return 1
 
     mac=`ovs-vsctl get Interface $bridge mac_in_use | sed s/\"//g`
@@ -232,7 +232,7 @@ ovn_az_attach() {
     ovs-vsctl \
         -- set Open_vSwitch . external-ids:system-id=$sandbox \
         -- set Open_vSwitch . external-ids:ovn-remote=$ovn_remote \
-        -- set Open_vSwitch . external-ids:ovn-encap-type=geneve,vxlan \
+        -- set Open_vSwitch . external-ids:ovn-encap-type=$encap \
         -- set Open_vSwitch . external-ids:ovn-encap-ip=$ip \
         -- --may-exist add-br br-int \
         -- set bridge br-int fail-mode=secure other-config:disable-in-band=true \

commit 06cb97e87b3c24b0d42126ac2edc16ecbdc39cc2

ovn-northd: Avoid verification of NB_Global.options if nothing changed.
northd constructs transaction for the Northbound database options even
if they don't need to be changed.  This also includes verification of
a current state.  IDL will eventually drop the transaction if it will
not contain any other operations, but if there are some other
operations, this useless update to the same value will be included
along with the verification.  This causes transaction failures because
NB_Global.options can be updated by CMS at the same time.

For example, ovn-kubernetes sets 'options:e2e_timestamp' for it's own
purposes, and if this value will be updated while there is an in-flight
transaction from the northd, transaction will fail the verification and
northd will have to re-try it.

To avoid these issues, updating and verifying options only if needed.

Fixes: b07f1bc3d068 ("Add VXLAN support for non-VTEP datapath bindings")
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/ovn-northd.c
@@ -14233,8 +14233,10 @@ ovnnb_db_run(struct northd_context *ctx,
                      ovn_internal_version);
     }
 
-    nbrec_nb_global_verify_options(nb);
-    nbrec_nb_global_set_options(nb, &options);
+    if (!smap_equal(&nb->options, &options)) {
+        nbrec_nb_global_verify_options(nb);
+        nbrec_nb_global_set_options(nb, &options);
+    }
 
     smap_destroy(&options);
 