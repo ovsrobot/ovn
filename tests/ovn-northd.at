ovsrobot/ovn

commit 15348b7b806f7a9680606c3e9348708980129949

ovn-northd: Multiple distributed gateway port support.
By default, OVN support only one DGP (distributed gateway port) per
logical router. While a single DGP port suffices for most of the North
South connectivity, there are requirements where a logical router could
be connected to multiple external networks and based on routing decision
packet could go to different ones.

This patch adds flexibility of having multiple DGPs per logical router.

Changes can classified as following:
a. Data structure changes to allow multiple DGPs per ovn_datapath.

b. Consumption of new data structure in logical flows for
   individual features.

c. Features that require changes are:
   i. Regular NS traffic flow.
  ii. Network Address Translation.
 iii. Load Balancer
  iv. Gateway_mtu.
   v. reside-on-redirect-chassis
  vi. Misc code sections that assumed a single DGP.

d. Except for reside-on-redirect-chassis all the other features
   could be extended to multiple DGPs. Reside on redirect
   chassis with its current specification could not be extended
   and hence should be used only with the logical router that
   has a single DGP.

This patch doesn't support NAT & load-balancer features for multiple
DGPs yet, but added validations that disables NAT/load-balancer
features when there are more than one DGP configured per router.

Signed-off-by: Ankur Sharma <ankurmnnit2004@gmail.com>
Co-authored-by: Dhathri Purohith <dhathri.purohith@nutanix.com>
Signed-off-by: Dhathri Purohith <dhathri.purohith@nutanix.com>
Co-authored-by: Abhiram Sangana <sangana.abhiram@nutanix.com>
Signed-off-by: Abhiram Sangana <sangana.abhiram@nutanix.com>
Co-authored-by: Han Zhou <hzhou@ovn.org>
Signed-off-by: Han Zhou <hzhou@ovn.org>
Acked-by: Numan Siddique <numans@ovn.org>

tests/ovn-northd.at
@@ -5021,3 +5021,84 @@ AT_CHECK([grep -e "lr_in_ip_routing_ecmp" lr0flows | sed 's/192\.168\.0\..0/192.
 
 AT_CLEANUP
 ])
+
+AT_SETUP([ovn-northd -- lr multiple gw ports])
+AT_KEYWORDS([multiple-l3dgw-ports])
+ovn_start
+
+# Logical network:
+# 1 Logical Router, 3 bridged Logical Switches,
+# 1 gateway chassis attached to each corresponding LRP.
+#
+#                | S1 (gw1)
+#                |
+#      ls  ----  DR -- S3 (gw3)
+# (20.0.0.0/24)  |
+#                | S2 (gw2)
+#
+# Validate basic LR logical flows.
+
+check ovn-sbctl chassis-add gw1 geneve 127.0.0.1
+check ovn-sbctl chassis-add gw2 geneve 128.0.0.1
+check ovn-sbctl chassis-add gw3 geneve 129.0.0.1
+
+check ovn-nbctl lr-add DR
+check ovn-nbctl lrp-add DR DR-S1 02:ac:10:01:00:01 172.16.1.1/24
+check ovn-nbctl lrp-add DR DR-S2 03:ac:10:01:00:01 172.16.2.1/24
+check ovn-nbctl lrp-add DR DR-S3 04:ac:10:01:00:01 172.16.3.1/24
+check ovn-nbctl lrp-add DR DR-ls 05:ac:10:01:00:01 20.0.0.1/24
+
+check ovn-nbctl ls-add S1
+check ovn-nbctl lsp-add S1 S1-DR
+check ovn-nbctl lsp-set-type S1-DR router
+check ovn-nbctl lsp-set-addresses S1-DR router
+check ovn-nbctl --wait=sb lsp-set-options S1-DR router-port=DR-S1
+
+check ovn-nbctl ls-add S2
+check ovn-nbctl lsp-add S2 S2-DR
+check ovn-nbctl lsp-set-type S2-DR router
+check ovn-nbctl lsp-set-addresses S2-DR router
+check ovn-nbctl --wait=sb lsp-set-options S2-DR router-port=DR-S2
+
+check ovn-nbctl ls-add S3
+check ovn-nbctl lsp-add S3 S3-DR
+check ovn-nbctl lsp-set-type S3-DR router
+check ovn-nbctl lsp-set-addresses S3-DR router
+check ovn-nbctl --wait=sb lsp-set-options S3-DR router-port=DR-S3
+
+check ovn-nbctl ls-add  ls
+check ovn-nbctl lsp-add ls ls-DR
+check ovn-nbctl lsp-set-type ls-DR router
+check ovn-nbctl lsp-set-addresses ls-DR router
+check ovn-nbctl --wait=sb lsp-set-options ls-DR router-port=DR-ls
+
+check ovn-nbctl lrp-set-gateway-chassis DR-S1 gw1
+check ovn-nbctl lrp-set-gateway-chassis DR-S2 gw2
+check ovn-nbctl lrp-set-gateway-chassis DR-S3 gw3
+
+check ovn-nbctl --wait=sb sync
+
+ovn-sbctl dump-flows DR > lrflows
+AT_CAPTURE_FILE([lrflows])
+
+# Check the flows in lr_in_admission stage
+AT_CHECK([grep lr_in_admission lrflows | grep cr-DR | sort], [0], [dnl
+  table=0 (lr_in_admission    ), priority=50   , match=(eth.dst == 02:ac:10:01:00:01 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1")), action=(xreg0[[0..47]] = 02:ac:10:01:00:01; next;)
+  table=0 (lr_in_admission    ), priority=50   , match=(eth.dst == 03:ac:10:01:00:01 && inport == "DR-S2" && is_chassis_resident("cr-DR-S2")), action=(xreg0[[0..47]] = 03:ac:10:01:00:01; next;)
+  table=0 (lr_in_admission    ), priority=50   , match=(eth.dst == 04:ac:10:01:00:01 && inport == "DR-S3" && is_chassis_resident("cr-DR-S3")), action=(xreg0[[0..47]] = 04:ac:10:01:00:01; next;)
+])
+# Check the flows in lr_in_lookup_neighbor stage
+AT_CHECK([grep lr_in_lookup_neighbor lrflows | grep cr-DR | sort], [0], [dnl
+  table=1 (lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S1" && arp.spa == 172.16.1.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S1")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
+  table=1 (lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S2" && arp.spa == 172.16.2.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S2")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
+  table=1 (lr_in_lookup_neighbor), priority=100  , match=(inport == "DR-S3" && arp.spa == 172.16.3.0/24 && arp.op == 1 && is_chassis_resident("cr-DR-S3")), action=(reg9[[2]] = lookup_arp(inport, arp.spa, arp.sha); next;)
+])
+# Check the flows in lr_in_gw_redirect stage
+AT_CHECK([grep lr_in_gw_redirect lrflows | grep cr-DR | sort], [0], [dnl
+  table=17(lr_in_gw_redirect  ), priority=50   , match=(outport == "DR-S1"), action=(outport = "cr-DR-S1"; next;)
+  table=17(lr_in_gw_redirect  ), priority=50   , match=(outport == "DR-S2"), action=(outport = "cr-DR-S2"; next;)
+  table=17(lr_in_gw_redirect  ), priority=50   , match=(outport == "DR-S3"), action=(outport = "cr-DR-S3"; next;)
+])
+
+AT_CLEANUP
+])

northd/lrouter.dl
@@ -138,14 +138,14 @@ Warning[message] :-
     var message = "Bad configuration: distributed gateway port configured on "
     "port ${lrp.name} on L3 gateway router".
 
-/* DistributedGatewayPortCandidate.
+/* Distributed gateway ports.
  *
- * Each row pairs a logical router with its distributed gateway port,
- * but without checking that there is at most one DGP per LR.
+ * Each row means 'lrp' is a distributed gateway port on 'lr_uuid'.
  *
- * (Use DistributedGatewayPort instead, since it guarantees uniqueness.) */
-relation DistributedGatewayPortCandidate(lr_uuid: uuid, lrp_uuid: uuid)
-DistributedGatewayPortCandidate(lr_uuid, lrp_uuid) :-
+ * A logical router can have multiple distributed gateway ports. */
+relation DistributedGatewayPort(lrp: Intern<nb::Logical_Router_Port>,
+                                lr_uuid: uuid)
+DistributedGatewayPort(lrp, lr_uuid) :-
     lr in nb::Logical_Router(._uuid = lr_uuid),
     LogicalRouterPort(lrp_uuid, lr._uuid),
     lrp in &nb::Logical_Router_Port(._uuid = lrp_uuid),
@@ -153,30 +153,10 @@ DistributedGatewayPortCandidate(lr_uuid, lrp_uuid) :-
     var has_hcg = lrp.ha_chassis_group.is_some(),
     var has_gc = not lrp.gateway_chassis.is_empty(),
     has_hcg or has_gc.
-Warning[message] :-
-    DistributedGatewayPortCandidate(lr_uuid, lrp_uuid),
-    var lrps = lrp_uuid.group_by(lr_uuid).to_set(),
-    lrps.size() > 1,
-    lr in nb::Logical_Router(._uuid = lr_uuid),
-    var message = "Bad configuration: multiple distributed gateway ports on "
-    "logical router ${lr.name}; ignoring all of them".
-
-/* Distributed gateway ports.
- *
- * Each row means 'lrp' is the distributed gateway port on 'lr_uuid'.
- *
- * There is at most one distributed gateway port per logical router. */
-relation DistributedGatewayPort(lrp: Intern<nb::Logical_Router_Port>, lr_uuid: uuid)
-DistributedGatewayPort(lrp, lr_uuid) :-
-    DistributedGatewayPortCandidate(lr_uuid, lrp_uuid),
-    var lrps = lrp_uuid.group_by(lr_uuid).to_set(),
-    lrps.size() == 1,
-    Some{var lrp_uuid} = lrps.nth(0),
-    lrp in &nb::Logical_Router_Port(._uuid = lrp_uuid).
 
 /* HAChassis is an abstraction over nb::Gateway_Chassis and nb::HA_Chassis, which
  * are different ways to represent the same configuration.  Each row is
- * effectively one HA_Chassis record.  (Usually, we could associated each
+ * effectively one HA_Chassis record.  (Usually, we could associate each
  * row with a particular 'lr_uuid', but it's permissible for more than one
  * logical router to use a HA chassis group, so we omit it so that multiple
  * references get merged.)
@@ -236,18 +216,20 @@ HAChassisGroup(ha_chassis_group_uuid(hac_group_uuid),
                         .name = name,
                         .external_ids = external_ids).
 
-/* Each row maps from a logical router to the name of its HAChassisGroup.
- * This level of indirection is needed because multiple logical routers
- * are allowed to reference a given HAChassisGroup. */
-relation LogicalRouterHAChassisGroup(lr_uuid: uuid,
-                                     hacg_uuid: uuid)
-LogicalRouterHAChassisGroup(lr_uuid, ha_chassis_group_uuid(lrp._uuid)) :-
-    DistributedGatewayPort(lrp, lr_uuid),
+/* Each row maps from a distributed gateway logical router port to the name of
+ * its HAChassisGroup.
+ * This level of indirection is needed because multiple distributed gateway
+ * logical router ports are allowed to reference a given HAChassisGroup. */
+relation DistributedGatewayPortHAChassisGroup(
+    lrp: Intern<nb::Logical_Router_Port>,
+    hacg_uuid: uuid)
+DistributedGatewayPortHAChassisGroup(lrp, ha_chassis_group_uuid(lrp._uuid)) :-
+    DistributedGatewayPort(.lrp = lrp),
     lrp.ha_chassis_group == None,
     lrp.gateway_chassis.size() > 0.
-LogicalRouterHAChassisGroup(lr_uuid,
-                            ha_chassis_group_uuid(hac_group_uuid)) :-
-    DistributedGatewayPort(lrp, lr_uuid),
+DistributedGatewayPortHAChassisGroup(lrp,
+                                     ha_chassis_group_uuid(hac_group_uuid)) :-
+    DistributedGatewayPort(.lrp = lrp),
     Some{var hac_group_uuid} = lrp.ha_chassis_group,
     nb::HA_Chassis_Group(._uuid = hac_group_uuid).
 
@@ -259,14 +241,19 @@ RouterPortIsRedirect(lrp, false) :-
     &nb::Logical_Router_Port(._uuid = lrp),
     not DistributedGatewayPort(&nb::Logical_Router_Port{._uuid = lrp}, _).
 
-relation LogicalRouterRedirectPort(lr: uuid, has_redirect_port: Option<Intern<nb::Logical_Router_Port>>)
-
-LogicalRouterRedirectPort(lr, Some{lrp}) :-
-    DistributedGatewayPort(lrp, lr).
-
-LogicalRouterRedirectPort(lr, None) :-
-    nb::Logical_Router(._uuid = lr),
-    not DistributedGatewayPort(_, lr).
+/*
+ * LogicalRouterDGWPorts maps from each logical router UUID
+ * to the logical router's set of distributed gateway (or redirect) ports. */
+relation LogicalRouterDGWPorts(
+    lr_uuid: uuid,
+    l3dgw_ports: Vec<Intern<nb::Logical_Router_Port>>)
+LogicalRouterDGWPorts(lr_uuid, l3dgw_ports) :-
+    DistributedGatewayPort(lrp, lr_uuid),
+    var l3dgw_ports = lrp.group_by(lr_uuid).to_vec().
+LogicalRouterDGWPorts(lr_uuid, vec_empty()) :-
+    lr in nb::Logical_Router(),
+    var lr_uuid = lr._uuid,
+    not DistributedGatewayPort(_, lr_uuid).
 
 typedef ExceptionalExtIps = AllowedExtIps{ips: Intern<nb::Address_Set>}
                           | ExemptedExtIps{ips: Intern<nb::Address_Set>}
@@ -450,9 +437,7 @@ LogicalRouterCopp0(lr, meters) :-
 
 /* Router relation collects all attributes of a logical router.
  *
- * `l3dgw_port` - optional redirect port (see `DistributedGatewayPort`)
- * `redirect_port_name` - derived redirect port name (or empty string if
- *      router does not have a redirect port)
+ * `l3dgw_ports` - optional redirect ports (see `DistributedGatewayPort`)
  * `is_gateway` - true iff the router is a gateway router.  Together with
  *      `l3dgw_port`, this flag affects the generation of various flows
  *      related to NAT and load balancing.
@@ -474,8 +459,7 @@ typedef Router = Router {
     external_ids:       Map<string,string>,
 
     /* Additional computed fields. */
-    l3dgw_port:         Option<Intern<nb::Logical_Router_Port>>,
-    redirect_port_name: string,
+    l3dgw_ports:        Vec<Intern<nb::Logical_Router_Port>>,
     is_gateway:         bool,
     nats:               Vec<NAT>,
     snat_ips:           Map<v46_ip, Set<NAT>>,
@@ -498,23 +482,18 @@ Router[Router{
         .options       =    lr.options,
         .external_ids  =    lr.external_ids,
 
-        .l3dgw_port = l3dgw_port,
-        .redirect_port_name =
-            match (l3dgw_port) {
-                Some{rport} -> json_string_escape(chassis_redirect_name(rport.name)),
-                _ -> ""
-            },
-        .is_gateway = lr.options.contains_key("chassis"),
-        .nats       = nats,
-        .snat_ips   = snat_ips,
-        .lbs        = lbs,
-        .mcast_cfg  = mcast_cfg,
+        .l3dgw_ports = l3dgw_ports,
+        .is_gateway  = lr.options.contains_key("chassis"),
+        .nats        = nats,
+        .snat_ips    = snat_ips,
+        .lbs         = lbs,
+        .mcast_cfg   = mcast_cfg,
         .learn_from_arp_request = learn_from_arp_request,
         .force_lb_snat = force_lb_snat,
         .copp       = copp}.intern()] :-
     lr in nb::Logical_Router(),
     lr.is_enabled(),
-    LogicalRouterRedirectPort(lr._uuid, l3dgw_port),
+    LogicalRouterDGWPorts(lr._uuid, l3dgw_ports),
     LogicalRouterNATs(lr._uuid, nats),
     LogicalRouterLBs(lr._uuid, lbs),
     LogicalRouterSnatIPs(lr._uuid, snat_ips),

northd/ovn-northd.8.xml
@@ -3815,10 +3815,10 @@ icmp6 {
     <h3>Ingress Table 17: Gateway Redirect</h3>
 
     <p>
-      For distributed logical routers where one of the logical router
+      For distributed logical routers where one or more of the logical router
       ports specifies a gateway chassis, this table redirects
-      certain packets to the distributed gateway port instance on the
-      gateway chassis.  This table has the following flows:
+      certain packets to the distributed gateway port instances on the
+      gateway chassises.  This table has the following flows:
     </p>
 
     <ul>

ovn-architecture.7.xml
@@ -731,6 +731,13 @@
     highest-priority gateway that is online.
   </p>
 
+  <p>
+    A logical router can have multiple distributed gateway ports, each
+    connecting different external networks. However, some features, such as NAT
+    and load balancers, are not supported yet for logical routers with more
+    than one distributed gateway port configured.
+  </p>
+
   <h4>Physical VLAN MTU Issues</h4>
 
   <p>
@@ -1968,8 +1975,9 @@
 
   <p>
     If the logical router doesn't have a distributed gateway port connecting
-    to the localnet logical switch which provides external connectivity,
-    then this option is ignored by <code>OVN</code>.
+    to the localnet logical switch which provides external connectivity, or
+    if it has more than one distributed gateway ports, then this option is
+    ignored by <code>OVN</code>.
   </p>
 
   <p>
@@ -2084,6 +2092,13 @@
     gateway port, OVN redirects packets to the gateway chassis using the
     <code>localnet</code> port of the router's peer logical switch, instead of
     a tunnel.
+  </p>
+
+  <p>
+    If the logical router doesn't have a distributed gateway port connecting
+    to the localnet logical switch which provides external connectivity, or
+    if it has more than one distributed gateway ports, then this option is
+    ignored by <code>OVN</code>.
   </p>
 
   <p>

ovn-nb.xml
@@ -2032,13 +2032,14 @@
 
     <column name="nat">
       One or more NAT rules for the router.  NAT rules only work on
-      Gateway routers, and on distributed routers with logical gateway ports.
+      Gateway routers, and on distributed routers with one and only one
+      distributed gateway port.
     </column>
 
     <column name="load_balancer">
       Load balance a virtual ip address to a set of logical port ip
       addresses.  Load balancer rules only work on the Gateway routers or
-      routers with distributed gateway ports.
+      routers with one and only one distributed gateway port.
     </column>
 
     <group title="Naming">
@@ -2453,8 +2454,7 @@
         If either of these are set, this logical router port represents a
         distributed gateway port that connects this router to a
         logical switch with a <code>localnet</code> port or a
-        connection to another OVN deployment.  There may be at most
-        one such logical router port on each logical router.
+        connection to another OVN deployment.
       </p>
 
       <p>
@@ -2476,8 +2476,16 @@
       </p>
 
       <p>
-        When more than one gateway chassis is specified, OVN only uses
-        one at a time. OVN can rely on OVS BFD implementation to monitor
+        There can be more than one distributed gateway ports configured
+        on each logical router, each connecting to different L2 segments.
+        However, features such as NAT and load-balancer are not supported
+        on logical routers with more than one distributed gateway ports.
+      </p>
+
+      <p>
+        For each distributed gateway port, it may have more than one gateway
+        chassises. When more than one gateway chassis is specified, OVN only
+        uses one at a time.  OVN can rely on OVS BFD implementation to monitor
         gateway connectivity, preferring the highest-priority gateway
         that is online.  Priorities are specified in the <code>priority</code>
         column of <ref table="Gateway_Chassis"/> or <ref table="HA_Chassis"/>.
@@ -2563,8 +2571,8 @@
           </p>
 
           <p>
-            OVN honors this option only if the logical router has a distributed
-            gateway port and if the LRP's peer switch has a
+            OVN honors this option only if the logical router has one and only
+            one distributed gateway port and if the LRP's peer switch has a
             <code>localnet</code> port.
           </p>
         </column>
@@ -2588,7 +2596,8 @@
           <p>
             Setting this option to <code>overlay</code> or leaving it unset has
             no effect.  This option may usefully be set only on a distributed
-            gateway port.  It is otherwise ignored.
+            gateway port when there is one and only one distributed gateway
+            port on the logical router.  It is otherwise ignored.
           </p>
         </column>
       </group>

commit a675d9ddc3063f434158e15bab5882bb01976e6f

northd: Properly warn for NAT on LR with multiple gw ports.
This is not supported and ovn-northd was not logging it properly.
The previous attempt tried to log in init_nat_entries() but there
the logical router port configuration has not yet been parsed.

Move the check and log instead to where we install the NAT flows.
Also add a test.

Fixes: 15348b7b806f ("ovn-northd: Multiple distributed gateway port support.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -763,16 +763,6 @@ init_nat_entries(struct ovn_datapath *od)
         return;
     }
 
-    if (od->n_l3dgw_ports > 1) {
-        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
-        VLOG_WARN_RL(&rl, "NAT is configured on logical router %s, which has %"
-                     PRIuSIZE" distributed gateway ports. NAT is not supported"
-                     " yet when there is more than one distributed gateway "
-                     "port on the router.",
-                     od->nbr->name, od->n_l3dgw_ports);
-        return;
-    }
-
     od->nat_entries = xmalloc(od->nbr->n_nat * sizeof *od->nat_entries);
 
     for (size_t i = 0; i < od->nbr->n_nat; i++) {
@@ -13240,6 +13230,18 @@ build_lrouter_nat_defrag_and_lb(struct ovn_datapath *od, struct hmap *lflows,
      * l3dgw_port (router has a port with gateway chassis
      * specified). */
     if (!od->is_gw_router && !od->n_l3dgw_ports) {
+        return;
+    }
+
+    /* NAT rules are not currently supported on logical routers with multiple
+     * distributed gateway ports. */
+    if (od->n_l3dgw_ports > 1) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
+        VLOG_WARN_RL(&rl, "NAT is configured on logical router %s, which has %"
+                     PRIuSIZE" distributed gateway ports. NAT is not supported"
+                     " yet when there is more than one distributed gateway "
+                     "port on the router.",
+                     od->nbr->name, od->n_l3dgw_ports);
         return;
     }
 

tests/ovn-northd.at
@@ -5804,6 +5804,12 @@ AT_CHECK([grep lr_in_gw_redirect lrflows | grep cr-DR | sed 's/table=../table=??
   table=??(lr_in_gw_redirect  ), priority=50   , match=(outport == "DR-S3"), action=(outport = "cr-DR-S3"; next;)
 ])
 
+# Check that ovn-northd logs a warning when trying to configure NAT
+# on the router with multiple distributed gw ports.  Such configurations are
+# not supported yet.
+check ovn-nbctl lr-nat-add DR dnat_and_snat 42.42.42.1 20.0.0.2
+AT_CHECK([grep -q 'NAT is configured on logical router DR, which has 2 distributed gateway ports. NAT is not supported yet when there is more than one distributed gateway port on the router.' northd/ovn-northd.log], [0])
+
 AT_CLEANUP
 ])
 