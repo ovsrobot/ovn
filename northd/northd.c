ovsrobot/ovn

commit b8194738c99ee09ad6d4762a3c999e04d58d1a0f

northd: Properly warn for NAT on LR with multiple gw ports.
This is not supported and ovn-northd was not logging it properly.
The previous attempt tried to log in init_nat_entries() but there
the logical router port configuration has not yet been parsed.

Move the check and log instead to where we install the NAT flows.
Also add a test.

Fixes: 15348b7b806f ("ovn-northd: Multiple distributed gateway port support.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/northd.c
@@ -763,16 +763,6 @@ init_nat_entries(struct ovn_datapath *od)
         return;
     }
 
-    if (od->n_l3dgw_ports > 1) {
-        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
-        VLOG_WARN_RL(&rl, "NAT is configured on logical router %s, which has %"
-                     PRIuSIZE" distributed gateway ports. NAT is not supported"
-                     " yet when there is more than one distributed gateway "
-                     "port on the router.",
-                     od->nbr->name, od->n_l3dgw_ports);
-        return;
-    }
-
     od->nat_entries = xmalloc(od->nbr->n_nat * sizeof *od->nat_entries);
 
     for (size_t i = 0; i < od->nbr->n_nat; i++) {
@@ -13240,6 +13230,18 @@ build_lrouter_nat_defrag_and_lb(struct ovn_datapath *od, struct hmap *lflows,
      * l3dgw_port (router has a port with gateway chassis
      * specified). */
     if (!od->is_gw_router && !od->n_l3dgw_ports) {
+        return;
+    }
+
+    /* NAT rules are not currently supported on logical routers with multiple
+     * distributed gateway ports. */
+    if (od->n_l3dgw_ports > 1) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
+        VLOG_WARN_RL(&rl, "NAT is configured on logical router %s, which has %"
+                     PRIuSIZE" distributed gateway ports. NAT is not supported"
+                     " yet when there is more than one distributed gateway "
+                     "port on the router.",
+                     od->nbr->name, od->n_l3dgw_ports);
         return;
     }
 

tests/ovn-northd.at
@@ -5804,6 +5804,12 @@ AT_CHECK([grep lr_in_gw_redirect lrflows | grep cr-DR | sed 's/table=../table=??
   table=??(lr_in_gw_redirect  ), priority=50   , match=(outport == "DR-S3"), action=(outport = "cr-DR-S3"; next;)
 ])
 
+# Check that ovn-northd logs a warning when trying to configure NAT
+# on the router with multiple distributed gw ports.  Such configurations are
+# not supported yet.
+check ovn-nbctl lr-nat-add DR dnat_and_snat 42.42.42.1 20.0.0.2
+AT_CHECK([grep -q 'NAT is configured on logical router DR, which has 2 distributed gateway ports. NAT is not supported yet when there is more than one distributed gateway port on the router.' northd/ovn-northd.log], [0])
+
 AT_CLEANUP
 ])
 

commit d2c5d79bc3841c81ba540eedc695512c891d1f3c

northd: Fix check for NAT on LR with multiple gw ports.
Add check to see if the LR actually has NAT entries configured.
Otherwise we spam the log unnecessarily.

Fixes: b8194738c99e ("northd: Properly warn for NAT on LR with multiple gw ports.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -13242,7 +13242,7 @@ build_lrouter_nat_defrag_and_lb(struct ovn_datapath *od, struct hmap *lflows,
 
     /* NAT rules are not currently supported on logical routers with multiple
      * distributed gateway ports. */
-    if (od->n_l3dgw_ports > 1) {
+    if (od->nbr->n_nat && od->n_l3dgw_ports > 1) {
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
         VLOG_WARN_RL(&rl, "NAT is configured on logical router %s, which has %"
                      PRIuSIZE" distributed gateway ports. NAT is not supported"

tests/ovn-northd.at
@@ -5807,8 +5807,8 @@ AT_CHECK([grep lr_in_gw_redirect lrflows | grep cr-DR | sed 's/table=../table=??
 # Check that ovn-northd logs a warning when trying to configure NAT
 # on the router with multiple distributed gw ports.  Such configurations are
 # not supported yet.
-check ovn-nbctl lr-nat-add DR dnat_and_snat 42.42.42.1 20.0.0.2
-AT_CHECK([grep -q 'NAT is configured on logical router DR, which has 2 distributed gateway ports. NAT is not supported yet when there is more than one distributed gateway port on the router.' northd/ovn-northd.log], [0])
+check ovn-nbctl --wait=sb lr-nat-add DR dnat_and_snat 42.42.42.1 20.0.0.2
+AT_CHECK([grep -q 'NAT is configured on logical router DR, which has 3 distributed gateway ports. NAT is not supported yet when there is more than one distributed gateway port on the router.' northd/ovn-northd.log], [0])
 
 AT_CLEANUP
 ])