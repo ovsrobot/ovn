ovsrobot/ovn

commit 647920b6a824b472841ba6a9c11c477e0ea10c66

controller: incrementally create ipv6 prefix delegation port_binding list
Incrementally manage local_active_ports_ipv6_pd map for interfaces
where IPv6 prefix-delegation has been enabled. This patch allows to
avoid looping over all local interfaces to check if prefix-delegation
is running on the current port binding.

Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

controller/binding.c
@@ -574,6 +574,32 @@ remove_related_lport(const struct sbrec_port_binding *pb,
     }
 }
 
+static void
+update_active_pb_ras_pd(const struct sbrec_port_binding *pb,
+                        struct hmap *local_datapaths,
+                        struct shash *map, const char *conf)
+{
+    bool ras_pd_conf = smap_get_bool(&pb->options, conf, false);
+    struct shash_node *iter = shash_find(map, pb->logical_port);
+    struct pb_ld_binding *ras_pd = iter ? iter->data : NULL;
+
+    if (iter && !ras_pd_conf) {
+        shash_delete(map, iter);
+        free(ras_pd);
+        return;
+    }
+    if (ras_pd_conf) {
+        if (!ras_pd) {
+            ras_pd = xzalloc(sizeof *ras_pd);
+            ras_pd->pb = pb;
+            shash_add(map, pb->logical_port, ras_pd);
+        }
+        ovs_assert(ras_pd);
+        ras_pd->ld = get_local_datapath(local_datapaths,
+                                        pb->datapath->tunnel_key);
+    }
+}
+
 /* Corresponds to each Port_Binding.type. */
 enum en_lport_type {
     LP_UNKNOWN,
@@ -1645,6 +1671,10 @@ binding_run(struct binding_ctx_in *b_ctx_in, struct binding_ctx_out *b_ctx_out)
     const struct sbrec_port_binding *pb;
     SBREC_PORT_BINDING_TABLE_FOR_EACH (pb,
                                        b_ctx_in->port_binding_table) {
+        update_active_pb_ras_pd(pb, b_ctx_out->local_datapaths,
+                                b_ctx_out->local_active_ports_ipv6_pd,
+                                "ipv6_prefix_delegation");
+
         enum en_lport_type lport_type = get_lport_type(pb);
 
         switch (lport_type) {
@@ -2481,6 +2511,10 @@ delete_done:
         if (sbrec_port_binding_is_deleted(pb)) {
             continue;
         }
+
+        update_active_pb_ras_pd(pb, b_ctx_out->local_datapaths,
+                                b_ctx_out->local_active_ports_ipv6_pd,
+                                "ipv6_prefix_delegation");
 
         enum en_lport_type lport_type = get_lport_type(pb);
 

controller/ovn-controller.c
@@ -1045,6 +1045,8 @@ struct ed_type_runtime_data {
     bool tracked;
     bool local_lports_changed;
     struct hmap tracked_dp_bindings;
+
+    struct shash local_active_ports_ipv6_pd;
 };
 
 /* struct ed_type_runtime_data has the below members for tracking the
@@ -1132,6 +1134,7 @@ en_runtime_data_init(struct engine_node *node OVS_UNUSED,
     sset_init(&data->egress_ifaces);
     smap_init(&data->local_iface_ids);
     local_binding_data_init(&data->lbinding_data);
+    shash_init(&data->local_active_ports_ipv6_pd);
 
     /* Init the tracked data. */
     hmap_init(&data->tracked_dp_bindings);
@@ -1157,6 +1160,7 @@ en_runtime_data_cleanup(void *data)
         free(cur_node);
     }
     hmap_destroy(&rt_data->local_datapaths);
+    shash_destroy_free_data(&rt_data->local_active_ports_ipv6_pd);
     local_binding_data_destroy(&rt_data->lbinding_data);
 }
 
@@ -1235,6 +1239,8 @@ init_binding_ctx(struct engine_node *node,
     b_ctx_in->ovs_table = ovs_table;
 
     b_ctx_out->local_datapaths = &rt_data->local_datapaths;
+    b_ctx_out->local_active_ports_ipv6_pd =
+        &rt_data->local_active_ports_ipv6_pd;
     b_ctx_out->local_lports = &rt_data->local_lports;
     b_ctx_out->local_lports_changed = false;
     b_ctx_out->related_lports = &rt_data->related_lports;
@@ -1252,6 +1258,7 @@ en_runtime_data_run(struct engine_node *node, void *data)
 {
     struct ed_type_runtime_data *rt_data = data;
     struct hmap *local_datapaths = &rt_data->local_datapaths;
+    struct shash *local_active_ipv6_pd = &rt_data->local_active_ports_ipv6_pd;
     struct sset *local_lports = &rt_data->local_lports;
     struct sset *active_tunnels = &rt_data->active_tunnels;
 
@@ -1267,6 +1274,7 @@ en_runtime_data_run(struct engine_node *node, void *data)
             free(cur_node);
         }
         hmap_clear(local_datapaths);
+        shash_clear_free_data(local_active_ipv6_pd);
         local_binding_data_destroy(&rt_data->lbinding_data);
         sset_destroy(local_lports);
         related_lports_destroy(&rt_data->related_lports);
@@ -3282,7 +3290,8 @@ main(int argc, char *argv[])
                                     sbrec_bfd_table_get(ovnsb_idl_loop.idl),
                                     br_int, chassis,
                                     &runtime_data->local_datapaths,
-                                    &runtime_data->active_tunnels);
+                                    &runtime_data->active_tunnels,
+                                    &runtime_data->local_active_ports_ipv6_pd);
                         /* Updating monitor conditions if runtime data or
                          * logical datapath goups changed. */
                         if (engine_node_changed(&en_runtime_data)

controller/ovn-controller.h
@@ -87,4 +87,10 @@ enum chassis_tunnel_type {
 
 uint32_t get_tunnel_type(const char *name);
 
+struct pb_ld_binding {
+    const struct sbrec_port_binding *pb;
+    const struct local_datapath *ld;
+    struct hmap_node hmap_node;
+};
+
 #endif /* controller/ovn-controller.h */

controller/pinctrl.c
@@ -1249,83 +1249,76 @@ fill_ipv6_prefix_state(struct ovsdb_idl_txn *ovnsb_idl_txn,
 static void
 prepare_ipv6_prefixd(struct ovsdb_idl_txn *ovnsb_idl_txn,
                      struct ovsdb_idl_index *sbrec_port_binding_by_name,
-                     const struct hmap *local_datapaths,
+                     const struct shash *local_active_ports_ipv6_pd,
                      const struct sbrec_chassis *chassis,
                      const struct sset *active_tunnels)
     OVS_REQUIRES(pinctrl_mutex)
 {
-    const struct local_datapath *ld;
     bool changed = false;
 
-    HMAP_FOR_EACH (ld, hmap_node, local_datapaths) {
-        if (datapath_is_switch(ld->datapath)) {
-            /* logical switch */
+    struct shash_node *iter;
+    SHASH_FOR_EACH (iter, local_active_ports_ipv6_pd) {
+        const struct pb_ld_binding *pb_ipv6 = iter->data;
+        const struct sbrec_port_binding *pb = pb_ipv6->pb;
+        int j;
+
+        if (!pb_ipv6->ld) {
             continue;
         }
 
-        for (size_t i = 0; i < ld->n_peer_ports; i++) {
-            const struct sbrec_port_binding *pb = ld->peer_ports[i].local;
-            int j;
-
-            if (!smap_get_bool(&pb->options, "ipv6_prefix_delegation",
-                               false)) {
-                continue;
-            }
-
-            const char *peer_s = smap_get(&pb->options, "peer");
-            if (!peer_s) {
-                continue;
-            }
-
-            const struct sbrec_port_binding *peer
-                = lport_lookup_by_name(sbrec_port_binding_by_name, peer_s);
-            if (!peer) {
-                continue;
-            }
-
-            char *redirect_name = xasprintf("cr-%s", pb->logical_port);
-            bool resident = lport_is_chassis_resident(
-                    sbrec_port_binding_by_name, chassis, active_tunnels,
-                    redirect_name);
-            free(redirect_name);
-            if (!resident && strcmp(pb->type, "l3gateway")) {
-                continue;
-            }
-
-            struct in6_addr ip6_addr;
-            struct eth_addr ea = eth_addr_zero;
-            for (j = 0; j < pb->n_mac; j++) {
-                struct lport_addresses laddrs;
-
-                if (!extract_lsp_addresses(pb->mac[j], &laddrs)) {
-                    continue;
-                }
-
-                ea = laddrs.ea;
-                if (laddrs.n_ipv6_addrs > 0) {
-                    ip6_addr = laddrs.ipv6_addrs[0].addr;
-                    destroy_lport_addresses(&laddrs);
-                    break;
-                }
-                destroy_lport_addresses(&laddrs);
-            }
-
-            if (eth_addr_is_zero(ea)) {
-                continue;
-            }
-
-            if (j == pb->n_mac) {
-                in6_generate_lla(ea, &ip6_addr);
-            }
-
-            changed |= fill_ipv6_prefix_state(ovnsb_idl_txn, ld,
-                                              ea, ip6_addr,
-                                              peer->tunnel_key,
-                                              peer->datapath->tunnel_key);
+        const char *peer_s = smap_get(&pb->options, "peer");
+        if (!peer_s) {
+            continue;
         }
+
+        const struct sbrec_port_binding *peer
+            = lport_lookup_by_name(sbrec_port_binding_by_name, peer_s);
+        if (!peer) {
+            continue;
+        }
+
+        char *redirect_name = xasprintf("cr-%s", pb->logical_port);
+        bool resident = lport_is_chassis_resident(
+                sbrec_port_binding_by_name, chassis, active_tunnels,
+                redirect_name);
+        free(redirect_name);
+        if (!resident && strcmp(pb->type, "l3gateway")) {
+            continue;
+        }
+
+        struct in6_addr ip6_addr;
+        struct eth_addr ea = eth_addr_zero;
+        for (j = 0; j < pb->n_mac; j++) {
+            struct lport_addresses laddrs;
+
+            if (!extract_lsp_addresses(pb->mac[j], &laddrs)) {
+                continue;
+            }
+
+            ea = laddrs.ea;
+            if (laddrs.n_ipv6_addrs > 0) {
+                ip6_addr = laddrs.ipv6_addrs[0].addr;
+                destroy_lport_addresses(&laddrs);
+                break;
+            }
+            destroy_lport_addresses(&laddrs);
+        }
+
+        if (eth_addr_is_zero(ea)) {
+            continue;
+        }
+
+        if (j == pb->n_mac) {
+            in6_generate_lla(ea, &ip6_addr);
+        }
+
+        changed |= fill_ipv6_prefix_state(ovnsb_idl_txn, pb_ipv6->ld,
+                                          ea, ip6_addr,
+                                          peer->tunnel_key,
+                                          peer->datapath->tunnel_key);
     }
 
-    struct shash_node *iter, *next;
+    struct shash_node *next;
     SHASH_FOR_EACH_SAFE (iter, next, &ipv6_prefixd) {
         struct ipv6_prefixd_state *pfd = iter->data;
         if (pfd->last_used + IPV6_PREFIXD_STALE_TIMEOUT < time_msec()) {
@@ -3411,7 +3404,8 @@ pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
             const struct ovsrec_bridge *br_int,
             const struct sbrec_chassis *chassis,
             const struct hmap *local_datapaths,
-            const struct sset *active_tunnels)
+            const struct sset *active_tunnels,
+            const struct shash *local_active_ports_ipv6_pd)
 {
     ovs_mutex_lock(&pinctrl_mutex);
     pinctrl_set_br_int_name_(br_int->name);
@@ -3426,7 +3420,8 @@ pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
                            local_datapaths, active_tunnels);
     prepare_ipv6_ras(local_datapaths);
     prepare_ipv6_prefixd(ovnsb_idl_txn, sbrec_port_binding_by_name,
-                         local_datapaths, chassis, active_tunnels);
+                         local_active_ports_ipv6_pd, chassis,
+                         active_tunnels);
     sync_dns_cache(dns_table);
     controller_event_run(ovnsb_idl_txn, ce_table, chassis);
     ip_mcast_sync(ovnsb_idl_txn, chassis, local_datapaths,

controller/binding.h
@@ -72,6 +72,7 @@ void related_lports_destroy(struct related_lports *);
 
 struct binding_ctx_out {
     struct hmap *local_datapaths;
+    struct shash *local_active_ports_ipv6_pd;
     struct local_binding_data *lbinding_data;
 
     /* sset of (potential) local lports. */

controller/pinctrl.h
@@ -23,6 +23,7 @@
 #include "openvswitch/meta-flow.h"
 
 struct hmap;
+struct shash;
 struct lport_index;
 struct ovsdb_idl_index;
 struct ovsdb_idl_txn;
@@ -49,7 +50,8 @@ void pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
                  const struct sbrec_bfd_table *,
                  const struct ovsrec_bridge *, const struct sbrec_chassis *,
                  const struct hmap *local_datapaths,
-                 const struct sset *active_tunnels);
+                 const struct sset *active_tunnels,
+                 const struct shash *local_active_ports_ipv6_pd);
 void pinctrl_wait(struct ovsdb_idl_txn *ovnsb_idl_txn);
 void pinctrl_destroy(void);
 void pinctrl_set_br_int_name(char *br_int_name);

commit f64017fe55f60dc6360e8d7e8ed1b08d9e40b76d

controller: Fix IPv6 prefix delegation
The local_datapaths are populated by iterating through
all available port bindings or new ones added during I-C.
The port bindings are not ordered, it might happen that
the order between engine runs are different. This can cause
the prefix delegation shash to be with NULL local_datapath.
Even in cases that the port binding is actually part of
the local_datapath. Store only port binding in the shash
and during pinctrl_run check if the stored port binding
is part of local_datapaths. Because pinctrl runs only
after the engine computation is complete we should have
complete hmap of all local_datapaths.

Fixes: 647920b6 ("controller: incrementally create ipv6 prefix delegation port_binding list")
Reported-at: https://bugzilla.redhat.com/2108726
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/binding.c
@@ -509,35 +509,20 @@ static void
 delete_active_pb_ras_pd(const struct sbrec_port_binding *pb,
                         struct shash *ras_pd_map)
 {
-    struct pb_ld_binding *ras_pd =
-        shash_find_and_delete(ras_pd_map, pb->logical_port);
-
-    free(ras_pd);
+    shash_find_and_delete(ras_pd_map, pb->logical_port);
 }
 
 static void
 update_active_pb_ras_pd(const struct sbrec_port_binding *pb,
-                        struct hmap *local_datapaths,
                         struct shash *map, const char *conf)
 {
     bool ras_pd_conf = smap_get_bool(&pb->options, conf, false);
     struct shash_node *iter = shash_find(map, pb->logical_port);
-    struct pb_ld_binding *ras_pd = iter ? iter->data : NULL;
 
-    if (iter && !ras_pd_conf) {
+    if (!ras_pd_conf && iter) {
         shash_delete(map, iter);
-        free(ras_pd);
-        return;
-    }
-    if (ras_pd_conf) {
-        if (!ras_pd) {
-            ras_pd = xzalloc(sizeof *ras_pd);
-            ras_pd->pb = pb;
-            shash_add(map, pb->logical_port, ras_pd);
-        }
-        ovs_assert(ras_pd);
-        ras_pd->ld = get_local_datapath(local_datapaths,
-                                        pb->datapath->tunnel_key);
+    } else if (ras_pd_conf && !iter) {
+        shash_add(map, pb->logical_port, pb);
     }
 }
 
@@ -1735,11 +1720,9 @@ binding_run(struct binding_ctx_in *b_ctx_in, struct binding_ctx_out *b_ctx_out)
     const struct sbrec_port_binding *pb;
     SBREC_PORT_BINDING_TABLE_FOR_EACH (pb,
                                        b_ctx_in->port_binding_table) {
-        update_active_pb_ras_pd(pb, b_ctx_out->local_datapaths,
-                                b_ctx_out->local_active_ports_ipv6_pd,
+        update_active_pb_ras_pd(pb, b_ctx_out->local_active_ports_ipv6_pd,
                                 "ipv6_prefix_delegation");
-        update_active_pb_ras_pd(pb, b_ctx_out->local_datapaths,
-                                b_ctx_out->local_active_ports_ras,
+        update_active_pb_ras_pd(pb, b_ctx_out->local_active_ports_ras,
                                 "ipv6_ra_send_periodic");
 
         enum en_lport_type lport_type = get_lport_type(pb);
@@ -2629,12 +2612,10 @@ delete_done:
             continue;
         }
 
-        update_active_pb_ras_pd(pb, b_ctx_out->local_datapaths,
-                                b_ctx_out->local_active_ports_ipv6_pd,
+        update_active_pb_ras_pd(pb, b_ctx_out->local_active_ports_ipv6_pd,
                                 "ipv6_prefix_delegation");
 
-        update_active_pb_ras_pd(pb, b_ctx_out->local_datapaths,
-                                b_ctx_out->local_active_ports_ras,
+        update_active_pb_ras_pd(pb, b_ctx_out->local_active_ports_ras,
                                 "ipv6_ra_send_periodic");
 
         enum en_lport_type lport_type = get_lport_type(pb);

controller/ovn-controller.c
@@ -1312,8 +1312,8 @@ en_runtime_data_cleanup(void *data)
     sset_destroy(&rt_data->egress_ifaces);
     smap_destroy(&rt_data->local_iface_ids);
     local_datapaths_destroy(&rt_data->local_datapaths);
-    shash_destroy_free_data(&rt_data->local_active_ports_ipv6_pd);
-    shash_destroy_free_data(&rt_data->local_active_ports_ras);
+    shash_destroy(&rt_data->local_active_ports_ipv6_pd);
+    shash_destroy(&rt_data->local_active_ports_ras);
     local_binding_data_destroy(&rt_data->lbinding_data);
 }
 
@@ -1425,8 +1425,8 @@ en_runtime_data_run(struct engine_node *node, void *data)
         first_run = false;
     } else {
         local_datapaths_destroy(local_datapaths);
-        shash_clear_free_data(local_active_ipv6_pd);
-        shash_clear_free_data(local_active_ras);
+        shash_clear(local_active_ipv6_pd);
+        shash_clear(local_active_ras);
         local_binding_data_destroy(&rt_data->lbinding_data);
         sset_destroy(local_lports);
         related_lports_destroy(&rt_data->related_lports);

controller/ovn-controller.h
@@ -45,10 +45,4 @@ const struct ovsrec_bridge *get_bridge(const struct ovsrec_bridge_table *,
 
 uint32_t get_tunnel_type(const char *name);
 
-struct pb_ld_binding {
-    const struct sbrec_port_binding *pb;
-    const struct local_datapath *ld;
-    struct hmap_node hmap_node;
-};
-
 #endif /* controller/ovn-controller.h */

controller/pinctrl.c
@@ -1292,18 +1292,20 @@ prepare_ipv6_prefixd(struct ovsdb_idl_txn *ovnsb_idl_txn,
                      struct ovsdb_idl_index *sbrec_port_binding_by_name,
                      const struct shash *local_active_ports_ipv6_pd,
                      const struct sbrec_chassis *chassis,
-                     const struct sset *active_tunnels)
+                     const struct sset *active_tunnels,
+                     const struct hmap *local_datapaths)
     OVS_REQUIRES(pinctrl_mutex)
 {
     bool changed = false;
 
     struct shash_node *iter;
     SHASH_FOR_EACH (iter, local_active_ports_ipv6_pd) {
-        const struct pb_ld_binding *pb_ipv6 = iter->data;
-        const struct sbrec_port_binding *pb = pb_ipv6->pb;
+        const struct sbrec_port_binding *pb = iter->data;
+        const struct local_datapath *ld =
+            get_local_datapath(local_datapaths, pb->datapath->tunnel_key);
         int j;
 
-        if (!pb_ipv6->ld) {
+        if (!ld) {
             continue;
         }
 
@@ -1354,7 +1356,7 @@ prepare_ipv6_prefixd(struct ovsdb_idl_txn *ovnsb_idl_txn,
             in6_generate_lla(ea, &ip6_addr);
         }
 
-        changed |= fill_ipv6_prefix_state(ovnsb_idl_txn, pb_ipv6->ld,
+        changed |= fill_ipv6_prefix_state(ovnsb_idl_txn, ld,
                                           ea, ip6_addr,
                                           peer->tunnel_key,
                                           peer->datapath->tunnel_key);
@@ -3538,7 +3540,7 @@ pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
     prepare_ipv6_ras(local_active_ports_ras, sbrec_port_binding_by_name);
     prepare_ipv6_prefixd(ovnsb_idl_txn, sbrec_port_binding_by_name,
                          local_active_ports_ipv6_pd, chassis,
-                         active_tunnels);
+                         active_tunnels, local_datapaths);
     sync_dns_cache(dns_table);
     controller_event_run(ovnsb_idl_txn, ce_table, chassis);
     ip_mcast_sync(ovnsb_idl_txn, chassis, local_datapaths,
@@ -3982,8 +3984,8 @@ prepare_ipv6_ras(const struct shash *local_active_ports_ras,
 
     bool changed = false;
     SHASH_FOR_EACH (iter, local_active_ports_ras) {
-        const struct pb_ld_binding *ras = iter->data;
-        const struct sbrec_port_binding *pb = ras->pb;
+        const struct sbrec_port_binding *pb = iter->data;
+        VLOG_INFO("Pop %p", pb);
 
         const char *peer_s = smap_get(&pb->options, "peer");
         if (!peer_s) {