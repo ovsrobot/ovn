6fe4179961e434a12472a0b1912022dabd56668f
Ales Musil <amusil@redhat.com>
2022-08-18 11:41:10 +0200

    controller: Fix wrong controller action definition
    The controller action had a wrong wrong userdata_len due to
    missing substraction of previous data len. The pointer to oc
    was not moved correctly so it could in theory point to wrong
    memory. In order to fix that expose encode_start_controller_op
    and encode_finish_controller_op which are helper methods
    to define the controller action. Use those instead of
    manually creating the action. At the same time use stack
    buffer for the ofpbuf which should be large enough to hold
    related data without any heap allocations.
    
    Fixes: e52c2451 ("physical.c: Fix bug of wrong use in vm migration")
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/actions.c
+++ lib/actions.c
@@ -78,7 +78,7 @@ ovnact_init(struct ovnact *ovnact, enum ovnact_type type, size_t len)
     ovnact->len = len;
 }
 
-static size_t
+size_t
 encode_start_controller_op(enum action_opcode opcode, bool pause,
                            uint32_t meter_id, struct ofpbuf *ofpacts)
 {
@@ -99,7 +99,7 @@ encode_start_controller_op(enum action_opcode opcode, bool pause,
     return ofs;
 }
 
-static void
+void
 encode_finish_controller_op(size_t ofs, struct ofpbuf *ofpacts)
 {
     struct ofpact_controller *oc = ofpbuf_at_assert(ofpacts, ofs, sizeof *oc);
