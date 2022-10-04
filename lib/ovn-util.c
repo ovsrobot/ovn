9972a520087e3ce82af274d13f3cd258dd167cb8
Ihar Hrachyshka <ihrachys@redhat.com>
2022-10-04 17:30:58 +0000

    Support ovn-...-<chassis> specific global ovsdb options
    Before the patch, all controller instances were reading configuration
    from the same external-ids:ovn-* options. This patch adds support for
    distinct config otions for different chassis names stored in the same
    ovsdb global config object.
    
    To configure an option for a distinct chassis name, an admin may add a
    suffix with the desired chassis name to a config option. For example, if
    the following is configured in ovsdb, only a controller with the
    corresponding chassis name (either 'hv1' or 'hv2') would read just one
    of the following options:
    
    ovs-vsctl set open . external-ids:ovn-bridge-mappings-hv1=phys:br-phys-1
    ovs-vsctl set open . external-ids:ovn-bridge-mappings-hv2=phys:br-phys-2
    
    Chassis specific config options override any global settings, so for
    example if the following configuration is used, then controller 'hv1'
    will use the first setting but not the latter. Any other controllers
    will use the global setting, which is the second setting..
    
    ovs-vsctl set open . external-ids:ovn-bridge-mappings-hv1=phys:br-phys-1
    ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys-2
    
    This is supported for other options too.
    
    This is in preparation to support running multiple controller instances
    using the same vswitchd instance.
    
    Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ovn-util.c
+++ lib/ovn-util.c
@@ -935,3 +935,90 @@ daemon_started_recently(void)
     /* Ensure that at least an amount of time has passed. */
     return time_wall_msec() - startup_ts <= DAEMON_STARTUP_DELAY_MS;
 }
+
+const char *
+get_chassis_external_id_value(const struct smap *external_ids,
+                              const char *chassis_id,
+                              const char *option_key,
+                              const char *def)
+{
+    const char *option_value = NULL;
+    if (chassis_id) {
+        char *chassis_option_key = xasprintf("%s-%s", option_key, chassis_id);
+        option_value = smap_get(external_ids, chassis_option_key);
+        free(chassis_option_key);
+    }
+    if (!option_value) {
+        option_value = smap_get_def(external_ids, option_key, def);
+    }
+    return option_value;
+}
+
+int
+get_chassis_external_id_value_int(const struct smap *external_ids,
+                                  const char *chassis_id,
+                                  const char *option_key,
+                                  int def)
+{
+    const char *value = get_chassis_external_id_value(
+        external_ids, chassis_id, option_key, NULL);
+
+    int i_value;
+    if (!value || !str_to_int(value, 10, &i_value)) {
+        return def;
+    }
+
+    return i_value;
+}
+
+unsigned int
+get_chassis_external_id_value_uint(const struct smap *external_ids,
+                                   const char *chassis_id,
+                                   const char *option_key,
+                                   unsigned int def)
+{
+    const char *value = get_chassis_external_id_value(
+        external_ids, chassis_id, option_key, NULL);
+
+    unsigned int u_value;
+
+    if (!value || !str_to_uint(value, 10, &u_value)) {
+        return def;
+    }
+
+    return u_value;
+}
+
+unsigned long long int
+get_chassis_external_id_value_ullong(const struct smap *external_ids,
+                                     const char *chassis_id,
+                                     const char *option_key,
+                                     unsigned long long int def)
+{
+    const char *value = get_chassis_external_id_value(
+        external_ids, chassis_id, option_key, NULL);
+
+    unsigned long long ull_value;
+
+    if (!value || !str_to_ullong(value, 10, &ull_value)) {
+        return def;
+    }
+
+    return ull_value;
+}
+
+bool
+get_chassis_external_id_value_bool(const struct smap *external_ids,
+                                   const char *chassis_id,
+                                   const char *option_key,
+                                   bool def)
+{
+    const char *value = get_chassis_external_id_value(
+        external_ids, chassis_id, option_key, "");
+
+    if (def) {
+        return strcasecmp("false", value) != 0;
+    } else {
+        return !strcasecmp("true", value);
+    }
+}
