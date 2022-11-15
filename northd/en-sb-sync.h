8123ac399edcab23ce15def01f20caf07bbcb04b
Numan Siddique <numans@ovn.org>
2022-11-15 10:19:21 -0500

    northd: Add I-P for syncing SB address sets.
    Updates to NB address sets and NB port groups are handled
    incrementally for syncing the SB address sets.  This patch
    doesn't support syncing the SB Address sets for the router
    load balancer vips incrementally, instead a full recompute is
    triggered for any changes to NB load balancers, NB load balancer
    groups and NB logical routers.
    
    Signed-off-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/en-sb-sync.h
+++ northd/en-sb-sync.h
@@ -3,12 +3,18 @@
 
 #include "lib/inc-proc-eng.h"
 
+/* en_sb_sync engine node functions. */
 void *en_sb_sync_init(struct engine_node *, struct engine_arg *);
 void en_sb_sync_run(struct engine_node *, void *data);
 void en_sb_sync_cleanup(void *data);
 
+/* en_address_set_sync engine node functions. */
 void *en_address_set_sync_init(struct engine_node *, struct engine_arg *);
 void en_address_set_sync_run(struct engine_node *, void *data);
 void en_address_set_sync_cleanup(void *data);
+bool address_set_sync_nb_address_set_handler(struct engine_node *,
+                                             void *data);
+bool address_set_sync_nb_port_group_handler(struct engine_node *,
+                                            void *data);
 
 #endif
