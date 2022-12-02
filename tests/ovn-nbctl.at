ovsrobot/ovn

commit c23a46839c443979367c77d8e9e5e6c107120edb

ovn-nbctl: add --bfd option to lr-route-add
Introduce the --bfd option to lr-route-add command.
If the BFD session UUID is provided, it will be used for the OVN route
otherwise the next-hop will be used to perform a lookup in the OVN BFD
table.
If the lookup fails and outport is specified, a new entry in the BFD table
will be created using the nexthop as dst_ip and outport as logical_port.

Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

utilities/ovn-nbctl.c
@@ -3960,6 +3960,29 @@ nbctl_lr_route_add(struct ctl_context *ctx)
         goto cleanup;
     }
 
+    struct shash_node *bfd = shash_find(&ctx->options, "--bfd");
+    const struct nbrec_bfd *nb_bt = NULL;
+    if (bfd) {
+        if (bfd->data) {
+            struct uuid bfd_uuid;
+            if (uuid_from_string(&bfd_uuid, bfd->data)) {
+                nb_bt = nbrec_bfd_get_for_uuid(ctx->idl, &bfd_uuid);
+            }
+            if (!nb_bt) {
+                ctl_error(ctx, "no entry found in the BFD table");
+                goto cleanup;
+            }
+        } else {
+            const struct nbrec_bfd *iter;
+            NBREC_BFD_FOR_EACH (iter, ctx->idl) {
+                if (!strcmp(iter->dst_ip, next_hop)) {
+                    nb_bt = iter;
+                    break;
+                }
+            }
+        }
+    }
+
     bool may_exist = shash_find(&ctx->options, "--may-exist") != NULL;
     bool ecmp_symmetric_reply = shash_find(&ctx->options,
                                            "--ecmp-symmetric-reply") != NULL;
@@ -4014,6 +4037,18 @@ nbctl_lr_route_add(struct ctl_context *ctx)
             if (policy) {
                  nbrec_logical_router_static_route_set_policy(route, policy);
             }
+            if (bfd) {
+                if (!nb_bt) {
+                    if (ctx->argc != 5) {
+                        ctl_error(ctx, "insert entry in the BFD table failed");
+                        goto cleanup;
+                    }
+                    nb_bt = nbrec_bfd_insert(ctx->txn);
+                    nbrec_bfd_set_dst_ip(nb_bt, next_hop);
+                    nbrec_bfd_set_logical_port(nb_bt, ctx->argv[4]);
+                }
+                nbrec_logical_router_static_route_set_bfd(route, nb_bt);
+            }
             free(rt_prefix);
             goto cleanup;
         }
@@ -4038,6 +4073,18 @@ nbctl_lr_route_add(struct ctl_context *ctx)
     }
 
     nbrec_logical_router_update_static_routes_addvalue(lr, route);
+    if (bfd) {
+        if (!nb_bt) {
+            if (ctx->argc != 5) {
+                ctl_error(ctx, "insert entry in the BFD table failed");
+                goto cleanup;
+            }
+            nb_bt = nbrec_bfd_insert(ctx->txn);
+            nbrec_bfd_set_dst_ip(nb_bt, next_hop);
+            nbrec_bfd_set_logical_port(nb_bt, ctx->argv[4]);
+        }
+        nbrec_logical_router_static_route_set_bfd(route, nb_bt);
+    }
 
 cleanup:
     free(next_hop);
@@ -6551,7 +6598,7 @@ static const struct ctl_command_syntax nbctl_commands[] = {
     /* logical router route commands. */
     { "lr-route-add", 3, 4, "ROUTER PREFIX NEXTHOP [PORT]", NULL,
       nbctl_lr_route_add, NULL, "--may-exist,--ecmp,--ecmp-symmetric-reply,"
-      "--policy=", RW },
+      "--policy=,--bfd?", RW },
     { "lr-route-del", 1, 4, "ROUTER [PREFIX [NEXTHOP [PORT]]]", NULL,
       nbctl_lr_route_del, NULL, "--if-exists,--policy=", RW },
     { "lr-route-list", 1, 1, "ROUTER", NULL, nbctl_lr_route_list, NULL,

tests/ovn-northd.at
@@ -2305,7 +2305,7 @@ AT_KEYWORDS([northd-bfd])
 ovn_start
 
 check ovn-nbctl --wait=sb lr-add r0
-for i in $(seq 1 4); do
+for i in $(seq 1 5); do
     check ovn-nbctl --wait=sb lrp-add r0 r0-sw$i 00:00:00:00:00:0$i 192.168.$i.1/24
     check ovn-nbctl --wait=sb ls-add sw$i
     check ovn-nbctl --wait=sb lsp-add sw$i sw$i-r0
@@ -2350,17 +2350,24 @@ check_column 1000 bfd min_tx logical_port=r0-sw1
 check_column 1000 bfd min_rx logical_port=r0-sw1
 check_column 100 bfd detect_mult logical_port=r0-sw1
 
-check ovn-nbctl lr-route-add r0 100.0.0.0/8 192.168.10.2
-route_uuid=$(fetch_column nb:logical_router_static_route _uuid ip_prefix="100.0.0.0/8")
-check ovn-nbctl set logical_router_static_route $route_uuid bfd=$uuid
+check ovn-nbctl --bfd=$uuid lr-route-add r0 100.0.0.0/8 192.168.10.2
 check_column down bfd status logical_port=r0-sw1
 AT_CHECK([ovn-nbctl lr-route-list r0 | grep 192.168.10.2 | grep -q bfd],[0])
 
+check ovn-nbctl --bfd lr-route-add r0 200.0.0.0/8 192.168.20.2
+check_column down bfd status logical_port=r0-sw2
+AT_CHECK([ovn-nbctl lr-route-list r0 | grep 192.168.20.2 | grep -q bfd],[0])
+
+check ovn-nbctl --bfd lr-route-add r0 240.0.0.0/8 192.168.50.2 r0-sw5
+check_column down bfd status logical_port=r0-sw5
+AT_CHECK([ovn-nbctl lr-route-list r0 | grep 192.168.50.2 | grep -q bfd],[0])
+
+route_uuid=$(fetch_column nb:logical_router_static_route _uuid ip_prefix="100.0.0.0/8")
 check ovn-nbctl clear logical_router_static_route $route_uuid bfd
 check_column admin_down bfd status logical_port=r0-sw1
 
 ovn-nbctl destroy bfd $uuid
-check_row_count bfd 2
+check_row_count bfd 3
 
 AT_CLEANUP
 

tests/system-ovn.at
@@ -5606,10 +5606,9 @@ NS_CHECK_EXEC([server], [bfdd-control allow 172.16.1.1], [0], [dnl
 Allowing connections from 172.16.1.1
 ])
 
-uuid=$(ovn-nbctl create bfd logical_port=rp-public dst_ip=172.16.1.50 min_tx=250 min_rx=250 detect_mult=10)
-check ovn-nbctl lr-route-add R1 100.0.0.0/8 172.16.1.50
+check ovn-nbctl --bfd lr-route-add R1 100.0.0.0/8 172.16.1.50 rp-public
+uuid=$(fetch_column nb:bfd _uuid logical_port="rp-public")
 route_uuid=$(fetch_column nb:logical_router_static_route _uuid ip_prefix="100.0.0.0/8")
-check ovn-nbctl set logical_router_static_route $route_uuid bfd=$uuid
 check ovn-nbctl --wait=hv sync
 
 wait_column "up" nb:bfd status logical_port=rp-public

utilities/ovn-nbctl.8.xml
@@ -659,6 +659,7 @@
     <dl>
       <dt>[<code>--may-exist</code>] [<code>--policy</code>=<var>POLICY</var>]
         [<code>--ecmp</code>] [<code>--ecmp-symmetric-reply</code>]
+        [<code>--bfd[=<var>UUID</var></code>]]
         <code>lr-route-add</code> <var>router</var>
         <var>prefix</var> <var>nexthop</var> [<var>port</var>]</dt>
       <dd>
@@ -693,6 +694,16 @@
           sent out over that same route. Setting
           <code>--ecmp-symmetric-reply</code> implies <code>--ecmp</code> so
           it is not necessary to set both.
+        </p>
+
+        <p>
+          <code>--bfd</code> option is used to link a BFD session to the
+          OVN route. If the BFD session UUID is provided, it will be used
+          for the OVN route otherwise the next-hop will be used to perform
+          a lookup in the OVN BFD table.
+          If the lookup fails and <var>port</var> is specified, a new entry
+          in the BFD table will be created using the <var>nexthop</var> as
+          <var>dst_ip</var> and <var>port</var> as <var>logical_port</var>.
         </p>
 
         <p>

commit f54e133a2061d37e28c80f91045f28329720c787

ovn-nbctl: Fix removal of BFD entry on route deletion
When creating a route with BFD, ovn-nbctl creates an entry both
in the Logical_Router_Static_Route and BFD tables.

However, before this patch, removing a route does not remove the
corresponding entry in the BFD table.

Fixes: c23a46839c44 ("ovn-nbctl: add --bfd option to lr-route-add")
Reported-at: https://bugs.launchpad.net/ubuntu/+source/ovn/+bug/1998617
Signed-off-by: Frode Nordahl <frode.nordahl@canonical.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

utilities/ovn-nbctl.c
@@ -4427,6 +4427,8 @@ nbctl_pre_lr_route_del(struct ctl_context *ctx)
     ovsdb_idl_add_column(ctx->idl,
                          &nbrec_logical_router_static_route_col_policy);
     ovsdb_idl_add_column(ctx->idl,
+                         &nbrec_logical_router_static_route_col_bfd);
+    ovsdb_idl_add_column(ctx->idl,
                          &nbrec_logical_router_static_route_col_ip_prefix);
     ovsdb_idl_add_column(ctx->idl,
                          &nbrec_logical_router_static_route_col_nexthop);
@@ -4438,7 +4440,7 @@ nbctl_pre_lr_route_del(struct ctl_context *ctx)
 }
 
 static void
-nbctl_lr_route_del(struct ctl_context *ctx)
+ nbctl_lr_route_del(struct ctl_context *ctx)
 {
     const struct nbrec_logical_router *lr;
     char *error = lr_by_name_or_uuid(ctx, ctx->argv[1], true, &lr);
@@ -4555,6 +4557,10 @@ nbctl_lr_route_del(struct ctl_context *ctx)
         }
 
         /* Everything matched. Removing. */
+        if (lr->static_routes[i]->bfd) {
+            nbrec_bfd_delete(lr->static_routes[i]->bfd);
+        }
+
         nbrec_logical_router_update_static_routes_delvalue(
             lr, lr->static_routes[i]);
         n_removed++;

tests/ovn-nbctl.at
@@ -1614,6 +1614,7 @@ AT_CHECK([ovn-nbctl lr-route-add lr0 0.0.0.0/0 192.168.0.1])
 AT_CHECK([ovn-nbctl lr-route-add lr0 10.0.1.0/24 11.0.1.1 lp0])
 AT_CHECK([ovn-nbctl lr-route-add lr0 10.0.0.1/24 11.0.0.2])
 AT_CHECK([ovn-nbctl lr-route-add lr0 10.0.10.0/24 lp0])
+AT_CHECK([ovn-nbctl --bfd lr-route-add lr0 10.0.20.0/24 11.0.2.1 lp0])
 AT_CHECK([ovn-nbctl lr-route-add lr0 10.0.10.0/24 lp1], [1], [],
   [ovn-nbctl: bad IPv4 nexthop argument: lp1
 ])
@@ -1667,12 +1668,17 @@ Route Table <main>:
               10.0.0.0/24                  11.0.0.1 dst-ip
               10.0.1.0/24                  11.0.1.1 dst-ip lp0
              10.0.10.0/24                           dst-ip lp0
+             10.0.20.0/24                  11.0.2.1 dst-ip lp0 bfd
               20.0.0.0/24                   discard dst-ip
               9.16.1.0/24                  11.0.0.1 src-ip
               10.0.0.0/24                  11.0.0.2 src-ip
               20.0.0.0/24                   discard src-ip
                 0.0.0.0/0               192.168.0.1 dst-ip
 ])
+
+check_row_count nb:BFD 1
+AT_CHECK([ovn-nbctl lr-route-del lr0 10.0.20.0/24])
+check_row_count nb:BFD 0
 
 AT_CHECK([ovn-nbctl lrp-add lr0 lp1 f0:00:00:00:00:02 11.0.0.254/24])
 AT_CHECK([ovn-nbctl --may-exist lr-route-add lr0 10.0.0.111/24 11.0.0.1 lp1])