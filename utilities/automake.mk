7ee8c91d24261d95ace3d6fb84bea11b9b853043
Ales Musil <amusil@redhat.com>
2023-02-10 10:28:24 +0100

    utilities: Add simple container automation
    Add simple script that allows user to run tests and builds
    using container. At the same time add example Dockerfile for
    Fedora 37.
    
    Basic usage is:
    ./utilities/containers/ci.sh
    
    This will compile the project with GCC, it expects podman
    as container platform with default image being "ovn-org/ovn-tests".
    
    The test parameters can be specified as environment variables.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- utilities/automake.mk
+++ utilities/automake.mk
@@ -37,6 +37,9 @@ EXTRA_DIST += \
     utilities/ovn_detrace.py.in \
     utilities/ovndb-servers.ocf \
     utilities/checkpatch.py \
+    utilities/containers/ci.sh \
+    utilities/containers/Makefile \
+    utilities/containers/fedora/Dockerfile \
     utilities/docker/Makefile \
     utilities/docker/start-ovn \
     utilities/docker/ovn_default_nb_port \
