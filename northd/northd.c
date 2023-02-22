ovsrobot/ovn

commit d91f359b76947a28d63b0b65491d1203a7dce1c4

northd: Add VIP port to established flows in DNAT table for Load Balancers
When adding a load balancer to a logical router, two flows are added to
the ingress DNAT table. One flow is for established connections and one is
for new connections. They have the following form:

ct.est && ip4 && reg0 == 10.0.0.10 && ct_label.natted == 1 && tcp

As the established flow does not specify the VIP port, if two load
balancers are added with the same VIP but different VIP ports, then
two conflicting flows will be added. For example,

ct.est && ip4 && reg0 == 10.0.0.10 && ct_label.natted == 1 && tcp
ct.est && ip4 && reg0 == 10.0.0.10 && ct_label.natted == 1 && tcp

This normally does not give an issue as both flows will have the same
action: next.

However, if the logical router specifies "force_snat_for_lb" and one
load balancer specifies "skip_snat" then both flows will have the
same match but different, conflicting actions: "flags.force_snat_for_lb = 1; next;"
and "flags.skip_snat_for_lb = 1; next;". This can cause unintended
consequences.

This commit adds the VIP port to the DNAT flow. It also updates
the defrag table to save that port in a register (before it gets
DNATted).

Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=1995326
Signed-off-by: Mark Gray <mark.d.gray@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/ovn-northd.8.xml
@@ -2855,15 +2855,18 @@ icmp6 {
       If load balancing rules with virtual IP addresses and ports are
       configured in <code>OVN_Northbound</code> database for a Gateway router,
       a priority-110 flow is added for each configured virtual IP address
-      <var>VIP</var> and protocol <var>PROTO</var>. For IPv4 <var>VIPs</var>
-      the flow matches <code>ip &amp;&amp; ip4.dst == <var>VIP</var> &amp;&amp;
-      <var>PROTO</var></code>. For IPv6 <var>VIPs</var>, the flow matches
+      <var>VIP</var>, protocol <var>PROTO</var> and port <var>PORT</var>.
+      For IPv4 <var>VIPs</var> the flow matches
+      <code>ip &amp;&amp; ip4.dst == <var>VIP</var> &amp;&amp;
+      <var>PROTO</var> &amp;&amp; <var>PROTO</var>.dst ==
+      <var>PORT</var></code>. For IPv6 <var>VIPs</var>, the flow matches
       <code>ip &amp;&amp; ip6.dst == <var>VIP</var> &amp;&amp;
-      <var>PROTO</var></code>. The flow applies the action <code>reg0 =
-      <var>VIP</var>; ct_dnat;</code> (or <code>xxreg0</code> for IPv6) to send
-      IP packets to the connection tracker for packet de-fragmentation and to
-      dnat the destination IP for the committed connection before sending it to
-      the next table.
+      <var>PROTO</var> &amp;&amp; <var>PROTO</var>.dst ==
+      <var>PORT</var></code>. The flow applies the action <code>reg0 =
+      <var>VIP</var>; reg9[16..31] = <var>PROTO</var>.dst; ct_dnat;</code>
+      (or <code>xxreg0</code> for IPv6) to send IP packets to the connection
+      tracker for packet de-fragmentation and to dnat the destination IP for
+      the committed connection before sending it to the next table.
     </p>
 
     <p>
@@ -2913,14 +2916,14 @@ icmp6 {
           includes a L4 port <var>PORT</var> of protocol <var>P</var> and IPv4
           or IPv6 address <var>VIP</var>, a priority-120 flow that matches on
           <code>ct.new &amp;&amp; ip &amp;&amp; reg0 == <var>VIP</var>
-          &amp;&amp; <var>P</var> &amp;&amp; <var>P</var>.dst == <var>PORT
-          </var></code> (<code>xxreg0 == <var>VIP</var></code> in the IPv6
-          case) with an action of <code>ct_lb(<var>args</var>)</code>,
-          where <var>args</var> contains comma separated IPv4 or IPv6 addresses
-          (and optional port numbers) to load balance to.  If the router is
-          configured to force SNAT any load-balanced packets, the above action
-          will be replaced by <code>flags.force_snat_for_lb = 1;
-          ct_lb(<var>args</var>);</code>.
+          &amp;&amp; <var>P</var> &amp;&amp; reg9[16..31] == </code>
+          <code><var>PORT</var></code> (<code>xxreg0 == <var>VIP</var></code>
+          in the IPv6 case) with an action of
+          <code>ct_lb(<var>args</var>)</code>, where <var>args</var> contains
+          comma separated IPv4 or IPv6 addresses (and optional port numbers) to
+          load balance to.  If the router is configured to force SNAT any
+          load-balanced packets, the above action will be replaced by
+          <code>flags.force_snat_for_lb = 1; ct_lb(<var>args</var>);</code>.
           If the load balancing rule is configured with <code>skip_snat</code>
           set to true, the above action will be replaced by
           <code>flags.skip_snat_for_lb = 1; ct_lb(<var>args</var>);</code>.
@@ -2945,14 +2948,15 @@ icmp6 {
           <var>PORT</var> of protocol <var>P</var> and IPv4 or IPv6 address
           <var>VIP</var>, a priority-120 flow that matches on
           <code>ct.est &amp;&amp; ip4 &amp;&amp; reg0 == <var>VIP</var>
-          &amp;&amp; <var>P</var> &amp;&amp; <var>P</var>.dst == <var>PORT
-          </var></code> (<code>ip6</code> and <code>xxreg0 == <var>VIP</var>
-          </code> in the IPv6 case) with an action of <code>next;</code>. If
-          the router is configured to force SNAT any load-balanced packets, the
-          above action will be replaced by <code>flags.force_snat_for_lb = 1;
-          next;</code>. If the load balancing rule is configured with
-          <code>skip_snat</code> set to true, the above action will be replaced
-          by <code>flags.skip_snat_for_lb = 1; next;</code>.
+          &amp;&amp; <var>P</var> &amp;&amp; reg9[16..31] == </code>
+          <code><var>PORT</var></code> (<code>ip6</code> and
+          <code>xxreg0 == <var>VIP</var></code> in the IPv6 case) with an
+          action of <code>next;</code>. If the router is configured to force
+          SNAT any load-balanced packets, the above action will be replaced by
+          <code>flags.force_snat_for_lb = 1; next;</code>. If the load
+          balancing rule is configured with <code>skip_snat</code> set to true,
+          the above action will be replaced by
+          <code>flags.skip_snat_for_lb = 1; next;</code>.
         </p>
 
         <p>

northd/ovn-northd.c
@@ -274,6 +274,8 @@ enum ovn_stage {
 #define REG_SRC_IPV4 "reg1"
 #define REG_SRC_IPV6 "xxreg1"
 
+#define REG_ORIG_TP_DPORT_ROUTER   "reg9[16..31]"
+
 /* Register used for setting a label for ACLs in a Logical Switch. */
 #define REG_LABEL "reg3"
 
@@ -344,6 +346,9 @@ enum ovn_stage {
  * | R9  |   PKT_LARGER/            | 4 |                 |
  * |     |   LOOKUP_NEIGHBOR_RESULT/|   |                 |
  * |     |   SKIP_LOOKUP_NEIGHBOR}  |   |                 |
+ * |     |                          |   |                 |
+ * |     | REG_ORIG_TP_DPORT_ROUTER |   |                 |
+ * |     |                          |   |                 |
  * +-----+--------------------------+---+-----------------+
  *
  */
@@ -9147,9 +9152,11 @@ build_lrouter_nat_flows_for_lb(struct ovn_lb_vip *lb_vip,
      * an action of "next;".
      */
     if (IN6_IS_ADDR_V4MAPPED(&lb_vip->vip)) {
-        ds_put_format(match, "ip4 && reg0 == %s", lb_vip->vip_str);
+        ds_put_format(match, "ip4 && "REG_NEXT_HOP_IPV4" == %s",
+                      lb_vip->vip_str);
     } else {
-        ds_put_format(match, "ip6 && xxreg0 == %s", lb_vip->vip_str);
+        ds_put_format(match, "ip6 && "REG_NEXT_HOP_IPV6" == %s",
+                      lb_vip->vip_str);
     }
 
     enum lb_snat_type snat_type = NO_FORCE_SNAT;
@@ -9164,11 +9171,13 @@ build_lrouter_nat_flows_for_lb(struct ovn_lb_vip *lb_vip,
     int prio = 110;
     if (lb_vip->vip_port) {
         prio = 120;
-        new_match = xasprintf("ct.new && %s && %s && %s.dst == %d",
-                              ds_cstr(match), lb->proto, lb->proto,
-                              lb_vip->vip_port);
-        est_match = xasprintf("ct.est && %s && ct_label.natted == 1 && %s",
-                              ds_cstr(match), lb->proto);
+        new_match = xasprintf("ct.new && %s && %s && "
+                              REG_ORIG_TP_DPORT_ROUTER" == %d",
+                              ds_cstr(match), lb->proto, lb_vip->vip_port);
+        est_match = xasprintf("ct.est && %s && %s && "
+                              REG_ORIG_TP_DPORT_ROUTER" == %d && "
+                              "ct_label.natted == 1",
+                              ds_cstr(match), lb->proto, lb_vip->vip_port);
     } else {
         new_match = xasprintf("ct.new && %s", ds_cstr(match));
         est_match = xasprintf("ct.est && %s && ct_label.natted == 1",
@@ -9399,18 +9408,23 @@ build_lrouter_defrag_flows_for_lb(struct ovn_northd_lb *lb,
 
         if (IN6_IS_ADDR_V4MAPPED(&lb_vip->vip)) {
             ds_put_format(match, "ip && ip4.dst == %s", lb_vip->vip_str);
-            ds_put_format(&defrag_actions, "reg0 = %s; ct_dnat;",
+            ds_put_format(&defrag_actions, REG_NEXT_HOP_IPV4" = %s; ",
                           lb_vip->vip_str);
         } else {
             ds_put_format(match, "ip && ip6.dst == %s", lb_vip->vip_str);
-            ds_put_format(&defrag_actions, "xxreg0 = %s; ct_dnat;",
+            ds_put_format(&defrag_actions, REG_NEXT_HOP_IPV6" = %s; ",
                           lb_vip->vip_str);
         }
 
         if (lb_vip->vip_port) {
             ds_put_format(match, " && %s", lb->proto);
             prio = 110;
+
+            ds_put_format(&defrag_actions, REG_ORIG_TP_DPORT_ROUTER
+                          " = %s.dst; ", lb->proto);
         }
+
+        ds_put_format(&defrag_actions, "ct_dnat;");
 
         for (size_t j = 0; j < lb->n_nb_lr; j++) {
             ovn_lflow_add_with_hint(lflows, lb->nb_lr[j], S_ROUTER_IN_DEFRAG,

northd/ovn_northd.dl
@@ -1596,6 +1596,9 @@ function s_ROUTER_OUT_DELIVERY():       Intern<Stage> { Stage{ Egress,  4, "lr_o
  * | R9  |   PKT_LARGER/            | 4 |                 |
  * |     |   LOOKUP_NEIGHBOR_RESULT/|   |                 |
  * |     |   SKIP_LOOKUP_NEIGHBOR}  |   |                 |
+ * |     |                          |   |                 |
+ * |     | REG_ORIG_TP_DPORT_ROUTER |   |                 |
+ * |     |                          |   |                 |
  * +-----+--------------------------+---+-----------------+
  *
  */
@@ -1644,6 +1647,8 @@ function rEG_INPORT_ETH_ADDR() : string = "xreg0[0..47]"
 /* Register for ECMP bucket selection. */
 function rEG_ECMP_GROUP_ID()  : string = "reg8[0..15]"
 function rEG_ECMP_MEMBER_ID() : string = "reg8[16..31]"
+
+function rEG_ORIG_TP_DPORT_ROUTER()  : string = "reg9[16..31]"
 
 /* Register used for setting a label for ACLs in a Logical Switch. */
 function rEG_LABEL() : string = "reg3"
@@ -3147,7 +3152,8 @@ function get_match_for_lb_key(ip_address: v46_ip,
                               port: bit<16>,
                               protocol: Option<string>,
                               redundancy: bool,
-                              use_nexthop_reg: bool): string = {
+                              use_nexthop_reg: bool,
+                              use_dest_tp_reg: bool): string = {
     var port_match = if (port != 0) {
         var proto = if (protocol == Some{"udp"}) {
             "udp"
@@ -3155,7 +3161,11 @@ function get_match_for_lb_key(ip_address: v46_ip,
             "tcp"
         };
         if (redundancy) { " && ${proto}" } else { "" } ++
-        " && ${proto}.dst == ${port}"
+        if (use_dest_tp_reg) {
+            " && ${rEG_ORIG_TP_DPORT_ROUTER()} == ${port}"
+        } else {
+            " && ${proto}.dst == ${port}"
+        }
     } else {
         ""
     };
@@ -3268,7 +3278,7 @@ Flow(.logical_datapath = sw._uuid,
     } else {
         None
     },
-    var __match = "ct.new && " ++ get_match_for_lb_key(lbvip.vip_addr, lbvip.vip_port, lb.protocol, false, false).
+    var __match = "ct.new && " ++ get_match_for_lb_key(lbvip.vip_addr, lbvip.vip_port, lb.protocol, false, false, false).
 
 /* Ingress Pre-Hairpin/Nat-Hairpin/Hairpin tabled (Priority 0).
  * Packets that don't need hairpinning should continue processing.
@@ -6731,7 +6741,14 @@ for (RouterLBVIP(
             } in
         var __match = match1 ++ match2 in
         var xx = ip_address.xxreg() in
-        var __actions = i"${xx}${rEG_NEXT_HOP()} = ${ip_address}; ct_dnat;" in
+        var actions1 = "${xx}${rEG_NEXT_HOP()} = ${ip_address}; " in
+        var actions2 =
+            if (port != 0) {
+                "${rEG_ORIG_TP_DPORT_ROUTER()} = ${proto}.dst; ct_dnat;"
+            } else {
+                "ct_dnat;"
+            } in
+        var __actions = actions1 ++ actions2 in
         /* One of these flows must be created for each unique LB VIP address.
          * We create one for each VIP:port pair; flows with the same IP and
          * different port numbers will produce identical flows that will
@@ -6740,7 +6757,7 @@ for (RouterLBVIP(
              .stage            = s_ROUTER_IN_DEFRAG(),
              .priority         = prio,
              .__match          = __match.intern(),
-             .actions          = __actions,
+             .actions          = __actions.intern(),
              .stage_hint       = 0,
              .io_port          = None,
              .controller_meter = None);
@@ -6754,7 +6771,7 @@ for (RouterLBVIP(
         var match1 = "${ipX} && ${xx}${rEG_NEXT_HOP()} == ${ip_address}" in
         (var prio, var match2) =
             if (port != 0) {
-                (120, " && ${proto} && ${proto}.dst == ${port}")
+                (120, " && ${proto} && ${rEG_ORIG_TP_DPORT_ROUTER()} == ${port}")
             } else {
                 (110, "")
             } in
@@ -6766,12 +6783,7 @@ for (RouterLBVIP(
         var snat_for_lb = snat_for_lb(r.options, lb) in
         {
             /* A match and actions for established connections. */
-            var est_match = "ct.est && " ++ match1 ++ " && ct_label.natted == 1" ++
-                if (port != 0) {
-                    " && ${proto}"
-                } else {
-                    ""
-                } ++
+            var est_match = "ct.est && " ++ match1 ++ match2 ++ " && ct_label.natted == 1" ++
                 match ((l3dgw_ports.nth(0), backends != "" or lb.options.get_bool_def("reject", false))) {
                     (Some {var gw_port}, true) -> " && is_chassis_resident(${json_string_escape(chassis_redirect_name(gw_port.name))})",
                     _ -> ""
@@ -6879,7 +6891,7 @@ Flow(.logical_datapath = r._uuid,
     r.load_balancer.contains(lb._uuid),
     var __match
         = "ct.new && " ++
-          get_match_for_lb_key(lbvip.vip_addr, lbvip.vip_port, lb.protocol, true, true) ++
+          get_match_for_lb_key(lbvip.vip_addr, lbvip.vip_port, lb.protocol, true, true, true) ++
           match (r.l3dgw_ports.nth(0)) {
               Some{gw_port} -> " && is_chassis_resident(${json_string_escape(chassis_redirect_name(gw_port.name))})",
               _ -> ""

tests/ovn.at
@@ -21098,7 +21098,7 @@ AT_CAPTURE_FILE([sbflows2])
 OVS_WAIT_FOR_OUTPUT(
   [ovn-sbctl dump-flows > sbflows2
    ovn-sbctl dump-flows lr0 | grep ct_lb | grep priority=120 | sed 's/table=..//'], 0,
-  [  (lr_in_dnat         ), priority=120  , match=(ct.new && ip4 && reg0 == 10.0.0.10 && tcp && tcp.dst == 80 && is_chassis_resident("cr-lr0-public")), action=(ct_lb(backends=10.0.0.3:80,20.0.0.3:80; hash_fields="ip_dst,ip_src,tcp_dst,tcp_src");)
+  [  (lr_in_dnat         ), priority=120  , match=(ct.new && ip4 && reg0 == 10.0.0.10 && tcp && reg9[[16..31]] == 80 && is_chassis_resident("cr-lr0-public")), action=(ct_lb(backends=10.0.0.3:80,20.0.0.3:80; hash_fields="ip_dst,ip_src,tcp_dst,tcp_src");)
 ])
 
 # get the svc monitor mac.
@@ -21139,8 +21139,8 @@ AT_CHECK(
 AT_CAPTURE_FILE([sbflows4])
 ovn-sbctl dump-flows lr0 > sbflows4
 AT_CHECK([grep lr_in_dnat sbflows4 | grep priority=120 | sed 's/table=..//' | sort], [0], [dnl
-  (lr_in_dnat         ), priority=120  , match=(ct.est && ip4 && reg0 == 10.0.0.10 && ct_label.natted == 1 && tcp && is_chassis_resident("cr-lr0-public")), action=(next;)
-  (lr_in_dnat         ), priority=120  , match=(ct.new && ip4 && reg0 == 10.0.0.10 && tcp && tcp.dst == 80 && is_chassis_resident("cr-lr0-public")), action=(drop;)
+  (lr_in_dnat         ), priority=120  , match=(ct.est && ip4 && reg0 == 10.0.0.10 && tcp && reg9[[16..31]] == 80 && ct_label.natted == 1 && is_chassis_resident("cr-lr0-public")), action=(next;)
+  (lr_in_dnat         ), priority=120  , match=(ct.new && ip4 && reg0 == 10.0.0.10 && tcp && reg9[[16..31]] == 80 && is_chassis_resident("cr-lr0-public")), action=(drop;)
 ])
 
 # Delete sw0-p1

commit 8ab9793a5fd21a36b1f912c5cb94c841ba53eabf

northd: add flows to defrag IP traffic
Introduce a priority-100 flow in the ingress router defrag stage in
order to just perform IP traffic defragmentation without doing any dnat
operation. This change is necessary since the logical flow reported
below fails for IP fragmented traffic since L4 port info is available
just in the first fragment:

table=5 (lr_in_defrag       ), priority=110  , match=(ip && ip4.dst == 172.16.0.111 && udp), action=(reg0 = 172.16.0.111; reg9[16..31] = udp.dst; ct_dnat;)

Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=2170885
Fixes: d91f359b7694 ("northd: Add VIP port to established flows in DNAT table for Load Balancers")
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -14238,6 +14238,10 @@ build_lrouter_nat_defrag_and_lb(struct ovn_datapath *od, struct hmap *lflows,
     ovn_lflow_add(lflows, od, S_ROUTER_OUT_EGR_LOOP, 0, "1", "next;");
     ovn_lflow_add(lflows, od, S_ROUTER_IN_ECMP_STATEFUL, 0, "1", "next;");
 
+    /* Add flow for defrag ip traffic. */
+    ovn_lflow_add(lflows, od, S_ROUTER_IN_DEFRAG, 100,
+                  "ip && ip.is_frag", "ct_next;");
+
     /* Ingress DNAT and DEFRAG Table (Priority 50/70).
      *
      * The defrag stage needs to have flows for ICMP in order to get

northd/ovn-northd.8.xml
@@ -3277,9 +3277,20 @@ icmp6 {
 
     <p>
       This is to send packets to connection tracker for tracking and
-      defragmentation.  It contains a priority-0 flow that simply moves traffic
-      to the next table.
+      defragmentation.
     </p>
+
+    <ul>
+      <li>
+        A priority 100 flow is added with match <code>ip &amp;&amp;
+        ip.is_frag</code> and action <code>ct_next;</code>
+      </li>
+
+      <li>
+        A priority 0 flow is added which matches on all packets and applies
+        the action <code>next;</code>.
+      </li>
+    </ul>
 
     <h3>Ingress Table 6: POST_DEFRAG</h3>
 