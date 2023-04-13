547ec1ff4eec062bf4605e25a1ddc9c95a92e45e
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2023-04-13 16:45:05 +0200

    controller: get rid of ovn-egress-iface parameter
    Remove ovn-egress-iface paramter since it is no longer used.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=2129742
    Tested-by: Rodolfo Alonso <ralonsoh@redhat.com>
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/binding.c
+++ controller/binding.c
@@ -197,9 +197,7 @@ get_qos_egress_interface_name(struct shash *bridge_mappings,
                 continue;
             }
 
-            bool is_egress_iface = smap_get_bool(&iface_rec->external_ids,
-                                                 "ovn-egress-iface", false);
-            if (is_egress_iface || !strcmp(iface_rec->type, "")) {
+            if (!strcmp(iface_rec->type, "")) {
                 return iface_rec->name;
             }
         }
@@ -457,25 +455,6 @@ add_localnet_egress_interface_mappings(
             &port_binding->options, "qos_physical_network");
     if (qos_physical_network && !strcmp(qos_physical_network, network)) {
             smap_replace(egress_ifaces, port_binding->logical_port, network);
-            return;
-    }
-
-    /* Add egress-ifaces from the connected bridge */
-    for (size_t i = 0; i < br_ln->n_ports; i++) {
-        const struct ovsrec_port *port_rec = br_ln->ports[i];
-
-        for (size_t j = 0; j < port_rec->n_interfaces; j++) {
-            const struct ovsrec_interface *iface_rec;
-
-            iface_rec = port_rec->interfaces[j];
-            bool is_egress_iface = smap_get_bool(&iface_rec->external_ids,
-                                                 "ovn-egress-iface", false);
-            if (!is_egress_iface) {
-                continue;
-            }
-            smap_replace(egress_ifaces, port_binding->logical_port,
-                         network);
-        }
     }
 }
 
