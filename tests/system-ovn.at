ovsrobot/ovn

commit ba8aa26e44cb62362d468cf835aa0951165ba51c

OVN Remote Port Mirroring: controller changes to create ovs mirrors
Mirror creation just creates the mirror. The lsp-attach-mirror
triggers the sequence to create Mirror in OVS DB on compute node.
OVS already supports Port Mirroring.

Further added test cases in ovn.at to verify end to end
the functioning of Port Mirror and also verify bulk updates
to mirrors.

Note: This is targeted to mirror to destinations anywhere outside the
cluster where the analyser resides and it need not be an OVN node.

Example commands are as below:

Mirror creation
ovn-nbctl mirror-add mirror1 gre 0 from-lport 10.10.10.2

Attach a logical port to the mirror.
ovn-nbctl lsp-attach-mirror sw0-port1 mirror1

Detach a source from Mirror
ovn-nbctl lsp-detach-mirror sw0-port1 mirror1

Mirror deletion
ovn-nbctl mirror-del mirror1

This feature is marked as experimental.

Co-authored-by: Veda Barrenkala <vedabarrenkala@gmail.com>
Signed-off-by: Veda Barrenkala <vedabarrenkala@gmail.com>
Signed-off-by: Abhiram R N <abhiramrn@gmail.com>
Acked-By: Ihar Hrachyshka <ihrachys@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/mirror.c
@@ -1,0 +1,400 @@
+/* Copyright (c) 2022 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+#include <unistd.h>
+
+/* library headers */
+#include "lib/sset.h"
+#include "lib/util.h"
+
+/* OVS includes. */
+#include "lib/vswitch-idl.h"
+#include "include/openvswitch/shash.h"
+#include "openvswitch/vlog.h"
+
+/* OVN includes. */
+#include "binding.h"
+#include "lib/ovn-sb-idl.h"
+#include "mirror.h"
+
+VLOG_DEFINE_THIS_MODULE(port_mirror);
+
+struct ovn_mirror {
+    char *name;
+    const struct sbrec_mirror *sb_mirror;
+    const struct ovsrec_mirror *ovs_mirror;
+    struct ovs_list mirror_src_lports;
+    struct ovs_list mirror_dst_lports;
+};
+
+struct mirror_lport {
+    struct ovs_list list_node;
+
+    struct local_binding *lbinding;
+};
+
+static struct ovn_mirror *ovn_mirror_create(char *mirror_name);
+static void ovn_mirror_add(struct shash *ovn_mirrors,
+                           struct ovn_mirror *);
+static struct ovn_mirror *ovn_mirror_find(struct shash *ovn_mirrors,
+                                          const char *mirror_name);
+static void ovn_mirror_delete(struct ovn_mirror *);
+static void ovn_mirror_add_lport(struct ovn_mirror *, struct local_binding *);
+static void sync_ovn_mirror(struct ovn_mirror *, struct ovsdb_idl_txn *,
+                            const struct ovsrec_bridge *);
+
+static void create_ovs_mirror(struct ovn_mirror *, struct ovsdb_idl_txn *,
+                              const struct ovsrec_bridge *);
+static void sync_ovs_mirror_ports(struct ovn_mirror *,
+                                  const struct ovsrec_bridge *);
+static void delete_ovs_mirror(struct ovn_mirror *,
+                              const struct ovsrec_bridge *);
+static bool should_delete_ovs_mirror(struct ovn_mirror *);
+static void set_mirror_iface_options(struct ovsrec_interface *,
+                                     const struct sbrec_mirror *);
+
+static const struct ovsrec_port *get_iface_port(
+    const struct ovsrec_interface *, const struct ovsrec_bridge *);
+
+
+void
+mirror_register_ovs_idl(struct ovsdb_idl *ovs_idl)
+{
+    ovsdb_idl_add_column(ovs_idl, &ovsrec_bridge_col_mirrors);
+
+    ovsdb_idl_add_table(ovs_idl, &ovsrec_table_mirror);
+    ovsdb_idl_add_column(ovs_idl, &ovsrec_mirror_col_name);
+    ovsdb_idl_add_column(ovs_idl, &ovsrec_mirror_col_output_port);
+    ovsdb_idl_add_column(ovs_idl, &ovsrec_mirror_col_select_dst_port);
+    ovsdb_idl_add_column(ovs_idl, &ovsrec_mirror_col_select_src_port);
+    ovsdb_idl_add_column(ovs_idl, &ovsrec_mirror_col_external_ids);
+}
+
+void
+mirror_init(void)
+{
+}
+
+void
+mirror_destroy(void)
+{
+}
+
+void
+mirror_run(struct ovsdb_idl_txn *ovs_idl_txn,
+           const struct ovsrec_mirror_table *ovs_mirror_table,
+           const struct sbrec_mirror_table *sb_mirror_table,
+           const struct ovsrec_bridge *br_int,
+           struct shash *local_bindings)
+{
+    if (!ovs_idl_txn) {
+        return;
+    }
+
+    struct shash ovn_mirrors = SHASH_INITIALIZER(&ovn_mirrors);
+    struct shash tmp_mirrors = SHASH_INITIALIZER(&tmp_mirrors);
+
+    /* Iterate through sb mirrors and build the 'ovn_mirrors'. */
+    const struct sbrec_mirror *sb_mirror;
+    SBREC_MIRROR_TABLE_FOR_EACH (sb_mirror, sb_mirror_table) {
+        struct ovn_mirror *m = ovn_mirror_create(sb_mirror->name);
+        m->sb_mirror = sb_mirror;
+        ovn_mirror_add(&ovn_mirrors, m);
+    }
+
+    /* Iterate through ovs mirrors and add to the 'ovn_mirrors'. */
+    const struct ovsrec_mirror *ovs_mirror;
+    OVSREC_MIRROR_TABLE_FOR_EACH (ovs_mirror, ovs_mirror_table) {
+        bool ovn_owned_mirror = smap_get_bool(&ovs_mirror->external_ids,
+                                              "ovn-owned", false);
+        if (!ovn_owned_mirror) {
+            continue;
+        }
+
+        struct ovn_mirror *m = ovn_mirror_find(&ovn_mirrors, ovs_mirror->name);
+        if (!m) {
+            m = ovn_mirror_create(ovs_mirror->name);
+            ovn_mirror_add(&ovn_mirrors, m);
+        }
+        m->ovs_mirror = ovs_mirror;
+    }
+
+    if (shash_is_empty(&ovn_mirrors)) {
+        shash_destroy(&ovn_mirrors);
+        return;
+    }
+
+    /* Iterate through the local bindings and if the local binding's 'pb' has
+     * mirrors associated, add it to the ovn_mirror. */
+    struct shash_node *node;
+    SHASH_FOR_EACH (node, local_bindings) {
+        struct local_binding *lbinding = node->data;
+        const struct sbrec_port_binding *pb =
+            local_binding_get_primary_pb(local_bindings, lbinding->name);
+        if (!pb || !pb->n_mirror_rules) {
+            continue;
+        }
+
+        for (size_t i = 0; i < pb->n_mirror_rules; i++) {
+            struct ovn_mirror *m = ovn_mirror_find(&ovn_mirrors,
+                                                   pb->mirror_rules[i]->name);
+            ovs_assert(m);
+            ovn_mirror_add_lport(m, lbinding);
+        }
+    }
+
+    /* Iterate through the built 'ovn_mirrors' and
+     * sync with the local ovsdb i.e.
+     * create/update or delete the ovsrec mirror(s). */
+     SHASH_FOR_EACH (node, &ovn_mirrors) {
+        struct ovn_mirror *m = node->data;
+        sync_ovn_mirror(m, ovs_idl_txn, br_int);
+     }
+
+    SHASH_FOR_EACH_SAFE (node, &ovn_mirrors) {
+        ovn_mirror_delete(node->data);
+        shash_delete(&ovn_mirrors, node);
+    }
+
+    shash_destroy(&ovn_mirrors);
+}
+
+/* Static functions. */
+static struct ovn_mirror *
+ovn_mirror_create(char *mirror_name)
+{
+    struct ovn_mirror *m = xzalloc(sizeof *m);
+    m->name = xstrdup(mirror_name);
+    ovs_list_init(&m->mirror_src_lports);
+    ovs_list_init(&m->mirror_dst_lports);
+    return m;
+}
+
+static void
+ovn_mirror_add(struct shash *ovn_mirrors, struct ovn_mirror *m)
+{
+    shash_add(ovn_mirrors, m->name, m);
+}
+
+static struct ovn_mirror *
+ovn_mirror_find(struct shash *ovn_mirrors, const char *mirror_name)
+{
+    return shash_find_data(ovn_mirrors, mirror_name);
+}
+
+static void
+ovn_mirror_delete(struct ovn_mirror *m)
+{
+    free(m->name);
+    struct mirror_lport *m_lport;
+    LIST_FOR_EACH_POP (m_lport, list_node, &m->mirror_src_lports) {
+        free(m_lport);
+    }
+
+    LIST_FOR_EACH_POP (m_lport, list_node, &m->mirror_dst_lports) {
+        free(m_lport);
+    }
+
+    free(m);
+}
+
+static void
+ovn_mirror_add_lport(struct ovn_mirror *m, struct local_binding *lbinding)
+{
+    struct mirror_lport *m_lport = xzalloc(sizeof *m_lport);
+    m_lport->lbinding = lbinding;
+    if (!strcmp(m->sb_mirror->filter, "from-lport")) {
+        ovs_list_push_back(&m->mirror_src_lports, &m_lport->list_node);
+    } else {
+        ovs_list_push_back(&m->mirror_dst_lports, &m_lport->list_node);
+    }
+}
+
+static void
+set_mirror_iface_options(struct ovsrec_interface *iface,
+                         const struct sbrec_mirror *sb_mirror)
+{
+    struct smap options = SMAP_INITIALIZER(&options);
+    char *key;
+
+    key = xasprintf("%ld", (long int) sb_mirror->index);
+    smap_add(&options, "remote_ip", sb_mirror->sink);
+    smap_add(&options, "key", key);
+    if (!strcmp(sb_mirror->type, "erspan")) {
+        /* Set the ERSPAN index */
+        smap_add(&options, "erspan_idx", key);
+        smap_add(&options, "erspan_ver", "1");
+    }
+    ovsrec_interface_set_options(iface, &options);
+
+    free(key);
+    smap_destroy(&options);
+}
+
+static void
+check_and_update_interface_table(const struct sbrec_mirror *sb_mirror,
+                                 const struct ovsrec_mirror *ovs_mirror)
+{
+    char *type;
+    struct ovsrec_interface *iface =
+                          ovs_mirror->output_port->interfaces[0];
+    struct smap *opts = &iface->options;
+    const char *erspan_ver = smap_get(opts, "erspan_ver");
+    if (erspan_ver) {
+        type = "erspan";
+    } else {
+        type = "gre";
+    }
+    if (strcmp(type, sb_mirror->type)) {
+        ovsrec_interface_set_type(iface, sb_mirror->type);
+    }
+    set_mirror_iface_options(iface, sb_mirror);
+}
+
+static void
+sync_ovn_mirror(struct ovn_mirror *m, struct ovsdb_idl_txn *ovs_idl_txn,
+                const struct ovsrec_bridge *br_int)
+{
+    if (should_delete_ovs_mirror(m)) {
+        /* Delete the ovsrec mirror. */
+        delete_ovs_mirror(m, br_int);
+        return;
+    }
+
+    if (ovs_list_is_empty(&m->mirror_src_lports) &&
+            ovs_list_is_empty(&m->mirror_dst_lports)) {
+        /* Nothing to do. */
+        return;
+    }
+
+    if (m->sb_mirror && !m->ovs_mirror) {
+        create_ovs_mirror(m, ovs_idl_txn, br_int);
+    } else {
+        check_and_update_interface_table(m->sb_mirror, m->ovs_mirror);
+    }
+
+    sync_ovs_mirror_ports(m, br_int);
+}
+
+static bool
+should_delete_ovs_mirror(struct ovn_mirror *m)
+{
+    if (!m->ovs_mirror) {
+        return false;
+    }
+
+    if (m->ovs_mirror && !m->sb_mirror) {
+        return true;
+    }
+
+    return (ovs_list_is_empty(&m->mirror_src_lports) &&
+            ovs_list_is_empty(&m->mirror_dst_lports));
+}
+
+static const struct ovsrec_port *
+get_iface_port(const struct ovsrec_interface *iface,
+               const struct ovsrec_bridge *br_int)
+{
+    for (size_t i = 0; i < br_int->n_ports; i++) {
+        const struct ovsrec_port *p = br_int->ports[i];
+        for (size_t j = 0; j < p->n_interfaces; j++) {
+            if (!strcmp(iface->name, p->interfaces[j]->name)) {
+                return p;
+            }
+        }
+    }
+    return NULL;
+}
+
+static void
+create_ovs_mirror(struct ovn_mirror *m, struct ovsdb_idl_txn *ovs_idl_txn,
+                  const struct ovsrec_bridge *br_int)
+{
+    struct ovsrec_interface *iface = ovsrec_interface_insert(ovs_idl_txn);
+    char *port_name = xasprintf("ovn-%s", m->name);
+
+    ovsrec_interface_set_name(iface, port_name);
+    ovsrec_interface_set_type(iface, m->sb_mirror->type);
+    set_mirror_iface_options(iface, m->sb_mirror);
+
+    struct ovsrec_port *port = ovsrec_port_insert(ovs_idl_txn);
+    ovsrec_port_set_name(port, port_name);
+    ovsrec_port_set_interfaces(port, &iface, 1);
+    ovsrec_bridge_update_ports_addvalue(br_int, port);
+
+    free(port_name);
+
+    m->ovs_mirror = ovsrec_mirror_insert(ovs_idl_txn);
+    ovsrec_mirror_set_name(m->ovs_mirror, m->name);
+    ovsrec_mirror_set_output_port(m->ovs_mirror, port);
+
+    const struct smap external_ids =
+        SMAP_CONST1(&external_ids, "ovn-owned", "true");
+    ovsrec_mirror_set_external_ids(m->ovs_mirror, &external_ids);
+
+    ovsrec_bridge_update_mirrors_addvalue(br_int, m->ovs_mirror);
+}
+
+static void
+sync_ovs_mirror_ports(struct ovn_mirror *m, const struct ovsrec_bridge *br_int)
+{
+    struct mirror_lport *m_lport;
+
+    if (ovs_list_is_empty(&m->mirror_src_lports)) {
+        ovsrec_mirror_set_select_src_port(m->ovs_mirror, NULL, 0);
+    } else {
+        size_t n_lports = ovs_list_size(&m->mirror_src_lports);
+        struct ovsrec_port **ovs_ports = xmalloc(sizeof *ovs_ports * n_lports);
+
+        size_t i = 0;
+        LIST_FOR_EACH (m_lport, list_node, &m->mirror_src_lports) {
+            const struct ovsrec_port *p =
+                get_iface_port(m_lport->lbinding->iface, br_int);
+            ovs_assert(p);
+            ovs_ports[i++] = (struct ovsrec_port *) p;
+        }
+
+        ovsrec_mirror_set_select_src_port(m->ovs_mirror, ovs_ports, n_lports);
+        free(ovs_ports);
+    }
+
+    if (ovs_list_is_empty(&m->mirror_dst_lports)) {
+        ovsrec_mirror_set_select_dst_port(m->ovs_mirror, NULL, 0);
+    } else {
+        size_t n_lports = ovs_list_size(&m->mirror_dst_lports);
+        struct ovsrec_port **ovs_ports = xmalloc(sizeof *ovs_ports * n_lports);
+
+        size_t i = 0;
+        LIST_FOR_EACH (m_lport, list_node, &m->mirror_dst_lports) {
+            const struct ovsrec_port *p =
+                get_iface_port(m_lport->lbinding->iface, br_int);
+            ovs_assert(p);
+            ovs_ports[i++] = (struct ovsrec_port *) p;
+        }
+
+        ovsrec_mirror_set_select_dst_port(m->ovs_mirror, ovs_ports, n_lports);
+        free(ovs_ports);
+    }
+}
+
+static void
+delete_ovs_mirror(struct ovn_mirror *m, const struct ovsrec_bridge *br_int)
+{
+    ovsrec_bridge_update_ports_delvalue(br_int, m->ovs_mirror->output_port);
+    ovsrec_bridge_update_mirrors_delvalue(br_int, m->ovs_mirror);
+    ovsrec_port_delete(m->ovs_mirror->output_port);
+    ovsrec_mirror_delete(m->ovs_mirror);
+}

controller/automake.mk
@@ -41,7 +41,9 @@ controller_ovn_controller_SOURCES = \
 	controller/ovsport.h \
 	controller/ovsport.c \
 	controller/vif-plug.h \
-	controller/vif-plug.c
+	controller/vif-plug.c \
+	controller/mirror.h \
+	controller/mirror.c
 
 controller_ovn_controller_LDADD = lib/libovn.la $(OVS_LIBDIR)/libopenvswitch.la
 man_MANS += controller/ovn-controller.8

controller/mirror.h
@@ -1,0 +1,33 @@
+/* Copyright (c) 2022 Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OVN_MIRROR_H
+#define OVN_MIRROR_H 1
+
+struct ovsdb_idl_txn;
+struct ovsrec_mirror_table;
+struct sbrec_mirror_table;
+struct ovsrec_bridge;
+struct shash;
+
+void mirror_register_ovs_idl(struct ovsdb_idl *);
+void mirror_init(void);
+void mirror_destroy(void);
+void mirror_run(struct ovsdb_idl_txn *ovs_idl_txn,
+                const struct ovsrec_mirror_table *,
+                const struct sbrec_mirror_table *,
+                const struct ovsrec_bridge *,
+                struct shash *local_bindings);
+#endif

controller/ovn-controller.c
@@ -78,6 +78,7 @@
 #include "lib/inc-proc-eng.h"
 #include "lib/ovn-l7.h"
 #include "hmapx.h"
+#include "mirror.h"
 
 VLOG_DEFINE_THIS_MODULE(main);
 
@@ -1012,6 +1013,7 @@ ctrl_register_ovs_idl(struct ovsdb_idl *ovs_idl)
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_port_col_name);
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_port_col_interfaces);
     ovsdb_idl_track_add_column(ovs_idl, &ovsrec_port_col_external_ids);
+    mirror_register_ovs_idl(ovs_idl);
 }
 
 #define SB_NODES \
@@ -3934,6 +3936,7 @@ main(int argc, char *argv[])
     patch_init();
     pinctrl_init();
     lflow_init();
+    mirror_init();
     vif_plug_provider_initialize();
 
     /* Connect to OVS OVSDB instance. */
@@ -4648,6 +4651,11 @@ main(int argc, char *argv[])
                                     &runtime_data->local_active_ports_ras);
                         stopwatch_stop(PINCTRL_RUN_STOPWATCH_NAME,
                                        time_msec());
+                        mirror_run(ovs_idl_txn,
+                                   ovsrec_mirror_table_get(ovs_idl_loop.idl),
+                                   sbrec_mirror_table_get(ovnsb_idl_loop.idl),
+                                   br_int,
+                                   &runtime_data->lbinding_data.bindings);
                         /* Updating monitor conditions if runtime data or
                          * logical datapath goups changed. */
                         if (engine_node_changed(&en_runtime_data)
@@ -4894,6 +4902,7 @@ loop_done:
     pinctrl_destroy();
     binding_destroy();
     patch_destroy();
+    mirror_destroy();
     if_status_mgr_destroy(if_mgr);
     shash_destroy(&vif_plug_deleted_iface_ids);
     shash_destroy(&vif_plug_changed_iface_ids);

commit 1444c5311e3b5fa70b2099f0e7e6dfbd890c196d

mirror: fix ovn mirror support with IPv6
Fix IPv6 support for OVN mirroring configuring properly interface type
in OVS interface table

Fixes: ba8aa26e44cb ("OVN Remote Port Mirroring: controller changes to create ovs mirrors")
Reported-at: https://bugzilla.redhat.com/show_bug.cgi?id=2168119
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/mirror.c
@@ -69,6 +69,7 @@ static void set_mirror_iface_options(struct ovsrec_interface *,
 static const struct ovsrec_port *get_iface_port(
     const struct ovsrec_interface *, const struct ovsrec_bridge *);
 
+const char *get_mirror_tunnel_type(const struct sbrec_mirror *);
 
 void
 mirror_register_ovs_idl(struct ovsdb_idl *ovs_idl)
@@ -244,21 +245,33 @@ set_mirror_iface_options(struct ovsrec_interface *iface,
     smap_destroy(&options);
 }
 
+const char *
+get_mirror_tunnel_type(const struct sbrec_mirror *sb_mirror)
+{
+    struct in6_addr remote_addr;
+    bool ipv6 = ipv6_parse(sb_mirror->sink, &remote_addr);
+
+    if (!strcmp(sb_mirror->type, "erspan")) {
+        if (ipv6) {
+            return "ip6erspan";
+        } else {
+            return "erspan";
+        }
+    } if (ipv6) {
+        return "ip6gre";
+    }
+
+    return "gre";
+}
+
 static void
 check_and_update_interface_table(const struct sbrec_mirror *sb_mirror,
                                  const struct ovsrec_mirror *ovs_mirror)
 {
-    char *type;
-    struct ovsrec_interface *iface =
-                          ovs_mirror->output_port->interfaces[0];
-    struct smap *opts = &iface->options;
-    const char *erspan_ver = smap_get(opts, "erspan_ver");
-    if (erspan_ver) {
-        type = "erspan";
-    } else {
-        type = "gre";
-    }
-    if (strcmp(type, sb_mirror->type)) {
+    struct ovsrec_interface *iface = ovs_mirror->output_port->interfaces[0];
+    const char *type = get_mirror_tunnel_type(sb_mirror);
+
+    if (strcmp(type, iface->type)) {
         ovsrec_interface_set_type(iface, sb_mirror->type);
     }
     set_mirror_iface_options(iface, sb_mirror);
@@ -327,7 +340,9 @@ create_ovs_mirror(struct ovn_mirror *m, struct ovsdb_idl_txn *ovs_idl_txn,
     char *port_name = xasprintf("ovn-%s", m->name);
 
     ovsrec_interface_set_name(iface, port_name);
-    ovsrec_interface_set_type(iface, m->sb_mirror->type);
+
+    const char *type = get_mirror_tunnel_type(m->sb_mirror);
+    ovsrec_interface_set_type(iface, type);
     set_mirror_iface_options(iface, m->sb_mirror);
 
     struct ovsrec_port *port = ovsrec_port_insert(ovs_idl_txn);

tests/system-ovn.at
@@ -10667,3 +10667,149 @@ OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
 /connection dropped.*/d"])
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn mirroring])
+AT_KEYWORDS([ovnnat])
+AT_SKIP_IF([test $HAVE_TCPDUMP = no])
+
+CHECK_CONNTRACK()
+CHECK_CONNTRACK_NAT()
+ovn_start
+OVS_TRAFFIC_VSWITCHD_START()
+ADD_BR([br-int])
+ADD_BR([br-mirror])
+
+# Set external-ids in br-int needed for ovn-controller
+ovs-vsctl \
+        -- set Open_vSwitch . external-ids:system-id=hv1 \
+        -- set Open_vSwitch . external-ids:ovn-remote=unix:$ovs_base/ovn-sb/ovn-sb.sock \
+        -- set Open_vSwitch . external-ids:ovn-encap-type=geneve \
+        -- set Open_vSwitch . external-ids:ovn-encap-ip=169.0.0.1 \
+        -- set bridge br-int fail-mode=secure other-config:disable-in-band=true
+
+# Start ovn-controller
+start_daemon ovn-controller
+
+ovs-ofctl add-flow br-mirror action=normal
+
+ovn-nbctl create Logical_Router name=R1 options:chassis=hv1
+
+ovn-nbctl ls-add foo
+ovn-nbctl ls-add bar
+
+# Connect foo to R1
+ovn-nbctl lrp-add R1 foo 00:00:01:01:02:03 192.168.1.1/24 2001::1/64
+ovn-nbctl lsp-add foo rp-foo -- set Logical_Switch_Port rp-foo \
+    type=router options:router-port=foo addresses=\"00:00:01:01:02:03\"
+
+# Connect bar to R1
+ovn-nbctl lrp-add R1 bar 00:00:01:01:02:04 192.168.2.1/24 2002::1/64
+ovn-nbctl lsp-add bar rp-bar -- set Logical_Switch_Port rp-bar \
+    type=router options:router-port=bar addresses=\"00:00:01:01:02:04\"
+
+# Logical port 'foo1' in switch 'foo'.
+ADD_NAMESPACES(foo1)
+ADD_VETH(foo1, foo1, br-int, "2001::2/64", "f0:00:00:01:02:03", \
+         "2001::1", "nodad", "192.168.1.2/24", "192.168.1.1")
+ovn-nbctl lsp-add foo foo1 \
+-- lsp-set-addresses foo1 "f0:00:00:01:02:03 192.168.1.2 2001::2"
+
+# Logical port 'bar1' in switch 'bar'.
+ADD_NAMESPACES(bar1)
+ADD_VETH(bar1, bar1, br-int, "2002::2/64", "f0:00:00:01:02:05", \
+         "2002::1", "nodad", "192.168.2.2/24", "192.168.2.1")
+ovn-nbctl lsp-add bar bar1 \
+-- lsp-set-addresses bar1 "f0:00:00:01:02:05 192.168.2.2 2002::2"
+
+ovn-nbctl mirror-add mirror0 gre 1 to-lport 172.16.0.100
+ovn-nbctl lsp-attach-mirror bar1 mirror0
+
+ADD_NAMESPACES(mirror)
+ADD_VETH(mirror, mirror, br-mirror, "2003::b/64", "f0:00:00:01:07:06", \
+         "2003::1", "nodad", "172.16.0.100/24", "172.16.0.1")
+AT_CHECK([ip addr add 172.16.0.101/24 dev br-mirror])
+AT_CHECK([ip addr add 2003::a/64 dev br-mirror nodad])
+AT_CHECK([ip link set dev br-mirror up])
+
+NS_CHECK_EXEC([mirror], [tcpdump -l -c 3 -neei mirror proto GRE > gre_mirror4.pcap 2>gre_mirror4_error &])
+OVS_WAIT_UNTIL([grep "listening" gre_mirror4_error])
+
+NS_CHECK_EXEC([foo1], [ping -q -c 3 -i 0.3 -w 2 192.168.2.2 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+OVS_WAIT_UNTIL([
+    n_pakets=$(grep "GRE" -c gre_mirror4.pcap)
+    test "${n_pakets}" = "3"
+])
+
+killall tcpdump
+
+ovn-nbctl mirror-del mirror0
+ovn-nbctl mirror-add mirror1 gre 2 to-lport 2003::b
+ovn-nbctl lsp-attach-mirror bar1 mirror1
+
+NS_CHECK_EXEC([mirror], [tcpdump -l -neei mirror proto GRE > gre_mirror6.pcap 2>gre_mirror6_error &])
+NS_CHECK_EXEC([bar1], [tcpdump -l -neei bar1 > bar1.pcap 2>bar1 &])
+OVS_WAIT_UNTIL([grep "listening" gre_mirror6_error])
+
+NS_CHECK_EXEC([foo1], [ping6 -q -c 3 -i 0.3 -w 2 2002::2 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+
+killall tcpdump
+
+ovn-nbctl mirror-del mirror1
+ovn-nbctl mirror-add mirror2 erspan 3 to-lport 172.16.0.100
+ovn-nbctl lsp-attach-mirror bar1 mirror2
+
+NS_CHECK_EXEC([mirror], [tcpdump -l -c 3 -neei mirror ip[[22:2]]=0x88be > erspan_mirror4.pcap 2>erspan_mirror4_error &])
+OVS_WAIT_UNTIL([grep "listening" erspan_mirror4_error])
+
+NS_CHECK_EXEC([foo1], [ping -q -c 3 -i 0.3 -w 2 192.168.2.2 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+OVS_WAIT_UNTIL([
+    n_pakets=$(grep "gre-proto-0x88be" -c erspan_mirror4.pcap)
+    test "${n_pakets}" = "3"
+])
+
+killall tcpdump
+
+ovn-nbctl mirror-del mirror2
+ovn-nbctl mirror-add mirror3 erspan 4 to-lport 2003::b
+ovn-nbctl lsp-attach-mirror bar1 mirror3
+
+NS_CHECK_EXEC([mirror], [tcpdump -l -c 3 -neei mirror ip6[[42:2]]=0x88be > erspan_mirror6.pcap 2>erspan_mirror6_error &])
+OVS_WAIT_UNTIL([grep "listening" erspan_mirror6_error])
+
+NS_CHECK_EXEC([foo1], [ping6 -q -c 3 -i 0.3 -w 2 2002::2 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
+OVS_WAIT_UNTIL([
+    n_pakets=$(grep "gre-proto-0x88be" -c erspan_mirror6.pcap)
+    test "${n_pakets}" = "3"
+])
+
+killall tcpdump
+
+OVS_APP_EXIT_AND_WAIT([ovn-controller])
+
+as ovn-sb
+OVS_APP_EXIT_AND_WAIT([ovsdb-server])
+
+as ovn-nb
+OVS_APP_EXIT_AND_WAIT([ovsdb-server])
+
+as northd
+OVS_APP_EXIT_AND_WAIT([NORTHD_TYPE])
+
+as
+OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
+/connection dropped.*/d"])
+AT_CLEANUP
+])