eac04c52ff77fc5de6aa2c7aafdf5d08ad4c65a3
Han Zhou <hzhou@ovn.org>
2023-05-12 17:03:43 -0700

    northd: Remove bfd_connections from northd_data.
    bfd_connections isn't related to the I-P engine node "northd". So move
    it from the northd_data to a local variable of en_lflow_run().
    
    Signed-off-by: Han Zhou <hzhou@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/northd.c
+++ northd/northd.c
@@ -16473,7 +16473,6 @@ northd_init(struct northd_data *data)
     shash_init(&data->meter_groups);
     hmap_init(&data->lbs);
     hmap_init(&data->lb_groups);
-    hmap_init(&data->bfd_connections);
     ovs_list_init(&data->lr_list);
     data->features = (struct chassis_features) {
         .ct_no_masked_label = true,
@@ -16504,7 +16503,6 @@ northd_destroy(struct northd_data *data)
     }
 
     hmap_destroy(&data->port_groups);
-    hmap_destroy(&data->bfd_connections);
 
     struct shash_node *node;
     SHASH_FOR_EACH_SAFE (node, &data->meter_groups) {
