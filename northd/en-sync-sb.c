ovsrobot/ovn

commit 0d5e0a6ced49f4cf059701bcfc29dd595c087c73

northd: Add I-P for syncing SB address sets.
Updates to NB address sets and NB port groups are handled
incrementally for syncing the SB address sets.  This patch
doesn't support syncing the SB Address sets for the router
load balancer vips incrementally, instead a full recompute is
triggered for any changes to NB load balancers, NB load balancer
groups and NB logical routers or if the 'en_northd' engine node
changes (which is the case for all the Northbound changes except
for the NB Address set for now).

Even though this patch has added handler for NB port group chnages,
it would still result in full recompute of 'sync_to_sb_addr_set'
node since 'en_northd' engine node doesn't yet handle NB port group
changes incrementally.

Acked-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Ales Musil <amusil@redhat.com>
Acked-by: Han Zhou <hzhou@ovn.org>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/en-sync-sb.c
@@ -22,6 +22,7 @@
 #include "openvswitch/util.h"
 
 #include "en-sync-sb.h"
+#include "include/ovn/expr.h"
 #include "lib/inc-proc-eng.h"
 #include "lib/lb.h"
 #include "lib/ovn-nb-idl.h"
@@ -41,6 +42,13 @@ static void sync_addr_sets(const struct nbrec_address_set_table *,
                            const struct sbrec_address_set_table *,
                            struct ovsdb_idl_txn *ovnsb_txn,
                            struct hmap *datapaths);
+static const struct sbrec_address_set *sb_address_set_lookup_by_name(
+    struct ovsdb_idl_index *, const char *name);
+static void update_sb_addr_set(const char **nb_addresses, size_t n_addresses,
+                               const struct sbrec_address_set *);
+static void build_port_group_address_set(const struct nbrec_port_group *,
+                                         struct svec *ipv4_addrs,
+                                         struct svec *ipv6_addrs);
 
 void *
 en_sync_to_sb_init(struct engine_node *node OVS_UNUSED,
@@ -94,6 +102,98 @@ en_sync_to_sb_addr_set_cleanup(void *data OVS_UNUSED)
 
 }
 
+bool
+sync_to_sb_addr_set_nb_address_set_handler(struct engine_node *node,
+                                           void *data OVS_UNUSED)
+{
+    const struct nbrec_address_set_table *nb_address_set_table =
+        EN_OVSDB_GET(engine_get_input("NB_address_set", node));
+
+    /* Return false if an address set is created or deleted.
+     * Handle I-P for only updated address sets. */
+    const struct nbrec_address_set *nb_addr_set;
+    NBREC_ADDRESS_SET_TABLE_FOR_EACH_TRACKED (nb_addr_set,
+                                              nb_address_set_table) {
+        if (nbrec_address_set_is_new(nb_addr_set) ||
+                nbrec_address_set_is_deleted(nb_addr_set)) {
+            return false;
+        }
+    }
+
+    struct ovsdb_idl_index *sbrec_address_set_by_name =
+        engine_ovsdb_node_get_index(
+                engine_get_input("SB_address_set", node),
+                "sbrec_address_set_by_name");
+
+    NBREC_ADDRESS_SET_TABLE_FOR_EACH_TRACKED (nb_addr_set,
+                                              nb_address_set_table) {
+        const struct sbrec_address_set *sb_addr_set =
+            sb_address_set_lookup_by_name(sbrec_address_set_by_name,
+                                          nb_addr_set->name);
+        if (!sb_addr_set) {
+            return false;
+        }
+        update_sb_addr_set((const char **) nb_addr_set->addresses,
+                           nb_addr_set->n_addresses, sb_addr_set);
+    }
+
+    return true;
+}
+
+bool
+sync_to_sb_addr_set_nb_port_group_handler(struct engine_node *node,
+                                          void *data OVS_UNUSED)
+{
+    const struct nbrec_port_group *nb_pg;
+    const struct nbrec_port_group_table *nb_port_group_table =
+        EN_OVSDB_GET(engine_get_input("NB_port_group", node));
+    NBREC_PORT_GROUP_TABLE_FOR_EACH_TRACKED (nb_pg, nb_port_group_table) {
+        if (nbrec_port_group_is_new(nb_pg) ||
+                nbrec_port_group_is_deleted(nb_pg)) {
+            return false;
+        }
+    }
+
+    struct ovsdb_idl_index *sbrec_address_set_by_name =
+        engine_ovsdb_node_get_index(
+                engine_get_input("SB_address_set", node),
+                "sbrec_address_set_by_name");
+    NBREC_PORT_GROUP_TABLE_FOR_EACH_TRACKED (nb_pg, nb_port_group_table) {
+        char *ipv4_addrs_name = xasprintf("%s_ip4", nb_pg->name);
+        const struct sbrec_address_set *sb_addr_set_v4 =
+            sb_address_set_lookup_by_name(sbrec_address_set_by_name,
+                                          ipv4_addrs_name);
+        if (!sb_addr_set_v4) {
+            free(ipv4_addrs_name);
+            return false;
+        }
+        char *ipv6_addrs_name = xasprintf("%s_ip6", nb_pg->name);
+        const struct sbrec_address_set *sb_addr_set_v6 =
+            sb_address_set_lookup_by_name(sbrec_address_set_by_name,
+                                          ipv6_addrs_name);
+        if (!sb_addr_set_v6) {
+            free(ipv4_addrs_name);
+            free(ipv6_addrs_name);
+            return false;
+        }
+
+        struct svec ipv4_addrs = SVEC_EMPTY_INITIALIZER;
+        struct svec ipv6_addrs = SVEC_EMPTY_INITIALIZER;
+        build_port_group_address_set(nb_pg, &ipv4_addrs, &ipv6_addrs);
+        update_sb_addr_set((const char **) ipv4_addrs.names, ipv4_addrs.n,
+                           sb_addr_set_v4);
+        update_sb_addr_set((const char **) ipv6_addrs.names, ipv6_addrs.n,
+                           sb_addr_set_v6);
+
+        free(ipv4_addrs_name);
+        free(ipv6_addrs_name);
+        svec_destroy(&ipv4_addrs);
+        svec_destroy(&ipv6_addrs);
+    }
+
+    return true;
+}
+
 /* static functions. */
 static void
 sync_addr_set(struct ovsdb_idl_txn *ovnsb_txn, const char *name,
@@ -106,10 +206,11 @@ sync_addr_set(struct ovsdb_idl_txn *ovnsb_txn, const char *name,
     if (!sb_address_set) {
         sb_address_set = sbrec_address_set_insert(ovnsb_txn);
         sbrec_address_set_set_name(sb_address_set, name);
+        sbrec_address_set_set_addresses(sb_address_set,
+                                        addrs, n_addrs);
+    } else {
+        update_sb_addr_set(addrs, n_addrs, sb_address_set);
     }
-
-    sbrec_address_set_set_addresses(sb_address_set,
-                                    addrs, n_addrs);
 }
 
 /* OVN_Southbound Address_Set table contains same records as in north
@@ -151,18 +252,7 @@ sync_addr_sets(const struct nbrec_address_set_table *nb_address_set_table,
                                      nb_port_group_table) {
         struct svec ipv4_addrs = SVEC_EMPTY_INITIALIZER;
         struct svec ipv6_addrs = SVEC_EMPTY_INITIALIZER;
-        for (size_t i = 0; i < nb_port_group->n_ports; i++) {
-            for (size_t j = 0; j < nb_port_group->ports[i]->n_addresses; j++) {
-                const char *addrs = nb_port_group->ports[i]->addresses[j];
-                if (!is_dynamic_lsp_address(addrs)) {
-                    split_addresses(addrs, &ipv4_addrs, &ipv6_addrs);
-                }
-            }
-            if (nb_port_group->ports[i]->dynamic_addresses) {
-                split_addresses(nb_port_group->ports[i]->dynamic_addresses,
-                                &ipv4_addrs, &ipv6_addrs);
-            }
-        }
+        build_port_group_address_set(nb_port_group, &ipv4_addrs, &ipv6_addrs);
         char *ipv4_addrs_name = xasprintf("%s_ip4", nb_port_group->name);
         char *ipv6_addrs_name = xasprintf("%s_ip6", nb_port_group->name);
         sync_addr_set(ovnsb_txn, ipv4_addrs_name,
@@ -230,4 +320,86 @@ sync_addr_sets(const struct nbrec_address_set_table *nb_address_set_table,
         shash_delete(&sb_address_sets, node);
     }
     shash_destroy(&sb_address_sets);
+}
+
+static void
+update_sb_addr_set(const char **nb_addresses, size_t n_addresses,
+                   const struct sbrec_address_set *sb_as)
+{
+    struct expr_constant_set *cs_nb_as =
+        expr_constant_set_create_integers(
+            (const char *const *) nb_addresses, n_addresses);
+    struct expr_constant_set *cs_sb_as =
+        expr_constant_set_create_integers(
+            (const char *const *) sb_as->addresses, sb_as->n_addresses);
+
+    struct expr_constant_set *addr_added = NULL;
+    struct expr_constant_set *addr_deleted = NULL;
+    expr_constant_set_integers_diff(cs_sb_as, cs_nb_as, &addr_added,
+                                    &addr_deleted);
+
+    struct ds ds = DS_EMPTY_INITIALIZER;
+    if (addr_added && addr_added->n_values) {
+        for (size_t i = 0; i < addr_added->n_values; i++) {
+            ds_clear(&ds);
+            expr_constant_format(&addr_added->values[i], EXPR_C_INTEGER, &ds);
+            sbrec_address_set_update_addresses_addvalue(sb_as, ds_cstr(&ds));
+        }
+    }
+
+    if (addr_deleted && addr_deleted->n_values) {
+        for (size_t i = 0; i < addr_deleted->n_values; i++) {
+            ds_clear(&ds);
+            expr_constant_format(&addr_deleted->values[i],
+                                 EXPR_C_INTEGER, &ds);
+            sbrec_address_set_update_addresses_delvalue(sb_as, ds_cstr(&ds));
+        }
+    }
+
+    ds_destroy(&ds);
+    expr_constant_set_destroy(cs_nb_as);
+    free(cs_nb_as);
+    expr_constant_set_destroy(cs_sb_as);
+    free(cs_sb_as);
+    expr_constant_set_destroy(addr_added);
+    free(addr_added);
+    expr_constant_set_destroy(addr_deleted);
+    free(addr_deleted);
+}
+
+static void
+build_port_group_address_set(const struct nbrec_port_group *nb_port_group,
+                             struct svec *ipv4_addrs,
+                             struct svec *ipv6_addrs)
+{
+    for (size_t i = 0; i < nb_port_group->n_ports; i++) {
+        for (size_t j = 0; j < nb_port_group->ports[i]->n_addresses; j++) {
+            const char *addrs = nb_port_group->ports[i]->addresses[j];
+            if (!is_dynamic_lsp_address(addrs)) {
+                split_addresses(addrs, ipv4_addrs, ipv6_addrs);
+            }
+        }
+        if (nb_port_group->ports[i]->dynamic_addresses) {
+            split_addresses(nb_port_group->ports[i]->dynamic_addresses,
+                            ipv4_addrs, ipv6_addrs);
+        }
+    }
+}
+
+/* Finds and returns the address set with the given 'name', or NULL if no such
+ * address set exists. */
+static const struct sbrec_address_set *
+sb_address_set_lookup_by_name(struct ovsdb_idl_index *sbrec_addr_set_by_name,
+                              const char *name)
+{
+    struct sbrec_address_set *target = sbrec_address_set_index_init_row(
+        sbrec_addr_set_by_name);
+    sbrec_address_set_index_set_name(target, name);
+
+    struct sbrec_address_set *retval = sbrec_address_set_index_find(
+        sbrec_addr_set_by_name, target);
+
+    sbrec_address_set_index_destroy_row(target);
+
+    return retval;
 }

northd/en-sync-sb.h
@@ -11,4 +11,9 @@ void *en_sync_to_sb_addr_set_init(struct engine_node *, struct engine_arg *);
 void en_sync_to_sb_addr_set_run(struct engine_node *, void *data);
 void en_sync_to_sb_addr_set_cleanup(void *data);
 
+bool sync_to_sb_addr_set_nb_address_set_handler(struct engine_node *,
+                                                void *data);
+bool sync_to_sb_addr_set_nb_port_group_handler(struct engine_node *,
+                                               void *data);
+
 #endif /* end of EN_SYNC_SB_H */

northd/inc-proc-northd.c
@@ -232,8 +232,10 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
      * once I-P engine allows multiple root nodes. */
     engine_add_input(&en_lflow, &en_mac_binding_aging, NULL);
 
-    engine_add_input(&en_sync_to_sb_addr_set, &en_nb_address_set, NULL);
-    engine_add_input(&en_sync_to_sb_addr_set, &en_nb_port_group, NULL);
+    engine_add_input(&en_sync_to_sb_addr_set, &en_nb_address_set,
+                     sync_to_sb_addr_set_nb_address_set_handler);
+    engine_add_input(&en_sync_to_sb_addr_set, &en_nb_port_group,
+                     sync_to_sb_addr_set_nb_port_group_handler);
     engine_add_input(&en_sync_to_sb_addr_set, &en_northd, NULL);
     engine_add_input(&en_sync_to_sb_addr_set, &en_sb_address_set, NULL);
 
@@ -290,6 +292,12 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_ovsdb_node_add_index(&en_sb_mac_binding,
                                 "sbrec_mac_binding_by_datapath",
                                 sbrec_mac_binding_by_datapath);
+
+    struct ovsdb_idl_index *sbrec_address_set_by_name
+        = ovsdb_idl_index_create1(sb->idl, &sbrec_address_set_col_name);
+    engine_ovsdb_node_add_index(&en_sb_address_set,
+                                "sbrec_address_set_by_name",
+                                sbrec_address_set_by_name);
 }
 
 void inc_proc_northd_run(struct ovsdb_idl_txn *ovnnb_txn,

northd/ovn-northd.c
@@ -796,6 +796,8 @@ main(int argc, char *argv[])
 
     /* Disable alerting for pure write-only columns. */
     ovsdb_idl_omit_alert(ovnsb_idl_loop.idl, &sbrec_sb_global_col_nb_cfg);
+    ovsdb_idl_omit_alert(ovnsb_idl_loop.idl, &sbrec_address_set_col_name);
+    ovsdb_idl_omit_alert(ovnsb_idl_loop.idl, &sbrec_address_set_col_addresses);
 
     unixctl_command_register("sb-connection-status", "", 0, 0,
                              ovn_conn_show, ovnsb_idl_loop.idl);

tests/ovn-northd.at
@@ -8007,3 +8007,105 @@ AT_CHECK([grep "lr_in_lb_aff_learn" R1flows | sort], [0], [dnl
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([Address set incremental processing])
+ovn_start
+
+# Create a few resources.
+check ovn-nbctl ls-add sw0
+check ovn-nbctl lsp-add sw0 sw0-p1 -- lsp-set-addresses sw0-p1 "02:ac:10:01:00:04 20.0.0.4"
+check ovn-nbctl lsp-add sw0 sw0-p2 -- lsp-set-addresses sw0-p2 "02:ac:10:01:00:05 20.0.0.5"
+
+fetch_column NB:Logical_Switch_Port _uuid name=sw0-p1
+
+ovn-nbctl list logical_switch_port name=sw0-p1
+
+p1_uuid=$(fetch_column nb:Logical_Switch_Port _uuid name=sw0-p1)
+p2_uuid=$(fetch_column nb:Logical_Switch_Port _uuid name=sw0-p2)
+
+echo "p1 uuid - $p1_uuid"
+ovn-nbctl --wait=sb sync
+
+check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
+foo_as_uuid=$(ovn-nbctl create address_set name=foo addresses=\"1.1.1.1\",\"1.1.1.2\")
+wait_column '1.1.1.1 1.1.1.2' Address_Set addresses name=foo
+AT_CHECK([as northd ovn-appctl -t NORTHD_TYPE inc-engine/show-stats sync_to_sb_addr_set recompute], [0], [1
+])
+
+rm -f northd/ovn-northd.log
+check as northd ovn-appctl -t NORTHD_TYPE vlog/reopen
+check as northd ovn-appctl -t NORTHD_TYPE vlog/set jsonrpc:dbg
+check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
+check ovn-nbctl add address_set $foo_as_uuid addresses 1.1.1.3
+wait_column '1.1.1.1 1.1.1.2 1.1.1.3' Address_Set addresses name=foo
+
+# There should be no recompute of the sync_to_sb_addr_set engine node .
+AT_CHECK([as northd ovn-appctl -t NORTHD_TYPE inc-engine/show-stats sync_to_sb_addr_set recompute], [0], [0
+])
+
+AT_CHECK([grep transact northd/ovn-northd.log | grep Address_Set | \
+grep -c mutate], [0], [1
+])
+
+check ovn-nbctl add address_set $foo_as_uuid addresses \
+1.1.1.4 -- remove address_set $foo_as_uuid addresses 1.1.1.1
+wait_column '1.1.1.2 1.1.1.3 1.1.1.4' Address_Set addresses name=foo
+
+# There should be no recompute of the sync_to_sb_addr_set engine node .
+AT_CHECK([as northd ovn-appctl -t NORTHD_TYPE inc-engine/show-stats sync_to_sb_addr_set recompute], [0], [0
+])
+
+AT_CHECK([grep transact northd/ovn-northd.log | grep Address_Set | \
+grep -c mutate], [0], [2
+])
+
+# Pause ovn-northd and add/remove few addresses.  when it is resumed
+# it should use mutate for updating the address sets.
+check as northd ovn-appctl -t NORTHD_TYPE pause
+check as northd-backup ovn-appctl -t NORTHD_TYPE pause
+
+check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
+check ovn-nbctl add address_set $foo_as_uuid addresses 1.1.1.5
+check ovn-nbctl add address_set $foo_as_uuid addresses 1.1.1.6
+check ovn-nbctl remove address_set $foo_as_uuid addresses 1.1.1.2
+
+check_column '1.1.1.2 1.1.1.3 1.1.1.4' Address_Set addresses name=foo
+
+# Resume northd now
+check as northd ovn-appctl -t NORTHD_TYPE resume
+wait_column '1.1.1.3 1.1.1.4 1.1.1.5 1.1.1.6' Address_Set addresses name=foo
+# There should be recompute of the sync_to_sb_addr_set engine node .
+recompute_stat=$(as northd ovn-appctl -t NORTHD_TYPE inc-engine/show-stats sync_to_sb_addr_set recompute)
+AT_CHECK([test $recompute_stat -ge 1])
+
+AT_CHECK([grep transact northd/ovn-northd.log | grep Address_Set | \
+grep -c mutate], [0], [3
+])
+
+# Create a port group. This should result in recompute of sb_to_sync_addr_set engine node.
+check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
+check ovn-nbctl pg-add pg1
+wait_column '' Address_Set addresses name=pg1_ip4
+recompute_stat=$(as northd ovn-appctl -t NORTHD_TYPE inc-engine/show-stats sync_to_sb_addr_set recompute)
+AT_CHECK([test $recompute_stat -ge 1])
+
+# Add sw0-p1 to port group pg1
+check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
+check ovn-nbctl add port_group pg1 ports ${p1_uuid}
+wait_column '20.0.0.4' Address_Set addresses name=pg1_ip4
+
+# There should be recompute of the sync_to_sb_addr_set engine node since northd engine changes.
+# There will be another recompute when the update message is received from the sb ovsdb-server.
+# Once we add I-P for Port_Groups, there should be no recompute here.
+recompute_stat=$(as northd ovn-appctl -t NORTHD_TYPE inc-engine/show-stats sync_to_sb_addr_set recompute)
+AT_CHECK([test $recompute_stat -ge 1])
+
+# Any change to northd engine node should result in full recompute of sync_to_sb_addr_set node.
+check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
+check ovn-nbctl --wait=sb sync
+recompute_stat=$(as northd ovn-appctl -t NORTHD_TYPE inc-engine/show-stats sync_to_sb_addr_set recompute)
+AT_CHECK([test $recompute_stat -ge 1])
+
+AT_CLEANUP
+])

commit c2e5c3e80f4eb45b64c88efd3a9be25eeaf69bc6

northd: Fix address set incremental processing
The incremental processing is broken for addresses
that have mask which could "erase" portion of the address
itself e.g. 10.16.0.47/4, after applying the mask with token
parser the address becomes 0.0.0.0/4, which is fine for
logical flows. However, for the deletion/addition to database
we need the original string representation which cannot be
built from the parsed token.

Use svec instead for the comparison which allows us to keep the
original strings.

The change to svec shouldn't have any significant performance
impact, see the numbers below show. The setup was created by
the scale script from Han [0].

Numbers with expr:
Time spent on processing nb_cfg 1:
        ovn-northd delay before processing:     21ms
        ovn-northd completion:                  544ms
        ovn-controller(s) completion:           544ms
Time spent on processing nb_cfg 2:
        ovn-northd delay before processing:     17ms
        ovn-northd completion:                  537ms
        ovn-controller(s) completion:           535ms
Time spent on processing nb_cfg 3:
        ovn-northd delay before processing:     20ms
        ovn-northd completion:                  552ms
        ovn-controller(s) completion:           550ms
Time spent on processing nb_cfg 4:
        ovn-northd delay before processing:     16ms
        ovn-northd completion:                  529ms
        ovn-controller(s) completion:           528ms

Numbers with svec:
Time spent on processing nb_cfg 1:
        ovn-northd delay before processing:     19ms
        ovn-northd completion:                  548ms
        ovn-controller(s) completion:           548ms
Time spent on processing nb_cfg 2:
        ovn-northd delay before processing:     19ms
        ovn-northd completion:                  537ms
        ovn-controller(s) completion:           537ms
Time spent on processing nb_cfg 3:
        ovn-northd delay before processing:     15ms
        ovn-northd completion:                  522ms
        ovn-controller(s) completion:           521ms
Time spent on processing nb_cfg 4:
        ovn-northd delay before processing:     17ms
        ovn-northd completion:                  522ms
        ovn-controller(s) completion:           520ms

[0] https://github.com/hzhou8/ovn-test-script/blob/e1149318201309db6d96ae8d5a33fcfbbe1872a3/test_ovn_controller_scale.sh

Fixes: 0d5e0a6ced49 ("northd: Add I-P for syncing SB address sets.")

northd/en-sync-sb.c
@@ -22,7 +22,6 @@
 #include "openvswitch/util.h"
 
 #include "en-sync-sb.h"
-#include "include/ovn/expr.h"
 #include "lib/inc-proc-eng.h"
 #include "lib/lb.h"
 #include "lib/ovn-nb-idl.h"
@@ -325,45 +324,40 @@ static void
 update_sb_addr_set(const char **nb_addresses, size_t n_addresses,
                    const struct sbrec_address_set *sb_as)
 {
-    struct expr_constant_set *cs_nb_as =
-        expr_constant_set_create_integers(
-            (const char *const *) nb_addresses, n_addresses);
-    struct expr_constant_set *cs_sb_as =
-        expr_constant_set_create_integers(
-            (const char *const *) sb_as->addresses, sb_as->n_addresses);
+    size_t i;
+    char *ip;
 
-    struct expr_constant_set *addr_added = NULL;
-    struct expr_constant_set *addr_deleted = NULL;
-    expr_constant_set_integers_diff(cs_sb_as, cs_nb_as, &addr_added,
-                                    &addr_deleted);
+    struct svec svec_nb_as = SVEC_EMPTY_INITIALIZER;
+    struct svec svec_sb_as = SVEC_EMPTY_INITIALIZER;
 
-    struct ds ds = DS_EMPTY_INITIALIZER;
-    if (addr_added && addr_added->n_values) {
-        for (size_t i = 0; i < addr_added->n_values; i++) {
-            ds_clear(&ds);
-            expr_constant_format(&addr_added->values[i], EXPR_C_INTEGER, &ds);
-            sbrec_address_set_update_addresses_addvalue(sb_as, ds_cstr(&ds));
-        }
+    for (i = 0; i < n_addresses; i++) {
+        svec_add(&svec_nb_as, nb_addresses[i]);
     }
 
-    if (addr_deleted && addr_deleted->n_values) {
-        for (size_t i = 0; i < addr_deleted->n_values; i++) {
-            ds_clear(&ds);
-            expr_constant_format(&addr_deleted->values[i],
-                                 EXPR_C_INTEGER, &ds);
-            sbrec_address_set_update_addresses_delvalue(sb_as, ds_cstr(&ds));
-        }
+
+    for (i = 0; i < sb_as->n_addresses; i++) {
+        svec_add(&svec_sb_as, sb_as->addresses[i]);
     }
 
-    ds_destroy(&ds);
-    expr_constant_set_destroy(cs_nb_as);
-    free(cs_nb_as);
-    expr_constant_set_destroy(cs_sb_as);
-    free(cs_sb_as);
-    expr_constant_set_destroy(addr_added);
-    free(addr_added);
-    expr_constant_set_destroy(addr_deleted);
-    free(addr_deleted);
+    struct svec addr_added;
+    struct svec addr_deleted;
+
+    svec_sort(&svec_nb_as);
+    svec_sort(&svec_sb_as);
+    svec_diff(&svec_nb_as, &svec_sb_as, &addr_added, NULL, &addr_deleted);
+
+    SVEC_FOR_EACH (i, ip, &addr_added) {
+        sbrec_address_set_update_addresses_addvalue(sb_as, ip);
+    }
+
+    SVEC_FOR_EACH (i, ip, &addr_deleted) {
+        sbrec_address_set_update_addresses_delvalue(sb_as, ip);
+    }
+
+    svec_destroy(&svec_nb_as);
+    svec_destroy(&svec_sb_as);
+    svec_destroy(&addr_added);
+    svec_destroy(&addr_deleted);
 }
 
 static void