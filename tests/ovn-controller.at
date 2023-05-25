ovsrobot/ovn

commit c0fe8dca767fb5e516a1905a1c473d58275a2b06

ovn-controller: Tracking SB address set updates.
This patch tracks the added and removed addresses for each updated
address sets.

Signed-off-by: Han Zhou <hzhou@ovn.org>
Acked-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Numan Siddique <numans@ovn.org>

controller/ovn-controller.c
@@ -1399,7 +1399,7 @@ struct ed_type_addr_sets {
     bool change_tracked;
     struct sset new;
     struct sset deleted;
-    struct sset updated;
+    struct shash updated;
 };
 
 static void *
@@ -1412,19 +1412,44 @@ en_addr_sets_init(struct engine_node *node OVS_UNUSED,
     as->change_tracked = false;
     sset_init(&as->new);
     sset_init(&as->deleted);
-    sset_init(&as->updated);
+    shash_init(&as->updated);
     return as;
+}
+
+struct addr_set_diff {
+    struct expr_constant_set *added;
+    struct expr_constant_set *deleted;
+};
+
+static void
+en_addr_sets_clear_tracked_data(void *data)
+{
+    struct ed_type_addr_sets *as = data;
+    sset_clear(&as->new);
+    sset_clear(&as->deleted);
+    struct shash_node *node, *next;
+    SHASH_FOR_EACH_SAFE (node, next, &as->updated) {
+        struct addr_set_diff *asd = node->data;
+        expr_constant_set_destroy(asd->added);
+        free(asd->added);
+        expr_constant_set_destroy(asd->deleted);
+        free(asd->deleted);
+    }
+    shash_clear_free_data(&as->updated);
+    as->change_tracked = false;
 }
 
 static void
 en_addr_sets_cleanup(void *data)
 {
+    en_addr_sets_clear_tracked_data(data);
+
     struct ed_type_addr_sets *as = data;
     expr_const_sets_destroy(&as->addr_sets);
     shash_destroy(&as->addr_sets);
     sset_destroy(&as->new);
     sset_destroy(&as->deleted);
-    sset_destroy(&as->updated);
+    shash_destroy(&as->updated);
 }
 
 /* Iterate address sets in the southbound database.  Create and update the
@@ -1443,8 +1468,8 @@ addr_sets_init(const struct sbrec_address_set_table *address_set_table,
 
 static void
 addr_sets_update(const struct sbrec_address_set_table *address_set_table,
-                 struct shash *addr_sets, struct sset *new,
-                 struct sset *deleted, struct sset *updated)
+                 struct shash *addr_sets, struct sset *added,
+                 struct sset *deleted, struct shash *updated)
 {
     const struct sbrec_address_set *as;
     SBREC_ADDRESS_SET_TABLE_FOR_EACH_TRACKED (as, address_set_table) {
@@ -1452,13 +1477,23 @@ addr_sets_update(const struct sbrec_address_set_table *address_set_table,
             expr_const_sets_remove(addr_sets, as->name);
             sset_add(deleted, as->name);
         } else {
-            expr_const_sets_add_integers(addr_sets, as->name,
-                                         (const char *const *) as->addresses,
-                                         as->n_addresses);
-            if (sbrec_address_set_is_new(as)) {
-                sset_add(new, as->name);
+            struct expr_constant_set *cs_old = shash_find_data(addr_sets,
+                                                               as->name);
+            if (!cs_old) {
+                sset_add(added, as->name);
+                expr_const_sets_add_integers(addr_sets, as->name,
+                    (const char *const *) as->addresses, as->n_addresses);
             } else {
-                sset_add(updated, as->name);
+                /* Find out the diff for the updated address set. */
+                struct expr_constant_set *cs_new =
+                    expr_constant_set_create_integers(
+                        (const char *const *) as->addresses, as->n_addresses);
+                struct addr_set_diff *as_diff = xmalloc(sizeof *as_diff);
+                expr_constant_set_integers_diff(cs_old, cs_new,
+                                                &as_diff->added,
+                                                &as_diff->deleted);
+                shash_add(updated, as->name, as_diff);
+                expr_const_sets_add(addr_sets, as->name, cs_new);
             }
         }
     }
@@ -1469,9 +1504,6 @@ en_addr_sets_run(struct engine_node *node, void *data)
 {
     struct ed_type_addr_sets *as = data;
 
-    sset_clear(&as->new);
-    sset_clear(&as->deleted);
-    sset_clear(&as->updated);
     expr_const_sets_destroy(&as->addr_sets);
 
     struct sbrec_address_set_table *as_table =
@@ -1489,10 +1521,6 @@ addr_sets_sb_address_set_handler(struct engine_node *node, void *data)
 {
     struct ed_type_addr_sets *as = data;
 
-    sset_clear(&as->new);
-    sset_clear(&as->deleted);
-    sset_clear(&as->updated);
-
     struct sbrec_address_set_table *as_table =
         (struct sbrec_address_set_table *)EN_OVSDB_GET(
             engine_get_input("SB_address_set", node));
@@ -1501,7 +1529,7 @@ addr_sets_sb_address_set_handler(struct engine_node *node, void *data)
                      &as->deleted, &as->updated);
 
     if (!sset_is_empty(&as->new) || !sset_is_empty(&as->deleted) ||
-            !sset_is_empty(&as->updated)) {
+            !shash_is_empty(&as->updated)) {
         engine_set_node_state(node, EN_UPDATED);
     } else {
         engine_set_node_state(node, EN_UNCHANGED);
@@ -2492,14 +2520,10 @@ lflow_output_sb_port_binding_handler(struct engine_node *node, void *data)
 }
 
 static bool
-_lflow_output_resource_ref_handler(struct engine_node *node, void *data,
-                                  enum ref_type ref_type)
+lflow_output_addr_sets_handler(struct engine_node *node, void *data)
 {
     struct ed_type_addr_sets *as_data =
         engine_get_input_data("addr_sets", node);
-
-    struct ed_type_port_groups *pg_data =
-        engine_get_input_data("port_groups", node);
 
     struct ed_type_lflow_output *fo = data;
 
@@ -2509,42 +2533,13 @@ _lflow_output_resource_ref_handler(struct engine_node *node, void *data,
 
     bool changed;
     const char *ref_name;
-    struct sset *new, *updated, *deleted;
 
-    switch (ref_type) {
-        case REF_TYPE_ADDRSET:
-            /* XXX: The change_tracked check may be added to inc-proc
-             * framework. */
-            if (!as_data->change_tracked) {
-                return false;
-            }
-            new = &as_data->new;
-            updated = &as_data->updated;
-            deleted = &as_data->deleted;
-            break;
-        case REF_TYPE_PORTGROUP:
-            if (!pg_data->change_tracked) {
-                return false;
-            }
-            new = &pg_data->new;
-            updated = &pg_data->updated;
-            deleted = &pg_data->deleted;
-            break;
-
-        case REF_TYPE_PORTBINDING:
-            /* This ref type is handled in:
-             * - flow_output_runtime_data_handler
-             * - flow_output_sb_port_binding_handler. */
-        case REF_TYPE_MC_GROUP:
-            /* This ref type is handled in the
-             * flow_output_sb_multicast_group_handler. */
-        default:
-            OVS_NOT_REACHED();
+    if (!as_data->change_tracked) {
+        return false;
     }
 
-
-    SSET_FOR_EACH (ref_name, deleted) {
-        if (!lflow_handle_changed_ref(ref_type, ref_name, &l_ctx_in,
+    SSET_FOR_EACH (ref_name, &as_data->deleted) {
+        if (!lflow_handle_changed_ref(REF_TYPE_ADDRSET, ref_name, &l_ctx_in,
                                       &l_ctx_out, &changed)) {
             return false;
         }
@@ -2552,17 +2547,18 @@ _lflow_output_resource_ref_handler(struct engine_node *node, void *data,
             engine_set_node_state(node, EN_UPDATED);
         }
     }
-    SSET_FOR_EACH (ref_name, updated) {
-        if (!lflow_handle_changed_ref(ref_type, ref_name, &l_ctx_in,
-                                      &l_ctx_out, &changed)) {
+    struct shash_node *shash_node;
+    SHASH_FOR_EACH (shash_node, &as_data->updated) {
+        if (!lflow_handle_changed_ref(REF_TYPE_ADDRSET, shash_node->name,
+                                      &l_ctx_in, &l_ctx_out, &changed)) {
             return false;
         }
         if (changed) {
             engine_set_node_state(node, EN_UPDATED);
         }
     }
-    SSET_FOR_EACH (ref_name, new) {
-        if (!lflow_handle_changed_ref(ref_type, ref_name, &l_ctx_in,
+    SSET_FOR_EACH (ref_name, &as_data->new) {
+        if (!lflow_handle_changed_ref(REF_TYPE_ADDRSET, ref_name, &l_ctx_in,
                                       &l_ctx_out, &changed)) {
             return false;
         }
@@ -2575,15 +2571,53 @@ _lflow_output_resource_ref_handler(struct engine_node *node, void *data,
 }
 
 static bool
-lflow_output_addr_sets_handler(struct engine_node *node, void *data)
-{
-    return _lflow_output_resource_ref_handler(node, data, REF_TYPE_ADDRSET);
-}
-
-static bool
 lflow_output_port_groups_handler(struct engine_node *node, void *data)
 {
-    return _lflow_output_resource_ref_handler(node, data, REF_TYPE_PORTGROUP);
+    struct ed_type_port_groups *pg_data =
+        engine_get_input_data("port_groups", node);
+
+    struct ed_type_lflow_output *fo = data;
+
+    struct lflow_ctx_in l_ctx_in;
+    struct lflow_ctx_out l_ctx_out;
+    init_lflow_ctx(node, fo, &l_ctx_in, &l_ctx_out);
+
+    bool changed;
+    const char *ref_name;
+
+    if (!pg_data->change_tracked) {
+        return false;
+    }
+
+    SSET_FOR_EACH (ref_name, &pg_data->deleted) {
+        if (!lflow_handle_changed_ref(REF_TYPE_PORTGROUP, ref_name, &l_ctx_in,
+                                      &l_ctx_out, &changed)) {
+            return false;
+        }
+        if (changed) {
+            engine_set_node_state(node, EN_UPDATED);
+        }
+    }
+    SSET_FOR_EACH (ref_name, &pg_data->updated) {
+        if (!lflow_handle_changed_ref(REF_TYPE_PORTGROUP, ref_name, &l_ctx_in,
+                                      &l_ctx_out, &changed)) {
+            return false;
+        }
+        if (changed) {
+            engine_set_node_state(node, EN_UPDATED);
+        }
+    }
+    SSET_FOR_EACH (ref_name, &pg_data->new) {
+        if (!lflow_handle_changed_ref(REF_TYPE_PORTGROUP, ref_name, &l_ctx_in,
+                                      &l_ctx_out, &changed)) {
+            return false;
+        }
+        if (changed) {
+            engine_set_node_state(node, EN_UPDATED);
+        }
+    }
+
+    return true;
 }
 
 static bool
@@ -3176,7 +3210,7 @@ main(int argc, char *argv[])
     ENGINE_NODE(pflow_output, "physical_flow_output");
     ENGINE_NODE_WITH_CLEAR_TRACK_DATA(lflow_output, "logical_flow_output");
     ENGINE_NODE(flow_output, "flow_output");
-    ENGINE_NODE(addr_sets, "addr_sets");
+    ENGINE_NODE_WITH_CLEAR_TRACK_DATA(addr_sets, "addr_sets");
     ENGINE_NODE_WITH_CLEAR_TRACK_DATA(port_groups, "port_groups");
 
 #define SB_NODE(NAME, NAME_STR) ENGINE_NODE_SB(NAME, NAME_STR);

include/ovn/expr.h
@@ -540,6 +540,13 @@ struct expr_constant_set {
 bool expr_constant_set_parse(struct lexer *, struct expr_constant_set *);
 void expr_constant_set_format(const struct expr_constant_set *, struct ds *);
 void expr_constant_set_destroy(struct expr_constant_set *cs);
+struct expr_constant_set * expr_constant_set_create_integers(
+                                const char *const *values, size_t n_values);
+void expr_constant_set_integers_diff(
+                                struct expr_constant_set *old,
+                                struct expr_constant_set *new,
+                                struct expr_constant_set **p_diff_added,
+                                struct expr_constant_set **p_diff_deleted);
 
 
 /* Constant sets.
@@ -553,7 +560,8 @@ void expr_constant_set_destroy(struct expr_constant_set *cs);
  * integer/masked-integer values. The values that don't qualify
  * are ignored.
  */
-
+void expr_const_sets_add(struct shash *const_sets, const char *name,
+                         struct expr_constant_set *);
 void expr_const_sets_add_integers(struct shash *const_sets, const char *name,
                                   const char * const *values, size_t n_values);
 void expr_const_sets_add_strings(struct shash *const_sets, const char *name,

lib/expr.c
@@ -1084,17 +1084,33 @@ expr_constant_set_destroy(struct expr_constant_set *cs)
     }
 }
 
-/* Adds an constant set named 'name' to 'const_sets', replacing any existing
- * constant set entry with the given name. The 'values' must be strings that
+static int
+compare_expr_constant_integer_cb(const void *a_, const void *b_)
+{
+    const union expr_constant *a = a_;
+    const union expr_constant *b = b_;
+
+    int d = memcmp(&a->value, &b->value, sizeof a->value);
+    if (d) {
+        return d;
+    }
+
+    if (!a->masked && !b->masked) {
+        return 0;
+    } else if (a->masked && !b->masked) {
+        return -1;
+    } else if (!a->masked && b->masked) {
+        return 1;
+    }
+    return memcmp(&a->mask, &b->mask, sizeof a->mask);
+}
+
+/* Create an integer type constant set. The 'values' must be strings that
  * can be converted to integers or masked integers, such as IP addresses.
  * Values that can't be converted are skipped. */
-void
-expr_const_sets_add_integers(struct shash *const_sets, const char *name,
-                             const char *const *values, size_t n_values)
+struct expr_constant_set *
+expr_constant_set_create_integers(const char *const *values, size_t n_values)
 {
-    /* Replace any existing entry for this name. */
-    expr_const_sets_remove(const_sets, name);
-
     struct expr_constant_set *cs = xzalloc(sizeof *cs);
     cs->in_curlies = true;
     cs->n_values = 0;
@@ -1122,7 +1138,103 @@ expr_const_sets_add_integers(struct shash *const_sets, const char *name,
         lexer_destroy(&lex);
     }
 
+    /* Sort the result, so that it is efficient to generate diffs in the
+     * function expr_constant_set_diff */
+    qsort(cs->values, cs->n_values, sizeof *cs->values,
+          compare_expr_constant_integer_cb);
+
+    return cs;
+}
+
+static void
+expr_constant_set_add_value(struct expr_constant_set **p_cs,
+                            union expr_constant *c, size_t *allocated)
+{
+    struct expr_constant_set *cs = *p_cs;
+    if (!cs) {
+        cs = xzalloc(sizeof *cs);
+        *p_cs = cs;
+    }
+
+    if (cs->n_values >= *allocated) {
+        cs->values = x2nrealloc(cs->values, allocated,
+                                sizeof *cs->values);
+    }
+    cs->values[cs->n_values++] = *c;
+}
+
+/* Find the differences between old and new. Both old and new must be integer
+ * type and must be sorted (which is true if they are generated by
+ * expr_constant_set_create_integers() or expr_const_sets_add_integers().
+ *
+ * The differences, added and deleted elements, are stored in p_diff_added and
+ * p_diff_deleted respectively. Caller takes the ownership of these.
+ *
+ * *p_diff_added and *p_diff_deleted can be NULL, if no such elements found. */
+void
+expr_constant_set_integers_diff(struct expr_constant_set *old,
+                                struct expr_constant_set *new,
+                                struct expr_constant_set **p_diff_added,
+                                struct expr_constant_set **p_diff_deleted)
+{
+    struct expr_constant_set *diff_added = NULL;
+    struct expr_constant_set *diff_deleted = NULL;
+
+    size_t oi, ni, added_n_allocated, deleted_n_allocated;
+    added_n_allocated = deleted_n_allocated = 0;
+
+    for (oi = ni = 0; oi < old->n_values && ni < new->n_values;) {
+        int d = compare_expr_constant_integer_cb(&old->values[oi],
+                                                 &new->values[ni]);
+        if (d < 0) {
+            expr_constant_set_add_value(&diff_deleted, &old->values[oi],
+                                        &deleted_n_allocated);
+            oi++;
+        } else if (d > 0) {
+            expr_constant_set_add_value(&diff_added, &new->values[ni],
+                                        &added_n_allocated);
+            ni++;
+        } else {
+            oi++; ni++;
+        }
+    }
+
+    for (; oi < old->n_values; oi++) {
+        expr_constant_set_add_value(&diff_deleted, &old->values[oi],
+                                    &deleted_n_allocated);
+    }
+
+    for (; ni < new->n_values; ni++) {
+        expr_constant_set_add_value(&diff_added, &new->values[ni],
+                                    &added_n_allocated);
+    }
+
+    *p_diff_added = diff_added;
+    *p_diff_deleted = diff_deleted;
+}
+
+
+/* Adds an constant set named 'name' to 'const_sets', replacing any existing
+ * constant set entry with the given name. */
+void
+expr_const_sets_add(struct shash *const_sets, const char *name,
+                    struct expr_constant_set *cs)
+{
+    expr_const_sets_remove(const_sets, name);
     shash_add(const_sets, name, cs);
+}
+
+/* Adds an constant set named 'name' to 'const_sets', replacing any existing
+ * constant set entry with the given name. The 'values' must be strings that
+ * can be converted to integers or masked integers, such as IP addresses.
+ * Values that can't be converted are skipped. */
+void
+expr_const_sets_add_integers(struct shash *const_sets, const char *name,
+                             const char *const *values, size_t n_values)
+{
+    struct expr_constant_set *cs = expr_constant_set_create_integers(values,
+                                                                     n_values);
+    expr_const_sets_add(const_sets, name, cs);
 }
 
 /* Adds an constant set named 'name' to 'const_sets', replacing any existing
@@ -1135,9 +1247,6 @@ expr_const_sets_add_strings(struct shash *const_sets, const char *name,
                             const char *const *values, size_t n_values,
                             const struct sset *filter)
 {
-    /* Replace any existing entry for this name. */
-    expr_const_sets_remove(const_sets, name);
-
     struct expr_constant_set *cs = xzalloc(sizeof *cs);
     cs->in_curlies = true;
     cs->n_values = 0;
@@ -1154,7 +1263,7 @@ expr_const_sets_add_strings(struct shash *const_sets, const char *name,
         c->string = xstrdup(values[i]);
     }
 
-    shash_add(const_sets, name, cs);
+    expr_const_sets_add(const_sets, name, cs);
 }
 
 void

commit 79d5a3f0a4653e917ef55167d4da388751b47117

ovn-controller.c: Fix assertion failure during address set update.
When an address set is deleted and a new one is created immediately with
the same name, the OVSDB deletion and creation notifications can come to
ovn-controller within the same message, and the order of the deletion
and addition in the IDL is undefined. In this case, if the deletion
happens to be processed after the addition, it will result in problems.

If the content of the new address set is the same as the old one, the
addition will be handled as no change to an existing address set and
ignored, but later when handling the deletion, the related flows will be
deleted, while we actually expect no change to the flows.

If the content of the new address set is different from the old one, the
addition will be handled as an update to the old address set, and the
delta will be tracked in the I-P engine node data's "updated" member.
Later when handling the deletion, the address set will be deleted from
the engine node data. So at this point the tracked change in "updated"
doesn't have a related address set object in the data, so later when the
lflow node handles the addr_sets changes, the assertion fails as shown
in below example backtrace:

0  0x00007ffbe5fcabc5 in raise () from /lib64/libc.so.6
1  0x00007ffbe5fb38a4 in abort () from /lib64/libc.so.6
2  0x00000000004f579e in ovs_abort_valist (err_no=<optimized out>, format=<optimized out>, args=<optimized out>) at ../lib/util.c:444
3  0x00000000004fd001 in vlog_abort_valist (module_=<optimized out>, message=0x5e6fd0 "%s: assertion %s failed in %s()", args=args@entry=0x7ffed304caa8) at ../lib/vlog.c:1249
4  0x00000000004fd097 in vlog_abort (module=<optimized out>, message=<optimized out>) at ../lib/vlog.c:1263
5  0x00000000004f54e1 in ovs_assert_failure (where=<optimized out>, function=<optimized out>, condition=<optimized out>) at ../lib/util.c:86
6  0x000000000041941d in as_update_can_be_handled (l_ctx_in=0x7ffed304cda0, l_ctx_in=0x7ffed304cda0, as_diff=0x25520f0, as_name=0x2550a00 "as1") at ../controller/lflow.c:766
7  lflow_handle_addr_set_update (as_name=0x2550a00 "as1", as_diff=0x25520f0, l_ctx_in=0x7ffed304cda0, l_ctx_out=0x7ffed304cd50, changed=0x7ffed304cd4f) at ../controller/lflow.c:869
8  0x0000000000438376 in lflow_output_addr_sets_handler (node=0x7ffed3054420, data=<optimized out>) at ../controller/ovn-controller.c:2648
9  0x000000000045708e in engine_compute (recompute_allowed=<optimized out>, node=<optimized out>) at ../lib/inc-proc-eng.c:417
10 engine_run_node (recompute_allowed=true, node=0x7ffed3054420) at ../lib/inc-proc-eng.c:479
11 engine_run (recompute_allowed=true) at ../lib/inc-proc-eng.c:504
12 0x000000000040939b in main (argc=<optimized out>, argv=<optimized out>) at ../controller/ovn-controller.c:3757

This patch fixes it by handling deletions before additions/updates,
similar to how we handle other changes such as lflow and port-groups.

Fixes: c0fe8dca767f ("ovn-controller: Tracking SB address set updates.")

controller/ovn-controller.c
@@ -2015,7 +2015,11 @@ addr_sets_update(const struct sbrec_address_set_table *address_set_table,
         if (sbrec_address_set_is_deleted(as)) {
             expr_const_sets_remove(addr_sets, as->name);
             sset_add(deleted, as->name);
-        } else {
+        }
+    }
+
+    SBREC_ADDRESS_SET_TABLE_FOR_EACH_TRACKED (as, address_set_table) {
+        if (!sbrec_address_set_is_deleted(as)) {
             struct expr_constant_set *cs_old = shash_find_data(addr_sets,
                                                                as->name);
             if (!cs_old) {

tests/ovn-controller.at
@@ -2065,6 +2065,57 @@ AT_CHECK([echo $(($reprocess_count_new - $reprocess_count_old))], [0], [2
 OVN_CLEANUP([hv1])
 AT_CLEANUP
 
+AT_SETUP([ovn-controller - address set del-and-add])
+
+ovn_start
+
+net_add n1
+sim_add hv1
+as hv1
+check ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+check ovs-vsctl -- add-port br-int hv1-vif1 -- \
+    set interface hv1-vif1 external-ids:iface-id=ls1-lp1
+
+check ovn-nbctl ls-add ls1
+
+check ovn-nbctl lsp-add ls1 ls1-lp1 \
+-- lsp-set-addresses ls1-lp1 "f0:00:00:00:00:01"
+
+wait_for_ports_up
+ovn-appctl -t ovn-controller vlog/set file:dbg
+
+ovn-nbctl create address_set name=as1 addresses=8.8.8.8
+check ovn-nbctl acl-add ls1 to-lport 100 'outport == "ls1-lp1" && ip4.src == $as1' drop
+check ovn-nbctl --wait=hv sync
+AT_CHECK([ovs-ofctl dump-flows br-int table=44 | grep -c "priority=1100"], [0], [1
+])
+
+# pause ovn-northd
+check as northd ovn-appctl -t ovn-northd pause
+check as northd-backup ovn-appctl -t ovn-northd pause
+
+# Simulate a SB address set "del and add" notification to ovn-controller in the
+# same IDL iteration. The flows programmed by ovn-controller should reflect the
+# newly added address set. In reality it can happen when CMS deletes an
+# address-set and immediately creates a new address-set with the same name
+# (with same or different content). The notification of the changes can come to
+# ovn-controller in one shot and the order of the "del" and "add" in the IDL is
+# undefined. This test runs the scenario ten times to make sure different
+# orders are covered and handled properly.
+
+flow_count=$(ovs-ofctl dump-flows br-int table=44 | grep -c "priority=1100")
+for i in $(seq 10); do
+    # Delete and recreate the SB address set with same name and an extra IP.
+    addrs_=$(fetch_column address_set addresses name=as1)
+    addrs=${addrs_// /,}
+    AT_CHECK([ovn-sbctl destroy address_set as1 -- create address_set name=as1 addresses=$addrs,1.1.1.$i], [0], [ignore])
+    OVS_WAIT_UNTIL([test $(as hv1 ovs-ofctl dump-flows br-int table=44 | grep -c "priority=1100") = "$(($i + 1))"])
+done
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+
 AT_SETUP([ovn-controller - I-P handle lb_hairpin_use_ct_mark change])
 
 ovn_start --backup-northd=none