3f9ed304fce66c3b1fcd98932fb56261b3b26feb
Numan Siddique <numans@ovn.org>
2023-07-25 23:08:45 +0530

    northd: Add initial I-P for load balancer and load balancer groups
    Any changes to load balancers and load balancer groups
    are handled incrementally in the newly added 'lb_data'
    engine node.  'lb_data' is input to 'northd' node
    and the handler - northd_lb_data_handler in 'northd'
    node handles the changes.
    
    If a load balancer or load balancer group is associated to
    a logical switch or router then 'northd' node falls back
    to full recompute.  Upcoming patch will handle this scenario.
    
    Signed-off-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/en-lb-data.h
+++ northd/en-lb-data.h
@@ -4,8 +4,41 @@
 #include <config.h>
 
 #include "openvswitch/hmap.h"
+#include "include/openvswitch/list.h"
+#include "lib/hmapx.h"
 
 #include "lib/inc-proc-eng.h"
+
+struct ovn_northd_lb;
+struct ovn_lb_group;
+
+struct crupdated_lb_group {
+    struct ovn_lb_group *lbg;
+
+    /* hmapx of newly associated lbs to this lb group.
+     * hmapx node is 'struct ovn_northd_lb *' */
+    struct hmapx assoc_lbs;
+};
+
+struct tracked_lb_data {
+    /* Both created and updated lbs. hmapx node is 'struct ovn_northd_lb *'. */
+    struct hmapx crupdated_lbs;
+    struct hmapx deleted_lbs;
+
+    /* Both created and updated lb_groups. hmapx node is
+     * 'struct crupdated_lb_group'. */
+    struct hmapx crupdated_lb_groups;
+
+    /* Deleted lb_groups. hmapx node is  'struct ovn_lb_group *'. */
+    struct hmapx deleted_lb_groups;
+
+    /* Indicates if any of the tracked lb has health checks enabled. */
+    bool has_health_checks;
+
+    /* Indicates if any lb got disassociated from a lb group
+     * but not deleted. */
+    bool has_dissassoc_lbs_from_lb_grops;
+};
 
 /* struct which maintains the data of the engine node lb_data. */
 struct ed_type_lb_data {
@@ -14,10 +47,18 @@ struct ed_type_lb_data {
 
     /* hmap of load balancer groups.  hmap node is 'struct ovn_lb_group *' */
     struct hmap lb_groups;
+
+    /* tracked data*/
+    bool tracked;
+    struct tracked_lb_data tracked_lb_data;
 };
 
 void *en_lb_data_init(struct engine_node *, struct engine_arg *);
 void en_lb_data_run(struct engine_node *, void *data);
 void en_lb_data_cleanup(void *data);
+void en_lb_data_clear_tracked_data(void *data);
+
+bool lb_data_load_balancer_handler(struct engine_node *, void *data);
+bool lb_data_load_balancer_group_handler(struct engine_node *, void *data);
 
 #endif /* end of EN_NORTHD_LB_DATA_H */
