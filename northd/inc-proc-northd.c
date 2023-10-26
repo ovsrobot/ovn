4822f61771c2ad80f7e0c54815928bee1b245a67
Numan Siddique <numans@ovn.org>
2023-10-26 14:16:21 -0400

    northd: Refactor lflow management into a separate module.
    ovn_lflow_add() and other related functions/macros are now moved
    into a separate module - lflow-mgr.c.  This module maintains a
    table 'struct lflow_table' for the logical flows.  lflow table
    maintains a hmap to store the logical flows.
    
    It also maintains the logical switch and router dp groups.
    
    Previous commits which added lflow incremental processing for
    the VIF logical ports, stored the references to
    the logical ports' lflows using 'struct lflow_ref_list'.  This
    struct is renamed to 'struct lflow_ref' and is part of lflow-mgr.c.
    It is  modified a bit to store the resource to lflow references.
    
    Example usage of 'struct lflow_ref'.
    
    'struct ovn_port' maintains 2 instances of lflow_ref.  i,e
    
    struct ovn_port {
       ...
       ...
       struct lflow_ref *lflow_ref;
       struct lflow_ref *lbnat_lflow_ref;
    };
    
    All the logical flows generated by
    build_lswitch_and_lrouter_iterate_by_lsp() uses the ovn_port->lflow_ref.
    
    All the logical flows generated by build_lsp_lflows_for_lbnats()
    uses the ovn_port->lbnat_lflow_ref.
    
    When handling the ovn_port changes incrementally, the lflows referenced
    in 'struct ovn_port' are cleared and regenerated and synced to the
    SB logical flows.
    
    eg.
    
    lflow_ref_clear_lflows(op->lflow_ref);
    build_lswitch_and_lrouter_iterate_by_lsp(op, ...);
    lflow_ref_sync_lflows_to_sb(op->lflow_ref, ...);
    
    This patch does few more changes which are significant:
      -  Logical flows are now hashed without the logical
         datapaths.  If a logical flow is referenced by just one
         datapath, we don't rehash it.
    
      -  The synthetic 'hash' column of sbrec_logical_flow now
         doesn't use the logical datapath.  This means that
         when ovn-northd is updated/upgraded and has this commit,
         all the logical flows with 'logical_datapath' column
         set will get deleted and re-added causing some disruptions.
    
      -  With the commit [1] which added I-P support for logical
         port changes, multiple logical flows with same match 'M'
         and actions 'A' are generated and stored without the
         dp groups, which was not the case prior to
         that patch.
         One example to generate these lflows is:
                 ovn-nbctl lsp-set-addresses sw0p1 "MAC1 IP1"
                 ovn-nbctl lsp-set-addresses sw1p1 "MAC1 IP1"
    	     ovn-nbctl lsp-set-addresses sw2p1 "MAC1 IP1"
    
         Now with this patch we go back to the earlier way.  i.e
         one logical flow with logical_dp_groups set.
    
      -  With this patch any updates to a logical port which
         doesn't result in new logical flows will not result in
         deletion and addition of same logical flows.
         Eg.
         ovn-nbctl set logical_switch_port sw0p1 external_ids:foo=bar
         will be a no-op to the SB logical flow table.
    
    [1] - 8bbd678("northd: Incremental processing of VIF additions in 'lflow' node.")
    
    Signed-off-by: Numan Siddique <numans@ovn.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/inc-proc-northd.c
+++ northd/inc-proc-northd.c
@@ -99,7 +99,8 @@ static unixctl_cb_func chassis_features_list;
     SB_NODE(bfd, "bfd") \
     SB_NODE(fdb, "fdb") \
     SB_NODE(static_mac_binding, "static_mac_binding") \
-    SB_NODE(chassis_template_var, "chassis_template_var")
+    SB_NODE(chassis_template_var, "chassis_template_var") \
+    SB_NODE(logical_dp_group, "logical_dp_group")
 
 enum sb_engine_node {
 #define SB_NODE(NAME, NAME_STR) SB_##NAME,
@@ -234,6 +235,7 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_lflow, &en_sb_logical_flow, NULL);
     engine_add_input(&en_lflow, &en_sb_multicast_group, NULL);
     engine_add_input(&en_lflow, &en_sb_igmp_group, NULL);
+    engine_add_input(&en_lflow, &en_sb_logical_dp_group, NULL);
     engine_add_input(&en_lflow, &en_northd, lflow_northd_handler);
     engine_add_input(&en_lflow, &en_port_group, lflow_port_group_handler);
     engine_add_input(&en_lflow, &en_lr_lb_nat_data, NULL);
