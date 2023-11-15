ovsrobot/ovn

commit 60a53abaa38a60d4c0792ce74d262b00da9c5312

ci: Add automation for building the containers
In order to use the containers in CI build
the containers regularly so everything is up
to date. Build both Dockerfiles (Fedora, Ubuntu)
that are currently available.

Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

.github/workflows/containers.yml
@@ -1,0 +1,55 @@
+name: Containers
+on:
+  # Be able to run the job manually when needed
+  workflow_dispatch:
+  # Build every week on Monday 00:00
+  schedule:
+    - cron:  '0 0 * * 1'
+
+env:
+  IMAGE_REGISTRY: ghcr.io
+  IMAGE_NAMESPACE: ovn-org
+  IMAGE_NAME: ovn-tests
+  CONTAINERS_PATH: ./utilities/containers
+  DEPENDENCIES: podman
+
+jobs:
+  container:
+    runs-on: ubuntu-latest
+    strategy:
+      matrix:
+        distro: [ fedora, ubuntu ]
+    steps:
+      - uses: actions/checkout@v3
+
+      - name: Update APT cache
+        run: sudo apt update
+
+      - name: Install dependencies
+        run: sudo apt install -y ${{ env.DEPENDENCIES }}
+
+      - name: Set up QEMU
+        uses: docker/setup-qemu-action@v2
+
+      - name: Build container images
+        id: build-image
+        uses: redhat-actions/buildah-build@v2
+        with:
+          image: ${{ env.IMAGE_NAME }}
+          archs: amd64, arm64
+          tags: ${{ matrix.distro }}
+          build-args: CONTAINERS_PATH=${{ env.CONTAINERS_PATH }}
+          dockerfiles: ${{ env.CONTAINERS_PATH }}/${{ matrix.distro }}/Dockerfile
+
+      - name: Push to ghcr.io
+        id: push-to-registry
+        uses: redhat-actions/push-to-registry@v2
+        with:
+          image: ${{ steps.build-image.outputs.image }}
+          tags: ${{ steps.build-image.outputs.tags }}
+          registry: ${{ env.IMAGE_REGISTRY }}/${{ env.IMAGE_NAMESPACE }}
+          username: ${{ github.actor }}
+          password: ${{ secrets.GITHUB_TOKEN }}
+
+      - name: Print image url
+        run: echo "Image pushed to ${{ steps.push-to-registry.outputs.registry-paths }}"

Makefile.am
@@ -93,6 +93,7 @@ EXTRA_DIST = \
 	.ci/ovn-kubernetes/Dockerfile \
 	.ci/ovn-kubernetes/prepare.sh \
 	.ci/ovn-kubernetes/custom.patch \
+	.github/workflows/containers.yml \
 	.github/workflows/test.yml \
 	.github/workflows/ovn-kubernetes.yml \
 	boot.sh \

commit 2c11e18621774590a0efc7010d1155b1a7333f35

ci: Use per-branch CI container images.
That allows us to use different distro bases for different stable
branches.

Fixes: 60a53abaa38a ("ci: Add automation for building the containers")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

.ci/ci.sh
@@ -20,7 +20,8 @@ DPDK_PATH=${DPDK_PATH:-$OVN_PATH/dpdk-dir}
 CONTAINER_CMD=${CONTAINER_CMD:-podman}
 CONTAINER_WORKSPACE="/workspace"
 CONTAINER_WORKDIR="/workspace/ovn-tmp"
-IMAGE_NAME=${IMAGE_NAME:-"ovn-org/ovn-tests"}
+DEFAULT_IMAGE_NAME="ovn-org/ovn-tests:main-fedora"
+IMAGE_NAME=${IMAGE_NAME:-${DEFAULT_IMAGE_NAME}}
 
 # Test variables
 ARCH=${ARCH:-$(uname -m)}
@@ -165,6 +166,12 @@ fi
 
 if [ -z "$DPDK" ]; then
    mkdir -p "$DPDK_PATH"
+fi
+
+# Check if the IMAGE_NAME is a real image we can pull, otherwise fall
+# back to DEFAULT_IMAGE_NAME.
+if ! $CONTAINER_CMD pull $IMAGE_NAME; then
+    IMAGE_NAME=$DEFAULT_IMAGE_NAME
 fi
 
 CONTAINER_ID="$($CONTAINER_CMD run --privileged -d \

.cirrus.yml
@@ -1,7 +1,7 @@
 arm_unit_tests_task:
 
   arm_container:
-    image: ghcr.io/ovn-org/ovn-tests:fedora
+    image: ghcr.io/ovn-org/ovn-tests:main-fedora
     memory: 4G
     cpu: 2
 

.github/workflows/containers.yml
@@ -7,8 +7,6 @@ on:
     - cron:  '0 0 * * 1'
 
 env:
-  IMAGE_REGISTRY: ghcr.io
-  IMAGE_NAMESPACE: ovn-org
   IMAGE_NAME: ovn-tests
   CONTAINERS_PATH: ./utilities/containers
   DEPENDENCIES: podman
@@ -31,13 +29,22 @@ jobs:
       - name: Set up QEMU
         uses: docker/setup-qemu-action@v2
 
+      - name: Build image name
+        run: |
+          echo "IMAGE_BRANCH=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}" \
+            >> $GITHUB_ENV
+          echo "IMAGE_REGISTRY=ghcr.io" \
+            >> $GITHUB_ENV
+          echo "IMAGE_NAMESPACE=$GITHUB_REPOSITORY_OWNER" \
+            >> $GITHUB_ENV
+
       - name: Build container images
         id: build-image
         uses: redhat-actions/buildah-build@v2
         with:
           image: ${{ env.IMAGE_NAME }}
           archs: amd64, arm64
-          tags: ${{ matrix.distro }}
+          tags: ${{ env.IMAGE_BRANCH }}-${{ matrix.distro }}
           build-args: CONTAINERS_PATH=${{ env.CONTAINERS_PATH }}
           dockerfiles: ${{ env.CONTAINERS_PATH }}/${{ matrix.distro }}/Dockerfile
 

.github/workflows/test.yml
@@ -83,7 +83,6 @@ jobs:
   build-linux:
     needs: build-dpdk
     env:
-      IMAGE_NAME:  ghcr.io/ovn-org/ovn-tests:ubuntu
       ARCH:        ${{ matrix.cfg.arch }}
       CC:          ${{ matrix.cfg.compiler }}
       DPDK:        ${{ matrix.cfg.dpdk }}
@@ -127,6 +126,12 @@ jobs:
       uses: actions/checkout@v3
       with:
         submodules: recursive
+
+    - name: Build image name
+      run: |
+        IMAGE_BRANCH=${GITHUB_HEAD_REF:-${GITHUB_REF#refs/heads/}}
+        echo "IMAGE_NAME=ghcr.io/$GITHUB_REPOSITORY_OWNER/ovn-tests:${IMAGE_BRANCH}-ubuntu" \
+          >> $GITHUB_ENV
 
     # For weekly runs, don't update submodules
     - name: checkout without submodule

utilities/containers/Makefile
@@ -1,5 +1,5 @@
 CONTAINER_CMD ?= podman
-IMAGE_NAME ?= "ovn-org/ovn-tests"
+IMAGE_NAME ?= "ovn-org/ovn-tests:main"
 CONTAINERS_PATH ?= "."
 
 distros := fedora ubuntu
@@ -7,5 +7,5 @@ distros := fedora ubuntu
 .PHONY: $(distros)
 
 $(distros):
-	$(CONTAINER_CMD) build --no-cache --rm -t $(IMAGE_NAME):$@ \
+	$(CONTAINER_CMD) build --no-cache --rm -t $(IMAGE_NAME)-$@ \
 	-f $@/Dockerfile . --build-arg=CONTAINERS_PATH=$(CONTAINERS_PATH)