ovsrobot/ovn

commit a35725a7a24b85e1d83a047b2d8ee583d5a2de30

pinctrl: send RST instead of RST_ACK bit for lb hc
Currently an ovn-controller send RST_ACK after receive SYN_ACK in
load balancer health check, but for a windows as load balancer
backend this behavior unexcpected and windows send SYN_ACK like a
tcp retransmission after every RST_ACK, finaly after few reties windows
send RST bit to ovn-controller. In this case the svc_mon status flapping
between online and offline, which triger the ovn-northd for each changes
and increase CPU-usage ovn-northd to 100%.

This patch fixing this behavior for windows and doesn't affect linux with
send RST instead of RST_ACK after received SYN_ACK.

In RFC1122 section 4.2.2.13 described the ways for closing a connection:
https://datatracker.ietf.org/doc/html/rfc1122#page-87
A TCP connection may terminate in two ways: (1) the normal
    TCP close sequence using a FIN handshake, and (2) an "abort"
    in which one or more RST segments are sent and the
    connection state is immediately discarded.
For example, nmap tool use the same logic with RST for scaning ports.

Signed-off-by: Evgenii Kovalev <ekovalev.off@gmail.com>
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/pinctrl.c
@@ -7827,7 +7827,6 @@ pinctrl_handle_tcp_svc_check(struct rconn *swconn,
         return false;
     }
 
-    uint32_t tcp_seq = ntohl(get_16aligned_be32(&th->tcp_seq));
     uint32_t tcp_ack = ntohl(get_16aligned_be32(&th->tcp_ack));
 
     if (th->tcp_dst != svc_mon->tp_src) {
@@ -7845,9 +7844,9 @@ pinctrl_handle_tcp_svc_check(struct rconn *swconn,
         svc_mon->state = SVC_MON_S_ONLINE;
 
         /* Send RST-ACK packet. */
-        svc_monitor_send_tcp_health_check__(swconn, svc_mon, TCP_RST | TCP_ACK,
-                                            htonl(tcp_ack + 1),
-                                            htonl(tcp_seq + 1), th->tcp_dst);
+        svc_monitor_send_tcp_health_check__(swconn, svc_mon, TCP_RST,
+                                            htonl(tcp_ack),
+                                            htonl(0), th->tcp_dst);
         /* Calculate next_send_time. */
         svc_mon->next_send_time = time_msec() + svc_mon->interval;
         return true;

commit 35d0b2af4bd5ec3ca78a60abfa88362709298c8c

pinctrl: Fix up comments about sending RST packets for healthcheck.
We're sending TCP RST not RST-ACK as the comment suggests.

Reported-at: https://mail.openvswitch.org/pipermail/ovs-dev/2023-November/409288.html
Reported-by: Mark Michelson <mmichels@redhat.com>
Fixes: a35725a7a24b ("pinctrl: send RST instead of RST_ACK bit for lb hc")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/pinctrl.c
@@ -7887,7 +7887,7 @@ pinctrl_handle_tcp_svc_check(struct rconn *swconn,
         svc_mon->n_success++;
         svc_mon->state = SVC_MON_S_ONLINE;
 
-        /* Send RST-ACK packet. */
+        /* Send RST packet. */
         svc_monitor_send_tcp_health_check__(swconn, svc_mon, TCP_RST,
                                             htonl(tcp_ack),
                                             htonl(0), th->tcp_dst);