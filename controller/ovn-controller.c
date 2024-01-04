db2a7f627da8d02ee39fd8cb8c846c1c369ae620
Ales Musil <amusil@redhat.com>
2024-01-04 13:54:24 +0100

    treewide: Fix small memory leaks reported by static analysis
    Fix a couple of small memory leaks that were reported by coverity
    static analysis on ovn rpm package.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/ovn-controller.c
+++ controller/ovn-controller.c
@@ -6186,6 +6186,7 @@ parse_options(int argc, char *argv[])
             break;
 
         case 'n':
+            free(cli_system_id);
             cli_system_id = xstrdup(optarg);
             break;
 
