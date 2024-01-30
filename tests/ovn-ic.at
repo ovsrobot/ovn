ovsrobot/ovn

commit 57b347c551684fc9d13ffecf64df9969bd903a88

ovn-ic: Route advertisement.
Support automatical route advertisement and learning for OVN
interconnection.  Static routes and directly connected subnets
can be automatically advertised to avoid manual configuration
across AZs.  This feature is disabled by default, and can be
enabled at each AZ level by:

    ovn-nbctl set NB_Global . options:ic-route-ad=true \
                              options:ic-route-learn=true

More options are available. See ovn-nb(5).

Acked-by: Numan Siddique <numans@ovn.org>
Signed-off-by: Han Zhou <hzhou@ovn.org>

tests/ovn-ic.at
@@ -186,3 +186,128 @@ OVN_CLEANUP_SBOX(gw1)
 OVN_CLEANUP_IC([az1], [az2])
 
 AT_CLEANUP
+
+AT_SETUP([ovn-ic -- route sync])
+
+ovn_init_ic_db
+ovn-ic-nbctl ts-add ts1
+
+for i in 1 2; do
+    ovn_start az$i
+    ovn_as az$i
+
+    # Enable route learning at AZ level
+    ovn-nbctl set nb_global . options:ic-route-learn=true
+    # Enable route advertising at AZ level
+    ovn-nbctl set nb_global . options:ic-route-adv=true
+
+    # Create LRP and connect to TS
+    ovn-nbctl lr-add lr$i
+    ovn-nbctl lrp-add lr$i lrp-lr$i-ts1 aa:aa:aa:aa:aa:0$i 169.254.100.$i/24
+    ovn-nbctl lsp-add ts1 lsp-ts1-lr$i \
+            -- lsp-set-addresses lsp-ts1-lr$i router \
+            -- lsp-set-type lsp-ts1-lr$i router \
+            -- lsp-set-options lsp-ts1-lr$i router-port=lrp-lr$i-ts1
+
+    # Create static routes
+    ovn-nbctl lr-route-add lr$i 10.11.$i.0/24 169.254.0.1
+
+    # Create a src-ip route, which shouldn't be synced
+    ovn-nbctl --policy=src-ip lr-route-add lr$i 10.22.$i.0/24 169.254.0.2
+done
+
+for i in 1 2; do
+    OVS_WAIT_UNTIL([ovn_as az$i ovn-nbctl lr-route-list lr$i | grep learned])
+done
+
+AT_CHECK([ovn_as az1 ovn-nbctl lr-route-list lr1], [0], [dnl
+IPv4 Routes
+             10.11.1.0/24               169.254.0.1 dst-ip
+             10.11.2.0/24             169.254.100.2 dst-ip (learned)
+             10.22.1.0/24               169.254.0.2 src-ip
+])
+
+# Delete route in AZ1, AZ2's learned route should be deleted.
+ovn_as az1 ovn-nbctl lr-route-del lr1 10.11.1.0/24
+OVS_WAIT_WHILE([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned])
+
+# Add the route back
+ovn_as az1 ovn-nbctl lr-route-add lr1 10.11.1.0/24 169.254.0.1
+OVS_WAIT_UNTIL([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned])
+
+# Disable route-learning for AZ1
+ovn_as az1 ovn-nbctl set nb_global . options:ic-route-learn=false
+OVS_WAIT_WHILE([ovn_as az1 ovn-nbctl lr-route-list lr1 | grep learned])
+AT_CHECK([ovn_as az1 ovn-nbctl lr-route-list lr1], [0], [dnl
+IPv4 Routes
+             10.11.1.0/24               169.254.0.1 dst-ip
+             10.22.1.0/24               169.254.0.2 src-ip
+])
+
+# AZ1 should still advertise and AZ2 should still learn the route
+AT_CHECK([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned], [0], [ignore])
+
+# Disable route-advertising for AZ1
+ovn_as az1 ovn-nbctl set nb_global . options:ic-route-adv=false
+
+# AZ2 shouldn't have the route learned, because AZ1 should have stopped
+# advertising.
+OVS_WAIT_WHILE([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned])
+AT_CHECK([ovn_as az2 ovn-nbctl lr-route-list lr2], [0], [dnl
+IPv4 Routes
+             10.11.2.0/24               169.254.0.1 dst-ip
+             10.22.2.0/24               169.254.0.2 src-ip
+])
+
+# Add default route in AZ1
+ovn_as az1 ovn-nbctl lr-route-add lr1 0.0.0.0/0 169.254.0.3
+
+# Re-enable router-advertising & learn for AZ1
+ovn_as az1 ovn-nbctl set nb_global . options:ic-route-adv=true
+ovn_as az1 ovn-nbctl set nb_global . options:ic-route-learn=true
+
+for i in 1 2; do
+    OVS_WAIT_UNTIL([ovn_as az$i ovn-nbctl lr-route-list lr$i | grep learned])
+done
+
+# Default route should NOT get advertised or learned, by default.
+AT_CHECK([ovn_as az2 ovn-nbctl lr-route-list lr2], [0], [dnl
+IPv4 Routes
+             10.11.1.0/24             169.254.100.1 dst-ip (learned)
+             10.11.2.0/24               169.254.0.1 dst-ip
+             10.22.2.0/24               169.254.0.2 src-ip
+])
+
+# Enable default route advertising in AZ1
+ovn_as az1 ovn-nbctl set nb_global . options:ic-route-adv-default=true
+OVS_WAIT_UNTIL([ovn-ic-sbctl list route | grep 0.0.0.0])
+
+# Enable default route learning in AZ2
+ovn_as az2 ovn-nbctl set nb_global . options:ic-route-learn-default=true
+OVS_WAIT_UNTIL([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned | grep 0.0.0.0])
+
+# Test directly connected subnet route advertising.
+ovn_as az1 ovn-nbctl lrp-add lr1 lrp-lr1-ls1 aa:aa:aa:aa:bb:01 "192.168.0.1/24"
+OVS_WAIT_UNTIL([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned | grep 192.168])
+
+# Delete the directly connected subnet from AZ1, learned route should be
+# removed from AZ2.
+ovn_as az1 ovn-nbctl lrp-del lrp-lr1-ls1
+OVS_WAIT_WHILE([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned | grep 192.168])
+
+# Test blacklist routes
+# Add back the directly connected 192.168 route.
+ovn_as az1 ovn-nbctl lrp-add lr1 lrp-lr1-ls1 aa:aa:aa:aa:bb:01 "192.168.0.1/24"
+OVS_WAIT_UNTIL([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned | grep 192.168])
+# Ensure AZ1 learned AZ2's 10.11.2.0 route as well.
+OVS_WAIT_UNTIL([ovn_as az1 ovn-nbctl lr-route-list lr1 | grep learned | grep 10.11])
+# Now black list 10.11.0.0/16 and 192.168.0.0/16 in AZ2.
+ovn_as az2 ovn-nbctl set nb_global . options:ic-route-blacklist="10.11.0.0/16,192.168.0.0/16"
+# AZ2 shouldn't learn 192.168 route any more.
+OVS_WAIT_WHILE([ovn_as az2 ovn-nbctl lr-route-list lr2 | grep learned | grep 192.168])
+# AZ1 shouldn't learn 10.11 any more.
+OVS_WAIT_WHILE([ovn_as az1 ovn-nbctl lr-route-list lr1 | grep learned | grep 10.11])
+
+OVN_CLEANUP_IC([az1], [az2])
+
+AT_CLEANUP

ovn-architecture.7.xml
@@ -1910,6 +1910,17 @@
       can be produced and then translated to OVS flows locally, which finally
       enables data plane communication.
     </li>
+    <li>
+      Routes that are advertised between different AZs.  If enabled,
+      routes are automatically exchanged by <code>ovn-ic</code>.
+      Both static routes and directly connected subnets are advertised.
+      Options in <ref column="options" table="NB_Global" db="OVN_NB"/>
+      column of the <ref table="NB_Global" db="OVN_NB"/> table of
+      <ref db="OVN_NB"/> database control the behavior of route
+      advertisement, such as enable/disable the advertising/learning
+      routes, whether default routes are advertised/learned, and
+      blacklisted CIDRs.  See <code>ovn-nb</code>(5) for more details.
+    </li>
   </ul>
 
   <p>

ovn-ic-sb.ovsschema
@@ -1,7 +1,7 @@
 {
     "name": "OVN_IC_Southbound",
     "version": "1.0.0",
-    "cksum": "1702378250 6056",
+    "cksum": "108951192 6585",
     "tables": {
         "IC_SB_Global": {
             "columns": {
@@ -86,6 +86,17 @@
                                  "min": 0,
                                  "max": "unlimited"}}},
             "indexes": [["transit_switch", "tunnel_key"], ["logical_port"]],
+            "isRoot": true},
+        "Route": {
+            "columns": {
+                "transit_switch": {"type": "string"},
+                "availability_zone": {"type": {"key": {"type": "uuid",
+                                      "refTable": "Availability_Zone"}}},
+                "ip_prefix": {"type": "string"},
+                "nexthop": {"type": "string"},
+                "external_ids": {
+                    "type": {"key": "string", "value": "string",
+                             "min": 0, "max": "unlimited"}}},
             "isRoot": true},
         "Connection": {
             "columns": {

ovn-ic-sb.xml
@@ -292,6 +292,38 @@
     </group>
   </table>
 
+  <table name="Route" title="Route">
+    <p>
+      Each row in this table represents a route advertised.
+    </p>
+
+    <group title="Core Features">
+      <column name="transit_switch">
+        The name of the transit switch, upon which the route is advertised.
+      </column>
+
+      <column name="availability_zone">
+        The availability zone that has advertised the route.
+      </column>
+
+      <column name="ip_prefix">
+        IP prefix of this route (e.g. 192.168.100.0/24).
+      </column>
+
+      <column name="nexthop">
+        Nexthop IP address for this route.
+      </column>
+    </group>
+
+    <group title="Common Columns">
+      <column name="external_ids">
+        <p>
+          See <em>External IDs</em> at the beginning of this document.
+        </p>
+      </column>
+    </group>
+  </table>
+
   <table name="Connection" title="OVSDB client connections.">
     <p>
       Configuration for a database connection to an Open vSwitch database

ovn-nb.xml
@@ -133,6 +133,75 @@
         </ul>
 
       </column>
+
+      <group title="Options for configuring interconnection route advertisement">
+        <p>
+          These options control how routes are advertised between OVN
+          deployments for interconnection.  If enabled, <code>ovn-ic</code>
+          from different OVN deployments exchanges routes between each other
+          through the global <ref db="OVN_IC_Southbound"/> database.  Only
+          routers with ports connected to interconnection transit switches
+          participate in route advertisement.  For each of these routers, there
+          are two types of routes to be advertised:
+        </p>
+
+        <p>
+          Firstly, the static routes configured in the router are advertised.
+        </p>
+
+        <p>
+          Secondly, the <code>networks</code> configured in the logical router
+          ports that are not on the transit switches are advertised.  These
+          are considered as directly connected subnets on the router.
+        </p>
+
+        <p>
+          Link local prefixes (IPv4 169.254.0.0/16 and IPv6 FE80::/10)
+          are never advertised.
+        </p>
+
+        <p>
+          The learned routes are added to the
+          <ref column="static_routes" table="Logical_Router"/> column of the
+          <ref table="Logical_Router"/> table, with
+          <code>external_ids:ic-learned-route</code> set to the uuid
+          of the row in <ref table="Route" db="OVN_IC_Southbound"/>
+          table of the <ref db="OVN_IC_Southbound"/> database.
+        </p>
+
+        <column name="options" key="ic-route-adv">
+          A boolean value that enables route advertisement to the global
+          <ref db="OVN_IC_Southbound"/> database.  Default is
+          <code>false</code>.
+        </column>
+
+        <column name="options" key="ic-route-learn">
+          A boolean value that enables route learning from the global
+          <ref db="OVN_IC_Southbound"/> database.  Default is
+          <code>false</code>.
+        </column>
+
+        <column name="options" key="ic-route-adv-default">
+          A boolean value that enables advertising default route to the global
+          <ref db="OVN_IC_Southbound"/> database.  Default is
+          <code>false</code>.  This option takes effect only when option
+          <code>ic-route-adv</code> is <code>true</code>.
+        </column>
+
+        <column name="options" key="ic-route-learn-default">
+          A boolean value that enables learning default route from the global
+          <ref db="OVN_IC_Southbound"/> database.  Default is
+          <code>false</code>.  This option takes effect only when option
+          <code>ic-route-learn</code> is <code>true</code>.
+        </column>
+
+        <column name="options" key="ic-route-blacklist">
+          A string value contains a list of CIDRs delimited by ",".  A route
+          will not be advertised or learned if the route's prefix belongs to
+          any of the CIDRs listed.
+        </column>
+      </group>
+
     </group>
 
     <group title="Connection Options">
@@ -2353,6 +2422,14 @@
         same subnet of <ref column="nexthop"/>, OVN chooses the first IP
         address as the one via which the <ref column="nexthop"/> is reachable.
       </p>
+    </column>
+
+    <column name="external_ids" key="ic-learned-route">
+      <code>ovn-ic</code> populates this key if the route is learned from the
+      global <ref db="OVN_IC_Southbound"/> database.  In this case the value
+      will be set to the uuid of the row in <ref table="Route"
+      db="OVN_IC_Southbound"/> table of the <ref db="OVN_IC_Southbound"/>
+      database.
     </column>
 
     <group title="Common Columns">

tests/ovn.at
@@ -18063,6 +18063,8 @@ done
 
 for az in `seq 1 $n_az`; do
     ovn_as az$az
+    ovn-nbctl set nb_global . options:ic-route-learn=true
+    ovn-nbctl set nb_global . options:ic-route-adv=true
 
     # Each AZ has n_ts LSPi->LSi->LRi connecting to each TSi
     for i in `seq 1 $n_ts`; do
@@ -18091,13 +18093,6 @@ for az in `seq 1 $n_az`; do
         ovn-nbctl lsp-set-type lsp-ts$i-lr$az-$i router
         ovn-nbctl lsp-set-options lsp-ts$i-lr$az-$i router-port=lrp-lr$az-$i-ts$i
         ovn-nbctl lrp-set-gateway-chassis lrp-lr$az-$i-ts$i gw$az
-
-        for remote in `seq 1 $n_az`; do
-            if test $az = $remote; then
-                continue
-            fi
-            ovn-nbctl lr-route-add lr$az-$i 10.$remote.$i.0/24 169.254.$i.$remote
-        done
     done
 done
 
@@ -18156,6 +18151,8 @@ echo "---------------------"
 ovn-ic-sbctl list datapath_binding
 echo "---------------------"
 ovn-ic-sbctl list port_binding
+echo "---------------------"
+ovn-ic-sbctl list route
 echo "---------------------"
 
 for az in `seq 1 $n_az`; do

utilities/ovn-nbctl.c
@@ -5149,6 +5149,10 @@ print_route(const struct nbrec_logical_router_static_route *route, struct ds *s)
     if (route->output_port) {
         ds_put_format(s, " %s", route->output_port);
     }
+
+    if (smap_get(&route->external_ids, "ic-learned-route")) {
+        ds_put_format(s, " (learned)");
+    }
     ds_put_char(s, '\n');
 }
 

commit f948ba97fd6a4cf43f804409c2ee0b6edaca3962

ovn-ic: fix global blacklist filter for IPv6 addresses
This commit fixes the prefix filter function as the return condition for IPv6 addresses is disabling the advertisement of all learned prefixes regardless of the match with the blacklist or not.

Reported-at: https://bugs.launchpad.net/ubuntu/+source/ovn/+bug/2046804
Fixes: 57b347c55168 ("ovn-ic: Route advertisement.")
Signed-off-by: 0-day Robot <robot@bytheb.org>

ic/ovn-ic.c
@@ -1025,6 +1025,20 @@ prefix_is_link_local(struct in6_addr *prefix, unsigned int plen)
 }
 
 static bool
+compare_ipv6_prefixes(const struct in6_addr *s_prefix,
+                      const struct in6_addr *d_prefix2, int plen)
+{
+    struct in6_addr mask = ipv6_create_mask(plen);
+    for (int i = 0; i < (plen/8); i++) {
+        if ((s_prefix->s6_addr[i] & mask.s6_addr[i]) ^
+            (d_prefix2->s6_addr[i] & mask.s6_addr[i])) {
+                return false;
+            }
+    }
+    return true;
+}
+
+static bool
 prefix_is_black_listed(const struct smap *nb_options,
                        struct in6_addr *prefix,
                        unsigned int plen)
@@ -1064,12 +1078,8 @@ prefix_is_black_listed(const struct smap *nb_options,
                 continue;
             }
         } else {
-            struct in6_addr mask = ipv6_create_mask(bl_plen);
-            for (int i = 0; i < 16 && mask.s6_addr[i] != 0; i++) {
-                if ((prefix->s6_addr[i] & mask.s6_addr[i])
-                    != (bl_prefix.s6_addr[i] & mask.s6_addr[i])) {
-                    continue;
-                }
+            if (!compare_ipv6_prefixes(prefix, &bl_prefix, bl_plen)) {
+                continue;
             }
         }
         matched = true;

tests/ovn-ic.at
@@ -1274,3 +1274,95 @@ OVN_CLEANUP_IC([az1], [az2])
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn-ic -- route sync -- IPv6 blacklist filter])
+AT_KEYWORDS([IPv6-route-sync-blacklist])
+
+ovn_init_ic_db
+ovn-ic-nbctl ts-add ts1
+
+for i in 1 2; do
+    ovn_start az$i
+    ovn_as az$i
+
+    # Enable route learning at AZ level
+    ovn-nbctl set nb_global . options:ic-route-learn=true
+    # Enable route advertising at AZ level
+    ovn-nbctl set nb_global . options:ic-route-adv=true
+    # Enable blacklist single filter for IPv6
+    ovn-nbctl set nb_global . options:ic-route-blacklist="2003:db8:1::/64,\
+            2004:aaaa::/32,2005:1234:5678::/40"
+
+    OVS_WAIT_UNTIL([ovn-nbctl show | grep ts1])
+
+    # Create LRP and connect to TS
+    ovn-nbctl lr-add lr$i
+    ovn-nbctl lrp-add lr$i lrp-lr$i-ts1 aa:aa:aa:aa:aa:0$i 2001:db8:1::$i/64
+    ovn-nbctl lsp-add ts1 lsp-ts1-lr$i \
+            -- lsp-set-addresses lsp-ts1-lr$i router \
+            -- lsp-set-type lsp-ts1-lr$i router \
+            -- lsp-set-options lsp-ts1-lr$i router-port=lrp-lr$i-ts1
+
+    ovn-nbctl lrp-add lr$i lrp-lr$i-p$i 00:00:00:00:00:0$i 2002:db8:1::$i/64
+
+    # Create blacklisted LRPs and connect to TS
+    ovn-nbctl lrp-add lr$i lrp-lr$i-p-ext$i \
+            11:11:11:11:11:1$i 2003:db8:1::$i/64
+
+    ovn-nbctl lrp-add lr$i lrp-lr$i-p-ext2$i \
+            22:22:22:22:22:2$i 2004:aaaa:bbb::$i/48
+
+    ovn-nbctl lrp-add lr$i lrp-lr$i-p-ext3$i \
+            33:33:33:33:33:3$i 2005:1234:5678::$i/50
+
+done
+
+for i in 1 2; do
+    OVS_WAIT_UNTIL([ovn_as az$i ovn-nbctl lr-route-list lr$i | grep learned])
+done
+
+AT_CHECK([ovn_as az1 ovn-nbctl lr-route-list lr1 |
+    awk '/learned/{print $1, $2}' ], [0], [dnl
+2002:db8:1::/64 2001:db8:1::2
+])
+
+for i in 1 2; do
+    ovn_as az$i
+
+    # Drop blacklist
+    ovn-nbctl remove nb_global . options ic-route-blacklist
+
+done
+
+OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr1 |
+    awk '/learned/{print $1, $2}' | sort ], [0], [dnl
+2002:db8:1::/64 2001:db8:1::2
+2003:db8:1::/64 2001:db8:1::2
+2004:aaaa:bbb::/48 2001:db8:1::2
+2005:1234:5678::/50 2001:db8:1::2
+])
+
+for i in 1 2; do
+    ovn_as az$i
+
+    ovn-nbctl set nb_global . \
+            options:ic-route-blacklist="2003:db8:1::/64,2004:db8:1::/64"
+
+    # Create an 'extra' blacklisted LRP and connect to TS
+    ovn-nbctl lrp-add lr$i lrp-lr$i-p-ext2$i \
+            44:44:44:44:44:4$i 2004:db8:1::$i/64
+
+done
+
+OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr1 |
+    awk '/learned/{print $1, $2}' | sort ], [0], [dnl
+2002:db8:1::/64 2001:db8:1::2
+2004:aaaa:bbb::/48 2001:db8:1::2
+2005:1234:5678::/50 2001:db8:1::2
+])
+
+OVN_CLEANUP_IC([az1], [az2])
+
+AT_CLEANUP
+])