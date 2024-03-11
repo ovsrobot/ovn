ovsrobot/ovn

commit 4885e337f929a74bff6d7ea23175f352e0e9ea08

rbac: Only allow relevant chassis to update BFD.
This adds a new "chassis_name" column to the BFD table. ovn-northd sets
this to the logical port's chassis name when creating the BFD record.
RBAC has been updated so that chassis may only update their own records.

Signed-off-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

northd/northd.c
@@ -10933,6 +10933,7 @@ build_bfd_table(struct ovsdb_idl_txn *ovnsb_txn,
             nbrec_bfd_set_status(nb_bt, "admin_down");
         }
 
+        struct ovn_port *op = ovn_port_find(lr_ports, nb_bt->logical_port);
         bfd_e = bfd_port_lookup(&sb_only, nb_bt->logical_port, nb_bt->dst_ip);
         if (!bfd_e) {
             int udp_src = bfd_get_unused_port(bfd_src_ports);
@@ -10946,6 +10947,9 @@ build_bfd_table(struct ovsdb_idl_txn *ovnsb_txn,
             sbrec_bfd_set_disc(sb_bt, 1 + random_uint32());
             sbrec_bfd_set_src_port(sb_bt, udp_src);
             sbrec_bfd_set_status(sb_bt, nb_bt->status);
+            if (op && op->sb && op->sb->chassis) {
+                sbrec_bfd_set_chassis_name(sb_bt, op->sb->chassis->name);
+            }
 
             int min_tx = nb_bt->n_min_tx ? nb_bt->min_tx[0] : BFD_DEF_MINTX;
             sbrec_bfd_set_min_tx(sb_bt, min_tx);
@@ -10964,6 +10968,10 @@ build_bfd_table(struct ovsdb_idl_txn *ovnsb_txn,
                 }
             }
             build_bfd_update_sb_conf(nb_bt, bfd_e->sb_bt);
+            if (op && op->sb && op->sb->chassis &&
+                strcmp(op->sb->chassis->name, sb_bt->chassis_name)) {
+                sbrec_bfd_set_chassis_name(sb_bt, op->sb->chassis->name);
+            }
 
             hmap_remove(&sb_only, &bfd_e->hmap_node);
             bfd_e->ref = false;
@@ -10972,7 +10980,6 @@ build_bfd_table(struct ovsdb_idl_txn *ovnsb_txn,
             hmap_insert(bfd_connections, &bfd_e->hmap_node, hash);
         }
 
-        struct ovn_port *op = ovn_port_find(lr_ports, nb_bt->logical_port);
         if (op) {
             op->has_bfd = true;
         }

northd/ovn-northd.c
@@ -122,7 +122,7 @@ static const char *rbac_igmp_group_auth[] =
 static const char *rbac_igmp_group_update[] =
     {"address", "chassis", "datapath", "ports"};
 static const char *rbac_bfd_auth[] =
-    {""};
+    {"chassis_name"};
 static const char *rbac_bfd_update[] =
     {"status"};
 

ovn-sb.ovsschema
@@ -1,7 +1,7 @@
 {
     "name": "OVN_Southbound",
-    "version": "20.32.0",
-    "cksum": "1262133774 31276",
+    "version": "20.33.0",
+    "cksum": "4076371179 31328",
     "tables": {
         "SB_Global": {
             "columns": {
@@ -578,7 +578,8 @@
                              "min": 0, "max": "unlimited"}},
                 "options": {
                     "type": {"key": "string", "value": "string",
-                             "min": 0, "max": "unlimited"}}},
+                             "min": 0, "max": "unlimited"}},
+                "chassis_name": {"type": "string"}},
             "indexes": [["logical_port", "dst_ip", "src_port", "disc"]],
             "isRoot": true},
         "FDB": {

ovn-sb.xml
@@ -4989,6 +4989,10 @@ tcp.flags = RST;
         receiving system in Asynchronous mode.
       </column>
 
+      <column name="chassis_name">
+        The name of the chassis where the logical port is bound.
+      </column>
+
       <column name="options">
         Reserved for future use.
       </column>

commit 6dd0db0365ccd77f4adf08e2969ec3c56c03fa16

northd: Fix NULL pointer deref within build_bfd_table.
The cb_bt pointer was pointing to different struct and might
have been NULL at this point. Make sure we are using the correct
pointer in the strcmp() and sbrec_bfd_set_chassis_name() call.

At the same time make sure we are actually running all tests that
do require the BFD beacon, as it was skipped in all CI stages. The
OpenBFDD project is not available as rpm, so we need to compile it
from source.

Fixes: 4885e337f929 ("rbac: Only allow relevant chassis to update BFD.")
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -9804,8 +9804,9 @@ build_bfd_table(struct ovsdb_idl_txn *ovnsb_txn,
             }
             build_bfd_update_sb_conf(nb_bt, bfd_e->sb_bt);
             if (op && op->sb && op->sb->chassis &&
-                strcmp(op->sb->chassis->name, sb_bt->chassis_name)) {
-                sbrec_bfd_set_chassis_name(sb_bt, op->sb->chassis->name);
+                strcmp(op->sb->chassis->name, bfd_e->sb_bt->chassis_name)) {
+                sbrec_bfd_set_chassis_name(bfd_e->sb_bt,
+                                           op->sb->chassis->name);
             }
 
             hmap_remove(&sb_only, &bfd_e->hmap_node);

utilities/automake.mk
@@ -38,6 +38,7 @@ EXTRA_DIST += \
     utilities/ovndb-servers.ocf \
     utilities/checkpatch.py \
     utilities/containers/Makefile \
+    utilities/containers/openbfdd.patch \
     utilities/containers/py-requirements.txt \
     utilities/containers/fedora/Dockerfile \
     utilities/containers/ubuntu/Dockerfile \

utilities/containers/fedora/Dockerfile
@@ -52,6 +52,24 @@ RUN git clone git://git.kernel.org/pub/scm/devel/sparse/sparse.git \
     && \
     make -j4 PREFIX=/usr HAVE_LLVM= HAVE_SQLITE= install
 
+# Compile OpenBFDD from source
+WORKDIR /workspace/OpenBFDD
+
+COPY $CONTAINERS_PATH/openbfdd.patch /tmp/openbfdd.patch
+
+RUN git clone https://github.com/dyninc/OpenBFDD.git \
+    /workspace/OpenBFDD \
+    && \
+    git apply /tmp/openbfdd.patch \
+    && \
+    ./autogen.sh \
+    && \
+    ./configure --enable-silent-rules \
+    && \
+    make \
+    && \
+    make install
+
 WORKDIR /workspace
 
 COPY $CONTAINERS_PATH/py-requirements.txt /tmp/py-requirements.txt

utilities/containers/openbfdd.patch
@@ -1,0 +1,39 @@
+diff --git a/Logger.cpp b/Logger.cpp
+index f375b20..e72afba 100755
+--- a/Logger.cpp
++++ b/Logger.cpp
+@@ -11,6 +11,8 @@
+ #include <errno.h>
+ #include <string.h>
+ #include <unistd.h>
++#include <time.h>
++#include <pthread.h>
+ 
+ const size_t Logger::MaxMessageLen;
+ 
+diff --git a/SchedulerBase.cpp b/SchedulerBase.cpp
+index 3eab298..c1dffc4 100755
+--- a/SchedulerBase.cpp
++++ b/SchedulerBase.cpp
+@@ -11,6 +11,8 @@
+ #include <fcntl.h>
+ #include <string.h>
+ #include <unistd.h>
++#include <time.h>
++#include <pthread.h>
+ 
+ using namespace std;
+ 
+diff --git a/utils.cpp b/utils.cpp
+index 79c7b68..f846459 100755
+--- a/utils.cpp
++++ b/utils.cpp
+@@ -13,6 +13,8 @@
+ #include <sys/stat.h>
+ #include <sys/param.h>
+ #include <sys/socket.h>
++#include <time.h>
++#include <pthread.h>
+ 
+ using namespace std;
+ 

utilities/containers/ubuntu/Dockerfile
@@ -51,6 +51,24 @@ RUN git clone git://git.kernel.org/pub/scm/devel/sparse/sparse.git \
     && \
     make -j4 PREFIX=/usr HAVE_LLVM= HAVE_SQLITE= install
 
+# Compile OpenBFDD from source
+WORKDIR /workspace/OpenBFDD
+
+COPY $CONTAINERS_PATH/openbfdd.patch /tmp/openbfdd.patch
+
+RUN git clone https://github.com/dyninc/OpenBFDD.git \
+    /workspace/OpenBFDD \
+    && \
+    git apply /tmp/openbfdd.patch \
+    && \
+    ./autogen.sh \
+    && \
+    ./configure --enable-silent-rules \
+    && \
+    make \
+    && \
+    make install
+
 WORKDIR /workspace
 
 COPY $CONTAINERS_PATH/py-requirements.txt /tmp/py-requirements.txt