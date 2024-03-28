ovsrobot/ovn

commit b600316f252aa29f15d153d9331f8557f1f77874

Don't delete patch ports that don't belong to br-int
Same as for tunnel ports, controllers should not touch OVN patch ports
that don't belong to their active integration bridge (except when those
are peers to corresponding patch ports).

Signed-off-by: Ihar Hrachyshka <ihrachys@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Acked-by: Mark Michelson <mmichels@redhat.com>

controller/patch.c
@@ -271,12 +271,26 @@ add_bridge_mappings(struct ovsdb_idl_txn *ovs_idl_txn,
     shash_destroy(&bridge_mappings);
 }
 
+static const struct ovsrec_port *
+get_port(struct ovsdb_idl_index *ovsrec_port_by_name, const char *name)
+{
+    struct ovsrec_port *target =
+        ovsrec_port_index_init_row(ovsrec_port_by_name);
+    ovsrec_port_index_set_name(target, name);
+
+    const struct ovsrec_port *port = NULL;
+    OVSREC_PORT_FOR_EACH_EQUAL (port, target, ovsrec_port_by_name) {
+    }
+    ovsrec_port_index_destroy_row(target);
+    return port;
+}
+
 void
 patch_run(struct ovsdb_idl_txn *ovs_idl_txn,
           struct ovsdb_idl_index *sbrec_port_binding_by_type,
           const struct ovsrec_bridge_table *bridge_table,
           const struct ovsrec_open_vswitch_table *ovs_table,
-          const struct ovsrec_port_table *port_table,
+          struct ovsdb_idl_index *ovsrec_port_by_name,
           const struct ovsrec_bridge *br_int,
           const struct sbrec_chassis *chassis,
           const struct hmap *local_datapaths)
@@ -293,7 +307,8 @@ patch_run(struct ovsdb_idl_txn *ovs_idl_txn,
      * leaving useless ports on upgrade. */
     struct shash existing_ports = SHASH_INITIALIZER(&existing_ports);
     const struct ovsrec_port *port;
-    OVSREC_PORT_TABLE_FOR_EACH (port, port_table) {
+    for (size_t i = 0; i < br_int->n_ports; i++) {
+        port = br_int->ports[i];
         if (smap_get(&port->external_ids, "ovn-localnet-port")
             || smap_get(&port->external_ids, "ovn-l2gateway-port")
             || smap_get(&port->external_ids, "ovn-l3gateway-port")
@@ -321,6 +336,23 @@ patch_run(struct ovsdb_idl_txn *ovs_idl_txn,
          * ovn-controller.  Otherwise it may cause unncessary dataplane
          * interruption during restart/upgrade. */
         if (!daemon_started_recently()) {
+            /* delete peer patch port first */
+            for (size_t i = 0; i < port->n_interfaces; i++) {
+                struct ovsrec_interface *iface = port->interfaces[i];
+                if (strcmp(iface->type, "patch")) {
+                    continue;
+                }
+                const char *iface_peer = smap_get(&iface->options, "peer");
+                if (iface_peer) {
+                    const struct ovsrec_port *peer_port =
+                        get_port(ovsrec_port_by_name, iface_peer);
+                    if (peer_port) {
+                        remove_port(bridge_table, peer_port);
+                    }
+                }
+            }
+
+            /* now delete integration bridge patch port */
             remove_port(bridge_table, port);
         }
     }

tests/ovn.at
@@ -34479,3 +34479,55 @@ OVS_WAIT_UNTIL([
 OVN_CLEANUP([hv1])
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn-controller deletes OVN patch ports for its integration bridge only])
+ovn_start
+net_add n1
+
+sim_add hv1
+as hv1
+
+# prepare OVN patch ports on both "integration" bridges
+ovs-vsctl add-br br-phys
+ovs-vsctl add-br br-int
+ovs-vsctl -- add-port br-int fake-int-to-phys -- \
+    set port fake-int-to-phys external-ids:ovn-localnet-port=fake-port -- \
+    set interface fake-int-to-phys options:peer=fake-phys-to-int type=patch
+ovs-vsctl -- add-port br-phys fake-phys-to-int -- \
+    set port fake-phys-to-int external-ids:ovn-localnet-port=fake-port -- \
+    set interface fake-phys-to-int options:peer=fake-int-to-phys type=patch
+
+ovs-vsctl add-br br-phys-2
+ovs-vsctl add-br br-int-2
+ovs-vsctl -- add-port br-int-2 fake-int-2-to-phys-2 -- \
+    set port fake-int-2-to-phys-2 external-ids:ovn-localnet-port=fake-port-2 -- \
+    set interface fake-int-2-to-phys-2 options:peer=fake-phys-2-to-int-2 type=patch
+ovs-vsctl -- add-port br-phys-2 fake-phys-2-to-int-2 -- \
+    set port fake-phys-2-to-int-2 external-ids:ovn-localnet-port=fake-port-2 -- \
+    set interface fake-phys-2-to-int-2 options:peer=fake-int-2-to-phys-2 type=patch
+
+ovn_attach n1 br-phys 192.168.1.1 24
+
+# allow controller to immediately clean patch ports up
+check ovn-appctl -t ovn-controller debug/ignore-startup-delay
+
+# check that patch port on br-int is cleaned up (and also its peer)
+OVS_WAIT_UNTIL([
+    test 0 = $(ovs-vsctl --columns _uuid --bare find Port name=fake-int-to-phys | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 0 = $(ovs-vsctl --columns _uuid --bare find Port name=fake-phys-to-int | wc -l)
+])
+
+# check that patch port on a different bridge is not touched
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=fake-int-2-to-phys-2 | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=fake-phys-2-to-int-2 | wc -l)
+])
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])

controller/ovn-controller.c
@@ -3999,6 +3999,9 @@ main(int argc, char *argv[])
     struct ovsdb_idl_index *ovsrec_port_by_interfaces
         = ovsdb_idl_index_create1(ovs_idl_loop.idl,
                                   &ovsrec_port_col_interfaces);
+    struct ovsdb_idl_index *ovsrec_port_by_name
+        = ovsdb_idl_index_create1(ovs_idl_loop.idl,
+                                  &ovsrec_port_col_name);
 
     ovsdb_idl_get_initial_snapshot(ovs_idl_loop.idl);
 
@@ -4647,7 +4650,7 @@ main(int argc, char *argv[])
                             sbrec_port_binding_by_type,
                             ovsrec_bridge_table_get(ovs_idl_loop.idl),
                             ovsrec_open_vswitch_table_get(ovs_idl_loop.idl),
-                            ovsrec_port_table_get(ovs_idl_loop.idl),
+                            ovsrec_port_by_name,
                             br_int, chassis, &runtime_data->local_datapaths);
                         stopwatch_stop(PATCH_RUN_STOPWATCH_NAME, time_msec());
                         if (vif_plug_provider_has_providers() && ovs_idl_txn) {

controller/patch.h
@@ -40,7 +40,7 @@ void patch_run(struct ovsdb_idl_txn *ovs_idl_txn,
                struct ovsdb_idl_index *sbrec_port_binding_by_type,
                const struct ovsrec_bridge_table *,
                const struct ovsrec_open_vswitch_table *,
-               const struct ovsrec_port_table *,
+               struct ovsdb_idl_index *ovsrec_port_by_name,
                const struct ovsrec_bridge *br_int,
                const struct sbrec_chassis *,
                const struct hmap *local_datapaths);

commit 150cd14445742753b55d7867b09fb7082844e8f2

controller: Fix an issue wrt cleanup of stale patch port.
Issue:
Upon updating the network_name option of localnet port from one physical
bridge to another, ovn-controller fails to cleanup the peer localnet
patch port from the old bridge and ends up creating a duplicate peer
localnet patch port which fails in the following ovsdb transaction:

```
"Transaction causes multiple rows in \"Interface\" table to have
identical values
(patch-localnet_a7d47490-8a90-4c4d-8266-2915ad07c185-to-br-int)
```

Current workflow:
1. Keep a set of all existing localnet ports on br-int. Let us call this
   set: existing_ports.
2. For each localnet port in SB:
   2.1 Create a patch port on br-int. (if it already exists on br-int,
       do not create but remove the entry from exisitng_ports set).
   2.2 Create a peer patch port on br-phys-x. (if it already exists on the
       bridge, do not create but remove the entry from exisitng_ports set).
3. Finally, cleanup all the ports and its peers from exisiting_ports.

With the above workflow, upon network_name change of localnet LSP, since
ports on br-int does not change, only peer port needs to be move from
br-phys-x to br-phys-y, the localnet port is removed from
exisiting_ports in #2.1 and its peer never becomes eligible for cleanup.

Fix:
Include both patch port on br-int and its peer port in the
exisiting_ports set as part of #1.
This make sures that the peer port is only removed from existing_ports
only if it is already present on the correct bridge. (#2.1/#2.2)
Otherwise, during the cleanup in #3 it will be considered.

Fixes: b600316 ("Don't delete patch ports that don't belong to br-int")
Signed-off-by: Priyankar Jain <priyankar.jain@nutanix.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/patch.c
@@ -314,6 +314,21 @@ patch_run(struct ovsdb_idl_txn *ovs_idl_txn,
             || smap_get(&port->external_ids, "ovn-l3gateway-port")
             || smap_get(&port->external_ids, "ovn-logical-patch-port")) {
             shash_add(&existing_ports, port->name, port);
+            /* Also add peer ports to the list. */
+            for (size_t j = 0; j < port->n_interfaces; j++) {
+                struct ovsrec_interface *p_iface = port->interfaces[j];
+                if (strcmp(p_iface->type, "patch")) {
+                    continue;
+                }
+                const char *peer_name = smap_get(&p_iface->options, "peer");
+                if (peer_name) {
+                    const struct ovsrec_port *peer_port =
+                        get_port(ovsrec_port_by_name, peer_name);
+                    if (peer_port) {
+                        shash_add(&existing_ports, peer_port->name, peer_port);
+                    }
+                }
+            }
         }
     }
 
@@ -336,23 +351,6 @@ patch_run(struct ovsdb_idl_txn *ovs_idl_txn,
          * ovn-controller.  Otherwise it may cause unncessary dataplane
          * interruption during restart/upgrade. */
         if (!daemon_started_recently()) {
-            /* delete peer patch port first */
-            for (size_t i = 0; i < port->n_interfaces; i++) {
-                struct ovsrec_interface *iface = port->interfaces[i];
-                if (strcmp(iface->type, "patch")) {
-                    continue;
-                }
-                const char *iface_peer = smap_get(&iface->options, "peer");
-                if (iface_peer) {
-                    const struct ovsrec_port *peer_port =
-                        get_port(ovsrec_port_by_name, iface_peer);
-                    if (peer_port) {
-                        remove_port(bridge_table, peer_port);
-                    }
-                }
-            }
-
-            /* now delete integration bridge patch port */
             remove_port(bridge_table, port);
         }
     }

tests/ovn.at
@@ -36454,6 +36454,115 @@ OVN_CLEANUP([hv1])
 AT_CLEANUP
 ])
 
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn-controller update network_name option for localnet port])
+ovn_start
+net_add n1
+
+sim_add hv1
+as hv1
+
+# Bridge Topology
+# Initial: br-int and br-phys-1 connected through ovn-localnet patch port.
+#
+# br-phys-1 -- br-int
+#
+# Final: br-int and br-phys-3 connected through ovn-localnet patch port.
+# Similarly br-int-2 and br-hys-2 connected through localnet patch port
+# but not owned by this controller.
+#
+# br-phys-1  br-int -- br-phys-3
+# br-int-2 -- br-phys-2
+
+# Create bridges
+ovs-vsctl add-br br-int
+ovs-vsctl add-br br-int-2
+ovs-vsctl add-br br-phys-1
+ovs-vsctl add-br br-phys-2
+ovs-vsctl add-br br-phys-3
+ovs-vsctl set open . external-ids:ovn-bridge-mappings-hv1=phys-1:br-phys-1,phy-2:br-phys-2,phys-3:br-phys-3
+
+ovn_attach n1 br-phys-1 192.168.1.1 24
+
+ovs-vsctl -- add-port br-int vif1 -- \
+    set interface vif1 external-ids:iface-id=lp1
+
+ovn-nbctl ls-add ls
+
+ovn-nbctl lsp-add ls lp1
+ovn-nbctl lsp-set-addresses lp1 "00:00:00:00:00:01 10.0.0.1"
+
+ovn-nbctl lsp-add ls ln_port
+ovn-nbctl lsp-set-addresses ln_port unknown
+ovn-nbctl lsp-set-type ln_port localnet
+ovn-nbctl lsp-set-options ln_port network_name=phys-1
+wait_for_ports_up
+ovn-nbctl --wait=hv sync
+
+# Allow controller to immediately clean patch ports up if any.
+check ovn-appctl -t ovn-controller debug/ignore-startup-delay
+
+# check that patch port created on br-int and br-phys-1.
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=patch-br-int-to-ln_port | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl list-ports br-int | grep patch-br-int-to-ln_port | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=patch-ln_port-to-br-int | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl list-ports br-phys-1 | grep patch-ln_port-to-br-int | wc -l)
+])
+
+ovn-appctl debug/pause
+
+# Now move the localnet port from phys-1 to phys-3.
+ovn-nbctl lsp-set-options ln_port network_name=phys-3
+
+# Also create fake localnet ports on br-int-2
+ovs-vsctl -- add-port br-int-2 fake-int-2-to-phys-2 -- \
+    set port fake-int-2-to-phys-2 external-ids:ovn-localnet-port=fake-port -- \
+    set interface fake-int-2-to-phys-2 options:peer=fake-phys-2-to-int-2 type=patch
+ovs-vsctl -- add-port br-phys-2 fake-phys-2-to-int-2 -- \
+    set port fake-phys-2-to-int-2 external-ids:ovn-localnet-port=fake-port -- \
+    set interface fake-phys-2-to-int-2 options:peer=fake-int-2-to-phys-2 type=patch
+
+ovn-appctl debug/resume
+ovn-nbctl --wait=hv sync
+
+# patch for port on br-phys-1 is cleanedup.
+OVS_WAIT_UNTIL([
+    test 0 = $(ovs-vsctl list-ports br-phys-1 | grep patch-ln_port-to-br-int | wc -l)
+])
+
+# Patch port created on br-int and br-phy-3
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=patch-br-int-to-ln_port | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl list-ports br-int | grep patch-br-int-to-ln_port | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=patch-ln_port-to-br-int | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl list-ports br-phys-3 | grep patch-ln_port-to-br-int | wc -l)
+])
+
+# check that patch port on a different bridge is not touched
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=fake-int-2-to-phys-2 | wc -l)
+])
+OVS_WAIT_UNTIL([
+    test 1 = $(ovs-vsctl --columns _uuid --bare find Port name=fake-phys-2-to-int-2 | wc -l)
+])
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])
+
 # NOTE: This test case runs two ovn-controllers inside the same sandbox (hv1).
 # Each controller uses a unique chassis name - hv1 and hv2 - and manage
 # different bridges with different ports. This is why all 'as' commands below