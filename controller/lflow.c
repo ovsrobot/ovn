5ca03b169f6c0172fd39f5121257e9750d1ce0bb
Ales Musil <amusil@redhat.com>
2024-04-30 18:56:42 +0200

    controller: Track individual address set constants.
    Instead of tracking address set per struct expr_constant_set track it
    per individual struct expr_constant. This allows more fine grained
    control for I-P processing of address sets in controller. It helps with
    scenarios like matching on two address sets in one expression e.g.
    "ip4.src == {$as1, $as2}". This allows any addition or removal of
    individual adress from the set to be incrementally processed instead
    of reprocessing all the flows.
    
    This unfortunately doesn't help with the following flows:
    "ip4.src == $as1 && ip4.dst == $as2"
    "ip4.src == $as1 || ip4.dst == $as2"
    
    The memory impact should be minimal as there is only increase of 8 bytes
    per the struct expr_constant.
    
    Reported-at: https://issues.redhat.com/browse/FDP-509
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/lflow.c
+++ controller/lflow.c
@@ -278,7 +278,7 @@ lflow_handle_changed_flows(struct lflow_ctx_in *l_ctx_in,
 }
 
 static bool
-as_info_from_expr_const(const char *as_name, const union expr_constant *c,
+as_info_from_expr_const(const char *as_name, const struct expr_constant *c,
                         struct addrset_info *as_info)
 {
     as_info->name = as_name;
@@ -647,9 +647,8 @@ as_update_can_be_handled(const char *as_name, struct addr_set_diff *as_diff,
  *        expressions/constants, usually because of disjunctions between
  *        sub-expressions/constants, e.g.:
  *
+ *          ip.src == $as1 && ip.dst == $as2
  *          ip.src == $as1 || ip.dst == $as2
- *          ip.src == {$as1, $as2}
- *          ip.src == {$as1, ip1}
  *
  *        All these could have been split into separate lflows.
  *
@@ -714,7 +713,7 @@ lflow_handle_addr_set_update(const char *as_name,
         if (as_diff->deleted) {
             struct addrset_info as_info;
             for (size_t i = 0; i < as_diff->deleted->n_values; i++) {
-                union expr_constant *c = &as_diff->deleted->values[i];
+                struct expr_constant *c = &as_diff->deleted->values[i];
                 if (!as_info_from_expr_const(as_name, c, &as_info)) {
                     continue;
                 }
