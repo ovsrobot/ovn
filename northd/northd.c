ovsrobot/ovn

commit 5f0809be565713d3634a6c663e122c0bfb5dff92

Revert "northd: Don't skip transit switch LSP when creating mcast groups."
This reverts commit 85ca2b75369c9a73f4750d5914666a54ebb3f2e0.

The commit above breaks inter-AZ IP multicast for the case when one of
the multicast receivers is co-located in the same zone as the sender.
In those cases traffic is not correctly forwarded to other receivers
that joined the group in other AZs.

This is often the case in ovn-kubernetes deployments (with IC enabled).
The current "interconnection - IGMP/MLD multicast" unit test failed to
cover such topologies.

CC: Mohammad Heib <mheib@redhat.com>
Fixes: 85ca2b75369c ("northd: Don't skip transit switch LSP when creating mcast groups.")
Reported-at: https://issues.redhat.com/browse/FDP-656
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Acked-by: Mohammad Heib <mheib@redhat.com>
Acked-by: Numan Siddique <numans@ovn.org>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

northd/northd.c
@@ -6127,13 +6127,11 @@ ovn_igmp_group_get_ports(const struct sbrec_igmp_group *sb_igmp_group,
             continue;
         }
 
-        /* If this is already a port of a router on which relay is enabled
-         * and it's not a transit switch to router port, skip it for the
-         * group.  Traffic is flooded there anyway.
+        /* If this is already a port of a router on which relay is enabled,
+         * skip it for the group. Traffic is flooded there anyway.
          */
         if (port->peer && port->peer->od &&
-                port->peer->od->mcast_info.rtr.relay &&
-                !ovn_datapath_is_transit_switch(port->od)) {
+                port->peer->od->mcast_info.rtr.relay) {
             continue;
         }
 

commit feef41fa92085119bacccc6737ccea6b76c5b0fa

northd: Remove unused ovn_datapath_is_transit_switch().
Fixes: 5f0809be5657 ("Revert "northd: Don't skip transit switch LSP when creating mcast groups."")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -806,12 +806,6 @@ ovn_datapath_is_stale(const struct ovn_datapath *od)
     return !od->nbr && !od->nbs;
 }
 
-static inline bool
-ovn_datapath_is_transit_switch(const struct ovn_datapath *od)
-{
-    return od->tunnel_key >= OVN_MIN_DP_KEY_GLOBAL;
-}
-
 static struct ovn_datapath *
 ovn_datapath_from_sbrec(const struct hmap *ls_datapaths,
                         const struct hmap *lr_datapaths,