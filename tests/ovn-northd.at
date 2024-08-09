ovsrobot/ovn

commit b22684d4e37c3dd205a959925b6e3f1827bbe045

Add a northbound interface to program MAC_Binding table
Add a new NB and SB table for managing Static MAC_Binding entries.
This table is currently supported for logical routers. OVN northd
is responsible for propagating the values from NB to SB. OVN controller
is responsible for installation MAC lookup flows. The priority of
the installed flows are based on override_dynamic_mac flag. This helps
control the precedence of statically programmed vs dynamically learnt
MAC Bindings.

Signed-off-by: Karthik Chandrashekar <karthik.c@nutanix.com>
Tetsted-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/northd.c
@@ -28,6 +28,7 @@
 #include "ovn/lex.h"
 #include "lib/chassis-index.h"
 #include "lib/ip-mcast-index.h"
+#include "lib/static-mac-binding-index.h"
 #include "lib/copp.h"
 #include "lib/mcast-group-index.h"
 #include "lib/ovn-l7.h"
@@ -15054,6 +15055,80 @@ build_meter_groups(struct northd_input *input_data,
     }
 }
 
+static const struct nbrec_static_mac_binding *
+static_mac_binding_by_port_ip(struct northd_input *input_data,
+                       const char *logical_port, const char *ip)
+{
+    const struct nbrec_static_mac_binding *nb_smb = NULL;
+
+    NBREC_STATIC_MAC_BINDING_TABLE_FOR_EACH (
+        nb_smb, input_data->nbrec_static_mac_binding_table) {
+        if (!strcmp(nb_smb->logical_port, logical_port) &&
+            !strcmp(nb_smb->ip, ip)) {
+            break;
+        }
+    }
+
+    return nb_smb;
+}
+
+static void
+build_static_mac_binding_table(struct northd_input *input_data,
+                               struct ovsdb_idl_txn *ovnsb_txn,
+                               struct hmap *ports)
+{
+    /* Cleanup SB Static_MAC_Binding entries which do not have corresponding
+     * NB Static_MAC_Binding entries. */
+    const struct nbrec_static_mac_binding *nb_smb;
+    const struct sbrec_static_mac_binding *sb_smb, *sb_smb_next;
+    SBREC_STATIC_MAC_BINDING_TABLE_FOR_EACH_SAFE (sb_smb, sb_smb_next,
+        input_data->sbrec_static_mac_binding_table) {
+        nb_smb = static_mac_binding_by_port_ip(input_data,
+                                               sb_smb->logical_port,
+                                               sb_smb->ip);
+        if (!nb_smb) {
+            sbrec_static_mac_binding_delete(sb_smb);
+        }
+    }
+
+    /* Create/Update SB Static_MAC_Binding entries with corresponding values
+     * from NB Static_MAC_Binding entries. */
+    NBREC_STATIC_MAC_BINDING_TABLE_FOR_EACH (
+        nb_smb, input_data->nbrec_static_mac_binding_table) {
+        struct ovn_port *op = ovn_port_find(ports, nb_smb->logical_port);
+        if (op && op->nbrp) {
+            struct ovn_datapath *od = op->od;
+            if (od && od->sb) {
+                const struct sbrec_static_mac_binding *mb =
+                    static_mac_binding_lookup(
+                        input_data->sbrec_static_mac_binding_by_lport_ip,
+                        nb_smb->logical_port, nb_smb->ip);
+                if (!mb) {
+                    /* Create new entry */
+                    mb = sbrec_static_mac_binding_insert(ovnsb_txn);
+                    sbrec_static_mac_binding_set_logical_port(
+                        mb, nb_smb->logical_port);
+                    sbrec_static_mac_binding_set_ip(mb, nb_smb->ip);
+                    sbrec_static_mac_binding_set_mac(mb, nb_smb->mac);
+                    sbrec_static_mac_binding_set_override_dynamic_mac(mb,
+                        nb_smb->override_dynamic_mac);
+                    sbrec_static_mac_binding_set_datapath(mb, od->sb);
+                } else {
+                    /* Update existing entry if there is a change*/
+                    if (strcmp(mb->mac, nb_smb->mac)) {
+                        sbrec_static_mac_binding_set_mac(mb, nb_smb->mac);
+                    }
+                    if (mb->override_dynamic_mac !=
+                        nb_smb->override_dynamic_mac) {
+                        sbrec_static_mac_binding_set_override_dynamic_mac(mb,
+                            nb_smb->override_dynamic_mac);
+                    }
+                }
+            }
+        }
+    }
+}
+
 void
 northd_init(struct northd_data *data)
 {
@@ -15207,6 +15282,7 @@ ovnnb_db_run(struct northd_input *input_data,
     build_lrouter_groups(&data->ports, &data->lr_list);
     build_ip_mcast(input_data, ovnsb_txn, &data->datapaths);
     build_meter_groups(input_data, &data->meter_groups);
+    build_static_mac_binding_table(input_data, ovnsb_txn, &data->ports);
     stopwatch_stop(BUILD_LFLOWS_CTX_STOPWATCH_NAME, time_msec());
     stopwatch_start(CLEAR_LFLOWS_CTX_STOPWATCH_NAME, time_msec());
     ovn_update_ipv6_prefix(&data->ports);

tests/ovn-northd.at
@@ -6640,4 +6640,27 @@ AT_CHECK([ovn-sbctl dump-flows DR | grep -e lr_in_unsnat -e lr_out_snat -e lr_in
 ])
 
 AT_CLEANUP
-])
+
+AT_SETUP([LR NB Static_MAC_Binding table])
+ovn_start
+
+# Create logical routers
+ovn-nbctl lr-add lr0
+ovn-nbctl lrp-add lr0 lr0-p0 00:00:01:01:02:03 192.168.10.1/24
+ovn-nbctl lrp-add lr0 lr0-p1 00:00:02:02:03:04 192.168.11.1/24
+
+ovn-nbctl static-mac-binding-add lr0-p0 192.168.10.10 00:00:11:22:33:44
+ovn-nbctl static-mac-binding-add lr0-p0 192.168.10.100 00:00:22:33:44:55
+
+wait_row_count nb:Static_MAC_Binding 2 logical_port=lr0-p0
+wait_row_count Static_MAC_Binding 1 logical_port=lr0-p0 ip=192.168.10.10 mac="00\:00\:11\:22\:33\:44"
+wait_row_count Static_MAC_Binding 1 logical_port=lr0-p0 ip=192.168.10.100 mac="00\:00\:22\:33\:44\:55"
+
+ovn-nbctl static-mac-binding-add lr0-p1 10.0.0.10 00:00:33:44:55:66
+wait_row_count nb:Static_MAC_Binding 1 logical_port=lr0-p1
+wait_row_count Static_MAC_Binding 1 logical_port=lr0-p1 ip=10.0.0.10 mac="00\:00\:33\:44\:55\:66"
+
+ovn-nbctl --may-exist static-mac-binding-add lr0-p0 192.168.10.100 00:00:22:33:55:66
+wait_row_count Static_MAC_Binding 1 logical_port=lr0-p0 ip=192.168.10.100 mac="00\:00\:22\:33\:55\:66"
+
+AT_CLEANUP

controller/lflow.c
@@ -1622,40 +1622,50 @@ static void
 consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
                        const struct hmap *local_datapaths,
                        const struct sbrec_mac_binding *b,
-                       struct ovn_desired_flow_table *flow_table)
+                       const struct sbrec_static_mac_binding *smb,
+                       struct ovn_desired_flow_table *flow_table,
+                       uint16_t priority)
 {
+    if (!b && !smb) {
+        return;
+    }
+
+    char *logical_port = b ? b->logical_port : smb->logical_port;
+    char *ip = b ? b->ip : smb->ip;
+    char *mac = b ? b->mac : smb->mac;
+
     const struct sbrec_port_binding *pb
-        = lport_lookup_by_name(sbrec_port_binding_by_name, b->logical_port);
+        = lport_lookup_by_name(sbrec_port_binding_by_name, logical_port);
     if (!pb || !get_local_datapath(local_datapaths,
                                    pb->datapath->tunnel_key)) {
         return;
     }
 
-    struct eth_addr mac;
-    if (!eth_addr_from_string(b->mac, &mac)) {
+    struct eth_addr mac_addr;
+    if (!eth_addr_from_string(mac, &mac_addr)) {
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
-        VLOG_WARN_RL(&rl, "bad 'mac' %s", b->mac);
+        VLOG_WARN_RL(&rl, "bad 'mac' %s", mac);
         return;
     }
 
     struct match get_arp_match = MATCH_CATCHALL_INITIALIZER;
     struct match lookup_arp_match = MATCH_CATCHALL_INITIALIZER;
 
-    if (strchr(b->ip, '.')) {
-        ovs_be32 ip;
-        if (!ip_parse(b->ip, &ip)) {
+    if (strchr(ip, '.')) {
+        ovs_be32 ip_addr;
+        if (!ip_parse(ip, &ip_addr)) {
             static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
-            VLOG_WARN_RL(&rl, "bad 'ip' %s", b->ip);
+            VLOG_WARN_RL(&rl, "bad 'ip' %s", ip);
             return;
         }
-        match_set_reg(&get_arp_match, 0, ntohl(ip));
-        match_set_reg(&lookup_arp_match, 0, ntohl(ip));
+        match_set_reg(&get_arp_match, 0, ntohl(ip_addr));
+        match_set_reg(&lookup_arp_match, 0, ntohl(ip_addr));
         match_set_dl_type(&lookup_arp_match, htons(ETH_TYPE_ARP));
     } else {
         struct in6_addr ip6;
-        if (!ipv6_parse(b->ip, &ip6)) {
+        if (!ipv6_parse(ip, &ip6)) {
             static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
-            VLOG_WARN_RL(&rl, "bad 'ip' %s", b->ip);
+            VLOG_WARN_RL(&rl, "bad 'ip' %s", ip);
             return;
         }
         ovs_be128 value;
@@ -1678,20 +1688,22 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
     uint64_t stub[1024 / 8];
     struct ofpbuf ofpacts = OFPBUF_STUB_INITIALIZER(stub);
     uint8_t value = 1;
-    put_load(mac.ea, sizeof mac.ea, MFF_ETH_DST, 0, 48, &ofpacts);
+    put_load(mac_addr.ea, sizeof mac_addr.ea, MFF_ETH_DST, 0, 48, &ofpacts);
     put_load(&value, sizeof value, MFF_LOG_FLAGS, MLF_LOOKUP_MAC_BIT, 1,
              &ofpacts);
-    ofctrl_add_flow(flow_table, OFTABLE_MAC_BINDING, 100,
-                    b->header_.uuid.parts[0], &get_arp_match,
-                    &ofpacts, &b->header_.uuid);
+    ofctrl_add_flow(flow_table, OFTABLE_MAC_BINDING, priority,
+                    b ? b->header_.uuid.parts[0] : smb->header_.uuid.parts[0],
+                    &get_arp_match, &ofpacts,
+                    b ? &b->header_.uuid : &smb->header_.uuid);
 
     ofpbuf_clear(&ofpacts);
     put_load(&value, sizeof value, MFF_LOG_FLAGS, MLF_LOOKUP_MAC_BIT, 1,
              &ofpacts);
-    match_set_dl_src(&lookup_arp_match, mac);
-    ofctrl_add_flow(flow_table, OFTABLE_MAC_LOOKUP, 100,
-                    b->header_.uuid.parts[0], &lookup_arp_match,
-                    &ofpacts, &b->header_.uuid);
+    match_set_dl_src(&lookup_arp_match, mac_addr);
+    ofctrl_add_flow(flow_table, OFTABLE_MAC_LOOKUP, priority,
+                    b ? b->header_.uuid.parts[0] : smb->header_.uuid.parts[0],
+                    &lookup_arp_match, &ofpacts,
+                    b ? &b->header_.uuid : &smb->header_.uuid);
 
     ofpbuf_uninit(&ofpacts);
 }
@@ -1701,13 +1713,23 @@ consider_neighbor_flow(struct ovsdb_idl_index *sbrec_port_binding_by_name,
 static void
 add_neighbor_flows(struct ovsdb_idl_index *sbrec_port_binding_by_name,
                    const struct sbrec_mac_binding_table *mac_binding_table,
+                   const struct sbrec_static_mac_binding_table *smb_table,
                    const struct hmap *local_datapaths,
                    struct ovn_desired_flow_table *flow_table)
 {
+    /* Add flows for learnt MAC bindings */
     const struct sbrec_mac_binding *b;
     SBREC_MAC_BINDING_TABLE_FOR_EACH (b, mac_binding_table) {
         consider_neighbor_flow(sbrec_port_binding_by_name, local_datapaths,
-                               b, flow_table);
+                               b, NULL, flow_table, 100);
+    }
+
+    /* Add flows for statically configured MAC bindings */
+    const struct sbrec_static_mac_binding *smb;
+    SBREC_STATIC_MAC_BINDING_TABLE_FOR_EACH (smb, smb_table) {
+        consider_neighbor_flow(sbrec_port_binding_by_name, local_datapaths,
+                               NULL, smb, flow_table,
+                               smb->override_dynamic_mac ? 150 : 50);
     }
 }
 
@@ -2366,7 +2388,7 @@ add_lb_hairpin_flows(const struct sbrec_load_balancer_table *lb_table,
 
 /* Handles neighbor changes in mac_binding table. */
 void
-lflow_handle_changed_neighbors(
+lflow_handle_changed_mac_bindings(
     struct ovsdb_idl_index *sbrec_port_binding_by_name,
     const struct sbrec_mac_binding_table *mac_binding_table,
     const struct hmap *local_datapaths,
@@ -2393,7 +2415,36 @@ lflow_handle_changed_neighbors(
             VLOG_DBG("handle new mac_binding "UUID_FMT,
                      UUID_ARGS(&mb->header_.uuid));
             consider_neighbor_flow(sbrec_port_binding_by_name, local_datapaths,
-                                   mb, flow_table);
+                                   mb, NULL, flow_table, 100);
+        }
+    }
+}
+
+/* Handles changes to static_mac_binding table. */
+void
+lflow_handle_changed_static_mac_bindings(
+    struct ovsdb_idl_index *sbrec_port_binding_by_name,
+    const struct sbrec_static_mac_binding_table *smb_table,
+    const struct hmap *local_datapaths,
+    struct ovn_desired_flow_table *flow_table)
+{
+    const struct sbrec_static_mac_binding *smb;
+    SBREC_STATIC_MAC_BINDING_TABLE_FOR_EACH_TRACKED (smb, smb_table) {
+        if (sbrec_static_mac_binding_is_deleted(smb)) {
+            VLOG_DBG("handle deleted static_mac_binding "UUID_FMT,
+                     UUID_ARGS(&smb->header_.uuid));
+            ofctrl_remove_flows(flow_table, &smb->header_.uuid);
+        } else {
+            if (!sbrec_static_mac_binding_is_new(smb)) {
+                VLOG_DBG("handle updated static_mac_binding "UUID_FMT,
+                         UUID_ARGS(&smb->header_.uuid));
+                ofctrl_remove_flows(flow_table, &smb->header_.uuid);
+            }
+            VLOG_DBG("handle new static_mac_binding "UUID_FMT,
+                     UUID_ARGS(&smb->header_.uuid));
+            consider_neighbor_flow(sbrec_port_binding_by_name, local_datapaths,
+                                   NULL, smb, flow_table,
+                                   smb->override_dynamic_mac ? 150 : 50);
         }
     }
 }
@@ -2463,7 +2514,9 @@ lflow_run(struct lflow_ctx_in *l_ctx_in, struct lflow_ctx_out *l_ctx_out)
 
     add_logical_flows(l_ctx_in, l_ctx_out);
     add_neighbor_flows(l_ctx_in->sbrec_port_binding_by_name,
-                       l_ctx_in->mac_binding_table, l_ctx_in->local_datapaths,
+                       l_ctx_in->mac_binding_table,
+                       l_ctx_in->static_mac_binding_table,
+                       l_ctx_in->local_datapaths,
                        l_ctx_out->flow_table);
     add_lb_hairpin_flows(l_ctx_in->lb_table, l_ctx_in->local_datapaths,
                          l_ctx_in->check_ct_label_for_lb_hairpin,

controller/lflow.h
@@ -147,6 +147,7 @@ struct lflow_ctx_in {
     const struct sbrec_fdb_table *fdb_table;
     const struct sbrec_chassis *chassis;
     const struct sbrec_load_balancer_table *lb_table;
+    const struct sbrec_static_mac_binding_table *static_mac_binding_table;
     const struct hmap *local_datapaths;
     const struct shash *addr_sets;
     const struct shash *port_groups;
@@ -192,9 +193,14 @@ bool lflow_handle_addr_set_update(const char *as_name, struct addr_set_diff *,
                                   struct lflow_ctx_out *,
                                   bool *changed);
 
-void lflow_handle_changed_neighbors(
+void lflow_handle_changed_mac_bindings(
     struct ovsdb_idl_index *sbrec_port_binding_by_name,
-    const struct sbrec_mac_binding_table *,
+    const struct sbrec_mac_binding_table *mac_binding_table,
+    const struct hmap *local_datapaths,
+    struct ovn_desired_flow_table *);
+void lflow_handle_changed_static_mac_bindings(
+    struct ovsdb_idl_index *sbrec_port_binding_by_name,
+    const struct sbrec_static_mac_binding_table *smb_table,
     const struct hmap *local_datapaths,
     struct ovn_desired_flow_table *);
 bool lflow_handle_changed_lbs(struct lflow_ctx_in *, struct lflow_ctx_out *);

controller/ovn-controller.c
@@ -977,7 +977,8 @@ ctrl_register_ovs_idl(struct ovsdb_idl *ovs_idl)
     SB_NODE(dns, "dns") \
     SB_NODE(load_balancer, "load_balancer") \
     SB_NODE(fdb, "fdb") \
-    SB_NODE(meter, "meter")
+    SB_NODE(meter, "meter") \
+    SB_NODE(static_mac_binding, "static_mac_binding")
 
 enum sb_engine_node {
 #define SB_NODE(NAME, NAME_STR) SB_##NAME,
@@ -2362,6 +2363,10 @@ init_lflow_ctx(struct engine_node *node,
         (struct sbrec_fdb_table *)EN_OVSDB_GET(
             engine_get_input("SB_fdb", node));
 
+    struct sbrec_static_mac_binding_table *smb_table =
+        (struct sbrec_static_mac_binding_table *)EN_OVSDB_GET(
+            engine_get_input("SB_static_mac_binding", node));
+
     struct ovsrec_open_vswitch_table *ovs_table =
         (struct ovsrec_open_vswitch_table *)EN_OVSDB_GET(
             engine_get_input("OVS_open_vswitch", node));
@@ -2414,6 +2419,7 @@ init_lflow_ctx(struct engine_node *node,
     l_ctx_in->fdb_table = fdb_table,
     l_ctx_in->chassis = chassis;
     l_ctx_in->lb_table = lb_table;
+    l_ctx_in->static_mac_binding_table = smb_table;
     l_ctx_in->local_datapaths = &rt_data->local_datapaths;
     l_ctx_in->addr_sets = addr_sets;
     l_ctx_in->port_groups = port_groups;
@@ -2558,8 +2564,34 @@ lflow_output_sb_mac_binding_handler(struct engine_node *node, void *data)
 
     struct ed_type_lflow_output *lfo = data;
 
-    lflow_handle_changed_neighbors(sbrec_port_binding_by_name,
+    lflow_handle_changed_mac_bindings(sbrec_port_binding_by_name,
             mac_binding_table, local_datapaths, &lfo->flow_table);
+
+    engine_set_node_state(node, EN_UPDATED);
+    return true;
+}
+
+static bool
+lflow_output_sb_static_mac_binding_handler(struct engine_node *node,
+                                           void *data)
+{
+    struct ovsdb_idl_index *sbrec_port_binding_by_name =
+        engine_ovsdb_node_get_index(
+                engine_get_input("SB_port_binding", node),
+                "name");
+
+    struct sbrec_static_mac_binding_table *smb_table =
+        (struct sbrec_static_mac_binding_table *)EN_OVSDB_GET(
+            engine_get_input("SB_static_mac_binding", node));
+
+    struct ed_type_runtime_data *rt_data =
+        engine_get_input_data("runtime_data", node);
+    const struct hmap *local_datapaths = &rt_data->local_datapaths;
+
+    struct ed_type_lflow_output *lfo = data;
+
+    lflow_handle_changed_static_mac_bindings(sbrec_port_binding_by_name,
+        smb_table, local_datapaths, &lfo->flow_table);
 
     engine_set_node_state(node, EN_UPDATED);
     return true;
@@ -3401,6 +3433,8 @@ main(int argc, char *argv[])
 
     engine_add_input(&en_lflow_output, &en_sb_mac_binding,
                      lflow_output_sb_mac_binding_handler);
+    engine_add_input(&en_lflow_output, &en_sb_static_mac_binding,
+                     lflow_output_sb_static_mac_binding_handler);
     engine_add_input(&en_lflow_output, &en_sb_logical_flow,
                      lflow_output_sb_logical_flow_handler);
     /* Using a noop handler since we don't really need any data from datapath

lib/automake.mk
@@ -38,6 +38,8 @@ lib_libovn_la_SOURCES = \
 	lib/inc-proc-eng.h \
 	lib/lb.c \
 	lib/lb.h \
+	lib/static-mac-binding-index.c \
+	lib/static-mac-binding-index.h \
 	lib/stopwatch-names.h \
 	lib/vif-plug-provider.h \
 	lib/vif-plug-provider.c \

lib/static-mac-binding-index.c
@@ -1,0 +1,43 @@
+/* Copyright (c) 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "lib/static-mac-binding-index.h"
+#include "lib/ovn-sb-idl.h"
+
+struct ovsdb_idl_index *
+static_mac_binding_index_create(struct ovsdb_idl *idl)
+{
+    return ovsdb_idl_index_create2(idl,
+                                   &sbrec_static_mac_binding_col_logical_port,
+                                   &sbrec_static_mac_binding_col_ip);
+}
+
+const struct sbrec_static_mac_binding *
+static_mac_binding_lookup(struct ovsdb_idl_index *smb_index,
+                          const char *logical_port, const char *ip)
+{
+    struct sbrec_static_mac_binding *target =
+        sbrec_static_mac_binding_index_init_row(smb_index);
+    sbrec_static_mac_binding_index_set_logical_port(target, logical_port);
+    sbrec_static_mac_binding_index_set_ip(target, ip);
+
+    struct sbrec_static_mac_binding *smb =
+        sbrec_static_mac_binding_index_find(smb_index, target);
+    sbrec_static_mac_binding_index_destroy_row(target);
+
+    return smb;
+}

lib/static-mac-binding-index.h
@@ -1,0 +1,27 @@
+/* Copyright (c) 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef OVN_STATIC_MAC_BINDING_INDEX_H
+#define OVN_STATIC_MAC_BINDING_INDEX_H 1
+
+struct ovsdb_idl;
+
+struct ovsdb_idl_index *static_mac_binding_index_create(struct ovsdb_idl *);
+const struct sbrec_static_mac_binding *static_mac_binding_lookup(
+    struct ovsdb_idl_index *smb_index,
+    const char *logical_port,
+    const char *ip);
+
+#endif /* lib/static-mac-binding-index.h */

commit feb871b3334619e9a24c3fd618cba04229a3b14e

northd: Clean up SB MAC bindings for deleted ports.
Static_MAC_Binding (SMB) records are only deleted from the
southbound-db via ovn-northd when there is no such SMB found in the
northbound-db. This leads to problems when a port becomes stale (ex.
through lr deletion), because the Logical_Router and Logical_Router_Port
records are removed from the nb-db, but not the Static_MAC_Binding record.

So, when ovn-northd attempts to remove the corresponding
Datapath_Binding and Port_Binding records from sb-db, it fails because
of a referential integrity violation: the Static_MAC_Binding record
contains a reference to Datapath_Binding.

The corrected behavior is that SMB's are now deleted:
1. when there's no such SMB in the nb-db, and
2. when there is no port in the nb-db with the SMB's associated port name.
Thus, all three records, Datapath_Binding, Port_Binding, and
Static_MAC_Binding, are removed within the same transaction.

Also added a unit test to verify correct behavior.

Fixes: b22684d4e37c ("Add a northbound interface to program MAC_Binding table")
Reported-at: https://issues.redhat.com/browse/FDP-723
Signed-off-by: Rosemarie O'Riorden <roriorden@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -18336,7 +18336,8 @@ build_static_mac_binding_table(
     struct hmap *lr_ports)
 {
     /* Cleanup SB Static_MAC_Binding entries which do not have corresponding
-     * NB Static_MAC_Binding entries. */
+     * NB Static_MAC_Binding entries, and SB Static_MAC_Binding entries for
+     * which there is not a NB Port_Binding of the same name. */
     const struct nbrec_static_mac_binding *nb_smb;
     const struct sbrec_static_mac_binding *sb_smb;
     SBREC_STATIC_MAC_BINDING_TABLE_FOR_EACH_SAFE (sb_smb,
@@ -18344,7 +18345,16 @@ build_static_mac_binding_table(
         nb_smb = static_mac_binding_by_port_ip(nbrec_static_mb_table,
                                                sb_smb->logical_port,
                                                sb_smb->ip);
-        if (!nb_smb) {
+        struct ovn_port *op = NULL;
+        struct ovn_datapath *od = NULL;
+        if (nb_smb) {
+            op = ovn_port_find(lr_ports, nb_smb->logical_port);
+            if (op) {
+                od = op->od;
+            }
+        }
+        bool delete_smb = !nb_smb || !op || !op->nbrp || !od || !od->sb;
+        if (delete_smb) {
             sbrec_static_mac_binding_delete(sb_smb);
         }
     }

tests/ovn-northd.at
@@ -8081,6 +8081,19 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([LR NB Static_MAC_Binding garbage collection])
+ovn_start
+
+check ovn-nbctl lr-add lr -- lrp-add lr lr-lrp 00:00:00:00:00:01 1.1.1.1/24
+check ovn-nbctl static-mac-binding-add lr-lrp 1.1.1.2 00:00:00:00:00:02
+wait_row_count sb:Static_MAC_Binding 1 logical_port=lr-lrp ip=1.1.1.2 mac="00\:00\:00\:00\:00\:02"
+check ovn-nbctl lr-del lr
+wait_row_count sb:Static_MAC_Binding 0
+
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
 AT_SETUP([LR neighbor lookup and learning flows])
 ovn_start
 