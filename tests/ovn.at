ovsrobot/ovn

commit 0688d7e834b95cccfb9bd6bc79cee08f5f3724dc

northd: Allow multichassis port to be bound on remote chassis.
In order to support tunneling with multichassis port across
multiple AZ allow the port to be bound to remote chassis if
there is multiple chassis requested.

Reported-at: https://issues.redhat.com/browse/FDP-850
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/northd.c
@@ -3199,25 +3199,25 @@ ovn_port_update_sbrec(struct ovsdb_idl_txn *ovnsb_txn,
                 }
             }
 
-            if (lsp_is_remote(op->nbsp)) {
+            if (lsp_is_remote(op->nbsp) ||
+                op->sb->requested_additional_chassis) {
                 /* ovn-northd is supposed to set port_binding for remote ports
-                 * if requested chassis is marked as remote. */
+                 * if requested chassis is marked as remote. In addition,
+                 * if the primary chassis is remote bind it.*/
                 if (op->sb->requested_chassis &&
                     smap_get_bool(&op->sb->requested_chassis->other_config,
                                   "is-remote", false)) {
                     sbrec_port_binding_set_chassis(op->sb,
                                                    op->sb->requested_chassis);
-                    smap_add(&options, "is-remote-nb-bound", "true");
-                } else if (smap_get_bool(&op->sb->options,
-                                         "is-remote-nb-bound", false)) {
+                } else {
                     sbrec_port_binding_set_chassis(op->sb, NULL);
-                    smap_add(&options, "is-remote-nb-bound", "false");
                 }
             } else if (op->sb->chassis &&
                        smap_get_bool(&op->sb->chassis->other_config,
                                      "is-remote", false)) {
-                /* Its not a remote port but if the chassis is set and if its a
-                 * remote chassis then clear it. */
+                /* It's not a remote port but if the chassis is set and if
+                 * it's a remote chassis, and we don't have any additional
+                 * chassis then clear it. */
                 sbrec_port_binding_set_chassis(op->sb, NULL);
             }
 

tests/ovn.at
@@ -39823,3 +39823,53 @@ OVN_CLEANUP([hv1])
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([Multichassis port with remote chassis])
+ovn_start
+
+net_add n1
+
+sim_add hv1
+as hv1
+check ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.11
+
+check ovn-sbctl chassis-add hv2 geneve 192.168.0.12 \
+    -- set chassis hv2 other_config:is-remote=true
+
+check ovn-nbctl ls-add ls
+check ovn-nbctl lsp-add ls multi
+
+check ovs-vsctl -- add-port br-int multi \
+    -- set Interface multi external-ids:iface-id=multi
+
+local_chassis=$(fetch_column Chassis _uuid name=hv1)
+remote_chassis=$(fetch_column Chassis _uuid name=hv2)
+
+check ovn-nbctl --wait=hv lsp-set-options multi requested-chassis=hv1
+wait_for_ports_up multi
+
+# Check that it is bound locally
+check_column $local_chassis Port_Binding chassis logical_port=multi
+check_column "[]" Port_Binding additional_chassis logical_port=multi
+
+check ovn-nbctl --wait=hv lsp-set-options multi requested-chassis=hv2,hv1
+wait_for_ports_up multi
+
+# Check that it is bound on remote as primary and additional locally
+check_column $remote_chassis Port_Binding chassis logical_port=multi
+check_column "[$local_chassis]" Port_Binding additional_chassis logical_port=multi
+
+check ovn-nbctl --wait=hv lsp-set-options multi requested-chassis=hv2 \
+    -- lsp-set-type multi remote
+wait_for_ports_up multi
+
+# Check that it is bound remotely
+check_column $remote_chassis Port_Binding chassis logical_port=multi
+check_column "[]" Port_Binding additional_chassis logical_port=multi
+
+OVN_CLEANUP([hv1])
+
+AT_CLEANUP
+])

commit ab2a9b0d5a153f526ea7ccff88eb8e708b4a9d1d

northd: Make sure that we keep explicit chassis for remote PB.
Ensure the chassis for remote port binding is not cleared when it's
set explicitly by CMS. The requested-chassis is still preferred
method to direct changes to SB DB, this option remains for
backward compatibility.

Fixes: 0688d7e834b9 ("northd: Allow multichassis port to be bound on remote chassis.")
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -3209,8 +3209,11 @@ ovn_port_update_sbrec(struct ovsdb_idl_txn *ovnsb_txn,
                                   "is-remote", false)) {
                     sbrec_port_binding_set_chassis(op->sb,
                                                    op->sb->requested_chassis);
-                } else {
+                    smap_add(&options, "is-remote-nb-bound", "true");
+                } else if (smap_get_bool(&op->sb->options,
+                                         "is-remote-nb-bound", false)) {
                     sbrec_port_binding_set_chassis(op->sb, NULL);
+                    smap_add(&options, "is-remote-nb-bound", "false");
                 }
             } else if (op->sb->chassis &&
                        smap_get_bool(&op->sb->chassis->other_config,

tests/ovn.at
@@ -39686,3 +39686,37 @@ OVN_CLEANUP([hv1])
 
 AT_CLEANUP
 ])
+
+# ovn-kubernetes used to explicitly set chassis,
+# test if we don't accidentally remove it.
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([Remote port with explicit SB chassis])
+ovn_start
+
+net_add n1
+
+sim_add hv1
+as hv1
+check ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.11
+
+check ovn-sbctl chassis-add hv2 geneve 192.168.0.12 \
+    -- set chassis hv2 other_config:is-remote=true
+
+check ovn-nbctl ls-add ls
+check ovn-nbctl lsp-add ls lsp
+check ovn-nbctl lsp-set-type lsp remote
+
+remote_chassis=$(fetch_column Chassis _uuid name=hv2)
+lsp_uuid=$(fetch_column Port_Binding _uuid logical_port=lsp)
+
+check ovn-sbctl set Port_Binding $lsp_uuid chassis=$remote_chassis
+
+check ovn-nbctl --wait=hv sync
+
+check_column $remote_chassis Port_Binding chassis logical_port=lsp
+
+OVN_CLEANUP([hv1])
+
+AT_CLEANUP
+])