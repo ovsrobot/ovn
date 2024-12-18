674824239d5d54ff5ec0da633741db9c252855ac
Felix Huettner <felix.huettner@stackit.cloud>
2024-12-18 11:25:03 +0100

    northd: Sync routing data to pb.
    this allows the the ovn-controller to later find all ports that
    participate in dynamic routing.
    
    Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/northd.c
+++ northd/northd.c
@@ -4052,6 +4052,21 @@ sync_pb_for_lrp(struct ovn_port *op,
         }
     }
 
+    if (is_cr_port(op) || chassis_name) {
+        if (smap_get_bool(&op->nbrp->options, "maintain-vrf", false)) {
+            smap_add(&new, "maintain-vrf", "true");
+        }
+        if (smap_get_bool(&op->od->nbr->options, "dynamic-routing", false)) {
+            smap_add(&new, "dynamic-routing", "true");
+        }
+        const char *ifname = smap_get(&op->nbrp->options,
+                                      "dynamic-routing-ifname");
+        if (ifname) {
+            smap_add(&new, "dynamic-routing-ifname", ifname);
+        }
+    }
+
+
     const char *ipv6_pd_list = smap_get(&op->sb->options, "ipv6_ra_pd_list");
     if (ipv6_pd_list) {
         smap_add(&new, "ipv6_ra_pd_list", ipv6_pd_list);
