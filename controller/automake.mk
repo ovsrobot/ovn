7abc6d49c1449fc9c6ba6f74eff1743f5758db38
Felix Huettner <felix.huettner@stackit.cloud>
2024-12-19 15:02:52 +0100

    controller: Announce routes via route-exchange.
    This engine node takes the routes from the "route" engine node and ensures
    they are written to the linux side.
    
    It is separate from the "route" engine node as it will also be used to
    learn routes in the future.
    
    Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/automake.mk
+++ controller/automake.mk
@@ -52,13 +52,18 @@ controller_ovn_controller_SOURCES = \
 	controller/ct-zone.c \
 	controller/ovn-dns.c \
 	controller/ovn-dns.h \
+	controller/route-exchange.h \
 	controller/route.h \
 	controller/route.c
 
 if HAVE_NETLINK
 controller_ovn_controller_SOURCES += \
 	controller/route-exchange-netlink.h \
-	controller/route-exchange-netlink.c
+	controller/route-exchange-netlink.c \
+	controller/route-exchange.c
+else
+controller_ovn_controller_SOURCES += \
+	controller/route-exchange-stub.c
 endif
 
 controller_ovn_controller_LDADD = lib/libovn.la $(OVS_LIBDIR)/libopenvswitch.la
