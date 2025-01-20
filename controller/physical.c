026daa6f40bf3b94aa1648c03bd671184e67de45
Ales Musil <amusil@redhat.com>
2025-01-20 10:16:42 +0100

    physical: Simplify handler for logical port changes.
    Simplify incremental processing for port bindings especially
    with peer ports.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/physical.c
+++ controller/physical.c
@@ -2413,22 +2413,15 @@ physical_handle_flows_for_lport(const struct sbrec_port_binding *pb,
     }
 
     /* Always update pb and the configured peer for patch ports. */
-    if (!removed || !strcmp(pb->type, "patch")) {
+    if (!removed) {
         physical_eval_port_binding(p_ctx, pb, flow_table);
     }
 
-    if (!strcmp(pb->type, "patch")) {
-        if (removed) {
-            ofctrl_remove_flows(flow_table, &pb->header_.uuid);
-        }
-        const struct sbrec_port_binding *peer =
-            get_binding_peer(p_ctx->sbrec_port_binding_by_name, pb);
-        if (peer) {
-            physical_eval_port_binding(p_ctx, peer, flow_table);
-            if (removed) {
-                ofctrl_remove_flows(flow_table, &peer->header_.uuid);
-            }
-        }
+    const struct sbrec_port_binding *peer =
+        get_binding_peer(p_ctx->sbrec_port_binding_by_name, pb);
+    if (peer) {
+        ofctrl_remove_flows(flow_table, &peer->header_.uuid);
+        physical_eval_port_binding(p_ctx, peer, flow_table);
     }
 
     return true;
