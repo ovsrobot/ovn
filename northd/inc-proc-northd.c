4f6f13f4bfc852206d18a2298e5267eb5a3e5f66
Felix Huettner <felix.huettner@stackit.cloud>
2025-01-21 16:47:35 +0100

    northd: Handle learned routes.
    Here we expand the previous routes-sync engine node to not only
    advertise routes to the southbound table, but also learn received routes
    from this table.
    
    These routes are then passed to the same logic that connected and static
    routes are using for flow generation.
    However we prioritize these routes lower than connected or static routes
    as information in cluster (for the same prefix length) should always be
    more correct then learned routes.
    This is also consistent with the behaviour of phyiscal routers.
    
    Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/inc-proc-northd.c
+++ northd/inc-proc-northd.c
@@ -42,6 +42,7 @@
 #include "en-sync-sb.h"
 #include "en-sync-from-sb.h"
 #include "en-advertised-route-sync.h"
+#include "en-learned-route-sync.h"
 #include "unixctl.h"
 #include "util.h"
 
@@ -104,7 +105,8 @@ static unixctl_cb_func chassis_features_list;
     SB_NODE(static_mac_binding, "static_mac_binding") \
     SB_NODE(chassis_template_var, "chassis_template_var") \
     SB_NODE(logical_dp_group, "logical_dp_group") \
-    SB_NODE(advertised_route, "advertised_route")
+    SB_NODE(advertised_route, "advertised_route") \
+    SB_NODE(learned_route, "learned_route")
 
 enum sb_engine_node {
 #define SB_NODE(NAME, NAME_STR) SB_##NAME,
@@ -164,6 +166,7 @@ static ENGINE_NODE(routes, "routes");
 static ENGINE_NODE(bfd, "bfd");
 static ENGINE_NODE(bfd_sync, "bfd_sync");
 static ENGINE_NODE(advertised_route_sync, "advertised_route_sync");
+static ENGINE_NODE(learned_route_sync, "learned_route_sync");
 
 void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                           struct ovsdb_idl_loop *sb)
@@ -270,6 +273,11 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_advertised_route_sync, &en_sb_advertised_route,
                      NULL);
 
+    engine_add_input(&en_learned_route_sync, &en_routes, NULL);
+    engine_add_input(&en_learned_route_sync, &en_sb_learned_route, NULL);
+    engine_add_input(&en_learned_route_sync, &en_northd,
+                     learned_route_sync_northd_change_handler);
+
     engine_add_input(&en_sync_meters, &en_nb_acl, NULL);
     engine_add_input(&en_sync_meters, &en_nb_meter, NULL);
     engine_add_input(&en_sync_meters, &en_sb_meter, NULL);
@@ -283,6 +291,10 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_lflow, &en_bfd_sync, NULL);
     engine_add_input(&en_lflow, &en_route_policies, NULL);
     engine_add_input(&en_lflow, &en_routes, NULL);
+    /* XXX: This causes a full lflow recompute on each change to any route.
+     * At least for learned routes we should add incremental processing here.
+     * */
+    engine_add_input(&en_lflow, &en_learned_route_sync, NULL);
     engine_add_input(&en_lflow, &en_global_config,
                      node_global_config_handler);
 
