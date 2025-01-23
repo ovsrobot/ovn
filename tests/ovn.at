ovsrobot/ovn

commit b52ba504ed6fd7d1068c1b6194bf62e892a9aa94

northd: Fix relying on naming coincidences.
We previous relied on the fact the the southbound Port_Binding gets the
same name as the Northbound port. However in all of the cases we use
this we already know the name the southbound port will have. Therefore we
just use this name, instead of getting it from northbound.

This is a prerequisite for later patches that use derived router ports.

Acked-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/northd.c
@@ -3298,9 +3298,17 @@ ovn_port_update_sbrec(struct ovsdb_idl_txn *ovnsb_txn,
                 smap_init(&new);
 
                 if (is_cr_port(op)) {
-                    smap_add(&new, "distributed-port", op->nbsp->name);
+                    smap_add(&new, "distributed-port", op->primary_port->key);
                 } else if (router_port) {
-                    smap_add(&new, "peer", router_port);
+                    /* op->peer can be null if the peer is disabed. In this
+                     * case we fall back to the router_port string which
+                     * might be wrong, but since the port does not exist that
+                     * does not matter. */
+                    if (op->peer) {
+                        smap_add(&new, "peer", op->peer->key);
+                    } else {
+                        smap_add(&new, "peer", router_port);
+                    }
                 }
                 if (chassis) {
                     smap_add(&new, "l3gateway-chassis", chassis);
@@ -4016,7 +4024,7 @@ sync_pb_for_lrp(struct ovn_port *op,
             lr_stateful_table_find_by_index(lr_stateful_table, op->od->index);
         ovs_assert(lr_stateful_rec);
 
-        smap_add(&new, "distributed-port", op->nbrp->name);
+        smap_add(&new, "distributed-port", op->primary_port->key);
 
         bool always_redirect =
             !lr_stateful_rec->lrnat_rec->has_distributed_nat &&
@@ -4041,10 +4049,7 @@ sync_pb_for_lrp(struct ovn_port *op,
             smap_add(&new, "peer", op->peer->key);
             if (op->nbrp->ha_chassis_group ||
                 op->nbrp->n_gateway_chassis) {
-                char *redirect_name =
-                    ovn_chassis_redirect_name(op->nbrp->name);
-                smap_add(&new, "chassis-redirect-port", redirect_name);
-                free(redirect_name);
+                smap_add(&new, "chassis-redirect-port", op->cr_port->key);
             }
         }
         if (chassis_name) {

commit 7199516a10a7f21374605bf14ce58625baacb4c9

northd: Fix segfault when changing the router to a gateway router.
When a distributed router with a gateway router port is changed
to a gateway router (options:chassis=<ch>), during the recompute
run, op->cr_port is not created and instead a warning is logged
because of bad configuration.

The commit b52ba504ed6f is accessing op->cr_port even though cr_port
is not created for the 'op' and this results in a segfault.

This patch fixes it.

Fixes: b52ba504ed6f ("northd: Fix relying on naming coincidences.")
Signed-off-by: Numan Siddique <numans@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -4086,8 +4086,12 @@ sync_pb_for_lrp(struct ovn_port *op,
     } else {
         if (op->peer) {
             smap_add(&new, "peer", op->peer->key);
-            if (op->nbrp->ha_chassis_group ||
-                op->nbrp->n_gateway_chassis) {
+            /* Even if the router port has ha_chassis_group or
+             * gateway_chassis configured, don't assume that its
+             * chassis-redirect port is created.
+             * Check op->cr_port for NULL before accessing. */
+            if (op->cr_port && (op->nbrp->ha_chassis_group ||
+                op->nbrp->n_gateway_chassis)) {
                 smap_add(&new, "chassis-redirect-port", op->cr_port->key);
             }
         }

tests/ovn.at
@@ -40852,6 +40852,36 @@ OVN_CLEANUP([hv1],[hv2])
 AT_CLEANUP
 ])
 
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([Switch from Distributed Router to Gateway Router])
+
+ovn_start
+net_add n1
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.11
+
+check ovn-nbctl ls-add public
+check ovn-nbctl lsp-add public public-lr0
+check ovn-nbctl lsp-set-type public-lr0 router
+check ovn-nbctl lsp-set-addresses public-lr0 router
+check ovn-nbctl lsp-set-options public-lr0 router-port=lr0-public
+
+check ovn-nbctl lr-add lr0
+check ovn-nbctl lrp-add lr0 lr0-public 00:00:20:20:12:13 172.168.0.100/24
+check ovn-nbctl --wait=hv lrp-set-gateway-chassis lr0-public hv1
+wait_column "chassisredirect" sb:Port_Binding type logical_port=cr-lr0-public
+
+check ovn-nbctl --wait=hv set logical_router lr0 options:chassis=hv1
+wait_row_count sb:Port_Binding 0 logical_port=cr-lr0-public
+check ovn-nbctl --wait=hv clear logical_router_port lr0-public gateway_chassis
+wait_column "l3gateway" sb:Port_Binding type logical_port=lr0-public
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])
+
 OVN_FOR_EACH_NORTHD([
 AT_SETUP([requested-tnl-key-recompute])
 AT_KEYWORDS([requested-tnl-key-recompute])