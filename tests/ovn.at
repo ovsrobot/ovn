ovsrobot/ovn

commit 551527a5e68e7233ad80d212d549df98f13e37bc

controller: Update FDB timestamp
Add FDB timestamp update to mac_cache and
use the statctrl thread to dump the needed stats.
The updates should happen once per dump_period
(3/4 of the aging threshold) per chassis only if
the FDB is actively used.

Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>

tests/ovn.at
@@ -36460,14 +36460,27 @@ send_packet() {
 # Send packet to create FDB row
 send_packet 20
 wait_row_count fdb 1 mac='"00:00:00:00:10:20"'
+timestamp=$(fetch_column fdb timestamp mac='"00:00:00:00:10:20"')
 
 # Set the FDB aging threshold
-check ovn-nbctl set logical_switch ls0 other_config:fdb_age_threshold=1
-AT_CHECK([fetch_column nb:logical_switch other_config | grep -q fdb_age_threshold=1])
+check ovn-nbctl set logical_switch ls0 other_config:fdb_age_threshold=5
+AT_CHECK([fetch_column nb:logical_switch other_config | grep -q fdb_age_threshold=5])
 check ovn-nbctl --wait=sb sync
 
+# Send few packets for "00:00:00:00:10:20" to indicate that it is still in use
+send_packet 20
+sleep 1
+send_packet 20
+
 # Set the timeout for OVS_WAIT* functions to 5 seconds
-OVS_CTL_TIMEOUT=5
+OVS_CTL_TIMEOUT=10
+OVS_WAIT_UNTIL([
+    curr_timestamp=$(fetch_column fdb timestamp mac='"00:00:00:00:10:20"')
+    test "$timestamp" != "$curr_timestamp"
+])
+timestamp=$(fetch_column fdb timestamp mac='"00:00:00:00:10:20"')
+check $(test "$timestamp" != "")
+
 # Check if the records are removed after some inactivity
 wait_row_count fdb 0 mac='"00:00:00:00:10:20"'
 

controller/mac_cache.c
@@ -316,6 +316,56 @@ mac_cache_mb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
 }
 
 void
+mac_cache_fdb_stats_process_flow_stats(struct ovs_list *stats_list,
+                                       struct ofputil_flow_stats *ofp_stats)
+{
+    struct mac_cache_stats *stats = xmalloc(sizeof *stats);
+
+    stats->idle_age_ms = ofp_stats->idle_age * 1000;
+    stats->data.fdb = (struct mac_cache_fdb_data) {
+            .port_key = ofp_stats->match.flow.regs[MFF_LOG_INPORT - MFF_REG0],
+            .dp_key = ntohll(ofp_stats->match.flow.metadata),
+            .mac = ofp_stats->match.flow.dl_src
+    };
+
+    ovs_list_push_back(stats_list, &stats->list_node);
+}
+
+void
+mac_cache_fdb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
+                        void *data)
+{
+    struct mac_cache_data *cache_data = data;
+    struct hmap *thresholds = &cache_data->thresholds[MAC_CACHE_FDB];
+    long long timewall_now = time_wall_msec();
+
+    struct mac_cache_stats *stats;
+    LIST_FOR_EACH_POP (stats, list_node, stats_list) {
+        struct mac_cache_fdb *mc_fdb = mac_cache_fdb_find(cache_data,
+                                                          &stats->data.fdb);
+        if (!mc_fdb) {
+            free(stats);
+            continue;
+        }
+
+        struct mac_cache_threshold *threshold =
+                mac_cache_threshold_find(thresholds, &mc_fdb->dp_uuid);
+        /* If "idle_age" is under threshold it means that the mac binding is
+         * used on this chassis. Also make sure that we don't update the
+         * timestamp more than once during the dump period. */
+        if (stats->idle_age_ms < threshold->value &&
+            (timewall_now - mc_fdb->sbrec_fdb->timestamp) >=
+            threshold->dump_period) {
+            sbrec_fdb_set_timestamp(mc_fdb->sbrec_fdb, timewall_now);
+        }
+
+        free(stats);
+    }
+
+    mac_cache_update_req_delay(thresholds, req_delay);
+}
+
+void
 mac_cache_stats_destroy(struct ovs_list *stats_list)
 {
     struct mac_cache_stats *stats;

controller/mac_cache.h
@@ -112,6 +112,13 @@ mac_cache_mb_stats_process_flow_stats(struct ovs_list *stats_list,
                                       struct ofputil_flow_stats *ofp_stats);
 void mac_cache_mb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
                             void *data);
+
+void
+mac_cache_fdb_stats_process_flow_stats(struct ovs_list *stats_list,
+                                       struct ofputil_flow_stats *ofp_stats);
+void mac_cache_fdb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
+                             void *data);
+
 void mac_cache_stats_destroy(struct ovs_list *stats_list);
 
 #endif /* controller/mac_cache.h */

controller/statctrl.c
@@ -38,6 +38,7 @@ VLOG_DEFINE_THIS_MODULE(statctrl);
 
 enum stat_type {
     STATS_MAC_BINDING = 0,
+    STATS_FDB,
     STATS_MAX,
 };
 
@@ -137,6 +138,16 @@ statctrl_init(void)
     STATS_NODE(MAC_BINDING, mac_binding_request, mac_cache_stats_destroy,
                mac_cache_mb_stats_process_flow_stats, mac_cache_mb_stats_run);
 
+    struct ofputil_flow_stats_request fdb_request = {
+            .cookie = htonll(0),
+            .cookie_mask = htonll(0),
+            .out_port = OFPP_ANY,
+            .out_group = OFPG_ANY,
+            .table_id = OFTABLE_LOOKUP_FDB,
+    };
+    STATS_NODE(FDB, fdb_request, mac_cache_stats_destroy,
+               mac_cache_fdb_stats_process_flow_stats,
+               mac_cache_fdb_stats_run);
 
     statctrl_ctx.thread = ovs_thread_create("ovn_statctrl",
                                             statctrl_thread_handler,
@@ -151,7 +162,7 @@ statctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
         return;
     }
 
-    void *node_data[STATS_MAX] = {mac_cache_data};
+    void *node_data[STATS_MAX] = {mac_cache_data, mac_cache_data};
 
     bool schedule_updated = false;
     long long now = time_msec();

commit d3ed60483f2e56e2b08d79d53921f64b1d0bd9ad

mac-cache: Fix expiration of active FDB entry due to skipped update.
ovn-controller doesn't update timestamps until 3/4 of the expiration
threshold have passed, but then it may wait for another 3/4.  And since
3/4 + 3/4 > 1, northd would remove the entry before ovn-controller
wakes up to refresh it next time.

Fix the issue the same way it is fixed for MAC bindings - by using
cooldown period of 1/4 and the dump period of 1/2 of the threshold.

Fixes: 551527a5e68e ("controller: Update FDB timestamp")
Reported-at: https://issues.redhat.com/browse/FDP-1132
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/mac-cache.c
@@ -497,6 +497,34 @@ fdb_stats_process_flow_stats(struct ovs_list *stats_list,
     ovs_list_push_back(stats_list, &stats->list_node);
 }
 
+static void
+fdb_update_log(const char *action,
+               const struct fdb_data *data,
+               bool print_times,
+               const struct mac_cache_threshold *threshold,
+               int64_t idle_age_ms, uint64_t since_updated_ms)
+{
+    if (!VLOG_IS_DBG_ENABLED()) {
+        return;
+    }
+
+    struct ds s = DS_EMPTY_INITIALIZER;
+
+    ds_put_cstr(&s, action);
+    ds_put_format(&s, " FDB entry (datapath-key: %"PRIu32", "
+                      "port-key: %"PRIu32", mac: " ETH_ADDR_FMT,
+                  data->dp_key, data->port_key, ETH_ADDR_ARGS(data->mac));
+    if (print_times) {
+        ds_put_format(&s, "), last update: %"PRIu64"ms ago,"
+                          " idle age: %"PRIi64"ms, threshold: %"PRIu64"ms",
+                      since_updated_ms, idle_age_ms, threshold->value);
+    } else {
+        ds_put_char(&s, ')');
+    }
+    VLOG_DBG("%s.", ds_cstr_ro(&s));
+    ds_destroy(&s);
+}
+
 void
 fdb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
               void *data)
@@ -509,26 +537,43 @@ fdb_stats_run(struct ovs_list *stats_list, uint64_t *req_delay,
         struct fdb *fdb = fdb_find(&cache_data->fdbs, &stats->data.fdb);
 
         if (!fdb) {
+            fdb_update_log("Not found in the cache:", &stats->data.fdb,
+                           false, NULL, 0, 0);
             free(stats);
             continue;
         }
 
+        uint64_t since_updated_ms = timewall_now - fdb->sbrec_fdb->timestamp;
         struct mac_cache_threshold *threshold =
                 mac_cache_threshold_find(cache_data, fdb->data.dp_key);
 
-        /* If "idle_age" is under threshold it means that the mac binding is
-         * used on this chassis. Also make sure that we don't update the
-         * timestamp more than once during the dump period. */
-        if (stats->idle_age_ms < threshold->value &&
-                (timewall_now - fdb->sbrec_fdb->timestamp) >=
-            threshold->dump_period) {
-            sbrec_fdb_set_timestamp(fdb->sbrec_fdb, timewall_now);
+        /* If "idle_age" is under threshold it means that the fdb entry is
+         * used on this chassis. */
+        if (stats->idle_age_ms < threshold->value) {
+            if (since_updated_ms >= threshold->cooldown_period) {
+                fdb_update_log("Updating active", &fdb->data, true,
+                               threshold, stats->idle_age_ms,
+                               since_updated_ms);
+                sbrec_fdb_set_timestamp(fdb->sbrec_fdb, timewall_now);
+            } else {
+                /* Postponing the update to avoid sending database transactions
+                 * too frequently. */
+                fdb_update_log("Not updating active", &fdb->data, true,
+                               threshold, stats->idle_age_ms,
+                               since_updated_ms);
+            }
+        } else {
+            fdb_update_log("Not updating non-active", &fdb->data, true,
+                           threshold, stats->idle_age_ms, since_updated_ms);
         }
 
         free(stats);
     }
 
     mac_cache_update_req_delay(&cache_data->thresholds, req_delay);
+    if (*req_delay) {
+        VLOG_DBG("FDB entry statistics dalay: %"PRIu64, *req_delay);
+    }
 }
 
 /* Packet buffering. */

tests/ovn.at
@@ -37209,6 +37209,7 @@ ovs-vsctl -- add-port br-phys ext0 -- \
     options:rxq_pcap=hv1/ext0-rx.pcap \
     ofport-request=2
 ovs-vsctl set open . external_ids:ovn-bridge-mappings=physnet1:br-phys
+ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
 
 OVN_POPULATE_ARP
 wait_for_ports_up
@@ -37249,6 +37250,88 @@ check test "$timestamp" != ""
 
 # Check if the records are removed after some inactivity
 wait_row_count fdb 0 mac='"00:00:00:00:10:20"'
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([FDB aging - persistence of the active entry])
+AT_SKIP_IF([test $HAVE_SCAPY = no])
+ovn_start
+
+net_add n1
+
+check ovn-nbctl ls-add ls0
+
+check ovn-nbctl lsp-add ls0 ln_port -- \
+      lsp-set-addresses ln_port unknown -- \
+      lsp-set-type ln_port localnet -- \
+      lsp-set-options ln_port network_name=physnet1 -- \
+      set logical_switch_port ln_port options:localnet_learn_fdb=true -- \
+      set logical_switch ls0 other_config:fdb_age_threshold=4
+
+check ovn-nbctl lsp-add ls0 vif1 -- \
+      lsp-set-addresses vif1 "00:00:00:00:10:10 192.168.10.10"
+
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-underlay
+ovn_attach n1 br-underlay 192.168.0.1
+ovs-vsctl add-br br-phys
+ovs-vsctl -- add-port br-int vif1 -- \
+    set interface vif1 external-ids:iface-id=vif1 \
+    options:tx_pcap=hv1/vif1-tx.pcap \
+    options:rxq_pcap=hv1/vif1-rx.pcap \
+    ofport-request=1
+ovs-vsctl -- add-port br-phys ext0 -- \
+    set interface ext0 \
+    options:tx_pcap=hv1/ext0-tx.pcap \
+    options:rxq_pcap=hv1/ext0-rx.pcap \
+    ofport-request=2
+ovs-vsctl set open . external_ids:ovn-bridge-mappings=physnet1:br-phys
+ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
+
+OVN_POPULATE_ARP
+wait_for_ports_up
+check ovn-nbctl --wait=hv sync
+
+send_packet() {
+    packet=$(fmt_pkt "
+            Ether(dst='00:00:00:00:10:10', src='00:00:00:00:10:${1}') /
+            IP(src='192.168.10.${1}', dst='192.168.10.10') /
+            UDP(sport=1234, dport=1235)
+           ")
+    check ovs-appctl netdev-dummy/receive ext0 $packet
+}
+
+# Send packets to create FDB entries.  Wait between two, so they have an
+# initial difference in timestamps around half the age threshold.
+send_packet 20
+wait_row_count fdb 1 mac='"00:00:00:00:10:20"'
+sleep 2
+send_packet 30
+wait_row_count fdb 1 mac='"00:00:00:00:10:30"'
+
+timestamp=$(fetch_column fdb timestamp mac='"00:00:00:00:10:20"')
+
+uuid=$(fetch_column fdb _uuid mac='"00:00:00:00:10:30"')
+for i in $(seq 12); do
+    # Keep one entry alive by sending traffic that uses it.
+    send_packet 30
+    sleep 1
+    # The entry must not expire.
+    check_row_count fdb 1 mac='"00:00:00:00:10:30"'
+    as hv1 ovs-ofctl dump-flows br-int table=OFTABLE_LOOKUP_FDB
+done
+# Check that it's the same entry.
+uuid2=$(fetch_column fdb _uuid mac='"00:00:00:00:10:30"')
+check test "$uuid" = "$uuid"
+
+# The other entry must have expired by now.
+wait_row_count fdb 0 mac='"00:00:00:00:10:20"'
+# Wait for the alive one to expire as well.
+wait_row_count fdb 0 mac='"00:00:00:00:10:30"'
 
 OVN_CLEANUP([hv1])
 AT_CLEANUP