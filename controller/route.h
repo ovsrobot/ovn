e096a959e67238f6521e42469ee011fac8baa910
Felix Huettner <felix.huettner@stackit.cloud>
2025-02-04 15:00:03 +0100

    controller: Prioritize host routes.
    northd allows us to announce host routes instead of connected routes of
    LRs.
    
    For the host routes we also know the LSP that host the address.
    We can then check if this LSP is pointing to a LRP that is also local to
    the current chassis, so if a LR is chained behind an LR and both use
    chassisredirect ports.
    In this case we can announce the route with a more preferable priority
    than otherwise.
    
    This helps in the following case:
    * the backend router is bound on only a single chassis
    * the frontend router is bound to multiple chassis (with multiple LRPs)
    * one of the chassis of the frontend router matches the backend router
    
    In this case it would be preferable if the network fabric sends the
    traffic to the chassis that hosts both the frontend and backend router.
    Other chassis would work as well, but then OVN would redirect the
    traffic from one chassis to the other.
    So this allows us to skip tunneling traffic in one case.
    
    Acked-by: Dumitru Ceara <dceara@redhat.com>
    Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/route.h
+++ controller/route.h
@@ -40,6 +40,13 @@ struct route_ctx_in {
 
 struct route_ctx_out {
     struct hmap *tracked_re_datapaths;
+
+    /* Contains the tracked_ports that in the last run were bound locally. */
+    struct sset *tracked_ports_local;
+
+    /* Contains the tracked_ports that in the last run were not bound local. */
+    struct sset *tracked_ports_remote;
+
     /* Contains struct advertise_datapath_entry */
     struct hmap *announce_routes;
 };
@@ -60,8 +67,14 @@ struct advertise_route_entry {
     struct hmap_node node;
     struct in6_addr addr;
     unsigned int plen;
+    unsigned int priority;
 };
 
+const struct sbrec_port_binding *route_exchange_find_port(
+    struct ovsdb_idl_index *sbrec_port_binding_by_name,
+    const struct sbrec_chassis *chassis,
+    const struct sset *active_tunnels,
+    const struct sbrec_port_binding *pb);
 bool route_exchange_relevant_port(const struct sbrec_port_binding *pb);
 uint32_t advertise_route_hash(const struct in6_addr *dst, unsigned int plen);
 void route_run(struct route_ctx_in *, struct route_ctx_out *);
