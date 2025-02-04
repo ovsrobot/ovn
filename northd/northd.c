15dfeae327d1a9d4a1a534dda9d22b54738d252d
Felix Huettner <felix.huettner@stackit.cloud>
2025-02-04 14:59:41 +0100

    northd: Remove learned routes if lrp is removed.
    Learned routes must be bound to a lrp on which the routes where learned.
    In case the lrp is deleted for whatever reason no ovn-controller would
    clean these routes up, therefor we do this in northd.
    
    Acked-by: Dumitru Ceara <dceara@redhat.com>
    Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/northd.c
+++ northd/northd.c
@@ -1341,6 +1341,12 @@ ovn_port_find(const struct hmap *ports, const char *name)
     return ovn_port_find__(ports, name, false);
 }
 
+static struct ovn_port *
+ovn_port_find_bound(const struct hmap *ports, const char *name)
+{
+    return ovn_port_find__(ports, name, true);
+}
+
 static bool
 lsp_is_clone_to_unknown(const struct nbrec_logical_switch_port *nbsp)
 {
@@ -1353,12 +1359,6 @@ lsp_is_clone_to_unknown(const struct nbrec_logical_switch_port *nbsp)
         }
     }
     return false;
-}
-
-static struct ovn_port *
-ovn_port_find_bound(const struct hmap *ports, const char *name)
-{
-    return ovn_port_find__(ports, name, true);
 }
 
 /* Returns true only if the logical switch port 'up' column is set to true.
