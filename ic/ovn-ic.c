e10819862dee715b4081fbb806b7e96bf1588a1b
Frank Wagner <frank.wagner@dbosoft.eu>
2025-03-01 14:53:14 +0100

    ic: Build on windows - fix ovn-icc route hash.
    On windows the ic_route_hash declaration dosn't work in compilation.
    
    Signed-off-by: Frank Wagner <frank.wagner@dbosoft.eu>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- ic/ovn-ic.c
+++ ic/ovn-ic.c
@@ -1171,7 +1171,7 @@ add_to_routes_ad(struct hmap *routes_ad, const struct in6_addr prefix,
         route_table = "";
     }
 
-    uint hash = ic_route_hash(&prefix, plen, &nexthop, origin, route_table);
+    unsigned int hash = ic_route_hash(&prefix, plen, &nexthop, origin, route_table);
 
     if (!ic_route_find(routes_ad, &prefix, plen, &nexthop, origin, route_table,
                        hash)) {
