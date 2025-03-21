4ea3e4576b964e07f8934b5dc77841fe2f4d45e1
Ales Musil <amusil@redhat.com>
2025-03-21 07:31:23 +0100

    lib: Replace manual x2nrealloc with vector.
    The vector has the same functionality as the x2nrealloc
    for insert. Use vector instead which slightly simplifies
    the final code. There is one exception that wasn't converted
    that's the expr_constant_set.
    
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/lb.h
+++ lib/lb.h
@@ -24,6 +24,7 @@
 #include "ovn-util.h"
 #include "sset.h"
 #include "uuid.h"
+#include "vec.h"
 
 struct uuid;
 
@@ -36,8 +37,7 @@ struct ovn_lb_vip {
     char *port_str;      /* Actual port string representation.  To be used
                           * in ovn-northd.
                           */
-    struct ovn_lb_backend *backends;
-    size_t n_backends;
+    struct vector backends; /* Vector of struct ovn_lb_backend. */
 
     bool empty_backend_rej;
     int address_family;
