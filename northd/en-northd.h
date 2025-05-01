9ce2baa0c8bbedfdf4c092de85f21ea3a8b2bf14
Mark Michelson <mmichels@redhat.com>
2025-05-01 19:05:13 -0400

    northd: Refactor port binding pairing.
    This is similar to a recent change that refactored datapath syncing.
    This works in a very similar way.
    
    * Input nodes create type-agnostic ovn_unpaired_port_bindings. These are
      fed into the en-port-binding-pair node.
    * The en-port-binding-pair node ensures that a southbound Port_Binding
      is created for each unpaired port binding. Any remaining soutbound
      Port_Bindings are deleted.
    * Type-specific nodes then convert the paired port bindings into
      type-specific paired port bindings that can be consumed by other
      nodes.
    
    However, there are some important differences to note between this and
    the datapath sync patch:
    
    * This patch opts for the word "pair" instead of "sync". This is because
      en-port-binding-pair ensures that all southbound Port_Bindings are
      paired with some northbound structure. However, the columns in the
      southobund Port_Binding are not all synced with their northd
      counterpart. Other engine nodes are responsible for fully syncing the
      data.
    * Not all southbound Port_Bindings have a corresponding northbound row.
      Therefore, the engine nodes that create unpaired port bindings pass an
      opaque cookie pointer to the pairing node instead of a database row.
    * Port bindings tend to be identified by name. This means the code has
      to have several safeguards in place to catch scenarios such as a port
      "moving" from one datapath to another, or a port being deleted and
      re-added quickly.
    * Unlike with the datapath syncing code, this required changes to
      northd's incremental processing since northd was already incrementally
      processing some northbound logical switch port changes.
    
    Signed-off-by: Mark Michelson <mmichels@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/en-northd.h
+++ northd/en-northd.h
@@ -19,7 +19,6 @@ bool northd_nb_logical_switch_handler(struct engine_node *, void *data);
 bool northd_nb_logical_router_handler(struct engine_node *, void *data);
 bool northd_sb_port_binding_handler(struct engine_node *, void *data);
 bool northd_lb_data_handler(struct engine_node *, void *data);
-bool northd_sb_fdb_change_handler(struct engine_node *node, void *data);
 void *en_routes_init(struct engine_node *node OVS_UNUSED,
                             struct engine_arg *arg OVS_UNUSED);
 void en_route_policies_cleanup(void *data);
