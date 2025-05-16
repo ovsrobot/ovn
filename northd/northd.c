ovsrobot/ovn

commit d15b12da6fe6840555bf0e527764775eba76156b

northd: Add ACL Sampling.
Introduce a new table called Sample where per-flow IPFIX configuration
can be specified.
Also, reference rows from such table from the ACL table to enable the
configuration of ACL sampling. If enabled, northd will add a sample
action to each ACL related logical flow.

Packets that hit stateful ACLs are sampled in different ways depending
whether they are initiating a new session or are just forwarded on an
existing (already allowed) session.  Two new columns ("sample_new" and
"sample_est") are added to the ACL table to allow for potentially
different sampling rates for the two cases.

Note: If an ACL has both sampling enabled and a label associated to it
then the label value overrides the observation point ID defined in the
sample configuration.  This is a side effect of the implementation
(observation point IDs are stored in conntrack in the same part of the
ct_label where ACL labels are also stored).  The two features
(sampling and ACL labels) serve however similar purposes so it's not
expected that they're both enabled together.

When sampling is enabled on an ACL additional logical flows are created
for that ACL (one for stateless ACLs and 3 for stateful ACLs) in the ACL
action stage of the logical pipeline.  These additional flows match on a
combination of conntrack state values and observation point id values
(either against a logical register or against the stored ct_label state)
in order to determine whether the packets hitting the ACLs must be
sampled or not.  This comes with a slight increase in the number of
logical flows and in the number of OpenFlow rules.  The number of
additional flows _does not_ depend on the original ACL match or action.

New --sample-new and --sample-est optional arguments are added to the
'ovn-nbctl acl-add' command to allow configuring these new types of
sampling for ACLs.

An example workflow of configuring ACL samples is:
  # Create Sampling_App mappings for ACL traffic types:

ovn-nb.xml
@@ -511,6 +511,48 @@
 
   </table>
 
+  <table name="Sample_Collector" title="Sample_Collector">
+    <column name="id">
+      Sample collector unique id used for differentiating collectors that use
+      the same <code>set_id</code> with different <code>probability</code>
+      values.  The supported value range for IDs is <code>1-255</code>.
+    </column>
+    <column name="name">Name of the sample collector.</column>
+    <column name="probability">
+      Sampling probability for this collector.  It must be an integer number
+      between 0 and 65535.  A value of 0 corresponds to no packets being
+      sampled while a value of 65535 corresponds to all packets being sampled.
+    </column>
+    <column name="set_id">
+      The 8-bit integer identifier of the set of of collectors to send
+      packets to. See Flow_Sample_Collector_Set Table in ovs-vswitchd's
+      database schema.
+    </column>
+    <column name="external_ids">
+      See <em>External IDs</em> at the beginning of this document.
+    </column>
+  </table>
+
+  <table name="Sample" title="Sample">
+    <p>
+      This table describes a Sampling configuration. Entries in other tables
+      might be associated with Sample entries to indicate how the sample
+      should be generated.
+
+      For an example, see <ref table="ACL"/>.
+    </p>
+    <column name="collectors">
+      A list of references to <ref table="Sample_Collector"/> records to be
+      used when generating samples (e.g., IPFIX).  A sample can be sent to
+      multiple collectors simultaneously.
+    </column>
+    <column name="metadata">
+      Will be used as Observation Point ID in every sample.  The Observation
+      Domain ID will be generated by ovn-northd and includes the logical
+      datapath key as the least significant 24 bits and the sampling
+      application type (e.g., drop debugging) as the 8 most significant bits.
+    </column>
+  </table>
   <table name="Copp" title="Control plane protection">
     <p>
       This table is used to define control plane protection policies, i.e.,
@@ -2342,6 +2384,12 @@ or
         created only for allowed connections so the label is valid only
         for allow and allow-related actions.
       </p>
+
+      <p>
+        Note: if an ACL has both sampling enabled and a label associated to it
+        then the label value overrides the observation point ID defined in the
+        <code>sample_new</code> or <code>sample_est</code> configuration.
+      </p>
     </column>
     <column name="priority">
       <p>
@@ -2550,6 +2598,33 @@ or
         </p>
       </column>
     </group>
+
+    <column name="sample_new">
+      <p>
+        The entry in the <ref table="Sample"/> table to use for sampling for
+        new sessions matched by this ACL.  In case the ACL is stateless
+        this is used for sampling all traffic matched by the ACL.
+      </p>
+
+      <p>
+        Note: if an ACL has both sampling enabled and a label associated to it
+        then the label value overrides the observation point ID defined in the
+        <code>sample_new</code> configuration.
+      </p>
+    </column>
+
+    <column name="sample_est">
+      <p>
+        The entry in the <ref table="Sample"/> table to use for sampling for
+        established/related sessions matched by this ACL.
+      </p>
+
+      <p>
+        Note: if an ACL has both sampling enabled and a label associated to it
+        then the label value overrides the observation point ID defined in the
+        <code>sample_est</code> configuration.
+      </p>
+    </column>
 
     <group title="Common Columns">
       <column name="options">

commit ab97ce415dafdb16f5f2281f7346666a4984f152

northd: Sample_Collector.set_ids can actually be 32-bit values.
These get matched against OVS Flow_Sample_Collector.ID which is a 32-bit
value.  OVN doesn't store the set_id anywhere (in conntrack) so there's
no reason to artificially limit this.  Also update the documentation of
the NB.Sample_Collector.set_id field.

Reported-at: https://mail.openvswitch.org/pipermail/ovs-discuss/2025-May/053640.html
Reported-by: Trọng Đạt Trần <trantrongdat7900@gmail.com>
Fixes: d15b12da6fe6 ("northd: Add ACL Sampling.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -6813,11 +6813,11 @@ build_acl_sample_action(struct ds *actions, const struct nbrec_acl *acl,
 
     for (size_t i = 0; i < sample->n_collectors; i++) {
         ds_put_format(actions, "sample(probability=%"PRIu16","
-                               "collector_set=%"PRIu8","
+                               "collector_set=%"PRIu32","
                                "obs_domain=%"PRIu32","
                                "obs_point=%"PRIu32");",
                                (uint16_t) sample->collectors[i]->probability,
-                               (uint8_t) sample->collectors[i]->set_id,
+                               (uint32_t) sample->collectors[i]->set_id,
                                domain_id, point_id);
     }
     ds_put_cstr(actions, " next;");
@@ -7081,12 +7081,12 @@ build_acl_sample_generic_new_flows(const struct ovn_datapath *od,
                          (uint8_t) obs_stage);
 
     ds_put_format(actions, "sample(probability=%"PRIu16","
-                           "collector_set=%"PRIu8","
+                           "collector_set=%"PRIu32","
                            "obs_domain=%"PRIu32","
                            "obs_point="REG_OBS_POINT_ID_NEW");"
                            " next;",
                            (uint16_t) coll->probability,
-                           (uint8_t) coll->set_id,
+                           (uint32_t) coll->set_id,
                            sample_domain_id);
 
     ovn_lflow_add(lflows, od, stage, stateful ? 1000 : 900, ds_cstr(match),
@@ -7120,12 +7120,12 @@ build_acl_sample_generic_est_flows(const struct ovn_datapath *od,
                          (uint8_t) obs_stage);
 
     ds_put_format(actions, "sample(probability=%"PRIu16","
-                           "collector_set=%"PRIu8","
+                           "collector_set=%"PRIu32","
                            "obs_domain=%"PRIu32","
                            "obs_point=ct_label.obs_point_id);"
                            " next;",
                            (uint16_t) coll->probability,
-                           (uint8_t) coll->set_id,
+                           (uint32_t) coll->set_id,
                            sample_domain_id);
 
     ovn_lflow_add(lflows, od, stage, 1000, ds_cstr(match),

ovn-nb.xml
@@ -553,7 +553,7 @@
       sampled while a value of 65535 corresponds to all packets being sampled.
     </column>
     <column name="set_id">
-      The 8-bit integer identifier of the set of of collectors to send
+      The 32-bit integer identifier of the set of of collectors to send
       packets to. See Flow_Sample_Collector_Set Table in ovs-vswitchd's
       database schema.
     </column>

tests/system-ovn.at
@@ -13415,7 +13415,7 @@ ADD_VETH(vm1, vm1, br-int, "42.42.42.2/24", "00:00:00:00:00:01", "42.42.42.1")
 ADD_NAMESPACES(vm2)
 ADD_VETH(vm2, vm2, br-int, "42.42.42.3/24", "00:00:00:00:00:02", "42.42.42.1")
 
-collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=100)
+collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=4294967295)
 collector2=$(ovn-nbctl create Sample_Collector id=2 name=c2 probability=65535 set_id=200)
 check_row_count nb:Sample_Collector 2
 
@@ -13481,7 +13481,7 @@ OVS_WAIT_UNTIL([grep -q 'Startup nfcapd.' collector.err])
 dnl Configure the OVS flow sample collector.
 ovs-vsctl --id=@br get Bridge br-int \
     -- --id=@ipfix create IPFIX targets=\"127.0.0.1:4242\" template_interval=1 \
-    -- --id=@cs create Flow_Sample_Collector_Set id=100 bridge=@br ipfix=@ipfix
+    -- --id=@cs create Flow_Sample_Collector_Set id=4294967295 bridge=@br ipfix=@ipfix
 
 check ovn-nbctl --wait=hv sync
 dnl And wait for it to be up and running.
@@ -13612,7 +13612,7 @@ ADD_VETH(vm1, vm1, br-int, "42.42.42.2/24", "00:00:00:00:00:01", "42.42.42.1")
 ADD_NAMESPACES(vm2)
 ADD_VETH(vm2, vm2, br-int, "42.42.42.3/24", "00:00:00:00:00:02", "42.42.42.1")
 
-collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=100)
+collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=4294967295)
 check_row_count nb:Sample_Collector 1
 
 check_uuid ovn-nbctl create Sampling_App type="acl-new" id="42"
@@ -13650,7 +13650,7 @@ OVS_WAIT_UNTIL([grep -q 'Startup nfcapd.' collector.err])
 dnl Configure the OVS flow sample collector.
 ovs-vsctl --id=@br get Bridge br-int \
     -- --id=@ipfix create IPFIX targets=\"127.0.0.1:4242\" template_interval=1 \
-    -- --id=@cs create Flow_Sample_Collector_Set id=100 bridge=@br ipfix=@ipfix
+    -- --id=@cs create Flow_Sample_Collector_Set id=4294967295 bridge=@br ipfix=@ipfix
 
 check ovn-nbctl --wait=hv sync
 dnl And wait for it to be up and running.
@@ -13739,7 +13739,7 @@ ADD_VETH(vm1, vm1, br-int, "42.42.42.2/24", "00:00:00:00:00:01", "42.42.42.1")
 ADD_NAMESPACES(vm2)
 ADD_VETH(vm2, vm2, br-int, "42.42.42.3/24", "00:00:00:00:00:02", "42.42.42.1")
 
-collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=100)
+collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=4294967295)
 check_row_count nb:Sample_Collector 1
 
 check_uuid ovn-nbctl create Sampling_App type="acl-new" id="42"
@@ -13775,7 +13775,7 @@ OVS_WAIT_UNTIL([grep -q 'Startup nfcapd.' collector.err])
 dnl Configure the OVS flow sample collector.
 ovs-vsctl --id=@br get Bridge br-int \
     -- --id=@ipfix create IPFIX targets=\"127.0.0.1:4242\" template_interval=1 \
-    -- --id=@cs create Flow_Sample_Collector_Set id=100 bridge=@br ipfix=@ipfix
+    -- --id=@cs create Flow_Sample_Collector_Set id=4294967295 bridge=@br ipfix=@ipfix
 
 check ovn-nbctl --wait=hv sync
 dnl And wait for it to be up and running.
@@ -13869,7 +13869,7 @@ check ovn-nbctl --wait=sb sync
 ADD_NAMESPACES(vm1)
 ADD_VETH(vm1, vm1, br-int, "42.42.42.2/24", "00:00:00:00:00:01", "42.42.42.1")
 
-collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=100)
+collector1=$(ovn-nbctl create Sample_Collector id=1 name=c1 probability=65535 set_id=4294967295)
 collector2=$(ovn-nbctl create Sample_Collector id=2 name=c2 probability=65535 set_id=200)
 check_row_count nb:Sample_Collector 2
 
@@ -13904,7 +13904,7 @@ OVS_WAIT_UNTIL([grep -q 'Startup nfcapd.' collector.err])
 dnl Configure the OVS flow sample collector.
 ovs-vsctl --id=@br get Bridge br-int \
     -- --id=@ipfix create IPFIX targets=\"127.0.0.1:4242\" template_interval=1 \
-    -- --id=@cs create Flow_Sample_Collector_Set id=100 bridge=@br ipfix=@ipfix
+    -- --id=@cs create Flow_Sample_Collector_Set id=4294967295 bridge=@br ipfix=@ipfix
 
 check ovn-nbctl --wait=hv sync
 dnl And wait for it to be up and running.