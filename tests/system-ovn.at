ovsrobot/ovn

commit 712fca55b3b12f2b6d1efb05655ee1bf97ba8578

controller: Prioritize host routes.
northd allows us to announce host routes instead of connected routes of
LRs.

For the host routes we also know the LSP that host the address.
We can then check if this LSP is pointing to a LRP that is also local to
the current chassis, so if a LR is chained behind an LR and both use
chassisredirect ports.
In this case we can announce the route with a more preferable priority
than otherwise.

This helps in the following case:
* the backend router is bound on only a single chassis
* the frontend router is bound to multiple chassis (with multiple LRPs)
* one of the chassis of the frontend router matches the backend router

In this case it would be preferable if the network fabric sends the
traffic to the chassis that hosts both the frontend and backend router.
Other chassis would work as well, but then OVN would redirect the
traffic from one chassis to the other.
So this allows us to skip tunneling traffic in one case.

Acked-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/route.c
@@ -33,6 +33,9 @@
 
 VLOG_DEFINE_THIS_MODULE(exchange);
 
+#define PRIORITY_DEFAULT 1000
+#define PRIORITY_LOCAL_BOUND 100
+
 bool
 route_exchange_relevant_port(const struct sbrec_port_binding *pb)
 {
@@ -46,11 +49,11 @@ advertise_route_hash(const struct in6_addr *dst, unsigned int plen)
     return hash_int(plen, hash);
 }
 
-static const struct sbrec_port_binding*
-find_route_exchange_pb(struct ovsdb_idl_index *sbrec_port_binding_by_name,
-                       const struct sbrec_chassis *chassis,
-                       const struct sset *active_tunnels,
-                       const struct sbrec_port_binding *pb)
+const struct sbrec_port_binding*
+route_exchange_find_port(struct ovsdb_idl_index *sbrec_port_binding_by_name,
+                         const struct sbrec_chassis *chassis,
+                         const struct sset *active_tunnels,
+                         const struct sbrec_port_binding *pb)
 {
     if (!pb) {
         return NULL;
@@ -169,10 +172,10 @@ route_run(struct route_ctx_in *r_ctx_in,
             const struct sbrec_port_binding *local_peer
                 = ld->peer_ports[i].local;
             const struct sbrec_port_binding *repb =
-                find_route_exchange_pb(r_ctx_in->sbrec_port_binding_by_name,
-                                       r_ctx_in->chassis,
-                                       r_ctx_in->active_tunnels,
-                                       local_peer);
+                route_exchange_find_port(r_ctx_in->sbrec_port_binding_by_name,
+                                         r_ctx_in->chassis,
+                                         r_ctx_in->active_tunnels,
+                                         local_peer);
             if (!repb) {
                 continue;
             }
@@ -228,9 +231,25 @@ route_run(struct route_ctx_in *r_ctx_in,
             continue;
         }
 
-        struct advertise_route_entry *ar = xmalloc(sizeof *ar);
+        unsigned int priority = PRIORITY_DEFAULT;
+        if (route->tracked_port) {
+            if (lport_is_local(r_ctx_in->sbrec_port_binding_by_name,
+                               r_ctx_in->chassis,
+                               r_ctx_in->active_tunnels,
+                               route->tracked_port->logical_port)) {
+                priority = PRIORITY_LOCAL_BOUND;
+                sset_add(r_ctx_out->tracked_ports_local,
+                         route->tracked_port->logical_port);
+            } else {
+                sset_add(r_ctx_out->tracked_ports_remote,
+                         route->tracked_port->logical_port);
+            }
+        }
+
+        struct advertise_route_entry *ar = xmalloc(sizeof(*ar));
         ar->addr = prefix;
         ar->plen = plen;
+        ar->priority = priority;
         hmap_insert(&ad->routes, &ar->node,
                     advertise_route_hash(&prefix, plen));
     }

tests/system-ovn.at
@@ -16082,8 +16082,8 @@ ovnvrf1337 1337
 # "ip route list" output has a trailing space on each line.
 # The awk magic removes all trailing spaces.
 OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
-blackhole 192.0.2.0/24 proto 84
-blackhole 198.51.100.0/24 proto 84])
+blackhole 192.0.2.0/24 proto 84 metric 1000
+blackhole 198.51.100.0/24 proto 84 metric 1000])
 
 # We now switch to announcing host routes and expect 192.0.2.0/24 to be gone
 # and the following to be added:
@@ -16091,15 +16091,39 @@ blackhole 198.51.100.0/24 proto 84])
 # * 192.0.2.2/32
 # * 192.0.2.3/32
 # * 192.0.2.10/32
+# The last 3 of them are local to the current chassis so we expect a better
+# prio.
 check ovn-nbctl --wait=hv set Logical_Router_Port internet-public \
     options:dynamic-routing-redistribute="connected-as-host,static"
 
 OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
-blackhole 192.0.2.1 proto 84
-blackhole 192.0.2.2 proto 84
-blackhole 192.0.2.3 proto 84
-blackhole 192.0.2.10 proto 84
-blackhole 198.51.100.0/24 proto 84])
+blackhole 192.0.2.1 proto 84 metric 1000
+blackhole 192.0.2.2 proto 84 metric 100
+blackhole 192.0.2.3 proto 84 metric 100
+blackhole 192.0.2.10 proto 84 metric 100
+blackhole 198.51.100.0/24 proto 84 metric 1000])
+
+# If the pr1-public lrp is now removed from this hypervisor the route metric
+# will go back to the default.
+# For this we just schedule it on a non existing chassis.
+check ovn-nbctl lrp-del-gateway-chassis pr1-public hv1
+check ovn-nbctl --wait=hv lrp-set-gateway-chassis pr1-public hv123
+OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
+blackhole 192.0.2.1 proto 84 metric 1000
+blackhole 192.0.2.2 proto 84 metric 1000
+blackhole 192.0.2.3 proto 84 metric 100
+blackhole 192.0.2.10 proto 84 metric 1000
+blackhole 198.51.100.0/24 proto 84 metric 1000])
+
+# Moving pr1-public back will also change the route metrics again.
+check ovn-nbctl lrp-del-gateway-chassis pr1-public hv123
+check ovn-nbctl --wait=hv lrp-set-gateway-chassis pr1-public hv1
+OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
+blackhole 192.0.2.1 proto 84 metric 1000
+blackhole 192.0.2.2 proto 84 metric 100
+blackhole 192.0.2.3 proto 84 metric 100
+blackhole 192.0.2.10 proto 84 metric 100
+blackhole 198.51.100.0/24 proto 84 metric 1000])
 
 # Disable maintaining the vrf as some tests below would cause the vrfs to be
 # removed.
@@ -16340,8 +16364,8 @@ ovnvrf1337 1337
 # "ip route list" output has a trailing space on each line.
 # The awk magic removes all trailing spaces.
 OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
-blackhole 192.0.2.0/24 proto 84
-blackhole 198.51.100.0/24 proto 84])
+blackhole 192.0.2.0/24 proto 84 metric 1000
+blackhole 198.51.100.0/24 proto 84 metric 1000])
 
 # We now switch to announcing host routes and expect 192.0.2.0/24 to be gone
 # and the following to be added:
@@ -16349,15 +16373,39 @@ blackhole 198.51.100.0/24 proto 84])
 # * 192.0.2.2/32
 # * 192.0.2.3/32
 # * 192.0.2.10/32
+# The last 3 of them are local to the current chassis so we expect a better
+# prio.
 check ovn-nbctl --wait=hv set Logical_Router_Port internet-public \
     options:dynamic-routing-redistribute="connected-as-host,static"
 
 OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
-blackhole 192.0.2.1 proto 84
-blackhole 192.0.2.2 proto 84
-blackhole 192.0.2.3 proto 84
-blackhole 192.0.2.10 proto 84
-blackhole 198.51.100.0/24 proto 84])
+blackhole 192.0.2.1 proto 84 metric 100
+blackhole 192.0.2.2 proto 84 metric 100
+blackhole 192.0.2.3 proto 84 metric 100
+blackhole 192.0.2.10 proto 84 metric 100
+blackhole 198.51.100.0/24 proto 84 metric 1000])
+
+# If the pr1-public lrp is now removed from this hypervisor the route metric
+# will go back to the default.
+# For this we just schedule it on a non existing chassis.
+check ovn-nbctl lrp-del-gateway-chassis pr1-public hv1
+check ovn-nbctl --wait=hv lrp-set-gateway-chassis pr1-public hv123
+OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
+blackhole 192.0.2.1 proto 84 metric 100
+blackhole 192.0.2.2 proto 84 metric 1000
+blackhole 192.0.2.3 proto 84 metric 100
+blackhole 192.0.2.10 proto 84 metric 1000
+blackhole 198.51.100.0/24 proto 84 metric 1000])
+
+# Moving pr1-public back will also change the route metrics again.
+check ovn-nbctl lrp-del-gateway-chassis pr1-public hv123
+check ovn-nbctl --wait=hv lrp-set-gateway-chassis pr1-public hv1
+OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
+blackhole 192.0.2.1 proto 84 metric 100
+blackhole 192.0.2.2 proto 84 metric 100
+blackhole 192.0.2.3 proto 84 metric 100
+blackhole 192.0.2.10 proto 84 metric 100
+blackhole 198.51.100.0/24 proto 84 metric 1000])
 
 # Disable maintaining the vrf as some tests below would cause the vrfs to be
 # removed.

controller/lport.c
@@ -63,14 +63,11 @@ lport_lookup_by_key(struct ovsdb_idl_index *sbrec_datapath_binding_by_key,
                                        port_key);
 }
 
-bool
-lport_is_chassis_resident(struct ovsdb_idl_index *sbrec_port_binding_by_name,
-                          const struct sbrec_chassis *chassis,
-                          const struct sset *active_tunnels,
-                          const char *port_name)
+static bool
+lport_pb_is_chassis_resident(const struct sbrec_chassis *chassis,
+                             const struct sset *active_tunnels,
+                             const struct sbrec_port_binding *pb)
 {
-    const struct sbrec_port_binding *pb
-        = lport_lookup_by_name(sbrec_port_binding_by_name, port_name);
     if (!pb || !pb->chassis) {
         return false;
     }
@@ -80,6 +77,39 @@ lport_is_chassis_resident(struct ovsdb_idl_index *sbrec_port_binding_by_name,
         return ha_chassis_group_is_active(pb->ha_chassis_group,
                                           active_tunnels, chassis);
     }
+}
+
+bool
+lport_is_chassis_resident(struct ovsdb_idl_index *sbrec_port_binding_by_name,
+                          const struct sbrec_chassis *chassis,
+                          const struct sset *active_tunnels,
+                          const char *port_name)
+{
+    const struct sbrec_port_binding *pb
+        = lport_lookup_by_name(sbrec_port_binding_by_name, port_name);
+    return lport_pb_is_chassis_resident(chassis, active_tunnels, pb);
+}
+
+bool
+lport_is_local(struct ovsdb_idl_index *sbrec_port_binding_by_name,
+               const struct sbrec_chassis *chassis,
+               const struct sset *active_tunnels,
+               const char *port_name)
+{
+    const struct sbrec_port_binding *pb = lport_lookup_by_name(
+        sbrec_port_binding_by_name, port_name);
+
+    if (lport_pb_is_chassis_resident(chassis, active_tunnels, pb)) {
+        return true;
+    }
+
+    const char *crp = smap_get(&pb->options, "chassis-redirect-port");
+    if (!crp) {
+        return false;
+    }
+
+    return lport_is_chassis_resident(sbrec_port_binding_by_name, chassis,
+                                     active_tunnels, crp);
 }
 
 const struct sbrec_port_binding *

controller/lport.h
@@ -64,6 +64,10 @@ lport_is_chassis_resident(struct ovsdb_idl_index *sbrec_port_binding_by_name,
                           const struct sbrec_chassis *chassis,
                           const struct sset *active_tunnels,
                           const char *port_name);
+bool lport_is_local(struct ovsdb_idl_index *sbrec_port_binding_by_name,
+                    const struct sbrec_chassis *chassis,
+                    const struct sset *active_tunnels,
+                    const char *port_name);
 const struct sbrec_port_binding *lport_get_peer(
     const struct sbrec_port_binding *,
     struct ovsdb_idl_index *sbrec_port_binding_by_name);

controller/ovn-controller.c
@@ -4973,6 +4973,13 @@ struct ed_type_route {
      * route exchange. */
     struct hmap tracked_route_datapaths;
 
+    /* Contains the tracked_ports that in the last run were bound locally. */
+    struct sset tracked_ports_local;
+
+    /* Contains the tracked_ports that in the last run were not bound
+     * locally. */
+    struct sset tracked_ports_remote;
+
     /* Contains struct advertise_datapath_entry */
     struct hmap announce_routes;
 
@@ -5025,11 +5032,16 @@ en_route_run(struct engine_node *node, void *data)
 
     struct route_ctx_out r_ctx_out = {
         .tracked_re_datapaths = &re_data->tracked_route_datapaths,
+        .tracked_ports_local = &re_data->tracked_ports_local,
+        .tracked_ports_remote = &re_data->tracked_ports_remote,
         .announce_routes = &re_data->announce_routes,
     };
 
     route_cleanup(&re_data->announce_routes);
     tracked_datapaths_clear(r_ctx_out.tracked_re_datapaths);
+    sset_clear(r_ctx_out.tracked_ports_local);
+    sset_clear(r_ctx_out.tracked_ports_remote);
+
     route_run(&r_ctx_in, &r_ctx_out);
     engine_set_node_state(node, EN_UPDATED);
 }
@@ -5042,6 +5054,8 @@ en_route_init(struct engine_node *node OVS_UNUSED,
     struct ed_type_route *data = xzalloc(sizeof *data);
 
     hmap_init(&data->tracked_route_datapaths);
+    sset_init(&data->tracked_ports_local);
+    sset_init(&data->tracked_ports_remote);
     hmap_init(&data->announce_routes);
     data->ovnsb_idl = arg->sb_idl;
 
@@ -5054,6 +5068,8 @@ en_route_cleanup(void *data)
     struct ed_type_route *re_data = data;
 
     tracked_datapaths_destroy(&re_data->tracked_route_datapaths);
+    sset_destroy(&re_data->tracked_ports_local);
+    sset_destroy(&re_data->tracked_ports_remote);
     route_cleanup(&re_data->announce_routes);
     hmap_destroy(&re_data->announce_routes);
 }
@@ -5101,6 +5117,26 @@ route_sb_port_binding_data_handler(struct engine_node *node, void *data)
     const struct sbrec_port_binding_table *pb_table =
         EN_OVSDB_GET(engine_get_input("SB_port_binding", node));
 
+    const struct ovsrec_open_vswitch_table *ovs_table =
+        EN_OVSDB_GET(engine_get_input("OVS_open_vswitch", node));
+    const char *chassis_id = get_ovs_chassis_id(ovs_table);
+    ovs_assert(chassis_id);
+
+    struct ovsdb_idl_index *sbrec_chassis_by_name =
+        engine_ovsdb_node_get_index(
+                engine_get_input("SB_chassis", node),
+                "name");
+    const struct sbrec_chassis *chassis
+        = chassis_lookup_by_name(sbrec_chassis_by_name, chassis_id);
+    ovs_assert(chassis);
+
+    struct ovsdb_idl_index *sbrec_port_binding_by_name =
+        engine_ovsdb_node_get_index(
+                engine_get_input("SB_port_binding", node),
+                "name");
+    struct ed_type_runtime_data *rt_data =
+        engine_get_input_data("runtime_data", node);
+
     const struct sbrec_port_binding *sbrec_pb;
     SBREC_PORT_BINDING_TABLE_FOR_EACH_TRACKED (sbrec_pb, pb_table) {
         struct tracked_datapath *re_t_dp =
@@ -5116,6 +5152,26 @@ route_sb_port_binding_data_handler(struct engine_node *node, void *data)
             /* XXX: Until we get I-P support for route exchange we need to
              * request recompute. */
             return false;
+        }
+
+        if (sset_contains(&re_data->tracked_ports_local,
+                          sbrec_pb->logical_port)) {
+            if (!route_exchange_find_port(sbrec_port_binding_by_name, chassis,
+                                          &rt_data->active_tunnels,
+                                          sbrec_pb)) {
+                /* The port was previously local but now it no longer is. */
+                return false;
+            }
+        }
+
+        if (sset_contains(&re_data->tracked_ports_remote,
+                          sbrec_pb->logical_port)) {
+            if (route_exchange_find_port(sbrec_port_binding_by_name, chassis,
+                                         &rt_data->active_tunnels,
+                                         sbrec_pb)) {
+                /* The port was previously remote but now we bound it. */
+                return false;
+            }
         }
 
     }

commit fc555db5c19bcfdd4653e7abd6fc8936534d303a

northd: controller: Support local-only dynamic route advertisements.
In 712fca55b3b1 ("controller: Prioritize host routes.") and later in
cd4ad2f56179 ("northd: Redistribution of NAT/LB routes.") support was
added for advertising routes for objects (logical port / NAT / LB IPs)
that are bound to a single chassis (e.g., distributed NAT) with a better
metric on the chassis where they're bound.  On all other chassis,
however, the route was still advertised but only with a worse metric.

While this works fine in deployments as described in 712fca55b3b1
("controller: Prioritize host routes."), this behavior actually makes
the dynamic routing feature unusable in cases when all inter-node
traffic is forwarded through the L3 fabric, e.g. spine-leaf topologies
with iBGP between leaves and spines and eBGP between OVN compute nodes
and fabric leafs:  that's due to the fact that eBGP routes are usually
preferred over iBGP routes.

Consider the following example:
       +------+        +------+
       |Spine1|        |Spine2|
       +------+        +------+
          |  \         /  |
          |    \     /    |
          |      \ /      |
          |       X       |
          |     /   \     |
          |   /       \   |
          | /           \ |
       +-----+          +----+
       |Leaf1|          |Leaf2|
       +-----+          +----+
          |                |
          |                |
     +----------+     +----------+
     | Chassis1 |     | Chassis2 |
     +----------+     +----------+

An OVN distributed NAT, e.g., 42.42.42.42 "bound" to Chassis1 would be
advertised with a metric of 100 on the eBGP Chassis1 <-> Leaf1
connection and with a metric of 1000 (worse) on the eBGP Chasssi2 <->
Leaf2 connection.  Leaf2 will also learn an iBGP route (through Spine1
and Spine2) for the same prefix (towards Chassis1) but because eBGP
administrative distance is better than the iBGP one, Leaf2 will always
prefer the metric 1000 route.  That means Leaf2 will always forward
traffic destined to 42.42.42.42 via Chassis2 which is sub-optimal.

The main reason for advertising the (NAT) IP on both chassis was likely
to provide redundancy in case traffic hits the OVN cluster on a node
that doesn't host the NAT.  But with topologies as the one depicted
above the redundancy is handled by the fabric.

OVN didn't have a way to disable worse metric route advertisements. This
commit adds one, through a new logical router / logical router port
option, "dynamic-routing-redistribute-local-only" which, if enabled,
informs ovn-controller to not advertise routes for chassis bound IPs
(Sb.Advertised_Route.tracked_port set) on chassis where the tracked port
is not bound.  By default this option is disabled.

Fixes: 712fca55b3b1 ("controller: Prioritize host routes.")

controller/route.c
@@ -253,15 +253,23 @@ route_run(struct route_ctx_in *r_ctx_in,
 
         unsigned int priority = PRIORITY_DEFAULT;
         if (route->tracked_port) {
+            bool redistribute_local_bound_only =
+                smap_get_bool(&route->logical_port->options,
+                              "dynamic-routing-redistribute-local-only",
+                              false);
             if (lport_is_local(r_ctx_in->sbrec_port_binding_by_name,
                                r_ctx_in->chassis,
                                route->tracked_port->logical_port)) {
                 priority = PRIORITY_LOCAL_BOUND;
                 sset_add(r_ctx_out->tracked_ports_local,
                          route->tracked_port->logical_port);
-            } else {
+            } else if (!redistribute_local_bound_only) {
                 sset_add(r_ctx_out->tracked_ports_remote,
                          route->tracked_port->logical_port);
+            } else {
+                /* Here redistribute_local_bound_only is 'true' and
+                 * 'tracked_port' is not local so skip this route. */
+                continue;
             }
         }
 

northd/northd.c
@@ -4387,6 +4387,17 @@ sync_pb_for_lrp(struct ovn_port *op,
         if (portname) {
             smap_add(&new, "dynamic-routing-port-name", portname);
         }
+        const char *redistribute_local_only_name =
+            "dynamic-routing-redistribute-local-only";
+        bool redistribute_local_only_val =
+            smap_get_bool(&op->nbrp->options,
+                          redistribute_local_only_name,
+                          smap_get_bool(&op->od->nbr->options,
+                                        redistribute_local_only_name,
+                                        false));
+        if (redistribute_local_only_val) {
+            smap_add(&new, redistribute_local_only_name, "true");
+        }
     }
 
     const char *ipv6_pd_list = smap_get(&op->sb->options, "ipv6_ra_pd_list");

ovn-nb.xml
@@ -3192,6 +3192,22 @@ or
         </p>
       </column>
 
+      <column name="options"
+          key="dynamic-routing-redistribute-local-only"
+          type='{"type": "boolean"}'>
+        <p>
+          Only relevant if <ref column="options" key="dynamic-routing"/>
+          is set to <code>true</code>.
+        </p>
+
+        <p>
+          This controls whether <code>ovn-controller</code> will advertise
+          <ref table="Advertised_Route" db="OVN_Southbound"/> records
+          only on the chassis where their <code>tracked_port</code> is
+          bound.  Default: <code>false</code>.
+        </p>
+      </column>
+
       <column name="options" key="dynamic-routing-vrf-name"
           type='{"type": "string"}'>
         <p>
@@ -4164,6 +4180,29 @@ or
           Logical_Router will be used.
         </p>
 
+      </column>
+
+      <column name="options"
+          key="dynamic-routing-redistribute-local-only"
+          type='{"type": "boolean"}'>
+        <p>
+          Only relevant if <ref column="options" key="dynamic-routing"/>
+          is set to <code>true</code>.
+        </p>
+
+        <p>
+          This controls whether <code>ovn-controller</code> will advertise
+          <ref table="Advertised_Route" db="OVN_Southbound"/> records
+          only on the chassis where their <code>tracked_port</code> is
+          bound.
+        </p>
+
+        <p>
+          If not set the value from <ref column="options"
+          key="dynamic-routing-redistribute-local-only"
+          table="Logical_Router"/> on the <ref table="Logical_Router"/> will
+          be used.
+        </p>
       </column>
 
       <column name="options" key="dynamic-routing-maintain-vrf"

ovn-sb.xml
@@ -3908,6 +3908,24 @@ tcp.flags = RST;
       </column>
     </group>
 
+    <group title="Dynamic Routing">
+      <column name="options"
+          key="dynamic-routing-redistribute-local-only"
+          type='{"type": "boolean"}'>
+        <p>
+          Only relevant if <ref column="options" key="dynamic-routing"/>
+          is set to <code>true</code>.
+        </p>
+
+        <p>
+          This controls whether <code>ovn-controller</code> will advertise
+          <ref table="Advertised_Route" db="OVN_Southbound"/> records
+          only on the chassis where their <code>tracked_port</code> is
+          bound.  Default: <code>false</code>.
+        </p>
+      </column>
+    </group>
+
     <group title="Nested Containers">
       <p>
         These columns support containers nested within a VM.  Specifically,

tests/system-ovn.at
@@ -16759,6 +16759,29 @@ OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
 blackhole 10.42.10.10 proto ovn metric 1000
 blackhole 172.16.1.150 proto ovn metric 1000])
 
+# Set LR/LRP.options.dynamic-routing-redistribute-local-only=true
+# and verify that lower priority routes are not advertised anymore.
+check ovn-nbctl --wait=hv set logical_router_port r1-join \
+    options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
+blackhole 172.16.1.150 proto ovn metric 1000])
+
+check ovn-nbctl --wait=hv remove logical_router_port r1-join     \
+    options dynamic-routing-redistribute-local-only              \
+    -- set logical_router R1                                     \
+            options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
+blackhole 172.16.1.150 proto ovn metric 1000])
+
+check ovn-nbctl --wait=hv remove logical_router R1  \
+    options dynamic-routing-redistribute-local-only
+
+OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
+blackhole 10.42.10.10 proto ovn metric 1000
+blackhole 172.16.1.150 proto ovn metric 1000])
+
 # Before cleanup of hv1 ovn-controller, trigger a recompute
 # to cleanup the local datapaths. Otherwise, the test will fail.
 # This is because we don't remove a datapath from
@@ -16899,6 +16922,29 @@ check ovn-nbctl                              \
     -- lrp-del-gateway-chassis r2-join hv1   \
     -- lrp-set-gateway-chassis r2-join hv123
 check ovn-nbctl --wait=hv sync
+
+OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
+blackhole 2001:db8:1001::150 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:3001::150 dev lo proto ovn metric 1000 pref medium])
+
+# Set LR/LRP.options.dynamic-routing-redistribute-local-only=true
+# and verify that lower priority routes are not advertised anymore.
+check ovn-nbctl --wait=hv set logical_router_port r1-join \
+    options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
+blackhole 2001:db8:1001::150 dev lo proto ovn metric 1000 pref medium])
+
+check ovn-nbctl --wait=hv remove logical_router_port r1-join     \
+    options dynamic-routing-redistribute-local-only              \
+    -- set logical_router R1                                     \
+            options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
+blackhole 2001:db8:1001::150 dev lo proto ovn metric 1000 pref medium])
+
+check ovn-nbctl --wait=hv remove logical_router R1  \
+    options dynamic-routing-redistribute-local-only
 
 OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
 blackhole 2001:db8:1001::150 dev lo proto ovn metric 1000 pref medium
@@ -17060,6 +17106,35 @@ check ovn-nbctl                             \
   -- ha-chassis-group-add-chassis g1 hv2 20 \
   -- ha-chassis-group-remove-chassis g1 hv1
 check ovn-nbctl --wait=hv sync
+
+OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
+blackhole 10.42.10.10 proto ovn metric 1000
+blackhole 10.42.10.11 proto ovn metric 1000
+blackhole 10.42.10.12 proto ovn metric 1000
+blackhole 10.42.10.13 proto ovn metric 1000
+blackhole 172.16.1.10 proto ovn metric 1000
+blackhole 172.16.1.11 proto ovn metric 1000])
+
+# Set LR/LRP.options.dynamic-routing-redistribute-local-only=true
+# and verify that lower priority routes are not advertised anymore.
+check ovn-nbctl --wait=hv set logical_router_port r1-join \
+    options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
+blackhole 172.16.1.10 proto ovn metric 1000
+blackhole 172.16.1.11 proto ovn metric 1000])
+
+check ovn-nbctl --wait=hv remove logical_router_port r1-join     \
+    options dynamic-routing-redistribute-local-only              \
+    -- set logical_router R1                                     \
+            options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
+blackhole 172.16.1.10 proto ovn metric 1000
+blackhole 172.16.1.11 proto ovn metric 1000])
+
+check ovn-nbctl --wait=hv remove logical_router R1  \
+    options dynamic-routing-redistribute-local-only
 
 OVN_ROUTE_EQUAL([ovnvrf$vrf], [dnl
 blackhole 10.42.10.10 proto ovn metric 1000
@@ -17274,6 +17349,35 @@ check ovn-nbctl                             \
   -- ha-chassis-group-add-chassis g1 hv2 20 \
   -- ha-chassis-group-remove-chassis g1 hv1
 check ovn-nbctl --wait=hv sync
+
+OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
+blackhole 2001:db8:1003::150 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:1003::151 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:1004::150 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:1004::151 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:1004::152 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:1004::153 dev lo proto ovn metric 1000 pref medium])
+
+# Set LR/LRP.options.dynamic-routing-redistribute-local-only=true
+# and verify that lower priority routes are not advertised anymore.
+check ovn-nbctl --wait=hv set logical_router_port r1-join \
+    options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
+blackhole 2001:db8:1003::150 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:1003::151 dev lo proto ovn metric 1000 pref medium])
+
+check ovn-nbctl --wait=hv remove logical_router_port r1-join     \
+    options dynamic-routing-redistribute-local-only              \
+    -- set logical_router R1                                     \
+            options:dynamic-routing-redistribute-local-only=true
+
+OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
+blackhole 2001:db8:1003::150 dev lo proto ovn metric 1000 pref medium
+blackhole 2001:db8:1003::151 dev lo proto ovn metric 1000 pref medium])
+
+check ovn-nbctl --wait=hv remove logical_router R1  \
+    options dynamic-routing-redistribute-local-only
 
 OVN_ROUTE_V6_EQUAL([ovnvrf$vrf], [dnl
 blackhole 2001:db8:1003::150 dev lo proto ovn metric 1000 pref medium