ovsrobot/ovn

commit 40136a2f2c84dca48f7b67c7dc63759e82a44668

northd: Fix direct access to SNAT network.
This change fixes bug that breaks ability of machines from external
networks, to communicate with machines in SNATed networks (specifically
when using a Distributed router).

Currently when a machine (S1) on an external network tries to talk
over TCP with a machine (A1) in a network that has enabled SNAT, the
connection is established successfully. However after the three-way
handshake, any packets that come from the A1 machine will have their
source address translated by the Distributed router, breaking the
communication.

Existing rule in `build_lrouter_out_snat_flow` that decides which
packets should be SNATed already tries to avoid SNATing packets in
reply direction with `(!ct.trk || !ct.rpl)`. However, previous stages
in the distributed LR egress pipeline do not initiate the CT state.

Additionally we need to commit new connections that originate from
external networks into CT, so that the packets in the reply direction
(back to the external network) can be properly identified.

Rationale:

In my original RFC [0], there were questions about the motivation for
fixing this issue. I'll try to summarize why I think this is a bug
that should be fixed.

1. Current implementation for Distributed router already tries to
   avoid SNATing packets in the reply direction, it's just missing
   initialized CT states to make proper decisions.

2. This same scenario works with Gateway Router. I tested with
   following setup:

    foo -- R1 -- join - R3 -- alice
                  |
    bar ----------R2

    R1 is a Distributed router with SNAT for foo. R2 is a Gateway
    router with SNAT for bar. R3 is a Gateway router with no SNAT.
    Using 'alice1' as a client I was able to talk over TCP with
    'bar1' but connection with 'foo1' failed.

3. Regarding security and "leaking" of internal IPs. Reading through
   RFC 4787 [1], 5382 [2] and their update in 7857 [3], the

northd/northd.c
@@ -14426,19 +14426,26 @@ build_lrouter_out_is_dnat_local(struct lflow_table *lflows,
 static void
 build_lrouter_out_snat_match(struct lflow_table *lflows,
                              const struct ovn_datapath *od,
-                             const struct nbrec_nat *nat, struct ds *match,
-                             bool distributed_nat, int cidr_bits, bool is_v6,
+                             const struct nbrec_nat *nat,
+                             struct ds *match,
+                             bool distributed_nat, int cidr_bits,
+                             bool is_v6,
                              struct ovn_port *l3dgw_port,
-                             struct lflow_ref *lflow_ref)
+                             struct lflow_ref *lflow_ref,
+                             bool is_reverse)
 {
     ds_clear(match);
 
-    ds_put_format(match, "ip && ip%c.src == %s", is_v6 ? '6' : '4',
+    ds_put_format(match, "ip && ip%c.%s == %s",
+                  is_v6 ? '6' : '4',
+                  is_reverse ? "dst" : "src",
                   nat->logical_ip);
 
     if (!od->is_gw_router) {
         /* Distributed router. */
-        ds_put_format(match, " && outport == %s", l3dgw_port->json_key);
+        ds_put_format(match, " && %s == %s",
+                      is_reverse ? "inport" : "outport",
+                      l3dgw_port->json_key);
         if (od->n_l3dgw_ports) {
             ds_put_format(match, " && is_chassis_resident(\"%s\")",
                           distributed_nat
@@ -14449,7 +14456,7 @@ build_lrouter_out_snat_match(struct lflow_table *lflows,
 
     if (nat->allowed_ext_ips || nat->exempted_ext_ips) {
         lrouter_nat_add_ext_ip_match(od, lflows, match, nat,
-                                     is_v6, false, cidr_bits,
+                                     is_v6, is_reverse, cidr_bits,
                                      lflow_ref);
     }
 }
@@ -14476,7 +14483,8 @@ build_lrouter_out_snat_stateless_flow(struct lflow_table *lflows,
     uint16_t priority = cidr_bits + 1;
 
     build_lrouter_out_snat_match(lflows, od, nat, match, distributed_nat,
-                                 cidr_bits, is_v6, l3dgw_port, lflow_ref);
+                                 cidr_bits, is_v6, l3dgw_port, lflow_ref,
+                                 false);
 
     if (!od->is_gw_router) {
         /* Distributed router. */
@@ -14523,7 +14531,7 @@ build_lrouter_out_snat_in_czone_flow(struct lflow_table *lflows,
 
     build_lrouter_out_snat_match(lflows, od, nat, match, distributed_nat,
                                  cidr_bits, is_v6, l3dgw_port,
-                                 lflow_ref);
+                                 lflow_ref, false);
 
     if (od->n_l3dgw_ports) {
         priority += 128;
@@ -14572,7 +14580,8 @@ build_lrouter_out_snat_flow(struct lflow_table *lflows,
                             struct ds *actions, bool distributed_nat,
                             struct eth_addr mac, int cidr_bits, bool is_v6,
                             struct ovn_port *l3dgw_port,
-                            struct lflow_ref *lflow_ref)
+                            struct lflow_ref *lflow_ref,
+                            const struct chassis_features *features)
 {
     if (strcmp(nat->type, "snat") && strcmp(nat->type, "dnat_and_snat")) {
         return;
@@ -14586,7 +14595,9 @@ build_lrouter_out_snat_flow(struct lflow_table *lflows,
     uint16_t priority = cidr_bits + 1;
 
     build_lrouter_out_snat_match(lflows, od, nat, match, distributed_nat,
-                                 cidr_bits, is_v6, l3dgw_port, lflow_ref);
+                                 cidr_bits, is_v6, l3dgw_port, lflow_ref,
+                                 false);
+    size_t original_match_len = match->length;
 
     if (!od->is_gw_router) {
         /* Distributed router. */
@@ -14611,6 +14622,35 @@ build_lrouter_out_snat_flow(struct lflow_table *lflows,
                             priority, ds_cstr(match),
                             ds_cstr(actions), &nat->header_,
                             lflow_ref);
+
+    /* For the SNAT networks, we need to make sure that connections are
+     * properly tracked so we can decide whether to perform SNAT on traffic
+     * exiting the network. */
+    if (features->ct_commit_to_zone && !strcmp(nat->type, "snat") &&
+        !od->is_gw_router) {
+        /* For traffic that comes from SNAT network, initiate CT state before
+         * entering S_ROUTER_OUT_SNAT to allow matching on various CT states.
+         */
+        ds_truncate(match, original_match_len);
+        ovn_lflow_add(lflows, od, S_ROUTER_OUT_POST_UNDNAT, 70,
+                      ds_cstr(match), "ct_snat;",
+                      lflow_ref);
+
+        build_lrouter_out_snat_match(lflows, od, nat, match,
+                                     distributed_nat, cidr_bits, is_v6,
+                                     l3dgw_port, lflow_ref, true);
+
+        /* New traffic that goes into SNAT network is committed to CT to avoid
+         * SNAT-ing replies.*/
+        ovn_lflow_add(lflows, od, S_ROUTER_OUT_SNAT, priority,
+                      ds_cstr(match), "ct_snat;",
+                      lflow_ref);
+
+        ds_put_cstr(match, " && ct.new");
+        ovn_lflow_add(lflows, od, S_ROUTER_OUT_POST_SNAT, priority,
+                      ds_cstr(match), "ct_commit_to_zone(snat);",
+                      lflow_ref);
+    }
 }
 
 static void
@@ -15149,7 +15189,7 @@ build_lrouter_nat_defrag_and_lb(
         } else {
             build_lrouter_out_snat_flow(lflows, od, nat, match, actions,
                                         distributed_nat, mac, cidr_bits, is_v6,
-                                        l3dgw_port, lflow_ref);
+                                        l3dgw_port, lflow_ref, features);
         }
 
         /* S_ROUTER_IN_ADMISSION - S_ROUTER_IN_IP_INPUT */

tests/ovn-northd.at
@@ -1107,7 +1107,8 @@ ovn_start
 #
 # DR is connected to S1 and CR is connected to S2
 
-check ovn-sbctl chassis-add gw1 geneve 127.0.0.1
+check ovn-sbctl chassis-add gw1 geneve 127.0.0.1 \
+  -- set chassis gw1 other_config:ct-commit-to-zone="true"
 
 check ovn-nbctl lr-add DR
 check ovn-nbctl lrp-add DR DR-S1 02:ac:10:01:00:01 172.16.1.1/24
@@ -1155,7 +1156,13 @@ AT_CAPTURE_FILE([crflows])
 AT_CHECK([grep -e "lr_out_snat" drflows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=0    , match=(1), action=(next;)
   table=??(lr_out_snat        ), priority=120  , match=(nd_ns), action=(next;)
+  table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ip4.src == $allowed_range), action=(ct_snat;)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 50.0.0.11 && outport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ip4.dst == $allowed_range && (!ct.trk || !ct.rpl)), action=(ct_snat(172.16.1.1);)
+])
+
+AT_CHECK([grep -e "lr_out_post_snat" drflows | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ip4.src == $allowed_range && ct.new), action=(ct_commit_to_zone(snat);)
 ])
 
 AT_CHECK([grep -e "lr_out_snat" crflows | ovn_strip_lflows], [0], [dnl
@@ -1164,6 +1171,9 @@ AT_CHECK([grep -e "lr_out_snat" crflows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=33   , match=(ip && ip4.src == 50.0.0.11 && ip4.dst == $allowed_range && (!ct.trk || !ct.rpl)), action=(ct_snat(172.16.1.1);)
 ])
 
+AT_CHECK([grep -e "lr_out_post_snat" crflows | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
+])
 
 # SNAT with DISALLOWED_IPs
 check ovn-nbctl lr-nat-del DR snat  50.0.0.11
@@ -1185,8 +1195,14 @@ AT_CAPTURE_FILE([crflows2])
 AT_CHECK([grep -e "lr_out_snat" drflows2 | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=0    , match=(1), action=(next;)
   table=??(lr_out_snat        ), priority=120  , match=(nd_ns), action=(next;)
+  table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1")), action=(ct_snat;)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 50.0.0.11 && outport == "DR-S1" && is_chassis_resident("cr-DR-S1") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.16.1.1);)
   table=??(lr_out_snat        ), priority=163  , match=(ip && ip4.src == 50.0.0.11 && outport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ip4.dst == $disallowed_range), action=(next;)
+])
+
+AT_CHECK([grep -e "lr_out_post_snat" drflows2 | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ct.new), action=(ct_commit_to_zone(snat);)
 ])
 
 AT_CHECK([grep -e "lr_out_snat" crflows2 | ovn_strip_lflows], [0], [dnl
@@ -1194,6 +1210,10 @@ AT_CHECK([grep -e "lr_out_snat" crflows2 | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=120  , match=(nd_ns), action=(next;)
   table=??(lr_out_snat        ), priority=33   , match=(ip && ip4.src == 50.0.0.11 && (!ct.trk || !ct.rpl)), action=(ct_snat(172.16.1.1);)
   table=??(lr_out_snat        ), priority=35   , match=(ip && ip4.src == 50.0.0.11 && ip4.dst == $disallowed_range), action=(next;)
+])
+
+AT_CHECK([grep -e "lr_out_post_snat" crflows2 | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
 ])
 
 # Stateful FIP with ALLOWED_IPs
@@ -1217,10 +1237,18 @@ AT_CHECK([grep -e "lr_out_snat" drflows3 | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 50.0.0.11 && outport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ip4.dst == $allowed_range && (!ct.trk || !ct.rpl)), action=(ct_snat(172.16.1.2);)
 ])
 
+AT_CHECK([grep -e "lr_out_post_snat" drflows3 | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
+])
+
 AT_CHECK([grep -e "lr_out_snat" crflows3 | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=0    , match=(1), action=(next;)
   table=??(lr_out_snat        ), priority=120  , match=(nd_ns), action=(next;)
   table=??(lr_out_snat        ), priority=33   , match=(ip && ip4.src == 50.0.0.11 && ip4.dst == $allowed_range && (!ct.trk || !ct.rpl)), action=(ct_snat(172.16.1.2);)
+])
+
+AT_CHECK([grep -e "lr_out_post_snat" crflows3 | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
 ])
 
 # Stateful FIP with DISALLOWED_IPs
@@ -1279,7 +1307,7 @@ AT_CHECK([grep -e "lr_out_snat" crflows5 | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=33   , match=(ip && ip4.src == 50.0.0.11 && ip4.dst == $allowed_range), action=(ip4.src=172.16.1.2; next;)
 ])
 
-# Stateful FIP with DISALLOWED_IPs
+# Stateless FIP with DISALLOWED_IPs
 ovn-nbctl lr-nat-del DR dnat_and_snat  172.16.1.2
 ovn-nbctl lr-nat-del CR dnat_and_snat  172.16.1.2
 
@@ -5521,7 +5549,8 @@ AT_CHECK([grep "lr_out_snat" lr0flows | ovn_strip_lflows], [0], [dnl
 
 check ovn-sbctl chassis-add gw1 geneve 127.0.0.1 \
   -- set chassis gw1 other_config:ct-no-masked-label="true" \
-  -- set chassis gw1 other_config:ovn-ct-lb-related="true"
+  -- set chassis gw1 other_config:ovn-ct-lb-related="true" \
+  -- set chassis gw1 other_config:ct-commit-to-zone="true"
 
 # Create a distributed gw port on lr0
 check ovn-nbctl ls-add public
@@ -5622,14 +5651,24 @@ AT_CHECK([grep "lr_out_undnat" lr0flows | ovn_strip_lflows], [0], [dnl
 
 AT_CHECK([grep "lr_out_post_undnat" lr0flows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_post_undnat ), priority=0    , match=(1), action=(next;)
+  table=??(lr_out_post_undnat ), priority=70   , match=(ip && ip4.src == 10.0.0.0/24 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
+  table=??(lr_out_post_undnat ), priority=70   , match=(ip && ip4.src == 10.0.0.10 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
 ])
 
 AT_CHECK([grep "lr_out_snat" lr0flows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=0    , match=(1), action=(next;)
   table=??(lr_out_snat        ), priority=120  , match=(nd_ns), action=(next;)
+  table=??(lr_out_snat        ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
   table=??(lr_out_snat        ), priority=153  , match=(ip && ip4.src == 10.0.0.0/24 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.168.0.10);)
+  table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 10.0.0.10 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.168.0.30);)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 10.0.0.3 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.168.0.20);)
+])
+
+AT_CHECK([grep "lr_out_post_snat" lr0flows | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
+  table=??(lr_out_post_snat   ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
 ])
 
 # Associate load balancer to lr0
@@ -5770,14 +5809,24 @@ AT_CHECK([grep "lr_out_undnat" lr0flows | ovn_strip_lflows], [0], [dnl
 
 AT_CHECK([grep "lr_out_post_undnat" lr0flows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_post_undnat ), priority=0    , match=(1), action=(next;)
+  table=??(lr_out_post_undnat ), priority=70   , match=(ip && ip4.src == 10.0.0.0/24 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
+  table=??(lr_out_post_undnat ), priority=70   , match=(ip && ip4.src == 10.0.0.10 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
 ])
 
 AT_CHECK([grep "lr_out_snat" lr0flows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_snat        ), priority=0    , match=(1), action=(next;)
   table=??(lr_out_snat        ), priority=120  , match=(nd_ns), action=(next;)
+  table=??(lr_out_snat        ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
   table=??(lr_out_snat        ), priority=153  , match=(ip && ip4.src == 10.0.0.0/24 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.168.0.10);)
+  table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public")), action=(ct_snat;)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 10.0.0.10 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.168.0.30);)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 10.0.0.3 && outport == "lr0-public" && is_chassis_resident("cr-lr0-public") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.168.0.20);)
+])
+
+AT_CHECK([grep "lr_out_post_snat" lr0flows | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
+  table=??(lr_out_post_snat   ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
 ])
 
 # Make the logical router as Gateway router
@@ -7609,9 +7658,14 @@ ovn_start
 # Validate SNAT, DNAT and DNAT_AND_SNAT behavior with multiple
 # distributed gateway LRPs.
 
-check ovn-sbctl chassis-add gw1 geneve 127.0.0.1
-check ovn-sbctl chassis-add gw2 geneve 128.0.0.1
-check ovn-sbctl chassis-add gw3 geneve 129.0.0.1
+check ovn-sbctl chassis-add gw1 geneve 127.0.0.1 \
+  -- set chassis gw1 other_config:ct-commit-to-zone="true"
+
+check ovn-sbctl chassis-add gw2 geneve 128.0.0.1 \
+  -- set chassis gw2 other_config:ct-commit-to-zone="true"
+
+check ovn-sbctl chassis-add gw3 geneve 129.0.0.1 \
+  -- set chassis gw3 other_config:ct-commit-to-zone="true"
 
 check ovn-nbctl lr-add DR
 check ovn-nbctl lrp-add DR DR-S1 02:ac:10:01:00:01 172.16.1.1/24
@@ -7676,9 +7730,19 @@ AT_CHECK([grep lr_in_unsnat lrflows | grep ct_snat | ovn_strip_lflows], [0], [dn
 ])
 
 AT_CHECK([grep lr_out_snat lrflows | grep ct_snat | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1")), action=(ct_snat;)
+  table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S2" && is_chassis_resident("cr-DR-S2")), action=(ct_snat;)
+  table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S3" && is_chassis_resident("cr-DR-S3")), action=(ct_snat;)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 20.0.0.10 && outport == "DR-S1" && is_chassis_resident("cr-DR-S1") && (!ct.trk || !ct.rpl)), action=(ct_snat(172.16.1.10);)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 20.0.0.10 && outport == "DR-S2" && is_chassis_resident("cr-DR-S2") && (!ct.trk || !ct.rpl)), action=(ct_snat(10.0.0.10);)
   table=??(lr_out_snat        ), priority=161  , match=(ip && ip4.src == 20.0.0.10 && outport == "DR-S3" && is_chassis_resident("cr-DR-S3") && (!ct.trk || !ct.rpl)), action=(ct_snat(192.168.0.10);)
+])
+
+AT_CHECK([grep lr_out_post_snat lrflows | ovn_strip_lflows], [0], [dnl
+  table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S2" && is_chassis_resident("cr-DR-S2") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S3" && is_chassis_resident("cr-DR-S3") && ct.new), action=(ct_commit_to_zone(snat);)
 ])
 
 check ovn-nbctl --wait=sb lr-nat-del DR snat 20.0.0.10

commit a6a621c0e9438ee04b4d04a6387fc5af9ecde8d0

northd: Fix unreplied conntrack entries.
This patch fix the behavior introduced by the commit 40136a2,
where a regular communication between an external IP to a
dnat_and_snat rule IP would always keep an unreplied
conntrack entry:

nat d8b589d8-7948-4470-a3ee-a8ae7edb6425
        external ip: "172.16.1.101"
        logical ip: "192.168.10.10"
        type: "dnat_and_snat"

The conntrack entry would be like below:

tcp      6 118 SYN_SENT src=172.16.1.50 dst=192.168.10.10 \
  sport=44742 dport=80 [UNREPLIED] src=192.168.10.10 \
  dst=172.16.1.50 sport=80 dport=44742 mark=0 zone=13 use=1

When we have many connections on the chassis gateways, more
than 50% of the conntrack entries stay in this UNREPLIED state
until the entry is expired according with the
nf_conntrack_tcp_timeout_syn_sent setting.

Running the ab(apache benchmarking tool) test with 3000 requests
to a dnat_and_snat IP, without this patch we would have 2997
SYN_SENT unreplied entries and with this patch applied
we would have 0 entries.

Fixes: 40136a2 ("northd: Fix direct access to SNAT network.")
Signed-off-by: Tiago Pires <tiago.pires@luizalabs.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -16521,7 +16521,7 @@ build_lrouter_out_snat_flow(struct lflow_table *lflows,
                       ds_cstr(match), "ct_snat;",
                       lflow_ref);
 
-        ds_put_cstr(match, " && ct.new");
+        ds_put_cstr(match, " && ct.new && flags.unsnat_not_tracked == 1");
         ovn_lflow_add(lflows, od, S_ROUTER_OUT_POST_SNAT, priority,
                       ds_cstr(match), "ct_commit_to_zone(snat);",
                       lflow_ref);

tests/ovn-northd.at
@@ -1248,7 +1248,7 @@ AT_CHECK([grep -e "lr_out_snat" drflows | ovn_strip_lflows], [0], [dnl
 
 AT_CHECK([grep -e "lr_out_post_snat" drflows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
-  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ip4.src == $allowed_range && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ip4.src == $allowed_range && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
 ])
 
 AT_CHECK([grep -e "lr_out_snat" crflows | ovn_strip_lflows], [0], [dnl
@@ -1288,7 +1288,7 @@ AT_CHECK([grep -e "lr_out_snat" drflows2 | ovn_strip_lflows], [0], [dnl
 
 AT_CHECK([grep -e "lr_out_post_snat" drflows2 | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
-  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 50.0.0.11 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
 ])
 
 AT_CHECK([grep -e "lr_out_snat" crflows2 | ovn_strip_lflows], [0], [dnl
@@ -6013,8 +6013,8 @@ AT_CHECK([grep "lr_out_snat" lr0flows | ovn_strip_lflows], [0], [dnl
 
 AT_CHECK([grep "lr_out_post_snat" lr0flows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
-  table=??(lr_out_post_snat   ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
-  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
 ])
 
 # Associate load balancer to lr0
@@ -6171,8 +6171,8 @@ AT_CHECK([grep "lr_out_snat" lr0flows | ovn_strip_lflows], [0], [dnl
 
 AT_CHECK([grep "lr_out_post_snat" lr0flows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
-  table=??(lr_out_post_snat   ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
-  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=153  , match=(ip && ip4.dst == 10.0.0.0/24 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 10.0.0.10 && inport == "lr0-public" && is_chassis_resident("cr-lr0-public") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
 ])
 
 # Make the logical router as Gateway router
@@ -8399,9 +8399,9 @@ AT_CHECK([grep lr_out_snat lrflows | grep ct_snat | ovn_strip_lflows], [0], [dnl
 
 AT_CHECK([grep lr_out_post_snat lrflows | ovn_strip_lflows], [0], [dnl
   table=??(lr_out_post_snat   ), priority=0    , match=(1), action=(next;)
-  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ct.new), action=(ct_commit_to_zone(snat);)
-  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S2" && is_chassis_resident("cr-DR-S2") && ct.new), action=(ct_commit_to_zone(snat);)
-  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S3" && is_chassis_resident("cr-DR-S3") && ct.new), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S1" && is_chassis_resident("cr-DR-S1") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S2" && is_chassis_resident("cr-DR-S2") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
+  table=??(lr_out_post_snat   ), priority=161  , match=(ip && ip4.dst == 20.0.0.10 && inport == "DR-S3" && is_chassis_resident("cr-DR-S3") && ct.new && flags.unsnat_not_tracked == 1), action=(ct_commit_to_zone(snat);)
 ])
 
 check ovn-nbctl --wait=sb lr-nat-del DR snat 20.0.0.10