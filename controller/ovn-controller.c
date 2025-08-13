ovsrobot/ovn

commit 30587c6e47940bdf2178b61bdc6fe7ccc1aac7c4

controller: Add en_route_exchange_status I-P node.
Introduce en_route_exchange_status I-P node to store Last route_exchange
netlink operation and immediately trigger en_route_exchange if netlink
configuration fails.

Reported-at: https://issues.redhat.com/browse/FDP-1440
Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/ovn-controller.c
@@ -5399,6 +5399,39 @@ en_route_table_notify_cleanup(void *data OVS_UNUSED)
 {
 }
 
+struct ed_type_route_exchange_status {
+    bool netlink_trigger_run;
+};
+
+static void *
+en_route_exchange_status_init(struct engine_node *node OVS_UNUSED,
+                              struct engine_arg *arg OVS_UNUSED)
+{
+    return xzalloc(sizeof(struct ed_type_route_exchange_status));
+}
+
+static enum engine_node_state
+en_route_exchange_status_run(struct engine_node *node OVS_UNUSED, void *data)
+{
+    struct ed_type_route_exchange_status *res = data;
+    enum engine_node_state state;
+
+    if (res->netlink_trigger_run) {
+        state = EN_UPDATED;
+        poll_immediate_wake();
+    } else {
+        state = EN_UNCHANGED;
+    }
+    res->netlink_trigger_run = false;
+
+    return state;
+}
+
+static void
+en_route_exchange_status_cleanup(void *data OVS_UNUSED)
+{
+}
+
 /* Returns false if the northd internal version stored in SB_Global
  * and ovn-controller internal version don't match.
  */
@@ -5706,6 +5739,7 @@ main(int argc, char *argv[])
     ENGINE_NODE(route);
     ENGINE_NODE(route_table_notify);
     ENGINE_NODE(route_exchange);
+    ENGINE_NODE(route_exchange_status);
 
 #define SB_NODE(NAME) ENGINE_NODE_SB(NAME);
     SB_NODES
@@ -5743,6 +5777,7 @@ main(int argc, char *argv[])
     engine_add_input(&en_route_exchange, &en_sb_port_binding,
                      engine_noop_handler);
     engine_add_input(&en_route_exchange, &en_route_table_notify, NULL);
+    engine_add_input(&en_route_exchange, &en_route_exchange_status, NULL);
 
     engine_add_input(&en_addr_sets, &en_sb_address_set,
                      addr_sets_sb_address_set_handler);
@@ -6280,6 +6315,10 @@ main(int argc, char *argv[])
                     struct ed_type_route_table_notify *rtn =
                         engine_get_internal_data(&en_route_table_notify);
                     rtn->changed = route_table_notify_run();
+
+                    struct ed_type_route_exchange_status *res =
+                        engine_get_internal_data(&en_route_exchange_status);
+                    res->netlink_trigger_run = !!route_exchange_status_run();
 
                     stopwatch_start(CONTROLLER_LOOP_STOPWATCH_NAME,
                                     time_msec());

controller/route-exchange.c
@@ -204,6 +204,21 @@ sb_sync_learned_routes(const struct vector *learned_routes,
     hmap_destroy(&sync_routes);
 }
 
+/* Last route_exchange netlink operation. */
+static int route_exchange_nl_status;
+
+#define CLEAR_ROUTE_EXCHANGE_NL_STATUS() \
+    do {                                 \
+        route_exchange_nl_status = 0;    \
+    } while (0)
+
+#define SET_ROUTE_EXCHANGE_NL_STATUS(error)     \
+    do {                                        \
+        if (!route_exchange_nl_status) {        \
+            route_exchange_nl_status = (error); \
+        }                                       \
+    } while (0)
+
 void
 route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
                    struct route_exchange_ctx_out *r_ctx_out)
@@ -215,6 +230,7 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
     hmap_swap(&_maintained_route_tables, &old_maintained_route_table);
     int error;
 
+    CLEAR_ROUTE_EXCHANGE_NL_STATUS();
     const struct advertise_datapath_entry *ad;
     HMAP_FOR_EACH (ad, node, r_ctx_in->announce_routes) {
         uint32_t table_id = ad->db->tunnel_key;
@@ -228,6 +244,7 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
                                  "%"PRIi32": %s.",
                                  ad->vrf_name, table_id,
                                  ovs_strerror(error));
+                    SET_ROUTE_EXCHANGE_NL_STATUS(error);
                     continue;
                 }
             }
@@ -244,8 +261,9 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
         struct vector received_routes =
             VECTOR_EMPTY_INITIALIZER(struct re_nl_received_route_node);
 
-        re_nl_sync_routes(ad->db->tunnel_key, &ad->routes,
-                          &received_routes, ad->db);
+        error = re_nl_sync_routes(ad->db->tunnel_key, &ad->routes,
+                                  &received_routes, ad->db);
+        SET_ROUTE_EXCHANGE_NL_STATUS(error);
 
         sb_sync_learned_routes(&received_routes, ad->db,
                                &ad->bound_ports, r_ctx_in->ovnsb_idl_txn,
@@ -266,6 +284,7 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
             if (error) {
                 /* If netlink transaction fails, we will retry next time. */
                 maintained_route_table_add(mrt->table_id);
+                SET_ROUTE_EXCHANGE_NL_STATUS(error);
             }
         }
         free(mrt);
@@ -280,6 +299,7 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
             if (error) {
                 /* If netlink transaction fails, we will retry next time. */
                 sset_add(&_maintained_vrfs, vrf_name);
+                SET_ROUTE_EXCHANGE_NL_STATUS(error);
             }
         }
         sset_delete(&old_maintained_vrfs, SSET_NODE_FROM_NAME(vrf_name));
@@ -316,4 +336,10 @@ route_exchange_destroy(void)
 
     sset_destroy(&_maintained_vrfs);
     hmap_destroy(&_maintained_route_tables);
+}
+
+int
+route_exchange_status_run(void)
+{
+    return route_exchange_nl_status;
 }

controller/route-exchange-stub.c
@@ -35,3 +35,8 @@ void
 route_exchange_destroy(void)
 {
 }
+
+int route_exchange_status_run(void)
+{
+    return 0;
+}

controller/route-exchange.h
@@ -38,4 +38,6 @@ void route_exchange_run(const struct route_exchange_ctx_in *,
 void route_exchange_cleanup_vrfs(void);
 void route_exchange_destroy(void);
 
+int route_exchange_status_run(void);
+
 #endif /* ROUTE_EXCHANGE_H */

commit b5a51de3262299b754a67227bdc87b99633477c8

controller/route-exchange: Fix route-exchange retry on error mechanism.
As pointed out in the bug report, ovn-controller was not scheduling an
immediate wake when it had detected a NetLink error due to a route
operation it tried to do.  Instead it was doing that too late, in the
en_route_exchange_status I-P node callback.  That one however is called
_before_ en_route_exchange runs.

Instead schedule an immediate wake exactly when we detect the error.  In
the next iteration en_route_exchange_status will propagate the error and
trigger en_route_exchange_status to run.

Fixes: 30587c6e4794 ("controller: Add en_route_exchange_status I-P node.")
Reported-at: https://mail.openvswitch.org/pipermail/ovs-dev/2025-August/425298.html
Reported-by: Xavier Simonart <xsimonar@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/ovn-controller.c
@@ -5441,7 +5441,6 @@ en_route_exchange_status_run(struct engine_node *node OVS_UNUSED, void *data)
 
     if (res->netlink_trigger_run) {
         state = EN_UPDATED;
-        poll_immediate_wake();
     } else {
         state = EN_UNCHANGED;
     }

controller/route-exchange.c
@@ -21,6 +21,7 @@
 #include <net/if.h>
 #include <stdbool.h>
 
+#include "openvswitch/poll-loop.h"
 #include "openvswitch/vlog.h"
 #include "openvswitch/list.h"
 
@@ -221,6 +222,9 @@ static int route_exchange_nl_status;
     do {                                        \
         if (!route_exchange_nl_status) {        \
             route_exchange_nl_status = (error); \
+            if (error) {                        \
+                poll_immediate_wake();          \
+            }                                   \
         }                                       \
     } while (0)
 