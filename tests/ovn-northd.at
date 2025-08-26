ovsrobot/ovn

commit 417f1415b2f51b08d81327afb93ae4f3a61e2087

northd: Clean up SB MAC bindings for deleted ports.
Static_MAC_Binding (SMB) records are only deleted from the
southbound-db via ovn-northd when there is no such SMB found in the
northbound-db. This leads to problems when a port becomes stale (ex.
through lr deletion), because the Logical_Router and Logical_Router_Port
records are removed from the nb-db, but not the Static_MAC_Binding record.

So, when ovn-northd attempts to remove the corresponding
Datapath_Binding and Port_Binding records from sb-db, it fails because
of a referential integrity violation: the Static_MAC_Binding record
contains a reference to Datapath_Binding.

The corrected behavior is that SMB's are now deleted:
1. when there's no such SMB in the nb-db, and
2. when there is no port in the nb-db with the SMB's associated port name.
Thus, all three records, Datapath_Binding, Port_Binding, and
Static_MAC_Binding, are removed within the same transaction.

Also added a unit test to verify correct behavior.

Fixes: b22684d4e37c ("Add a northbound interface to program MAC_Binding table")
Reported-at: https://issues.redhat.com/browse/FDP-723
Signed-off-by: Rosemarie O'Riorden <roriorden@redhat.com>
Acked-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/northd.c
@@ -18409,7 +18409,8 @@ build_static_mac_binding_table(
     struct hmap *lr_ports)
 {
     /* Cleanup SB Static_MAC_Binding entries which do not have corresponding
-     * NB Static_MAC_Binding entries. */
+     * NB Static_MAC_Binding entries, and SB Static_MAC_Binding entries for
+     * which there is not a NB Logical_Router_Port of the same name. */
     const struct nbrec_static_mac_binding *nb_smb;
     const struct sbrec_static_mac_binding *sb_smb;
     SBREC_STATIC_MAC_BINDING_TABLE_FOR_EACH_SAFE (sb_smb,
@@ -18418,6 +18419,12 @@ build_static_mac_binding_table(
                                                sb_smb->logical_port,
                                                sb_smb->ip);
         if (!nb_smb) {
+            sbrec_static_mac_binding_delete(sb_smb);
+            continue;
+        }
+
+        struct ovn_port *op = ovn_port_find(lr_ports, nb_smb->logical_port);
+        if (!op || !op->nbrp || !op->od || !op->od->sb) {
             sbrec_static_mac_binding_delete(sb_smb);
         }
     }

tests/ovn-northd.at
@@ -8085,6 +8085,19 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([LR NB Static_MAC_Binding garbage collection])
+ovn_start
+
+check ovn-nbctl lr-add lr -- lrp-add lr lr-lrp 00:00:00:00:00:01 1.1.1.1/24
+check ovn-nbctl static-mac-binding-add lr-lrp 1.1.1.2 00:00:00:00:00:02
+wait_row_count sb:Static_MAC_Binding 1 logical_port=lr-lrp ip=1.1.1.2 mac="00\:00\:00\:00\:00\:02"
+check ovn-nbctl lr-del lr
+wait_row_count sb:Static_MAC_Binding 0
+
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
 AT_SETUP([LR neighbor lookup and learning flows])
 ovn_start
 

commit 941be7e27ebb71b79542007fedd7a33a5629d00d

northd: Unify the the check for stale MAC Binding.
The SB MAC_Binding and Static_MAC_Binding use the same columns
as an indication of logical port and datapath. However, the check
if any of those rows is stale wasn't consistent. Unify it in a single
function to make sure that both tables have the same condition to
identify stale entries.

Fixes: 417f1415b2f5 ("northd: Clean up SB MAC bindings for deleted ports.")
Fixes: 6856adc616a7 ("ovn-northd: Clear SB records depending on stale datapaths.")
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -2867,6 +2867,26 @@ common:
     }
 }
 
+static bool
+mac_binding_is_stale(const struct hmap *lr_datapaths,
+                     const struct hmap *lr_ports,
+                     const struct sbrec_datapath_binding *dp,
+                     const char *logical_port)
+{
+    const struct ovn_datapath *od =
+        ovn_datapath_from_sbrec(NULL, lr_datapaths, dp);
+    if (!od || ovn_datapath_is_stale(od)) {
+        return true;
+    }
+
+    const struct ovn_port *op = ovn_port_find(lr_ports, logical_port);
+    if (!op || !op->nbrp) {
+        return true;
+    }
+
+    return false;
+}
+
 /* Remove mac_binding entries that refer to logical_ports which are
  * deleted. */
 static void
@@ -2876,11 +2896,8 @@ cleanup_mac_bindings(
 {
     const struct sbrec_mac_binding *b;
     SBREC_MAC_BINDING_TABLE_FOR_EACH_SAFE (b, sbrec_mac_binding_table) {
-        const struct ovn_datapath *od =
-            ovn_datapath_from_sbrec(NULL, lr_datapaths, b->datapath);
-
-        if (!od || ovn_datapath_is_stale(od) ||
-                !ovn_port_find(lr_ports, b->logical_port)) {
+        if (mac_binding_is_stale(lr_datapaths, lr_ports, b->datapath,
+                                 b->logical_port)) {
             sbrec_mac_binding_delete(b);
         }
     }
@@ -18850,7 +18867,7 @@ build_static_mac_binding_table(
     struct ovsdb_idl_txn *ovnsb_txn,
     const struct nbrec_static_mac_binding_table *nbrec_static_mb_table,
     const struct sbrec_static_mac_binding_table *sbrec_static_mb_table,
-    struct hmap *lr_ports)
+    const struct hmap *lr_datapaths, const struct hmap *lr_ports)
 {
     /* Cleanup SB Static_MAC_Binding entries which do not have corresponding
      * NB Static_MAC_Binding entries, and SB Static_MAC_Binding entries for
@@ -18866,8 +18883,8 @@ build_static_mac_binding_table(
             continue;
         }
 
-        struct ovn_port *op = ovn_port_find(lr_ports, nb_smb->logical_port);
-        if (!op || !op->nbrp || !op->od || !op->od->sdp->sb_dp) {
+        if (mac_binding_is_stale(lr_datapaths, lr_ports, sb_smb->datapath,
+                                 sb_smb->logical_port)) {
             sbrec_static_mac_binding_delete(sb_smb);
         }
     }
@@ -19187,7 +19204,7 @@ ovnnb_db_run(struct northd_input *input_data,
     build_static_mac_binding_table(ovnsb_txn,
         input_data->nbrec_static_mac_binding_table,
         input_data->sbrec_static_mac_binding_table,
-        &data->lr_ports);
+        &data->lr_datapaths.datapaths, &data->lr_ports);
     stopwatch_stop(BUILD_LFLOWS_CTX_STOPWATCH_NAME, time_msec());
     stopwatch_start(CLEAR_LFLOWS_CTX_STOPWATCH_NAME, time_msec());
 

tests/ovn-northd.at
@@ -8518,6 +8518,14 @@ wait_row_count sb:Static_MAC_Binding 1 logical_port=lr-lrp ip=1.1.1.2 mac="00\:0
 check ovn-nbctl lr-del lr
 wait_row_count sb:Static_MAC_Binding 0
 
+dnl Also check for router (and router port) being recreated in the same
+dnl transaction.
+check ovn-nbctl lr-add lr -- lrp-add lr lr-lrp 00:00:00:00:00:01 1.1.1.1/24
+wait_row_count sb:Static_MAC_Binding 1 logical_port=lr-lrp ip=1.1.1.2 mac="00\:00\:00\:00\:00\:02"
+check ovn-nbctl lrp-del lr-lrp -- lr-del lr -- lr-add lr -- lrp-add lr lr-lrp 00:00:00:00:00:01 1.1.1.1/24
+wait_row_count sb:Static_MAC_Binding 1
+check ovn-nbctl --wait=sb sync
+
 AT_CLEANUP
 ])
 