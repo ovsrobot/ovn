ovsrobot/ovn

commit 6919992d8781bf5735e15ce7d9269f94c448fafd

Datapath_Binding: Separate type column and sync NB.UUID to SB.
Prior to this commit, if you wanted to find the corresponding northbound
UUID for a southbound Logical Datapath, you could find it in one of two
places:

For logical switches, it was in external-ids:logical-switch.
For logical routers, it was in external-ids:logical-router.

With this commit, ovn-northd will be using the Northbound UUID of the
logical switch or router as UUID when creating the corresponding
Southbound Datapath_Binding. This way, no matter the type of the
datapath, you can find the Northbound record by UUID. And you can
find the type of the datapath without having to potentially check
multiple external-ids keys to do so.

The "type" field is going to be used pretty heavily by northd in upcoming
patches, so instead of making it an external-id, it is now a
full-fledged column on the southbound Datapath_Binding. The type
of the logical datapath is stored in a column called "type".

Acked-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

lib/ovn-util.c
@@ -1503,3 +1503,49 @@ put_load(uint64_t value, enum mf_field_id dst, size_t ofs, size_t n_bits,
     ovs_be64 n_value = htonll(value);
     put_load_bytes(&n_value, 8, dst, ofs, n_bits, ofpacts);
 }
+
+bool
+datapath_get_nb_uuid_and_type(const struct sbrec_datapath_binding *sb,
+                              struct uuid *nb_uuid, const char **type)
+{
+    if (sb->type && *sb->type) {
+        /* New style. The UUID is propagated from the NB logical datapath
+         * and the type is a direct column, so use those. */
+        *nb_uuid = sb->header_.uuid;
+        *type = sb->type;
+        return true;
+    }
+
+    /* Old style. The UUID is stored in external_ids, and the key
+     * corresponds to the datapath type. This only works with
+     * logical switches and logical routers.
+     */
+    *type = "logical-switch";
+    if (smap_get_uuid(&sb->external_ids, *type, nb_uuid)) {
+        return true;
+    }
+    *type = "logical-router";
+    if (smap_get_uuid(&sb->external_ids, *type, nb_uuid)) {
+        return true;
+    }
+    *type = "";
+    *nb_uuid = UUID_ZERO;
+    return false;
+}
+
+bool
+datapath_get_nb_uuid(const struct sbrec_datapath_binding *sb,
+                     struct uuid *nb_uuid)
+{
+    const char *type;
+    return datapath_get_nb_uuid_and_type(sb, nb_uuid, &type);
+}
+
+const char *
+datapath_get_nb_type(const struct sbrec_datapath_binding *sb)
+{
+    const char *type;
+    struct uuid nb_uuid;
+    datapath_get_nb_uuid_and_type(sb, &nb_uuid, &type);
+    return type;
+}

lib/ovn-util.h
@@ -546,4 +546,12 @@ void put_load(uint64_t value, enum mf_field_id dst, size_t ofs, size_t n_bits,
 #define _VFUNC(name, n) _VFUNC_(name, n)
 #define VFUNC(func, ...) _VFUNC(func, __NARG__(__VA_ARGS__)) (__VA_ARGS__)
 
+bool datapath_get_nb_uuid_and_type(const struct sbrec_datapath_binding *sb,
+                                   struct uuid *nb_uuid, const char **type);
+
+bool datapath_get_nb_uuid(const struct sbrec_datapath_binding *sb,
+                          struct uuid *nb_uuid);
+
+const char *datapath_get_nb_type(const struct sbrec_datapath_binding *sb);
+
 #endif /* OVN_UTIL_H */

northd/northd.c
@@ -608,19 +608,21 @@ ovn_datapath_from_sbrec(const struct hmap *ls_datapaths,
                         const struct hmap *lr_datapaths,
                         const struct sbrec_datapath_binding *sb)
 {
-    struct uuid key;
     const struct hmap *dps;
+    struct uuid key;
+    const char *type;
+    if (!datapath_get_nb_uuid_and_type(sb, &key, &type)) {
+        return NULL;
+    }
 
-    if (smap_get_uuid(&sb->external_ids, "logical-switch", &key)) {
+    if (!strcmp(type, "logical-switch")) {
         dps = ls_datapaths;
-    } else if (smap_get_uuid(&sb->external_ids, "logical-router", &key)) {
+    } else if (!strcmp(type, "logical-router")) {
         dps = lr_datapaths;
     } else {
         return NULL;
     }
-    if (!dps) {
-        return NULL;
-    }
+
     struct ovn_datapath *od = ovn_datapath_find_(dps, &key);
     if (od && (od->sb == sb)) {
         return od;
@@ -768,11 +770,9 @@ store_mcast_info_for_switch_datapath(const struct sbrec_ip_multicast *sb,
 static void
 ovn_datapath_update_external_ids(struct ovn_datapath *od)
 {
-    /* Get the logical-switch or logical-router UUID to set in
-     * external-ids. */
+    /* Get the NB  UUID to set in external-ids. */
     char uuid_s[UUID_LEN + 1];
     sprintf(uuid_s, UUID_FMT, UUID_ARGS(&od->key));
-    const char *key = od->nbs ? "logical-switch" : "logical-router";
 
     /* Get names to set in external-ids. */
     const char *name = od->nbs ? od->nbs->name : od->nbr->name;
@@ -784,7 +784,6 @@ ovn_datapath_update_external_ids(struct ovn_datapath *od)
 
     /* Set external-ids. */
     struct smap ids = SMAP_INITIALIZER(&ids);
-    smap_add(&ids, key, uuid_s);
     smap_add(&ids, "name", name);
     if (name2 && name2[0]) {
         smap_add(&ids, "name2", name2);
@@ -912,13 +911,10 @@ join_datapaths(const struct nbrec_logical_switch_table *nbrec_ls_table,
     const struct sbrec_datapath_binding *sb;
     SBREC_DATAPATH_BINDING_TABLE_FOR_EACH_SAFE (sb, sbrec_dp_table) {
         struct uuid key;
-        if (!smap_get_uuid(&sb->external_ids, "logical-switch", &key) &&
-            !smap_get_uuid(&sb->external_ids, "logical-router", &key)) {
+        if (!datapath_get_nb_uuid(sb, &key)) {
             ovsdb_idl_txn_add_comment(
                 ovnsb_txn,
-                "deleting Datapath_Binding "UUID_FMT" that lacks "
-                "external-ids:logical-switch and "
-                "external-ids:logical-router",
+                "deleting Datapath_Binding "UUID_FMT" that lacks nb_uuid",
                 UUID_ARGS(&sb->header_.uuid));
             sbrec_datapath_binding_delete(sb);
             continue;
@@ -928,13 +924,14 @@ join_datapaths(const struct nbrec_logical_switch_table *nbrec_ls_table,
             static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
             VLOG_INFO_RL(
                 &rl, "deleting Datapath_Binding "UUID_FMT" with "
-                "duplicate external-ids:logical-switch/router "UUID_FMT,
-                UUID_ARGS(&sb->header_.uuid), UUID_ARGS(&key));
+                "duplicate nb_uuid "UUID_FMT,
+                UUID_ARGS(&sb->header_.uuid), UUID_ARGS(&sb->header_.uuid));
             sbrec_datapath_binding_delete(sb);
             continue;
         }
 
-        struct ovn_datapath *od = ovn_datapath_create(datapaths, &key,
+        struct ovn_datapath *od = ovn_datapath_create(datapaths,
+                                                      &sb->header_.uuid,
                                                       NULL, NULL, sb);
         ovs_list_push_back(sb_only, &od->list);
     }
@@ -1140,11 +1137,16 @@ build_datapaths(struct ovsdb_idl_txn *ovnsb_txn,
             sbrec_datapath_binding_set_tunnel_key(od->sb, od->tunnel_key);
         }
         ovn_datapath_update_external_ids(od);
+        sbrec_datapath_binding_set_type(od->sb, od->nbs ? "logical-switch" :
+                                        "logical-router");
     }
     LIST_FOR_EACH (od, list, &nb_only) {
-        od->sb = sbrec_datapath_binding_insert(ovnsb_txn);
+        od->sb = sbrec_datapath_binding_insert_persist_uuid(ovnsb_txn,
+                                                            &od->key);
         ovn_datapath_update_external_ids(od);
         sbrec_datapath_binding_set_tunnel_key(od->sb, od->tunnel_key);
+        sbrec_datapath_binding_set_type(od->sb, od->nbs ? "logical-switch" :
+                                        "logical-router");
     }
     ovn_destroy_tnlids(&dp_tnlids);
 

northd/northd.h
@@ -351,7 +351,7 @@ DRR_MODES
 #undef DRR_MODE
 
 /* The 'key' comes from nbs->header_.uuid or nbr->header_.uuid or
- * sb->external_ids:logical-switch. */
+ * sb->header_.uuid. */
 struct ovn_datapath {
     struct hmap_node key_node;  /* Index on 'key'. */
     struct uuid key;            /* (nbs/nbr)->header_.uuid. */
@@ -454,7 +454,8 @@ ovn_datapath_is_stale(const struct ovn_datapath *od)
 /* The two purposes for which ovn-northd uses OVN logical datapaths. */
 enum ovn_datapath_type {
     DP_SWITCH,                  /* OVN logical switch. */
-    DP_ROUTER                   /* OVN logical router. */
+    DP_ROUTER,                  /* OVN logical router. */
+    DP_MAX,
 };
 
 /* Returns an "enum ovn_stage" built from the arguments.

controller/local_data.c
@@ -708,7 +708,7 @@ local_datapath_peer_port_add(struct local_datapath *ld,
 static bool
 datapath_is_switch(const struct sbrec_datapath_binding *ldp)
 {
-    return smap_get(&ldp->external_ids, "logical-switch") != NULL;
+    return strcmp(datapath_get_nb_type(ldp), "logical-switch") == 0;
 }
 
 static bool

ic/ovn-ic.c
@@ -613,8 +613,7 @@ get_router_uuid_by_sb_pb(struct ic_context *ctx,
         return NULL;
     }
 
-    return smap_get_uuid(&router_pb->datapath->external_ids, "logical-router",
-                         router_uuid);
+    return datapath_get_nb_uuid(router_pb->datapath, router_uuid);
 }
 
 static void
@@ -2580,6 +2579,7 @@ main(int argc, char *argv[])
     ovsdb_idl_add_column(ovnsb_idl_loop.idl, &sbrec_encap_col_options);
 
     ovsdb_idl_add_table(ovnsb_idl_loop.idl, &sbrec_table_datapath_binding);
+    ovsdb_idl_add_column(ovnsb_idl_loop.idl, &sbrec_datapath_binding_col_type);
     ovsdb_idl_add_column(ovnsb_idl_loop.idl,
                          &sbrec_datapath_binding_col_external_ids);
 

ovn-sb.ovsschema
@@ -1,7 +1,7 @@
 {
     "name": "OVN_Southbound",
-    "version": "21.2.0",
-    "cksum": "29145795 34859",
+    "version": "21.3.0",
+    "cksum": "3714238857 35128",
     "tables": {
         "SB_Global": {
             "columns": {
@@ -196,6 +196,10 @@
                 "load_balancers": {"type": {"key": {"type": "uuid"},
                                             "min": 0,
                                             "max": "unlimited"}},
+                "type": {"type": {"key": {"type": "string",
+                                          "enum": ["set",
+                                                      ["logical-switch",
+                                                       "logical-router"]]}}},
                 "external_ids": {
                     "type": {"key": "string", "value": "string",
                              "min": 0, "max": "unlimited"}}},

ovn-sb.xml
@@ -3236,18 +3236,14 @@ tcp.flags = RST;
         db="OVN_Northbound"/> database.
       </p>
 
-      <column name="external_ids" key="logical-switch" type='{"type": "uuid"}'>
-        For a logical datapath that represents a logical switch,
-        <code>ovn-northd</code> stores in this key the UUID of the
-        corresponding <ref table="Logical_Switch" db="OVN_Northbound"/> row in
-        the <ref db="OVN_Northbound"/> database.
-      </column>
-
-      <column name="external_ids" key="logical-router" type='{"type": "uuid"}'>
-        For a logical datapath that represents a logical router,
-        <code>ovn-northd</code> stores in this key the UUID of the
-        corresponding <ref table="Logical_Router" db="OVN_Northbound"/> row in
-        the <ref db="OVN_Northbound"/> database.
+      <column name="type" type='{"type": "string"}'>
+        This represents the type of the northbound logical datapath that is
+        represented by this southbound <code>Datapath_Binding</code>. The
+        possible values for this are:
+        <ul>
+          <li><code>logical-switch</code></li>
+          <li><code>logical-router</code></li>
+        </ul>
       </column>
 
       <column name="external_ids" key="interconn-ts" type='{"type": "string"}'>

commit 1165c837fa8c69b22773cea46b8d4f4bc2b4b852

northd, controller: Reconcile SB datapaths to new type in a single run.
Since 6919992d8781 ("Datapath_Binding: Separate type column and sync
NB.UUID to SB."), a new type column is added to SB datapaths to
differentiate between datapath types (switch vs router).  The same
commit also changed northd such that newly created SB datapaths have as
UUID the same value as the NB switch/router UUID.  All SB.Datapath
records were also updated and their 'type' field populated

It also added a helper function, datapath_get_nb_uuid_and_type(), to
abstract out the extraction of a SB datapath's corresponding NB UUID and
type in the following way:
- if the SB.Datapath.type field has a value, the helper assumed that the
  SB.Datapath.UUID is equal to the NB.Logical_Switch/Router.UUID so it
  returned the 'type' value and the SB UUID
- else (for "old style") datapaths it extracted the NB UUID and type
  from the SB.Datapath.external_ids (as used to be done before the
  commit).

This creates an upgrade issue thoughi: older (already existing before
upgrade) SB datapaths are not immediately recreated and their 'type'
is set; so the NB UUID value returned by datapath_get_nb_uuid_and_type()
for these records is incorrect.

That stays like that until datapaths are finally recreated (due to other
events, e.g., full recompute of the datapath-sync nodes).  All that time
the lflow manager will incorrectly determine that the existing lflows
that are stale.  That's because the lflow manager uses
ovn_datapath_from_sbrec() which in turn relied on
datapath_get_nb_uuid_and_type() to determine the in-memory ovn_datapath
corresponding to the logical flow.

There's no need to avoid re-creating SB.Datapath bindings as soon as
ovn-northd determines that they're not using the new scheme.  In order
to achieve that we add two sets of helpers:
a. one to be used by ovn-northd, ovn_datapath_get_nb_uuid_and_type()
   which only parses the new type of datapaths (everything else, i.e.
   old-style datapaths, will be considered stale)
b. one to be used by all other readers of SB datapaths (e.g.,
   ovn-controller, ovn-ic), datapath_get_nb_uuid_and_type_legacy()
   which relies on parsing external IDs (even in the new type of
   datapaths external IDs are still populated with the NB UUID in order
   to maintain backwards compatibility).

This ensures that all old-style datapaths are re-created in the first
iteration of northd that generates a SB transaction.  The logical flows
and datapath groups are also re-created in the same transaction.  This
essentially means that most likely ovn-controller will process these as
a single update generating an atomic bundle of OpenFlow changes thus
also avoiding traffic disruption.

Fixes: 6919992d8781 ("Datapath_Binding: Separate type column and sync NB.UUID to SB.")

lib/ovn-util.c
@@ -1518,22 +1518,15 @@ put_load(uint64_t value, enum mf_field_id dst, size_t ofs, size_t n_bits,
     put_load_bytes(&n_value, 8, dst, ofs, n_bits, ofpacts);
 }
 
+/* Fetch the NB UUID and type of the datapath using the "old style" of
+ * UUIDs being stored in external_ids, and key as datapath type. This only
+ * works with logical switches and logical routers.  And should be used by
+ * all "non-writers" of SB datapath records (e.g., ovn-controller, ovn-ic).
+ */
 bool
-datapath_get_nb_uuid_and_type(const struct sbrec_datapath_binding *sb,
-                              struct uuid *nb_uuid, const char **type)
+datapath_get_nb_uuid_and_type_legacy(const struct sbrec_datapath_binding *sb,
+                                     struct uuid *nb_uuid, const char **type)
 {
-    if (sb->type && *sb->type) {
-        /* New style. The UUID is propagated from the NB logical datapath
-         * and the type is a direct column, so use those. */
-        *nb_uuid = sb->header_.uuid;
-        *type = sb->type;
-        return true;
-    }
-
-    /* Old style. The UUID is stored in external_ids, and the key
-     * corresponds to the datapath type. This only works with
-     * logical switches and logical routers.
-     */
     *type = "logical-switch";
     if (smap_get_uuid(&sb->external_ids, *type, nb_uuid)) {
         return true;
@@ -1552,7 +1545,7 @@ datapath_get_nb_uuid(const struct sbrec_datapath_binding *sb,
                      struct uuid *nb_uuid)
 {
     const char *type;
-    return datapath_get_nb_uuid_and_type(sb, nb_uuid, &type);
+    return datapath_get_nb_uuid_and_type_legacy(sb, nb_uuid, &type);
 }
 
 const char *
@@ -1560,7 +1553,7 @@ datapath_get_nb_type(const struct sbrec_datapath_binding *sb)
 {
     const char *type;
     struct uuid nb_uuid;
-    datapath_get_nb_uuid_and_type(sb, &nb_uuid, &type);
+    datapath_get_nb_uuid_and_type_legacy(sb, &nb_uuid, &type);
     return type;
 }
 

lib/ovn-util.h
@@ -552,8 +552,9 @@ void put_load(uint64_t value, enum mf_field_id dst, size_t ofs, size_t n_bits,
 #define _VFUNC(name, n) _VFUNC_(name, n)
 #define VFUNC(func, ...) _VFUNC(func, __NARG__(__VA_ARGS__)) (__VA_ARGS__)
 
-bool datapath_get_nb_uuid_and_type(const struct sbrec_datapath_binding *sb,
-                                   struct uuid *nb_uuid, const char **type);
+bool datapath_get_nb_uuid_and_type_legacy(
+    const struct sbrec_datapath_binding *,
+    struct uuid *nb_uuid, const char **type);
 
 bool datapath_get_nb_uuid(const struct sbrec_datapath_binding *sb,
                           struct uuid *nb_uuid);

northd/en-datapath-sync.c
@@ -22,6 +22,7 @@
 #include "en-global-config.h"
 #include "datapath-sync.h"
 #include "ovn-sb-idl.h"
+#include "northd.h"
 #include "openvswitch/vlog.h"
 #include "vec.h"
 
@@ -52,7 +53,7 @@ find_unsynced_datapath(const struct ovn_unsynced_datapath_map **maps,
     const char *type;
     struct uuid nb_uuid;
 
-    if (!datapath_get_nb_uuid_and_type(sb_dp, &nb_uuid, &type)) {
+    if (!ovn_datapath_get_nb_uuid_and_type(sb_dp, &nb_uuid, &type)) {
         return NULL;
     }
 

northd/northd.c
@@ -609,6 +609,24 @@ ovn_datapath_find_by_key(struct hmap *datapaths, uint32_t dp_key)
     return NULL;
 }
 
+/* Fetch the NB UUID and type of the datapath using the "new style" of
+ * UUIDs being synced from NB to SB and the type is a direct column.
+ */
+bool
+ovn_datapath_get_nb_uuid_and_type(const struct sbrec_datapath_binding *sb,
+                                  struct uuid *nb_uuid, const char **type)
+{
+    if (sb->type && *sb->type) {
+        *nb_uuid = sb->header_.uuid;
+        *type = sb->type;
+        return true;
+    }
+
+    *type = "";
+    *nb_uuid = UUID_ZERO;
+    return false;
+}
+
 struct ovn_datapath *
 ovn_datapath_from_sbrec(const struct hmap *ls_datapaths,
                         const struct hmap *lr_datapaths,
@@ -617,7 +635,7 @@ ovn_datapath_from_sbrec(const struct hmap *ls_datapaths,
     const struct hmap *dps;
     struct uuid key;
     const char *type;
-    if (!datapath_get_nb_uuid_and_type(sb, &key, &type)) {
+    if (!ovn_datapath_get_nb_uuid_and_type(sb, &key, &type)) {
         return NULL;
     }
 

northd/northd.h
@@ -460,6 +460,10 @@ ovn_datapaths_find_by_index(const struct ovn_datapaths *ovn_datapaths,
     return ovn_datapaths->array[od_index];
 }
 
+bool ovn_datapath_get_nb_uuid_and_type(const struct sbrec_datapath_binding *,
+                                       struct uuid *nb_uuid,
+                                       const char **type);
+
 struct ovn_datapath *ovn_datapath_from_sbrec(
     const struct hmap *ls_datapaths, const struct hmap *lr_datapaths,
     const struct sbrec_datapath_binding *);