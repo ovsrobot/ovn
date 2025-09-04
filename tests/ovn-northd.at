ovsrobot/ovn

commit f2deb24c5c43b370e1d2dce13f5b46edacba3d78

northd: Sync Advertised_Route to sb.
in order to exchange routes between OVN and the network fabric we
use the new Advertised_Route sb table. Northd here advertises all routes
where the user explicitly opted-in.

ovn-controller will later use this table to share these routes to the
outside.

Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-advertised-route-sync.c
@@ -1,0 +1,180 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "stopwatch.h"
+#include "northd.h"
+
+#include "en-advertised-route-sync.h"
+#include "lib/stopwatch-names.h"
+#include "openvswitch/hmap.h"
+#include "ovn-util.h"
+
+static void
+advertised_route_table_sync(
+    struct ovsdb_idl_txn *ovnsb_txn,
+    const struct sbrec_advertised_route_table *sbrec_advertised_route_table,
+    const struct hmap *parsed_routes);
+
+void *
+en_advertised_route_sync_init(struct engine_node *node OVS_UNUSED,
+                              struct engine_arg *arg OVS_UNUSED)
+{
+    return NULL;
+}
+
+void
+en_advertised_route_sync_cleanup(void *data OVS_UNUSED)
+{
+}
+
+void
+en_advertised_route_sync_run(struct engine_node *node, void *data OVS_UNUSED)
+{
+    struct routes_data *routes_data
+        = engine_get_input_data("routes", node);
+    const struct engine_context *eng_ctx = engine_get_context();
+    const struct sbrec_advertised_route_table *sbrec_advertised_route_table =
+        EN_OVSDB_GET(engine_get_input("SB_advertised_route", node));
+
+    stopwatch_start(ADVERTISED_ROUTE_SYNC_RUN_STOPWATCH_NAME, time_msec());
+
+    advertised_route_table_sync(eng_ctx->ovnsb_idl_txn,
+                                sbrec_advertised_route_table,
+                                &routes_data->parsed_routes);
+
+    stopwatch_stop(ADVERTISED_ROUTE_SYNC_RUN_STOPWATCH_NAME, time_msec());
+    engine_set_node_state(node, EN_UPDATED);
+}
+
+struct ar_entry {
+    struct hmap_node hmap_node;
+
+    const struct sbrec_datapath_binding *sb_db;
+
+    const struct sbrec_port_binding *logical_port;
+    char *ip_prefix;
+};
+
+/* Add a new entries to the to-be-advertised routes.
+ * Takes ownership of ip_prefix. */
+static struct ar_entry *
+ar_add_entry(struct hmap *routes, const struct sbrec_datapath_binding *sb_db,
+             const struct sbrec_port_binding *logical_port, char *ip_prefix)
+{
+    struct ar_entry *route_e = xzalloc(sizeof *route_e);
+
+    route_e->sb_db = sb_db;
+    route_e->logical_port = logical_port;
+    route_e->ip_prefix = ip_prefix;
+    uint32_t hash = uuid_hash(&sb_db->header_.uuid);
+    hash = hash_string(logical_port->logical_port, hash);
+    hash = hash_string(ip_prefix, hash);
+    hmap_insert(routes, &route_e->hmap_node, hash);
+
+    return route_e;
+}
+
+static struct ar_entry *
+ar_find(struct hmap *route_map, const struct sbrec_datapath_binding *sb_db,
+        const struct sbrec_port_binding *logical_port, const char *ip_prefix)
+{
+    struct ar_entry *route_e;
+    uint32_t hash;
+
+    hash = uuid_hash(&sb_db->header_.uuid);
+    hash = hash_string(logical_port->logical_port, hash);
+    hash = hash_string(ip_prefix, hash);
+
+    HMAP_FOR_EACH_WITH_HASH (route_e, hmap_node, hash, route_map) {
+        if (!uuid_equals(&sb_db->header_.uuid,
+                         &route_e->sb_db->header_.uuid)) {
+            continue;
+        }
+        if (!uuid_equals(&logical_port->header_.uuid,
+                         &route_e->logical_port->header_.uuid)) {
+            continue;
+        }
+        if (strcmp(ip_prefix, route_e->ip_prefix)) {
+            continue;
+        }
+
+        return route_e;
+    }
+
+    return NULL;
+}
+
+static void
+ar_entry_free(struct ar_entry *route_e)
+{
+    free(route_e->ip_prefix);
+    free(route_e);
+}
+
+static void
+advertised_route_table_sync(
+    struct ovsdb_idl_txn *ovnsb_txn,
+    const struct sbrec_advertised_route_table *sbrec_advertised_route_table,
+    const struct hmap *parsed_routes)
+{
+    struct hmap sync_routes = HMAP_INITIALIZER(&sync_routes);
+    const struct parsed_route *route;
+
+    struct ar_entry *route_e;
+    const struct sbrec_advertised_route *sb_route;
+    HMAP_FOR_EACH (route, key_node, parsed_routes) {
+        if (route->is_discard_route) {
+            continue;
+        }
+        if (prefix_is_link_local(&route->prefix, route->plen)) {
+            continue;
+        }
+        if (!route->od->dynamic_routing) {
+            continue;
+        }
+
+        char *ip_prefix = normalize_v46_prefix(&route->prefix, route->plen);
+        route_e = ar_add_entry(&sync_routes, route->od->sb,
+                               route->out_port->sb, ip_prefix);
+    }
+
+    SBREC_ADVERTISED_ROUTE_TABLE_FOR_EACH_SAFE (sb_route,
+                                                sbrec_advertised_route_table) {
+        route_e = ar_find(&sync_routes, sb_route->datapath,
+                          sb_route->logical_port,
+                          sb_route->ip_prefix);
+        if (route_e) {
+          hmap_remove(&sync_routes, &route_e->hmap_node);
+          ar_entry_free(route_e);
+        } else {
+          sbrec_advertised_route_delete(sb_route);
+        }
+    }
+
+    HMAP_FOR_EACH_POP (route_e, hmap_node, &sync_routes) {
+        const struct sbrec_advertised_route *sr =
+            sbrec_advertised_route_insert(ovnsb_txn);
+        sbrec_advertised_route_set_datapath(sr, route_e->sb_db);
+        sbrec_advertised_route_set_logical_port(sr, route_e->logical_port);
+        sbrec_advertised_route_set_ip_prefix(sr, route_e->ip_prefix);
+        ar_entry_free(route_e);
+    }
+
+    hmap_destroy(&sync_routes);
+}
+

tests/ovn-northd.at
@@ -14917,3 +14917,140 @@ AT_CHECK([ovn-sbctl lflow-list sw | grep ls_out_acl_eval | grep priority=2002 | 
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([dynamic-routing - sync to sb])
+AT_KEYWORDS([dynamic-routing])
+ovn_start
+
+# Adding a router - no route advertised.
+check ovn-nbctl lr-add lr0
+check ovn-nbctl --wait=sb set Logical_Router lr0 option:dynamic-routing=true
+check_row_count Advertised_Route 0
+datapath=$(fetch_column datapath_binding _uuid external_ids:name=lr0)
+
+# Adding a LRP adds a route entry for the associated network.
+check ovn-nbctl --wait=sb lrp-add lr0 lr0-sw0 00:00:00:00:ff:01 10.0.0.1/24
+pb=$(fetch_column port_binding _uuid logical_port=lr0-sw0)
+check_row_count Advertised_Route 1
+check_column 10.0.0.0/24 Advertised_Route ip_prefix datapath=$datapath logical_port=$pb
+
+# Adding a second LRP adds an additional route entry.
+check ovn-nbctl --wait=sb lrp-add lr0 lr0-sw1 00:00:00:00:ff:02 10.0.1.1/24
+pb2=$(fetch_column port_binding _uuid logical_port=lr0-sw1)
+check_row_count Advertised_Route 2
+check_column 10.0.0.0/24 Advertised_Route ip_prefix datapath=$datapath logical_port=$pb
+check_column 10.0.1.0/24 Advertised_Route ip_prefix datapath=$datapath logical_port=$pb2
+
+# Adding a static route adds an additional entry.
+check ovn-nbctl --wait=sb lr-route-add lr0 192.168.0.0/24 10.0.0.10
+check_row_count Advertised_Route 3
+check_row_count Advertised_Route 2 logical_port=$pb
+check_row_count Advertised_Route 1 logical_port=$pb ip_prefix=192.168.0.0/24
+
+# Adding an ipv6 LRP adds an addition route entry.
+check ovn-nbctl --wait=sb lrp-add lr0 lr0-sw2 00:00:00:00:ff:03 2001:db8::1/64 fe80::1/64
+pb3=$(fetch_column port_binding _uuid logical_port=lr0-sw2)
+check_row_count Advertised_Route 4
+check_row_count Advertised_Route 2 logical_port=$pb
+check_row_count Advertised_Route 1 logical_port=$pb ip_prefix=192.168.0.0/24
+check_column 10.0.1.0/24 Advertised_Route ip_prefix datapath=$datapath logical_port=$pb2
+check_column 2001:db8::/64 Advertised_Route ip_prefix datapath=$datapath logical_port=$pb3
+
+# Removing the option:dynamic-routing removes all routes.
+check ovn-nbctl --wait=sb remove Logical_Router lr0 option dynamic-routing
+check_row_count Advertised_Route 0
+
+# And setting it again adds them again.
+check ovn-nbctl --wait=sb set Logical_Router lr0 option:dynamic-routing=true
+check_row_count Advertised_Route 4
+
+# Removing the lrp used for the static route removes both route entries.
+check ovn-nbctl --wait=sb lrp-del lr0-sw0
+check_row_count Advertised_Route 2
+check_row_count Advertised_Route 1 logical_port=$pb2
+check_row_count Advertised_Route 1 logical_port=$pb3
+
+# Removing the lr will remove all routes.
+check ovn-nbctl --wait=sb lr-del lr0
+check_row_count Advertised_Route 0
+
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([dynamic-routing incremental processing])
+AT_KEYWORDS([dynamic-routing])
+ovn_start
+
+# Test I-P for dynamic-routing.
+# Presently ovn-northd has no I-P for Advertised_Route.
+# Wait for sb to be connected before clearing stats.
+check ovn-nbctl --wait=sb sync
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl lr-add lr0
+check ovn-nbctl --wait=sb set Logical_Router lr0 option:dynamic-routing=true
+
+check_engine_stats northd recompute nocompute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb lrp-add lr0 lr0-sw0 00:00:00:00:ff:01 10.0.0.1/24
+check_engine_stats northd recompute compute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb lrp-add lr0 lr0-sw1 00:00:00:00:ff:02 10.0.1.1/24
+check_engine_stats northd recompute compute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb lr-route-add lr0 192.168.0.0/24 10.0.0.10
+check_engine_stats northd recompute nocompute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb lrp-add lr0 lr0-sw2 00:00:00:00:ff:03 2001:db8::1/64 fe80::1/64
+check_engine_stats northd recompute compute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb remove Logical_Router lr0 option dynamic-routing
+check_engine_stats northd recompute nocompute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb set Logical_Router lr0 option:dynamic-routing=true
+check_engine_stats northd recompute nocompute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb lrp-del lr0-sw0
+check_engine_stats northd recompute compute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
+check ovn-nbctl --wait=sb lr-del lr0
+check_engine_stats northd recompute nocompute
+check_engine_stats routes recompute nocompute
+check_engine_stats advertised_route_sync recompute nocompute
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+AT_CLEANUP
+])

ic/ovn-ic.c
@@ -1070,27 +1070,6 @@ get_nexthop_from_lport_addresses(bool is_v4,
 }
 
 static bool
-prefix_is_link_local(struct in6_addr *prefix, unsigned int plen)
-{
-    if (IN6_IS_ADDR_V4MAPPED(prefix)) {
-        /* Link local range is "169.254.0.0/16". */
-        if (plen < 16) {
-            return false;
-        }
-        ovs_be32 lla;
-        inet_pton(AF_INET, "169.254.0.0", &lla);
-        return ((in6_addr_get_mapped_ipv4(prefix) & htonl(0xffff0000)) == lla);
-    }
-
-    /* ipv6, link local range is "fe80::/10". */
-    if (plen < 10) {
-        return false;
-    }
-    return (((prefix->s6_addr[0] & 0xff) == 0xfe) &&
-            ((prefix->s6_addr[1] & 0xc0) == 0x80));
-}
-
-static bool
 prefix_is_deny_listed(const struct smap *nb_options,
                       struct in6_addr *prefix,
                       unsigned int plen)

lib/ovn-util.c
@@ -1353,6 +1353,27 @@ ovn_update_swconn_at(struct rconn *swconn, const char *target,
     return notify;
 }
 
+bool
+prefix_is_link_local(const struct in6_addr *prefix, unsigned int plen)
+{
+    if (IN6_IS_ADDR_V4MAPPED(prefix)) {
+        /* Link local range is "169.254.0.0/16". */
+        if (plen < 16) {
+            return false;
+        }
+        ovs_be32 lla;
+        inet_pton(AF_INET, "169.254.0.0", &lla);
+        return ((in6_addr_get_mapped_ipv4(prefix) & htonl(0xffff0000)) == lla);
+    }
+
+    /* ipv6, link local range is "fe80::/10". */
+    if (plen < 10) {
+        return false;
+    }
+    return (((prefix->s6_addr[0] & 0xff) == 0xfe) &&
+            ((prefix->s6_addr[1] & 0xc0) == 0x80));
+}
+
 const struct sbrec_port_binding *
 lport_lookup_by_name(struct ovsdb_idl_index *sbrec_port_binding_by_name,
                      const char *name)

lib/ovn-util.h
@@ -489,6 +489,8 @@ void ovn_exit_args_finish(struct ovn_exit_args *exit_args);
 bool ovn_update_swconn_at(struct rconn *swconn, const char *target,
                           int probe_interval, const char *where);
 
+bool prefix_is_link_local(const struct in6_addr *prefix, unsigned int plen);
+
 const struct sbrec_port_binding *lport_lookup_by_name(
     struct ovsdb_idl_index *sbrec_port_binding_by_name,
     const char *name);

lib/stopwatch-names.h
@@ -34,5 +34,6 @@
 #define LR_NAT_RUN_STOPWATCH_NAME "lr_nat_run"
 #define LR_STATEFUL_RUN_STOPWATCH_NAME "lr_stateful"
 #define LS_STATEFUL_RUN_STOPWATCH_NAME "ls_stateful"
+#define ADVERTISED_ROUTE_SYNC_RUN_STOPWATCH_NAME "advertised_route_sync"
 
 #endif

northd/automake.mk
@@ -40,6 +40,8 @@ northd_ovn_northd_SOURCES = \
 	northd/en-sampling-app.h \
 	northd/en-acl-ids.c \
 	northd/en-acl-ids.h \
+	northd/en-advertised-route-sync.c \
+	northd/en-advertised-route-sync.h \
 	northd/inc-proc-northd.c \
 	northd/inc-proc-northd.h \
 	northd/ipam.c \

northd/en-advertised-route-sync.h
@@ -1,0 +1,28 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef EN_ADVERTISED_ROUTE_SYNC_H
+#define EN_ADVERTISED_ROUTE_SYNC_H 1
+
+#include "lib/inc-proc-eng.h"
+
+struct advertised_route_sync_data {
+};
+
+void *en_advertised_route_sync_init(struct engine_node *, struct engine_arg *);
+void en_advertised_route_sync_cleanup(void *data);
+void en_advertised_route_sync_run(struct engine_node *, void *data);
+
+#endif /* EN_ADVERTISED_ROUTE_SYNC_H */

commit 6cff4d917b3b29d2af358079823b9f72f496afd0

en-advertised-route-sync: Fix connected-as-host behavior for LLA-only ports.
Commit f2deb24c5c43 ("northd: Sync Advertised_Route to sb.") correctly
skipped over "link-local connected networks" not creating
Advertised_Route records for the link-local subnet.  However, when
implementing the connected-as-host semantics it shouldn't have skipped
over the (non-LLA) IPs configured on routers/switches connected to said
interfaces.

It introduced an inconsistency between the case (a) when a router port is
configured with an explicit IP (and network) and the case (b) when the
router port is only configured with MAC address (just a link-local IPv6
address).  In (a), if the router port is configured to advertise
"connected-as-host" ovn-northd creates Advertised_Route records for all
IPs known to be configured on the peer switch/router.  In (b), however
it does not.

This forces CMS to always configure an explicit IP on the router port
even though that's not necessary for any of the routing decisions.

Fix it by advertising the connected-as-host routes for all connected
routes generated for a router port.

NOTE: we don't try to verify that advertised host routes are actually
reachable (there's a route prefix on the router that matches on them).
This was already the ovn-northd behavior.

Reported-at: https://issues.redhat.com/browse/FDP-1563
Reported-by: Jakub Libosvar <jlibosva@redhat.com>
Fixes: f2deb24c5c43 ("northd: Sync Advertised_Route to sb.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/en-advertised-route-sync.c
@@ -649,36 +649,44 @@ should_advertise_route(const struct uuidset *host_route_lrps,
     }
 }
 
-/* Returns true if the route has already been fully processed for
- * advertising (e.g., for connected routes when connected-as-host is set).
- * Returns false otherwise, in which case the a new 'ar_entry' needs to
- * be created for this route.
- */
+/* Returns true if the connected route was advertised as a set of host routes
+ * (/32 for IPv4 and /128 for IPv6), one for each individual IP known to be
+ * reachable in the connected route's subnet.  Returns false otherwise. */
 static bool
-process_prereqs_advertise_route(
+advertise_routes_as_host_prefix(
     struct advertised_route_sync_data *data,
     struct uuidset *host_route_lrps,
     struct hmap *sync_routes,
     const struct ovn_datapath *advertising_od,
     const struct ovn_port *advertising_op,
-    const struct ovn_port *tracked_op,
-    enum route_source source)
+    enum route_source source
+)
 {
+    if (source != ROUTE_SOURCE_CONNECTED) {
+        return false;
+    }
+
     enum dynamic_routing_redistribute_mode drr =
         advertising_op->dynamic_routing_redistribute;
+    if (!drr_mode_CONNECTED_AS_HOST_is_set(drr)) {
+        return false;
+    }
 
+    uuidset_insert(host_route_lrps, &advertising_op->nbrp->header_.uuid);
+    publish_host_routes(sync_routes, advertising_od, advertising_op, data);
+    return true;
+}
+
+/* Track datapaths (routers/switches) whose changes should trigger
+ * the set of advertised routes.  That includes NAT and LB related
+ * advertised routes. */
+static void
+advertise_route_track_od(struct advertised_route_sync_data *data,
+                         const struct ovn_datapath *advertising_od,
+                         const struct ovn_port *tracked_op,
+                         enum route_source source)
+{
     switch (source) {
-    case ROUTE_SOURCE_CONNECTED:
-        if (drr_mode_CONNECTED_AS_HOST_is_set(drr)) {
-            const struct uuid *lrp_uuid = &advertising_op->nbrp->header_.uuid;
-            uuidset_insert(host_route_lrps, lrp_uuid);
-            publish_host_routes(sync_routes, advertising_od, advertising_op,
-                                data);
-            return true;
-        }
-        break;
-    case ROUTE_SOURCE_STATIC:
-        break;
     case ROUTE_SOURCE_NAT:
         /* If NAT route tracks port on a different DP than the one that
          * advertises the route, we need to watch for changes on that DP as
@@ -702,12 +710,14 @@ process_prereqs_advertise_route(
                            &tracked_op->od->nbr->header_.uuid);
         }
         break;
+    case ROUTE_SOURCE_CONNECTED:
+    case ROUTE_SOURCE_STATIC:
+        break;
     case ROUTE_SOURCE_LEARNED:
         OVS_NOT_REACHED();
     default:
         OVS_NOT_REACHED();
     }
-    return false;
 }
 
 static void
@@ -728,22 +738,24 @@ advertised_route_table_sync(
             continue;
         }
 
-        if (prefix_is_link_local(&route->prefix, route->plen)) {
-            continue;
-        }
-
         if (!should_advertise_route(&host_route_lrps, route->od,
                                     route->out_port, route->source)) {
             continue;
         }
 
-        if (process_prereqs_advertise_route(data, &host_route_lrps,
+        if (advertise_routes_as_host_prefix(data, &host_route_lrps,
                                             &sync_routes, route->od,
                                             route->out_port,
-                                            route->tracked_port,
                                             route->source)) {
             continue;
         }
+
+        if (prefix_is_link_local(&route->prefix, route->plen)) {
+            continue;
+        }
+
+        advertise_route_track_od(data, route->od, route->tracked_port,
+                                 route->source);
 
         ar_entry_add_nocopy(&sync_routes, route->od, route->out_port,
                             normalize_v46_prefix(&route->prefix, route->plen),
@@ -759,12 +771,8 @@ advertised_route_table_sync(
             continue;
         }
 
-        if (process_prereqs_advertise_route(data, &host_route_lrps,
-                                            &sync_routes, route_e->od,
-                                            route_e->op, route_e->tracked_port,
-                                            route_e->source)) {
-            continue;
-        }
+        advertise_route_track_od(data, route_e->od, route_e->tracked_port,
+                                 route_e->source);
 
         const struct sbrec_port_binding *tracked_pb =
             route_e->tracked_port ? route_e->tracked_port->sb : NULL;

tests/ovn-northd.at
@@ -15592,6 +15592,78 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([dynamic-routing - host routes - unnumbered LRP interfaces])
+AT_KEYWORDS([dynamic-routing])
+ovn_start
+
+dnl Logical topology:
+dnl
+dnl                       internal-lr
+dnl                           |
+dnl                           |
+dnl                       internal-ls
+dnl                           |
+dnl                   +-------+----+
+dnl                   |            |
+dnl                   |            +--- ovs-bridge
+dnl                   |            |
+dnl                   |       +----+
+dnl                   |       |
+dnl                   |   public-ls
+dnl                   |       |
+dnl                   |       |
+dnl                   |       | (explicit IP)
+dnl  (no explicit IP) +-- public-lr (BGP enabled)
+dnl     (just LLA)            |
+dnl                           |
+dnl                        <fabric>
+dnl
+dnl When configured to advertise connected-routes "as-host" prefixes
+dnl router ports should also include connected router/switch IPs reachable
+dnl via unnumbered router ports (that only have LLA configured).
+
+check ovn-nbctl --wait=sb                                             \
+  -- lr-add internal-lr                                               \
+     -- lrp-add internal-lr internal-lrp 00:00:00:00:00:01 1.1.1.1/24 \
+  -- ls-add internal-ls                                               \
+     -- lsp-add internal-ls internal-ls-lrp                           \
+     -- lsp-set-type internal-ls-lrp router                           \
+     -- lsp-set-options internal-ls-lrp router-port=internal-lrp      \
+     -- lsp-set-addresses internal-ls-lrp router                      \
+     -- lsp-add internal-ls internal-ls-lla-lrp                       \
+     -- lsp-set-type internal-ls-lla-lrp router                       \
+     -- lsp-set-options internal-ls-lla-lrp router-port=lla-lrp       \
+     -- lsp-set-addresses internal-ls-lla-lrp router                  \
+     -- lsp-add internal-ls internal-ls-localnet                      \
+     -- lsp-set-type internal-ls-localnet localnet                    \
+  -- ls-add public-ls                                                 \
+     -- lsp-add public-ls public-ls-lrp                               \
+     -- lsp-set-type public-ls-lrp router                             \
+     -- lsp-set-options public-ls-lrp router-port=public-lrp          \
+     -- lsp-set-addresses public-ls-lrp router                        \
+     -- lsp-add public-ls public-ls-localnet                          \
+     -- lsp-set-type public-ls-localnet localnet                      \
+  -- lr-add public-lr                                                 \
+     -- lrp-add public-lr public-lrp 00:00:00:00:00:02 1.1.1.2/24     \
+     -- lrp-add public-lr lla-lrp 00:00:00:00:00:03                   \
+     -- lrp-add public-lr fab-lrp 00:00:00:00:00:04 2.2.2.1/24        \
+     -- set Logical_Router public-lr option:dynamic-routing=true      \
+     -- set Logical_Router_Port lla-lrp                               \
+        options:dynamic-routing-redistribute="connected-as-host"
+
+pub_lr_dp=$(fetch_column Datapath_Binding _uuid external_ids:name=public-lr)
+lla_lrp_pb=$(fetch_column Port_Binding _uuid logical_port=lla-lrp)
+int_lrp_pb=$(fetch_column Port_Binding _uuid logical_port=internal-lrp)
+check_column 1.1.1.1 Advertised_Route ip_prefix \
+    datapath=$pub_lr_dp                         \
+    logical_port=$lla_lrp_pb                    \
+    tracked_port=$int_lrp_pb
+check_row_count Advertised_Route 1
+
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
 AT_SETUP([dynamic-routing incremental processing])
 AT_KEYWORDS([dynamic-routing])
 ovn_start