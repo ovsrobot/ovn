ovsrobot/ovn

commit 92ba14e0d34ede0a9d5239c02a68f3ebbc62520c

features: Consolidate chassis features usage, reporting and testing.
Consolidate the feature flags by removing the unused features
"port-up-notif", "ct-no-masked-label" and "ovn-ct-lb-related". At
the same time make sure the debug command reports all the features
that are currently supported. And update the feature testing to
test all features that do not depend on OvS detection.

Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/chassis.c
@@ -408,10 +408,7 @@ chassis_build_other_config(const struct ovs_chassis_cfg *ovs_cfg,
     smap_replace(config, "ovn-evpn-local-ip", ovs_cfg->evpn_local_ip);
     smap_replace(config, "is-interconn",
                  ovs_cfg->is_interconn ? "true" : "false");
-    smap_replace(config, OVN_FEATURE_PORT_UP_NOTIF, "true");
-    smap_replace(config, OVN_FEATURE_CT_NO_MASKED_LABEL, "true");
     smap_replace(config, OVN_FEATURE_MAC_BINDING_TIMESTAMP, "true");
-    smap_replace(config, OVN_FEATURE_CT_LB_RELATED, "true");
     smap_replace(config, OVN_FEATURE_FDB_TIMESTAMP, "true");
     smap_replace(config, OVN_FEATURE_LS_DPG_COLUMN, "true");
     smap_replace(config, OVN_FEATURE_CT_COMMIT_NAT_V2, "true");
@@ -538,26 +535,8 @@ chassis_other_config_changed(const struct ovs_chassis_cfg *ovs_cfg,
         return true;
     }
 
-
-    if (!smap_get_bool(&chassis_rec->other_config, OVN_FEATURE_PORT_UP_NOTIF,
-                       false)) {
-        return true;
-    }
-
-    if (!smap_get_bool(&chassis_rec->other_config,
-                       OVN_FEATURE_CT_NO_MASKED_LABEL,
-                       false)) {
-        return true;
-    }
-
     if (!smap_get_bool(&chassis_rec->other_config,
                        OVN_FEATURE_MAC_BINDING_TIMESTAMP,
-                       false)) {
-        return true;
-    }
-
-    if (!smap_get_bool(&chassis_rec->other_config,
-                       OVN_FEATURE_CT_LB_RELATED,
                        false)) {
         return true;
     }
@@ -763,10 +742,7 @@ update_supported_sset(struct sset *supported)
     /* Internal options. */
     sset_add(supported, "is-vtep");
     sset_add(supported, "is-remote");
-    sset_add(supported, OVN_FEATURE_PORT_UP_NOTIF);
-    sset_add(supported, OVN_FEATURE_CT_NO_MASKED_LABEL);
     sset_add(supported, OVN_FEATURE_MAC_BINDING_TIMESTAMP);
-    sset_add(supported, OVN_FEATURE_CT_LB_RELATED);
     sset_add(supported, OVN_FEATURE_FDB_TIMESTAMP);
     sset_add(supported, OVN_FEATURE_LS_DPG_COLUMN);
     sset_add(supported, OVN_FEATURE_CT_COMMIT_NAT_V2);

include/ovn/features.h
@@ -21,10 +21,7 @@
 #include "smap.h"
 
 /* ovn-controller supported feature names. */
-#define OVN_FEATURE_PORT_UP_NOTIF      "port-up-notif"
-#define OVN_FEATURE_CT_NO_MASKED_LABEL "ct-no-masked-label"
 #define OVN_FEATURE_MAC_BINDING_TIMESTAMP "mac-binding-timestamp"
-#define OVN_FEATURE_CT_LB_RELATED "ovn-ct-lb-related"
 #define OVN_FEATURE_FDB_TIMESTAMP "fdb-timestamp"
 #define OVN_FEATURE_LS_DPG_COLUMN "ls-dpg-column"
 #define OVN_FEATURE_CT_COMMIT_NAT_V2 "ct-commit-nat-v2"

northd/inc-proc-northd.c
@@ -629,6 +629,23 @@ chassis_features_list(struct unixctl_conn *conn, int argc OVS_UNUSED,
 
     ds_put_format(&ds, "mac_binding_timestamp: %s\n",
                   features->mac_binding_timestamp ? "true" : "false");
+    ds_put_format(&ds, "fdb_timestamp: %s\n",
+                  features->fdb_timestamp ? "true" : "false");
+    ds_put_format(&ds, "ls_dpg_column: %s\n",
+                  features->ls_dpg_column ? "true" : "false");
+    ds_put_format(&ds, "ct_commit_nat_v2: %s\n",
+                  features->ct_commit_nat_v2 ? "true" : "false");
+    ds_put_format(&ds, "ct_commit_to_zone: %s\n",
+                  features->ct_commit_to_zone ? "true" : "false");
+    ds_put_format(&ds, "sample_with_reg: %s\n",
+                  features->sample_with_reg ? "true" : "false");
+    ds_put_format(&ds, "ct_next_zone: %s\n",
+                  features->ct_next_zone ? "true" : "false");
+    ds_put_format(&ds, "ct_label_flush: %s\n",
+                  features->ct_label_flush ? "true" : "false");
+    ds_put_format(&ds, "ct_state_save: %s\n",
+                  features->ct_state_save ? "true" : "false");
+
     unixctl_command_reply(conn, ds_cstr(&ds));
     ds_destroy(&ds);
 }

tests/ovn-controller.at
@@ -641,12 +641,25 @@ ovs-vsctl add-br br-phys
 ovn_attach n1 br-phys 192.168.0.1
 
 # Wait for ovn-controller to register in the SB.
-OVS_WAIT_UNTIL([
-    test "$(ovn-sbctl get chassis hv1 other_config:port-up-notif)" = '"true"'
-])
+wait_row_count Chassis 1 name=hv1 other_config:mac-binding-timestamp="true"
+check_row_count Chassis 1 name=hv1 other_config:fdb-timestamp="true"
+check_row_count Chassis 1 name=hv1 other_config:ls-dpg-column="true"
+check_row_count Chassis 1 name=hv1 other_config:ct-commit-nat-v2="true"
+check_row_count Chassis 1 name=hv1 other_config:ct-commit-to-zone="true"
+check_row_count Chassis 1 name=hv1 other_config:fdb-timestamp="true"
+check_row_count Chassis 1 name=hv1 other_config:ct-next-zone="true"
+check_row_count Chassis 1 name=hv1 other_config:ct-state-save="true"
 
-OVS_WAIT_UNTIL([
-    test "$(ovn-sbctl get chassis hv1 other_config:ls-dpg-column)" = '"true"'
+# Check the features reported by northd without features that are detected from ovs.
+AT_CHECK([as northd ovn-appctl -t ovn-northd debug/chassis-features-list | sort | \
+          grep -v "sample_with_reg" | grep -v "ct_label_flush"], [0], [dnl
+ct_commit_nat_v2: true
+ct_commit_to_zone: true
+ct_next_zone: true
+ct_state_save: true
+fdb_timestamp: true
+ls_dpg_column: true
+mac_binding_timestamp: true
 ])
 
 OVN_CLEANUP([hv1])

tests/ovn-northd.at
@@ -10537,7 +10537,7 @@ check ovn-sbctl chassis-add hv1 geneve 127.0.0.1 \
 
 check ovn-nbctl --wait=sb sync
 
-AT_CHECK([as northd ovn-appctl -t ovn-northd debug/chassis-features-list], [0], [dnl
+AT_CHECK([as northd ovn-appctl -t ovn-northd debug/chassis-features-list | grep "mac_binding_timestamp"], [0], [dnl
 mac_binding_timestamp: true
 ])
 
@@ -10548,7 +10548,7 @@ check ovn-sbctl chassis-add hv2 geneve 127.0.0.2 \
 
 check ovn-nbctl --wait=sb sync
 
-AT_CHECK([as northd ovn-appctl -t ovn-northd debug/chassis-features-list], [0], [dnl
+AT_CHECK([as northd ovn-appctl -t ovn-northd debug/chassis-features-list | grep "mac_binding_timestamp"], [0], [dnl
 mac_binding_timestamp: true
 ])
 

commit f53c85e23524b1dc5edc824b534a760a672d483b

features: Add back removed features.
The commit mentioned below removed bunch of features, however in
order for ovn-controller to work with older northd we need to
report them. Those features can be removed once we release the
next LTS. As the upgrade beyond the next LTS is not supported.

Fixes: 92ba14e0d34e ("features: Consolidate chassis features usage, reporting and testing.")
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/chassis.c
@@ -408,7 +408,10 @@ chassis_build_other_config(const struct ovs_chassis_cfg *ovs_cfg,
     smap_replace(config, "ovn-evpn-local-ip", ovs_cfg->evpn_local_ip);
     smap_replace(config, "is-interconn",
                  ovs_cfg->is_interconn ? "true" : "false");
+    smap_replace(config, OVN_FEATURE_PORT_UP_NOTIF, "true");
+    smap_replace(config, OVN_FEATURE_CT_NO_MASKED_LABEL, "true");
     smap_replace(config, OVN_FEATURE_MAC_BINDING_TIMESTAMP, "true");
+    smap_replace(config, OVN_FEATURE_CT_LB_RELATED, "true");
     smap_replace(config, OVN_FEATURE_FDB_TIMESTAMP, "true");
     smap_replace(config, OVN_FEATURE_LS_DPG_COLUMN, "true");
     smap_replace(config, OVN_FEATURE_CT_COMMIT_NAT_V2, "true");
@@ -742,7 +745,10 @@ update_supported_sset(struct sset *supported)
     /* Internal options. */
     sset_add(supported, "is-vtep");
     sset_add(supported, "is-remote");
+    sset_add(supported, OVN_FEATURE_PORT_UP_NOTIF);
+    sset_add(supported, OVN_FEATURE_CT_NO_MASKED_LABEL);
     sset_add(supported, OVN_FEATURE_MAC_BINDING_TIMESTAMP);
+    sset_add(supported, OVN_FEATURE_CT_LB_RELATED);
     sset_add(supported, OVN_FEATURE_FDB_TIMESTAMP);
     sset_add(supported, OVN_FEATURE_LS_DPG_COLUMN);
     sset_add(supported, OVN_FEATURE_CT_COMMIT_NAT_V2);

include/ovn/features.h
@@ -31,6 +31,12 @@
 #define OVN_FEATURE_CT_LABEL_FLUSH "ct-label-flush"
 #define OVN_FEATURE_CT_STATE_SAVE "ct-state-save"
 
+/* DEPRACATED: The following features can be removed
+ * after the next LTS version release. */
+#define OVN_FEATURE_PORT_UP_NOTIF      "port-up-notif"
+#define OVN_FEATURE_CT_NO_MASKED_LABEL "ct-no-masked-label"
+#define OVN_FEATURE_CT_LB_RELATED "ovn-ct-lb-related"
+
 /* OVS datapath supported features.  Based on availability OVN might generate
  * different types of openflows.
  */