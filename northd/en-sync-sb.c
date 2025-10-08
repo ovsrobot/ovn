ovsrobot/ovn

commit 876179c674cd8a1ec9163498a97b666b98997ffc

northd: Refactor the northd change tracking.
northd engine tracking data now has the following tracking data
  - changed ovn_ports (right now only changed logical switch ports are
    tracked.)
  - changed load balancers.

This separation becomes easier to add lflow handling for these
changes in lflow northd engine handler.  This patch doesn't
handle the load balancer changes in lflow handler.  It will
be handled in upcoming commits.

Before this patch, any changes to load balancers or lb groups
resulted in full recompute of 'sync_to_sb_lb' and 'lflow' engine
nodes.  Now this scenario is optimized and it results in
recomputes of these nodes only if 'northd' engine node adds
any changed load balancers in its tracked data.  One example
is creating or updating an empty load balancer group.

Acked-by: Han Zhou <hzhou@ovn.org>
Acked-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

northd/en-sync-sb.c
@@ -236,7 +236,8 @@ sync_to_sb_lb_northd_handler(struct engine_node *node, void *data OVS_UNUSED)
 {
     struct northd_data *nd = engine_get_input_data("northd", node);
 
-    if (!nd->change_tracked || nd->lb_changed) {
+    if (!northd_has_tracked_data(&nd->trk_data) ||
+            northd_has_lbs_in_tracked_data(&nd->trk_data)) {
         /* Return false if no tracking data or if lbs changed. */
         return false;
     }
@@ -306,11 +307,13 @@ sync_to_sb_pb_northd_handler(struct engine_node *node, void *data OVS_UNUSED)
     }
 
     struct northd_data *nd = engine_get_input_data("northd", node);
-    if (!nd->change_tracked) {
+    if (!northd_has_tracked_data(&nd->trk_data) ||
+            northd_has_lbs_in_tracked_data(&nd->trk_data)) {
+        /* Return false if no tracking data or if lbs changed. */
         return false;
     }
 
-    if (!sync_pbs_for_northd_ls_changes(&nd->tracked_ls_changes)) {
+    if (!sync_pbs_for_northd_changed_ovn_ports(&nd->trk_data.trk_lsps)) {
         return false;
     }
 

northd/en-lflow.c
@@ -104,12 +104,12 @@ lflow_northd_handler(struct engine_node *node,
                      void *data)
 {
     struct northd_data *northd_data = engine_get_input_data("northd", node);
-    if (!northd_data->change_tracked) {
+    if (!northd_has_tracked_data(&northd_data->trk_data)) {
         return false;
     }
 
-    /* Fall back to recompute if lb related data has changed. */
-    if (northd_data->lb_changed) {
+    /* Fall back to recompute if load balancers have changed. */
+    if (northd_has_lbs_in_tracked_data(&northd_data->trk_data)) {
         return false;
     }
 
@@ -119,9 +119,9 @@ lflow_northd_handler(struct engine_node *node,
     struct lflow_input lflow_input;
     lflow_get_input_data(node, &lflow_input);
 
-    if (!lflow_handle_northd_ls_changes(eng_ctx->ovnsb_idl_txn,
-                                        &northd_data->tracked_ls_changes,
-                                        &lflow_input, &lflow_data->lflows)) {
+    if (!lflow_handle_northd_port_changes(eng_ctx->ovnsb_idl_txn,
+                                          &northd_data->trk_data.trk_lsps,
+                                          &lflow_input, &lflow_data->lflows)) {
         return false;
     }
 

northd/en-northd.c
@@ -171,7 +171,7 @@ northd_nb_logical_switch_handler(struct engine_node *node,
         return false;
     }
 
-    if (nd->change_tracked) {
+    if (northd_has_lsps_in_tracked_data(&nd->trk_data)) {
         engine_set_node_state(node, EN_UPDATED);
     }
 
@@ -209,10 +209,6 @@ northd_nb_logical_router_handler(struct engine_node *node,
         return false;
     }
 
-    if (nd->change_tracked) {
-        engine_set_node_state(node, EN_UPDATED);
-    }
-
     return true;
 }
 
@@ -230,15 +226,15 @@ northd_lb_data_handler(struct engine_node *node, void *data)
                                        &nd->ls_datapaths,
                                        &nd->lr_datapaths,
                                        &nd->lb_datapaths_map,
-                                       &nd->lb_group_datapaths_map)) {
+                                       &nd->lb_group_datapaths_map,
+                                       &nd->trk_data)) {
         return false;
     }
 
-    /* Indicate the depedendant engine nodes that load balancer/group
-     * related data has changed (including association to logical
-     * switch/router). */
-    nd->lb_changed = true;
-    engine_set_node_state(node, EN_UPDATED);
+    if (northd_has_lbs_in_tracked_data(&nd->trk_data)) {
+        engine_set_node_state(node, EN_UPDATED);
+    }
+
     return true;
 }
 

northd/en-sync-from-sb.c
@@ -65,7 +65,7 @@ sync_from_sb_northd_handler(struct engine_node *node,
                             void *data OVS_UNUSED)
 {
     struct northd_data *nd = engine_get_input_data("northd", node);
-    if (nd->change_tracked) {
+    if (northd_has_tracked_data(&nd->trk_data)) {
         /* So far the changes tracked in northd don't impact this node.
          *
          * In particular, for the LS related changes, the only field this node

northd/northd.h
@@ -83,22 +83,44 @@ struct ovn_datapaths {
     struct ovn_datapath **array;
 };
 
-/* Track what's changed for a single LS.
- * Now only track port changes. */
-struct ls_change {
-    struct ovs_list list_node;
-    struct ovn_datapath *od;
-    struct ovs_list added_ports;
-    struct ovs_list deleted_ports;
-    struct ovs_list updated_ports;
-    bool had_only_router_ports;
+struct tracked_ovn_ports {
+    /* tracked created ports.
+     * hmapx node data is 'struct ovn_port *' */
+    struct hmapx created;
+
+    /* tracked updated ports.
+     * hmapx node data is 'struct ovn_port *' */
+    struct hmapx updated;
+
+    /* tracked deleted ports.
+     * hmapx node data is 'struct ovn_port *' */
+    struct hmapx deleted;
 };
 
-/* Track what's changed for logical switches.
- * Now only track updated ones (added or deleted may be supported in the
- * future). */
-struct tracked_ls_changes {
-    struct ovs_list updated; /* Contains struct ls_change */
+struct tracked_lbs {
+    /* Tracked created or updated load balancers.
+     * hmapx node data is 'struct ovn_lb_datapaths' */
+    struct hmapx crupdated;
+
+    /* Tracked deleted lbs.
+     * hmapx node data is 'struct ovn_lb_datapaths' */
+    struct hmapx deleted;
+};
+
+enum northd_tracked_data_type {
+    NORTHD_TRACKED_NONE,
+    NORTHD_TRACKED_PORTS = (1 << 0),
+    NORTHD_TRACKED_LBS   = (1 << 1),
+};
+
+/* Track what's changed in the northd engine node.
+ * Now only tracks ovn_ports (of vif type) - created, updated
+ * and deleted. */
+struct northd_tracked_data {
+    /* Indicates the type of data tracked.  One or all of NORTHD_TRACKED_*. */
+    enum northd_tracked_data_type type;
+    struct tracked_ovn_ports trk_lsps;
+    struct tracked_lbs trk_lbs;
 };
 
 struct northd_data {
@@ -114,10 +136,9 @@ struct northd_data {
     struct chassis_features features;
     struct sset svc_monitor_lsps;
     struct hmap svc_monitor_map;
-    bool change_tracked;
-    struct tracked_ls_changes tracked_ls_changes;
-    bool lb_changed; /* Indicates if load balancers changed or association of
-                      * load balancer to logical switch/router changed. */
+
+    /* Change tracking data. */
+    struct northd_tracked_data trk_data;
 };
 
 struct lflow_data {
@@ -338,9 +359,10 @@ void northd_indices_create(struct northd_data *data,
 void build_lflows(struct ovsdb_idl_txn *ovnsb_txn,
                   struct lflow_input *input_data,
                   struct hmap *lflows);
-bool lflow_handle_northd_ls_changes(struct ovsdb_idl_txn *ovnsb_txn,
-                                    struct tracked_ls_changes *,
-                                    struct lflow_input *, struct hmap *lflows);
+bool lflow_handle_northd_port_changes(struct ovsdb_idl_txn *ovnsb_txn,
+                                      struct tracked_ovn_ports *,
+                                      struct lflow_input *,
+                                      struct hmap *lflows);
 bool northd_handle_sb_port_binding_changes(
     const struct sbrec_port_binding_table *, struct hmap *ls_ports);
 
@@ -349,7 +371,8 @@ bool northd_handle_lb_data_changes(struct tracked_lb_data *,
                                    struct ovn_datapaths *ls_datapaths,
                                    struct ovn_datapaths *lr_datapaths,
                                    struct hmap *lb_datapaths_map,
-                                   struct hmap *lbgrp_datapaths_map);
+                                   struct hmap *lbgrp_datapaths_map,
+                                   struct northd_tracked_data *);
 
 void build_bfd_table(struct ovsdb_idl_txn *ovnsb_txn,
                      const struct nbrec_bfd_table *,
@@ -372,6 +395,23 @@ void sync_lbs(struct ovsdb_idl_txn *, const struct sbrec_load_balancer_table *,
 bool check_sb_lb_duplicates(const struct sbrec_load_balancer_table *);
 
 void sync_pbs(struct ovsdb_idl_txn *, struct hmap *ls_ports);
-bool sync_pbs_for_northd_ls_changes(struct tracked_ls_changes *);
+bool sync_pbs_for_northd_changed_ovn_ports( struct tracked_ovn_ports *);
+
+static inline bool
+northd_has_tracked_data(struct northd_tracked_data *trk_nd_changes) {
+    return trk_nd_changes->type != NORTHD_TRACKED_NONE;
+}
+
+static inline bool
+northd_has_lbs_in_tracked_data(struct northd_tracked_data *trk_nd_changes)
+{
+    return trk_nd_changes->type & NORTHD_TRACKED_LBS;
+}
+
+static inline bool
+northd_has_lsps_in_tracked_data(struct northd_tracked_data *trk_nd_changes)
+{
+    return trk_nd_changes->type & NORTHD_TRACKED_PORTS;
+}
 
 #endif /* NORTHD_H */

tests/ovn-northd.at
@@ -10656,9 +10656,8 @@ check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
 lbg1_uuid=$(ovn-nbctl --wait=sb create load_balancer_group name=lbg1)
 check_engine_stats lb_data norecompute compute
 check_engine_stats northd norecompute compute
-check_engine_stats lflow recompute nocompute
-check_engine_stats sync_to_sb_lb recompute nocompute
-
+check_engine_stats lflow norecompute nocompute
+check_engine_stats sync_to_sb_lb norecompute nocompute
 CHECK_NO_CHANGE_AFTER_RECOMPUTE
 check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
 
@@ -10686,7 +10685,6 @@ check_engine_stats lb_data norecompute compute
 check_engine_stats northd norecompute compute
 check_engine_stats lflow recompute nocompute
 check_engine_stats sync_to_sb_lb recompute nocompute
-
 CHECK_NO_CHANGE_AFTER_RECOMPUTE
 
 check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
@@ -10834,8 +10832,8 @@ check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
 lbg1_uuid=$(ovn-nbctl --wait=sb create load_balancer_group name=lbg1)
 check_engine_stats lb_data norecompute compute
 check_engine_stats northd norecompute compute
-check_engine_stats lflow recompute nocompute
-check_engine_stats sync_to_sb_lb recompute nocompute
+check_engine_stats lflow norecompute nocompute
+check_engine_stats sync_to_sb_lb norecompute nocompute
 
 check as northd ovn-appctl -t ovn-northd inc-engine/clear-stats
 check ovn-nbctl --wait=sb set load_balancer_group . load_balancer="$lb2_uuid,$lb3_uuid,$lb4_uuid"

commit 50b441620a06a604edaae2ab56bdf5996f10fd64

Remove erroneus check in sync_to_sb_pb_northd_handler()
This check was brought in during refactor 876179... it was a mistake to
copy this check from sync_to_sb_lb_northd_handler() and the tracking of
lbs has nothing to do with what is going on in
sync_to_sb_pb_northd_handler()

Co-authored-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Jacob Tanenbaum <jtanenba@redhat.com>
Fixes: 876179c674cd ("northd: Refactor the northd change tracking.")
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/en-sync-sb.c
@@ -409,9 +409,8 @@ enum engine_input_handler_result
 sync_to_sb_pb_northd_handler(struct engine_node *node, void *data OVS_UNUSED)
 {
     struct northd_data *nd = engine_get_input_data("northd", node);
-    if (!northd_has_tracked_data(&nd->trk_data) ||
-            northd_has_lbs_in_tracked_data(&nd->trk_data)) {
-        /* Return false if no tracking data or if lbs changed. */
+    if (!northd_has_tracked_data(&nd->trk_data)) {
+        /* Return false if no tracking data */
         return EN_UNHANDLED;
     }
 