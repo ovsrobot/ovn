ovsrobot/ovn

commit abbc272ac77119822e98d78dfd664023635868be

northd: Add option to make work lb with stateless ACL.
[0] Removed support for using load balancers in conjunction with stateless ACL.
This commit adds an option enable-stateless-acl-with-lb for logical switch to
make load balancers work.

[0] - https://github.com/ovn-org/ovn/commit/a0f82efdd9dfd3ef2d9606c1890e353df1097a51

Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/northd.c
@@ -503,6 +503,7 @@ ovn_datapath_create(struct hmap *datapaths, const struct uuid *key,
     od->router_ports = VECTOR_EMPTY_INITIALIZER(struct ovn_port *);
     od->l3dgw_ports = VECTOR_EMPTY_INITIALIZER(struct ovn_port *);
     od->localnet_ports = VECTOR_EMPTY_INITIALIZER(struct ovn_port *);
+    od->lb_with_stateless_mode = false;
     return od;
 }
 
@@ -941,6 +942,11 @@ join_datapaths(const struct nbrec_logical_switch_table *nbrec_ls_table,
 
         if (smap_get_bool(&nbs->other_config, "ic-vxlan_mode", false)) {
             vxlan_ic_mode = true;
+        }
+
+        if (smap_get_bool(&nbs->other_config, "enable-stateless-acl-with-lb",
+                          false)) {
+            od->lb_with_stateless_mode = true;
         }
     }
 
@@ -3967,6 +3973,9 @@ build_lb_datapaths(const struct hmap *lbs, const struct hmap *lb_groups,
             lb_dps = ovn_lb_datapaths_find(lb_datapaths_map, lb_uuid);
             ovs_assert(lb_dps);
             ovn_lb_datapaths_add_ls(lb_dps, 1, &od);
+            if (od->lb_with_stateless_mode) {
+                hmapx_add(&lb_dps->ls_lb_with_stateless_mode, od);
+            }
         }
 
         for (size_t i = 0; i < od->nbs->n_load_balancer_group; i++) {
@@ -5549,6 +5558,10 @@ northd_handle_lb_data_changes(struct tracked_lb_data *trk_lb_data,
             ovs_assert(lb_dps);
             ovn_lb_datapaths_add_ls(lb_dps, 1, &od);
 
+            if (od->lb_with_stateless_mode) {
+                hmapx_add(&lb_dps->ls_lb_with_stateless_mode, od);
+            }
+
             /* Add the lb to the northd tracked data. */
             hmapx_add(&nd_changes->trk_lbs.crupdated, lb_dps);
         }
@@ -6230,7 +6243,7 @@ build_stateless_filter(const struct ovn_datapath *od,
                                 action,
                                 &acl->header_,
                                 lflow_ref);
-    } else {
+    } else if (!od->lb_with_stateless_mode) {
         ovn_lflow_add_with_hint(lflows, od, S_SWITCH_OUT_PRE_ACL,
                                 acl->priority + OVN_ACL_PRI_OFFSET,
                                 acl->match,
@@ -7795,8 +7808,14 @@ build_acls(const struct ls_stateful_record *ls_stateful_rec,
          *
          * This is enforced at a higher priority than ACLs can be defined. */
         ds_clear(&match);
-        ds_put_format(&match, "%s(ct.est && ct.rpl && ct_mark.blocked == 1)",
-                      use_ct_inv_match ? "ct.inv || " : "");
+
+        if (use_ct_inv_match && !od->lb_with_stateless_mode) {
+            ds_put_cstr(&match, "ct.inv || (ct.est && ct.rpl && "
+                                "ct_mark.blocked == 1)");
+        } else {
+            ds_put_cstr(&match, "(ct.est && ct.rpl && ct_mark.blocked == 1)");
+        }
+
         ovn_lflow_add(lflows, od, S_SWITCH_IN_ACL_EVAL, UINT16_MAX - 3,
                       ds_cstr(&match), REGBIT_ACL_VERDICT_DROP " = 1; next;",
                       lflow_ref);
@@ -8099,6 +8118,42 @@ build_lb_rules_pre_stateful(struct lflow_table *lflows,
             lflows, lb_dps->nb_ls_map, ods_size(ls_datapaths),
             S_SWITCH_IN_PRE_STATEFUL, 120, ds_cstr(match), ds_cstr(action),
             &lb->nlb->header_, lb_dps->lflow_ref);
+
+        struct lflow_ref *lflow_ref = lb_dps->lflow_ref;
+        struct hmapx_node *hmapx_node;
+        struct ovn_datapath *od;
+        HMAPX_FOR_EACH (hmapx_node, &lb_dps->ls_lb_with_stateless_mode) {
+            od = hmapx_node->data;
+
+            ds_clear(action);
+            ds_clear(match);
+
+            ds_put_format(match, "%s.dst == %s", ip_match, lb_vip->vip_str);
+
+            if (lb_vip->port_str) {
+                ds_put_format(match, " && %s.dst == %s", lb->proto,
+                              lb_vip->port_str);
+            }
+
+            ds_put_cstr(action, "ct_lb_mark;");
+
+            ovn_lflow_add(lflows, od, S_SWITCH_IN_PRE_STATEFUL, 150,
+                          ds_cstr(match), ds_cstr(action), lflow_ref);
+
+            if (lb->hairpin_snat_ip || lb_vip->port_str) {
+                ds_clear(action);
+                ds_clear(match);
+
+                ds_put_format(match, "%s && %s.dst == %s", lb->proto, ip_match,
+                                     lb->hairpin_snat_ip
+                                     ? lb->hairpin_snat_ip
+                                     : lb_vip->vip_str);
+                ds_put_cstr(action, "ct_lb_mark;");
+
+                ovn_lflow_add(lflows, od, S_SWITCH_IN_PRE_STATEFUL, 105,
+                              ds_cstr(match), ds_cstr(action), lflow_ref);
+            }
+        }
     }
 }
 

northd/ovn-northd.8.xml
@@ -653,6 +653,12 @@
       </li>
 
       <li>
+         A priority-105 added enabled when enable-stateless-acl-with-lb and
+         send all packet directed to VIP that don't match the above flows to
+         connection tracker.
+      </li>
+
+      <li>
          A priority-100 flow sends the packets to connection tracker based
          on a hint provided by the previous tables
          (with a match for <code>reg0[0] == 1</code>) by using the
@@ -2331,6 +2337,8 @@ output;
     <p>
       This is similar to ingress table <code>Pre-ACLs</code> except for
      <code>to-lport</code> traffic.
+     Except when the option enable-stateless-acl-with-lb is enabled:
+     REGBIT_ACL_STATELESS ignored.
     </p>
 
     <p>

commit 4873135951905538d7feefad053b26c50f98cfa3

northd: Fix connection tracking for enable-stateless-acl-with-lb option.
The commit [1] ("northd: Add support for stateless ACLs with load balancers")
incorrectly handled connection tracking when enable-stateless-acl-with-lb is enabled,
causing all stateless traffic in egress to be committed to conntrack.

This fix properly implements the enable-stateless-acl-with-lb behavior by:
When enable-stateless-acl-with-lb is enabled:
   - Still sending stateless traffic through connection tracker lookup
     in egress.
   - Adding new flow in ls_out_stateful to skip committing NEW stateless connections.
   - Only committing established connections for proper return traffic handling.

Fixes: abbc272ac771 ("northd: Add support for stateless ACLs with load balancers")
Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/northd.c
@@ -6098,7 +6098,7 @@ build_stateless_filter(const struct ovn_datapath *od,
                                 action,
                                 &acl->header_,
                                 lflow_ref);
-    } else if (!od->lb_with_stateless_mode) {
+    } else {
         ovn_lflow_add_with_hint(lflows, od, S_SWITCH_OUT_PRE_ACL,
                                 acl->priority + OVN_ACL_PRI_OFFSET,
                                 acl->match,
@@ -8435,6 +8435,29 @@ build_lrouter_lb_affinity_default_flows(struct ovn_datapath *od,
                   lflow_ref);
     ovn_lflow_add(lflows, od, S_ROUTER_IN_LB_AFF_LEARN, 0, "1", "next;",
                   lflow_ref);
+}
+
+static void
+build_lb_rules_for_stateless_acl(struct lflow_table *lflows,
+                                 struct ovn_lb_datapaths *lb_dps)
+{
+    /* When enable-stateless-acl-with-lb is enabled:
+     * 1. All stateless traffic must first pass through connection tracker
+     * in egress.
+     * 2. New connections (ct.new) will bypass commit phase.
+     */
+    struct hmapx_node *hmapx_node;
+    struct ovn_datapath *od;
+
+    HMAPX_FOR_EACH (hmapx_node, &lb_dps->ls_lb_with_stateless_mode) {
+        od = hmapx_node->data;
+        ovn_lflow_add(lflows, od, S_SWITCH_OUT_PRE_LB, 115,
+                      REGBIT_ACL_STATELESS" == 1",
+                      REGBIT_CONNTRACK_NAT" = 1; next;", lb_dps->lflow_ref);
+        ovn_lflow_add(lflows, od, S_SWITCH_OUT_STATEFUL, 110,
+                      REGBIT_ACL_STATELESS " == 1 && ct.new",
+                      "next;", lb_dps->lflow_ref);
+    }
 }
 
 static void
@@ -12857,6 +12880,7 @@ build_lswitch_flows_for_lb(struct ovn_lb_datapaths *lb_dps,
     build_lb_rules_pre_stateful(lflows, lb_dps, ls_datapaths, match, action);
     build_lb_rules(lflows, lb_dps, ls_datapaths, match, action,
                    meter_groups, svc_mons_data);
+    build_lb_rules_for_stateless_acl(lflows, lb_dps);
 }
 
 /* If there are any load balancing rules, we should send the packet to

northd/ovn-northd.8.xml
@@ -2485,8 +2485,6 @@ output;
     <p>
       This is similar to ingress table <code>Pre-ACLs</code> except for
      <code>to-lport</code> traffic.
-     Except when the option enable-stateless-acl-with-lb is enabled:
-     REGBIT_ACL_STATELESS ignored.
     </p>
 
     <p>
@@ -2553,6 +2551,12 @@ output;
       is the peer of a logical router port. This flow is added to
       skip the connection tracking of packets which will be entering
       logical router datapath from logical switch datapath for routing.
+    </p>
+
+    <p>
+      When <code>enable-stateless-acl-with-lb</code> is enabled,
+      additional priority-115 flow is added to match traffic with
+      <code>REGBIT_ACL_STATELESS</code> set and pass connection tracking.
     </p>
 
     <h3>Egress Table 4: Pre-stateful</h3>
@@ -2705,6 +2709,8 @@ output;
     <p>
       This is similar to ingress table <code>Stateful</code> except that
       there are no rules added for load balancing new connections.
+      When <code>enable-stateless-acl-with-lb</code> is enabled, new
+      stateless connections bypass connection tracking.
     </p>
 
     <ul>

tests/ovn-northd.at
@@ -17423,7 +17423,7 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD_NO_HV([
-AT_SETUP([enable-stateless-acl-with-lb usage])
+AT_SETUP([ovn-northd: enable-stateless-acl-with-lb usage])
 ovn_start ovn-northd
 
 AS_BOX([Create logical switches and ports.])
@@ -17449,51 +17449,44 @@ check ovn-nbctl --wait=sb ls-lb-add sw0 lb1
 
 ovn-sbctl dump-flows sw0 > sw0flows
 
-AT_CHECK(
-  [grep -E 'ls_(in|out)_pre_acl' sw0flows | grep reg0 | ovn_strip_lflows], [0], [dnl
-  table=??(ls_in_pre_acl      ), priority=100  , match=(ip), action=(reg0[[0]] = 1; next;)
-  table=??(ls_in_pre_acl      ), priority=2001 , match=(ip), action=(reg0[[16]] = 1; next;)
-  table=??(ls_out_pre_acl     ), priority=100  , match=(ip), action=(reg0[[0]] = 1; next;)
-  table=??(ls_out_pre_acl     ), priority=2001 , match=(ip), action=(reg0[[16]] = 1; next;)
+AT_CHECK([grep -E 'ls_out_pre_lb' sw0flows | ovn_strip_lflows], [0], [dnl
+  table=??(ls_out_pre_lb      ), priority=0    , match=(1), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=100  , match=(ip), action=(reg0[[2]] = 1; next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(eth.mcast), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(eth.src == $svc_monitor_mac), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(nd || nd_rs || nd_ra || mldv1 || mldv2), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(reg0[[16]] == 1), action=(next;)
 ])
 
-AT_CHECK(
-  [grep -E 'ls_out_acl_eval' sw0flows | grep 65532 | ovn_strip_lflows], [0], [dnl
-  table=??(ls_out_acl_eval    ), priority=65532, match=(!ct.est && ct.rel && !ct.new && ct_mark.blocked == 0), action=(reg8[[21]] = ct_label.nf_group; reg8[[16]] = 1; ct_commit_nat;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=(ct.est && !ct.rel && ct.rpl && ct_mark.blocked == 0), action=(reg8[[21]] = ct_label.nf_group; reg8[[16]] = 1; next;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=(ct.est && ct_mark.allow_established == 1), action=(reg8[[21]] = ct_label.nf_group; reg8[[16]] = 1; next;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=(ct.inv || (ct.est && ct.rpl && ct_mark.blocked == 1)), action=(reg8[[17]] = 1; next;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=(nd || nd_ra || nd_rs || mldv1 || mldv2), action=(reg8[[16]] = 1; next;)
+AT_CHECK([grep -E 'ls_out_stateful' sw0flows | ovn_strip_lflows], [0], [dnl
+  table=??(ls_out_stateful    ), priority=0    , match=(1), action=(next;)
+  table=??(ls_out_stateful    ), priority=100  , match=(reg0[[1]] == 1 && reg0[[13]] == 0), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 0; ct_label.nf_group_id = 0; }; next;)
+  table=??(ls_out_stateful    ), priority=100  , match=(reg0[[1]] == 1 && reg0[[13]] == 1), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_mark.obs_stage = reg8[[19..20]]; ct_mark.obs_collector_id = reg8[[8..15]]; ct_label.obs_point_id = reg9; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 0; ct_label.nf_group_id = 0; }; next;)
+  table=??(ls_out_stateful    ), priority=110  , match=(reg0[[1]] == 1 && reg0[[13]] == 0 && reg8[[21]] == 1), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 1; ct_label.nf_group_id = reg0[[22..29]]; }; next;)
+  table=??(ls_out_stateful    ), priority=110  , match=(reg0[[1]] == 1 && reg0[[13]] == 1 && reg8[[21]] == 1), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_mark.obs_stage = reg8[[19..20]]; ct_mark.obs_collector_id = reg8[[8..15]]; ct_label.obs_point_id = reg9; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 1; ct_label.nf_group_id = reg0[[22..29]]; }; next;)
 ])
 
 AS_BOX([Enable enable-stateless-acl-with-lb option.])
 check ovn-nbctl --wait=sb set logical_switch sw0 other_config:enable-stateless-acl-with-lb=true
 ovn-sbctl dump-flows sw0 > sw0flows
-AT_CHECK(
-  [grep -E 'ls_(in|out)_pre_acl' sw0flows | grep reg0 | ovn_strip_lflows], [0], [dnl
-  table=??(ls_in_pre_acl      ), priority=100  , match=(ip), action=(reg0[[0]] = 1; next;)
-  table=??(ls_in_pre_acl      ), priority=2001 , match=(ip), action=(reg0[[16]] = 1; next;)
-  table=??(ls_out_pre_acl     ), priority=100  , match=(ip), action=(reg0[[0]] = 1; next;)
+
+AT_CHECK([grep -E 'ls_out_stateful' sw0flows | ovn_strip_lflows], [0], [dnl
+  table=??(ls_out_stateful    ), priority=0    , match=(1), action=(next;)
+  table=??(ls_out_stateful    ), priority=100  , match=(reg0[[1]] == 1 && reg0[[13]] == 0), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 0; ct_label.nf_group_id = 0; }; next;)
+  table=??(ls_out_stateful    ), priority=100  , match=(reg0[[1]] == 1 && reg0[[13]] == 1), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_mark.obs_stage = reg8[[19..20]]; ct_mark.obs_collector_id = reg8[[8..15]]; ct_label.obs_point_id = reg9; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 0; ct_label.nf_group_id = 0; }; next;)
+  table=??(ls_out_stateful    ), priority=110  , match=(reg0[[16]] == 1 && ct.new), action=(next;)
+  table=??(ls_out_stateful    ), priority=110  , match=(reg0[[1]] == 1 && reg0[[13]] == 0 && reg8[[21]] == 1), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 1; ct_label.nf_group_id = reg0[[22..29]]; }; next;)
+  table=??(ls_out_stateful    ), priority=110  , match=(reg0[[1]] == 1 && reg0[[13]] == 1 && reg8[[21]] == 1), action=(ct_commit { ct_mark.blocked = 0; ct_mark.allow_established = reg0[[20]]; ct_mark.obs_stage = reg8[[19..20]]; ct_mark.obs_collector_id = reg8[[8..15]]; ct_label.obs_point_id = reg9; ct_label.acl_id = reg2[[16..31]]; ct_label.nf_group = 1; ct_label.nf_group_id = reg0[[22..29]]; }; next;)
 ])
 
-# We do not match conntrack invalid packets in case of load balancers with stateless ACLs.
-AT_CHECK(
-  [grep -E 'ls_out_acl_eval' sw0flows | grep 65532 | ovn_strip_lflows], [0], [dnl
-  table=??(ls_out_acl_eval    ), priority=65532, match=(!ct.est && ct.rel && !ct.new && ct_mark.blocked == 0), action=(reg8[[21]] = ct_label.nf_group; reg8[[16]] = 1; ct_commit_nat;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=((ct.est && ct.rpl && ct_mark.blocked == 1)), action=(reg8[[17]] = 1; next;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=(ct.est && !ct.rel && ct.rpl && ct_mark.blocked == 0), action=(reg8[[21]] = ct_label.nf_group; reg8[[16]] = 1; next;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=(ct.est && ct_mark.allow_established == 1), action=(reg8[[21]] = ct_label.nf_group; reg8[[16]] = 1; next;)
-  table=??(ls_out_acl_eval    ), priority=65532, match=(nd || nd_ra || nd_rs || mldv1 || mldv2), action=(reg8[[16]] = 1; next;)
-])
-
-AT_CHECK([grep -E 'ls_in_pre_stateful' sw0flows | ovn_strip_lflows], [0], [dnl
-  table=??(ls_in_pre_stateful ), priority=0    , match=(1), action=(next;)
-  table=??(ls_in_pre_stateful ), priority=100  , match=(reg0[[0]] == 1), action=(ct_next;)
-  table=??(ls_in_pre_stateful ), priority=105  , match=(tcp && ip4.dst == 10.0.0.4), action=(ct_lb_mark;)
-  table=??(ls_in_pre_stateful ), priority=110  , match=(reg0[[2]] == 1), action=(ct_lb_mark;)
-  table=??(ls_in_pre_stateful ), priority=115  , match=(reg0[[2]] == 1 && ip.is_frag), action=(reg0[[19]] = 1; ct_lb_mark;)
-  table=??(ls_in_pre_stateful ), priority=120  , match=(reg0[[2]] == 1 && ip4.dst == 10.0.0.4 && tcp.dst == 80), action=(reg4 = 10.0.0.4; reg2[[0..15]] = 80; ct_lb_mark;)
-  table=??(ls_in_pre_stateful ), priority=150  , match=(ip4.dst == 10.0.0.4 && tcp.dst == 80), action=(ct_lb_mark;)
+AT_CHECK([grep -E 'ls_out_pre_lb' sw0flows | ovn_strip_lflows], [0], [dnl
+  table=??(ls_out_pre_lb      ), priority=0    , match=(1), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=100  , match=(ip), action=(reg0[[2]] = 1; next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(eth.mcast), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(eth.src == $svc_monitor_mac), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(nd || nd_rs || nd_ra || mldv1 || mldv2), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=110  , match=(reg0[[16]] == 1), action=(next;)
+  table=??(ls_out_pre_lb      ), priority=115  , match=(reg0[[16]] == 1), action=(reg0[[2]] = 1; next;)
 ])
 
 AS_BOX([Create Load Balancer without port.])

tests/system-ovn.at
@@ -5099,13 +5099,14 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD([
-AT_SETUP([enable-stateless-acl-with-lb usage])
+AT_SETUP([ovn-system: enable-stateless-acl-with-lb usage])
 AT_SKIP_IF([test $HAVE_NC = no])
 
+CHECK_CONNTRACK()
 ovn_start
 OVS_TRAFFIC_VSWITCHD_START()
-
 ADD_BR([br-int])
+ADD_BR([br-ext], [set Bridge br-ext fail-mode=standalone])
 
 # Set external-ids in br-int needed for ovn-controller
 ovs-vsctl \
@@ -5115,62 +5116,127 @@ ovs-vsctl \
         -- set Open_vSwitch . external-ids:ovn-encap-ip=169.0.0.1 \
         -- set bridge br-int fail-mode=secure other-config:disable-in-band=true
 
+ovs-vsctl set Open_Vswitch . external_ids:ovn-bridge-mappings=phynet:br-ext
+
 # Start ovn-controller
 start_daemon ovn-controller
 
 # Logical network:
-# One logical switch with IPv4 load balancers that hairpin the traffic.
-check ovn-nbctl ls-add sw
-check ovn-nbctl lsp-add sw lsp1 -- lsp-set-addresses lsp1 00:00:00:00:00:01
-check ovn-nbctl lsp-add sw lsp2 -- lsp-set-addresses lsp2 00:00:00:00:00:02
+# Two LSs and one Lr - outside ls has access to a physical network
+#                    - ls1 has load balancers
+#   outside - lr1 - ls1
+# Сheck that lb work with stateless acl, external traffic not related
+# to lb doesn't create conntrack records.
+# In switches egress pipeline, on which the balancers and stateless ACL
+# are condigured together - all traffic is checked for connection tracker,
+# but only traffic related to balancing is committed (established connection)
 
-check ovn-nbctl lb-add lb-ipv4-tcp 88.88.88.88:8080 42.42.42.1:4041 tcp
-check ovn-nbctl ls-lb-add sw lb-ipv4-tcp
+check ovn-nbctl ls-add outside
 
-check ovn-nbctl lr-add rtr
-check ovn-nbctl lrp-add rtr rtr-sw 00:00:00:00:01:00 42.42.42.254/24
-check ovn-nbctl lsp-add-router-port sw sw-rtr rtr-sw
+check ovn-nbctl lsp-add outside public
+check ovn-nbctl lsp-set-type public localnet
+check ovn-nbctl lsp-set-addresses public unknown
+check ovn-nbctl lsp-set-options public network_name=phynet
+
+check ovn-nbctl lsp-add outside outside-down
+check ovn-nbctl lsp-set-addresses outside-down router
+check ovn-nbctl lsp-set-type outside-down router
+check ovn-nbctl lsp-set-options outside-down router-port=lr1-up
+
+check ovn-nbctl lr-add lr1
+
+check ovn-nbctl lrp-add lr1 lr1-up 00:00:01:01:02:03 169.254.0.1/24
+check ovn-nbctl lrp-add lr1 lr1-down 00:00:02:01:02:03 192.168.0.1/24 \
+      -- lrp-set-gateway-chassis lr1-up hv1
+
+check ovn-nbctl ls-add ls1
+
+check ovn-nbctl lsp-add ls1 ls1-up
+check ovn-nbctl lsp-set-addresses ls1-up router
+check ovn-nbctl lsp-set-type ls1-up router
+check ovn-nbctl lsp-set-options ls1-up router-port=lr1-down
+
+check ovn-nbctl lb-add lb-ipv4-tcp 192.168.0.1:8080 192.168.0.101:4041 tcp
+check ovn-nbctl ls-lb-add ls1 lb-ipv4-tcp
+
+check ovn-nbctl lb-add lb-ipv4-udp 192.168.0.1:8081 192.168.0.101:4042 udp
+check ovn-nbctl ls-lb-add ls1 lb-ipv4-udp
 
 ADD_NAMESPACES(lsp1)
-ADD_VETH(lsp1, lsp1, br-int, "42.42.42.1/24", "00:00:00:00:00:01", \
-         "42.42.42.254")
+ADD_VETH(lsp1, lsp1, br-int, "192.168.0.101/24", "00:00:00:00:00:01", \
+         "192.168.0.1")
+check ovn-nbctl lsp-add ls1 lsp1 \
+-- lsp-set-addresses lsp1 "00:00:00:00:00:01 192.168.0.101"
 
 ADD_NAMESPACES(lsp2)
-ADD_VETH(lsp2, lsp2, br-int, "42.42.42.2/24", "00:00:00:00:00:02", \
-         "42.42.42.254")
+ADD_VETH(lsp2, lsp2, br-int, "192.168.0.102/24", "00:00:00:00:00:02", \
+         "192.168.0.1")
+check ovn-nbctl lsp-add ls1 lsp2 \
+-- lsp-set-addresses lsp2 "00:00:00:00:00:02 192.168.0.102"
 
 # Wait for ovn-controller to catch up.
-wait_for_ports_up
 check ovn-nbctl --wait=hv sync
 
-# Start IPv4 TCP server on lsp1.
-NETNS_DAEMONIZE([lsp1], [nc -l -k 42.42.42.1 4041], [lsp1.pid])
+ADD_NAMESPACES(external)
+ADD_VETH(external, external, br-ext, "169.254.0.101/24", "00:00:00:00:00:04", \
+         "169.254.0.1")
 
-# Send the packet to VIP.
-NS_CHECK_EXEC([lsp1], [nc -z 88.88.88.88 8080], [0], [ignore], [ignore])
-NS_CHECK_EXEC([lsp2], [nc -z 88.88.88.88 8080], [0], [ignore], [ignore])
+NS_EXEC([external], [ip r add 192.168.0.0/24 via 169.254.0.1])
+NS_EXEC([lsp1], [ip r add 169.254.0.0/24 via 192.168.0.1])
+NS_EXEC([lsp2], [ip r add 169.254.0.1/24 via 192.168.0.1])
 
-check ovn-nbctl --wait=hv acl-add sw to-lport 2000 'ip' allow-stateless
-check ovn-nbctl --wait=hv acl-add sw from-lport 2000 'ip' allow-stateless
+# Add stateless acl with load balancers.
+check ovn-nbctl acl-add ls1 to-lport 2000 1 allow-stateless
+check ovn-nbctl acl-add ls1 from-lport 2000 1 allow-stateless
 
-# To provide work of load balancer with stateless ACL this is necessary
-# to set enable-stateless-acl-lb to true.
-check ovn-nbctl set logical_switch sw other_config:enable-stateless-acl-with-lb=true
+check ovn-nbctl --wait=sb set logical_switch ls1 other_config:enable-stateless-acl-with-lb=true
 
-check ovn-nbctl --wait=hv sync
+# Checking connectivity
+NS_CHECK_EXEC([external], [ping -q -c 3 -i 0.3 -w 2 192.168.0.101 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
 
-# Send the packet to VIP after add stateless acl.
-NS_CHECK_EXEC([lsp1], [nc -z 88.88.88.88 8080], [0], [ignore], [ignore])
-NS_CHECK_EXEC([lsp2], [nc -z 88.88.88.88 8080], [0], [ignore], [ignore])
+NS_CHECK_EXEC([lsp1], [ping -q -c 3 -i 0.3 -w 2 192.168.0.102 | FORMAT_PING], \
+[0], [dnl
+3 packets transmitted, 3 received, 0% packet loss, time 0ms
+])
 
-check ovn-nbctl --wait=hv acl-add sw to-lport 2001 'ip' allow-related
-check ovn-nbctl --wait=hv acl-add sw from-lport 2001 'ip' allow-related
+zone_lsp1_id=$(ovn-appctl -t ovn-controller ct-zone-list | grep lsp1 | cut -d ' ' -f2)
+zone_lsp2_id=$(ovn-appctl -t ovn-controller ct-zone-list | grep lsp2 | cut -d ' ' -f2)
 
-# Send the packet to VIP after add related acls.
-NS_CHECK_EXEC([lsp1], [nc -z 88.88.88.88 8080], [0], [ignore], [ignore])
-NS_CHECK_EXEC([lsp2], [nc -z 88.88.88.88 8080], [0], [ignore], [ignore])
+AT_CHECK([ovs-appctl dpctl/flush-conntrack])
 
-OVN_CLEANUP_CONTROLLER([hv1])
+# Start IPv4 TCP and UDP server on lsp1.
+NETNS_DAEMONIZE([lsp1], [nc -l -k 192.168.0.101 4041], [lsp1_tcp.pid])
+NETNS_DAEMONIZE([lsp1], [nc -u -l 192.168.0.101 4042], [lsp1_udp.pid])
+
+# Create another server without load balancer to check that it
+# does not create conntrack records.
+NETNS_DAEMONIZE([lsp1], [nc -l -k 192.168.0.101 4043], [lsp1_non_lb.pid])
+
+# Send the packet to VIP from private network.
+NS_CHECK_EXEC([lsp1], [nc -z 192.168.0.1 8080], [0], [ignore], [ignore])
+
+# Udp connections
+NS_CHECK_EXEC([lsp1], [echo a | nc -u 192.168.0.1 8081], [ignore], [ignore], [ignore])
+
+# Check conntrack zone of lsp1 has tcp entry for lb
+AT_CHECK([ovs-appctl dpctl/dump-conntrack zone=$zone_lsp1_id | \
+FORMAT_CT(192.168.0.1) | \
+sed -e 's/zone=[[0-9]]*/zone=<cleared>/'], [0], [dnl
+tcp,orig=(src=192.168.0.101,dst=192.168.0.1,sport=<cleared>,dport=<cleared>),reply=(src=192.168.0.101,dst=192.168.0.101,sport=<cleared>,dport=<cleared>),zone=<cleared>,mark=2,protoinfo=(state=<cleared>)
+udp,orig=(src=192.168.0.101,dst=192.168.0.1,sport=<cleared>,dport=<cleared>),reply=(src=192.168.0.101,dst=192.168.0.101,sport=<cleared>,dport=<cleared>),zone=<cleared>,mark=2
+])
+
+AT_CHECK([ovs-appctl dpctl/flush-conntrack])
+
+# Check that internal traffic not related to lb doesn't create conntrack records
+NS_CHECK_EXEC([external], [nc -z 192.168.0.101 4043], [0], [])
+
+AT_CHECK([ovs-appctl dpctl/dump-conntrack zone=$zone_lsp1_id | FORMAT_CT(192.168.0.101) | sed -e 's/zone=[[0-9]]*/zone=<cleared>/'], [0], [dnl])
+
+OVS_APP_EXIT_AND_WAIT([ovn-controller])
 
 as ovn-sb
 OVS_APP_EXIT_AND_WAIT([ovsdb-server])