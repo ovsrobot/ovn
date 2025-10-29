ovsrobot/ovn

commit cb7ba9e9057c46b11d8448c0a4dc400913989771

controller: Support user defined vrf names.
Previously vrf names where generated using "ovnvrf" + datapath id.
Now the controller supports the dynamic-routing-vrf-name setting to
freely configure the vrf name.
Note that the vrf ID is still the datapath id.

Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/route-exchange.c
@@ -217,27 +217,25 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
     const struct advertise_datapath_entry *ad;
     HMAP_FOR_EACH (ad, node, r_ctx_in->announce_routes) {
         uint32_t table_id = ad->db->tunnel_key;
-        char vrf_name[IFNAMSIZ + 1];
-        snprintf(vrf_name, sizeof vrf_name, "ovnvrf%"PRIi32, table_id);
 
         if (ad->maintain_vrf) {
-            if (!sset_contains(&old_maintained_vrfs, vrf_name)) {
-                int error = re_nl_create_vrf(vrf_name, table_id);
+            if (!sset_contains(&old_maintained_vrfs, ad->vrf_name)) {
+                int error = re_nl_create_vrf(ad->vrf_name, table_id);
                 if (error && error != EEXIST) {
                     VLOG_WARN_RL(&rl,
                                  "Unable to create VRF %s for datapath "
                                  "%"PRIi32": %s.",
-                                 vrf_name, table_id,
+                                 ad->vrf_name, table_id,
                                  ovs_strerror(error));
                     continue;
                 }
             }
-            sset_add(&_maintained_vrfs, vrf_name);
+            sset_add(&_maintained_vrfs, ad->vrf_name);
         } else {
             /* A previous maintain-vrf flag was removed. We should therefore
              * also not delete it even if we created it previously. */
-            sset_find_and_delete(&_maintained_vrfs, vrf_name);
-            sset_find_and_delete(&old_maintained_vrfs, vrf_name);
+            sset_find_and_delete(&_maintained_vrfs, ad->vrf_name);
+            sset_find_and_delete(&old_maintained_vrfs, ad->vrf_name);
         }
 
         maintained_route_table_add(table_id);

controller/route.c
@@ -185,6 +185,22 @@ route_run(struct route_ctx_in *r_ctx_in,
                               "dynamic-routing-maintain-vrf",
                               false);
 
+            const char *vrf_name = smap_get(&repb->options,
+                                            "dynamic-routing-vrf-name");
+            if (vrf_name && strlen(vrf_name) >= IFNAMSIZ) {
+                static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 20);
+                VLOG_WARN_RL(&rl, "Ignoring vrf name %s, since it is too long."
+                             "Maximum length is %d characters", vrf_name,
+                             IFNAMSIZ);
+                vrf_name = NULL;
+            }
+            if (vrf_name) {
+                memcpy(ad->vrf_name, vrf_name, strlen(vrf_name) + 1);
+            } else {
+                snprintf(ad->vrf_name, sizeof ad->vrf_name, "ovnvrf%"PRIi64,
+                         ad->db->tunnel_key);
+            }
+
             const char *port_name = smap_get(&repb->options,
                                             "dynamic-routing-port-name");
             if (!port_name) {

controller/route.h
@@ -20,6 +20,7 @@
 
 #include <stdbool.h>
 #include <netinet/in.h>
+#include <net/if.h>
 #include "openvswitch/hmap.h"
 #include "sset.h"
 #include "smap.h"
@@ -59,6 +60,7 @@ struct advertise_datapath_entry {
 
     const struct sbrec_datapath_binding *db;
     bool maintain_vrf;
+    char vrf_name[IFNAMSIZ + 1];
     struct hmap routes;
 
     /* The name of the port bindings locally bound for this datapath and

tests/system-ovn.at
@@ -15921,6 +15921,7 @@ OVN_FOR_EACH_NORTHD([
 AT_SETUP([dynamic-routing - DGP])
 
 VRF_RESERVE([1337])
+VRF_RESERVE([1338])
 
 # This test uses dynamic routing on a simulated multi-tenant internet
 # connection.
@@ -16195,10 +16196,14 @@ blackhole 198.51.100.0/24 proto 84 metric 1000
 233.252.0.0/24 via 192.168.10.10 dev lo onlink
 233.253.0.0/24 via 192.168.20.20 dev hv1-mll onlink])
 
-# Stoping with --restart will not touch the routes.
-check ovn-appctl -t ovn-controller exit --restart
-OVS_WAIT_UNTIL([test "$(ovn-appctl -t ovn-controller debug/status)" != "running"])
-OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1337 | awk '{$1=$1};1'], [dnl
+# Changing the vrf name will switch to the new one.
+# The old vrf will be removed.
+check ovn-nbctl --wait=hv set Logical_Router_Port internet-phys \
+    options:dynamic-routing-maintain-vrf=true
+check ovn-nbctl --wait=hv set Logical_Router internet \
+    options:dynamic-routing-vrf-name=ovnvrf1338
+AT_CHECK([ip vrf | grep -q ovnvrf1337], [1], [])
+OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1338 | awk '{$1=$1};1'], [dnl
 blackhole 192.0.2.1 proto 84 metric 1000
 blackhole 192.0.2.2 proto 84 metric 100
 blackhole 192.0.2.3 proto 84 metric 100
@@ -16207,14 +16212,24 @@ blackhole 198.51.100.0/24 proto 84 metric 1000
 233.252.0.0/24 via 192.168.10.10 dev lo onlink
 233.253.0.0/24 via 192.168.20.20 dev hv1-mll onlink])
 
-# Now we set maintain-vrf again and stop the ovn-controller.
-# It will then remove the VRF.
+# Stoping with --restart will not touch the routes.
+check ovn-appctl -t ovn-controller exit --restart
+OVS_WAIT_UNTIL([test "$(ovn-appctl -t ovn-controller debug/status)" != "running"])
+OVS_WAIT_UNTIL_EQUAL([ip route list vrf ovnvrf1338 | awk '{$1=$1};1'], [dnl
+blackhole 192.0.2.1 proto 84 metric 1000
+blackhole 192.0.2.2 proto 84 metric 100
+blackhole 192.0.2.3 proto 84 metric 100
+blackhole 192.0.2.10 proto 84 metric 100
+blackhole 198.51.100.0/24 proto 84 metric 1000
+233.252.0.0/24 via 192.168.10.10 dev lo onlink
+233.253.0.0/24 via 192.168.20.20 dev hv1-mll onlink])
+
+# When we now stop the ovn-controller it will remove the VRF.
 start_daemon ovn-controller
 OVS_WAIT_UNTIL([test "$(ovn-appctl -t ovn-controller debug/status)" == "running"])
-check ovn-nbctl --wait=hv set Logical_Router_Port internet-phys \
-        options:dynamic-routing-maintain-vrf=true
+check ovn-nbctl --wait=hv sync
 OVS_APP_EXIT_AND_WAIT([ovn-controller])
-AT_CHECK([ip vrf | grep -q ovnvrf1337], [1], [])
+AT_CHECK([ip vrf | grep -q ovnvrf1338], [1], [])
 
 as ovn-sb
 OVS_APP_EXIT_AND_WAIT([ovsdb-server])

commit 0882e82b6a409cdbc74dabcdd44aea49ed45595f

controller, northd: Add support for CMS to specify the vrf table_id.
Add an LR option, called "dynamic-routing-vrf-id", that allows CMS to
specify vrf table_id. The table_id will be used instead of tunnel_key
for given datapath corresponding to the LR. This also allows CMS to
share one vrf between multiple LRs.

Fixes: cb7ba9e9057c ("controller: Support user defined vrf names.")
Fixes: faf4df563f1d ("controller: Announce routes via route-exchange.")
Reported-at: https://issues.redhat.com/browse/FDP-1886
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/route-exchange.c
@@ -19,6 +19,7 @@
 
 #include <errno.h>
 #include <net/if.h>
+#include <linux/rtnetlink.h>
 #include <stdbool.h>
 
 #include "openvswitch/poll-loop.h"
@@ -242,7 +243,7 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
     CLEAR_ROUTE_EXCHANGE_NL_STATUS();
     const struct advertise_datapath_entry *ad;
     HMAP_FOR_EACH (ad, node, r_ctx_in->announce_routes) {
-        uint32_t table_id = ad->db->tunnel_key;
+        uint32_t table_id = route_get_table_id(ad->db);
 
         if (ad->maintain_vrf) {
             if (!sset_contains(&old_maintained_vrfs, ad->vrf_name)) {
@@ -250,8 +251,8 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
                 if (error && error != EEXIST) {
                     VLOG_WARN_RL(&rl,
                                  "Unable to create VRF %s for datapath "
-                                 "%"PRIi32": %s.",
-                                 ad->vrf_name, table_id,
+                                 UUID_FMT": %s.", ad->vrf_name,
+                                 UUID_ARGS(&ad->db->header_.uuid),
                                  ovs_strerror(error));
                     SET_ROUTE_EXCHANGE_NL_STATUS(error);
                     continue;
@@ -270,7 +271,7 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
         struct vector received_routes =
             VECTOR_EMPTY_INITIALIZER(struct re_nl_received_route_node);
 
-        error = re_nl_sync_routes(ad->db->tunnel_key, &ad->routes,
+        error = re_nl_sync_routes(table_id, &ad->routes,
                                   &received_routes, ad->db);
         SET_ROUTE_EXCHANGE_NL_STATUS(error);
 

controller/route.c
@@ -218,8 +218,8 @@ route_run(struct route_ctx_in *r_ctx_in,
             if (vrf_name) {
                 memcpy(ad->vrf_name, vrf_name, strlen(vrf_name) + 1);
             } else {
-                snprintf(ad->vrf_name, sizeof ad->vrf_name, "ovnvrf%"PRIi64,
-                         ad->db->tunnel_key);
+                snprintf(ad->vrf_name, sizeof ad->vrf_name, "ovnvrf%"PRIu32,
+                         route_get_table_id(ad->db));
             }
 
             if (!port_name) {
@@ -322,4 +322,12 @@ route_cleanup(struct hmap *announce_routes)
     HMAP_FOR_EACH_POP (ad, node, announce_routes) {
         advertise_datapath_cleanup(ad);
     }
+}
+
+uint32_t
+route_get_table_id(const struct sbrec_datapath_binding *dp)
+{
+    int64_t vrf_id = ovn_smap_get_llong(&dp->external_ids,
+                                    "dynamic-routing-vrf-id", -1);
+    return (vrf_id >= 1 && vrf_id <= UINT32_MAX) ? vrf_id : dp->tunnel_key;
 }

controller/route.h
@@ -29,6 +29,7 @@ struct hmap;
 struct ovsdb_idl_index;
 struct sbrec_chassis;
 struct sbrec_port_binding;
+struct sbrec_datapath_binding;
 
 struct route_ctx_in {
     const struct sbrec_advertised_route_table *advertised_route_table;
@@ -85,5 +86,6 @@ const struct sbrec_port_binding *route_exchange_find_port(
 uint32_t advertise_route_hash(const struct in6_addr *dst, unsigned int plen);
 void route_run(struct route_ctx_in *, struct route_ctx_out *);
 void route_cleanup(struct hmap *announce_routes);
+uint32_t route_get_table_id(const struct sbrec_datapath_binding *dp);
 
 #endif /* ROUTE_H */

northd/en-datapath-logical-router.c
@@ -88,6 +88,13 @@ gather_external_ids(const struct nbrec_logical_router *nbr,
     smap_add_format(external_ids, "disable_garp_rarp",
                     disable_garp_rarp ? "true" : "false");
 
+    int64_t vrf_id = ovn_smap_get_llong(&nbr->options,
+                                        "dynamic-routing-vrf-id", -1);
+    if (vrf_id > 0) {
+        smap_add_format(external_ids, "dynamic-routing-vrf-id", "%"PRId64,
+                        vrf_id);
+    }
+
     /* For backwards-compatibility, also store the NB UUID in
      * external-ids:logical-router. This is useful if ovn-controller
      * has not updated and expects this to be where to find the

ovn-nb.xml
@@ -3383,7 +3383,9 @@ or
         <p>
           This defines the name of the vrf the ovn-controller will use to
           advertise and learn routes. If not set the vrf will be named "ovnvrf"
-          with the datapath id of the Logical Router appended to it.
+          with the <ref column="options" key="dynamic-routing-vrf-id"
+          table="Logical_Router"/> or datapath id of the Logical Router
+          appended to it.
         </p>
 
         <p>
@@ -3394,6 +3396,27 @@ or
         <p>
           The vrf table id is not affected by this setting. For details see
           <ref column="options" key="dynamic-routing-maintain-vrf"
+               table="Logical_Router"/> on the Logical_Router.
+        </p>
+      </column>
+
+      <column name="options" key="dynamic-routing-vrf-id"
+              type='{"type": "integer", "minInteger": 1,
+                     "maxInteger": 4294967295}'>
+        <p>
+          Only relevant if <ref column="options" key="dynamic-routing"/>
+          is set to <code>true</code>.
+        </p>
+
+        <p>
+          This defines the vrf table id used by the specified vrf. When not
+          set or set to invalid value datapath id of the Logical Router
+          will be used instead.
+        </p>
+
+        <p>
+          This can also affect the vrf name. For details see
+          <ref column="options" key="dynamic-routing-vrf-name"
                table="Logical_Router"/> on the Logical_Router.
         </p>
       </column>
@@ -4416,7 +4439,9 @@ or
           this LRP.
           Unless <ref column="options" key="dynamic-routing-vrf-name"
           table="Logical_Router"/> is set the vrf will be named "ovnvrf" with
-          the datapath id of the Logical Router appended to it.
+          the <ref column="options" key="dynamic-routing-vrf-id"
+          table="Logical_Router"/> or datapath id of the Logical Router
+          appended to it.
         </p>
 
         <p>

tests/multinode-bgp-macros.at
@@ -275,7 +275,7 @@ m_setup_ovn_frr_router() {
 
     check multinode_nbctl lr-add $lr
     check multinode_nbctl set Logical_Router $lr options:chassis=$node \
-        options:dynamic-routing=true options:requested-tnl-key=$vni
+        options:dynamic-routing=true options:dynamic-routing-vrf-id=$vni
 
     check multinode_nbctl lrp-add $lr $lrp $bgp_mac
     check multinode_nbctl lrp-set-options $lrp \

tests/system-ovn.at
@@ -15821,7 +15821,7 @@ check ovn-nbctl ls-add p2
 
 check ovn-nbctl lr-add internet \
     -- set Logical_Router internet options:dynamic-routing=true \
-                                   options:requested-tnl-key=1337
+                                   options:dynamic-routing-vrf-id=1337
 
 check ovn-nbctl lrp-add internet internet-public \
         00:00:02:01:02:03 192.0.2.1/24 \
@@ -15845,7 +15845,7 @@ check ovn-nbctl lsp-add phys phys-internet \
 # LR pr1 setup.
 
 check ovn-nbctl lr-add pr1 \
-    -- set Logical_Router pr1 options:requested-tnl-key=1338
+    -- set Logical_Router pr1 options:dynamic-routing-vrf-id=1338
 
 check ovn-nbctl lrp-add pr1 pr1-public \
         00:00:02:01:02:04 192.0.2.2/24
@@ -15867,7 +15867,7 @@ check ovn-nbctl lr-route-add pr1 0.0.0.0/0 192.0.2.1
 # LR pr2 setup.
 
 check ovn-nbctl lr-add pr2 \
-    -- set Logical_Router pr2 options:requested-tnl-key=1339
+    -- set Logical_Router pr2 options:dynamic-routing-vrf-id=1339
 
 check ovn-nbctl lrp-add pr2 pr2-public \
         00:00:02:01:02:05 192.0.2.3/24
@@ -16177,7 +16177,7 @@ check ovn-nbctl ls-add p2
 
 check ovn-nbctl lr-add internet \
     -- set Logical_Router internet options:dynamic-routing=true \
-                                   options:requested-tnl-key=1337 \
+                                   options:dynamic-routing-vrf-id=1337 \
                                    options:chassis=hv1
 
 check ovn-nbctl lrp-add internet internet-public \
@@ -16201,7 +16201,7 @@ check ovn-nbctl lsp-add phys phys-internet \
 # LR pr1 setup.
 
 check ovn-nbctl lr-add pr1 \
-    -- set Logical_Router pr1 options:requested-tnl-key=1338
+    -- set Logical_Router pr1 options:dynamic-routing-vrf-id=1338
 
 check ovn-nbctl lrp-add pr1 pr1-public \
         00:00:02:01:02:04 192.0.2.2/24
@@ -16223,7 +16223,7 @@ check ovn-nbctl lr-route-add pr1 0.0.0.0/0 192.0.2.1
 # LR pr2 setup.
 
 check ovn-nbctl lr-add pr2 \
-    -- set Logical_Router pr2 options:requested-tnl-key=1339
+    -- set Logical_Router pr2 options:dynamic-routing-vrf-id=1339
 
 check ovn-nbctl lrp-add pr2 pr2-public \
         00:00:02:01:02:05 192.0.2.3/24
@@ -16484,7 +16484,7 @@ OVS_WAIT_WHILE([ip link | grep -q ovnvrf$vrf:.*UP])
 check ovn-nbctl                                             \
     -- lr-add R1                                            \
     -- set Logical_Router R1                                \
-            options:requested-tnl-key=$vrf                  \
+            options:dynamic-routing-vrf-id=$vrf             \
             options:dynamic-routing=true                    \
     -- ls-add sw0                                           \
     -- ls-add public                                        \
@@ -16642,7 +16642,7 @@ OVS_WAIT_WHILE([ip link | grep -q ovnvrf$vrf:.*UP])
 check ovn-nbctl                                                   \
     -- lr-add R1                                                  \
     -- set Logical_Router R1                                      \
-            options:requested-tnl-key=$vrf                        \
+            options:dynamic-routing-vrf-id=$vrf                   \
             options:dynamic-routing=true                          \
     -- ls-add sw0                                                 \
     -- ls-add public                                              \
@@ -16802,7 +16802,7 @@ OVS_WAIT_WHILE([ip link | grep -q ovnvrf$vrf:.*UP])
 check ovn-nbctl                                             \
     -- lr-add R1                                            \
     -- set Logical_Router R1                                \
-            options:requested-tnl-key=$vrf                  \
+            options:dynamic-routing-vrf-id=$vrf             \
             options:dynamic-routing=true                    \
     -- ls-add sw0                                           \
     -- ls-add public                                        \
@@ -17032,7 +17032,7 @@ OVS_WAIT_WHILE([ip link | grep -q ovnvrf$vrf:.*UP])
 check ovn-nbctl                                                   \
     -- lr-add R1                                                  \
     -- set Logical_Router R1                                      \
-            options:requested-tnl-key=$vrf                        \
+            options:dynamic-routing-vrf-id=$vrf                   \
             options:dynamic-routing=true                          \
     -- ls-add sw0                                                 \
     -- ls-add public                                              \
@@ -17297,7 +17297,7 @@ check ovn-nbctl                                           \
 check ovn-nbctl                                           \
     -- lr-add R2                                          \
     -- set Logical_Router R2                              \
-            options:requested-tnl-key=$vrf                \
+            options:dynamic-routing-vrf-id=$vrf           \
             options:dynamic-routing=true                  \
             options:dynamic-routing-redistribute=lb,nat   \
     -- lrp-add R2 R2-PUB1 00:02:02:01:02:03 172.16.1.2/24 \