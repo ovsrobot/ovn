586ae78fa73b1b4cf1f7d93073a16f5af4f6d8ac
Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
2025-12-17 14:40:00 +0100

    Add the capability to specify multiple EVPN device names for vxlan.
    We need to be able to monitor two vxlan interfaces (one for IPv4 and one
    for IPv6) for dual stack EVPN logical switches since we can't configure a
    single vxlan interface with multiple local IPs (one for v4 one for v6).
    Fix the probelm allowing the CMS to specify a list of vxlan device names
    using the following syntax:
    
    dynamic-routing-vxlan-ifname=vxlan-if1,vxlan-if2,..,vxlan-ifn
    
    Fixes: 36737306806e ("Add the capability to specify EVPN device names.")
    Signed-off-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/en-datapath-logical-switch.c
+++ northd/en-datapath-logical-switch.c
@@ -58,6 +58,26 @@ get_requested_tunnel_key(const struct nbrec_logical_switch *nbs,
     return requested_tunnel_key;
 }
 
+static bool
+check_dynamic_device_name(const char *if_names)
+{
+    struct sset device_names;
+    sset_from_delimited_string(&device_names, if_names, ",");
+
+    bool ret = true;
+    const char *name;
+    SSET_FOR_EACH (name, &device_names) {
+        if (strlen(name) > IFNAMSIZ) {
+            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
+            VLOG_WARN_RL(&rl, "dynamic-routing-ifname %s is too long", name);
+            ret = false;
+            break;
+        }
+    }
+    sset_destroy(&device_names);
+    return ret;
+}
+
 static void
 gather_external_ids(const struct nbrec_logical_switch *nbs,
                     struct smap *external_ids)
@@ -95,42 +115,23 @@ gather_external_ids(const struct nbrec_logical_switch *nbs,
 
     const char *bridge_ifname = smap_get(&nbs->other_config,
                                          "dynamic-routing-bridge-ifname");
-    if (bridge_ifname) {
-        if (strlen(bridge_ifname) > IFNAMSIZ) {
-            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
-            VLOG_WARN_RL(&rl, "dynamic-routing-bridge-ifname %s is too long",
-                         bridge_ifname);
-        } else {
-            smap_add(external_ids, "dynamic-routing-bridge-ifname",
-                     bridge_ifname);
-        }
+    if (bridge_ifname && check_dynamic_device_name(bridge_ifname)) {
+        smap_add(external_ids, "dynamic-routing-bridge-ifname",
+                 bridge_ifname);
     }
 
     const char *vxlan_ifname = smap_get(&nbs->other_config,
                                         "dynamic-routing-vxlan-ifname");
-    if (vxlan_ifname) {
-        if (strlen(vxlan_ifname) > IFNAMSIZ) {
-            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
-            VLOG_WARN_RL(&rl, "dynamic-routing-vxlan-ifname %s is too long",
-                         vxlan_ifname);
-        } else {
-            smap_add(external_ids, "dynamic-routing-vxlan-ifname",
-                     vxlan_ifname);
-        }
+    if (vxlan_ifname && check_dynamic_device_name(vxlan_ifname)) {
+        smap_add(external_ids, "dynamic-routing-vxlan-ifname",
+                 vxlan_ifname);
     }
 
     const char *adv_ifname = smap_get(&nbs->other_config,
                                       "dynamic-routing-advertise-ifname");
-    if (adv_ifname) {
-        if (strlen(adv_ifname) > IFNAMSIZ) {
-            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
-            VLOG_WARN_RL(&rl,
-                         "dynamic-routing-advertise-ifname %s is too long",
-                         adv_ifname);
-        } else {
-            smap_add(external_ids, "dynamic-routing-advertise-ifname",
-                     adv_ifname);
-        }
+    if (adv_ifname && check_dynamic_device_name(adv_ifname)) {
+        smap_add(external_ids, "dynamic-routing-advertise-ifname",
+                 adv_ifname);
     }
 
     const char *redistribute =
