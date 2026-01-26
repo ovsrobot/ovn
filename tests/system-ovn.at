ovsrobot/ovn

commit 1b4058b9162c27edc6bd42c3dedbef622e2fd0a7

northd: Process external arps on ha chassis.
The northd creates flows that restrict processing ARP requests:
If ARP request comes from lswitch that has either localnet or
l2gateway ports and router's attachment port is L3 dgw port such
ARP will be allowed only on resident chassis.

However, same lswitch could also have 'normal' aka virtual lports.
For these lports restrictions described above should not be applied.

The fix moves is_chassis_resident check from lrouter to lswitch level.
The residence check is only applied for ARP requests that were
originated from localnet/l2gateway ports. If such check is succeeded
ARP request is allowed otherwise ARP request packet is dropped.
For ARP requests coming from virtual ports no residence restrictions
are applied.

Co-authored-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
Signed-off-by: Aleksandr Smirnov <alekssmirnov@k2.cloud>
Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-ls-arp.c
@@ -1,0 +1,360 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+/* OVS includes */
+#include "include/openvswitch/hmap.h"
+#include "openvswitch/util.h"
+#include "openvswitch/vlog.h"
+#include "stopwatch.h"
+
+/* OVN includes */
+#include "en-lr-nat.h"
+#include "en-ls-arp.h"
+#include "lib/inc-proc-eng.h"
+#include "lib/ovn-nb-idl.h"
+#include "lib/ovn-sb-idl.h"
+#include "lib/ovn-util.h"
+#include "lib/stopwatch-names.h"
+#include "lflow-mgr.h"
+#include "northd.h"
+
+VLOG_DEFINE_THIS_MODULE(en_ls_arp);
+
+/* Static functions. */
+struct ls_arp_input {
+    const struct ovn_datapaths *ls_datapaths;
+    const struct lr_nat_table *lr_nats;
+};
+
+static struct ls_arp_input
+ls_arp_get_input_data(struct engine_node *node)
+{
+    const struct northd_data *northd_data =
+        engine_get_input_data("northd", node);
+    struct ed_type_lr_nat_data *lr_nat_data =
+        engine_get_input_data("lr_nat", node);
+
+    return (struct ls_arp_input) {
+        .ls_datapaths = &northd_data->ls_datapaths,
+        .lr_nats = &lr_nat_data->lr_nats,
+    };
+}
+
+static void
+ls_arp_record_clear(struct ls_arp_record *ls_arp_record)
+{
+    lflow_ref_destroy(ls_arp_record->lflow_ref);
+    hmapx_destroy(&ls_arp_record->nat_records);
+    free(ls_arp_record);
+}
+
+static void
+ls_arp_table_clear(struct ls_arp_table *table)
+{
+    struct ls_arp_record *ls_arp_record;
+    HMAP_FOR_EACH_POP (ls_arp_record, key_node, &table->entries) {
+        ls_arp_record_clear(ls_arp_record);
+    }
+}
+
+static inline bool
+is_centralized_nat_record(const struct ovn_nat *nat_entry)
+{
+    return nat_entry->is_valid
+           && nat_entry->l3dgw_port
+           && nat_entry->l3dgw_port->peer
+           && nat_entry->l3dgw_port->peer->od
+           && !nat_entry->is_distributed;
+}
+
+static void
+nat_record_data_create(struct ls_arp_record *ls_arp_record,
+                       const struct ovn_datapath *od,
+                       const struct lr_nat_table *lr_nats)
+{
+    struct ovn_port *op;
+    VECTOR_FOR_EACH (&od->router_ports, op) {
+        const struct ovn_datapath *lr_od = op->peer->od;
+        const struct lr_nat_record *lrnat_rec =
+            lr_nat_table_find_by_uuid(lr_nats, lr_od->key);
+
+        if (!lrnat_rec) {
+            continue;
+        }
+
+        for (size_t i = 0; i < lrnat_rec->n_nat_entries; i++) {
+            const struct ovn_nat *nat_entry = &lrnat_rec->nat_entries[i];
+
+            if (is_centralized_nat_record(nat_entry)) {
+                hmapx_add(&ls_arp_record->nat_records,
+                          (struct lrnat_rec *) lrnat_rec);
+            }
+        }
+    }
+}
+
+static struct ls_arp_record *
+ls_arp_record_lookup_by_od_(const struct ls_arp_table *table,
+                            const struct ovn_datapath *od)
+{
+    struct ls_arp_record *ls_arp_record;
+    HMAP_FOR_EACH_WITH_HASH (ls_arp_record, key_node,
+                             uuid_hash(&od->nbs->header_.uuid),
+                             &table->entries) {
+        if (uuid_equals(&ls_arp_record->nbs_uuid,
+                        &od->nbs->header_.uuid)) {
+            return ls_arp_record;
+        }
+    }
+
+    return NULL;
+}
+
+static struct ls_arp_record *
+ls_arp_record_create(struct ls_arp_table *table,
+                     const struct ovn_datapath *od,
+                     const struct lr_nat_table *lr_nats)
+{
+    struct ls_arp_record *ls_arp_record = xzalloc(sizeof *ls_arp_record);
+
+    ls_arp_record->ls_index = od->index;
+    ls_arp_record->nbs_uuid = od->nbs->header_.uuid;
+
+    hmapx_init(&ls_arp_record->nat_records);
+    nat_record_data_create(ls_arp_record, od, lr_nats);
+
+    ls_arp_record->lflow_ref = lflow_ref_create();
+
+    hmap_insert(&table->entries, &ls_arp_record->key_node,
+                uuid_hash(&od->nbs->header_.uuid));
+
+    return ls_arp_record;
+}
+
+/* Public functions. */
+void*
+en_ls_arp_init(struct engine_node *node OVS_UNUSED,
+               struct engine_arg *arg OVS_UNUSED)
+{
+    struct ed_type_ls_arp *data = xzalloc(sizeof *data);
+
+    hmap_init(&data->table.entries);
+    hmapx_init(&data->trk_data.crupdated);
+    hmapx_init(&data->trk_data.deleted);
+
+    return data;
+}
+
+void
+en_ls_arp_clear_tracked_data(void *data_)
+{
+    struct ed_type_ls_arp *data = data_;
+    hmapx_clear(&data->trk_data.crupdated);
+
+    struct hmapx_node *n;
+    HMAPX_FOR_EACH_SAFE (n, &data->trk_data.deleted) {
+        ls_arp_record_clear(n->data);
+        hmapx_delete(&data->trk_data.deleted, n);
+    }
+    hmapx_clear(&data->trk_data.deleted);
+}
+
+void
+en_ls_arp_cleanup(void *data_)
+{
+    struct ed_type_ls_arp *data = data_;
+
+    ls_arp_table_clear(&data->table);
+    hmap_destroy(&data->table.entries);
+    hmapx_destroy(&data->trk_data.crupdated);
+
+    struct hmapx_node *n;
+    HMAPX_FOR_EACH_SAFE (n, &data->trk_data.deleted) {
+        ls_arp_record_clear(n->data);
+        hmapx_delete(&data->trk_data.deleted, n);
+    }
+    hmapx_destroy(&data->trk_data.deleted);
+}
+
+enum engine_node_state
+en_ls_arp_run(struct engine_node *node, void *data_)
+{
+    struct ls_arp_input input_data = ls_arp_get_input_data(node);
+    struct ed_type_ls_arp *data = data_;
+
+    stopwatch_start(LS_ARP_RUN_STOPWATCH_NAME, time_msec());
+
+    ls_arp_table_clear(&data->table);
+
+    const struct ovn_datapath *od;
+    HMAP_FOR_EACH (od, key_node, &input_data.ls_datapaths->datapaths) {
+        /* Filtering ARP entries at logical switch works
+         * when there are physical ports on the switch. */
+        if (hmapx_is_empty(&od->phys_ports)) {
+            continue;
+        }
+
+        ls_arp_record_create(&data->table, od, input_data.lr_nats);
+    }
+
+    stopwatch_stop(LS_ARP_RUN_STOPWATCH_NAME, time_msec());
+
+    return EN_UPDATED;
+}
+
+/* Handler functions. */
+enum engine_input_handler_result
+ls_arp_northd_handler(struct engine_node *node, void *data_)
+{
+    struct northd_data *northd_data = engine_get_input_data("northd", node);
+    if (!northd_has_tracked_data(&northd_data->trk_data)) {
+        return EN_UNHANDLED;
+    }
+
+    if (!northd_has_lswitches_in_tracked_data(&northd_data->trk_data)) {
+        return EN_HANDLED_UNCHANGED;
+    }
+
+    struct northd_tracked_data *nd_changes = &northd_data->trk_data;
+    struct ls_arp_input input_data = ls_arp_get_input_data(node);
+    struct ed_type_ls_arp *data = data_;
+    struct hmapx_node *hmapx_node;
+    struct ls_arp_record *ls_arp_record;
+
+    HMAPX_FOR_EACH (hmapx_node, &nd_changes->trk_switches.crupdated) {
+        const struct ovn_datapath *od = hmapx_node->data;
+
+        ls_arp_record = ls_arp_record_lookup_by_od_(&data->table, od);
+
+        if (!ls_arp_record) {
+            /* Filtering ARP entries at logical switch works
+             * when there are physical ports on the switch. */
+            if (hmapx_is_empty(&od->phys_ports)) {
+                /* NOTE: If the switch used to have physical ports but those
+                 * were removed the lr_nat node has recomputed and triggers
+                 * the ls_arp_lr_nat_handler() which cannot incrementally
+                 * process changes.  This implicitly triggers correct
+                 * handling of the removal.*/
+                continue;
+            }
+            ls_arp_record = ls_arp_record_create(&data->table,
+                                                 od, input_data.lr_nats);
+        } else {
+            nat_record_data_create(ls_arp_record, od, input_data.lr_nats);
+        }
+
+        hmapx_add(&data->trk_data.crupdated, ls_arp_record);
+    }
+
+    HMAPX_FOR_EACH (hmapx_node, &nd_changes->trk_switches.deleted) {
+        const struct ovn_datapath *od = hmapx_node->data;
+
+        ls_arp_record = ls_arp_record_lookup_by_od_(&data->table, od);
+        if (ls_arp_record) {
+            hmap_remove(&data->table.entries, &ls_arp_record->key_node);
+            hmapx_add(&data->trk_data.deleted, ls_arp_record);
+        }
+    }
+
+    if (ls_arp_has_tracked_data(&data->trk_data)) {
+        return EN_HANDLED_UPDATED;
+    }
+
+    return EN_HANDLED_UNCHANGED;
+}
+
+static void
+nat_odmap_create(struct lr_nat_record *lrnat_rec,
+                 struct hmapx *odmap)
+{
+    for (size_t i = 0; i < lrnat_rec->n_nat_entries; i++) {
+        const struct ovn_nat *nat_entry = &lrnat_rec->nat_entries[i];
+
+        if (is_centralized_nat_record(nat_entry)) {
+            hmapx_add(odmap, nat_entry->l3dgw_port->peer->od);
+        }
+    }
+}
+
+enum engine_input_handler_result
+ls_arp_lr_nat_handler(struct engine_node *node, void *data_)
+{
+    struct ed_type_lr_nat_data *lr_nat_data =
+        engine_get_input_data("lr_nat", node);
+    struct ls_arp_input input_data = ls_arp_get_input_data(node);
+
+    if (!lr_nat_has_tracked_data(&lr_nat_data->trk_data)) {
+        return EN_UNHANDLED;
+    }
+
+    struct ed_type_ls_arp *data = data_;
+
+    struct hmapx_node *hmapx_node;
+    struct ls_arp_record *ls_arp_record;
+    HMAPX_FOR_EACH (hmapx_node, &lr_nat_data->trk_data.crupdated) {
+        struct lr_nat_record *nat_record_p = hmapx_node->data;
+
+        struct hmapx ls_links_map = HMAPX_INITIALIZER(&ls_links_map);
+        nat_odmap_create(nat_record_p, &ls_links_map);
+
+        LS_ARP_TABLE_FOR_EACH (ls_arp_record, &data->table) {
+            struct hmapx_node *nr_node =
+                hmapx_find(&ls_arp_record->nat_records, nat_record_p);
+
+            if (nr_node) {
+                hmapx_add(&data->trk_data.crupdated, ls_arp_record);
+                hmapx_delete(&ls_arp_record->nat_records, nr_node);
+            }
+        }
+
+        struct hmapx_node *crupdated_ls_hmapx;
+        HMAPX_FOR_EACH (crupdated_ls_hmapx, &ls_links_map) {
+            struct ovn_datapath *crupdated_ls = crupdated_ls_hmapx->data;
+            ls_arp_record =
+                ls_arp_record_lookup_by_od_(&data->table, crupdated_ls);
+
+            if (!ls_arp_record) {
+                ls_arp_record = ls_arp_record_create(&data->table,
+                                                     crupdated_ls,
+                                                     input_data.lr_nats);
+            }
+
+            hmapx_add(&data->trk_data.crupdated, ls_arp_record);
+            hmapx_add(&ls_arp_record->nat_records, nat_record_p);
+        }
+        hmapx_destroy(&ls_links_map);
+    }
+
+    HMAPX_FOR_EACH (hmapx_node, &lr_nat_data->trk_data.deleted) {
+        struct lr_nat_record *nr_cur = hmapx_node->data;
+
+        struct ls_arp_record *ar;
+        LS_ARP_TABLE_FOR_EACH (ar, &data->table) {
+            struct hmapx_node *nr_node = hmapx_find(&ar->nat_records, nr_cur);
+
+            if (nr_node) {
+                hmapx_add(&data->trk_data.crupdated, ar);
+                hmapx_delete(&ar->nat_records, nr_node);
+            }
+        }
+    }
+
+    if (ls_arp_has_tracked_data(&data->trk_data)) {
+        return EN_HANDLED_UPDATED;
+    }
+
+    return EN_HANDLED_UNCHANGED;
+}

lib/stopwatch-names.h
@@ -34,6 +34,7 @@
 #define LR_NAT_RUN_STOPWATCH_NAME "lr_nat_run"
 #define LR_STATEFUL_RUN_STOPWATCH_NAME "lr_stateful"
 #define LS_STATEFUL_RUN_STOPWATCH_NAME "ls_stateful"
+#define LS_ARP_RUN_STOPWATCH_NAME "ls_arp"
 #define ADVERTISED_ROUTE_SYNC_RUN_STOPWATCH_NAME "advertised_route_sync"
 #define LEARNED_ROUTE_SYNC_RUN_STOPWATCH_NAME "learned_route_sync"
 #define DYNAMIC_ROUTES_RUN_STOPWATCH_NAME "dynamic_routes"

northd/automake.mk
@@ -44,6 +44,8 @@ northd_ovn_northd_SOURCES = \
 	northd/en-lr-stateful.h \
 	northd/en-ls-stateful.c \
 	northd/en-ls-stateful.h \
+	northd/en-ls-arp.c \
+	northd/en-ls-arp.h \
 	northd/en-sampling-app.c \
 	northd/en-sampling-app.h \
 	northd/en-acl-ids.c \

northd/en-lflow.c
@@ -23,6 +23,7 @@
 #include "en-lr-nat.h"
 #include "en-lr-stateful.h"
 #include "en-ls-stateful.h"
+#include "en-ls-arp.h"
 #include "en-multicast.h"
 #include "en-northd.h"
 #include "en-meters.h"
@@ -62,6 +63,8 @@ lflow_get_input_data(struct engine_node *node,
         engine_get_input_data("lr_stateful", node);
     struct ed_type_ls_stateful *ls_stateful_data =
         engine_get_input_data("ls_stateful", node);
+    struct ed_type_ls_arp *ls_arp_data =
+        engine_get_input_data("ls_arp", node);
     struct multicast_igmp_data *multicat_igmp_data =
         engine_get_input_data("multicast_igmp", node);
     struct ic_learned_svc_monitors_data *ic_learned_svc_monitors_data =
@@ -86,6 +89,7 @@ lflow_get_input_data(struct engine_node *node,
     lflow_input->ls_port_groups = &pg_data->ls_port_groups;
     lflow_input->lr_stateful_table = &lr_stateful_data->table;
     lflow_input->ls_stateful_table = &ls_stateful_data->table;
+    lflow_input->ls_arp_table = &ls_arp_data->table;
     lflow_input->meter_groups = &sync_meters_data->meter_groups;
     lflow_input->lb_datapaths_map = &northd_data->lb_datapaths_map;
     lflow_input->local_svc_monitors_map =
@@ -220,6 +224,31 @@ lflow_ls_stateful_handler(struct engine_node *node, void *data)
                                           &ls_sful_data->trk_data,
                                           &lflow_input,
                                           lflow_data->lflow_table)) {
+        return EN_UNHANDLED;
+    }
+
+    return EN_HANDLED_UPDATED;
+}
+
+enum engine_input_handler_result
+lflow_ls_arp_handler(struct engine_node *node, void *data)
+{
+    struct ed_type_ls_arp *ls_arp_data =
+        engine_get_input_data("ls_arp", node);
+
+    if (!ls_arp_has_tracked_data(&ls_arp_data->trk_data)) {
+        return EN_UNHANDLED;
+    }
+
+    const struct engine_context *eng_ctx = engine_get_context();
+    struct lflow_data *lflow_data = data;
+    struct lflow_input lflow_input;
+
+    lflow_get_input_data(node, &lflow_input);
+    if (!lflow_handle_ls_arp_changes(eng_ctx->ovnsb_idl_txn,
+                                     &ls_arp_data->trk_data,
+                                     &lflow_input,
+                                     lflow_data->lflow_table)) {
         return EN_UNHANDLED;
     }
 

northd/en-lflow.h
@@ -25,6 +25,8 @@ lflow_lr_stateful_handler(struct engine_node *, void *data);
 enum engine_input_handler_result
 lflow_ls_stateful_handler(struct engine_node *node, void *data);
 enum engine_input_handler_result
+lflow_ls_arp_handler(struct engine_node *, void *);
+enum engine_input_handler_result
 lflow_multicast_igmp_handler(struct engine_node *node, void *data);
 enum engine_input_handler_result
 lflow_group_ecmp_route_change_handler(struct engine_node *node, void *data);

northd/en-ls-arp.h
@@ -1,0 +1,86 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef EN_LS_ARP_H
+#define EN_LS_ARP_H 1
+
+/* OVS includes. */
+#include "lib/hmapx.h"
+#include "openvswitch/hmap.h"
+
+/* OVN includes. */
+#include "lib/inc-proc-eng.h"
+#include "lib/ovn-nb-idl.h"
+#include "lib/ovn-sb-idl.h"
+#include "lib/ovn-util.h"
+#include "lib/stopwatch-names.h"
+
+struct lflow_ref;
+
+struct ls_arp_record {
+    struct hmap_node key_node;
+
+    /* UUID of the NB Logical switch. */
+    struct uuid nbs_uuid;
+
+    /* Index of logical switch item in northd. */
+    size_t ls_index;
+
+    /* 'lflow_ref' is used to reference logical flows generated for
+     * this ls_arp record. */
+    struct lflow_ref *lflow_ref;
+
+    /* lr_nat_record ptrs that trigger this od to rebuild lflow. */
+    struct hmapx nat_records;
+};
+
+struct ls_arp_table {
+    struct hmap entries;
+};
+
+#define LS_ARP_TABLE_FOR_EACH(LS_ARP_REC, TABLE) \
+    HMAP_FOR_EACH (LS_ARP_REC, key_node, \
+                   &(TABLE)->entries)
+
+#define LS_ARP_TABLE_FOR_EACH_IN_P(LS_ARP_REC, JOBID, TABLE) \
+    HMAP_FOR_EACH_IN_PARALLEL (LS_ARP_REC, key_node, JOBID, \
+                               &(TABLE)->entries)
+
+struct ls_arp_tracked_data {
+    struct hmapx crupdated;
+    struct hmapx deleted;
+};
+
+struct ed_type_ls_arp {
+    struct ls_arp_table table;
+    struct ls_arp_tracked_data trk_data;
+};
+
+void *en_ls_arp_init(struct engine_node *, struct engine_arg *);
+void en_ls_arp_cleanup(void *);
+void en_ls_arp_clear_tracked_data(void *);
+enum engine_node_state en_ls_arp_run(struct engine_node *, void *);
+
+enum engine_input_handler_result
+ls_arp_lr_nat_handler(struct engine_node *, void *);
+enum engine_input_handler_result
+ls_arp_northd_handler(struct engine_node *, void *);
+
+static inline bool
+ls_arp_has_tracked_data(struct ls_arp_tracked_data *trk_data) {
+    return !hmapx_is_empty(&trk_data->crupdated) ||
+           !hmapx_is_empty(&trk_data->deleted);
+}
+
+#endif /* EN_LS_ARP_H */

northd/inc-proc-northd.c
@@ -34,6 +34,7 @@
 #include "en-lr-stateful.h"
 #include "en-lr-nat.h"
 #include "en-ls-stateful.h"
+#include "en-ls-arp.h"
 #include "en-multicast.h"
 #include "en-northd.h"
 #include "en-lflow.h"
@@ -174,6 +175,7 @@ static ENGINE_NODE(lb_data, CLEAR_TRACKED_DATA);
 static ENGINE_NODE(lr_nat, CLEAR_TRACKED_DATA);
 static ENGINE_NODE(lr_stateful, CLEAR_TRACKED_DATA);
 static ENGINE_NODE(ls_stateful, CLEAR_TRACKED_DATA);
+static ENGINE_NODE(ls_arp, CLEAR_TRACKED_DATA);
 static ENGINE_NODE(route_policies);
 static ENGINE_NODE(routes);
 static ENGINE_NODE(bfd);
@@ -305,6 +307,9 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                      ls_stateful_port_group_handler);
     engine_add_input(&en_ls_stateful, &en_nb_acl, ls_stateful_acl_handler);
 
+    engine_add_input(&en_ls_arp, &en_lr_nat, ls_arp_lr_nat_handler);
+    engine_add_input(&en_ls_arp, &en_northd, ls_arp_northd_handler);
+
     engine_add_input(&en_mac_binding_aging, &en_sb_mac_binding, NULL);
     engine_add_input(&en_mac_binding_aging, &en_northd, NULL);
     engine_add_input(&en_mac_binding_aging, &en_mac_binding_aging_waker, NULL);
@@ -409,6 +414,7 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_lflow, &en_port_group, engine_noop_handler);
     engine_add_input(&en_lflow, &en_lr_stateful, lflow_lr_stateful_handler);
     engine_add_input(&en_lflow, &en_ls_stateful, lflow_ls_stateful_handler);
+    engine_add_input(&en_lflow, &en_ls_arp, lflow_ls_arp_handler);
     engine_add_input(&en_lflow, &en_multicast_igmp,
                      lflow_multicast_igmp_handler);
     engine_add_input(&en_lflow, &en_sb_acl_id, NULL);

commit 23dd0901ba56beb857eff140ce52ffe7289696c8

northd: Fix for distributed dnat_and_snat ARP resolution.
NAT records with only distributed dnat_and_snat configured were missing
from logical switch ARP incremental processing node data, which was incorrect behavior.

Reported-at: https://mail.openvswitch.org/pipermail/ovs-dev/2026-January/429694.html
Fixes: 1b4058b9162c ("northd: Process external arps on ha chassis.")
Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/en-ls-arp.c
@@ -71,13 +71,12 @@ ls_arp_table_clear(struct ls_arp_table *table)
 }
 
 static inline bool
-is_centralized_nat_record(const struct ovn_nat *nat_entry)
+is_nat_dgp_connected(const struct ovn_nat *nat_entry)
 {
     return nat_entry->is_valid
            && nat_entry->l3dgw_port
            && nat_entry->l3dgw_port->peer
-           && nat_entry->l3dgw_port->peer->od
-           && !nat_entry->is_distributed;
+           && nat_entry->l3dgw_port->peer->od;
 }
 
 static void
@@ -98,9 +97,10 @@ nat_record_data_create(struct ls_arp_record *ls_arp_record,
         for (size_t i = 0; i < lrnat_rec->n_nat_entries; i++) {
             const struct ovn_nat *nat_entry = &lrnat_rec->nat_entries[i];
 
-            if (is_centralized_nat_record(nat_entry)) {
+            if (is_nat_dgp_connected(nat_entry)) {
                 hmapx_add(&ls_arp_record->nat_records,
                           (struct lrnat_rec *) lrnat_rec);
+                continue;
             }
         }
     }
@@ -283,7 +283,7 @@ nat_odmap_create(struct lr_nat_record *lrnat_rec,
     for (size_t i = 0; i < lrnat_rec->n_nat_entries; i++) {
         const struct ovn_nat *nat_entry = &lrnat_rec->nat_entries[i];
 
-        if (is_centralized_nat_record(nat_entry)) {
+        if (is_nat_dgp_connected(nat_entry)) {
             hmapx_add(odmap, nat_entry->l3dgw_port->peer->od);
         }
     }

tests/system-ovn.at
@@ -20531,3 +20531,57 @@ OVS_TRAFFIC_VSWITCHD_STOP(["/.*error receiving.*/d
 /.*terminating with signal 15.*/d"])
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([NAT with only dnat_and_snat NAT configured: IPv4])
+AT_KEYWORDS([ovnnat])
+
+ovn_start
+OVS_TRAFFIC_VSWITCHD_START()
+
+ADD_BR([br-int])
+ADD_BR([br-ext])
+
+ovs-ofctl add-flow br-ext action=normal
+# Set external-ids in br-int needed for ovn-controller
+ovs-vsctl \
+        -- set Open_vSwitch . external-ids:system-id=hv1 \
+        -- set Open_vSwitch . external-ids:ovn-remote=unix:$ovs_base/ovn-sb/ovn-sb.sock \
+        -- set Open_vSwitch . external-ids:ovn-encap-type=geneve \
+        -- set Open_vSwitch . external-ids:ovn-encap-ip=169.0.0.1 \
+        -- set Open_vSwitch . external-ids:ovn-bridge-mappings=phynet:br-ext
+        -- set bridge br-int fail-mode=secure other-config:disable-in-band=true
+
+# Start ovn-controller
+start_daemon ovn-controller
+
+check ovn-nbctl lr-add lr1
+check ovn-nbctl ls-add sw0
+check ovn-nbctl ls-add public
+
+check ovn-nbctl lrp-add lr1 rp-sw0 00:00:01:01:02:03 192.168.1.1/24
+check ovn-nbctl lrp-add lr1 rp-public 00:00:02:01:02:03 172.16.1.1/24
+
+check ovn-nbctl lsp-add-router-port sw0 sw0-rp rp-sw0
+check ovn-nbctl lsp-add-router-port public public-rp rp-public
+
+check ovn-nbctl lsp-add-localnet-port public localnet phynet
+
+NS_CHECK_CONNECTIVITY([ext-foo], [sw01-x], 172.16.0.1)
+
+# Create distributed nat and set gw chassis to non-exisrting
+# one - to check distributed NAT on non gw-chassis.
+check ovn-nbctl lrp-set-gateway-chassis rp-public hv2
+check ovn-nbctl lr-nat-del lr1
+check ovn-nbctl lr-nat-add lr1 dnat_and_snat 172.16.0.1 192.168.1.2 sw01-x 00:00:04:00:00:01
+
+NS_EXEC([ext-foo], [ip r add 172.16.0.1/32 dev ext-foo])
+NS_CHECK_CONNECTIVITY([ext-foo], [sw01-x], 172.16.0.1)
+
+OVN_CLEANUP_CONTROLLER([hv1])
+OVN_CLEANUP_NORTHD
+as
+OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
+/connection dropped.*/d"])
+AT_CLEANUP
+])