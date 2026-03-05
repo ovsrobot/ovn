ovsrobot/ovn

commit 7bb513bcfda380657efe2888b57dabcbd3544536

northd: Refactor datapath syncing.
In current OVN, the en-northd node (via code in northd.c) takes full
control of syncing logical switches and logical routers with southbound
datapath bindings. This is fine, so long as:
1) These are the only datapath types to sync.
2) northd will always be the arbiter of datapath syncing.

However, future commits will introduce new types of datapaths. These are
not good fits for the en-northd node, since they have completely
independent processing rules, and trying to shoehorn them into a struct
ovn_datapath would be wasteful.

This patch introduces a new way of syncing datapaths. Each datapath type
has a node that is responsible for creating an ovn_unsynced_datapath_map
for the type of datapath it creates. Then a type-agnostic datapath
syncing node syncs these datapaths with the southbound Datapath_Binding
type. Then, these synced datapaths are fed back into type-specific
datapath nodes, which translate these synced datapaths into specific
types.

Nodes can then use these as inputs if they need synced datapaths (i.e. a
northbound type with its corresponding southbound type). In this case,
en_northd uses the synced logical switch and logical router types in
order to create its ovn_datapath structures.

Doing this will provide an easy way to sync new datapath types to the
southbound database.

Acked-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-datapath-logical-router.c
@@ -1,0 +1,188 @@
+/*
+ * Copyright (c) 2025, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "openvswitch/hmap.h"
+#include "openvswitch/util.h"
+#include "openvswitch/vlog.h"
+
+#include "ovn-nb-idl.h"
+#include "aging.h"
+#include "datapath-sync.h"
+#include "en-datapath-logical-router.h"
+#include "ovn-util.h"
+
+VLOG_DEFINE_THIS_MODULE(en_datapath_logical_router);
+
+void *
+en_datapath_logical_router_init(struct engine_node *node OVS_UNUSED,
+                                struct engine_arg *args OVS_UNUSED)
+{
+    struct ovn_unsynced_datapath_map *map = xmalloc(sizeof *map);
+    ovn_unsynced_datapath_map_init(map, DP_ROUTER);
+    return map;
+}
+
+enum engine_node_state
+en_datapath_logical_router_run(struct engine_node *node , void *data)
+{
+    const struct nbrec_logical_router_table *nb_lr_table =
+        EN_OVSDB_GET(engine_get_input("NB_logical_router", node));
+
+    struct ovn_unsynced_datapath_map *map = data;
+
+    ovn_unsynced_datapath_map_destroy(map);
+    ovn_unsynced_datapath_map_init(map, DP_ROUTER);
+
+    const struct nbrec_logical_router *nbr;
+    NBREC_LOGICAL_ROUTER_TABLE_FOR_EACH (nbr, nb_lr_table) {
+        if (nbr->enabled && !(*nbr->enabled)) {
+            continue;
+        }
+        struct ovn_unsynced_datapath *dp =
+            ovn_unsynced_datapath_alloc(nbr->name, DP_ROUTER,
+                                        smap_get_int(&nbr->options,
+                                                     "requested-tnl-key", 0),
+                                        &nbr->header_);
+
+        smap_add(&dp->external_ids, "name", dp->name);
+        const char *neutron_router = smap_get(&nbr->options,
+                                               "neutron:router_name");
+        if (neutron_router && neutron_router[0]) {
+            smap_add(&dp->external_ids, "name2", neutron_router);
+        }
+
+        int64_t ct_zone_limit = ovn_smap_get_llong(&nbr->options,
+                                                   "ct-zone-limit", -1);
+        if (ct_zone_limit > 0) {
+            smap_add_format(&dp->external_ids, "ct-zone-limit", "%"PRId64,
+                            ct_zone_limit);
+        }
+
+        int nat_default_ct = smap_get_int(&nbr->options,
+                                          "snat-ct-zone", -1);
+        if (nat_default_ct >= 0) {
+            smap_add_format(&dp->external_ids, "snat-ct-zone", "%d",
+                            nat_default_ct);
+        }
+
+        bool learn_from_arp_request =
+            smap_get_bool(&nbr->options, "always_learn_from_arp_request",
+                          true);
+        if (!learn_from_arp_request) {
+            smap_add(&dp->external_ids, "always_learn_from_arp_request",
+                     "false");
+        }
+
+        /* For timestamp refreshing, the smallest threshold of the option is
+         * set to SB to make sure all entries are refreshed in time.
+         * This approach simplifies processing in ovn-controller, but it
+         * may be enhanced, if necessary, to parse the complete CIDR-based
+         * threshold configurations to SB to reduce unnecessary refreshes. */
+        uint32_t age_threshold = min_mac_binding_age_threshold(
+                                       smap_get(&nbr->options,
+                                               "mac_binding_age_threshold"));
+        if (age_threshold) {
+            smap_add_format(&dp->external_ids, "mac_binding_age_threshold",
+                            "%u", age_threshold);
+        }
+
+        /* For backwards-compatibility, also store the NB UUID in
+         * external-ids:logical-router. This is useful if ovn-controller
+         * has not updated and expects this to be where to find the
+         * UUID.
+         */
+        smap_add_format(&dp->external_ids, "logical-router", UUID_FMT,
+                        UUID_ARGS(&nbr->header_.uuid));
+
+        hmap_insert(&map->dps, &dp->hmap_node, uuid_hash(&nbr->header_.uuid));
+    }
+
+    return EN_UPDATED;
+}
+
+void
+en_datapath_logical_router_cleanup(void *data)
+{
+    struct ovn_unsynced_datapath_map *map = data;
+    ovn_unsynced_datapath_map_destroy(map);
+}
+
+static void
+synced_logical_router_map_init(
+    struct ovn_synced_logical_router_map *router_map)
+{
+    *router_map = (struct ovn_synced_logical_router_map) {
+        .synced_routers = HMAP_INITIALIZER(&router_map->synced_routers),
+    };
+}
+
+static void
+synced_logical_router_map_destroy(
+    struct ovn_synced_logical_router_map *router_map)
+{
+    struct ovn_synced_logical_router *lr;
+    HMAP_FOR_EACH_POP (lr, hmap_node, &router_map->synced_routers) {
+        free(lr);
+    }
+    hmap_destroy(&router_map->synced_routers);
+}
+
+void *
+en_datapath_synced_logical_router_init(struct engine_node *node OVS_UNUSED,
+                                      struct engine_arg *args OVS_UNUSED)
+{
+    struct ovn_synced_logical_router_map *router_map;
+    router_map = xmalloc(sizeof *router_map);
+    synced_logical_router_map_init(router_map);
+
+    return router_map;
+}
+
+enum engine_node_state
+en_datapath_synced_logical_router_run(struct engine_node *node , void *data)
+{
+    const struct ovn_synced_datapaths *dps =
+        engine_get_input_data("datapath_sync", node);
+    struct ovn_synced_logical_router_map *router_map = data;
+
+    synced_logical_router_map_destroy(router_map);
+    synced_logical_router_map_init(router_map);
+
+    struct ovn_synced_datapath *sdp;
+    HMAP_FOR_EACH (sdp, hmap_node, &dps->synced_dps) {
+        if (sdp->nb_row->table->class_ != &nbrec_table_logical_router) {
+            continue;
+        }
+        struct ovn_synced_logical_router *lr = xmalloc(sizeof *lr);
+        *lr = (struct ovn_synced_logical_router) {
+            .nb = CONTAINER_OF(sdp->nb_row, struct nbrec_logical_router,
+                               header_),
+            .sb = sdp->sb_dp,
+        };
+        hmap_insert(&router_map->synced_routers, &lr->hmap_node,
+                    uuid_hash(&lr->nb->header_.uuid));
+    }
+
+    return EN_UPDATED;
+}
+
+void en_datapath_synced_logical_router_cleanup(void *data)
+{
+    struct ovn_synced_logical_router_map *router_map = data;
+    synced_logical_router_map_destroy(router_map);
+}

northd/en-datapath-logical-switch.c
@@ -1,0 +1,182 @@
+/*
+ * Copyright (c) 2025, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "openvswitch/hmap.h"
+#include "openvswitch/util.h"
+#include "openvswitch/vlog.h"
+
+#include "inc-proc-eng.h"
+#include "ovn-nb-idl.h"
+#include "datapath-sync.h"
+#include "en-datapath-logical-switch.h"
+#include "en-global-config.h"
+#include "ovn-util.h"
+
+VLOG_DEFINE_THIS_MODULE(en_datapath_logical_switch);
+
+void *
+en_datapath_logical_switch_init(struct engine_node *node OVS_UNUSED,
+                                struct engine_arg *args OVS_UNUSED)
+{
+    struct ovn_unsynced_datapath_map *map = xmalloc(sizeof *map);
+    ovn_unsynced_datapath_map_init(map, DP_SWITCH);
+    return map;
+}
+
+enum engine_node_state
+en_datapath_logical_switch_run(struct engine_node *node , void *data)
+{
+    const struct nbrec_logical_switch_table *nb_ls_table =
+        EN_OVSDB_GET(engine_get_input("NB_logical_switch", node));
+    const struct ed_type_global_config *global_config =
+        engine_get_input_data("global_config", node);
+
+    struct ovn_unsynced_datapath_map *map = data;
+
+    ovn_unsynced_datapath_map_destroy(map);
+    ovn_unsynced_datapath_map_init(map, DP_SWITCH);
+
+    const struct nbrec_logical_switch *nbs;
+    NBREC_LOGICAL_SWITCH_TABLE_FOR_EACH (nbs, nb_ls_table) {
+        uint32_t requested_tunnel_key = smap_get_int(&nbs->other_config,
+                                                     "requested-tnl-key", 0);
+        const char *ts = smap_get(&nbs->other_config, "interconn-ts");
+
+        if (!ts && global_config->vxlan_mode &&
+            requested_tunnel_key >= 1 << 12) {
+            static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 1);
+            VLOG_WARN_RL(&rl, "Tunnel key %"PRIu32" for datapath %s is "
+                         "incompatible with VXLAN", requested_tunnel_key,
+                         nbs->name);
+            requested_tunnel_key = 0;
+        }
+
+        struct ovn_unsynced_datapath *dp
+            = ovn_unsynced_datapath_alloc(nbs->name, DP_SWITCH,
+                                          requested_tunnel_key, &nbs->header_);
+
+        smap_add(&dp->external_ids, "name", dp->name);
+        const char *neutron_network = smap_get(&nbs->other_config,
+                                               "neutron:network_name");
+        if (neutron_network && neutron_network[0]) {
+            smap_add(&dp->external_ids, "name2", neutron_network);
+        }
+
+        int64_t ct_zone_limit = ovn_smap_get_llong(&nbs->other_config,
+                                                   "ct-zone-limit", -1);
+        if (ct_zone_limit > 0) {
+            smap_add_format(&dp->external_ids, "ct-zone-limit", "%"PRId64,
+                            ct_zone_limit);
+        }
+
+        if (ts) {
+            smap_add(&dp->external_ids, "interconn-ts", ts);
+        }
+
+        uint32_t age_threshold = smap_get_uint(&nbs->other_config,
+                                               "fdb_age_threshold", 0);
+        if (age_threshold) {
+            smap_add_format(&dp->external_ids, "fdb_age_threshold",
+                            "%u", age_threshold);
+        }
+
+        /* For backwards-compatibility, also store the NB UUID in
+         * external-ids:logical-switch. This is useful if ovn-controller
+         * has not updated and expects this to be where to find the
+         * UUID.
+         */
+        smap_add_format(&dp->external_ids, "logical-switch", UUID_FMT,
+                        UUID_ARGS(&nbs->header_.uuid));
+
+        hmap_insert(&map->dps, &dp->hmap_node, uuid_hash(&nbs->header_.uuid));
+    }
+
+    return EN_UPDATED;
+}
+
+void
+en_datapath_logical_switch_cleanup(void *data)
+{
+    struct ovn_unsynced_datapath_map *map = data;
+    ovn_unsynced_datapath_map_destroy(map);
+}
+
+static void
+synced_logical_switch_map_init(
+    struct ovn_synced_logical_switch_map *switch_map)
+{
+    *switch_map = (struct ovn_synced_logical_switch_map) {
+        .synced_switches = HMAP_INITIALIZER(&switch_map->synced_switches),
+    };
+}
+
+static void
+synced_logical_switch_map_destroy(
+    struct ovn_synced_logical_switch_map *switch_map)
+{
+    struct ovn_synced_logical_switch *ls;
+    HMAP_FOR_EACH_POP (ls, hmap_node, &switch_map->synced_switches) {
+        free(ls);
+    }
+    hmap_destroy(&switch_map->synced_switches);
+}
+
+void *
+en_datapath_synced_logical_switch_init(struct engine_node *node OVS_UNUSED,
+                                      struct engine_arg *args OVS_UNUSED)
+{
+    struct ovn_synced_logical_switch_map *switch_map;
+    switch_map = xmalloc(sizeof *switch_map);
+    synced_logical_switch_map_init(switch_map);
+
+    return switch_map;
+}
+
+enum engine_node_state
+en_datapath_synced_logical_switch_run(struct engine_node *node , void *data)
+{
+    const struct ovn_synced_datapaths *dps =
+        engine_get_input_data("datapath_sync", node);
+    struct ovn_synced_logical_switch_map *switch_map = data;
+
+    synced_logical_switch_map_destroy(switch_map);
+    synced_logical_switch_map_init(switch_map);
+
+    struct ovn_synced_datapath *sdp;
+    HMAP_FOR_EACH (sdp, hmap_node, &dps->synced_dps) {
+        if (sdp->nb_row->table->class_ != &nbrec_table_logical_switch) {
+            continue;
+        }
+        struct ovn_synced_logical_switch *lsw = xmalloc(sizeof *lsw);
+        *lsw = (struct ovn_synced_logical_switch) {
+            .nb = CONTAINER_OF(sdp->nb_row, struct nbrec_logical_switch,
+                               header_),
+            .sb = sdp->sb_dp,
+        };
+        hmap_insert(&switch_map->synced_switches, &lsw->hmap_node,
+                    uuid_hash(&lsw->nb->header_.uuid));
+    }
+
+    return EN_UPDATED;
+}
+
+void en_datapath_synced_logical_switch_cleanup(void *data)
+{
+    struct ovn_synced_logical_switch_map *switch_map = data;
+    synced_logical_switch_map_destroy(switch_map);
+}

northd/automake.mk
@@ -3,11 +3,19 @@ bin_PROGRAMS += northd/ovn-northd
 northd_ovn_northd_SOURCES = \
 	northd/aging.c \
 	northd/aging.h \
+	northd/datapath-sync.c \
+	northd/datapath-sync.h \
 	northd/debug.c \
 	northd/debug.h \
 	northd/northd.c \
 	northd/northd.h \
 	northd/ovn-northd.c \
+	northd/en-datapath-logical-switch.c \
+	northd/en-datapath-logical-switch.h \
+	northd/en-datapath-logical-router.c \
+	northd/en-datapath-logical-router.h \
+	northd/en-datapath-sync.c \
+	northd/en-datapath-sync.h \
 	northd/en-ecmp-nexthop.c \
 	northd/en-ecmp-nexthop.h \
 	northd/en-global-config.c \

northd/datapath-sync.c
@@ -1,0 +1,90 @@
+/* Copyright (c) 2025, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include "datapath-sync.h"
+
+static const char *ovn_datapath_strings[] = {
+    [DP_SWITCH] = "logical-switch",
+    [DP_ROUTER] = "logical-router",
+    [DP_MAX] = "<invalid>",
+};
+
+enum ovn_datapath_type
+ovn_datapath_type_from_string(const char *type_str)
+{
+    for (enum ovn_datapath_type i = DP_SWITCH; i < DP_MAX; i++) {
+        if (!strcmp(type_str, ovn_datapath_strings[i])) {
+            return i;
+        }
+    }
+
+    return DP_MAX;
+}
+
+const char *
+ovn_datapath_type_to_string(enum ovn_datapath_type dp_type)
+{
+    if (dp_type > DP_MAX) {
+        dp_type = DP_MAX;
+    }
+    return ovn_datapath_strings[dp_type];
+}
+
+struct ovn_unsynced_datapath *
+ovn_unsynced_datapath_alloc(const char *name, enum ovn_datapath_type type,
+                            uint32_t requested_tunnel_key,
+                            const struct ovsdb_idl_row *nb_row)
+{
+    struct ovn_unsynced_datapath *dp = xmalloc(sizeof *dp);
+    *dp = (struct ovn_unsynced_datapath) {
+        .name = xstrdup(name),
+        .type = type,
+        .requested_tunnel_key = requested_tunnel_key,
+        .nb_row = nb_row,
+        .external_ids = SMAP_INITIALIZER(&dp->external_ids),
+    };
+
+    return dp;
+}
+
+void
+ovn_unsynced_datapath_destroy(struct ovn_unsynced_datapath *dp)
+{
+    free(dp->name);
+    smap_destroy(&dp->external_ids);
+}
+
+void
+ovn_unsynced_datapath_map_init(struct ovn_unsynced_datapath_map *map,
+                               enum ovn_datapath_type dp_type)
+{
+    *map = (struct ovn_unsynced_datapath_map) {
+        .dps = HMAP_INITIALIZER(&map->dps),
+        .dp_type = dp_type,
+    };
+}
+
+void
+ovn_unsynced_datapath_map_destroy(struct ovn_unsynced_datapath_map *map)
+{
+    struct ovn_unsynced_datapath *dp;
+    HMAP_FOR_EACH_POP (dp, hmap_node, &map->dps) {
+        ovn_unsynced_datapath_destroy(dp);
+        free(dp);
+    }
+    hmap_destroy(&map->dps);
+}

northd/datapath-sync.h
@@ -1,0 +1,86 @@
+/* Copyright (c) 2025, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef DATAPATH_SYNC_H
+#define DATAPATH_SYNC_H 1
+
+#include "openvswitch/hmap.h"
+#include "smap.h"
+
+/* Datapath syncing API. This file consists of utility functions
+ * that can be used when syncing northbound datapath types (e.g.
+ * Logical_Router and Logical_Switch) to southbound Datapath_Bindings.
+ *
+ * The basic flow of data is as such.
+ * 1. A northbound type is converted into an ovn_unsynced_datapath.
+ * All ovn_unsynced_datapaths are placed into an ovn_unsynced_datapath_map.
+ * 2. The en_datapath_sync node takes all of the maps in as input and
+ * syncs them with southbound datapath bindings. This includes allocating
+ * tunnel keys across all datapath types. The output of this node is
+ * ovn_synced_datapaths, which contains a list of all synced datapaths.
+ * 3. A northbound type-aware node then takes the ovn_synced_datapaths,
+ * and decodes the generic synced datapaths back into a type-specific
+ * version (e.g. ovn_synced_logical_router). Later nodes can then consume
+ * these type-specific synced datapath types in order to perform
+ * further processing.
+ */
+
+enum ovn_datapath_type {
+    DP_SWITCH,
+    DP_ROUTER,
+    DP_MAX,
+};
+
+enum ovn_datapath_type ovn_datapath_type_from_string(const char *type_str);
+const char *ovn_datapath_type_to_string(enum ovn_datapath_type dp_type);
+
+/* Represents a datapath from the northbound database
+ * that has not yet been synced with the southbound database.
+ */
+struct ovn_unsynced_datapath {
+    struct hmap_node hmap_node;
+    char *name;
+    enum ovn_datapath_type type;
+    uint32_t requested_tunnel_key;
+    struct smap external_ids;
+    const struct ovsdb_idl_row *nb_row;
+};
+
+struct ovn_unsynced_datapath_map {
+    /* ovn_unsynced_datapath */
+    struct hmap dps;
+    enum ovn_datapath_type dp_type;
+};
+
+struct ovn_synced_datapath {
+    struct hmap_node hmap_node;
+    const struct ovsdb_idl_row *nb_row;
+    const struct sbrec_datapath_binding *sb_dp;
+};
+
+struct ovn_synced_datapaths {
+    struct hmap synced_dps;
+};
+
+struct ovn_unsynced_datapath *ovn_unsynced_datapath_alloc(
+    const char *name, enum ovn_datapath_type type,
+    uint32_t requested_tunnel_key, const struct ovsdb_idl_row *nb_row);
+void ovn_unsynced_datapath_destroy(struct ovn_unsynced_datapath *);
+
+void ovn_unsynced_datapath_map_init(struct ovn_unsynced_datapath_map *,
+                                    enum ovn_datapath_type);
+void ovn_unsynced_datapath_map_destroy(struct ovn_unsynced_datapath_map *);
+
+#endif /* DATAPATH_SYNC_H */

northd/en-datapath-logical-router.h
@@ -1,0 +1,48 @@
+/*
+ * Copyright (c) 2025, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef EN_DATAPATH_LOGICAL_ROUTER_H
+#define EN_DATAPATH_LOGICAL_ROUTER_H
+
+#include "lib/inc-proc-eng.h"
+#include "openvswitch/hmap.h"
+
+void *en_datapath_logical_router_init(struct engine_node *,
+                                      struct engine_arg *);
+
+enum engine_node_state en_datapath_logical_router_run(struct engine_node *,
+                                                      void *data);
+void en_datapath_logical_router_cleanup(void *data);
+
+struct ovn_synced_logical_router {
+    struct hmap_node hmap_node;
+    const struct nbrec_logical_router *nb;
+    const struct sbrec_datapath_binding *sb;
+};
+
+struct ovn_synced_logical_router_map {
+    struct hmap synced_routers;
+};
+
+void *en_datapath_synced_logical_router_init(struct engine_node *,
+                                             struct engine_arg *);
+
+enum engine_node_state en_datapath_synced_logical_router_run(
+    struct engine_node *, void *data);
+
+void en_datapath_synced_logical_router_cleanup(void *data);
+
+#endif /* EN_DATAPATH_LOGICAL_ROUTER_H */

northd/en-datapath-logical-switch.h
@@ -1,0 +1,47 @@
+/*
+ * Copyright (c) 2025, Red Hat, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef EN_DATAPATH_LOGICAL_SWITCH_H
+#define EN_DATAPATH_LOGICAL_SWITCH_H
+
+#include "lib/inc-proc-eng.h"
+#include "openvswitch/hmap.h"
+
+void *en_datapath_logical_switch_init(struct engine_node *,
+                                      struct engine_arg *);
+
+enum engine_node_state en_datapath_logical_switch_run(struct engine_node *,
+                                                      void *data);
+void en_datapath_logical_switch_cleanup(void *data);
+
+struct ovn_synced_logical_switch {
+    struct hmap_node hmap_node;
+    const struct nbrec_logical_switch *nb;
+    const struct sbrec_datapath_binding *sb;
+};
+
+struct ovn_synced_logical_switch_map {
+    struct hmap synced_switches;
+};
+
+void *en_datapath_synced_logical_switch_init(struct engine_node *,
+                                             struct engine_arg *);
+
+enum engine_node_state en_datapath_synced_logical_switch_run(
+    struct engine_node *, void *data);
+void en_datapath_synced_logical_switch_cleanup(void *data);
+
+#endif /* EN_DATAPATH_LOGICAL_SWITCH_H */

commit 8570f820e672a8d10dfca114c948c16879706737

northd: Get neutron values from proper column.
OpenStack Neutron may set specific external_ids on logical switches and
logical routers in order to propagate those values into the
corresponding southbound Datapath_Bindings. Specifically, OVN looks for

 * external-ids:neutron:network_name on logical switches, and
 * external-ids:neutron:router_name on logical routers.

When refactoring datapath syncing in OVN 25.09, we mistakenly started
reading these values from the wrong column. Specifically, we were
reading:

 * other_config:neutron:network_name on logical switches, and
 * options:neutron:router_name on logical routers.

This commit fixes the issue by reading from the external_ids column on
both logical switches and logical routers.

Fixes: 7bb513bcfda3 ("northd: Refactor datapath syncing.")
Reported-at: https://issues.redhat.com/browse/FDP-3286
Signed-off-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/en-datapath-logical-router.c
@@ -43,7 +43,7 @@ gather_external_ids(const struct nbrec_logical_router *nbr,
                     struct smap *external_ids)
 {
     smap_add(external_ids, "name", nbr->name);
-    const char *neutron_router = smap_get(&nbr->options,
+    const char *neutron_router = smap_get(&nbr->external_ids,
                                            "neutron:router_name");
     if (neutron_router && neutron_router[0]) {
         smap_add(external_ids, "name2", neutron_router);

northd/en-datapath-logical-switch.c
@@ -111,7 +111,7 @@ gather_external_ids(const struct nbrec_logical_switch *nbs,
                     struct smap *external_ids)
 {
     smap_add(external_ids, "name", nbs->name);
-    const char *neutron_network = smap_get(&nbs->other_config,
+    const char *neutron_network = smap_get(&nbs->external_ids,
                                            "neutron:network_name");
     if (neutron_network && neutron_network[0]) {
         smap_add(external_ids, "name2", neutron_network);