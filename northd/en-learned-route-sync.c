ovsrobot/ovn

commit 966ca1c919cedacc58108114f6bd0a3005425b45

northd: Handle learned routes.
Here we expand the previous routes-sync engine node to not only
advertise routes to the southbound table, but also learn received routes
from this table.

These routes are then passed to the same logic that connected and static
routes are using for flow generation.
However we prioritize these routes lower than connected or static routes
as information in cluster (for the same prefix length) should always be
more correct then learned routes.
This is also consistent with the default behaviour of phyiscal routers.

Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-learned-route-sync.c
@@ -1,0 +1,208 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+#include <stdbool.h>
+
+#include "openvswitch/vlog.h"
+#include "stopwatch.h"
+#include "northd.h"
+
+#include "en-learned-route-sync.h"
+#include "lib/stopwatch-names.h"
+#include "openvswitch/hmap.h"
+#include "ovn-util.h"
+
+VLOG_DEFINE_THIS_MODULE(en_learned_route_sync);
+
+static void
+routes_table_sync(
+    const struct sbrec_learned_route_table *sbrec_learned_route_table,
+    const struct hmap *parsed_routes,
+    const struct hmap *lr_ports,
+    const struct ovn_datapaths *lr_datapaths,
+    struct hmap *parsed_routes_out);
+
+bool
+learned_route_sync_northd_change_handler(struct engine_node *node,
+                                         void *data_ OVS_UNUSED)
+{
+    struct northd_data *northd_data = engine_get_input_data("northd", node);
+    if (!northd_has_tracked_data(&northd_data->trk_data)) {
+        return false;
+    }
+
+    /* This node uses the below data from the en_northd engine node.
+     * See (lr_stateful_get_input_data())
+     *   1. northd_data->lr_datapaths
+     *   2. northd_data->lr_ports
+     *      This data gets updated when a logical router or logical router port
+     *      is created or deleted.
+     *      Northd engine node presently falls back to full recompute when
+     *      this happens and so does this node.
+     *      Note: When we add I-P to the created/deleted logical routers or
+     *      logical router ports, we need to revisit this handler.
+     */
+
+    return true;
+}
+
+static void
+routes_sync_clear(struct learned_route_sync_data *data)
+{
+    struct parsed_route *r;
+    HMAP_FOR_EACH_POP (r, key_node, &data->parsed_routes) {
+        parsed_route_free(r);
+    }
+}
+
+static void
+routes_sync_init(struct learned_route_sync_data *data)
+{
+    hmap_init(&data->parsed_routes);
+}
+
+static void
+routes_sync_destroy(struct learned_route_sync_data *data)
+{
+    routes_sync_clear(data);
+    hmap_destroy(&data->parsed_routes);
+}
+
+void *
+en_learned_route_sync_init(struct engine_node *node OVS_UNUSED,
+                     struct engine_arg *arg OVS_UNUSED)
+{
+    struct learned_route_sync_data *data = xzalloc(sizeof *data);
+    routes_sync_init(data);
+    return data;
+}
+
+void
+en_learned_route_sync_cleanup(void *data)
+{
+    routes_sync_destroy(data);
+}
+
+void
+en_learned_route_sync_run(struct engine_node *node, void *data)
+{
+    routes_sync_clear(data);
+
+    struct learned_route_sync_data *routes_sync_data = data;
+    struct routes_data *routes_data
+        = engine_get_input_data("routes", node);
+    const struct sbrec_learned_route_table *sbrec_learned_route_table =
+        EN_OVSDB_GET(engine_get_input("SB_learned_route", node));
+    struct northd_data *northd_data = engine_get_input_data("northd", node);
+
+    stopwatch_start(LEARNED_ROUTE_SYNC_RUN_STOPWATCH_NAME, time_msec());
+
+    routes_table_sync(sbrec_learned_route_table,
+                      &routes_data->parsed_routes,
+                      &northd_data->lr_ports,
+                      &northd_data->lr_datapaths,
+                      &routes_sync_data->parsed_routes);
+
+    stopwatch_stop(LEARNED_ROUTE_SYNC_RUN_STOPWATCH_NAME, time_msec());
+    engine_set_node_state(node, EN_UPDATED);
+}
+
+
+static void
+parse_route_from_sbrec_route(struct hmap *parsed_routes_out,
+                             const struct hmap *lr_ports,
+                             const struct hmap *lr_datapaths,
+                             const struct sbrec_learned_route *route)
+{
+    const struct ovn_datapath *od = ovn_datapath_from_sbrec(
+        NULL, lr_datapaths, route->datapath);
+
+    if (!od || ovn_datapath_is_stale(od)) {
+        return;
+    }
+
+    /* Verify that the next hop is an IP address with an all-ones mask. */
+    struct in6_addr *nexthop = xmalloc(sizeof *nexthop);
+    unsigned int plen;
+    if (!ip46_parse_cidr(route->nexthop, nexthop, &plen)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "bad 'nexthop' %s in learned route "
+                     UUID_FMT, route->nexthop,
+                     UUID_ARGS(&route->header_.uuid));
+        free(nexthop);
+        return;
+    }
+    if ((IN6_IS_ADDR_V4MAPPED(nexthop) && plen != 32) ||
+        (!IN6_IS_ADDR_V4MAPPED(nexthop) && plen != 128)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "bad next hop mask %s in learned route "
+                     UUID_FMT, route->nexthop,
+                     UUID_ARGS(&route->header_.uuid));
+        free(nexthop);
+        return;
+    }
+
+    /* Parse ip_prefix */
+    struct in6_addr prefix;
+    if (!ip46_parse_cidr(route->ip_prefix, &prefix, &plen)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "bad 'ip_prefix' %s in learned route "
+                     UUID_FMT, route->ip_prefix,
+                     UUID_ARGS(&route->header_.uuid));
+        free(nexthop);
+        return;
+    }
+
+    /* Verify that ip_prefix and nexthop are on the same network. */
+    const char *lrp_addr_s = NULL;
+    struct ovn_port *out_port = NULL;
+    if (!find_route_outport(lr_ports, route->logical_port->logical_port,
+                            route->ip_prefix, route->nexthop,
+                            IN6_IS_ADDR_V4MAPPED(&prefix),
+                            false,
+                            &out_port, &lrp_addr_s)) {
+        free(nexthop);
+        return;
+    }
+
+    parsed_route_add(od, nexthop, &prefix, plen, false, lrp_addr_s,
+                     out_port, 0, false, false, NULL,
+                     ROUTE_SOURCE_LEARNED, &route->header_, parsed_routes_out);
+}
+
+static void
+routes_table_sync(
+    const struct sbrec_learned_route_table *sbrec_learned_route_table,
+    const struct hmap *parsed_routes,
+    const struct hmap *lr_ports,
+    const struct ovn_datapaths *lr_datapaths,
+    struct hmap *parsed_routes_out)
+{
+    const struct sbrec_learned_route *sb_route;
+    SBREC_LEARNED_ROUTE_TABLE_FOR_EACH (sb_route, sbrec_learned_route_table) {
+        parse_route_from_sbrec_route(parsed_routes_out, lr_ports,
+                                     &lr_datapaths->datapaths,
+                                     sb_route);
+
+    }
+
+    const struct parsed_route *route;
+    HMAP_FOR_EACH (route, key_node, parsed_routes) {
+        hmap_insert(parsed_routes_out, &parsed_route_clone(route)->key_node,
+                    parsed_route_hash(route));
+    }
+}

lib/stopwatch-names.h
@@ -35,5 +35,6 @@
 #define LR_STATEFUL_RUN_STOPWATCH_NAME "lr_stateful"
 #define LS_STATEFUL_RUN_STOPWATCH_NAME "ls_stateful"
 #define ADVERTISED_ROUTE_SYNC_RUN_STOPWATCH_NAME "advertised_route_sync"
+#define LEARNED_ROUTE_SYNC_RUN_STOPWATCH_NAME "learned_route_sync"
 
 #endif

northd/automake.mk
@@ -42,6 +42,8 @@ northd_ovn_northd_SOURCES = \
 	northd/en-acl-ids.h \
 	northd/en-advertised-route-sync.c \
 	northd/en-advertised-route-sync.h \
+	northd/en-learned-route-sync.c \
+	northd/en-learned-route-sync.h \
 	northd/inc-proc-northd.c \
 	northd/inc-proc-northd.h \
 	northd/ipam.c \

northd/en-ecmp-nexthop.c
@@ -96,19 +96,26 @@ build_ecmp_nexthop_table(
             continue;
         }
 
-        const struct nbrec_logical_router_static_route *r = pr->route;
+        /* This route has ecmp-symmetric-reply configured, it must be a
+         * static route. */
+        ovs_assert(pr->source == ROUTE_SOURCE_STATIC);
+
+        struct ds nexthop_str = DS_EMPTY_INITIALIZER;
+        ipv6_format_mapped(pr->nexthop, &nexthop_str);
+        const char *nexthop = ds_cstr(&nexthop_str);
+
         struct ecmp_nexthop_data *e = ecmp_nexthop_find_entry(
-                r->nexthop, pr->out_port->sb, &sb_nexthops_map);
+                nexthop, pr->out_port->sb, &sb_nexthops_map);
         if (!e) {
             sb_ecmp_nexthop = sbrec_ecmp_nexthop_insert(ovnsb_txn);
-            sbrec_ecmp_nexthop_set_nexthop(sb_ecmp_nexthop, r->nexthop);
+            sbrec_ecmp_nexthop_set_nexthop(sb_ecmp_nexthop, nexthop);
             sbrec_ecmp_nexthop_set_port(sb_ecmp_nexthop, pr->out_port->sb);
             sbrec_ecmp_nexthop_set_datapath(sb_ecmp_nexthop,
                                             pr->out_port->sb->datapath);
             const struct sbrec_mac_binding *smb =
                 mac_binding_lookup(sbrec_mac_binding_by_lport_ip,
                                    pr->out_port->sb->logical_port,
-                                   r->nexthop);
+                                   nexthop);
             if (smb) {
                 sbrec_ecmp_nexthop_set_mac(sb_ecmp_nexthop, smb->mac);
             }
@@ -116,6 +123,7 @@ build_ecmp_nexthop_table(
             hmap_remove(&sb_nexthops_map, &e->hmap_node);
             free(e);
         }
+        ds_destroy(&nexthop_str);
     }
 
     struct ecmp_nexthop_data *e;

northd/en-learned-route-sync.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef EN_LEARNED_ROUTE_SYNC_H
+#define EN_LEARNED_ROUTE_SYNC_H 1
+
+#include "lib/inc-proc-eng.h"
+#include "openvswitch/hmap.h"
+
+struct learned_route_sync_data {
+    struct hmap parsed_routes;
+};
+
+bool learned_route_sync_northd_change_handler(struct engine_node *,
+                                              void *data);
+void *en_learned_route_sync_init(struct engine_node *, struct engine_arg *);
+void en_learned_route_sync_cleanup(void *data);
+void en_learned_route_sync_run(struct engine_node *, void *data);
+
+#endif /* EN_LEARNED_ROUTE_SYNC_H */

northd/en-lflow.c
@@ -27,6 +27,7 @@
 #include "en-northd.h"
 #include "en-meters.h"
 #include "en-sampling-app.h"
+#include "en-learned-route-sync.h"
 #include "lflow-mgr.h"
 
 #include "lib/inc-proc-eng.h"
@@ -47,6 +48,8 @@ lflow_get_input_data(struct engine_node *node,
         engine_get_input_data("bfd_sync", node);
     struct routes_data *routes_data =
         engine_get_input_data("routes", node);
+    struct learned_route_sync_data *learned_route_sync_data =
+        engine_get_input_data("learned_route_sync", node);
     struct route_policies_data *route_policies_data =
         engine_get_input_data("route_policies", node);
     struct port_group_data *pg_data =
@@ -83,7 +86,7 @@ lflow_get_input_data(struct engine_node *node,
     lflow_input->lb_datapaths_map = &northd_data->lb_datapaths_map;
     lflow_input->svc_monitor_map = &northd_data->svc_monitor_map;
     lflow_input->bfd_ports = &bfd_sync_data->bfd_ports;
-    lflow_input->parsed_routes = &routes_data->parsed_routes;
+    lflow_input->parsed_routes = &learned_route_sync_data->parsed_routes;
     lflow_input->route_tables = &routes_data->route_tables;
     lflow_input->route_policies = &route_policies_data->route_policies;
     lflow_input->igmp_groups = &multicat_igmp_data->igmp_groups;

northd/inc-proc-northd.c
@@ -45,6 +45,7 @@
 #include "en-ecmp-nexthop.h"
 #include "en-acl-ids.h"
 #include "en-advertised-route-sync.h"
+#include "en-learned-route-sync.h"
 #include "unixctl.h"
 #include "util.h"
 
@@ -109,7 +110,8 @@ static unixctl_cb_func chassis_features_list;
     SB_NODE(logical_dp_group, "logical_dp_group") \
     SB_NODE(ecmp_nexthop, "ecmp_nexthop") \
     SB_NODE(acl_id, "acl_id") \
-    SB_NODE(advertised_route, "advertised_route")
+    SB_NODE(advertised_route, "advertised_route") \
+    SB_NODE(learned_route, "learned_route")
 
 enum sb_engine_node {
 #define SB_NODE(NAME, NAME_STR) SB_##NAME,
@@ -172,6 +174,7 @@ static ENGINE_NODE(ecmp_nexthop, "ecmp_nexthop");
 static ENGINE_NODE(multicast_igmp, "multicast_igmp");
 static ENGINE_NODE(acl_id, "acl_id");
 static ENGINE_NODE(advertised_route_sync, "advertised_route_sync");
+static ENGINE_NODE(learned_route_sync, "learned_route_sync");
 
 void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                           struct ovsdb_idl_loop *sb)
@@ -290,6 +293,11 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_advertised_route_sync, &en_sb_advertised_route,
                      NULL);
 
+    engine_add_input(&en_learned_route_sync, &en_routes, NULL);
+    engine_add_input(&en_learned_route_sync, &en_sb_learned_route, NULL);
+    engine_add_input(&en_learned_route_sync, &en_northd,
+                     learned_route_sync_northd_change_handler);
+
     engine_add_input(&en_sync_meters, &en_nb_acl, NULL);
     engine_add_input(&en_sync_meters, &en_nb_meter, NULL);
     engine_add_input(&en_sync_meters, &en_sb_meter, NULL);
@@ -307,6 +315,10 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_lflow, &en_bfd_sync, NULL);
     engine_add_input(&en_lflow, &en_route_policies, NULL);
     engine_add_input(&en_lflow, &en_routes, NULL);
+    /* XXX: This causes a full lflow recompute on each change to any route.
+     * At least for learned routes we should add incremental processing here.
+     * */
+    engine_add_input(&en_lflow, &en_learned_route_sync, NULL);
     engine_add_input(&en_lflow, &en_global_config,
                      node_global_config_handler);
 

commit 9c64e2825017102ea90fa858ed6875e9d2b70e30

northd: Learned route use nexthop address family for outport IP selection.
The LRP source address used for route egress must match the nexthop's
address family instead of the prefix.  A follow up patch depends on this
fix and the test will cover this - without this fix the EVPN IPv6
overlay over IPv4 underlay with multiple L3 hops will not work.

Fixes: 966ca1c919ce ("northd: Handle learned routes.")
Assisted-by: Cursor, with model: claude-4.6-opus-high
Signed-off-by: Han Zhou <hzhou@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/en-learned-route-sync.c
@@ -188,7 +188,7 @@ parse_route_from_sbrec_route(struct hmap *parsed_routes_out,
     struct ovn_port *out_port = NULL;
     if (!find_route_outport(lr_ports, route->logical_port->logical_port,
                             "static route", route->ip_prefix, route->nexthop,
-                            IN6_IS_ADDR_V4MAPPED(&prefix),
+                            IN6_IS_ADDR_V4MAPPED(nexthop),
                             false,
                             &out_port, &lrp_addr_s)) {
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);