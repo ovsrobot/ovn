ovsrobot/ovn

commit 966ca1c919cedacc58108114f6bd0a3005425b45

northd: Handle learned routes.
Here we expand the previous routes-sync engine node to not only
advertise routes to the southbound table, but also learn received routes
from this table.

These routes are then passed to the same logic that connected and static
routes are using for flow generation.
However we prioritize these routes lower than connected or static routes
as information in cluster (for the same prefix length) should always be
more correct then learned routes.
This is also consistent with the default behaviour of phyiscal routers.

Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

northd/en-learned-route-sync.c
@@ -1,0 +1,208 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+#include <stdbool.h>
+
+#include "openvswitch/vlog.h"
+#include "stopwatch.h"
+#include "northd.h"
+
+#include "en-learned-route-sync.h"
+#include "lib/stopwatch-names.h"
+#include "openvswitch/hmap.h"
+#include "ovn-util.h"
+
+VLOG_DEFINE_THIS_MODULE(en_learned_route_sync);
+
+static void
+routes_table_sync(
+    const struct sbrec_learned_route_table *sbrec_learned_route_table,
+    const struct hmap *parsed_routes,
+    const struct hmap *lr_ports,
+    const struct ovn_datapaths *lr_datapaths,
+    struct hmap *parsed_routes_out);
+
+bool
+learned_route_sync_northd_change_handler(struct engine_node *node,
+                                         void *data_ OVS_UNUSED)
+{
+    struct northd_data *northd_data = engine_get_input_data("northd", node);
+    if (!northd_has_tracked_data(&northd_data->trk_data)) {
+        return false;
+    }
+
+    /* This node uses the below data from the en_northd engine node.
+     * See (lr_stateful_get_input_data())
+     *   1. northd_data->lr_datapaths
+     *   2. northd_data->lr_ports
+     *      This data gets updated when a logical router or logical router port
+     *      is created or deleted.
+     *      Northd engine node presently falls back to full recompute when
+     *      this happens and so does this node.
+     *      Note: When we add I-P to the created/deleted logical routers or
+     *      logical router ports, we need to revisit this handler.
+     */
+
+    return true;
+}
+
+static void
+routes_sync_clear(struct learned_route_sync_data *data)
+{
+    struct parsed_route *r;
+    HMAP_FOR_EACH_POP (r, key_node, &data->parsed_routes) {
+        parsed_route_free(r);
+    }
+}
+
+static void
+routes_sync_init(struct learned_route_sync_data *data)
+{
+    hmap_init(&data->parsed_routes);
+}
+
+static void
+routes_sync_destroy(struct learned_route_sync_data *data)
+{
+    routes_sync_clear(data);
+    hmap_destroy(&data->parsed_routes);
+}
+
+void *
+en_learned_route_sync_init(struct engine_node *node OVS_UNUSED,
+                     struct engine_arg *arg OVS_UNUSED)
+{
+    struct learned_route_sync_data *data = xzalloc(sizeof *data);
+    routes_sync_init(data);
+    return data;
+}
+
+void
+en_learned_route_sync_cleanup(void *data)
+{
+    routes_sync_destroy(data);
+}
+
+void
+en_learned_route_sync_run(struct engine_node *node, void *data)
+{
+    routes_sync_clear(data);
+
+    struct learned_route_sync_data *routes_sync_data = data;
+    struct routes_data *routes_data
+        = engine_get_input_data("routes", node);
+    const struct sbrec_learned_route_table *sbrec_learned_route_table =
+        EN_OVSDB_GET(engine_get_input("SB_learned_route", node));
+    struct northd_data *northd_data = engine_get_input_data("northd", node);
+
+    stopwatch_start(LEARNED_ROUTE_SYNC_RUN_STOPWATCH_NAME, time_msec());
+
+    routes_table_sync(sbrec_learned_route_table,
+                      &routes_data->parsed_routes,
+                      &northd_data->lr_ports,
+                      &northd_data->lr_datapaths,
+                      &routes_sync_data->parsed_routes);
+
+    stopwatch_stop(LEARNED_ROUTE_SYNC_RUN_STOPWATCH_NAME, time_msec());
+    engine_set_node_state(node, EN_UPDATED);
+}
+
+
+static void
+parse_route_from_sbrec_route(struct hmap *parsed_routes_out,
+                             const struct hmap *lr_ports,
+                             const struct hmap *lr_datapaths,
+                             const struct sbrec_learned_route *route)
+{
+    const struct ovn_datapath *od = ovn_datapath_from_sbrec(
+        NULL, lr_datapaths, route->datapath);
+
+    if (!od || ovn_datapath_is_stale(od)) {
+        return;
+    }
+
+    /* Verify that the next hop is an IP address with an all-ones mask. */
+    struct in6_addr *nexthop = xmalloc(sizeof *nexthop);
+    unsigned int plen;
+    if (!ip46_parse_cidr(route->nexthop, nexthop, &plen)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "bad 'nexthop' %s in learned route "
+                     UUID_FMT, route->nexthop,
+                     UUID_ARGS(&route->header_.uuid));
+        free(nexthop);
+        return;
+    }
+    if ((IN6_IS_ADDR_V4MAPPED(nexthop) && plen != 32) ||
+        (!IN6_IS_ADDR_V4MAPPED(nexthop) && plen != 128)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "bad next hop mask %s in learned route "
+                     UUID_FMT, route->nexthop,
+                     UUID_ARGS(&route->header_.uuid));
+        free(nexthop);
+        return;
+    }
+
+    /* Parse ip_prefix */
+    struct in6_addr prefix;
+    if (!ip46_parse_cidr(route->ip_prefix, &prefix, &plen)) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "bad 'ip_prefix' %s in learned route "
+                     UUID_FMT, route->ip_prefix,
+                     UUID_ARGS(&route->header_.uuid));
+        free(nexthop);
+        return;
+    }
+
+    /* Verify that ip_prefix and nexthop are on the same network. */
+    const char *lrp_addr_s = NULL;
+    struct ovn_port *out_port = NULL;
+    if (!find_route_outport(lr_ports, route->logical_port->logical_port,
+                            route->ip_prefix, route->nexthop,
+                            IN6_IS_ADDR_V4MAPPED(&prefix),
+                            false,
+                            &out_port, &lrp_addr_s)) {
+        free(nexthop);
+        return;
+    }
+
+    parsed_route_add(od, nexthop, &prefix, plen, false, lrp_addr_s,
+                     out_port, 0, false, false, NULL,
+                     ROUTE_SOURCE_LEARNED, &route->header_, parsed_routes_out);
+}
+
+static void
+routes_table_sync(
+    const struct sbrec_learned_route_table *sbrec_learned_route_table,
+    const struct hmap *parsed_routes,
+    const struct hmap *lr_ports,
+    const struct ovn_datapaths *lr_datapaths,
+    struct hmap *parsed_routes_out)
+{
+    const struct sbrec_learned_route *sb_route;
+    SBREC_LEARNED_ROUTE_TABLE_FOR_EACH (sb_route, sbrec_learned_route_table) {
+        parse_route_from_sbrec_route(parsed_routes_out, lr_ports,
+                                     &lr_datapaths->datapaths,
+                                     sb_route);
+
+    }
+
+    const struct parsed_route *route;
+    HMAP_FOR_EACH (route, key_node, parsed_routes) {
+        hmap_insert(parsed_routes_out, &parsed_route_clone(route)->key_node,
+                    parsed_route_hash(route));
+    }
+}

lib/stopwatch-names.h
@@ -35,5 +35,6 @@
 #define LR_STATEFUL_RUN_STOPWATCH_NAME "lr_stateful"
 #define LS_STATEFUL_RUN_STOPWATCH_NAME "ls_stateful"
 #define ADVERTISED_ROUTE_SYNC_RUN_STOPWATCH_NAME "advertised_route_sync"
+#define LEARNED_ROUTE_SYNC_RUN_STOPWATCH_NAME "learned_route_sync"
 
 #endif

northd/automake.mk
@@ -42,6 +42,8 @@ northd_ovn_northd_SOURCES = \
 	northd/en-acl-ids.h \
 	northd/en-advertised-route-sync.c \
 	northd/en-advertised-route-sync.h \
+	northd/en-learned-route-sync.c \
+	northd/en-learned-route-sync.h \
 	northd/inc-proc-northd.c \
 	northd/inc-proc-northd.h \
 	northd/ipam.c \

northd/en-ecmp-nexthop.c
@@ -96,19 +96,26 @@ build_ecmp_nexthop_table(
             continue;
         }
 
-        const struct nbrec_logical_router_static_route *r = pr->route;
+        /* This route has ecmp-symmetric-reply configured, it must be a
+         * static route. */
+        ovs_assert(pr->source == ROUTE_SOURCE_STATIC);
+
+        struct ds nexthop_str = DS_EMPTY_INITIALIZER;
+        ipv6_format_mapped(pr->nexthop, &nexthop_str);
+        const char *nexthop = ds_cstr(&nexthop_str);
+
         struct ecmp_nexthop_data *e = ecmp_nexthop_find_entry(
-                r->nexthop, pr->out_port->sb, &sb_nexthops_map);
+                nexthop, pr->out_port->sb, &sb_nexthops_map);
         if (!e) {
             sb_ecmp_nexthop = sbrec_ecmp_nexthop_insert(ovnsb_txn);
-            sbrec_ecmp_nexthop_set_nexthop(sb_ecmp_nexthop, r->nexthop);
+            sbrec_ecmp_nexthop_set_nexthop(sb_ecmp_nexthop, nexthop);
             sbrec_ecmp_nexthop_set_port(sb_ecmp_nexthop, pr->out_port->sb);
             sbrec_ecmp_nexthop_set_datapath(sb_ecmp_nexthop,
                                             pr->out_port->sb->datapath);
             const struct sbrec_mac_binding *smb =
                 mac_binding_lookup(sbrec_mac_binding_by_lport_ip,
                                    pr->out_port->sb->logical_port,
-                                   r->nexthop);
+                                   nexthop);
             if (smb) {
                 sbrec_ecmp_nexthop_set_mac(sb_ecmp_nexthop, smb->mac);
             }
@@ -116,6 +123,7 @@ build_ecmp_nexthop_table(
             hmap_remove(&sb_nexthops_map, &e->hmap_node);
             free(e);
         }
+        ds_destroy(&nexthop_str);
     }
 
     struct ecmp_nexthop_data *e;

northd/en-learned-route-sync.h
@@ -1,0 +1,32 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef EN_LEARNED_ROUTE_SYNC_H
+#define EN_LEARNED_ROUTE_SYNC_H 1
+
+#include "lib/inc-proc-eng.h"
+#include "openvswitch/hmap.h"
+
+struct learned_route_sync_data {
+    struct hmap parsed_routes;
+};
+
+bool learned_route_sync_northd_change_handler(struct engine_node *,
+                                              void *data);
+void *en_learned_route_sync_init(struct engine_node *, struct engine_arg *);
+void en_learned_route_sync_cleanup(void *data);
+void en_learned_route_sync_run(struct engine_node *, void *data);
+
+#endif /* EN_LEARNED_ROUTE_SYNC_H */

northd/en-lflow.c
@@ -27,6 +27,7 @@
 #include "en-northd.h"
 #include "en-meters.h"
 #include "en-sampling-app.h"
+#include "en-learned-route-sync.h"
 #include "lflow-mgr.h"
 
 #include "lib/inc-proc-eng.h"
@@ -47,6 +48,8 @@ lflow_get_input_data(struct engine_node *node,
         engine_get_input_data("bfd_sync", node);
     struct routes_data *routes_data =
         engine_get_input_data("routes", node);
+    struct learned_route_sync_data *learned_route_sync_data =
+        engine_get_input_data("learned_route_sync", node);
     struct route_policies_data *route_policies_data =
         engine_get_input_data("route_policies", node);
     struct port_group_data *pg_data =
@@ -83,7 +86,7 @@ lflow_get_input_data(struct engine_node *node,
     lflow_input->lb_datapaths_map = &northd_data->lb_datapaths_map;
     lflow_input->svc_monitor_map = &northd_data->svc_monitor_map;
     lflow_input->bfd_ports = &bfd_sync_data->bfd_ports;
-    lflow_input->parsed_routes = &routes_data->parsed_routes;
+    lflow_input->parsed_routes = &learned_route_sync_data->parsed_routes;
     lflow_input->route_tables = &routes_data->route_tables;
     lflow_input->route_policies = &route_policies_data->route_policies;
     lflow_input->igmp_groups = &multicat_igmp_data->igmp_groups;

northd/inc-proc-northd.c
@@ -45,6 +45,7 @@
 #include "en-ecmp-nexthop.h"
 #include "en-acl-ids.h"
 #include "en-advertised-route-sync.h"
+#include "en-learned-route-sync.h"
 #include "unixctl.h"
 #include "util.h"
 
@@ -109,7 +110,8 @@ static unixctl_cb_func chassis_features_list;
     SB_NODE(logical_dp_group, "logical_dp_group") \
     SB_NODE(ecmp_nexthop, "ecmp_nexthop") \
     SB_NODE(acl_id, "acl_id") \
-    SB_NODE(advertised_route, "advertised_route")
+    SB_NODE(advertised_route, "advertised_route") \
+    SB_NODE(learned_route, "learned_route")
 
 enum sb_engine_node {
 #define SB_NODE(NAME, NAME_STR) SB_##NAME,
@@ -172,6 +174,7 @@ static ENGINE_NODE(ecmp_nexthop, "ecmp_nexthop");
 static ENGINE_NODE(multicast_igmp, "multicast_igmp");
 static ENGINE_NODE(acl_id, "acl_id");
 static ENGINE_NODE(advertised_route_sync, "advertised_route_sync");
+static ENGINE_NODE(learned_route_sync, "learned_route_sync");
 
 void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
                           struct ovsdb_idl_loop *sb)
@@ -290,6 +293,11 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_advertised_route_sync, &en_sb_advertised_route,
                      NULL);
 
+    engine_add_input(&en_learned_route_sync, &en_routes, NULL);
+    engine_add_input(&en_learned_route_sync, &en_sb_learned_route, NULL);
+    engine_add_input(&en_learned_route_sync, &en_northd,
+                     learned_route_sync_northd_change_handler);
+
     engine_add_input(&en_sync_meters, &en_nb_acl, NULL);
     engine_add_input(&en_sync_meters, &en_nb_meter, NULL);
     engine_add_input(&en_sync_meters, &en_sb_meter, NULL);
@@ -307,6 +315,10 @@ void inc_proc_northd_init(struct ovsdb_idl_loop *nb,
     engine_add_input(&en_lflow, &en_bfd_sync, NULL);
     engine_add_input(&en_lflow, &en_route_policies, NULL);
     engine_add_input(&en_lflow, &en_routes, NULL);
+    /* XXX: This causes a full lflow recompute on each change to any route.
+     * At least for learned routes we should add incremental processing here.
+     * */
+    engine_add_input(&en_lflow, &en_learned_route_sync, NULL);
     engine_add_input(&en_lflow, &en_global_config,
                      node_global_config_handler);
 

commit dce0b44f48669c85641a5916d604f498ffffe74d

northd: Fix L3 EVPN when remote VTEP is not L2 adjacent.
Dynamic routing was designed so BGP-learned routes are only accepted
when the nexthop lies in a subnet of the logical router port they are
learned on—right for plain IP routing.

With L3 EVPN (IP-VRF / type-5), routes can be learned on a port with a
nexthop outside any configured LRP subnet (indirect nexthop).

find_route_outport(..., force_out_port=false) then fails and the route
never reaches the pipeline.  Pass force_out_port=true for SB
Learned_Route parsing so that the routes can be handled properly.

Add multinode L3 EVPN case with a transit router (non-L2-adjacent
VTEPs).

Reported-at: https://redhat.atlassian.net/browse/FDP-3476
Fixes: 966ca1c919ce ("northd: Handle learned routes.")
Assisted-by: Cursor, with model: claude-4.6-opus-high
Signed-off-by: Han Zhou <hzhou@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/en-learned-route-sync.c
@@ -189,7 +189,7 @@ parse_route_from_sbrec_route(struct hmap *parsed_routes_out,
     if (!find_route_outport(lr_ports, route->logical_port->logical_port,
                             "static route", route->ip_prefix, route->nexthop,
                             IN6_IS_ADDR_V4MAPPED(nexthop),
-                            false,
+                            true,
                             &out_port, &lrp_addr_s)) {
         static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
         VLOG_WARN_RL(&rl, "could not find output port %s for learned route "

tests/multinode.at
@@ -4580,6 +4580,203 @@ fe80::/64 dev lo-wl-10 proto kernel metric 256 pref medium])
 
 AT_CLEANUP
 
+AT_SETUP([ovn multinode bgp L3 EVPN with intermediate router - VTEPs not L2 adjacent])
+check_fake_multinode_setup
+cleanup_multinode_resources
+CHECK_VRF()
+
+gw=ovn-gw-1
+vni=10
+ext_bgp_ip_gw1=42.10.10.11
+router_ext_ip=42.10.10.1
+ext_bgp_ip6_gw1=42:10:10::11
+ext_bgp_mac_gw1=00:00:01:00:00:$vni
+host_bgp_ip_gw1=42.10.20.12
+router_host_ip=42.10.20.1
+host_bgp_ip6_gw1=42:10:20::12
+host_bgp_mac_gw1=00:00:00:01:00:$vni
+nat_ip_gw1=42.10.20.13
+nat_ip6_gw1=42:10:20::13
+lb_ip_gw1=42.10.20.14
+lb_ip6_gw1=42:10:20::14
+ext_as=4200000100
+host_as=4210000000
+router_as=4290000000
+
+check m_as $gw ovs-vsctl add-br br-$gw
+on_exit "m_as $gw ovs-vsctl del-br br-$gw"
+check m_as $gw ip netns add router-ns
+on_exit "m_as $gw ip netns del router-ns"
+check m_as $gw ip link add host-to-r type veth peer name r-host
+on_exit "m_as $gw ip link del host-to-r"
+check m_as $gw ip link set r-host netns router-ns
+check m_as $gw ovs-vsctl add-port br-$gw host-to-r
+check m_as $gw ip link set host-to-r up
+check m_as $gw ip link set br-$gw up
+check m_as $gw ip netns exec router-ns ip link set lo up
+check m_as $gw ip netns exec router-ns ip addr add $router_host_ip/24 dev r-host
+check m_as $gw ip netns exec router-ns ip link set r-host up
+
+check m_as $gw ip netns add frr-ns
+on_exit "m_as $gw ip netns del frr-ns"
+check m_as $gw ip link add ext1 type veth peer name ext1-r
+check m_as $gw ip link set ext1 netns frr-ns
+check m_as $gw ip link set ext1-r netns router-ns
+check m_as $gw ip netns exec frr-ns ip link set lo up
+check m_as $gw ip netns exec frr-ns ip addr add $ext_bgp_ip_gw1/24 dev ext1
+check m_as $gw ip netns exec frr-ns ip link set ext1 up
+check m_as $gw ip netns exec router-ns ip addr add $router_ext_ip/24 dev ext1-r
+check m_as $gw ip netns exec router-ns ip link set ext1-r up
+check m_as $gw ip netns exec router-ns sysctl -qw net.ipv4.ip_forward=1
+
+check m_as $gw ovs-vsctl set open . external-ids:ovn-bridge-mappings=public:br-ex
+check m_as $gw ovs-vsctl set open . external-ids:ovn-evpn-local-ip=$host_bgp_ip_gw1
+check m_as $gw ovs-vsctl set open . external-ids:ovn-evpn-vxlan-ports=4789
+
+# FRR in frr-ns (external EVPN speaker) and router-ns (BGP EVPN transit between ext1-r and r-host).
+check m_as $gw sed -i 's/bgpd=no/bgpd=yes/g' /etc/frr/daemons
+check m_as $gw sed -i 's/StartLimitBurst=.*/StartLimitBurst=100/g' /usr/lib/systemd/system/frr.service
+check m_as $gw systemctl daemon-reload
+for ns in frr-ns router-ns; do
+    check m_as $gw mkdir -p /etc/frr/$ns
+    check m_as $gw cp -r /etc/frr/daemons /etc/frr/frr.conf /etc/frr/$ns/
+    check m_as $gw rm -f /etc/frr/$ns/vtysh.conf
+    check m_as $gw touch /etc/frr/$ns/vtysh.conf
+done
+check m_as $gw systemctl stop frr
+
+check m_as $gw mkdir -p /run/frr/frr-ns /run/frr/router-ns
+if m_as $gw getent passwd frr > /dev/null; then
+    check m_as $gw chown -R frr:frr /run/frr/frr-ns /run/frr/router-ns
+fi
+for ns in frr-ns router-ns; do
+    for init in /usr/libexec/frr/frrinit.sh /usr/lib/frr/frrinit.sh; do
+        if m_as $gw test -x "$init"; then
+            m_as $gw ip netns exec $ns "$init" start $ns
+            break
+        fi
+    done
+done
+on_exit "m_as $gw ip netns exec frr-ns /usr/libexec/frr/frrinit.sh stop frr-ns"
+on_exit "m_as $gw ip netns exec router-ns /usr/libexec/frr/frrinit.sh stop router-ns"
+check m_as $gw systemctl start frr
+m_as $gw vtysh -c 'configure' -c "no router bgp $router_as" -c 'exit'
+
+m_config_external_frr_router_l3 $gw $ext_as $ext_bgp_ip_gw1 $ext_bgp_ip_gw1/24 $ext_bgp_ip6_gw1/64 $ext_bgp_mac_gw1 $vni
+
+router_flags=$(m_frr_ns_flags router-ns)
+echo "configure
+ip prefix-list accept-all seq 5 permit any
+ipv6 prefix-list accept-all seq 5 permit any
+router bgp $router_as
+ bgp router-id $router_ext_ip
+ no bgp ebgp-requires-policy
+ neighbor ext1-r interface remote-as $ext_as
+ neighbor ext1-r passive
+ neighbor r-host interface remote-as $host_as
+ address-family ipv4 unicast
+  neighbor ext1-r prefix-list accept-all in
+  neighbor r-host prefix-list accept-all in
+  redistribute connected
+ exit-address-family
+ address-family l2vpn evpn
+  neighbor ext1-r activate
+  neighbor r-host activate
+  advertise-all-vni
+ exit-address-family
+exit
+end
+" | podman exec -i $gw ip netns exec router-ns vtysh $router_flags
+
+check m_as $gw ip addr add $host_bgp_ip_gw1/24 dev br-$gw
+check m_as $gw ip link set br-$gw up
+
+check multinode_nbctl                                                              \
+    -- lr-add lr                                                                   \
+      -- set logical_router lr options:dynamic-routing=true                        \
+                               options:dynamic-routing-vrf-id=$vni                 \
+      -- lrp-add lr lr-gw1 $host_bgp_mac_gw1 $host_bgp_ip_gw1/24 $host_bgp_ip6_gw1/64 \
+        -- lrp-set-gateway-chassis lr-gw1 $gw 10                                  \
+        -- lrp-set-options lr-gw1 dynamic-routing-redistribute=nat,lb             \
+      -- lrp-add lr lr-int1 00:00:00:00:01:02 30.0.1.1/24 30:1::1/64               \
+        -- lrp-set-options lr-int1 dynamic-routing-redistribute=connected         \
+    -- ls-add ls                                                                   \
+      -- lsp-add-localnet-port ls ls-ln public                                     \
+      -- lsp-add-router-port ls ls-lr-gw1 lr-gw1                                   \
+    -- ls-add ls-int1                                                              \
+      -- lsp-add-router-port ls-int1 ls-int1-lr lr-int1
+check multinode_nbctl                                                              \
+    -- lsp-add ls-int1 w1                                                          \
+      -- lsp-set-addresses w1 "00:00:00:00:00:01 30.0.1.11 30:1::11"               \
+    -- lr-nat-add lr dnat_and_snat $nat_ip_gw1 30.0.1.11 w1 00:00:00:00:01:11      \
+    -- lr-nat-add lr dnat_and_snat $nat_ip6_gw1 30:1::11 w1 00:00:00:00:01:11      \
+    -- lb-add lb1 $lb_ip_gw1 30.0.1.11                                            \
+    -- lb-add lb2 $lb_ip6_gw1 30:1::11                                            \
+    -- lr-lb-add lr lb1                                                            \
+    -- lr-lb-add lr lb2
+check m_as $gw /data/create_fake_vm.sh w1 w1 \
+    00:00:00:00:00:01 1500 30.0.1.11 24 30.0.1.1 30:1::11/64 30:1::1
+m_wait_for_ports_up
+check multinode_nbctl --wait=hv sync
+check multinode_nbctl set logical_switch ls                                        \
+    other_config:dynamic-routing-vni=$vni                                          \
+    other_config:dynamic-routing-bridge-ifname=br-$vni                             \
+    other_config:dynamic-routing-vxlan-ifname=vxlan-$vni                         \
+    other_config:dynamic-routing-advertise-ifname=lo-$vni                        \
+    other_config:dynamic-routing-redistribute=fdb,ip
+check multinode_nbctl --wait=hv sync
+check m_as $gw ovs-ofctl add-flow br-$gw "priority=100,ip,nw_dst=$host_bgp_ip_gw1,udp,tp_dst=4789,actions=LOCAL"
+OVS_WAIT_UNTIL([m_as ovn-gw-1 test -d /sys/class/net/vxlan_sys_4789])
+m_as $gw vtysh -c 'configure' -c 'no router bgp 4290000000' -c 'no router bgp 4210000000' -c 'exit'
+m_config_host_frr_router_l3 $gw $host_as $host_bgp_ip_gw1 $host_bgp_ip_gw1/24 $host_bgp_mac_gw1 $vni
+OVS_WAIT_UNTIL([m_as ovn-gw-1 vtysh -c 'show bgp neighbors' | grep -qE 'Connections established 1'])
+
+check m_as $gw ip link add name lo-wl-$vni type dummy
+on_exit "m_as $gw ip link del lo-wl-$vni"
+check m_as $gw ip link set lo-wl-$vni address 00:01:01:01:01:$vni
+check m_as $gw ip link set lo-wl-$vni master vrf-$vni
+check m_as $gw ip addr add dev lo-wl-$vni 77.77.1.$vni/32
+check m_as $gw ip -6 addr add dev lo-wl-$vni 77:77::1:$vni/64 nodad
+check m_as $gw ip link set lo-wl-$vni up
+
+# Extra fabric in frr-ns vrf: 99.99.99.99/32, 88.88.0.0/24 + fabric-wl-ns workload.
+check m_as $gw ip netns exec frr-ns ip link add lo-99 type dummy
+on_exit "m_as $gw ip netns exec frr-ns ip link del lo-99"
+check m_as $gw ip netns exec frr-ns ip link set lo-99 master vrf-$vni
+check m_as $gw ip netns exec frr-ns ip addr add 99.99.99.99/32 dev lo-99
+check m_as $gw ip netns exec frr-ns ip link set lo-99 up
+check m_as $gw ip netns exec frr-ns sysctl -qw net.ipv4.ip_forward=1
+check m_as $gw ip netns add fabric-wl-ns
+on_exit "m_as $gw ip netns del fabric-wl-ns"
+check m_as $gw ip netns exec frr-ns ip link add br-fabric-wl type bridge
+check m_as $gw ip netns exec frr-ns ip link set br-fabric-wl master vrf-$vni
+check m_as $gw ip netns exec frr-ns ip addr add 88.88.0.1/24 dev br-fabric-wl
+check m_as $gw ip netns exec frr-ns ip link set br-fabric-wl up
+check m_as $gw ip netns exec frr-ns ip link add fabric-wl type veth peer name fabric-wl-r
+check m_as $gw ip netns exec frr-ns ip link set fabric-wl-r master br-fabric-wl
+check m_as $gw ip netns exec frr-ns ip link set fabric-wl-r up
+check m_as $gw ip netns exec frr-ns ip link set fabric-wl netns fabric-wl-ns
+check m_as $gw ip netns exec fabric-wl-ns ip link set lo up
+check m_as $gw ip netns exec fabric-wl-ns ip addr add 88.88.0.10/24 dev fabric-wl
+check m_as $gw ip netns exec fabric-wl-ns ip link set fabric-wl up
+check m_as $gw ip netns exec fabric-wl-ns ip route add default via 88.88.0.1
+
+OVS_WAIT_FOR_OUTPUT_UNQUOTED([m_as ovn-gw-1 ovn-appctl evpn/remote-vtep-list | sort], [0], [dnl
+IP: $ext_bgp_ip_gw1, port: 4789, vni: 10
+])
+
+m_wait_row_count Learned_Route 1 ip_prefix=88.88.0.0/24
+
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec frr-ns ip route show vrf vrf-$vni | grep -q 30.0.1])
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec frr-ns ip vrf exec vrf-$vni ping    -c1 30.0.1.11])
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec frr-ns ip vrf exec vrf-$vni ping -6 -c1 30:1::11])
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec frr-ns ip vrf exec vrf-$vni ping    -c1 $nat_ip_gw1])
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec frr-ns ip vrf exec vrf-$vni ping    -c1 $lb_ip_gw1])
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec fabric-wl-ns ping -c1 30.0.1.11])
+OVS_WAIT_UNTIL([m_as ovn-gw-1 ip netns exec frr-ns ip vrf exec vrf-$vni ping -I 99.99.99.99 -c1 30.0.1.11])
+
+AT_CLEANUP
+
 AT_SETUP([redirect-bridged to non-gw destination switch port])
 
 check_fake_multinode_setup