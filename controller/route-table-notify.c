ovsrobot/ovn

commit 673d90f1173f3abe1a2018792e4de785b94b1024

controller: Watch for route changes.
For each vrf we use we open a netlink watcher.
This allows us to reconcile on changed route entries from outside
routing agents.

Acked-by: Frode Nordahl <fnordahl@ubuntu.com>
Acked-by: Lorenzo Bianconi <lorenzo.bianconi@redhat.com>
Signed-off-by: Felix Huettner <felix.huettner@stackit.cloud>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/route-table-notify.c
@@ -1,0 +1,228 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include <net/if.h>
+#include <linux/rtnetlink.h>
+
+#include "netlink-notifier.h"
+#include "openvswitch/vlog.h"
+
+#include "binding.h"
+#include "hash.h"
+#include "hmapx.h"
+#include "route-table.h"
+#include "route.h"
+#include "route-table-notify.h"
+#include "route-exchange-netlink.h"
+
+VLOG_DEFINE_THIS_MODULE(route_table_notify);
+
+struct route_table_watch_request {
+    struct hmap_node node;
+    uint32_t table_id;
+};
+
+struct route_table_watch_entry {
+    struct hmap_node node;
+    uint32_t table_id;
+};
+
+static struct hmap watches = HMAP_INITIALIZER(&watches);
+static bool any_route_table_changed;
+static struct route_table_msg nln_rtmsg_change;
+
+static struct nln *nl_route_handle;
+static struct nln_notifier *nl_route_notifier_v4;
+static struct nln_notifier *nl_route_notifier_v6;
+
+static void route_table_change(const void *change_, void *aux);
+
+static void
+route_table_register_notifiers(void)
+{
+    VLOG_INFO("Adding route table watchers.");
+    ovs_assert(!nl_route_handle);
+
+    nl_route_handle = nln_create(NETLINK_ROUTE, route_table_parse,
+                                 &nln_rtmsg_change);
+    ovs_assert(nl_route_handle);
+
+    nl_route_notifier_v4 =
+        nln_notifier_create(nl_route_handle, RTNLGRP_IPV4_ROUTE,
+                            route_table_change, NULL);
+    if (!nl_route_notifier_v4) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "Failed to create ipv4 route table watcher.");
+    }
+
+    nl_route_notifier_v6 =
+        nln_notifier_create(nl_route_handle, RTNLGRP_IPV6_ROUTE,
+                            route_table_change, NULL);
+    if (!nl_route_notifier_v6) {
+        static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+        VLOG_WARN_RL(&rl, "Failed to create ipv6 route table watcher.");
+    }
+}
+
+static void
+route_table_deregister_notifiers(void)
+{
+    VLOG_INFO("Removing route table watchers.");
+    ovs_assert(nl_route_handle);
+
+    nln_notifier_destroy(nl_route_notifier_v4);
+    nln_notifier_destroy(nl_route_notifier_v6);
+    nln_destroy(nl_route_handle);
+    nl_route_notifier_v4 = NULL;
+    nl_route_notifier_v6 = NULL;
+    nl_route_handle = NULL;
+}
+
+static uint32_t
+route_table_notify_hash_watch(uint32_t table_id)
+{
+    return hash_add(0, table_id);
+}
+
+void
+route_table_add_watch_request(struct hmap *route_table_watches,
+                              uint32_t table_id)
+{
+    struct route_table_watch_request *wr = xzalloc(sizeof *wr);
+    wr->table_id = table_id;
+    hmap_insert(route_table_watches, &wr->node,
+                route_table_notify_hash_watch(wr->table_id));
+}
+
+void
+route_table_watch_request_cleanup(struct hmap *route_table_watches)
+{
+    struct route_table_watch_request *wr;
+    HMAP_FOR_EACH_POP (wr, node, route_table_watches) {
+        free(wr);
+    }
+}
+
+static struct route_table_watch_entry *
+find_watch_entry(uint32_t table_id)
+{
+    struct route_table_watch_entry *we;
+    uint32_t hash = route_table_notify_hash_watch(table_id);
+    HMAP_FOR_EACH_WITH_HASH (we, node, hash, &watches) {
+        if (table_id == we->table_id) {
+            return we;
+        }
+    }
+    return NULL;
+}
+
+static void
+route_table_change(const void *change_, void *aux OVS_UNUSED)
+{
+    const struct route_table_msg *change = change_;
+    if (change && change->rd.rtm_protocol != RTPROT_OVN) {
+        if (find_watch_entry(change->rd.rta_table_id)) {
+            any_route_table_changed = true;
+        }
+    }
+}
+
+static void
+add_watch_entry(uint32_t table_id)
+{
+   VLOG_INFO("Registering new route table watcher for table %d.",
+             table_id);
+
+    struct route_table_watch_entry *we;
+    uint32_t hash = route_table_notify_hash_watch(table_id);
+    we = xzalloc(sizeof *we);
+    we->table_id = table_id;
+    hmap_insert(&watches, &we->node, hash);
+
+    if (!nl_route_handle) {
+        route_table_register_notifiers();
+    }
+}
+
+static void
+remove_watch_entry(struct route_table_watch_entry *we)
+{
+    VLOG_INFO("Removing route table watcher for table %d.", we->table_id);
+    hmap_remove(&watches, &we->node);
+    free(we);
+
+    if (hmap_is_empty(&watches)) {
+        route_table_deregister_notifiers();
+    }
+}
+
+bool
+route_table_notify_run(void)
+{
+    any_route_table_changed = false;
+
+    if (nl_route_handle) {
+        nln_run(nl_route_handle);
+    }
+
+    return any_route_table_changed;
+}
+
+void
+route_table_notify_wait(void)
+{
+    if (nl_route_handle) {
+        nln_wait(nl_route_handle);
+    }
+}
+
+void
+route_table_notify_update_watches(const struct hmap *route_table_watches)
+{
+    struct hmapx sync_watches = HMAPX_INITIALIZER(&sync_watches);
+    struct route_table_watch_entry *we;
+    HMAP_FOR_EACH (we, node, &watches) {
+        hmapx_add(&sync_watches, we);
+    }
+
+    struct route_table_watch_request *wr;
+    HMAP_FOR_EACH (wr, node, route_table_watches) {
+        we = find_watch_entry(wr->table_id);
+        if (we) {
+            hmapx_find_and_delete(&sync_watches, we);
+        } else {
+            add_watch_entry(wr->table_id);
+        }
+    }
+
+    struct hmapx_node *node;
+    HMAPX_FOR_EACH (node, &sync_watches) {
+        remove_watch_entry(node->data);
+    }
+
+    hmapx_destroy(&sync_watches);
+}
+
+void
+route_table_notify_destroy(void)
+{
+    struct route_table_watch_entry *we;
+    HMAP_FOR_EACH_SAFE (we, node, &watches) {
+        remove_watch_entry(we);
+    }
+}

controller/automake.mk
@@ -57,6 +57,7 @@ controller_ovn_controller_SOURCES = \
 	controller/ecmp-next-hop-monitor.h \
 	controller/ecmp-next-hop-monitor.c \
 	controller/route-exchange.h \
+	controller/route-table-notify.h \
 	controller/route.h \
 	controller/route.c
 
@@ -64,10 +65,12 @@ if HAVE_NETLINK
 controller_ovn_controller_SOURCES += \
 	controller/route-exchange-netlink.h \
 	controller/route-exchange-netlink.c \
-	controller/route-exchange.c
+	controller/route-exchange.c \
+	controller/route-table-notify.c
 else
 controller_ovn_controller_SOURCES += \
-	controller/route-exchange-stub.c
+	controller/route-exchange-stub.c \
+	controller/route-table-notify-stub.c
 endif
 
 controller_ovn_controller_LDADD = lib/libovn.la $(OVS_LIBDIR)/libopenvswitch.la

controller/ovn-controller.c
@@ -91,6 +91,7 @@
 #include "acl-ids.h"
 #include "route.h"
 #include "route-exchange.h"
+#include "route-table-notify.h"
 
 VLOG_DEFINE_THIS_MODULE(main);
 
@@ -5237,10 +5238,16 @@ en_route_exchange_run(struct engine_node *node, void *data)
         .sbrec_port_binding_by_name = sbrec_port_binding_by_name,
         .announce_routes = &route_data->announce_routes,
     };
-    struct route_exchange_ctx_out r_ctx_out = {
-    };
+    struct route_exchange_ctx_out r_ctx_out = {};
+
+    hmap_init(&r_ctx_out.route_table_watches);
 
     route_exchange_run(&r_ctx_in, &r_ctx_out);
+    route_table_notify_update_watches(&r_ctx_out.route_table_watches);
+
+    route_table_watch_request_cleanup(&r_ctx_out.route_table_watches);
+    hmap_destroy(&r_ctx_out.route_table_watches);
+
     engine_set_node_state(node, EN_UPDATED);
 }
 
@@ -5257,6 +5264,39 @@ en_route_exchange_init(struct engine_node *node OVS_UNUSED,
 
 static void
 en_route_exchange_cleanup(void *data OVS_UNUSED)
+{
+}
+
+struct ed_type_route_table_notify {
+    /* For incremental processing this could be tracked per datapath in
+     * the future. */
+    bool changed;
+};
+
+static void
+en_route_table_notify_run(struct engine_node *node, void *data)
+{
+    struct ed_type_route_table_notify *rtn = data;
+    if (rtn->changed) {
+        engine_set_node_state(node, EN_UPDATED);
+    } else {
+        engine_set_node_state(node, EN_UNCHANGED);
+    }
+    rtn->changed = false;
+}
+
+
+static void *
+en_route_table_notify_init(struct engine_node *node OVS_UNUSED,
+                           struct engine_arg *arg OVS_UNUSED)
+{
+    struct ed_type_route_table_notify *rtn = xzalloc(sizeof *rtn);
+    rtn->changed = true;
+    return rtn;
+}
+
+static void
+en_route_table_notify_cleanup(void *data OVS_UNUSED)
 {
 }
 
@@ -5567,6 +5607,7 @@ main(int argc, char *argv[])
     ENGINE_NODE(acl_id, "acl_id");
     en_acl_id.is_valid = en_acl_id_is_valid;
     ENGINE_NODE(route, "route");
+    ENGINE_NODE(route_table_notify, "route_table_notify");
     ENGINE_NODE(route_exchange, "route_exchange");
 
 #define SB_NODE(NAME, NAME_STR) ENGINE_NODE_SB(NAME, NAME_STR);
@@ -5604,6 +5645,7 @@ main(int argc, char *argv[])
                      engine_noop_handler);
     engine_add_input(&en_route_exchange, &en_sb_port_binding,
                      engine_noop_handler);
+    engine_add_input(&en_route_exchange, &en_route_table_notify, NULL);
 
     engine_add_input(&en_addr_sets, &en_sb_address_set,
                      addr_sets_sb_address_set_handler);
@@ -6128,6 +6170,10 @@ main(int argc, char *argv[])
                                &transport_zones,
                                bridge_table);
 
+                    struct ed_type_route_table_notify *rtn =
+                        engine_get_internal_data(&en_route_table_notify);
+                    rtn->changed = route_table_notify_run();
+
                     stopwatch_start(CONTROLLER_LOOP_STOPWATCH_NAME,
                                     time_msec());
 
@@ -6413,6 +6459,7 @@ main(int argc, char *argv[])
             }
 
             binding_wait();
+            route_table_notify_wait();
         }
 
         unixctl_server_run(unixctl);
@@ -6567,6 +6614,7 @@ loop_done:
     ovsrcu_exit();
     dns_resolve_destroy();
     route_exchange_destroy();
+    route_table_notify_destroy();
 
     exit(retval);
 }

controller/route-exchange.c
@@ -29,6 +29,7 @@
 #include "ha-chassis.h"
 #include "local_data.h"
 #include "route.h"
+#include "route-table-notify.h"
 #include "route-exchange.h"
 #include "route-exchange-netlink.h"
 
@@ -165,7 +166,7 @@ sb_sync_learned_routes(const struct ovs_list *learned_routes,
 
 void
 route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
-                   struct route_exchange_ctx_out *r_ctx_out OVS_UNUSED)
+                   struct route_exchange_ctx_out *r_ctx_out)
 {
     struct sset old_maintained_vrfs = SSET_INITIALIZER(&old_maintained_vrfs);
     sset_swap(&_maintained_vrfs, &old_maintained_vrfs);
@@ -206,6 +207,9 @@ route_exchange_run(const struct route_exchange_ctx_in *r_ctx_in,
                                &ad->bound_ports, r_ctx_in->ovnsb_idl_txn,
                                r_ctx_in->sbrec_port_binding_by_name,
                                r_ctx_in->sbrec_learned_route_by_datapath);
+
+        route_table_add_watch_request(&r_ctx_out->route_table_watches,
+                                      table_id);
 
         re_nl_learned_routes_destroy(&received_routes);
     }

controller/route-exchange.h
@@ -18,6 +18,8 @@
 #ifndef ROUTE_EXCHANGE_H
 #define ROUTE_EXCHANGE_H 1
 
+#include "openvswitch/hmap.h"
+
 struct route_exchange_ctx_in {
     struct ovsdb_idl_txn *ovnsb_idl_txn;
     struct ovsdb_idl_index *sbrec_port_binding_by_name;
@@ -28,6 +30,7 @@ struct route_exchange_ctx_in {
 };
 
 struct route_exchange_ctx_out {
+    struct hmap route_table_watches;
 };
 
 void route_exchange_run(const struct route_exchange_ctx_in *,

controller/route-table-notify-stub.c
@@ -1,0 +1,55 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <config.h>
+
+#include <stdbool.h>
+
+#include "openvswitch/compiler.h"
+#include "route-table-notify.h"
+
+bool
+route_table_notify_run(void)
+{
+    return false;
+}
+
+void
+route_table_notify_wait(void)
+{
+}
+
+void
+route_table_add_watch_request(struct hmap *route_table_watches OVS_UNUSED,
+                              uint32_t table_id OVS_UNUSED)
+{
+}
+
+void
+route_table_watch_request_cleanup(struct hmap *route_table_watches OVS_UNUSED)
+{
+}
+
+void
+route_table_notify_update_watches(
+    const struct hmap *route_table_watches OVS_UNUSED)
+{
+}
+
+void
+route_table_notify_destroy(void)
+{
+}

controller/route-table-notify.h
@@ -1,0 +1,44 @@
+/*
+ * Copyright (c) 2025, STACKIT GmbH & Co. KG
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef ROUTE_TABLE_NOTIFY_H
+#define ROUTE_TABLE_NOTIFY_H 1
+
+#include <stdbool.h>
+#include "openvswitch/hmap.h"
+
+/* Returns true if any route table has changed enough that we need
+ * to learn new routes. */
+bool route_table_notify_run(void);
+void route_table_notify_wait(void);
+
+/* Add a watch request to the hmap. The hmap should later be passed to
+ * route_table_notify_update_watches*/
+void route_table_add_watch_request(struct hmap *route_table_watches,
+                                   uint32_t table_id);
+
+/* Cleanup all watch request in the provided hmap that where added using
+ * route_table_add_watch_request. */
+void route_table_watch_request_cleanup(struct hmap *route_table_watches);
+
+/* Updates the list of route table watches that are currently active.
+ * hmap should contain struct route_table_watch_request */
+void route_table_notify_update_watches(const struct hmap *route_table_watches);
+
+/* Cleans up all route table watches. */
+void route_table_notify_destroy(void);
+
+#endif /* ROUTE_TABLE_NOTIFY_H */

tests/system-ovn.at
@@ -16133,8 +16133,6 @@ check ovn-nbctl set Logical_Router_Port internet-phys \
 # Now we test route learning.
 check_row_count Learned_Route 0
 check ip route add 233.252.0.0/24 via 192.168.10.10 dev lo onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 1
 lp=$(fetch_column port_binding _uuid logical_port=internet-phys)
@@ -16142,8 +16140,6 @@ check_row_count Learned_Route 1 logical_port=$lp ip_prefix=233.252.0.0/24 nextho
 
 # If we remove the route it is also gone.
 check ip route del 233.252.0.0/24 via 192.168.10.10 dev lo onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 0
 
@@ -16152,8 +16148,6 @@ check_row_count Learned_Route 0
 check ovn-nbctl --wait=hv set Logical_Router_Port internet-phys \
       options:dynamic-routing-port-name=thisportdoesnotexist
 check ip route add 233.252.0.0/24 via 192.168.10.10 dev lo onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 0
 
@@ -16175,8 +16169,6 @@ check ovn-nbctl --wait=hv set Logical_Router_Port internet-phys \
       options:dynamic-routing-port-name=mylearninglsp
 
 check ip route add 233.253.0.0/24 via 192.168.20.20 dev hv1-mll onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 1 ip_prefix=233.253.0.0/24 nexthop=192.168.20.20
 
@@ -16415,8 +16407,6 @@ check ovn-nbctl set Logical_Router_Port internet-phys \
 # Now we test route learning.
 check_row_count Learned_Route 0
 check ip route add 233.252.0.0/24 via 192.168.10.10 dev lo onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 1
 lp=$(fetch_column port_binding _uuid logical_port=internet-phys)
@@ -16424,8 +16414,6 @@ check_row_count Learned_Route 1 logical_port=$lp ip_prefix=233.252.0.0/24 nextho
 
 # If we remove the route it is also gone.
 check ip route del 233.252.0.0/24 via 192.168.10.10 dev lo onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 0
 
@@ -16434,8 +16422,6 @@ check_row_count Learned_Route 0
 check ovn-nbctl --wait=hv set Logical_Router_Port internet-phys \
     options:dynamic-routing-port-name=thisportdoesnotexist
 check ip route add 233.252.0.0/24 via 192.168.10.10 dev lo onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 0
 
@@ -16457,8 +16443,6 @@ check ovn-nbctl --wait=hv set Logical_Router_Port internet-phys \
     options:dynamic-routing-port-name=mylearninglsp
 
 check ip route add 233.253.0.0/24 via 192.168.20.20 dev hv1-mll onlink vrf ovnvrf1337
-# For now we trigger a recompute as route watching is not yet implemented.
-check ovn-appctl -t ovn-controller inc-engine/recompute
 check ovn-nbctl --wait=hv sync
 check_row_count Learned_Route 1 ip_prefix=233.253.0.0/24 nexthop=192.168.20.20
 

commit 8889d05ca0dfdb75ec77a2b127dd5f8748f77958

controller: Make sure we free the route_data during change check.
The route_table_parse() can allocate data related to the nexthop
list. Those data need to be freed when we are done with the message.
Make sure we call route_data_destroy() when we are done with the
change.

Fixes: 673d90f1173f ("controller: Watch for route changes.")
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/route-table-notify.c
@@ -134,19 +134,22 @@ find_watch_entry(uint32_t table_id)
 static void
 route_table_change(const void *change_, void *aux OVS_UNUSED)
 {
-    /* We currently track whether at least one recent route table change
-     * was detected.  If that's the case already there's no need to
-     * continue. */
-    if (any_route_table_changed) {
+    if (!change_) {
         return;
     }
 
-    const struct route_table_msg *change = change_;
-    if (change && change->rd.rtm_protocol != RTPROT_OVN) {
+    /* We currently track whether at least one recent route table change
+     * was detected.  If that's the case already there's no need to
+     * continue. */
+    struct route_table_msg *change =
+        CONST_CAST(struct route_table_msg *, change_);
+    if (!any_route_table_changed && change->rd.rtm_protocol != RTPROT_OVN) {
         if (find_watch_entry(change->rd.rta_table_id)) {
             any_route_table_changed = true;
         }
     }
+
+    route_data_destroy(&change->rd);
 }
 
 static void