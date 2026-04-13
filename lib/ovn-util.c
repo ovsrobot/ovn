ovsrobot/ovn

commit 2a2fe266d09ca1cc4dbdb2c9083a09d2919fa40f

northd: Added support for port mirroring in OVN overlay.
Previously, the mirroring feature in OVN only supported tunneling
traffic to local and remote ports outside the OVN cluster.

With this update, it is now possible to mirror traffic passing through
a virtual port to a dedicated OVN port.

Added ls_in_mirror and ls_out_mirror stages in ls pipeline.

When attaching an lport mirror to a logical switch port, a new port
binding named mp-datapath-target-port is created. This port acts as
a mirror port, with its parent being the target port.

For lport mirroring, logical flows (flows) are generated to handle
traffic processing based on filtering criteria. Packets matching these
criteria are cloned and sent to the target port, while the original
traffic continues along its designated path. If an lport mirror is
created without specifying any rules, default logical flows are added
to mirror all incoming and outgoing traffic to the target port.

Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
Signed-off-by: Vladislav Odintsov <vlodintsov@k2.cloud>
Co-authored-by: Vladislav Odintsov <vlodintsov@k2.cloud>
Tested-by: Ivan Burnin <iburnin@k2.cloud>
Acked-by: Numan Siddique <numans@ovn.org>
Signed-off-by: Numan Siddique <numans@ovn.org>

lib/ovn-util.c
@@ -1413,3 +1413,10 @@ ovn_debug_commands_register(void)
     unixctl_command_register("debug/enable-timewarp", "", 0, 0,
                              ovn_enable_timewarp, NULL);
 }
+
+char *
+ovn_mirror_port_name(const char *datapath_name,
+                     const char *port_name)
+{
+    return xasprintf("mp-%s-%s", datapath_name, port_name);
+}

northd/northd.h
@@ -36,6 +36,7 @@ struct northd_input {
     const struct nbrec_chassis_template_var_table
         *nbrec_chassis_template_var_table;
     const struct nbrec_mirror_table *nbrec_mirror_table;
+    const struct nbrec_mirror_rule_table *nbrec_mirror_rule_table;
 
     /* Southbound table references */
     const struct sbrec_datapath_binding_table *sbrec_datapath_binding_table;
@@ -73,6 +74,7 @@ struct northd_input {
     struct ovsdb_idl_index *sbrec_ha_chassis_grp_by_name;
     struct ovsdb_idl_index *sbrec_ip_mcast_by_dp;
     struct ovsdb_idl_index *sbrec_fdb_by_dp_and_port;
+    struct ovsdb_idl_index *nbrec_mirror_by_type_and_sink;
 };
 
 /* A collection of datapaths. E.g. all logical switch datapaths, or all
@@ -472,37 +474,38 @@ enum ovn_stage {
     /* Logical switch ingress stages. */                                  \
     PIPELINE_STAGE(SWITCH, IN,  CHECK_PORT_SEC, 0, "ls_in_check_port_sec")   \
     PIPELINE_STAGE(SWITCH, IN,  APPLY_PORT_SEC, 1, "ls_in_apply_port_sec")   \
-    PIPELINE_STAGE(SWITCH, IN,  LOOKUP_FDB ,    2, "ls_in_lookup_fdb")    \
-    PIPELINE_STAGE(SWITCH, IN,  PUT_FDB,        3, "ls_in_put_fdb")       \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_ACL,        4, "ls_in_pre_acl")       \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_LB,         5, "ls_in_pre_lb")        \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_STATEFUL,   6, "ls_in_pre_stateful")  \
-    PIPELINE_STAGE(SWITCH, IN,  ACL_HINT,       7, "ls_in_acl_hint")      \
-    PIPELINE_STAGE(SWITCH, IN,  ACL_EVAL,       8, "ls_in_acl_eval")      \
-    PIPELINE_STAGE(SWITCH, IN,  ACL_SAMPLE,     9, "ls_in_acl_sample")    \
-    PIPELINE_STAGE(SWITCH, IN,  ACL_ACTION,    10, "ls_in_acl_action")    \
-    PIPELINE_STAGE(SWITCH, IN,  QOS,           11, "ls_in_qos")    \
-    PIPELINE_STAGE(SWITCH, IN,  LB_AFF_CHECK,  12, "ls_in_lb_aff_check")  \
-    PIPELINE_STAGE(SWITCH, IN,  LB,            13, "ls_in_lb")            \
-    PIPELINE_STAGE(SWITCH, IN,  LB_AFF_LEARN,  14, "ls_in_lb_aff_learn")  \
-    PIPELINE_STAGE(SWITCH, IN,  PRE_HAIRPIN,   15, "ls_in_pre_hairpin")   \
-    PIPELINE_STAGE(SWITCH, IN,  NAT_HAIRPIN,   16, "ls_in_nat_hairpin")   \
-    PIPELINE_STAGE(SWITCH, IN,  HAIRPIN,       17, "ls_in_hairpin")       \
-    PIPELINE_STAGE(SWITCH, IN,  ACL_AFTER_LB_EVAL,  18, \
-                   "ls_in_acl_after_lb_eval")                             \
-     PIPELINE_STAGE(SWITCH, IN,  ACL_AFTER_LB_SAMPLE,  19, \
+    PIPELINE_STAGE(SWITCH, IN,  MIRROR,         2, "ls_in_mirror")        \
+    PIPELINE_STAGE(SWITCH, IN,  LOOKUP_FDB,     3, "ls_in_lookup_fdb")    \
+    PIPELINE_STAGE(SWITCH, IN,  PUT_FDB,        4, "ls_in_put_fdb")       \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_ACL,        5, "ls_in_pre_acl")       \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_LB,         6, "ls_in_pre_lb")        \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_STATEFUL,   7, "ls_in_pre_stateful")  \
+    PIPELINE_STAGE(SWITCH, IN,  ACL_HINT,       8, "ls_in_acl_hint")      \
+    PIPELINE_STAGE(SWITCH, IN,  ACL_EVAL,       9, "ls_in_acl_eval")      \
+    PIPELINE_STAGE(SWITCH, IN,  ACL_SAMPLE,    10, "ls_in_acl_sample")    \
+    PIPELINE_STAGE(SWITCH, IN,  ACL_ACTION,    11, "ls_in_acl_action")    \
+    PIPELINE_STAGE(SWITCH, IN,  QOS,           12, "ls_in_qos")           \
+    PIPELINE_STAGE(SWITCH, IN,  LB_AFF_CHECK,  13, "ls_in_lb_aff_check")  \
+    PIPELINE_STAGE(SWITCH, IN,  LB,            14, "ls_in_lb")            \
+    PIPELINE_STAGE(SWITCH, IN,  LB_AFF_LEARN,  15, "ls_in_lb_aff_learn")  \
+    PIPELINE_STAGE(SWITCH, IN,  PRE_HAIRPIN,   16, "ls_in_pre_hairpin")   \
+    PIPELINE_STAGE(SWITCH, IN,  NAT_HAIRPIN,   17, "ls_in_nat_hairpin")   \
+    PIPELINE_STAGE(SWITCH, IN,  HAIRPIN,       18, "ls_in_hairpin")       \
+    PIPELINE_STAGE(SWITCH, IN,  ACL_AFTER_LB_EVAL,  19, \
+                   "ls_in_acl_after_lb_eval")    \
+     PIPELINE_STAGE(SWITCH, IN,  ACL_AFTER_LB_SAMPLE,  20, \
                    "ls_in_acl_after_lb_sample")  \
-    PIPELINE_STAGE(SWITCH, IN,  ACL_AFTER_LB_ACTION,  20, \
+    PIPELINE_STAGE(SWITCH, IN,  ACL_AFTER_LB_ACTION,  21,    \
                    "ls_in_acl_after_lb_action")  \
-    PIPELINE_STAGE(SWITCH, IN,  STATEFUL,      21, "ls_in_stateful")      \
-    PIPELINE_STAGE(SWITCH, IN,  ARP_ND_RSP,    22, "ls_in_arp_rsp")       \
-    PIPELINE_STAGE(SWITCH, IN,  DHCP_OPTIONS,  23, "ls_in_dhcp_options")  \
-    PIPELINE_STAGE(SWITCH, IN,  DHCP_RESPONSE, 24, "ls_in_dhcp_response") \
-    PIPELINE_STAGE(SWITCH, IN,  DNS_LOOKUP,    25, "ls_in_dns_lookup")    \
-    PIPELINE_STAGE(SWITCH, IN,  DNS_RESPONSE,  26, "ls_in_dns_response")  \
-    PIPELINE_STAGE(SWITCH, IN,  EXTERNAL_PORT, 27, "ls_in_external_port") \
-    PIPELINE_STAGE(SWITCH, IN,  L2_LKUP,       28, "ls_in_l2_lkup")       \
-    PIPELINE_STAGE(SWITCH, IN,  L2_UNKNOWN,    29, "ls_in_l2_unknown")    \
+    PIPELINE_STAGE(SWITCH, IN,  STATEFUL,      22, "ls_in_stateful")      \
+    PIPELINE_STAGE(SWITCH, IN,  ARP_ND_RSP,    23, "ls_in_arp_rsp")       \
+    PIPELINE_STAGE(SWITCH, IN,  DHCP_OPTIONS,  24, "ls_in_dhcp_options")  \
+    PIPELINE_STAGE(SWITCH, IN,  DHCP_RESPONSE, 25, "ls_in_dhcp_response") \
+    PIPELINE_STAGE(SWITCH, IN,  DNS_LOOKUP,    26, "ls_in_dns_lookup")    \
+    PIPELINE_STAGE(SWITCH, IN,  DNS_RESPONSE,  27, "ls_in_dns_response")  \
+    PIPELINE_STAGE(SWITCH, IN,  EXTERNAL_PORT, 28, "ls_in_external_port") \
+    PIPELINE_STAGE(SWITCH, IN,  L2_LKUP,       29, "ls_in_l2_lkup")       \
+    PIPELINE_STAGE(SWITCH, IN,  L2_UNKNOWN,    30, "ls_in_l2_unknown")    \
                                                                           \
     /* Logical switch egress stages. */                                   \
     PIPELINE_STAGE(SWITCH, OUT, LOOKUP_FDB,      0, "ls_out_lookup_fdb")     \
@@ -514,10 +517,11 @@ enum ovn_stage {
     PIPELINE_STAGE(SWITCH, OUT, ACL_EVAL,        6, "ls_out_acl_eval")       \
     PIPELINE_STAGE(SWITCH, OUT, ACL_SAMPLE,      7, "ls_out_acl_sample")     \
     PIPELINE_STAGE(SWITCH, OUT, ACL_ACTION,      8, "ls_out_acl_action")     \
-    PIPELINE_STAGE(SWITCH, OUT, QOS,             9, "ls_out_qos")            \
-    PIPELINE_STAGE(SWITCH, OUT, STATEFUL,       10, "ls_out_stateful")       \
-    PIPELINE_STAGE(SWITCH, OUT, CHECK_PORT_SEC, 11, "ls_out_check_port_sec") \
-    PIPELINE_STAGE(SWITCH, OUT, APPLY_PORT_SEC, 12, "ls_out_apply_port_sec") \
+    PIPELINE_STAGE(SWITCH, OUT, MIRROR,          9, "ls_out_mirror")         \
+    PIPELINE_STAGE(SWITCH, OUT, QOS,            10, "ls_out_qos")            \
+    PIPELINE_STAGE(SWITCH, OUT, STATEFUL,       11, "ls_out_stateful")       \
+    PIPELINE_STAGE(SWITCH, OUT, CHECK_PORT_SEC, 12, "ls_out_check_port_sec") \
+    PIPELINE_STAGE(SWITCH, OUT, APPLY_PORT_SEC, 13, "ls_out_apply_port_sec") \
                                                                       \
     /* Logical router ingress stages. */                              \
     PIPELINE_STAGE(ROUTER, IN,  ADMISSION,       0, "lr_in_admission")    \
@@ -687,6 +691,10 @@ struct ovn_port {
      * only if this port is a distributed gateway port. Otherwise this is set
      * to NULL. */
     struct ovn_port *cr_port;
+
+    /* If this ovn_port is a mirror serving port, this field is set for
+     * a parent port. */
+    struct ovn_port *mirror_target_port;
 
     bool has_unknown; /* If the addresses have 'unknown' defined. */
 

tests/ovn.at
@@ -803,6 +803,7 @@ m4_define([NEXT], [m4_if(
 
 m4_define([oflow_in_table], [NEXT(ingress, lflow_table)])
 m4_define([oflow_out_table], [NEXT(egress, lflow_table)])
+m4_define([remote_out_table], [OFTABLE_REMOTE_OUTPUT])
 
 AT_DATA([test-cases.txt], [
 # drop
@@ -2308,6 +2309,13 @@ ct_state_save;
 
 ct_state_save();
     Syntax error at `ct_state_save' expecting action.
+
+#mirror
+mirror("lsp1");
+    encodes as clone(set_field:ANY->in_port,set_field:0x11->reg15,resubmit(,remote_out_table))
+
+mirror(lsp1);
+    Syntax error at `lsp1' expecting port name string.
 
 # Miscellaneous negative tests.
 ;

controller/lflow.h
@@ -67,17 +67,17 @@ struct uuid;
 
 /* Start of LOG_PIPELINE_LEN tables. */
 #define OFTABLE_LOG_INGRESS_PIPELINE      8
-#define OFTABLE_OUTPUT_LARGE_PKT_DETECT  40
-#define OFTABLE_OUTPUT_LARGE_PKT_PROCESS 41
-#define OFTABLE_REMOTE_OUTPUT            42
-#define OFTABLE_LOCAL_OUTPUT             43
-#define OFTABLE_CHECK_LOOPBACK           44
+#define OFTABLE_OUTPUT_LARGE_PKT_DETECT  41
+#define OFTABLE_OUTPUT_LARGE_PKT_PROCESS 42
+#define OFTABLE_REMOTE_OUTPUT            43
+#define OFTABLE_LOCAL_OUTPUT             44
+#define OFTABLE_CHECK_LOOPBACK           45
 
 /* Start of the OUTPUT section of the pipeline. */
 #define OFTABLE_OUTPUT_INIT OFTABLE_OUTPUT_LARGE_PKT_DETECT
 
 /* Start of LOG_PIPELINE_LEN tables. */
-#define OFTABLE_LOG_EGRESS_PIPELINE      45
+#define OFTABLE_LOG_EGRESS_PIPELINE      46
 #define OFTABLE_SAVE_INPORT              64
 #define OFTABLE_LOG_TO_PHY               65
 #define OFTABLE_MAC_BINDING              66

include/ovn/actions.h
@@ -135,7 +135,8 @@ struct collector_set_ids;
     OVNACT(CT_ORIG_IP6_DST,   ovnact_result)          \
     OVNACT(CT_ORIG_TP_DST,    ovnact_result)          \
     OVNACT(FLOOD_REMOTE,      ovnact_null)            \
-    OVNACT(CT_STATE_SAVE,     ovnact_result)
+    OVNACT(CT_STATE_SAVE,     ovnact_result)          \
+    OVNACT(MIRROR,            ovnact_mirror)          \
 
 /* enum ovnact_type, with a member OVNACT_<ENUM> for each action. */
 enum OVS_PACKED_ENUM ovnact_type {
@@ -536,6 +537,15 @@ struct ovnact_commit_lb_aff {
     uint16_t backend_port;
 
     uint16_t timeout;
+};
+
+/* OVNACT_MIRROR. */
+struct ovnact_mirror {
+    struct ovnact ovnact;
+
+    /* Argument. */
+    char *port;     /* Mirror serving port for output
+                       action. */
 };
 
 #define OVN_FIELD_NOTE_MAGIC "ovn"

lib/actions.c
@@ -5575,6 +5575,76 @@ format_CT_STATE_SAVE(const struct ovnact_result *res, struct ds *s)
     ds_put_cstr(s, " = ct_state_save();");
 }
 
+static void
+parse_MIRROR_action(struct action_context *ctx)
+{
+    if (!lexer_force_match(ctx->lexer, LEX_T_LPAREN)) {
+        return;
+    }
+
+    if (ctx->lexer->token.type != LEX_T_STRING) {
+        lexer_syntax_error(ctx->lexer, "expecting port name string");
+        return;
+    }
+
+    struct ovnact_mirror *mirror = ovnact_put_MIRROR(ctx->ovnacts);
+    mirror->port = xstrdup(ctx->lexer->token.s);
+
+    lexer_get(ctx->lexer);
+    lexer_force_match(ctx->lexer, LEX_T_RPAREN);
+}
+
+static void
+format_MIRROR(const struct ovnact_mirror *mirror,
+              struct ds *s)
+{
+    ds_put_cstr(s, "mirror(");
+    ds_put_format(s, "\"%s\"", mirror->port);
+    ds_put_cstr(s, ");");
+}
+
+static void
+encode_MIRROR(const struct ovnact_mirror *mirror,
+              const struct ovnact_encode_params *ep,
+              struct ofpbuf *ofpacts)
+{
+    size_t clone_ofs = ofpacts->size;
+    uint32_t vport_key;
+
+    if (!ep->lookup_port(ep->aux, mirror->port, &vport_key)) {
+        return;
+    }
+
+    struct ofpact_nest *clone = ofpact_put_CLONE(ofpacts);
+
+    /*  We need set in_port to 0. Consider the following configuration:
+     *  - hostA (target lport, source lport) + (dst lport) hostB
+     *  - mirror in both directions (to and from lport) attached
+     *    to dst port.
+     *
+     *  When a packet comes from lport source to dst lport, for cloned
+     *  mirrored packet inport will be equal to outport, and incoming
+     *  traffic will not be mirrored.
+     *
+     *  We have no problem with zeroing in_port: it will be no recirculations
+     *  for packet proccesing on hostA, because we skip conntrack for traffic
+     *  directed to the target port.
+     */
+    put_load(ofp_to_u16(OFPP_NONE), MFF_IN_PORT, 0, 16, ofpacts);
+    put_load(vport_key, MFF_LOG_OUTPORT, 0, 32, ofpacts);
+    emit_resubmit(ofpacts, OFTABLE_REMOTE_OUTPUT);
+    clone = ofpbuf_at_assert(ofpacts, clone_ofs, sizeof *clone);
+    ofpacts->header = clone;
+
+    ofpact_finish_CLONE(ofpacts, &clone);
+}
+
+static void
+ovnact_mirror_free(struct ovnact_mirror *mirror OVS_UNUSED)
+{
+    free(mirror->port);
+}
+
 /* Parses an assignment or exchange or put_dhcp_opts action. */
 static void
 parse_set_action(struct action_context *ctx)
@@ -5808,6 +5878,8 @@ parse_action(struct action_context *ctx)
         ovnact_put_MAC_CACHE_USE(ctx->ovnacts);
     } else if (lexer_match_id(ctx->lexer, "flood_remote")) {
         ovnact_put_FLOOD_REMOTE(ctx->ovnacts);
+    } else if (lexer_match_id(ctx->lexer, "mirror")) {
+        parse_MIRROR_action(ctx);
     } else {
         lexer_syntax_error(ctx->lexer, "expecting action");
     }

lib/ovn-util.h
@@ -199,6 +199,8 @@ get_sb_port_group_name(const char *nb_pg_name, int64_t dp_tunnel_key,
 }
 
 char *ovn_chassis_redirect_name(const char *port_name);
+char *ovn_mirror_port_name(const char *datapath_name,
+                           const char *port_name);
 void ovn_set_pidfile(const char *name);
 
 bool ip46_parse_cidr(const char *str, struct in6_addr *prefix,
@@ -312,8 +314,8 @@ BUILD_ASSERT_DECL(
 #define SCTP_ABORT_CHUNK_FLAG_T (1 << 0)
 
 /* The number of tables for the ingress and egress pipelines. */
-#define LOG_PIPELINE_INGRESS_LEN 30
-#define LOG_PIPELINE_EGRESS_LEN 13
+#define LOG_PIPELINE_INGRESS_LEN 31
+#define LOG_PIPELINE_EGRESS_LEN 14
 
 static inline uint32_t
 hash_add_in6_addr(uint32_t hash, const struct in6_addr *addr)

northd/en-northd.c
@@ -61,6 +61,10 @@ northd_get_input_data(struct engine_node *node,
         engine_ovsdb_node_get_index(
             engine_get_input("SB_fdb", node),
             "sbrec_fdb_by_dp_and_port");
+    input_data->nbrec_mirror_by_type_and_sink =
+        engine_ovsdb_node_get_index(
+            engine_get_input("NB_mirror", node),
+            "nbrec_mirror_by_type_and_sink");
 
     input_data->nbrec_logical_switch_table =
         EN_OVSDB_GET(engine_get_input("NB_logical_switch", node));
@@ -72,6 +76,8 @@ northd_get_input_data(struct engine_node *node,
         EN_OVSDB_GET(engine_get_input("NB_chassis_template_var", node));
     input_data->nbrec_mirror_table =
         EN_OVSDB_GET(engine_get_input("NB_mirror", node));
+    input_data->nbrec_mirror_rule_table =
+        EN_OVSDB_GET(engine_get_input("NB_mirror_rule", node));
 
     input_data->sbrec_datapath_binding_table =
         EN_OVSDB_GET(engine_get_input("SB_datapath_binding", node));

commit c312441933f71458d93aa0b3dd192362555e75ec

northd: Move ingress logical port mirroring before port security.
Currently, ingress logical port mirroring does not take ACL rules into
account, allowing the receiver interface (the sink in OVN) to observe
the same traffic that exits the port.

Ingress mirroring should also bypass port security checks. This is
important because packets sent from a mirrored port that do not originate
from the port’s MAC address would otherwise be dropped, potentially
masking issues such as unexpected virtual machine behavior.

Fixes: 2a2fe266d09c ("northd: Added support for port mirroring in OVN overlay.")
Signed-off-by: Alexandra Rukomoinikova <arukomoinikova@k2.cloud>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/ovn-util.c
@@ -1026,8 +1026,8 @@ ip_address_and_port_from_lb_key(const char *key, char **ip_address,
  *
  * NOTE: If OVN_NORTHD_PIPELINE_CSUM is updated make sure to double check
  * whether an update of OVN_INTERNAL_MINOR_VER is required. */
-#define OVN_NORTHD_PIPELINE_CSUM "3760014456 11249"
-#define OVN_INTERNAL_MINOR_VER 13
+#define OVN_NORTHD_PIPELINE_CSUM "2129825571 11245"
+#define OVN_INTERNAL_MINOR_VER 14
 
 /* Returns the OVN version. The caller must free the returned value. */
 char *

northd/northd.h
@@ -509,9 +509,9 @@ ovn_datapath_is_stale(const struct ovn_datapath *od)
 /* Pipeline stages. */
 #define PIPELINE_STAGES                                                   \
     /* Logical switch ingress stages. */                                  \
-    PIPELINE_STAGE(SWITCH, IN,  CHECK_PORT_SEC, 0, "ls_in_check_port_sec")   \
-    PIPELINE_STAGE(SWITCH, IN,  APPLY_PORT_SEC, 1, "ls_in_apply_port_sec")   \
-    PIPELINE_STAGE(SWITCH, IN,  MIRROR,         2, "ls_in_mirror")        \
+    PIPELINE_STAGE(SWITCH, IN,  MIRROR,         0, "ls_in_mirror")        \
+    PIPELINE_STAGE(SWITCH, IN,  CHECK_PORT_SEC, 1, "ls_in_check_port_sec") \
+    PIPELINE_STAGE(SWITCH, IN,  APPLY_PORT_SEC, 2, "ls_in_apply_port_sec") \
     PIPELINE_STAGE(SWITCH, IN,  LOOKUP_FDB,     3, "ls_in_lookup_fdb")    \
     PIPELINE_STAGE(SWITCH, IN,  PUT_FDB,        4, "ls_in_put_fdb")       \
     PIPELINE_STAGE(SWITCH, IN,  PRE_ACL,        5, "ls_in_pre_acl")       \

tests/ovn.at
@@ -19122,9 +19122,25 @@ OVN_CHECK_PACKETS([hv3/vif1-tx.pcap], [expected])
 as hv2 reset_pcap_file hv2-vif1 hv2/vif1
 as hv3 reset_pcap_file hv3-vif1 hv3/vif1
 
-# Test mirror filtering.
 check ovn-nbctl lsp-attach-mirror ls1-lp1 mirror0
 
+# Ensure that port security on the source port does not impact mirroring:
+# send a packet with an unknown MAC and checking it appears on the sink port.
+check ovn-nbctl lsp-set-port-security ls1-lp1 $ls1_lp1_mac
+
+fake_mac="f1:f1:f1:f1:f1:04"
+packet="inport==\"ls1-lp1\" && eth.src==$fake_mac && eth.dst==$rp_ls1_mac &&
+        ip4 && ip.ttl==64 && ip4.src==$ls1_lp1_ip && ip4.dst==$ls2_lp1_ip &&
+        udp && udp.src==53 && udp.dst==4369"
+OVS_WAIT_UNTIL([as hv1 ovs-appctl -t ovn-controller inject-pkt "$packet"])
+
+echo $packet | ovstest test-ovn expr-to-packets > packet
+
+OVN_CHECK_PACKETS([hv3/vif1-tx.pcap], [packet])
+
+as hv3 reset_pcap_file hv3-vif1 hv3/vif1
+
+# Test mirror filtering.
 check ovn-nbctl mirror-rule-add mirror0 200 '1' skip
 check ovn-nbctl --wait=hv sync
 