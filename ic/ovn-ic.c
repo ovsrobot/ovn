ovsrobot/ovn

commit 052a298bb90e17cdb68c500f0ca4b9ee3e102b65

ovn-ic: Use dual IC-SB connections to prevent constraint violations.
When multiple Availability Zones (AZs) are connected via OVN-IC,
certain events trigger all AZs to attempt writing the same data to the
IC-SB simultaneously. This race condition leads to constraint
violations, causing transaction failures and forcing expensive full
recomputes.

To mitigate this, this patch introduces a write-segregation mechanism
employing two distinct connections to IC-SB:
1. Locked Connection: Acquires a lock to handle data that only a
   single AZ should write (e.g., creating a new datapath_binding for a
   transit switch/router).

2. Lockless Connections: Used for data that multiple AZs can safely
   insert concurrently (e.g., creating a new port_binding).

Signed-off-by: Tiago Matos <tiago.reis@luizalabs.com>
Signed-off-by: Ales Musil <amusil@redhat.com>

ic/ovn-ic.h
@@ -22,10 +22,12 @@ struct ic_context {
     struct ovsdb_idl *ovnsb_idl;
     struct ovsdb_idl *ovninb_idl;
     struct ovsdb_idl *ovnisb_idl;
+    struct ovsdb_idl *ovnisb_unlocked_idl;
     struct ovsdb_idl_txn *ovnnb_txn;
     struct ovsdb_idl_txn *ovnsb_txn;
     struct ovsdb_idl_txn *ovninb_txn;
     struct ovsdb_idl_txn *ovnisb_txn;
+    struct ovsdb_idl_txn *ovnisb_unlocked_txn;
     const struct icsbrec_availability_zone *runned_az;
     struct ovsdb_idl_index *nbrec_ls_by_name;
     struct ovsdb_idl_index *nbrec_lr_by_name;

tests/ovn-ic.at
@@ -4793,3 +4793,49 @@ OVN_CLEANUP_IC([az1], [az2])
 
 AT_CLEANUP
 ])
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn-ic - IC-SB lock acquisition and transit switch creation])
+
+ovn_init_ic_db
+ovn_start az1
+ovn_start az2
+
+wait_row_count ic-sb:Availability_Zone 2
+
+ovn_as az1
+check ovn-ic-nbctl ts-add ts1
+
+wait_row_count ic-sb:Datapath_Binding 1 transit_switch=ts1
+as az1 check ovn-appctl -t ic/ovn-ic pause
+
+ovn_as az2
+check ovn-ic-nbctl ts-add ts2
+wait_row_count ic-sb:Datapath_Binding 1 transit_switch=ts2
+
+check_row_count ic-sb:Datapath_Binding 2
+check_column "ts1 ts2" ic-sb:Datapath_Binding transit_switch
+
+as az1 check ovn-appctl -t ic/ovn-ic resume
+
+as az2 check ovn-appctl -t ic/ovn-ic pause
+
+ovn_as az1
+check ovn-ic-nbctl ts-add ts3
+
+wait_row_count ic-sb:Datapath_Binding 1 transit_switch=ts3
+check_row_count ic-sb:Datapath_Binding 3
+check_column "ts1 ts2 ts3" ic-sb:Datapath_Binding transit_switch
+
+as az2 check ovn-appctl -t ic/ovn-ic resume
+
+for i in 1 2; do
+    az=az$i
+    ovn_as $az
+    OVN_CLEANUP_AZ([$az])
+done
+
+OVN_CLEANUP_IC
+
+AT_CLEANUP
+])

ic/inc-proc-ic.c
@@ -27,6 +27,7 @@
 #include "openvswitch/vlog.h"
 #include "inc-proc-ic.h"
 #include "en-ic.h"
+#include "ovn-util.h"
 #include "unixctl.h"
 #include "util.h"
 
@@ -214,7 +215,7 @@ inc_proc_ic_run(struct ic_context *ctx,
                 struct ic_engine_context *ic_eng_ctx)
 {
     ovs_assert(ctx->ovnnb_txn && ctx->ovnsb_txn &&
-               ctx->ovninb_txn && ctx->ovnisb_txn);
+               ctx->ovninb_txn && ctx->ovnisb_unlocked_txn);
 
     int64_t start = time_msec();
     engine_init_run();
@@ -262,7 +263,8 @@ inc_proc_ic_can_run(struct ic_engine_context *ctx)
         ctx->nb_idl_duration_ms >= IDL_LOOP_MAX_DURATION_MS ||
         ctx->sb_idl_duration_ms >= IDL_LOOP_MAX_DURATION_MS ||
         ctx->inb_idl_duration_ms >= IDL_LOOP_MAX_DURATION_MS ||
-        ctx->isb_idl_duration_ms >= IDL_LOOP_MAX_DURATION_MS) {
+        ctx->isb_idl_duration_ms >= IDL_LOOP_MAX_DURATION_MS ||
+        ctx->isb_unlock_idl_duration_ms >= IDL_LOOP_MAX_DURATION_MS) {
         return true;
     }
 

ic/inc-proc-ic.h
@@ -13,6 +13,7 @@ struct ic_engine_context {
     uint64_t sb_idl_duration_ms;
     uint64_t inb_idl_duration_ms;
     uint64_t isb_idl_duration_ms;
+    uint64_t isb_unlock_idl_duration_ms;
     uint32_t backoff_ms;
 };
 

commit 15a0dc37aa1ec67d9f6ac4008f5f4398f4dd045c

ovn-ic: Fix race condition when ovn-ic is paused.
When az1 is paused, there is a small time window during which no az is
active and owns the lock (the time for ovn-ic-sb to receive the unlock
from az1 and notify az2).
Any ovn-ic-nb change received during this time, which would have resulted
in ovn-ic-sb changes, would be lost (from a ovn-ic-sb perspective), as
az1 is paused and az2 is not (yet) the leader.

While at it, properly log when ISB is really locked: previous log was
confusing as logging when acquiring the lock (which could fail/takes some
time).

This issue caused the following test to fail sporadically:
"ovn-ic - IC-SB lock acquisition and transit switch creation"

Fixes: 052a298bb90e ("ovn-ic: Use dual IC-SB connections to prevent constraint violations.")
Signed-off-by: Xavier Simonart <xsimonar@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ic/ovn-ic.c
@@ -3809,6 +3809,7 @@ main(int argc, char *argv[])
 
     exiting = false;
     state.had_lock = false;
+    state.had_isb_lock = false;
     state.paused = false;
 
     while (!exiting) {
@@ -3844,8 +3845,7 @@ main(int argc, char *argv[])
                  * Ensure that only a single ovn-ic has the permission to
                  * write to IC-SB.
                  */
-                VLOG_INFO("OVN ISB lock acquired. "
-                          "This ovn-ic instance is now active.");
+                VLOG_INFO("Acquiring OVN ISB lock.");
                 ovsdb_idl_set_lock(ovnisb_idl_loop.idl, "ovn_ic_sb");
             }
 
@@ -3969,6 +3969,17 @@ main(int argc, char *argv[])
                 state.had_lock = false;
             }
 
+            if (!state.had_isb_lock && ovsdb_idl_has_lock(ctx.ovnisb_idl)) {
+                VLOG_INFO("OVN ISB lock acquired. "
+                          "This ovn-ic instance is now active.");
+                state.had_isb_lock = true;
+                inc_proc_ic_force_recompute_immediate();
+            } else if (state.had_isb_lock &&
+                       !ovsdb_idl_has_lock(ctx.ovnisb_idl)) {
+                VLOG_INFO("OVN ISB lock lost.");
+                state.had_isb_lock = false;
+            }
+
             if (ovsdb_idl_has_lock(ctx.ovnsb_idl) &&
                 ovsdb_idl_has_ever_connected(ctx.ovnnb_idl) &&
                 ovsdb_idl_has_ever_connected(ctx.ovnsb_idl) &&
@@ -4055,6 +4066,7 @@ main(int argc, char *argv[])
                 VLOG_INFO("This ovn-ic instance is now paused. "
                           "Removing IC-SB lock.");
                 ovsdb_idl_set_lock(ovnisb_idl_loop.idl, NULL);
+                state.had_isb_lock = false;
             }
 
             if (ovsdb_idl_has_lock(ovnsb_idl_loop.idl) ||

ic/ovn-ic.h
@@ -53,7 +53,8 @@ struct ic_context {
 };
 
 struct ic_state {
-    bool had_lock;
+    bool had_lock; /* Lock to local AZ SB */
+    bool had_isb_lock; /* Lock to ISB */
     bool paused;
 };
 

tests/ovn-ic.at
@@ -4871,6 +4871,26 @@ check_column "ts1 ts2 ts3" ic-sb:Datapath_Binding transit_switch
 
 as az2 check ovn-appctl -t ic/ovn-ic resume
 
+# Now do the same on a "slow" ovn-ic-sb
+OVS_WAIT_UNTIL([test "x$(as az2 ovn-appctl -t ic/ovn-ic status)" = "xStatus: active"])
+
+AS_BOX([$(date +%H:%M:%S.%03N) Pausing ovn-ic-sb])
+AT_CHECK([kill -STOP $(cat ovn-ic-sb/ovsdb-server.pid)])
+
+as az1 check ovn-appctl -t ic/ovn-ic pause
+OVS_WAIT_UNTIL([test "x$(as az1 ovn-appctl -t ic/ovn-ic status)" = "xStatus: paused"])
+
+check ovn-ic-nbctl ts-add ts4
+
+AS_BOX([$(date +%H:%M:%S.%03N) Resuming ovn-ic-sb])
+AT_CHECK([kill -CONT $(cat ovn-ic-sb/ovsdb-server.pid)])
+
+wait_row_count ic-sb:Datapath_Binding 1 transit_switch=ts4
+check_row_count ic-sb:Datapath_Binding 4
+check_column "ts1 ts2 ts3 ts4" ic-sb:Datapath_Binding transit_switch
+
+as az1 check ovn-appctl -t ic/ovn-ic resume
+
 for i in 1 2; do
     az=az$i
     ovn_as $az