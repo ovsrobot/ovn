ovsrobot/ovn

commit ddc020051e0ac5ea2cf4f801cf7b17e8866c8bc5

ic: Fix loop disable/enable logical router.
icsbrec_port_binding set address to empty string, however
the in NB, the router-port exists. So, all the processes of
sync learned route executes.
Collect lr routes doesn't fill ic route info because
it doesn't extract lsp address. Sync learned routes doesn't find
the route and always add the route, generating a loop.
To fix it, sync learned route doesn't execute when address is empty
or logical router is disabled.

Signed-off-by: Lucas Vargas Dias <lucas.vdias@luizalabs.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

ic/ovn-ic.c
@@ -1519,6 +1519,10 @@ route_need_learn(const struct nbrec_logical_router *lr,
         return false;
     }
 
+    if (!lrouter_is_enabled(lr)) {
+        return false;
+    }
+
     if (prefix_is_link_local(prefix, plen)) {
         return false;
     }
@@ -1685,6 +1689,9 @@ sync_learned_routes(struct ic_context *ctx,
     const struct icsbrec_port_binding *isb_pb;
     const struct nbrec_logical_router_port *lrp;
     VECTOR_FOR_EACH (&ic_lr->isb_pbs, isb_pb) {
+        if (!strcmp(isb_pb->address, "")) {
+            continue;
+        }
         lrp_name = get_lrp_name_by_ts_port_name(ctx, isb_pb->logical_port);
         lrp = get_lrp_by_lrp_name(ctx, lrp_name);
         if (lrp) {
@@ -2943,6 +2950,8 @@ main(int argc, char *argv[])
                          &nbrec_logical_router_col_options);
     ovsdb_idl_add_column(ovnnb_idl_loop.idl,
                          &nbrec_logical_router_col_external_ids);
+    ovsdb_idl_add_column(ovnnb_idl_loop.idl,
+                         &nbrec_logical_router_col_enabled);
     ovsdb_idl_add_column(ovnnb_idl_loop.idl,
                          &nbrec_logical_router_col_load_balancer);
     ovsdb_idl_add_column(ovnnb_idl_loop.idl,

tests/ovn-ic.at
@@ -3526,6 +3526,17 @@ OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr11 | grep 192.168 |
 192.168.0.0/24 169.254.101.22
 ])
 
+check ovn_as az1 ovn-nbctl set logical_router lr11 enable=false
+OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr11 | grep 192.168 |
+             grep learned | awk '{print $1, $2}' | sort ], [0], [dnl
+])
+
+check ovn_as az1 ovn-nbctl set logical_router lr11 enable=true
+OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr11 | grep 192.168 |
+             grep learned | awk '{print $1, $2}' | sort ], [0], [dnl
+192.168.0.0/24 169.254.101.22
+])
+
 OVN_CLEANUP_IC([az1], [az2])
 
 AT_CLEANUP

lib/ovn-util.c
@@ -39,6 +39,12 @@ VLOG_DEFINE_THIS_MODULE(ovn_util);
 
 #define DEFAULT_PROBE_INTERVAL_MSEC 5000
 
+bool
+lrouter_is_enabled(const struct nbrec_logical_router *lrouter)
+{
+    return !lrouter->enabled || *lrouter->enabled;
+}
+
 void ovn_conn_show(struct unixctl_conn *conn, int argc OVS_UNUSED,
                    const char *argv[] OVS_UNUSED, void *idl_)
 {

lib/ovn-util.h
@@ -40,6 +40,7 @@
 #define VXLAN_TUNNEL_OVERHEAD 30
 
 struct eth_addr;
+struct nbrec_logical_router;
 struct nbrec_logical_router_port;
 struct ovsrec_flow_sample_collector_set_table;
 struct sbrec_datapath_binding;
@@ -100,6 +101,7 @@ ipv6_is_all_site_router(const struct in6_addr *addr)
     return ipv6_addr_equals(addr, &in6addr_all_site_routers);
 }
 
+bool lrouter_is_enabled(const struct nbrec_logical_router *);
 bool is_dynamic_lsp_address(const char *address);
 bool extract_addresses(const char *address, struct lport_addresses *,
                        int *ofs);

northd/northd.c
@@ -636,12 +636,6 @@ ovn_datapath_from_sbrec(const struct hmap *ls_datapaths,
     return NULL;
 }
 
-static bool
-lrouter_is_enabled(const struct nbrec_logical_router *lrouter)
-{
-    return !lrouter->enabled || *lrouter->enabled;
-}
-
 static void
 init_mcast_info_for_router_datapath(struct ovn_datapath *od)
 {

commit 19c93a2cfcafff7752ae27e38fed34818f626409

ovn-ic: Fix routes not deleted when lr is disabled.
Fix routes not deleted when lr is disabled and pb address is cleared
simultaneously. When a logical router is disabled, northd clears the
IC-SB port binding address. If ovn-ic handles both changes in the same
iteration, learned routes were not deleted.

This issue was highlighted by sporadic failures of test [0].
[0] "ovn-ic -- Check loop with lsp orphan in same subnet of new lsp in other TS".

Fixes: ddc020051e0a ("ic: Fix loop disable/enable logical router.")
Signed-off-by: Xavier Simonart <xsimonar@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

ic/ovn-ic.c
@@ -2405,6 +2405,32 @@ advertise_routes(struct ic_context *ctx,
 }
 
 static void
+collect_learned_routes(struct ic_router_info *ic_lr)
+{
+    const struct nbrec_logical_router *lr = ic_lr->lr;
+
+    /* Check static routes of the LR and collect learned routes */
+    for (int i = 0; i < lr->n_static_routes; i++) {
+        const struct nbrec_logical_router_static_route *nb_route
+            = lr->static_routes[i];
+        struct uuid isb_uuid;
+        if (smap_get_uuid(&nb_route->external_ids, "ic-learned-route",
+                          &isb_uuid)) {
+            /* It is a learned route */
+            if (!add_to_routes_learned(&ic_lr->routes_learned, nb_route, lr,
+                                       &isb_uuid)) {
+                static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(5, 1);
+                VLOG_WARN_RL(&rl, "Bad format of learned route in NB: "
+                             "%s -> %s. Delete it.", nb_route->ip_prefix,
+                             nb_route->nexthop);
+                nbrec_logical_router_update_static_routes_delvalue(lr,
+                    nb_route);
+            }
+        }
+    }
+}
+
+static void
 build_ts_routes_to_adv(struct ic_context *ctx,
                        struct ic_router_info *ic_lr,
                        struct hmap *routes_ad,
@@ -2526,6 +2552,7 @@ collect_lr_routes(struct ic_context *ctx,
 
     struct hmap *routes_ad;
     const struct icnbrec_transit_switch *t_sw;
+    bool routes_built = false;
     VECTOR_FOR_EACH (&ic_lr->isb_pbs, isb_pb) {
         key = icnbrec_transit_switch_index_init_row(
             ctx->icnbrec_transit_switch_by_name);
@@ -2561,9 +2588,16 @@ collect_lr_routes(struct ic_context *ctx,
             route_table = "";
             route_tag = "";
         }
+        routes_built = true;
         build_ts_routes_to_adv(ctx, ic_lr, routes_ad, &ts_port_addrs,
                                nb_global, route_table, route_tag, lrp);
         destroy_lport_addresses(&ts_port_addrs);
+    }
+    /* If no port binding had valid addresses (e.g. LR disabled
+     * and PB address cleared simultaneously), collect learned routes so
+     * they can be deleted by sync_learned_routes(). */
+    if (!routes_built) {
+        collect_learned_routes(ic_lr);
     }
 }
 

tests/ovn-ic.at
@@ -3407,6 +3407,25 @@ OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr11 | grep 192.168 |
 192.168.0.0/24 169.254.101.22
 ])
 
+# Do the same but with ovn-ic handling both northd and isb change at the same time.
+echo az1/ic going to sleep
+AT_CHECK([kill -STOP $(cat az1/ic/ovn-ic.pid)])
+on_exit "test -e az1/ic/ovn-ic.pid && kill -CONT $(cat az1/ic/ovn-ic.pid)"
+
+check ovn_as az1 ovn-nbctl --wait=sb set logical_router lr11 enable=false
+echo az1/ic going to wake up
+AT_CHECK([kill -CONT $(cat az1/ic/ovn-ic.pid)])
+
+OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr11 | grep 192.168 |
+             grep learned | awk '{print $1, $2}' | sort ], [0], [dnl
+])
+
+check ovn_as az1 ovn-nbctl set logical_router lr11 enable=true
+OVS_WAIT_FOR_OUTPUT([ovn_as az1 ovn-nbctl lr-route-list lr11 | grep 192.168 |
+             grep learned | awk '{print $1, $2}' | sort ], [0], [dnl
+192.168.0.0/24 169.254.101.22
+])
+
 OVN_CLEANUP_IC([az1], [az2])
 
 AT_CLEANUP