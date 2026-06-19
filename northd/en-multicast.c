ovsrobot/ovn

commit b741cb7e299c9e10cbec32af72f624c0cca39ba4

northd: Incremental processing of VIF updates and deletions in 'lflow' node.
This patch achieves zero recompute for VIF updates and deletions in
common scenarios. The performance gain for these scenarios is similar to
the patch "northd: Incremental processing of VIF additions in 'lflow'
node."

Signed-off-by: Han Zhou <hzhou@ovn.org>
Acked-by: Numan Siddique <numans@ovn.org>

northd/northd.c
@@ -16308,6 +16308,115 @@ void build_lflows(struct ovsdb_idl_txn *ovnsb_txn,
     hmap_destroy(&mcast_groups);
 }
 
+static void
+sync_lsp_lflows_to_sb(struct ovsdb_idl_txn *ovnsb_txn,
+                      struct lflow_input *lflow_input,
+                      struct hmap *lflows,
+                      struct ovn_lflow *lflow)
+{
+    size_t n_datapaths;
+    struct ovn_datapath **datapaths_array;
+    if (ovn_stage_to_datapath_type(lflow->stage) == DP_SWITCH) {
+        n_datapaths = ods_size(lflow_input->ls_datapaths);
+        datapaths_array = lflow_input->ls_datapaths->array;
+    } else {
+        n_datapaths = ods_size(lflow_input->lr_datapaths);
+        datapaths_array = lflow_input->lr_datapaths->array;
+    }
+    uint32_t n_ods = bitmap_count1(lflow->dpg_bitmap, n_datapaths);
+    ovs_assert(n_ods == 1);
+    /* There is only one datapath, so it should be moved out of the
+     * group to a single 'od'. */
+    size_t index = bitmap_scan(lflow->dpg_bitmap, true, 0,
+                               n_datapaths);
+
+    bitmap_set0(lflow->dpg_bitmap, index);
+    lflow->od = datapaths_array[index];
+
+    /* Logical flow should be re-hashed to allow lookups. */
+    uint32_t hash = hmap_node_hash(&lflow->hmap_node);
+    /* Remove from lflows. */
+    hmap_remove(lflows, &lflow->hmap_node);
+    hash = ovn_logical_flow_hash_datapath(&lflow->od->sb->header_.uuid,
+                                          hash);
+    /* Add back. */
+    hmap_insert(lflows, &lflow->hmap_node, hash);
+
+    /* Sync to SB. */
+    const struct sbrec_logical_flow *sbflow;
+    /* Note: uuid_random acquires a global mutex. If we parallelize the sync to
+     * SB this may become a bottleneck. */
+    lflow->sb_uuid = uuid_random();
+    sbflow = sbrec_logical_flow_insert_persist_uuid(ovnsb_txn,
+                                                    &lflow->sb_uuid);
+    const char *pipeline = ovn_stage_get_pipeline_name(lflow->stage);
+    uint8_t table = ovn_stage_get_table(lflow->stage);
+    sbrec_logical_flow_set_logical_datapath(sbflow, lflow->od->sb);
+    sbrec_logical_flow_set_logical_dp_group(sbflow, NULL);
+    sbrec_logical_flow_set_pipeline(sbflow, pipeline);
+    sbrec_logical_flow_set_table_id(sbflow, table);
+    sbrec_logical_flow_set_priority(sbflow, lflow->priority);
+    sbrec_logical_flow_set_match(sbflow, lflow->match);
+    sbrec_logical_flow_set_actions(sbflow, lflow->actions);
+    if (lflow->io_port) {
+        struct smap tags = SMAP_INITIALIZER(&tags);
+        smap_add(&tags, "in_out_port", lflow->io_port);
+        sbrec_logical_flow_set_tags(sbflow, &tags);
+        smap_destroy(&tags);
+    }
+    sbrec_logical_flow_set_controller_meter(sbflow, lflow->ctrl_meter);
+    /* Trim the source locator lflow->where, which looks something like
+     * "ovn/northd/northd.c:1234", down to just the part following the
+     * last slash, e.g. "northd.c:1234". */
+    const char *slash = strrchr(lflow->where, '/');
+#if _WIN32
+    const char *backslash = strrchr(lflow->where, '\\');
+    if (!slash || backslash > slash) {
+        slash = backslash;
+    }
+#endif
+    const char *where = slash ? slash + 1 : lflow->where;
+
+    struct smap ids = SMAP_INITIALIZER(&ids);
+    smap_add(&ids, "stage-name", ovn_stage_to_str(lflow->stage));
+    smap_add(&ids, "source", where);
+    if (lflow->stage_hint) {
+        smap_add(&ids, "stage-hint", lflow->stage_hint);
+    }
+    sbrec_logical_flow_set_external_ids(sbflow, &ids);
+    smap_destroy(&ids);
+}
+
+static bool
+delete_lflow_for_lsp(struct ovn_port *op, bool is_update,
+                     const struct sbrec_logical_flow_table *sb_lflow_table,
+                     struct hmap *lflows)
+{
+    struct lflow_ref_node *lfrn;
+    const char *operation = is_update ? "updated" : "deleted";
+    LIST_FOR_EACH_SAFE (lfrn, lflow_list_node, &op->lflows) {
+        VLOG_DBG("Deleting SB lflow "UUID_FMT" for %s port %s",
+                 UUID_ARGS(&lfrn->lflow->sb_uuid), operation, op->key);
+
+        const struct sbrec_logical_flow *sblflow =
+            sbrec_logical_flow_table_get_for_uuid(sb_lflow_table,
+                                              &lfrn->lflow->sb_uuid);
+        if (sblflow) {
+            sbrec_logical_flow_delete(sblflow);
+        } else {
+            static struct vlog_rate_limit rl =
+                VLOG_RATE_LIMIT_INIT(1, 1);
+            VLOG_WARN_RL(&rl, "SB lflow "UUID_FMT" not found when handling "
+                         "%s port %s. Recompute.",
+                         UUID_ARGS(&lfrn->lflow->sb_uuid), operation, op->key);
+            return false;
+        }
+
+        ovn_lflow_destroy(lflows, lfrn->lflow);
+    }
+    return true;
+}
+
 bool lflow_handle_northd_ls_changes(struct ovsdb_idl_txn *ovnsb_txn,
                                     struct tracked_ls_changes *ls_changes,
                                     struct lflow_input *lflow_input,
@@ -16315,13 +16424,6 @@ bool lflow_handle_northd_ls_changes(struct ovsdb_idl_txn *ovnsb_txn,
 {
     struct ls_change *ls_change;
     LIST_FOR_EACH (ls_change, list_node, &ls_changes->updated) {
-        if (!ovs_list_is_empty(&ls_change->updated_ports) ||
-            !ovs_list_is_empty(&ls_change->deleted_ports)) {
-            /* XXX: implement lflow index so that we can handle updated and
-             * deleted LSPs incrementally. */
-            return false;
-        }
-
         const struct sbrec_multicast_group *sbmc_flood =
             mcast_group_lookup(lflow_input->sbrec_mcast_group_by_name_dp,
                                MC_FLOOD, ls_change->od->sb);
@@ -16333,6 +16435,47 @@ bool lflow_handle_northd_ls_changes(struct ovsdb_idl_txn *ovnsb_txn,
                                MC_UNKNOWN, ls_change->od->sb);
 
         struct ovn_port *op;
+        LIST_FOR_EACH (op, list, &ls_change->deleted_ports) {
+            if (!delete_lflow_for_lsp(op, false,
+                                      lflow_input->sbrec_logical_flow_table,
+                                      lflows)) {
+                return false;
+            }
+
+            /* No need to update SB multicast groups, thanks to weak
+             * references. */
+        }
+
+        LIST_FOR_EACH (op, list, &ls_change->updated_ports) {
+            /* Delete old lflows. */
+            if (!delete_lflow_for_lsp(op, true,
+                                      lflow_input->sbrec_logical_flow_table,
+                                      lflows)) {
+                return false;
+            }
+
+            /* Generate new lflows. */
+            struct ds match = DS_EMPTY_INITIALIZER;
+            struct ds actions = DS_EMPTY_INITIALIZER;
+            build_lswitch_and_lrouter_iterate_by_lsp(op, lflow_input->ls_ports,
+                                                     lflow_input->lr_ports,
+                                                     lflow_input->meter_groups,
+                                                     &match, &actions,
+                                                     lflows);
+            ds_destroy(&match);
+            ds_destroy(&actions);
+
+            /* SB port_binding is not deleted, so don't update SB multicast
+             * groups. */
+
+            /* Sync the new flows to SB. */
+            struct lflow_ref_node *lfrn;
+            LIST_FOR_EACH (lfrn, lflow_list_node, &op->lflows) {
+                sync_lsp_lflows_to_sb(ovnsb_txn, lflow_input, lflows,
+                                      lfrn->lflow);
+            }
+        }
+
         LIST_FOR_EACH (op, list, &ls_change->added_ports) {
             struct ds match = DS_EMPTY_INITIALIZER;
             struct ds actions = DS_EMPTY_INITIALIZER;
@@ -16343,6 +16486,8 @@ bool lflow_handle_northd_ls_changes(struct ovsdb_idl_txn *ovnsb_txn,
                                                      lflows);
             ds_destroy(&match);
             ds_destroy(&actions);
+
+            /* Update SB multicast groups for the new port. */
             if (!sbmc_flood) {
                 sbmc_flood = create_sb_multicast_group(ovnsb_txn,
                     ls_change->od->sb, MC_FLOOD, OVN_MCAST_FLOOD_TUNNEL_KEY);
@@ -16369,80 +16514,8 @@ bool lflow_handle_northd_ls_changes(struct ovsdb_idl_txn *ovnsb_txn,
             /* Sync the newly added flows to SB. */
             struct lflow_ref_node *lfrn;
             LIST_FOR_EACH (lfrn, lflow_list_node, &op->lflows) {
-                struct ovn_lflow *lflow = lfrn->lflow;
-                size_t n_datapaths;
-                struct ovn_datapath **datapaths_array;
-                if (ovn_stage_to_datapath_type(lflow->stage) == DP_SWITCH) {
-                    n_datapaths = ods_size(lflow_input->ls_datapaths);
-                    datapaths_array = lflow_input->ls_datapaths->array;
-                } else {
-                    n_datapaths = ods_size(lflow_input->lr_datapaths);
-                    datapaths_array = lflow_input->lr_datapaths->array;
-                }
-                uint32_t n_ods = bitmap_count1(lflow->dpg_bitmap, n_datapaths);
-                ovs_assert(n_ods == 1);
-                /* There is only one datapath, so it should be moved out of the
-                 * group to a single 'od'. */
-                size_t index = bitmap_scan(lflow->dpg_bitmap, true, 0,
-                                           n_datapaths);
-
-                bitmap_set0(lflow->dpg_bitmap, index);
-                lflow->od = datapaths_array[index];
-
-                /* Logical flow should be re-hashed to allow lookups. */
-                uint32_t hash = hmap_node_hash(&lflow->hmap_node);
-                /* Remove from lflows. */
-                hmap_remove(lflows, &lflow->hmap_node);
-                hash = ovn_logical_flow_hash_datapath(
-                                          &lflow->od->sb->header_.uuid, hash);
-                /* Add back. */
-                hmap_insert(lflows, &lflow->hmap_node, hash);
-
-                /* Sync to SB. */
-                const struct sbrec_logical_flow *sbflow;
-                /* Note: uuid_random acquires a global mutex. If we parallelize
-                 * the sync to SB this may become a bottleneck. */
-                lflow->sb_uuid = uuid_random();
-                sbflow = sbrec_logical_flow_insert_persist_uuid(
-                                                ovnsb_txn, &lflow->sb_uuid);
-                const char *pipeline = ovn_stage_get_pipeline_name(
-                                                               lflow->stage);
-                uint8_t table = ovn_stage_get_table(lflow->stage);
-                sbrec_logical_flow_set_logical_datapath(sbflow, lflow->od->sb);
-                sbrec_logical_flow_set_logical_dp_group(sbflow, NULL);
-                sbrec_logical_flow_set_pipeline(sbflow, pipeline);
-                sbrec_logical_flow_set_table_id(sbflow, table);
-                sbrec_logical_flow_set_priority(sbflow, lflow->priority);
-                sbrec_logical_flow_set_match(sbflow, lflow->match);
-                sbrec_logical_flow_set_actions(sbflow, lflow->actions);
-                if (lflow->io_port) {
-                    struct smap tags = SMAP_INITIALIZER(&tags);
-                    smap_add(&tags, "in_out_port", lflow->io_port);
-                    sbrec_logical_flow_set_tags(sbflow, &tags);
-                    smap_destroy(&tags);
-                }
-                sbrec_logical_flow_set_controller_meter(sbflow,
-                                                        lflow->ctrl_meter);
-                /* Trim the source locator lflow->where, which looks something
-                 * like "ovn/northd/northd.c:1234", down to just the part
-                 * following the last slash, e.g. "northd.c:1234". */
-                const char *slash = strrchr(lflow->where, '/');
-#if _WIN32
-                const char *backslash = strrchr(lflow->where, '\\');
-                if (!slash || backslash > slash) {
-                    slash = backslash;
-                }
-#endif
-                const char *where = slash ? slash + 1 : lflow->where;
-
-                struct smap ids = SMAP_INITIALIZER(&ids);
-                smap_add(&ids, "stage-name", ovn_stage_to_str(lflow->stage));
-                smap_add(&ids, "source", where);
-                if (lflow->stage_hint) {
-                    smap_add(&ids, "stage-hint", lflow->stage_hint);
-                }
-                sbrec_logical_flow_set_external_ids(sbflow, &ids);
-                smap_destroy(&ids);
+                sync_lsp_lflows_to_sb(ovnsb_txn, lflow_input, lflows,
+                                      lfrn->lflow);
             }
         }
     }

tests/ovn-northd.at
@@ -9540,11 +9540,11 @@ for i in $(seq 10); do
 
     check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
     check ovn-nbctl --wait=hv lsp-del lsp${i}-1
-    check_recompute_counter 0 1 || continue
+    check_recompute_counter 0 0 || continue
 
     check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats
     check ovn-nbctl --wait=hv lsp-set-addresses lsp${i}-2 "aa:aa:aa:00:00:88 192.168.0.88"
-    check_recompute_counter 0 1 || continue
+    check_recompute_counter 0 0 || continue
 
     # No change, no recompute
     check as northd ovn-appctl -t NORTHD_TYPE inc-engine/clear-stats

commit 3f7cce9bdea5ace1b2b1b5037418b388346781cd

northd: Make sure we handle multicast group update.
The multicast groups were updated as part of the lflow handler
which is wrong. There is multicast group node which takes
care of the group updates. Also, the updates in the lflow handler
were partially wrong and not in line with the multicast group
node.

At the same time remove the lsp_can_receive_multicast() check that
would prevent the lflow creation for new LSPs.

Reported-at: https://redhat.atlassian.net/browse/FDP-4022
Assisted-by: Claude Opus 4.6, Claude Code
Fixes: b741cb7e299c ("northd: Incremental processing of VIF updates and deletions in 'lflow' node.")
Fixes: 8e2d6fa14804 ("northd, tests: Network Function insertion logical flow programming.")
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

northd/en-multicast.c
@@ -153,7 +153,10 @@ multicast_igmp_northd_handler(struct engine_node *node, void *data OVS_UNUSED)
         return EN_UNHANDLED;
     }
 
-    if (hmapx_count(&northd_data->trk_data.trk_switches.deleted)) {
+    struct tracked_ovn_ports *trk_lsps = &northd_data->trk_data.trk_lsps;
+    if (hmapx_count(&trk_lsps->created) ||
+        hmapx_count(&trk_lsps->updated) ||
+        hmapx_count(&trk_lsps->deleted)) {
         return EN_UNHANDLED;
     }
 
@@ -171,7 +174,7 @@ multicast_igmp_northd_handler(struct engine_node *node, void *data OVS_UNUSED)
      *      This node also accesses the router ports of the logical router
      *      (od->ports).  When these logical router ports gets updated,
      *      en_northd engine recomputes and so does this node.
-     *      Note: When we add I-P to handle switch/router port changes, we
+     *      Note: When we add I-P to handle router port changes, we
      *      need to revisit this handler.
      *
      * */

northd/northd.c
@@ -20464,29 +20464,12 @@ lflow_handle_northd_port_changes(struct ovsdb_idl_txn *ovnsb_txn,
         if (!handled) {
             return false;
         }
-
-        /* SB port_binding is not deleted, so don't update SB multicast
-         * groups. */
     }
 
     HMAPX_FOR_EACH (hmapx_node, &trk_lsps->created) {
         op = hmapx_node->data;
         /* Make sure 'op' is an lsp and not lrp. */
         ovs_assert(op->nbsp);
-
-        if (!lsp_can_receive_multicast(op->nbsp)) {
-            continue;
-        }
-
-        const struct sbrec_multicast_group *sbmc_flood =
-            mcast_group_lookup(lflow_input->sbrec_mcast_group_by_name_dp,
-                               MC_FLOOD, op->od->sdp->sb_dp);
-        const struct sbrec_multicast_group *sbmc_flood_l2 =
-            mcast_group_lookup(lflow_input->sbrec_mcast_group_by_name_dp,
-                               MC_FLOOD_L2, op->od->sdp->sb_dp);
-        const struct sbrec_multicast_group *sbmc_unknown =
-            mcast_group_lookup(lflow_input->sbrec_mcast_group_by_name_dp,
-                               MC_UNKNOWN, op->od->sdp->sb_dp);
 
         struct ds match = DS_EMPTY_INITIALIZER;
         struct ds actions = DS_EMPTY_INITIALIZER;
@@ -20521,30 +20504,6 @@ lflow_handle_northd_port_changes(struct ovsdb_idl_txn *ovnsb_txn,
 
         if (!handled) {
             return false;
-        }
-
-        /* Update SB multicast groups for the new port. */
-        if (!sbmc_flood) {
-            sbmc_flood = create_sb_multicast_group(ovnsb_txn,
-                op->od->sdp->sb_dp, MC_FLOOD, OVN_MCAST_FLOOD_TUNNEL_KEY);
-        }
-        sbrec_multicast_group_update_ports_addvalue(sbmc_flood, op->sb);
-
-        if (!sbmc_flood_l2) {
-            sbmc_flood_l2 = create_sb_multicast_group(ovnsb_txn,
-                op->od->sdp->sb_dp, MC_FLOOD_L2,
-                OVN_MCAST_FLOOD_L2_TUNNEL_KEY);
-        }
-        sbrec_multicast_group_update_ports_addvalue(sbmc_flood_l2, op->sb);
-
-        if (op->has_unknown) {
-            if (!sbmc_unknown) {
-                sbmc_unknown = create_sb_multicast_group(ovnsb_txn,
-                    op->od->sdp->sb_dp, MC_UNKNOWN,
-                    OVN_MCAST_UNKNOWN_TUNNEL_KEY);
-            }
-            sbrec_multicast_group_update_ports_addvalue(sbmc_unknown,
-                                                        op->sb);
         }
     }
 

tests/ovn-northd.at
@@ -12,6 +12,7 @@ m4_define([_DUMP_DB_TABLES], [
     ovn-sbctl list meter >> $1
     ovn-sbctl list meter_band >> $1
     ovn-sbctl list port_group >> $1
+    ovn-sbctl list multicast_group >> $1
     ovn-sbctl list bfd >> $1
     ovn-sbctl dump-flows > lflows_$1
 ])
@@ -15851,6 +15852,101 @@ wait_row_count IGMP_Group 0 address=239.0.1.68
 wait_row_count Multicast_Group 0 name=239.0.1.68
 
 OVN_CLEANUP([hv1], [hv2])
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD_NO_HV([
+AT_SETUP([Multicast group incremental processing])
+ovn_start
+
+dnl Helper: verify that Multicast_Group 'mc_name' on datapath 'dp_name'
+dnl contains exactly the listed ports.
+dnl Usage: check_mc_group_ports MC_NAME DP_NAME PORT1 [PORT2 ...]
+check_mc_group_ports() {
+    mc_group_name=$1; shift
+    mc_group_dp=$(fetch_column Datapath_Binding _uuid external_ids:name=$1); shift
+    mc_group_expected=""
+    for port_name; do
+        mc_group_expected="$mc_group_expected $(fetch_column Port_Binding _uuid logical_port=$port_name)"
+    done
+    mc_group_expected=$(echo $mc_group_expected | tr ' ' '\n' | sort | xargs)
+
+    OVS_WAIT_UNTIL([
+        mc_group_found=$(fetch_column Multicast_Group ports name=$mc_group_name datapath=$mc_group_dp)
+        test "$mc_group_found" = "$mc_group_expected"
+    ])
+}
+
+check ovn-nbctl --wait=sb ls-add ls0
+check ovn-nbctl --wait=sb lsp-add-localnet-port ls0 ln0 physnet0
+
+dnl Baseline: localnet port only.
+check_mc_group_ports _MC_flood ls0 ln0
+check_mc_group_ports _MC_flood_l2 ls0 ln0
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Add an enabled VIF.
+check ovn-nbctl --wait=sb lsp-add ls0 lsp0
+check_mc_group_ports _MC_flood ls0 ln0 lsp0
+check_mc_group_ports _MC_flood_l2 ls0 ln0 lsp0
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Add a disabled VIF; it must NOT appear in multicast groups.
+check ovn-nbctl --wait=sb \
+    lsp-add ls0 lsp1 \
+    -- set logical_switch_port lsp1 enabled=false
+check_mc_group_ports _MC_flood ls0 ln0 lsp0
+check_mc_group_ports _MC_flood_l2 ls0 ln0 lsp0
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Enable the disabled VIF.
+check ovn-nbctl --wait=sb set logical_switch_port lsp1 enabled=true
+check_mc_group_ports _MC_flood ls0 ln0 lsp0 lsp1
+check_mc_group_ports _MC_flood_l2 ls0 ln0 lsp0 lsp1
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Disable an enabled VIF.
+check ovn-nbctl --wait=sb set logical_switch_port lsp0 enabled=false
+check_mc_group_ports _MC_flood ls0 ln0 lsp1
+check_mc_group_ports _MC_flood_l2 ls0 ln0 lsp1
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Re-enable it.
+check ovn-nbctl --wait=sb set logical_switch_port lsp0 enabled=true
+check_mc_group_ports _MC_flood ls0 ln0 lsp0 lsp1
+check_mc_group_ports _MC_flood_l2 ls0 ln0 lsp0 lsp1
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Delete an enabled VIF.
+check ovn-nbctl --wait=sb lsp-del lsp0
+check_mc_group_ports _MC_flood ls0 ln0 lsp1
+check_mc_group_ports _MC_flood_l2 ls0 ln0 lsp1
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Add a router and connect it to ls0.  The router LSP should appear
+dnl in _MC_flood but NOT in _MC_flood_l2 (router ports are excluded
+dnl from _MC_flood_l2).
+check ovn-nbctl --wait=sb \
+    lr-add lr0 \
+    -- lrp-add lr0 lr0-ls0 00:00:00:00:01:00 10.0.0.254/24 \
+    -- lsp-add-router-port ls0 ls0-lr0 lr0-ls0
+check_mc_group_ports _MC_flood ls0 ln0 lsp1 ls0-lr0
+check_mc_group_ports _MC_flood_l2 ls0 ln0 lsp1
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+dnl Add a second switch connected to the router, with a localnet port
+dnl and a VIF.
+check ovn-nbctl --wait=sb \
+    ls-add ls1 \
+    -- lsp-add-localnet-port ls1 ln1 physnet1 \
+    -- lrp-add lr0 lr0-ls1 00:00:00:00:02:00 10.0.1.254/24 \
+    -- lsp-add-router-port ls1 ls1-lr0 lr0-ls1 \
+    -- lsp-add ls1 lsp2
+check_mc_group_ports _MC_flood ls1 ln1 ls1-lr0 lsp2
+check_mc_group_ports _MC_flood_l2 ls1 ln1 lsp2
+CHECK_NO_CHANGE_AFTER_RECOMPUTE
+
+OVN_CLEANUP_NORTHD
 AT_CLEANUP
 ])
 