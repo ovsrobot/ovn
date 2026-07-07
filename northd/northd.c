dd4b32df270d3a166287120e5d2d041443127332
Amir Aslan Aslani <amiraslanaslani6@gmail.com>
2026-07-07 22:39:06 +0330

    northd: Avoid extra ARP/ND flows for non-LB NATs.
    Currently, for non-LB NATs, northd generates ARP/ND response flows on
    every router port even when the NAT's "gateway_port" field is set.
    
    When "gateway_port" field is specified on NAT record on NB, it could
    be considered as reachable only through that LRP, so generating
    response flows for other router ports is unnecessary.
    
    This patch skips flow generation for router ports that do not match
    the NAT's "gateway_port".
    
    Signed-off-by: Amir Aslan Aslani <amiraslanaslani6@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- northd/northd.c
+++ northd/northd.c
@@ -9830,6 +9830,10 @@ build_lswitch_rport_arp_req_flows_for_lbnats(
             continue;
         }
 
+        if (nat->gateway_port && nat->gateway_port != op->nbrp) {
+            continue;
+        }
+
         /* Check if the ovn port has a network configured on which we could
          * expect ARP requests/NS for the DNAT external_ip.
          */
@@ -9869,6 +9873,10 @@ build_lswitch_rport_arp_req_flows_for_lbnats(
         }
 
         const struct nbrec_nat *nat = nat_entry->nb;
+
+        if (nat->gateway_port && nat->gateway_port != op->nbrp) {
+            continue;
+        }
 
         /* Check if the ovn port has a network configured on which we could
          * expect ARP requests/NS for the SNAT external_ip.
