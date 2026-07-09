ovsrobot/ovn

commit 744340f701b0449be7737f0d388af940fd117dc4

Allow LR to send RAs through localnet port.
Modifies the rule responsible for dropping the MLF_LOCAL_ONLY packets
to only drop them if the MLF_OVERRIDE_LOCAL_ONLY bit flag is not there.

This does also include the addition of MLF_OVERRIDE_LOCAL_ONLY bitflag applied
if a router announcement is being sent from either a gateway
or distributed router.

This is part of an ongoing unnumbered BGP effort.

Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/pinctrl.c
@@ -4126,6 +4126,7 @@ struct ipv6_ra_state {
     struct ipv6_ra_config *config;
     int64_t port_key;
     int64_t metadata;
+    bool preserved;
     bool delete_me;
 };
 
@@ -4451,6 +4452,9 @@ ipv6_ra_send(struct rconn *swconn, struct ipv6_ra_state *ra)
     put_load(dp_key, MFF_LOG_DATAPATH, 0, 64, &ofpacts);
     put_load(port_key, MFF_LOG_INPORT, 0, 32, &ofpacts);
     put_load(1, MFF_LOG_FLAGS, MLF_LOCAL_ONLY_BIT, 1, &ofpacts);
+    if (ra->preserved) {
+        put_load(1, MFF_LOG_FLAGS, MLF_OVERRIDE_LOCAL_ONLY_BIT, 1, &ofpacts);
+    }
     struct ofpact_resubmit *resubmit = ofpact_put_RESUBMIT(&ofpacts);
     resubmit->in_port = OFPP_CONTROLLER;
     resubmit->table_id = OFTABLE_LOG_INGRESS_PIPELINE;
@@ -4561,8 +4565,11 @@ prepare_ipv6_ras(const struct shash *local_active_ports_ras,
          * router port is connected to. The RA is injected
          * into that logical switch port.
          */
-        ra->port_key = peer->tunnel_key;
-        ra->metadata = peer->datapath->tunnel_key;
+        ra->port_key  = peer->tunnel_key;
+        ra->metadata  = peer->datapath->tunnel_key;
+        ra->preserved = (!strcmp(pb->type,"l2gateway") ||
+                        !strcmp(pb->type,"l3gateway") ||
+                        !strcmp(pb->type,"chassisredirect"));
         ra->delete_me = false;
 
         /* pinctrl_handler thread will send the IPv6 RAs. */

tests/ovn.at
@@ -16906,6 +16906,111 @@ OVN_CLEANUP([hv1],[hv2])
 AT_CLEANUP
 ])
 
+
+OVN_FOR_EACH_NORTHD([
+AT_SETUP([IPv6 periodic gateway RA enabled for localnet adjacent switch ports])
+ovn_start
+
+net_add n1
+sim_add hv1
+sim_add hv2
+as hv1
+check ovs-vsctl add-br br-phys
+check ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys
+ovn_attach n1 br-phys 192.168.0.2
+as hv2
+check ovs-vsctl add-br br-phys
+check ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys
+ovn_attach n1 br-phys 192.168.0.3
+
+check ovn-nbctl lr-add ro -- set Logical_Router ro options:chassis="hv1"
+check ovn-nbctl lrp-add ro ro-sw 00:00:00:00:00:01
+
+check ovn-nbctl ls-add sw
+check ovn-nbctl lsp-add sw ln
+check ovn-nbctl lsp-set-addresses ln unknown
+check ovn-nbctl lsp-set-type ln localnet
+check ovn-nbctl lsp-set-options ln network_name=phys
+
+check ovn-nbctl lsp-add sw sw-ro
+check ovn-nbctl lsp-set-type sw-ro router
+check ovn-nbctl lsp-set-options sw-ro router-port=ro-sw
+check ovn-nbctl lsp-set-addresses sw-ro 00:00:00:00:00:01
+check ovn-nbctl lsp-add sw sw-p1
+check ovn-nbctl lsp-set-addresses sw-p1 "00:00:00:00:00:02 aef0::200:ff:fe00:2"
+check ovn-nbctl lsp-add sw sw-p2
+check ovn-nbctl lsp-set-addresses sw-p2 "00:00:00:00:00:03 aef0::200:ff:fe00:3"
+
+AT_CHECK([ovn-sbctl get Port_Binding ro-sw type | tr -d '\n'],[0],[l3gateway])
+
+check ovn-nbctl set Logical_Router_Port ro-sw ipv6_ra_configs:send_periodic=true
+check ovn-nbctl set Logical_Router_Port ro-sw ipv6_ra_configs:address_mode=slaac
+check ovn-nbctl set Logical_Router_Port ro-sw ipv6_ra_configs:max_interval=1
+check ovn-nbctl set Logical_Router_Port ro-sw ipv6_ra_configs:min_interval=1
+
+for i in 1 2 ; do
+    as hv$i
+    check ovs-vsctl -- add-port br-int hv$i-vif1 -- \
+        set interface hv$i-vif1 external-ids:iface-id=sw-p$i \
+        options:tx_pcap=hv$i/vif1-tx.pcap \
+        options:rxq_pcap=hv$i/vif1-rx.pcap \
+        ofport-request=1
+done
+
+wait_for_ports_up
+check ovn-nbctl --wait=hv sync
+
+ra_received() {
+    $PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" $1 | sed '/^ffffffffffff/d' | wc -l
+}
+
+ra_test() {
+    interface=$1
+    shift 1
+    local ra_packet=$(fmt_pkt "
+        Ether(src='00:00:00:00:00:01', dst='33:33:00:00:00:01') /
+        IPv6(dst='ff02::1', src='fe80::200:ff:fe00:1') /
+        ICMPv6ND_RA(chlim=255, prf=0, routerlifetime=65535) /
+        ICMPv6NDOptSrcLLAddr(lladdr='00:00:00:00:00:01')
+    ")
+    intname="$interface"
+
+    for i in hv1 hv2 ; do
+        if echo "$interface" | grep -q -v "br"; then
+            intname="$i-$interface"
+        fi
+        echo $intname
+        as $i reset_pcap_file $intname $i/$interface
+
+        OVS_WAIT_WHILE([test 0 = $(ra_received $i/$interface-tx.pcap)])
+
+        $PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" $i/$interface-tx.pcap > packets
+        sed -i '/^ffffffffffff/d' packets
+
+        echo ${ra_packet} | cut -c -112 > expout
+        AT_CHECK([head -1 packets | cut -c -112], [0], [expout])
+
+        # Skip ICMPv6 checksum.
+        echo ${ra_packet} | cut -c 117- > expout
+        AT_CHECK([head -1 packets | cut -c 117-], [0], [expout])
+
+        rm -f packets
+        as $i reset_pcap_file $intname $i/$interface
+    done
+
+    rm -f expected
+}
+
+# check that RAs are sent
+ra_test vif1
+
+# check that RAs are recived on br-phys
+ra_test br-phys
+
+OVN_CLEANUP([hv1],[hv2])
+AT_CLEANUP
+])
+
 OVN_FOR_EACH_NORTHD([
 AT_SETUP([ACL reject rule test])
 AT_KEYWORDS([acl-reject])

controller/physical.c
@@ -1869,7 +1869,8 @@ consider_port_binding(struct ovsdb_idl_index *sbrec_port_binding_by_name,
             put_drop(debug, OFTABLE_CHECK_LOOPBACK, ofpacts_p);
             match_outport_dp_and_port_keys(&match, dp_key, port_key);
             match_set_reg_masked(&match, MFF_LOG_FLAGS - MFF_REG0,
-                                 MLF_LOCAL_ONLY, MLF_LOCAL_ONLY);
+                                 MLF_LOCAL_ONLY,
+                                 MLF_LOCAL_ONLY | MLF_OVERRIDE_LOCAL_ONLY);
             ofctrl_add_flow(flow_table, OFTABLE_CHECK_LOOPBACK, 160,
                             binding->header_.uuid.parts[0], &match,
                             ofpacts_p, &binding->header_.uuid);

include/ovn/logical-fields.h
@@ -87,6 +87,7 @@ enum mff_log_flags_bits {
     MLF_LOCALNET_BIT = 15,
     MLF_RX_FROM_TUNNEL_BIT = 16,
     MLF_ICMP_SNAT_BIT = 17,
+    MLF_OVERRIDE_LOCAL_ONLY_BIT = 18,
 };
 
 /* MFF_LOG_FLAGS_REG flag assignments */
@@ -142,6 +143,8 @@ enum mff_log_flags {
     MLF_RX_FROM_TUNNEL = (1 << MLF_RX_FROM_TUNNEL_BIT),
 
     MLF_ICMP_SNAT = (1 << MLF_ICMP_SNAT_BIT),
+
+    MLF_OVERRIDE_LOCAL_ONLY = (1 << MLF_OVERRIDE_LOCAL_ONLY_BIT),
 };
 
 /* OVN logical fields

ovn-architecture.7.xml
@@ -1546,8 +1546,10 @@
       <p>
         Table 44 matches and drops packets for which the logical input and
         output ports are the same and the MLF_ALLOW_LOOPBACK flag is not
-        set. It also drops MLF_LOCAL_ONLY packets directed to a localnet port.
-        It resubmits other packets to table 45.
+        set. It also drops MLF_LOCAL_ONLY packets directed to a localnet port,
+        provided they aren't RAs sent from a gateway or distributed router
+        which is checked via the presence of the bitflag
+        MLF_OVERRIDE_LOCAL_ONLY. It resubmits other packets to table 46.
       </p>
     </li>
 

commit 2099726ca3487e50879d96260f45e717a91b398c

pinctrl: Allow periodic RAs through localnet for DGP.
Unlike ports on a gateway router that end up with 'l3gateway' port
binding type, the distributed gateway ports have the 'patch' type and
their corresponding cr-* ports get 'chassisredirect'.

This means that while creating RA for DGP ports, the 'preserved' flag
ends up false and the MLF_OVERRIDE_LOCAL_ONLY flag not set as a result.
This leads to DGP RAs to not be sent out on localnet ports.

However, this is not a desired behavior.  The router is supposed to be
able to send RAs to the physical network via localnet on a chassis of
residence regardless of it being a full gateway router or a standard
logical router with a distributed gateway port.

Fix this by also checking for the 'chassis-redirect-port' option in
the port binding, which northd sets on DGP LRPs to reference their
cr-* companion port.

A new test is added to check the DGP path.  Old GR test converted into
a macro to avoid code duplication.  While at it, added the missing
Sb synchronization before the port type check.

Fixes: 744340f701b0 ("Allow LR to send RAs through localnet port.")
Reported-at: https://github.com/ovn-org/ovn/issues/313
Reported-at: https://mail.openvswitch.org/pipermail/ovs-discuss/2026-July/053988.html
Reported-by: Meno Abels <meno.abels@adviser.com>
Assisted-by: claude-opus-4.6, OpenCode
Signed-off-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/pinctrl.c
@@ -4704,7 +4704,8 @@ prepare_ipv6_ras(const struct shash *local_active_ports_ras,
         ra->metadata  = peer->datapath->tunnel_key;
         ra->preserved = (!strcmp(pb->type,"l2gateway") ||
                         !strcmp(pb->type,"l3gateway") ||
-                        !strcmp(pb->type,"chassisredirect"));
+                        !strcmp(pb->type,"chassisredirect") ||
+                        smap_get(&pb->options, "chassis-redirect-port"));
         ra->delete_me = false;
 
         /* pinctrl_handler thread will send the IPv6 RAs. */

tests/ovn.at
@@ -18038,9 +18038,16 @@ OVN_CLEANUP([hv1],[hv2])
 AT_CLEANUP
 ])
 
-
+dnl OVN_TEST_PERIODIC_RA_LOCALNET([mode])
+dnl
+dnl Tests that periodic IPv6 RAs on a gateway or distributed gateway port
+dnl are forwarded through localnet ports to the provider network.
+dnl
+dnl mode: "l3gateway" - LR with options:chassis (gateway router)
+dnl        "dgp"      - LRP with gateway_chassis (distributed gateway port)
+m4_define([OVN_TEST_PERIODIC_RA_LOCALNET], [dnl
 OVN_FOR_EACH_NORTHD([
-AT_SETUP([IPv6 periodic gateway RA enabled for localnet adjacent switch ports])
+AT_SETUP([IPv6 periodic $1 RA enabled for localnet adjacent switch ports])
 CHECK_SCAPY
 ovn_start
 
@@ -18056,8 +18063,14 @@ check ovs-vsctl add-br br-phys
 check ovs-vsctl set open . external-ids:ovn-bridge-mappings=phys:br-phys
 ovn_attach n1 br-phys 192.168.0.3
 
-check ovn-nbctl lr-add ro -- set Logical_Router ro options:chassis="hv1"
+check ovn-nbctl lr-add ro
 check ovn-nbctl lrp-add ro ro-sw 00:00:00:00:00:01
+
+m4_if([$1], [l3gateway], [
+check ovn-nbctl set Logical_Router ro options:chassis="hv1"
+], [
+check ovn-nbctl lrp-set-gateway-chassis ro-sw hv1
+])
 
 check ovn-nbctl ls-add sw
 check ovn-nbctl lsp-add-localnet-port sw ln phys
@@ -18068,7 +18081,14 @@ check ovn-nbctl lsp-set-addresses sw-p1 "00:00:00:00:00:02 aef0::200:ff:fe00:2"
 check ovn-nbctl lsp-add sw sw-p2
 check ovn-nbctl lsp-set-addresses sw-p2 "00:00:00:00:00:03 aef0::200:ff:fe00:3"
 
-AT_CHECK([ovn-sbctl get Port_Binding ro-sw type | tr -d '\n'],[0],[l3gateway])
+check ovn-nbctl --wait=sb sync
+
+m4_if([$1], [l3gateway], [dnl
+AT_CHECK([ovn-sbctl get Port_Binding ro-sw type | tr -d '\n'], [0], [l3gateway])
+], [dnl
+AT_CHECK([ovn-sbctl get Port_Binding ro-sw type | tr -d '\n'], [0], [patch])
+AT_CHECK([ovn-sbctl get Port_Binding cr-ro-sw type | tr -d '\n'], [0], [chassisredirect])
+])
 
 check ovn-nbctl set Logical_Router_Port ro-sw ipv6_ra_configs:send_periodic=true
 check ovn-nbctl set Logical_Router_Port ro-sw ipv6_ra_configs:address_mode=slaac
@@ -18087,11 +18107,11 @@ wait_for_ports_up
 check ovn-nbctl --wait=hv sync
 
 ra_received() {
-    $PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" $1 | sed '/^ffffffffffff/d' | wc -l
+    $PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" ${1} | sed '/^ffffffffffff/d' | wc -l
 }
 
 ra_test() {
-    interface=$1
+    interface=${1}
     shift 1
     local ra_packet=$(fmt_pkt "
         Ether(src='00:00:00:00:00:01', dst='33:33:00:00:00:01') /
@@ -18127,15 +18147,18 @@ ra_test() {
     rm -f expected
 }
 
-# check that RAs are sent
+# Check that RAs are sent to vifs.
 ra_test vif1
 
-# check that RAs are recived on br-phys
+# Check that RAs are received on br-phys.
 ra_test br-phys
 
 OVN_CLEANUP([hv1],[hv2])
 AT_CLEANUP
-])
+])])
+
+OVN_TEST_PERIODIC_RA_LOCALNET([l3gateway])
+OVN_TEST_PERIODIC_RA_LOCALNET([dgp])
 
 OVN_FOR_EACH_NORTHD([
 AT_SETUP([ACL reject rule test])