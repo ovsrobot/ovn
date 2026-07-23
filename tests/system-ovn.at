ovsrobot/ovn

commit 32d5252ceb65404a8bb4579558281dcc1e2ca7e8

controller, northd: Add an option to advertise FDB over EVPN.
Add an option to LS that is inline with existing LR option called
"dynamic-routing-redistribute". That option currently accepts only
"fdb" which will lead to advertisment of workloads connected to
the specified switch. This doesn't work without the switch having
"dynamic-routing-vni" option set.

Currently ovn-controller will advertise VIFs, virtual ports,
container ports, DGPs and GW router ports bound to that chassis.
In other words only local workloads are advertised to avoid
the potential confusion of defining the same MAC multiple times
as the same EVPN might be connected to more than one chassis.

Reported-at: https://issues.redhat.com/browse/FDP-1389
Acked-by: Xavier Simonart <xsimonar@redhat.com>
Acked-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: Ales Musil <amusil@redhat.com>

controller/neighbor.c
@@ -19,6 +19,7 @@
 #include "lib/packets.h"
 #include "lib/sset.h"
 #include "local_data.h"
+#include "lport.h"
 #include "ovn-sb-idl.h"
 
 #include "neighbor.h"
@@ -38,6 +39,14 @@ static struct neighbor_interface_monitor *
 neighbor_interface_monitor_alloc(enum neighbor_family family,
                                  enum neighbor_interface_type type,
                                  uint32_t vni);
+static void neighbor_collect_mac_to_advertise(
+    const struct neighbor_ctx_in *, struct hmap *neighbors,
+    struct sset *advertised_pbs, const struct sbrec_datapath_binding *);
+static const struct sbrec_port_binding *neighbor_get_relevant_port_binding(
+    struct ovsdb_idl_index *sbrec_pb_by_name,
+    const struct sbrec_port_binding *);
+static void advertise_neigh_add(struct hmap *neighbors, struct eth_addr mac,
+                                struct in6_addr ip);
 
 uint32_t
 advertise_neigh_hash(const struct eth_addr *eth, const struct in6_addr *ip)
@@ -102,6 +111,16 @@ neighbor_run(struct neighbor_ctx_in *n_ctx_in,
             neighbor_interface_monitor_alloc(NEIGH_AF_INET6,
                                              NEIGH_IFACE_BRIDGE, vni);
         vector_push(n_ctx_out->monitored_interfaces, &br_v6);
+
+        const char *redistribute = smap_get(&ld->datapath->external_ids,
+                                            "dynamic-routing-redistribute");
+        if (!redistribute || strcmp(redistribute, "fdb")) {
+            continue;
+        }
+
+        neighbor_collect_mac_to_advertise(n_ctx_in, &lo->announced_neighbors,
+                                          n_ctx_out->advertised_pbs,
+                                          ld->datapath);
     }
 }
 
@@ -113,6 +132,26 @@ neighbor_cleanup(struct vector *monitored_interfaces)
         neighbor_interface_monitor_destroy(nim);
     }
     vector_clear(monitored_interfaces);
+}
+
+bool
+neighbor_is_relevant_port_updated(struct ovsdb_idl_index *sbrec_pb_by_name,
+                                  const struct sbrec_chassis *chassis,
+                                  struct sset *advertised_pbs,
+                                  const struct tracked_lport *lport)
+{
+    if (lport->tracked_type == TRACKED_RESOURCE_REMOVED &&
+        sset_contains(advertised_pbs, lport->pb->logical_port)) {
+        return true;
+    }
+
+    if (lport->tracked_type != TRACKED_RESOURCE_NEW) {
+        return false;
+    }
+
+    const struct sbrec_port_binding *pb =
+        neighbor_get_relevant_port_binding(sbrec_pb_by_name, lport->pb);
+    return lport_pb_is_chassis_resident(chassis, pb);
 }
 
 static void
@@ -156,4 +195,82 @@ neighbor_interface_monitor_alloc(enum neighbor_family family,
     snprintf(nim->if_name, sizeof nim->if_name, "%s%"PRIu32,
              neighbor_interface_prefixes[type], vni);
     return nim;
+}
+
+static void
+neighbor_collect_mac_to_advertise(const struct neighbor_ctx_in *n_ctx_in,
+                                  struct hmap *neighbors,
+                                  struct sset *advertised_pbs,
+                                  const struct sbrec_datapath_binding *dp)
+{
+    struct sbrec_port_binding *target =
+        sbrec_port_binding_index_init_row(n_ctx_in->sbrec_pb_by_dp);
+    sbrec_port_binding_index_set_datapath(target, dp);
+
+    const struct sbrec_port_binding *dp_pb;
+    SBREC_PORT_BINDING_FOR_EACH_EQUAL (dp_pb, target,
+                                       n_ctx_in->sbrec_pb_by_dp) {
+        const struct sbrec_port_binding *pb =
+            neighbor_get_relevant_port_binding(n_ctx_in->sbrec_pb_by_name,
+                                               dp_pb);
+        if (!lport_pb_is_chassis_resident(n_ctx_in->chassis, pb)) {
+            continue;
+        }
+
+        for (size_t i = 0; i < pb->n_mac; i++) {
+            struct lport_addresses addresses;
+            if (!extract_lsp_addresses(pb->mac[i], &addresses)) {
+                continue;
+            }
+
+            if (!advertise_neigh_find(neighbors, addresses.ea, &in6addr_any)) {
+                advertise_neigh_add(neighbors, addresses.ea, in6addr_any);
+            }
+
+            destroy_lport_addresses(&addresses);
+        }
+
+        sset_add(advertised_pbs, pb->logical_port);
+    }
+
+    sbrec_port_binding_index_destroy_row(target);
+}
+
+static const struct sbrec_port_binding *
+neighbor_get_relevant_port_binding(struct ovsdb_idl_index *sbrec_pb_by_name,
+                                   const struct sbrec_port_binding *pb)
+{
+    enum en_lport_type type = get_lport_type(pb);
+    if (type == LP_VIF || type == LP_VIRTUAL || type == LP_CONTAINER) {
+        return pb;
+    }
+
+    if (type == LP_L3GATEWAY) {
+        return lport_get_peer(pb, sbrec_pb_by_name);
+    }
+
+    if (type == LP_PATCH) {
+        const struct sbrec_port_binding *peer =
+            lport_get_peer(pb, sbrec_pb_by_name);
+        if (!peer) {
+            return NULL;
+        }
+
+        return lport_get_cr_port(sbrec_pb_by_name, peer, NULL);
+    }
+
+    return NULL;
+}
+
+static void
+advertise_neigh_add(struct hmap *neighbors, struct eth_addr mac,
+                    struct in6_addr ip)
+{
+    struct advertise_neighbor_entry *ne = xmalloc(sizeof *ne);
+    *ne = (struct advertise_neighbor_entry) {
+        .lladdr = mac,
+        .addr = ip,
+    };
+
+    hmap_insert(neighbors, &ne->node, advertise_neigh_hash(&mac, &ip));
 }

controller/neighbor.h
@@ -31,6 +31,8 @@
 #define AF_BRIDGE AF_UNSPEC
 #endif
 
+struct tracked_lport;
+
 enum neighbor_family {
     NEIGH_AF_INET = AF_INET,
     NEIGH_AF_INET6 = AF_INET6,
@@ -40,11 +42,18 @@ enum neighbor_family {
 struct neighbor_ctx_in {
     /* Contains 'struct local_datapath'. */
     const struct hmap *local_datapaths;
+    /* Index for Port Binding by Datapath. */
+    struct ovsdb_idl_index *sbrec_pb_by_dp;
+    /* Index for Port Binding by name. */
+    struct ovsdb_idl_index *sbrec_pb_by_name;
+    const struct sbrec_chassis *chassis;
 };
 
 struct neighbor_ctx_out {
     /* Contains struct neighbor_interface_monitor pointers. */
     struct vector *monitored_interfaces;
+    /* Contains set of port binding names that are currently advertised. */
+    struct sset *advertised_pbs;
 };
 
 enum neighbor_interface_type {
@@ -79,5 +88,9 @@ struct advertise_neighbor_entry *advertise_neigh_find(
     const struct in6_addr *ip);
 void neighbor_run(struct neighbor_ctx_in *, struct neighbor_ctx_out *);
 void neighbor_cleanup(struct vector *monitored_interfaces);
+bool neighbor_is_relevant_port_updated(
+    struct ovsdb_idl_index *sbrec_pb_by_name,
+    const struct sbrec_chassis *chassis, struct sset *advertised_pbs,
+    const struct tracked_lport *lport);
 
 #endif /* NEIGHBOR_H */

controller/ovn-controller.c
@@ -5771,6 +5771,8 @@ en_host_if_monitor_run(struct engine_node *node OVS_UNUSED, void *data)
 struct ed_type_neighbor {
     /* Contains struct neighbor_interface_monitor pointers. */
     struct vector monitored_interfaces;
+    /* Contains set of PB names that are currently advertised. */
+    struct sset advertised_pbs;
 };
 
 static void *
@@ -5782,6 +5784,7 @@ en_neighbor_init(struct engine_node *node OVS_UNUSED,
     *data = (struct ed_type_neighbor) {
         .monitored_interfaces =
             VECTOR_EMPTY_INITIALIZER(struct neighbor_interface_monitor *),
+        .advertised_pbs = SSET_INITIALIZER(&data->advertised_pbs),
     };
     return data;
 }
@@ -5793,6 +5796,7 @@ en_neighbor_cleanup(void *data)
 
     neighbor_cleanup(&ne_data->monitored_interfaces);
     vector_destroy(&ne_data->monitored_interfaces);
+    sset_destroy(&ne_data->advertised_pbs);
 }
 
 static enum engine_node_state
@@ -5801,25 +5805,68 @@ en_neighbor_run(struct engine_node *node OVS_UNUSED, void *data)
     struct ed_type_neighbor *ne_data = data;
     struct ed_type_runtime_data *rt_data =
         engine_get_input_data("runtime_data", node);
+    struct ovsdb_idl_index *sbrec_port_binding_by_datapath =
+        engine_ovsdb_node_get_index(
+            engine_get_input("SB_port_binding", node),
+            "datapath");
+    struct ovsdb_idl_index *sbrec_port_binding_by_name =
+        engine_ovsdb_node_get_index(
+            engine_get_input("SB_port_binding", node),
+            "name");
+    const struct ovsrec_open_vswitch_table *ovs_table =
+        EN_OVSDB_GET(engine_get_input("OVS_open_vswitch", node));
+    struct ovsdb_idl_index *sbrec_chassis_by_name =
+        engine_ovsdb_node_get_index(
+            engine_get_input("SB_chassis", node),
+            "name");
+
+    const char *chassis_id = get_ovs_chassis_id(ovs_table);
+    ovs_assert(chassis_id);
+    const struct sbrec_chassis *chassis =
+        chassis_lookup_by_name(sbrec_chassis_by_name, chassis_id);
+    ovs_assert(chassis);
+
     struct neighbor_ctx_in n_ctx_in = {
         .local_datapaths = &rt_data->local_datapaths,
+        .sbrec_pb_by_dp = sbrec_port_binding_by_datapath,
+        .sbrec_pb_by_name = sbrec_port_binding_by_name,
+        .chassis = chassis,
     };
 
     struct neighbor_ctx_out n_ctx_out = {
         .monitored_interfaces = &ne_data->monitored_interfaces,
+        .advertised_pbs = &ne_data->advertised_pbs,
     };
 
     neighbor_cleanup(&ne_data->monitored_interfaces);
+    sset_clear(&ne_data->advertised_pbs);
     neighbor_run(&n_ctx_in, &n_ctx_out);
 
     return EN_UPDATED;
 }
 
 static enum engine_input_handler_result
-neighbor_runtime_data_handler(struct engine_node *node, void *data OVS_UNUSED)
+neighbor_runtime_data_handler(struct engine_node *node, void *data)
 {
+    struct ed_type_neighbor *ne_data = data;
     struct ed_type_runtime_data *rt_data =
         engine_get_input_data("runtime_data", node);
+    struct ovsdb_idl_index *sbrec_port_binding_by_name =
+        engine_ovsdb_node_get_index(
+            engine_get_input("SB_port_binding", node),
+            "name");
+    const struct ovsrec_open_vswitch_table *ovs_table =
+        EN_OVSDB_GET(engine_get_input("OVS_open_vswitch", node));
+    struct ovsdb_idl_index *sbrec_chassis_by_name =
+        engine_ovsdb_node_get_index(
+            engine_get_input("SB_chassis", node),
+            "name");
+
+    const char *chassis_id = get_ovs_chassis_id(ovs_table);
+    ovs_assert(chassis_id);
+    const struct sbrec_chassis *chassis =
+        chassis_lookup_by_name(sbrec_chassis_by_name, chassis_id);
+    ovs_assert(chassis);
 
     /* There are no tracked data. Fall back to full recompute. */
     if (!rt_data->tracked) {
@@ -5843,6 +5890,22 @@ neighbor_runtime_data_handler(struct engine_node *node, void *data OVS_UNUSED)
         if (tdp->tracked_type == TRACKED_RESOURCE_NEW ||
             tdp->tracked_type == TRACKED_RESOURCE_REMOVED) {
             return EN_UNHANDLED;
+        }
+
+        const char *redistribute = smap_get(&ld->datapath->external_ids,
+                                            "dynamic-routing-redistribute");
+        if (!redistribute || strcmp(redistribute, "fdb")) {
+            continue;
+        }
+
+        const struct shash_node *shash_node;
+        SHASH_FOR_EACH (shash_node, &tdp->lports) {
+            if (neighbor_is_relevant_port_updated(sbrec_port_binding_by_name,
+                                                  chassis,
+                                                  &ne_data->advertised_pbs,
+                                                  shash_node->data)) {
+                return EN_UNHANDLED;
+            }
         }
     }
 
@@ -5876,6 +5939,30 @@ neighbor_sb_datapath_binding_handler(struct engine_node *node,
 
         if (sbrec_datapath_binding_is_updated(
                 dp, SBREC_DATAPATH_BINDING_COL_EXTERNAL_IDS)) {
+            return EN_UNHANDLED;
+        }
+    }
+
+    return EN_HANDLED_UNCHANGED;
+}
+
+static enum engine_input_handler_result
+neighbor_sb_port_binding_handler(struct engine_node *node, void *data)
+{
+    struct ed_type_neighbor *ne_data = data;
+    const struct sbrec_port_binding_table *pb_table =
+        EN_OVSDB_GET(engine_get_input("SB_port_binding", node));
+
+    const struct sbrec_port_binding *pb;
+    SBREC_PORT_BINDING_TABLE_FOR_EACH_TRACKED (pb, pb_table) {
+        if (sbrec_port_binding_is_new(pb) ||
+            sbrec_port_binding_is_deleted(pb)) {
+            /* The removal and addition is handled via runtime_data. */
+            return EN_HANDLED_UNCHANGED;
+        }
+
+        if (sbrec_port_binding_is_updated(pb, SBREC_PORT_BINDING_COL_MAC) &&
+            sset_contains(&ne_data->advertised_pbs, pb->logical_port)) {
             return EN_UNHANDLED;
         }
     }
@@ -6786,10 +6873,14 @@ main(int argc, char *argv[])
     engine_add_input(&en_garp_rarp, &en_runtime_data,
                      garp_rarp_runtime_data_handler);
 
+    engine_add_input(&en_neighbor, &en_ovs_open_vswitch, NULL);
+    engine_add_input(&en_neighbor, &en_sb_chassis, NULL);
     engine_add_input(&en_neighbor, &en_runtime_data,
                      neighbor_runtime_data_handler);
     engine_add_input(&en_neighbor, &en_sb_datapath_binding,
                      neighbor_sb_datapath_binding_handler);
+    engine_add_input(&en_neighbor, &en_sb_port_binding,
+                     neighbor_sb_port_binding_handler);
     engine_add_input(&en_neighbor_exchange, &en_neighbor, NULL);
     engine_add_input(&en_neighbor_exchange, &en_host_if_monitor, NULL);
     engine_add_input(&en_neighbor_exchange, &en_neighbor_table_notify, NULL);

ovn-nb.xml
@@ -907,6 +907,26 @@
           removal/change in the future.
         </p>
       </column>
+
+      <column name="other_config" key="dynamic-routing-redistribute"
+              type='{"type": "string"}'>
+        <p>
+          Only relevant if <ref column="other_config" key="dynamic-routing-vni"
+                                table="Logical_switch"/> is set to valid VNI.
+        </p>
+
+        <p>
+          If <code>fdb</code> is specified then ovn-controller will advertise
+          all workloads that are local to the chassis. The applies to VIFs,
+          container ports, virtual ports, connected DGPs and connected GW
+          routers.
+        </p>
+
+        <p>
+          NOTE: this feature is experimental and may be subject to
+          removal/change in the future.
+        </p>
+      </column>
     </group>
 
     <group title="IP Multicast Snooping Options">

commit 5b5f1134058e479587283583fc8d03978d8eae33

neighbor: Advertise dynamically learned FDB entries.
When a logical switch has dynamic-routing-redistribute=fdb
set, neighbor_collect_mac_to_advertise() only iterated
statically configured port MAC addresses.  Dynamically
learned FDB entries from the SB FDB table were completely
ignored.

Add neighbor_collect_fdb_to_advertise() which iterates FDB
entries for the datapath and advertises each learned MAC as
an EVPN Type-2 MAC-only neighbor.  Only FDB entries learned
on VIF ports bound to this chassis or on localnet ports are
included.  The per-port dynamic-routing-advertise option is
honored.

An incremental processing handler for SB FDB changes is
added to trigger a recompute of the neighbor node only when
an FDB entry changes on a datapath that has FDB
advertisement enabled.  A set of relevant datapath pointers
is maintained to filter out irrelevant FDB changes.

Fixes: 32d5252ceb65 ("controller, northd: Add an option to advertise FDB over EVPN.")
Reported-at: https://redhat.atlassian.net/browse/FDP-4149
Assisted-by: Claude Opus 4.6, OpenCode
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/neighbor.c
@@ -46,6 +46,9 @@ neighbor_interface_monitor_alloc(enum neighbor_family family,
 static void neighbor_collect_mac_to_advertise(
     const struct neighbor_ctx_in *, struct hmap *neighbors,
     struct sset *advertised_pbs, const struct sbrec_datapath_binding *);
+static void neighbor_collect_fdb_to_advertise(
+    const struct neighbor_ctx_in *, struct hmap *neighbors,
+    struct hmapx *fdb_datapaths, struct local_datapath *);
 static void neighbor_collect_advertised_mac_bindings(
     const struct neighbor_ctx_in *, enum neigh_redistribute_mode mode,
     struct hmap *fdb_neighbors,
@@ -176,6 +179,10 @@ neighbor_run(struct neighbor_ctx_in *n_ctx_in,
                                               &lo->announced_neighbors,
                                               n_ctx_out->advertised_pbs,
                                               ld->datapath);
+            neighbor_collect_fdb_to_advertise(n_ctx_in,
+                                              &lo->announced_neighbors,
+                                              n_ctx_out->fdb_datapaths,
+                                              ld);
         }
         /* Advertise every SB Advertised_MAC_Binding row of the datapath as an
          * EVPN Type-2 MAC+IP neighbor; ovn-northd only populates rows that are
@@ -303,6 +310,60 @@ neighbor_collect_mac_to_advertise(const struct neighbor_ctx_in *n_ctx_in,
     }
 
     sbrec_port_binding_index_destroy_row(target);
+}
+
+/* Walks the SB FDB entries for 'dp' and advertises each learned MAC as an
+ * EVPN Type-2 MAC-only neighbor (into 'neighbors').  Only FDB entries
+ * learned on VIF ports bound to this chassis or on localnet ports are
+ * included.  The per-port 'dynamic-routing-advertise' option is honored. */
+static void
+neighbor_collect_fdb_to_advertise(const struct neighbor_ctx_in *n_ctx_in,
+                                  struct hmap *neighbors,
+                                  struct hmapx *fdb_datapaths,
+                                  struct local_datapath *ld)
+{
+    hmapx_add(fdb_datapaths, ld);
+
+    const struct sbrec_datapath_binding *dp = ld->datapath;
+    struct sbrec_fdb *fdb_target =
+        sbrec_fdb_index_init_row(n_ctx_in->sbrec_fdb_by_dp_key);
+    sbrec_fdb_index_set_dp_key(fdb_target, dp->tunnel_key);
+
+    const struct sbrec_fdb *fdb;
+    SBREC_FDB_FOR_EACH_EQUAL (fdb, fdb_target,
+                              n_ctx_in->sbrec_fdb_by_dp_key) {
+        const struct sbrec_port_binding *pb =
+            lport_lookup_by_key_with_dp(n_ctx_in->sbrec_pb_by_key,
+                                        dp, fdb->port_key);
+        if (!pb) {
+            continue;
+        }
+
+        enum en_lport_type type = get_lport_type(pb);
+        if (type == LP_VIF &&
+            !lport_pb_is_chassis_resident(n_ctx_in->chassis, pb)) {
+            continue;
+        }
+
+        if (type != LP_VIF && type != LP_LOCALNET) {
+            continue;
+        }
+
+        if (!smap_get_bool(&pb->options, "dynamic-routing-advertise", true)) {
+            continue;
+        }
+
+        struct eth_addr ea;
+        if (!eth_addr_from_string(fdb->mac, &ea)) {
+            continue;
+        }
+
+        if (!advertise_neigh_find(neighbors, ea, &in6addr_any)) {
+            advertise_neigh_add(neighbors, ea, in6addr_any);
+        }
+    }
+
+    sbrec_fdb_index_destroy_row(fdb_target);
 }
 
 /* Walks the SB Advertised_MAC_Binding rows of 'dp' and advertises each as an

controller/neighbor.h
@@ -21,6 +21,7 @@
 #include <net/if.h>
 #include <stdint.h>
 
+#include "hmapx.h"
 #include "lib/sset.h"
 #include "openvswitch/hmap.h"
 
@@ -48,6 +49,10 @@ struct neighbor_ctx_in {
     struct ovsdb_idl_index *sbrec_amb_by_dp;
     /* Index for Port Binding by name. */
     struct ovsdb_idl_index *sbrec_pb_by_name;
+    /* Index for Port Binding by datapath and tunnel key. */
+    struct ovsdb_idl_index *sbrec_pb_by_key;
+    /* Index for FDB by dp_key. */
+    struct ovsdb_idl_index *sbrec_fdb_by_dp_key;
     const struct sbrec_chassis *chassis;
 };
 
@@ -56,6 +61,9 @@ struct neighbor_ctx_out {
     struct vector *monitored_interfaces;
     /* Contains set of port binding names that are currently advertised. */
     struct sset *advertised_pbs;
+    /* Contains 'struct local_datapath' pointers for datapaths with FDB
+     * advertisement enabled. */
+    struct hmapx *fdb_datapaths;
 };
 
 enum neighbor_interface_type {

controller/ovn-controller.c
@@ -6081,6 +6081,9 @@ struct ed_type_neighbor {
     struct vector monitored_interfaces;
     /* Contains set of PB names that are currently advertised. */
     struct sset advertised_pbs;
+    /* Contains 'struct local_datapath' pointers for datapaths with FDB
+     * advertisement enabled. */
+    struct hmapx fdb_datapaths;
 };
 
 static void *
@@ -6093,6 +6096,7 @@ en_neighbor_init(struct engine_node *node OVS_UNUSED,
         .monitored_interfaces =
             VECTOR_EMPTY_INITIALIZER(struct neighbor_interface_monitor *),
         .advertised_pbs = SSET_INITIALIZER(&data->advertised_pbs),
+        .fdb_datapaths = HMAPX_INITIALIZER(&data->fdb_datapaths),
     };
     return data;
 }
@@ -6105,6 +6109,7 @@ en_neighbor_cleanup(void *data)
     neighbor_cleanup(&ne_data->monitored_interfaces);
     vector_destroy(&ne_data->monitored_interfaces);
     sset_destroy(&ne_data->advertised_pbs);
+    hmapx_destroy(&ne_data->fdb_datapaths);
 }
 
 static enum engine_node_state
@@ -6131,6 +6136,14 @@ en_neighbor_run(struct engine_node *node OVS_UNUSED, void *data)
         engine_ovsdb_node_get_index(
             engine_get_input("SB_advertised_mac_binding", node),
             "datapath");
+    struct ovsdb_idl_index *sbrec_port_binding_by_key =
+        engine_ovsdb_node_get_index(
+            engine_get_input("SB_port_binding", node),
+            "key");
+    struct ovsdb_idl_index *sbrec_fdb_by_dp_key =
+        engine_ovsdb_node_get_index(
+            engine_get_input("SB_fdb", node),
+            "dp_key");
 
     const char *chassis_id = get_ovs_chassis_id(ovs_table);
     ovs_assert(chassis_id);
@@ -6143,16 +6156,20 @@ en_neighbor_run(struct engine_node *node OVS_UNUSED, void *data)
         .sbrec_pb_by_dp = sbrec_port_binding_by_datapath,
         .sbrec_amb_by_dp = sbrec_advertised_mac_binding_by_datapath,
         .sbrec_pb_by_name = sbrec_port_binding_by_name,
+        .sbrec_pb_by_key = sbrec_port_binding_by_key,
+        .sbrec_fdb_by_dp_key = sbrec_fdb_by_dp_key,
         .chassis = chassis,
     };
 
     struct neighbor_ctx_out n_ctx_out = {
         .monitored_interfaces = &ne_data->monitored_interfaces,
         .advertised_pbs = &ne_data->advertised_pbs,
+        .fdb_datapaths = &ne_data->fdb_datapaths,
     };
 
     neighbor_cleanup(&ne_data->monitored_interfaces);
     sset_clear(&ne_data->advertised_pbs);
+    hmapx_clear(&ne_data->fdb_datapaths);
     neighbor_run(&n_ctx_in, &n_ctx_out);
 
     return EN_UPDATED;
@@ -6276,6 +6293,27 @@ neighbor_sb_port_binding_handler(struct engine_node *node, void *data)
 
         if (sbrec_port_binding_is_updated(pb, SBREC_PORT_BINDING_COL_MAC) &&
             sset_contains(&ne_data->advertised_pbs, pb->logical_port)) {
+            return EN_UNHANDLED;
+        }
+    }
+
+    return EN_HANDLED_UNCHANGED;
+}
+
+static enum engine_input_handler_result
+neighbor_sb_fdb_handler(struct engine_node *node, void *data)
+{
+    struct ed_type_neighbor *ne_data = data;
+    struct ed_type_runtime_data *rt_data =
+        engine_get_input_data("runtime_data", node);
+    const struct sbrec_fdb_table *fdb_table =
+        EN_OVSDB_GET(engine_get_input("SB_fdb", node));
+
+    const struct sbrec_fdb *fdb;
+    SBREC_FDB_TABLE_FOR_EACH_TRACKED (fdb, fdb_table) {
+        struct local_datapath *ld =
+            get_local_datapath(&rt_data->local_datapaths, fdb->dp_key);
+        if (ld && hmapx_contains(&ne_data->fdb_datapaths, ld)) {
             return EN_UNHANDLED;
         }
     }
@@ -7212,6 +7250,8 @@ inc_proc_ovn_controller_init(
                      neighbor_sb_datapath_binding_handler);
     engine_add_input(&en_neighbor, &en_sb_port_binding,
                      neighbor_sb_port_binding_handler);
+    engine_add_input(&en_neighbor, &en_sb_fdb,
+                     neighbor_sb_fdb_handler);
     engine_add_input(&en_neighbor_exchange, &en_neighbor, NULL);
     engine_add_input(&en_neighbor_exchange, &en_host_if_monitor, NULL);
     engine_add_input(&en_neighbor_exchange, &en_neighbor_table_notify, NULL);

ovn-nb.xml
@@ -1041,9 +1041,13 @@
 
         <p>
           If <code>fdb</code> is specified then ovn-controller will advertise
-          all workloads that are local to the chassis. The applies to VIFs,
+          all workloads that are local to the chassis. This applies to VIFs,
           container ports, virtual ports, connected DGPs and connected GW
-          routers.
+          routers.  Additionally, dynamically learned FDB entries (from ports
+          with <code>unknown</code> addresses) are also advertised.  For VIF
+          ports, only entries learned on the local chassis are advertised;
+          for localnet ports, entries are advertised on all chassis where the
+          datapath is local.
         </p>
 
         <p>

tests/ovn-inc-proc-graph-dump.at
@@ -395,6 +395,7 @@ digraph "Incremental-Processing-Engine" {
 	runtime_data -> neighbor [[label="neighbor_runtime_data_handler"]];
 	SB_datapath_binding -> neighbor [[label="neighbor_sb_datapath_binding_handler"]];
 	SB_port_binding -> neighbor [[label="neighbor_sb_port_binding_handler"]];
+	SB_fdb -> neighbor [[label="neighbor_sb_fdb_handler"]];
 	host_if_monitor [[style=filled, shape=box, fillcolor=white, label="host_if_monitor"]];
 	neighbor_table_notify [[style=filled, shape=box, fillcolor=white, label="neighbor_table_notify"]];
 	neighbor_exchange_status [[style=filled, shape=box, fillcolor=white, label="neighbor_exchange_status"]];

tests/system-ovn.at
@@ -18388,6 +18388,68 @@ f0:00:0f:16:01:20 dev $LO_NAME master $BR_NAME static
 f0:00:0f:16:01:20 dev $LO_NAME vlan 1 master $BR_NAME static
 ])
 
+AS_BOX([L2 EVPN learned FDB advertising])
+
+# Add a port with "unknown" addresses and FDB learning enabled.
+# redistribute=fdb is already set from the previous section.
+check ovn-nbctl --wait=hv                                          \
+    -- lsp-add ls-evpn wl-unknown                                  \
+    -- lsp-set-addresses wl-unknown "unknown"
+
+ADD_NAMESPACES(wl-unknown)
+ADD_VETH(wl-unknown, wl-unknown, br-int, "172.16.1.30/24", "f0:00:0f:16:01:30")
+check ovn-nbctl --wait=hv sync
+wait_for_ports_up wl-unknown
+
+# Trigger FDB learning by sending traffic from the namespace.
+NS_CHECK_EXEC([wl-unknown], [ping -c 1 -W 1 172.16.1.10 > /dev/null 2>&1 || true])
+
+# Wait for the FDB entry to appear in the SB database.
+learned_mac=f0:00:0f:16:01:30
+wait_row_count fdb 1 mac='"'$learned_mac'"'
+
+# The dynamically learned FDB MAC should be advertised on the loopback.
+OVS_WAIT_FOR_OUTPUT_UNQUOTED([bridge fdb show | grep $LO_NAME | grep "$learned_mac" | sort], [0], [dnl
+$learned_mac dev $LO_NAME master $BR_NAME static
+$learned_mac dev $LO_NAME vlan 1 master $BR_NAME static
+])
+
+# Disabling dynamic-routing-advertise on the port should remove it.
+check ovn-nbctl --wait=hv set Logical_Switch_Port wl-unknown \
+    options:dynamic-routing-advertise=false
+OVS_WAIT_FOR_OUTPUT_UNQUOTED([bridge fdb show | grep $LO_NAME | grep "$learned_mac" | sort], [0], [dnl
+])
+
+# Re-enabling should bring it back.
+check ovn-nbctl --wait=hv set Logical_Switch_Port wl-unknown \
+    options:dynamic-routing-advertise=true
+OVS_WAIT_FOR_OUTPUT_UNQUOTED([bridge fdb show | grep $LO_NAME | grep "$learned_mac" | sort], [0], [dnl
+$learned_mac dev $LO_NAME master $BR_NAME static
+$learned_mac dev $LO_NAME vlan 1 master $BR_NAME static
+])
+
+# Also verify that FDB entries learned on localnet ports are advertised.
+# The localnet port already has addresses="unknown" from lsp-add-localnet-port.
+check ovn-nbctl --wait=hv set Logical_Switch_Port ln_port \
+    options:localnet_learn_fdb=true
+
+# Send traffic from the physical side to trigger localnet FDB learning.
+ADD_NAMESPACES(ext-host)
+ADD_VETH(ext-host, ext-host, br-ext, "172.16.1.40/24", "aa:bb:cc:dd:ee:ff")
+NS_CHECK_EXEC([ext-host], [ping -c 1 -W 1 172.16.1.10 > /dev/null 2>&1 || true])
+
+ln_learned_mac=aa:bb:cc:dd:ee:ff
+wait_row_count fdb 1 mac='"'$ln_learned_mac'"'
+OVS_WAIT_FOR_OUTPUT_UNQUOTED([bridge fdb show | grep $LO_NAME | grep "$ln_learned_mac" | sort], [0], [dnl
+$ln_learned_mac dev $LO_NAME master $BR_NAME static
+$ln_learned_mac dev $LO_NAME vlan 1 master $BR_NAME static
+])
+
+# Cleanup.
+check ovn-sbctl --all destroy FDB
+check ovn-nbctl --wait=hv lsp-del wl-unknown \
+    -- remove Logical_Switch_Port ln_port options localnet_learn_fdb
+
 check ovn-nbctl --wait=hv lsp-del workload2
 OVS_WAIT_FOR_OUTPUT_UNQUOTED([bridge fdb show | grep $LO_NAME | grep "f0:00:0f:16:01" | sort], [0], [dnl
 f0:00:0f:16:01:10 dev $LO_NAME master $BR_NAME static