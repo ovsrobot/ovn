ovsrobot/ovn

commit 59c7361e2613a84abd59217b50dd3775e58a51cb

pinctrl: Use unicast for MAC binding ARP probe.
We already have the MAC address within the MAC binding row. There
is no need to flood the ARP request as this serves as liveness
check. Send the ARP probe as unicast instead.

The test wait_row_count checks are narrowed to specific IP/port
combinations because unicast ARP is not intercepted by the
pipeline and instead it travels to the target. That will result
in additional MAC bindings which will remain through out the test.

Acked-by: Ilya Maximets <i.maximets@ovn.org>
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: Dumitru Ceara <dceara@redhat.com>

controller/mac-cache.c
@@ -961,7 +961,8 @@ mac_binding_probe_stats_run(struct vector *stats_vec, uint64_t *req_delay,
             send_self_originated_neigh_packet(probe_data->swconn,
                                               sbrec->datapath->tunnel_key,
                                               pb->tunnel_key, laddr.ea,
-                                              &local, &mb->data.ip,
+                                              mb->data.mac, &local,
+                                              &mb->data.ip,
                                               OFTABLE_LOCAL_OUTPUT);
         }
 

tests/ovn.at
@@ -37196,8 +37196,8 @@ OVS_WAIT_UNTIL([test $(ovs-ofctl dump-flows br-int table=OFTABLE_MAC_CACHE_USE |
 send_udp hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2a 192.168.20.100 192.168.10.100
 send_udp hv1 vif2 00:00:00:00:20:00 00:00:00:00:10:2b 192.168.10.100 192.168.20.100
 # Wait for ARP requests to be generated.
-dump_arp 1 00:00:00:00:20:00 ff:ff:ff:ff:ff:ff 192.168.20.1 192.168.20.100 00:00:00:00:00:00 > expected1
-dump_arp 1 00:00:00:00:10:00 ff:ff:ff:ff:ff:ff 192.168.10.1 192.168.10.100 00:00:00:00:00:00 > expected2
+dump_arp 1 00:00:00:00:20:00 00:00:00:00:10:1b 192.168.20.1 192.168.20.100 00:00:00:00:10:1b > expected1
+dump_arp 1 00:00:00:00:10:00 00:00:00:00:10:1a 192.168.10.1 192.168.10.100 00:00:00:00:10:1a > expected2
 OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected2])
 
 # Check MAC_Binding timestamp is updated receiving the ARP replay.
@@ -37206,8 +37206,8 @@ send_garp hv1 vif2 2 00:00:00:00:10:1b 00:00:00:00:20:00 192.168.20.100 192.168.
 OVS_WAIT_UNTIL([test $(fetch_column Mac_Binding timestamp ip=192.168.10.100) -gt $ts0])
 
 # Refresh OFTABLE_MAC_BINDING entires.
-dump_arp 1 00:00:00:00:20:00 ff:ff:ff:ff:ff:ff 192.168.20.1 192.168.20.100 00:00:00:00:00:00 >> expected1
-dump_arp 1 00:00:00:00:10:00 ff:ff:ff:ff:ff:ff 192.168.10.1 192.168.10.100 00:00:00:00:00:00 >> expected2
+dump_arp 1 00:00:00:00:20:00 00:00:00:00:10:1b 192.168.20.1 192.168.20.100 00:00:00:00:10:1b >> expected1
+dump_arp 1 00:00:00:00:10:00 00:00:00:00:10:1a 192.168.10.1 192.168.10.100 00:00:00:00:10:1a >> expected2
 OVS_WAIT_UNTIL([test $(ovs-ofctl dump-flows br-int table=OFTABLE_MAC_CACHE_USE | \
                        awk '/arp_spa=192.168.20.100/{print substr($6,10,1)}') -ge $((aging_th/2))])
 send_udp hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2a 192.168.20.100 192.168.10.100
@@ -37234,8 +37234,8 @@ OVS_WAIT_UNTIL([test $(ovs-ofctl dump-flows br-int table=OFTABLE_MAC_CACHE_USE |
 send_udp6 hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2a fd12::64 fd11::64
 send_udp6 hv1 vif2 00:00:00:00:20:00 00:00:00:00:10:2b fd11::64 fd12::64
 
-dump_ns 33:33:ff:00:00:64 00:00:00:00:10:00 ff02::1:ff00:64 fd11::1 fd11::64 > expected2
-dump_ns 33:33:ff:00:00:64 00:00:00:00:10:00 ff02::1:ff00:64 fd11::1 fd11::64 >> expected2
+dump_ns 00:00:00:00:10:1a 00:00:00:00:10:00 fd11::64 fd11::1 fd11::64 > expected2
+dump_ns 00:00:00:00:10:1a 00:00:00:00:10:00 fd11::64 fd11::1 fd11::64 >> expected2
 OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected2])
 
 send_na hv1 vif1 00:00:00:00:10:1a 00:00:00:00:10:00 fd11::64 fd11::1
@@ -37336,16 +37336,16 @@ send_udp6 hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2b fd11::64 fd12::64
 
 # Verify ARP probes use the correct source IPs from matching subnets.
 # ARP for 10.10.10.100 must use source IP 10.10.10.1 (first subnet).
-dump_arp 1 00:00:00:00:10:00 ff:ff:ff:ff:ff:ff 10.10.10.1 10.10.10.100 00:00:00:00:00:00 > expected
+dump_arp 1 00:00:00:00:10:00 00:00:00:00:10:1a 10.10.10.1 10.10.10.100 00:00:00:00:10:1a > expected
 # ARP for 42.42.42.253 must use source IP 42.42.42.1 (second subnet).
-dump_arp 1 00:00:00:00:10:00 ff:ff:ff:ff:ff:ff 42.42.42.1 42.42.42.253 00:00:00:00:00:00 >> expected
+dump_arp 1 00:00:00:00:10:00 00:00:00:00:10:1b 42.42.42.1 42.42.42.253 00:00:00:00:10:1b >> expected
 OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected])
 
 # Verify NS probes use the correct source IPs from matching subnets.
 # NS for fd11::64 must use source IP fd11::1 (first IPv6 subnet).
-dump_ns 33:33:ff:00:00:64 00:00:00:00:10:00 ff02::1:ff00:64 fd11::1 fd11::64 > expected_v6
+dump_ns 00:00:00:00:10:1a 00:00:00:00:10:00 fd11::64 fd11::1 fd11::64 > expected_v6
 # NS for fd12::64 must use source IP fd12::1 (second IPv6 subnet).
-dump_ns 33:33:ff:00:00:64 00:00:00:00:10:00 ff02::1:ff00:64 fd12::1 fd12::64 >> expected_v6
+dump_ns 00:00:00:00:10:1b 00:00:00:00:10:00 fd12::64 fd12::1 fd12::64 >> expected_v6
 OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected_v6])
 
 # Send ARP/NA replies and check MAC_Binding UUIDs remain consistent.
@@ -37422,8 +37422,8 @@ wait_row_count mac_binding 0 ip="$ext_ip" logical_port="lr-ls"
 
 # hv1 should have sent out arps for mac binding refresh
 arp_req=$(fmt_pkt \
-    "Ether(dst='ff:ff:ff:ff:ff:ff', src='$lrp_mac')/ \
-     ARP(hwsrc='$lrp_mac', hwdst='00:00:00:00:00:00', psrc='$lrp_ip', pdst='$ext_ip')")
+    "Ether(dst='$ext_mac', src='$lrp_mac')/ \
+     ARP(hwsrc='$lrp_mac', hwdst='$ext_mac', psrc='$lrp_ip', pdst='$ext_ip')")
 echo $arp_req >> hv1_snoopvif.expected
 OVN_CHECK_PACKETS_CONTAIN([hv1/snoopvif-tx.pcap], [hv1_snoopvif.expected])
 
@@ -37533,7 +37533,8 @@ send_imcp_echo_req hv1 public 00:00:00:00:10:00 00:00:00:00:10:1a 192.168.20.2 1
 OVS_WAIT_UNTIL([test $(grep -c "Sending ARP/ND.*ip: 192.168.20.2" hv1/ovn-controller.log) -eq 2])
 
 check ovn-nbctl --wait=hv acl-del join
-wait_row_count mac_binding 0
+wait_row_count mac_binding 0 ip="192.168.10.100" logical_port="gw-public"
+wait_row_count mac_binding 0 ip="192.168.20.2" logical_port="gw-join"
 
 send_icmp6_echo_req hv1 public 00:00:00:00:10:00 00:00:00:00:50:01 fd12::2 fd11::64
 wait_row_count mac_binding 1 mac=\"00:00:00:00:30:00\" ip=\"fd12::2\"
@@ -37550,7 +37551,7 @@ sleep $((aging_th / 2))
 send_icmp6_echo_req hv1 public 00:00:00:00:10:00 00:00:00:00:50:01 fd12::2 fd11::64
 OVS_WAIT_UNTIL([test $(grep -c "Sending ARP/ND.*ip: fd12::2" hv1/ovn-controller.log) -eq 2])
 
-wait_row_count mac_binding 0
+wait_row_count mac_binding 0 ip=\"fd12::2\" logical_port="gw-join"
 
 OVN_CLEANUP([hv1])
 AT_CLEANUP

controller/pinctrl.c
@@ -5199,7 +5199,8 @@ send_arp_nd_update(const struct sbrec_port_binding *pb, const char *nexthop,
 void
 send_self_originated_neigh_packet(struct rconn *swconn,
                                   uint32_t dp_key, uint32_t port_key,
-                                  struct eth_addr eth,
+                                  struct eth_addr eth_src,
+                                  struct eth_addr eth_dst,
                                   struct in6_addr *local,
                                   struct in6_addr *target,
                                   uint8_t table_id)
@@ -5208,13 +5209,15 @@ send_self_originated_neigh_packet(struct rconn *swconn,
     struct dp_packet packet;
     dp_packet_use_stub(&packet, packet_stub, sizeof packet_stub);
     if (!local) {
-        compose_rarp(&packet, eth);
+        compose_rarp(&packet, eth_src);
     } else if (IN6_IS_ADDR_V4MAPPED(local)) {
-        compose_arp(&packet, ARP_OP_REQUEST, eth, eth_addr_zero, true,
+        compose_arp(&packet, ARP_OP_REQUEST, eth_src, eth_dst,
+                    eth_addr_is_zero(eth_dst),
                     in6_addr_get_mapped_ipv4(local),
                     in6_addr_get_mapped_ipv4(target));
     } else {
-        compose_nd_ns(&packet, true, eth, eth_addr_zero, local, target);
+        compose_nd_ns(&packet, eth_addr_is_zero(eth_dst), eth_src,
+                      eth_dst, local, target);
     }
 
     /* Inject GARP request. */
@@ -5269,13 +5272,15 @@ send_garp_rarp(struct rconn *swconn, struct garp_rarp_node *garp_rarp,
         send_self_originated_neigh_packet(swconn,
                                           garp_rarp->dp_key,
                                           garp_rarp->port_key,
-                                          garp_rarp->ea, &addr, &addr,
+                                          garp_rarp->ea, eth_addr_zero,
+                                          &addr, &addr,
                                           OFTABLE_LOG_INGRESS_PIPELINE);
     } else {
         send_self_originated_neigh_packet(swconn,
                                           garp_rarp->dp_key,
                                           garp_rarp->port_key,
-                                          garp_rarp->ea, NULL, NULL,
+                                          garp_rarp->ea, eth_addr_zero,
+                                          NULL, NULL,
                                           OFTABLE_LOG_INGRESS_PIPELINE);
     }
 
@@ -6387,7 +6392,8 @@ send_arp_nd(struct rconn *swconn, struct arp_nd_data *e,
     /* Compose a ARP request packet. */
     send_self_originated_neigh_packet(swconn,
                                       e->dp_key, e->port_key,
-                                      e->ea, &e->src_ip, &e->dst_ip,
+                                      e->ea, eth_addr_zero,
+                                      &e->src_ip, &e->dst_ip,
                                       OFTABLE_LOCAL_OUTPUT);
 
     /* Set the next announcement.  At most 5 announcements are sent for a

controller/pinctrl.h
@@ -79,7 +79,8 @@ struct ovs_list *get_ports_to_activate_in_engine(void);
 bool pinctrl_is_port_activated(int64_t dp_key, int64_t port_key);
 void send_self_originated_neigh_packet(struct rconn *swconn,
                                        uint32_t dp_key, uint32_t port_key,
-                                       struct eth_addr eth,
+                                       struct eth_addr eth_src,
+                                       struct eth_addr eth_dst,
                                        struct in6_addr *local,
                                        struct in6_addr *target,
                                        uint8_t table_id);

commit 73715e028fe5315b1c6ed85a1a315033fe0c23ce

mac-cache: Send broadcast re-ARP probes after a while.
The re-ARP probes were using unicast to check if the host is still
alive to prevent the entry from aging out. The unicast works fine
and prevents unnecessary floods, however, there is a case when the
MAC address could have been changed without OVN learning about that.
In that case the unicast won't be ever responded to and the only way
to refresh that entry is to wait for it to age out. Send broadcast
after two unicast attempts with the timing that gives us usually
2 unicast probes and 2 broadcast, we will age out if neither of them
is responded to.

Fixes: 59c7361e2613 ("pinctrl: Use unicast for MAC binding ARP probe.")
Reported-at: https://redhat.atlassian.net/browse/FDP-4224
Assisted-by: Claude Opus 4.6, OpenCode
Signed-off-by: Ales Musil <amusil@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/mac-cache.c
@@ -34,6 +34,7 @@ VLOG_DEFINE_THIS_MODULE(mac_cache);
 #define BUFFER_QUEUE_DEPTH          4
 #define BUFFERED_PACKETS_TIMEOUT_MS 10000
 #define BUFFERED_PACKETS_LOOKUP_MS  100
+#define ARP_BROADCAST_THRESHOLD     2
 
 static uint32_t
 mac_binding_data_hash(const struct mac_binding_data *mb_data);
@@ -178,6 +179,7 @@ mac_binding_add(struct hmap *map, struct mac_binding_data mb_data,
     mb->data = mb_data;
     mb->sbrec = smb;
     mb->timestamp = timestamp;
+    mb->arp_attempts = 0;
     mac_binding_update_log("Added", &mb_data, false, NULL, 0, 0);
 }
 
@@ -908,6 +910,7 @@ mac_binding_probe_stats_run(struct vector *stats_vec, uint64_t *req_delay,
                     "Not sending ARP/ND request for recently updated",
                     &mb->data, true, threshold, stats->idle_age_ms,
                     since_updated_ms);
+            mb->arp_attempts = 0;
             continue;
         }
 
@@ -954,6 +957,11 @@ mac_binding_probe_stats_run(struct vector *stats_vec, uint64_t *req_delay,
         }
 
         if (!ipv6_addr_equals(&local, &in6addr_any)) {
+            struct eth_addr eth_dst =
+                mb->arp_attempts < ARP_BROADCAST_THRESHOLD
+                ? mb->data.mac
+                : eth_addr_zero;
+
             mac_binding_update_log("Sending ARP/ND request for active",
                                    &mb->data, true, threshold,
                                    stats->idle_age_ms, since_updated_ms);
@@ -961,9 +969,10 @@ mac_binding_probe_stats_run(struct vector *stats_vec, uint64_t *req_delay,
             send_self_originated_neigh_packet(probe_data->swconn,
                                               sbrec->datapath->tunnel_key,
                                               pb->tunnel_key, laddr.ea,
-                                              mb->data.mac, &local,
+                                              eth_dst, &local,
                                               &mb->data.ip,
                                               OFTABLE_LOCAL_OUTPUT);
+            mb->arp_attempts++;
         }
 
         destroy_lport_addresses(&laddr);

controller/mac-cache.h
@@ -78,6 +78,8 @@ struct mac_binding {
     const struct sbrec_mac_binding *sbrec;
     /* User specified timestamp (in ms) */
     long long timestamp;
+    /* Number of re-ARP attempts for given entry. */
+    size_t arp_attempts;
 };
 
 struct fdb_data {

tests/ovn.at
@@ -37564,6 +37564,108 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD([
+AT_SETUP([MAC binding aging - probing unicast to broadcast transition])
+CHECK_SCAPY
+ovn_start
+
+aging_th=5
+net_add n1
+sim_add hv1
+as hv1
+check ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+ovn-appctl -t ovn-controller vlog/set mac_cache:file:dbg pinctrl:file:dbg
+
+check ovn-nbctl                                                             \
+    -- ls-add ls1                                                           \
+    -- lr-add lr                                                            \
+    -- set logical_router lr options:mac_binding_age_threshold=$aging_th    \
+    -- lrp-add lr lr-ls1 00:00:00:00:10:00 10.10.10.1/24 42.42.42.1/24     \
+       fd11::1/64 fd12::1/64                                                \
+    -- lsp-add-router-port ls1 ls1-lr lr-ls1                                \
+    -- lsp-add ls1 vif1                                                     \
+    -- lsp-set-addresses vif1 "unknown"
+
+check ovs-vsctl                                                             \
+    -- add-port br-int vif1                                                 \
+    -- set interface vif1 external-ids:iface-id=vif1                        \
+    options:tx_pcap=hv1/vif1-tx.pcap options:rxq_pcap=hv1/vif1-rx.pcap
+
+OVN_POPULATE_ARP
+wait_for_ports_up
+check ovn-nbctl --wait=hv sync
+
+# Wait for pinctrl thread to be connected.
+OVS_WAIT_UNTIL([grep pinctrl hv1/ovn-controller.log | grep -q connected])
+
+# Create one IPv4 and one IPv6 MAC binding.
+send_garp hv1 vif1 2 00:00:00:00:10:1a ff:ff:ff:ff:ff:ff 10.10.10.100 10.10.10.100
+wait_row_count mac_binding 1 ip="10.10.10.100" logical_port="lr-ls1"
+
+send_na hv1 vif1 00:00:00:00:10:1a 00:00:00:00:10:00 fd11::64 fd11::1
+wait_row_count mac_binding 1 ip=\"fd11::64\" logical_port=\"lr-ls1\"
+
+# The first 3 probes are unicast (arp_attempts 0-2); the entry then falls
+# back to broadcast ARP / multicast NS.
+dump_arp 1 00:00:00:00:10:00 ff:ff:ff:ff:ff:ff 10.10.10.1 10.10.10.100 00:00:00:00:00:00 > expected_bcast
+OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected_bcast])
+
+dump_ns 33:33:ff:00:00:64 00:00:00:00:10:00 ff02::1:ff00:64 fd11::1 fd11::64 > expected_mcast
+OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected_mcast])
+
+dump_arp 1 00:00:00:00:10:00 00:00:00:00:10:1a 10.10.10.1 10.10.10.100 00:00:00:00:10:1a > expected_ucast_v4
+OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected_ucast_v4])
+
+dump_ns 00:00:00:00:10:1a 00:00:00:00:10:00 fd11::64 fd11::1 fd11::64 > expected_ucast_v6
+OVN_CHECK_PACKETS_CONTAIN([hv1/vif1-tx.pcap], [expected_ucast_v6])
+
+# Verify the reset-to-zero mechanism using a distinct pair of neighbours
+# kept alive (never re-created) for the whole check.  A single entry emits
+# at most ARP_BROADCAST_THRESHOLD (3) unicast probes before falling back to
+# broadcast, so a 4th unicast probe proves arp_attempts was reset.
+send_garp hv1 vif1 2 00:00:00:00:10:1b ff:ff:ff:ff:ff:ff 10.10.10.101 10.10.10.101
+wait_row_count mac_binding 1 ip="10.10.10.101" logical_port="lr-ls1"
+v4_uuid=$(fetch_column Mac_Binding _uuid ip=10.10.10.101)
+
+send_na hv1 vif1 00:00:00:00:10:1b 00:00:00:00:10:00 fd11::65 fd11::1
+wait_row_count mac_binding 1 ip=\"fd11::65\" logical_port=\"lr-ls1\"
+v6_uuid=$(fetch_column Mac_Binding _uuid ip=\"fd11::65\")
+
+# Keep the entries active (Tx towards them) and let a couple of unicast
+# probes go out (arp_attempts reaches 2, below the broadcast threshold).
+send_udp hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2a 10.10.10.101 42.42.42.100
+send_udp6 hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2a fd11::65 fd12::100
+OVS_WAIT_UNTIL([test $(grep -c "Sending ARP/ND.*ip: 10.10.10.101" hv1/ovn-controller.log) -ge 2])
+OVS_WAIT_UNTIL([test $(grep -c "Sending ARP/ND.*ip: fd11::65" hv1/ovn-controller.log) -ge 2])
+
+# The neighbours answer, refreshing the rows in place (resetting
+# arp_attempts).  Confirm the timestamps advanced.
+v4_ts=$(fetch_column Mac_Binding timestamp ip=10.10.10.101)
+v6_ts=$(fetch_column Mac_Binding timestamp ip=\"fd11::65\")
+send_garp hv1 vif1 2 00:00:00:00:10:1b 00:00:00:00:10:00 10.10.10.101 10.10.10.1
+send_na hv1 vif1 00:00:00:00:10:1b 00:00:00:00:10:00 fd11::65 fd11::1
+OVS_WAIT_UNTIL([test $(fetch_column Mac_Binding timestamp ip=10.10.10.101) -gt $v4_ts])
+OVS_WAIT_UNTIL([test $(fetch_column Mac_Binding timestamp ip=\"fd11::65\") -gt $v6_ts])
+
+# Probing must restart from unicast: wait for a 4th unicast ARP/NS probe
+# while the rows are still the original ones (same UUID, never re-created).
+dump_arp 1 00:00:00:00:10:00 00:00:00:00:10:1b 10.10.10.1 10.10.10.101 00:00:00:00:10:1b > ucast_v4.pkt
+dump_ns 00:00:00:00:10:1b 00:00:00:00:10:00 fd11::65 fd11::1 fd11::65 > ucast_v6.pkt
+OVS_WAIT_UNTIL([
+    send_udp hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2a 10.10.10.101 42.42.42.100
+    send_udp6 hv1 vif1 00:00:00:00:10:00 00:00:00:00:10:2a fd11::65 fd12::100
+    test "$(fetch_column Mac_Binding _uuid ip=10.10.10.101)" = "$v4_uuid" && \
+    test "$(fetch_column Mac_Binding _uuid ip=\"fd11::65\")" = "$v6_uuid" && \
+    test $($PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" hv1/vif1-tx.pcap | \
+           grep -Fc "$(cat ucast_v4.pkt)") -ge 4 && \
+    test $($PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" hv1/vif1-tx.pcap | \
+           grep -Fc "$(cat ucast_v6.pkt)") -ge 4])
+
+OVN_CLEANUP([hv1])
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD([
 AT_SETUP([MAC binding aging - probing distributed GW router])
 CHECK_SCAPY
 ovn_start