1e6ae822fd665eb581f557c0389aede3e30f5ca6
Ales Musil <amusil@redhat.com>
2026-09-10 10:52:24 +0200

    neighbor-of: Lift the reply limitation for ARP mac_cache_use flows.
    The mac_cache_use would have flows for ARP replies to bump the
    timestamp when we receive a reply to our probe. However, simple
    ARP request from the MAC binding IP+MAC should be enough to indicate
    that the entry is alive. Remove the response match and use plain
    ARP with proper source MAC address and SPA.
    
    Fixes: 9fc291c889bb ("controller: Update OFTABLE_MAC_CACHE_USE for ARP reply generated by the tracked device.")
    Fixes: c23059318cb7 ("northd, controller: Use the MAC cache table")
    Signed-off-by: Ales Musil <amusil@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- controller/neighbor-of.c
+++ controller/neighbor-of.c
@@ -50,7 +50,6 @@ consider_neighbor_flow(const struct sbrec_port_binding *pb,
         match_set_dl_type(&mb_cache_use_match, htons(ETH_TYPE_IP));
         match_set_nw_src(&mb_cache_use_match, ip_addr);
 
-        match_set_arp_opcode_masked(&lookup_arp_for_stats_match, 2, 0xff);
         match_set_arp_spa_masked(&lookup_arp_for_stats_match, ip_addr,
                                  htonl(0xffffffff));
     } else {
