ovsrobot/ovn

commit 5c3371922994c2d8a3610c9353902156db27d108

if-status: Add OVS interface status management module.
The initial implementation of the notification mechanism that indicates
if OVS flows corresponding to a logical switch port have been installed
is not resilient enough.  It didn't cover the case when transactions to
the local OVS DB or to the Southbound fail.

In order to deal with that, factor out the code that tracks the state
of the interfaces to a new module, 'if-status', and implement it with
a state machine that will retry setting Port_Binding.UP and
OVS.Interface.external_ids:ovn-installed in the Southbound and local
OVS databases.

Having a separate module makes sense because tracking the interface
state doesn't really depend on the rest of the binding module, except
for interacting with the Port_Binding and Interface IDL records.  For
this we add some additional APIs to binding.c.

Reported-at: https://bugzilla.redhat.com/1952846
Fixes: 4d3cb42b076b ("binding: Set Logical_Switch_Port.up when all OVS flows are installed.")
Signed-off-by: Dumitru Ceara <dceara@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>
Acked-by: Mark Michelson <mmichels@redhat.com>

controller/binding.c
@@ -16,9 +16,9 @@
 #include <config.h>
 #include "binding.h"
 #include "ha-chassis.h"
+#include "if-status.h"
 #include "lflow.h"
 #include "lport.h"
-#include "ofctrl-seqno.h"
 #include "patch.h"
 
 #include "lib/bitmap.h"
@@ -40,32 +40,6 @@ VLOG_DEFINE_THIS_MODULE(binding);
  * flows have been installed.
  */
 #define OVN_INSTALLED_EXT_ID "ovn-installed"
-
-/* Set of OVS interface IDs that have been released in the most recent
- * processing iterations.  This gets updated in release_lport() and is
- * periodically emptied in binding_seqno_run().
- */
-static struct sset binding_iface_released_set =
-    SSET_INITIALIZER(&binding_iface_released_set);
-
-/* Set of OVS interface IDs that have been bound in the most recent
- * processing iterations.  This gets updated in release_lport() and is
- * periodically emptied in binding_seqno_run().
- */
-static struct sset binding_iface_bound_set =
-    SSET_INITIALIZER(&binding_iface_bound_set);
-
-static void
-binding_iface_released_add(const char *iface_id)
-{
-    sset_add(&binding_iface_released_set, iface_id);
-}
-
-static void
-binding_iface_bound_add(const char *iface_id)
-{
-    sset_add(&binding_iface_bound_set, iface_id);
-}
 
 #define OVN_QOS_TYPE "linux-htb"
 
@@ -672,7 +646,8 @@ static void local_binding_destroy(struct local_binding *,
                                   struct shash *binding_lports);
 static void local_binding_delete(struct local_binding *,
                                  struct shash *local_bindings,
-                                 struct shash *binding_lports);
+                                 struct shash *binding_lports,
+                                 struct if_status_mgr *if_mgr);
 static struct binding_lport *local_binding_add_lport(
     struct shash *binding_lports,
     struct local_binding *,
@@ -692,6 +667,8 @@ static void binding_lport_delete(struct shash *binding_lports,
                                  struct binding_lport *);
 static void binding_lport_add(struct shash *binding_lports,
                               struct binding_lport *);
+static void binding_lport_set_up(struct binding_lport *, bool sb_readonly);
+static void binding_lport_set_down(struct binding_lport *, bool sb_readonly);
 static struct binding_lport *binding_lport_find(
     struct shash *binding_lports, const char *lport_name);
 static const struct sbrec_port_binding *binding_lport_get_parent_pb(
@@ -737,6 +714,93 @@ local_binding_get_primary_pb(struct shash *local_bindings, const char *pb_name)
     struct binding_lport *b_lport = local_binding_get_primary_lport(lbinding);
 
     return b_lport ? b_lport->pb : NULL;
+}
+
+bool
+local_binding_is_up(struct shash *local_bindings, const char *pb_name)
+{
+    struct local_binding *lbinding =
+        local_binding_find(local_bindings, pb_name);
+    struct binding_lport *b_lport = local_binding_get_primary_lport(lbinding);
+    if (lbinding && b_lport && lbinding->iface) {
+        if (b_lport->pb->n_up && !b_lport->pb->up[0]) {
+            return false;
+        }
+        return smap_get_bool(&lbinding->iface->external_ids,
+                             OVN_INSTALLED_EXT_ID, false);
+    }
+    return false;
+}
+
+bool
+local_binding_is_down(struct shash *local_bindings, const char *pb_name)
+{
+    struct local_binding *lbinding =
+        local_binding_find(local_bindings, pb_name);
+
+    struct binding_lport *b_lport = local_binding_get_primary_lport(lbinding);
+
+    if (!lbinding) {
+        return true;
+    }
+
+    if (lbinding->iface && smap_get_bool(&lbinding->iface->external_ids,
+                                         OVN_INSTALLED_EXT_ID, false)) {
+        return false;
+    }
+
+    if (b_lport && b_lport->pb->n_up && b_lport->pb->up[0]) {
+        return false;
+    }
+
+    return true;
+}
+
+void
+local_binding_set_up(struct shash *local_bindings, const char *pb_name,
+                     bool sb_readonly, bool ovs_readonly)
+{
+    struct local_binding *lbinding =
+        local_binding_find(local_bindings, pb_name);
+    struct binding_lport *b_lport = local_binding_get_primary_lport(lbinding);
+
+    if (!ovs_readonly && lbinding && lbinding->iface
+            && !smap_get_bool(&lbinding->iface->external_ids,
+                              OVN_INSTALLED_EXT_ID, false)) {
+        ovsrec_interface_update_external_ids_setkey(lbinding->iface,
+                                                    OVN_INSTALLED_EXT_ID,
+                                                    "true");
+    }
+
+    if (!sb_readonly && lbinding && b_lport && b_lport->pb->n_up) {
+        binding_lport_set_up(b_lport, sb_readonly);
+        LIST_FOR_EACH (b_lport, list_node, &lbinding->binding_lports) {
+            binding_lport_set_up(b_lport, sb_readonly);
+        }
+    }
+}
+
+void
+local_binding_set_down(struct shash *local_bindings, const char *pb_name,
+                       bool sb_readonly, bool ovs_readonly)
+{
+    struct local_binding *lbinding =
+        local_binding_find(local_bindings, pb_name);
+    struct binding_lport *b_lport = local_binding_get_primary_lport(lbinding);
+
+    if (!ovs_readonly && lbinding && lbinding->iface
+            && smap_get_bool(&lbinding->iface->external_ids,
+                             OVN_INSTALLED_EXT_ID, false)) {
+        ovsrec_interface_update_external_ids_delkey(lbinding->iface,
+                                                    OVN_INSTALLED_EXT_ID);
+    }
+
+    if (!sb_readonly && b_lport && b_lport->pb->n_up) {
+        binding_lport_set_down(b_lport, sb_readonly);
+        LIST_FOR_EACH (b_lport, list_node, &lbinding->binding_lports) {
+            binding_lport_set_down(b_lport, sb_readonly);
+        }
+    }
 }
 
 void
@@ -959,7 +1023,7 @@ static void
 claimed_lport_set_up(const struct sbrec_port_binding *pb,
                      const struct sbrec_port_binding *parent_pb,
                      const struct sbrec_chassis *chassis_rec,
-                     bool notify_up)
+                     bool notify_up, struct if_status_mgr *if_mgr)
 {
     if (!notify_up) {
         bool up = true;
@@ -970,7 +1034,7 @@ claimed_lport_set_up(const struct sbrec_port_binding *pb,
     }
 
     if (pb->chassis != chassis_rec || (pb->n_up && !pb->up[0])) {
-        binding_iface_bound_add(pb->logical_port);
+        if_status_mgr_claim_iface(if_mgr, pb->logical_port);
     }
 }
 
@@ -983,10 +1047,11 @@ claim_lport(const struct sbrec_port_binding *pb,
             const struct sbrec_chassis *chassis_rec,
             const struct ovsrec_interface *iface_rec,
             bool sb_readonly, bool notify_up,
-            struct hmap *tracked_datapaths)
+            struct hmap *tracked_datapaths,
+            struct if_status_mgr *if_mgr)
 {
     if (!sb_readonly) {
-        claimed_lport_set_up(pb, parent_pb, chassis_rec, notify_up);
+        claimed_lport_set_up(pb, parent_pb, chassis_rec, notify_up, if_mgr);
     }
 
     if (pb->chassis != chassis_rec) {
@@ -1034,7 +1099,7 @@ claim_lport(const struct sbrec_port_binding *pb,
  */
 static bool
 release_lport(const struct sbrec_port_binding *pb, bool sb_readonly,
-              struct hmap *tracked_datapaths)
+              struct hmap *tracked_datapaths, struct if_status_mgr *if_mgr)
 {
     if (pb->encap) {
         if (sb_readonly) {
@@ -1057,12 +1122,8 @@ release_lport(const struct sbrec_port_binding *pb, bool sb_readonly,
         sbrec_port_binding_set_virtual_parent(pb, NULL);
     }
 
-    if (pb->n_up) {
-        bool up = false;
-        sbrec_port_binding_set_up(pb, &up, 1);
-    }
     update_lport_tracking(pb, tracked_datapaths);
-    binding_iface_released_add(pb->logical_port);
+    if_status_mgr_release_iface(if_mgr, pb->logical_port);
     VLOG_INFO("Releasing lport %s from this chassis.", pb->logical_port);
     return true;
 }
@@ -1113,9 +1174,11 @@ release_binding_lport(const struct sbrec_chassis *chassis_rec,
     if (is_binding_lport_this_chassis(b_lport, chassis_rec)) {
         remove_local_lport_ids(b_lport->pb, b_ctx_out);
         if (!release_lport(b_lport->pb, sb_readonly,
-            b_ctx_out->tracked_dp_bindings)) {
+                           b_ctx_out->tracked_dp_bindings,
+                           b_ctx_out->if_mgr)) {
             return false;
         }
+        binding_lport_set_down(b_lport, sb_readonly);
     }
 
     return true;
@@ -1140,7 +1203,8 @@ consider_vif_lport_(const struct sbrec_port_binding *pb,
             if (!claim_lport(pb, parent_pb, b_ctx_in->chassis_rec,
                              b_lport->lbinding->iface,
                              !b_ctx_in->ovnsb_idl_txn,
-                             !parent_pb, b_ctx_out->tracked_dp_bindings)){
+                             !parent_pb, b_ctx_out->tracked_dp_bindings,
+                             b_ctx_out->if_mgr)){
                 return false;
             }
 
@@ -1171,7 +1235,8 @@ consider_vif_lport_(const struct sbrec_port_binding *pb,
         /* Release the lport if there is no lbinding. */
         if (!lbinding_set || !can_bind) {
             return release_lport(pb, !b_ctx_in->ovnsb_idl_txn,
-                                 b_ctx_out->tracked_dp_bindings);
+                                 b_ctx_out->tracked_dp_bindings,
+                                 b_ctx_out->if_mgr);
         }
     }
 
@@ -1269,7 +1334,8 @@ consider_container_lport(const struct sbrec_port_binding *pb,
         if (is_binding_lport_this_chassis(container_b_lport,
                                           b_ctx_in->chassis_rec)) {
             return release_lport(pb, !b_ctx_in->ovnsb_idl_txn,
-                                 b_ctx_out->tracked_dp_bindings);
+                                 b_ctx_out->tracked_dp_bindings,
+                                 b_ctx_out->if_mgr);
         }
 
         return true;
@@ -1367,10 +1433,12 @@ consider_nonvif_lport_(const struct sbrec_port_binding *pb,
         update_local_lport_ids(pb, b_ctx_out);
         return claim_lport(pb, NULL, b_ctx_in->chassis_rec, NULL,
                            !b_ctx_in->ovnsb_idl_txn, false,
-                           b_ctx_out->tracked_dp_bindings);
+                           b_ctx_out->tracked_dp_bindings,
+                           b_ctx_out->if_mgr);
     } else if (pb->chassis == b_ctx_in->chassis_rec) {
         return release_lport(pb, !b_ctx_in->ovnsb_idl_txn,
-                             b_ctx_out->tracked_dp_bindings);
+                             b_ctx_out->tracked_dp_bindings,
+                             b_ctx_out->if_mgr);
     }
 
     return true;
@@ -1978,7 +2046,8 @@ consider_iface_release(const struct ovsrec_interface *iface_rec,
     /* Check if the lbinding has children of type PB_CONTAINER.
      * If so, don't delete the local_binding. */
     if (lbinding && !is_lbinding_container_parent(lbinding)) {
-        local_binding_delete(lbinding, local_bindings, binding_lports);
+        local_binding_delete(lbinding, local_bindings, binding_lports,
+                             b_ctx_out->if_mgr);
     }
 
     remove_local_lports(iface_id, b_ctx_out);
@@ -2533,160 +2602,6 @@ delete_done:
     return handled;
 }
 
-/* Registered ofctrl seqno type for port_binding flow installation. */
-static size_t binding_seq_type_pb_cfg;
-
-/* Binding specific seqno to be acked by ofctrl when flows for new interfaces
- * have been installed.
- */
-static uint32_t binding_iface_seqno = 0;
-
-/* Map indexed by iface-id containing the sequence numbers that when acked
- * indicate that the OVS flows for the iface-id have been installed.
- */
-static struct simap binding_iface_seqno_map =
-    SIMAP_INITIALIZER(&binding_iface_seqno_map);
-
-void
-binding_init(void)
-{
-    binding_seq_type_pb_cfg = ofctrl_seqno_add_type();
-}
-
-/* Processes new release/bind operations OVN ports.  For newly bound ports
- * it creates ofctrl seqno update requests that will be acked when
- * corresponding OVS flows have been installed.
- *
- * NOTE: Should be called only when valid SB and OVS transactions are
- * available.
- */
-void
-binding_seqno_run(struct local_binding_data *lbinding_data)
-{
-    const char *iface_id;
-    const char *iface_id_next;
-    struct shash *local_bindings = &lbinding_data->bindings;
-    SSET_FOR_EACH_SAFE (iface_id, iface_id_next, &binding_iface_released_set) {
-        struct shash_node *lb_node = shash_find(local_bindings, iface_id);
-
-        /* If the local binding still exists (i.e., the OVS interface is
-         * still configured locally) then remove the external id and remove
-         * it from the in-flight seqno map.
-         */
-        if (lb_node) {
-            struct local_binding *lb = lb_node->data;
-
-            if (lb->iface && smap_get(&lb->iface->external_ids,
-                                      OVN_INSTALLED_EXT_ID)) {
-                ovsrec_interface_update_external_ids_delkey(
-                    lb->iface, OVN_INSTALLED_EXT_ID);
-            }
-        }
-        simap_find_and_delete(&binding_iface_seqno_map, iface_id);
-        sset_delete(&binding_iface_released_set,
-                    SSET_NODE_FROM_NAME(iface_id));
-    }
-
-    bool new_ifaces = false;
-    uint32_t new_seqno = binding_iface_seqno + 1;
-
-    SSET_FOR_EACH_SAFE (iface_id, iface_id_next, &binding_iface_bound_set) {
-        struct shash_node *lb_node = shash_find(local_bindings, iface_id);
-
-        struct local_binding *lb = lb_node ? lb_node->data : NULL;
-
-        /* Make sure the binding is still complete, i.e., both SB port_binding
-         * and OVS interface still exist.
-         *
-         * If so, then this is a newly bound interface, make sure we reset the
-         * Port_Binding 'up' field and the OVS Interface 'external-id'.
-         */
-        struct binding_lport *b_lport = local_binding_get_primary_lport(lb);
-        if (lb && b_lport && lb->iface
-                && !simap_contains(&binding_iface_seqno_map, lb->name)) {
-            new_ifaces = true;
-
-            if (smap_get(&lb->iface->external_ids, OVN_INSTALLED_EXT_ID)) {
-                ovsrec_interface_update_external_ids_delkey(
-                    lb->iface, OVN_INSTALLED_EXT_ID);
-            }
-            if (b_lport->pb->n_up) {
-                bool up = false;
-                sbrec_port_binding_set_up(b_lport->pb, &up, 1);
-            }
-            simap_put(&binding_iface_seqno_map, lb->name, new_seqno);
-        }
-        sset_delete(&binding_iface_bound_set, SSET_NODE_FROM_NAME(iface_id));
-    }
-
-    /* Request a seqno update when the flows for new interfaces have been
-     * installed in OVS.
-     */
-    if (new_ifaces) {
-        binding_iface_seqno = new_seqno;
-        ofctrl_seqno_update_create(binding_seq_type_pb_cfg, new_seqno);
-    }
-}
-
-/* Processes ofctrl seqno ACKs for new bindings.  Sets the
- * 'OVN_INSTALLED_EXT_ID' external-id in the OVS interface and the
- * Port_Binding.up field for all ports for which OVS flows have been
- * installed.
- *
- * NOTE: Should be called only when valid SB and OVS transactions are
- * available.
- */
-void
-binding_seqno_install(struct local_binding_data *lbinding_data)
-{
-    struct ofctrl_acked_seqnos *acked_seqnos =
-            ofctrl_acked_seqnos_get(binding_seq_type_pb_cfg);
-    struct simap_node *node;
-    struct simap_node *node_next;
-    struct shash *local_bindings = &lbinding_data->bindings;
-
-    SIMAP_FOR_EACH_SAFE (node, node_next, &binding_iface_seqno_map) {
-        struct shash_node *lb_node = shash_find(local_bindings, node->name);
-
-        if (!lb_node) {
-            goto del_seqno;
-        }
-
-        struct local_binding *lb = lb_node->data;
-        struct binding_lport *b_lport = local_binding_get_primary_lport(lb);
-        if (!b_lport || !lb->iface) {
-            goto del_seqno;
-        }
-
-        if (!ofctrl_acked_seqnos_contains(acked_seqnos, node->data)) {
-            continue;
-        }
-
-        ovsrec_interface_update_external_ids_setkey(lb->iface,
-                                                    OVN_INSTALLED_EXT_ID,
-                                                    "true");
-        if (b_lport->pb->n_up) {
-            bool up = true;
-
-            sbrec_port_binding_set_up(b_lport->pb, &up, 1);
-            LIST_FOR_EACH (b_lport, list_node, &lb->binding_lports) {
-                sbrec_port_binding_set_up(b_lport->pb, &up, 1);
-            }
-        }
-
-del_seqno:
-        simap_delete(&binding_iface_seqno_map, node);
-    }
-
-    ofctrl_acked_seqnos_destroy(acked_seqnos);
-}
-
-void
-binding_seqno_flush(void)
-{
-    simap_clear(&binding_iface_seqno_map);
-}
-
 /* Static functions for local_lbindind and binding_lport. */
 static struct local_binding *
 local_binding_create(const char *name, const struct ovsrec_interface *iface)
@@ -2728,9 +2643,11 @@ local_binding_destroy(struct local_binding *lbinding,
 static void
 local_binding_delete(struct local_binding *lbinding,
                      struct shash *local_bindings,
-                     struct shash *binding_lports)
+                     struct shash *binding_lports,
+                     struct if_status_mgr *if_mgr)
 {
     shash_find_and_delete(local_bindings, lbinding->name);
+    if_status_mgr_delete_iface(if_mgr, lbinding->name);
     local_binding_destroy(lbinding, binding_lports);
 }
 
@@ -2897,6 +2814,27 @@ binding_lport_delete(struct shash *binding_lports,
     binding_lport_destroy(b_lport);
 }
 
+static void
+binding_lport_set_up(struct binding_lport *b_lport, bool sb_readonly)
+{
+    if (sb_readonly || !b_lport || !b_lport->pb->n_up || b_lport->pb->up[0]) {
+        return;
+    }
+
+    bool up = true;
+    sbrec_port_binding_set_up(b_lport->pb, &up, 1);
+}
+
+static void
+binding_lport_set_down(struct binding_lport *b_lport, bool sb_readonly)
+{
+    if (sb_readonly || !b_lport || !b_lport->pb->n_up || !b_lport->pb->up[0]) {
+        return;
+    }
+
+    bool up = false;
+    sbrec_port_binding_set_up(b_lport->pb, &up, 1);
+}
 
 static const struct sbrec_port_binding *
 binding_lport_get_parent_pb(struct binding_lport *b_lport)

controller/automake.mk
@@ -10,6 +10,8 @@ controller_ovn_controller_SOURCES = \
 	controller/encaps.h \
 	controller/ha-chassis.c \
 	controller/ha-chassis.h \
+	controller/if-status.c \
+	controller/if-status.h \
 	controller/ip-mcast.c \
 	controller/ip-mcast.h \
 	controller/lflow.c \

controller/binding.h
@@ -37,6 +37,7 @@ struct sbrec_port_binding_table;
 struct sset;
 struct sbrec_port_binding;
 struct ds;
+struct if_status_mgr;
 
 struct binding_ctx_in {
     struct ovsdb_idl_txn *ovnsb_idl_txn;
@@ -85,6 +86,8 @@ struct binding_ctx_out {
      * binding_handle_port_binding_changes) fills in for
      * the changed datapaths and port bindings. */
     struct hmap *tracked_dp_bindings;
+
+    struct if_status_mgr *if_mgr;
 };
 
 struct local_binding_data {
@@ -97,6 +100,12 @@ void local_binding_data_destroy(struct local_binding_data *);
 
 const struct sbrec_port_binding *local_binding_get_primary_pb(
     struct shash *local_bindings, const char *pb_name);
+bool local_binding_is_up(struct shash *local_bindings, const char *pb_name);
+bool local_binding_is_down(struct shash *local_bindings, const char *pb_name);
+void local_binding_set_up(struct shash *local_bindings, const char *pb_name,
+                          bool sb_readonly, bool ovs_readonly);
+void local_binding_set_down(struct shash *local_bindings, const char *pb_name,
+                            bool sb_readonly, bool ovs_readonly);
 
 /* Represents a tracked binding logical port. */
 struct tracked_binding_lport {
@@ -123,10 +132,6 @@ bool binding_handle_port_binding_changes(struct binding_ctx_in *,
                                          struct binding_ctx_out *);
 void binding_tracked_dp_destroy(struct hmap *tracked_datapaths);
 
-void binding_init(void);
-void binding_seqno_run(struct local_binding_data *lbinding_data);
-void binding_seqno_install(struct local_binding_data *lbinding_data);
-void binding_seqno_flush(void);
 void binding_dump_local_bindings(struct local_binding_data *, struct ds *);
 
 /* Generates a sset of lport names from local_binding_data.

commit f39e632f3541fdb4cd5fc7089549f58ab84191a9

controller: Set untracked ports down on release.
if_status_mgr_release_iface() only moves an interface to OIF_MARK_DOWN,
and therefore only clears Port_Binding "up", for interfaces this
ovn-controller instance claimed itself.  Ports that were already bound to
this chassis when ovn-controller started are adopted without being
tracked, so when the recompute path releases one of them, for example
after a hypervisor crash where the VM's tap is gone but the Port_Binding
still points at this chassis, the chassis is cleared but "up" stays true.

That leaves the port unbound but up, a state that cannot happen
otherwise.  Consumers that only look at "up", such as ovn-northd when it
decides whether a load balancer service monitor may stay "online", keep
treating the port as up although nothing probes the backend anymore, so
traffic is still sent to it.

Set "up" to false in release_lport() when the port is not tracked by
if-status, matching what the tracked path does.

CC: Dumitru Ceara <dceara@redhat.com>
Fixes: 5c3371922994 ("if-status: Add OVS interface status management module.")
Assisted-by: Claude Opus 5, Claude Code
Signed-off-by: Jaygue Lee <enginrect@gmail.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

controller/binding.c
@@ -1585,6 +1585,14 @@ release_lport(const struct sbrec_port_binding *pb,
         if (!release_lport_main_chassis(pb, sb_readonly, if_mgr)) {
             return false;
         }
+        /* A port claimed by a previous ovn-controller instance is not
+         * tracked by if-status, which would otherwise set it down. */
+        if (!sb_readonly && pb->n_up && pb->up[0] &&
+            !if_status_mgr_iface_is_present(if_mgr, pb->logical_port)) {
+            bool up = false;
+            sbrec_port_binding_set_up(pb, &up, 1);
+            VLOG_INFO("Setting lport %s down in Southbound", pb->logical_port);
+        }
     } else if (is_additional_chassis(pb, chassis_rec)) {
         if (!release_lport_additional_chassis(pb, chassis_rec, sb_readonly)) {
             return false;

tests/ovn-controller.at
@@ -3071,6 +3071,48 @@ AT_CLEANUP
 ])
 
 OVN_FOR_EACH_NORTHD([
+AT_SETUP([ovn-controller - released untracked port is set down])
+ovn_start
+
+net_add n1
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+
+check ovn-nbctl ls-add sw0
+check ovn-nbctl lsp-add sw0 sw0-p1 -- lsp-set-addresses sw0-p1 \
+"00:00:00:00:00:01 10.0.0.1"
+check ovs-vsctl -- add-port br-int hv1-vif1 -- \
+    set interface hv1-vif1 external-ids:iface-id=sw0-p1
+hv1_uuid=$(fetch_column Chassis _uuid name=hv1)
+wait_row_count Port_Binding 1 logical_port=sw0-p1 chassis=$hv1_uuid 'up=true'
+wait_row_count nb:Logical_Switch_Port 1 name=sw0-p1 'up=true'
+
+# Stop ovn-controller without releasing anything (as after a crash), remove
+# the VIF while it is stopped (the VM did not come back) and start it again.
+# The port is still bound to hv1 in the SB but this ovn-controller instance
+# never claimed it, so if-status does not track it.
+check ovn-appctl -t ovn-controller exit --restart
+check ovs-vsctl del-port br-int hv1-vif1
+start_daemon ovn-controller
+
+# The port must be released and set down.
+wait_row_count Port_Binding 1 logical_port=sw0-p1 'chassis=[[]]' 'up=false'
+wait_row_count nb:Logical_Switch_Port 1 name=sw0-p1 'up=false'
+
+# Adding the VIF back claims the port again as usual.
+check ovs-vsctl -- add-port br-int hv1-vif1 -- \
+    set interface hv1-vif1 external-ids:iface-id=sw0-p1
+wait_row_count Port_Binding 1 logical_port=sw0-p1 chassis=$hv1_uuid 'up=true'
+wait_row_count nb:Logical_Switch_Port 1 name=sw0-p1 'up=true'
+
+OVN_CLEANUP([hv1
+/Trying to release unknown interface sw0-p1/d])
+AT_CLEANUP
+])
+
+OVN_FOR_EACH_NORTHD([
 AT_SETUP([Encap enforce local_ip])
 ovn_start
 