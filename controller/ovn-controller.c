ovsrobot/ovn

commit 8be01f4a5329ed1d4c920e0b8488bd4a5410ffb9

Send service monitor health checks
ovn-controller will periodically sends out the service monitor packets
for the services configured in the SB DB Service_Monitor table. This
patch makes use of the action - handle_svc_check to handle the service
monitor reply packets from the service.

This patch supports IPv4 TCP and UDP service monitoring. For TCP services,
it sends out a TCP SYN packet and expects TCP ACK packet in response.
If the response is received on time, the status of the service is set to
"online", otherwise it is set to "offline".

For UDP services, it sends out a empty UDP packet and doesn't expect any
reply. In case the service is down, the host running the service, sends out
ICMP type 3 code 4 (destination unreachable) packet. If ovn-controller receives this
ICMP packet, it sets the status of the service to "offline".

Right now only IPv4 service monitoring is supported. An upcoming patch will add
the support for IPv6.

Acked-by: Mark Michelson <mmichels@redhat.com>
Signed-off-by: Numan Siddique <numans@ovn.org>

controller/ovn-controller.c
@@ -2083,6 +2083,8 @@ main(int argc, char *argv[])
                                 sbrec_dns_table_get(ovnsb_idl_loop.idl),
                                 sbrec_controller_event_table_get(
                                     ovnsb_idl_loop.idl),
+                                sbrec_service_monitor_table_get(
+                                    ovnsb_idl_loop.idl),
                                 br_int, chassis,
                                 &ed_runtime_data.local_datapaths,
                                 &ed_runtime_data.active_tunnels);

controller/pinctrl.h
@@ -30,6 +30,7 @@ struct ovsrec_bridge;
 struct sbrec_chassis;
 struct sbrec_dns_table;
 struct sbrec_controller_event_table;
+struct sbrec_service_monitor_table;
 
 void pinctrl_init(void);
 void pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
@@ -42,6 +43,7 @@ void pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
                  struct ovsdb_idl_index *sbrec_ip_multicast_opts,
                  const struct sbrec_dns_table *,
                  const struct sbrec_controller_event_table *,
+                 const struct sbrec_service_monitor_table *,
                  const struct ovsrec_bridge *, const struct sbrec_chassis *,
                  const struct hmap *local_datapaths,
                  const struct sset *active_tunnels);

northd/ovn-northd.8.xml
@@ -1010,6 +1010,16 @@ output;
 
     <ul>
       <li>
+        A priorirty-110 flow with the match
+        <code>eth.src == <var>E</var></code> for all logical switch
+        datapaths and applies the action <code>handle_svc_check(inport)</code>.
+        Where <var>E</var> is the service monitor mac defined in the
+        <ref column="options:svc_monitor_mac" table="NB_Global"
+        db="OVN_Northbound"/> colum of <ref table="NB_Global"
+        db="OVN_Northbound"/> table.
+      </li>
+
+      <li>
         A priority-100 flow that punts all IGMP packets to
         <code>ovn-controller</code> if IGMP snooping is enabled on the
         logical switch. The flow also forwards the IGMP packets to the

northd/ovn-northd.c
@@ -6182,12 +6182,16 @@ build_lswitch_flows(struct hmap *datapaths, struct hmap *ports,
         }
     }
 
+    char *svc_check_match = xasprintf("eth.dst == %s", svc_monitor_mac);
     /* Ingress table 17: Destination lookup, broadcast and multicast handling
      * (priority 70 - 100). */
     HMAP_FOR_EACH (od, key_node, datapaths) {
         if (!od->nbs) {
             continue;
         }
+
+        ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_LKUP, 110, svc_check_match,
+                      "handle_svc_check(inport);");
 
         struct mcast_switch_info *mcast_sw_info = &od->mcast_info.sw;
 
@@ -6248,6 +6252,7 @@ build_lswitch_flows(struct hmap *datapaths, struct hmap *ports,
         ovn_lflow_add(lflows, od, S_SWITCH_IN_L2_LKUP, 70, "eth.mcast",
                       "outport = \""MC_FLOOD"\"; output;");
     }
+    free(svc_check_match);
 
     /* Ingress table 17: Add IP multicast flows learnt from IGMP
      * (priority 90). */
@@ -10502,6 +10507,11 @@ static const char *rbac_mac_binding_auth[] =
 static const char *rbac_mac_binding_update[] =
     {"logical_port", "ip", "mac", "datapath"};
 
+static const char *rbac_svc_monitor_auth[] =
+    {""};
+static const char *rbac_svc_monitor_auth_update[] =
+    {"status"};
+
 static struct rbac_perm_cfg {
     const char *table;
     const char **auth;
@@ -10542,6 +10552,14 @@ static struct rbac_perm_cfg {
         .insdel = true,
         .update = rbac_mac_binding_update,
         .n_update = ARRAY_SIZE(rbac_mac_binding_update),
+        .row = NULL
+    },{
+        .table = "Service_Monitor",
+        .auth = rbac_svc_monitor_auth,
+        .n_auth = ARRAY_SIZE(rbac_svc_monitor_auth),
+        .insdel = false,
+        .update = rbac_svc_monitor_auth_update,
+        .n_update = ARRAY_SIZE(rbac_svc_monitor_auth_update),
         .row = NULL
     },{
         .table = NULL,

tests/ovn.at
@@ -16723,7 +16723,126 @@ as hv4 ovs-ofctl show br-phys
 as hv4 ovs-appctl fdb/show br-phys
 
 OVN_CLEANUP([hv1],[hv2],[hv3],[hv4])
+AT_CLEANUP
 
+AT_SETUP([ovn -- Load balancer health checks])
+AT_KEYWORDS([lb])
+ovn_start
+
+net_add n1
+
+sim_add hv1
+as hv1
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.1
+ovs-vsctl -- add-port br-int hv1-vif1 -- \
+    set interface hv1-vif1 external-ids:iface-id=sw0-p1 \
+    options:tx_pcap=hv1/vif1-tx.pcap \
+    options:rxq_pcap=hv1/vif1-rx.pcap \
+    ofport-request=1
+ovs-vsctl -- add-port br-int hv1-vif2 -- \
+    set interface hv1-vif2 external-ids:iface-id=sw0-p2 \
+    options:tx_pcap=hv1/vif2-tx.pcap \
+    options:rxq_pcap=hv1/vif2-rx.pcap \
+    ofport-request=2
+
+sim_add hv2
+as hv2
+ovs-vsctl add-br br-phys
+ovn_attach n1 br-phys 192.168.0.2
+ovs-vsctl -- add-port br-int hv2-vif1 -- \
+    set interface hv2-vif1 external-ids:iface-id=sw1-p1 \
+    options:tx_pcap=hv2/vif1-tx.pcap \
+    options:rxq_pcap=hv2/vif1-rx.pcap \
+    ofport-request=1
+
+ovn-nbctl ls-add sw0
+
+ovn-nbctl lsp-add sw0 sw0-p1
+ovn-nbctl lsp-set-addresses sw0-p1 "50:54:00:00:00:03 10.0.0.3"
+ovn-nbctl lsp-set-port-security sw0-p1 "50:54:00:00:00:03 10.0.0.3"
+
+ovn-nbctl lsp-set-addresses sw0-p2 "50:54:00:00:00:04 10.0.0.4"
+ovn-nbctl lsp-set-port-security sw0-p2 "50:54:00:00:00:04 10.0.0.4"
+
+# Create the second logical switch with one port
+ovn-nbctl ls-add sw1
+ovn-nbctl lsp-add sw1 sw1-p1
+ovn-nbctl lsp-set-addresses sw1-p1 "40:54:00:00:00:03 20.0.0.3"
+ovn-nbctl lsp-set-port-security sw1-p1 "40:54:00:00:00:03 20.0.0.3"
+
+# Create a logical router and attach both logical switches
+ovn-nbctl lr-add lr0
+ovn-nbctl lrp-add lr0 lr0-sw0 00:00:00:00:ff:01 10.0.0.1/24
+ovn-nbctl lsp-add sw0 sw0-lr0
+ovn-nbctl lsp-set-type sw0-lr0 router
+ovn-nbctl lsp-set-addresses sw0-lr0 router
+ovn-nbctl lsp-set-options sw0-lr0 router-port=lr0-sw0
+
+ovn-nbctl lrp-add lr0 lr0-sw1 00:00:00:00:ff:02 20.0.0.1/24
+ovn-nbctl lsp-add sw1 sw1-lr0
+ovn-nbctl lsp-set-type sw1-lr0 router
+ovn-nbctl lsp-set-addresses sw1-lr0 router
+ovn-nbctl lsp-set-options sw1-lr0 router-port=lr0-sw1
+
+ovn-nbctl lb-add lb1 10.0.0.10:80 10.0.0.3:80,20.0.0.3:80
+
+ovn-nbctl --wait=sb set load_balancer . ip_port_mappings:10.0.0.3=sw0-p1:10.0.0.2
+ovn-nbctl --wait=sb set load_balancer . ip_port_mappings:20.0.0.3=sw1-p1:20.0.0.2
+
+ovn-nbctl --wait=sb -- --id=@hc create \
+Load_Balancer_Health_Check vip="10.0.0.10\:80" -- add Load_Balancer . \
+health_check @hc
+
+ovn-nbctl --wait=sb ls-lb-add sw0 lb1
+ovn-nbctl --wait=sb ls-lb-add sw1 lb1
+ovn-nbctl --wait=sb lr-lb-add lr0 lb1
+
+OVN_POPULATE_ARP
+ovn-nbctl --wait=hv sync
+
+OVS_WAIT_UNTIL([test 2 = `ovn-sbctl --bare --columns _uuid find \
+service_monitor | sed '/^$/d' | wc -l`])
+
+ovn-sbctl dump-flows sw0 | grep ct_lb | grep priority=120 > lflows.txt
+AT_CHECK([cat lflows.txt], [0], [dnl
+  table=10(ls_in_stateful     ), priority=120  , match=(ct.new && ip4.dst == 10.0.0.10 && tcp.dst == 80), action=(ct_lb(10.0.0.3:80,20.0.0.3:80);)
+])
+
+# get the svc monitor mac.
+svc_mon_src_mac=`ovn-nbctl get NB_Global . options:svc_monitor_mac | \
+sed s/":"//g | sed s/\"//g`
+
+OVS_WAIT_UNTIL(
+    [test 1 = `$PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" hv1/vif1-tx.pcap | \
+grep "505400000003${svc_mon_src_mac}" | wc -l`]
+)
+
+OVS_WAIT_UNTIL(
+    [test 1 = `$PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" hv2/vif1-tx.pcap | \
+grep "405400000003${svc_mon_src_mac}" | wc -l`]
+)
+
+OVS_WAIT_UNTIL([test 2 = `ovn-sbctl --bare --columns status find \
+service_monitor | grep offline | wc -l`])
+
+OVS_WAIT_UNTIL(
+    [test 2 = `$PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" hv1/vif1-tx.pcap | \
+grep "505400000003${svc_mon_src_mac}" | wc -l`]
+)
+
+OVS_WAIT_UNTIL(
+    [test 2 = `$PYTHON "$ovs_srcdir/utilities/ovs-pcap.in" hv2/vif1-tx.pcap | \
+grep "405400000003${svc_mon_src_mac}" | wc -l`]
+)
+
+ovn-sbctl dump-flows sw0 | grep "ip4.dst == 10.0.0.10 && tcp.dst == 80" \
+| grep priority=120 > lflows.txt
+AT_CHECK([cat lflows.txt], [0], [dnl
+  table=10(ls_in_stateful     ), priority=120  , match=(ct.new && ip4.dst == 10.0.0.10 && tcp.dst == 80), action=(drop;)
+])
+
+OVN_CLEANUP([hv1], [hv2])
 AT_CLEANUP
 
 AT_SETUP([ovn -- ARP/ND request broadcast limiting])

tests/system-common-macros.at
@@ -267,6 +267,7 @@ m4_define([OVS_CHECK_FIREWALL],
 #
 m4_define([OVS_START_L7],
    [PIDFILE=$(mktemp $2XXX.pid)
+    echo $PIDFILE > l7_pid_file
     NETNS_DAEMONIZE([$1], [[$PYTHON $srcdir/test-l7.py $2]], [$PIDFILE])
 
     dnl netstat doesn't print http over IPv6 as "http6"; drop the number.

tests/system-ovn.at
@@ -3245,3 +3245,183 @@ as
 OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
 /connection dropped.*/d"])
 AT_CLEANUP
+
+AT_SETUP([ovn -- Load balancer health checks])
+AT_KEYWORDS([lb])
+ovn_start
+
+OVS_TRAFFIC_VSWITCHD_START()
+ADD_BR([br-int])
+
+# Set external-ids in br-int needed for ovn-controller
+ovs-vsctl \
+        -- set Open_vSwitch . external-ids:system-id=hv1 \
+        -- set Open_vSwitch . external-ids:ovn-remote=unix:$ovs_base/ovn-sb/ovn-sb.sock \
+        -- set Open_vSwitch . external-ids:ovn-encap-type=geneve \
+        -- set Open_vSwitch . external-ids:ovn-encap-ip=169.0.0.1 \
+        -- set bridge br-int fail-mode=secure other-config:disable-in-band=true
+
+# Start ovn-controller
+start_daemon ovn-controller
+
+ovn-nbctl ls-add sw0
+
+ovn-nbctl lsp-add sw0 sw0-p1
+ovn-nbctl lsp-set-addresses sw0-p1 "50:54:00:00:00:03 10.0.0.3"
+ovn-nbctl lsp-set-port-security sw0-p1 "50:54:00:00:00:03 10.0.0.3"
+
+ovn-nbctl lsp-add sw0 sw0-p2
+ovn-nbctl lsp-set-addresses sw0-p2 "50:54:00:00:00:04 10.0.0.4"
+ovn-nbctl lsp-set-port-security sw0-p2 "50:54:00:00:00:04 10.0.0.4"
+
+# Create the second logical switch with one port
+ovn-nbctl ls-add sw1
+ovn-nbctl lsp-add sw1 sw1-p1
+ovn-nbctl lsp-set-addresses sw1-p1 "40:54:00:00:00:03 20.0.0.3"
+ovn-nbctl lsp-set-port-security sw1-p1 "40:54:00:00:00:03 20.0.0.3"
+
+# Create a logical router and attach both logical switches
+ovn-nbctl lr-add lr0
+ovn-nbctl lrp-add lr0 lr0-sw0 00:00:00:00:ff:01 10.0.0.1/24
+ovn-nbctl lsp-add sw0 sw0-lr0
+ovn-nbctl lsp-set-type sw0-lr0 router
+ovn-nbctl lsp-set-addresses sw0-lr0 router
+ovn-nbctl lsp-set-options sw0-lr0 router-port=lr0-sw0
+
+ovn-nbctl lrp-add lr0 lr0-sw1 00:00:00:00:ff:02 20.0.0.1/24
+ovn-nbctl lsp-add sw1 sw1-lr0
+ovn-nbctl lsp-set-type sw1-lr0 router
+ovn-nbctl lsp-set-addresses sw1-lr0 router
+ovn-nbctl lsp-set-options sw1-lr0 router-port=lr0-sw1
+
+ovn-nbctl lb-add lb1 10.0.0.10:80 10.0.0.3:80,20.0.0.3:80
+
+ovn-nbctl --wait=sb set load_balancer . ip_port_mappings:10.0.0.3=sw0-p1:10.0.0.2
+ovn-nbctl --wait=sb set load_balancer . ip_port_mappings:20.0.0.3=sw1-p1:20.0.0.2
+
+ovn-nbctl --wait=sb -- --id=@hc create \
+Load_Balancer_Health_Check vip="10.0.0.10\:80" -- add Load_Balancer . \
+health_check @hc
+
+ovn-nbctl --wait=sb ls-lb-add sw0 lb1
+ovn-nbctl --wait=sb ls-lb-add sw1 lb1
+ovn-nbctl --wait=sb lr-lb-add lr0 lb1
+
+OVN_POPULATE_ARP
+ovn-nbctl --wait=hv sync
+
+ADD_NAMESPACES(sw0-p1)
+ADD_VETH(sw0-p1, sw0-p1, br-int, "10.0.0.3/24", "50:54:00:00:00:03", \
+         "10.0.0.1")
+
+ADD_NAMESPACES(sw1-p1)
+ADD_VETH(sw1-p1, sw1-p1, br-int, "20.0.0.3/24", "40:54:00:00:00:03", \
+         "20.0.0.1")
+
+ADD_NAMESPACES(sw0-p2)
+ADD_VETH(sw0-p2, sw0-p2, br-int, "10.0.0.4/24", "50:54:00:00:00:04", \
+         "10.0.0.1")
+
+# Wait until all the services are set to offline.
+OVS_WAIT_UNTIL([test 2 = `ovn-sbctl --bare --columns status find \
+service_monitor | sed '/^$/d' | grep offline | wc -l`])
+
+# Start webservers in 'sw0-p1' and 'sw1-p1'.
+OVS_START_L7([sw0-p1], [http])
+sw0_p1_pid_file=`cat l7_pid_file`
+OVS_START_L7([sw1-p1], [http])
+
+# Wait until the services are set to online.
+OVS_WAIT_UNTIL([test 2 = `ovn-sbctl --bare --columns status find \
+service_monitor | sed '/^$/d' | grep online | wc -l`])
+
+OVS_WAIT_UNTIL(
+    [ovn-sbctl dump-flows sw0 | grep ct_lb | grep priority=120 | grep "ip4.dst == 10.0.0.10" > lflows.txt
+     test 1 = `cat lflows.txt | grep "ct_lb(10.0.0.3:80,20.0.0.3:80)" | wc -l`]
+)
+
+# From sw0-p2 send traffic to vip - 10.0.0.10
+for i in `seq 1 20`; do
+    echo Request $i
+    ovn-sbctl list service_monitor
+    NS_CHECK_EXEC([sw0-p2], [wget 10.0.0.10 -t 5 -T 1 --retry-connrefused -v -o wget$i.log])
+done
+
+dnl Each server should have at least one connection.
+AT_CHECK([ovs-appctl dpctl/dump-conntrack | FORMAT_CT(10.0.0.10) | \
+sed -e 's/zone=[[0-9]]*/zone=<cleared>/'], [0], [dnl
+tcp,orig=(src=10.0.0.4,dst=10.0.0.10,sport=<cleared>,dport=<cleared>),reply=(src=10.0.0.3,dst=10.0.0.4,sport=<cleared>,dport=<cleared>),zone=<cleared>,protoinfo=(state=<cleared>)
+tcp,orig=(src=10.0.0.4,dst=10.0.0.10,sport=<cleared>,dport=<cleared>),reply=(src=20.0.0.3,dst=10.0.0.4,sport=<cleared>,dport=<cleared>),zone=<cleared>,protoinfo=(state=<cleared>)
+])
+
+# Stop webserer in sw0-p1
+kill `cat $sw0_p1_pid_file`
+
+# Wait until service_monitor for sw0-p1 is set to offline
+OVS_WAIT_UNTIL([test 1 = `ovn-sbctl --bare --columns status find \
+service_monitor logical_port=sw0-p1 | sed '/^$/d' | grep offline | wc -l`])
+
+OVS_WAIT_UNTIL(
+    [ovn-sbctl dump-flows sw0 | grep ct_lb | grep priority=120 | grep "ip4.dst == 10.0.0.10" > lflows.txt
+     test 1 = `cat lflows.txt | grep "ct_lb(20.0.0.3:80)" | wc -l`]
+)
+
+ovs-appctl dpctl/flush-conntrack
+# From sw0-p2 send traffic to vip - 10.0.0.10
+for i in `seq 1 20`; do
+    echo Request $i
+    NS_CHECK_EXEC([sw0-p2], [wget 10.0.0.10 -t 5 -T 1 --retry-connrefused -v -o wget$i.log])
+done
+
+AT_CHECK([ovs-appctl dpctl/dump-conntrack | FORMAT_CT(10.0.0.10) | \
+sed -e 's/zone=[[0-9]]*/zone=<cleared>/'], [0], [dnl
+tcp,orig=(src=10.0.0.4,dst=10.0.0.10,sport=<cleared>,dport=<cleared>),reply=(src=20.0.0.3,dst=10.0.0.4,sport=<cleared>,dport=<cleared>),zone=<cleared>,protoinfo=(state=<cleared>)
+])
+
+# Create udp load balancer.
+ovn-nbctl lb-add lb2 10.0.0.10:80 10.0.0.3:80,20.0.0.3:80 udp
+lb_udp=`ovn-nbctl lb-list | grep udp | awk '{print $1}'`
+
+echo "lb udp uuid = $lb_udp"
+
+ovn-nbctl list load_balancer
+
+ovn-nbctl --wait=sb set load_balancer $lb_udp ip_port_mappings:10.0.0.3=sw0-p1:10.0.0.2
+ovn-nbctl --wait=sb set load_balancer $lb_udp ip_port_mappings:20.0.0.3=sw1-p1:20.0.0.2
+
+ovn-nbctl --wait=sb -- --id=@hc create \
+Load_Balancer_Health_Check vip="10.0.0.10\:80" -- add Load_Balancer $lb_udp \
+health_check @hc
+
+ovn-nbctl --wait=sb ls-lb-add sw0 lb2
+ovn-nbctl --wait=sb ls-lb-add sw1 lb2
+ovn-nbctl --wait=sb lr-lb-add lr0 lb2
+
+sleep 10
+
+ovn-nbctl list load_balancer
+echo "*******Next is health check*******"
+ovn-nbctl list Load_Balancer_Health_Check
+echo "********************"
+ovn-sbctl list service_monitor
+
+# Wait until udp service_monitor are set to offline
+OVS_WAIT_UNTIL([test 2 = `ovn-sbctl --bare --columns status find \
+service_monitor protocol=udp | sed '/^$/d' | grep offline | wc -l`])
+
+OVS_APP_EXIT_AND_WAIT([ovn-controller])
+
+as ovn-sb
+OVS_APP_EXIT_AND_WAIT([ovsdb-server])
+
+as ovn-nb
+OVS_APP_EXIT_AND_WAIT([ovsdb-server])
+
+as northd
+OVS_APP_EXIT_AND_WAIT([ovn-northd])
+
+as
+OVS_TRAFFIC_VSWITCHD_STOP(["/failed to query port patch-.*/d
+/connection dropped.*/d"])
+
+AT_CLEANUP

commit eb61de34a8c768934724e02c95272e59624a1559

pinctrl: Probe new service monitors after their flows are installed.
A new service monitor sends its first probe as soon as
sync_svc_monitors() sees the Service_Monitor row.  pinctrl_run() runs
before ofctrl_put() in the same main loop iteration, so the probe
leaves before the flows computed from the same SB update are in OVS.
The reply depends on some of these flows.  ovn-northd adds the
Service_Monitor row and the ARP responder that answers for the monitor
source IP with 'svc_monitor_mac' in one transaction:

  priority 110, "arp.tpa == <src_ip> && arp.op == 1"

The backend usually has no ARP entry for the source IP yet and
broadcasts a request as soon as the SYN arrives.  If that request is
handled before the flow above is installed, and another responder for
the same IP exists, the backend learns the wrong MAC.  With
ovn-octavia-provider the source IP belongs to the "ovn-lb-hm-<subnet>"
localport, which has the usual priority 50 responder for broadcast
requests with its own MAC.  The SYN-ACKs then go to the localport and
never reach ovn-controller, the monitor goes offline after
failure_count probes, and the backends of the load balancer are
removed until the backend refreshes its ARP entry with a unicast
request, which hits the priority 110 flow.  We saw 7 to 41 seconds
without a working backend after the first health check of a subnet
was created.

On a bare metal deployment the backend sent the ARP request 0.11 ms
after the first SYN and got the localport MAC every time.  On a nested
test setup the request came 0.36 ms after the SYN and the priority 110
flow was installed just in time, although the SYN was sent about
0.8 ms before OVS reported that flow.

Keep a new monitor in a new SVC_MON_S_WAIT_FLOWS state and request an
ofctrl seqno for it in sync_svc_monitors(), which runs before
ofctrl_put() of the same iteration.  Move the monitor to
SVC_MON_S_INIT once the seqno is acked, i.e. once the flows for these
SB contents are installed.  If the OpenFlow connection drops,
ofctrl_seqno_flush() drops the request, so request it again.  Replies
are ignored while a monitor waits, and new monitors are zeroed so that
the probe fields are never used uninitialized.

This also covers monitors that start because the backend port was
bound to this chassis or came up.

There is no test that makes the race deterministic: the flows have to
stay uninstalled while the main loop keeps running.  The existing
health check tests pass and show that the probes still start.

CC: Numan Siddique <numans@ovn.org>
Fixes: 8be01f4a5329 ("Send service monitor health checks")

controller/ovn-controller.c
@@ -1073,6 +1073,7 @@ en_ofctrl_is_connected_run(struct engine_node *node OVS_UNUSED, void *data)
         if (!of_data->connected) {
             ofctrl_seqno_flush();
             if_status_mgr_clear(ctrl_ctx->if_mgr);
+            pinctrl_seqno_flush();
         }
         return EN_UPDATED;
     }
@@ -8671,6 +8672,7 @@ main(int argc, char *argv[])
                     stopwatch_start(OFCTRL_SEQNO_RUN_STOPWATCH_NAME,
                                     time_msec());
                     ofctrl_seqno_run(ofctrl_get_cur_cfg());
+                    pinctrl_seqno_run();
                     stopwatch_stop(OFCTRL_SEQNO_RUN_STOPWATCH_NAME,
                                    time_msec());
                     stopwatch_start(IF_STATUS_MGR_RUN_STOPWATCH_NAME,

controller/pinctrl.c
@@ -30,6 +30,7 @@
 #include "mac-cache.h"
 #include "nx-match.h"
 #include "ofctrl.h"
+#include "lib/ofctrl-seqno.h"
 #include "latch.h"
 #include "lib/packets.h"
 #include "lib/sset.h"
@@ -7121,6 +7122,8 @@ pinctrl_handle_bind_vport(
 }
 
 enum svc_monitor_state {
+    SVC_MON_S_WAIT_FLOWS,   /* New monitor: waits until the flows computed
+                             * from the same SB contents are installed. */
     SVC_MON_S_INIT,
     SVC_MON_S_WAITING,
     SVC_MON_S_ONLINE,
@@ -7204,17 +7207,29 @@ struct svc_monitor {
     ovs_be16 icmp_id;
     ovs_be16 icmp_seq_no;
 
+    /* ofctrl_seqno requested for SVC_MON_S_WAIT_FLOWS, 0 if none yet.
+     * Accessed only by the main ovn-controller thread. */
+    uint64_t flows_seqno;
+
     bool delete;
 };
 
 static struct hmap svc_monitors_map;
 static struct ovs_list svc_monitors;
 
+/* ofctrl_seqno type used to learn when the flows are installed for new
+ * service monitors, and the last requested and acked values.  Accessed
+ * only by the main ovn-controller thread. */
+static size_t svc_mon_seqno_type;
+static uint64_t svc_mon_seqno_requested;
+static uint64_t svc_mon_seqno_acked;
+
 static void
 init_svc_monitors(void)
 {
     hmap_init(&svc_monitors_map);
     ovs_list_init(&svc_monitors);
+    svc_mon_seqno_type = ofctrl_seqno_add_type();
 }
 
 static void
@@ -7428,13 +7443,13 @@ sync_svc_monitors(struct ovsdb_idl_txn *ovnsb_idl_txn,
                                            sb_svc_mon->port, protocol, hash);
 
         if (!svc_mon) {
-            svc_mon = xmalloc(sizeof *svc_mon);
+            svc_mon = xzalloc(sizeof *svc_mon);
             svc_mon->dp_key = dp_key;
             svc_mon->port_key = port_key;
             svc_mon->proto_port = sb_svc_mon->port;
             svc_mon->ip = ip_addr;
             svc_mon->is_ip6 = !is_ipv4;
-            svc_mon->state = SVC_MON_S_INIT;
+            svc_mon->state = SVC_MON_S_WAIT_FLOWS;
             svc_mon->status = SVC_MON_ST_UNKNOWN;
             svc_mon->protocol = protocol;
 
@@ -7501,10 +7516,74 @@ sync_svc_monitors(struct ovsdb_idl_txn *ovnsb_idl_txn,
         }
     }
 
+    /* The first probe of a new monitor must wait until the flows computed
+     * from these SB contents are installed.  Otherwise the reply can race
+     * with them, e.g. the backend resolves the probe source IP before the
+     * ARP responder for 'svc_monitor_mac' exists and the replies never reach
+     * ovn-controller.  This is called before ofctrl_put() of the same
+     * iteration, so the request is acked once those flows are installed. */
+    bool requested = false;
+    LIST_FOR_EACH (svc_mon, list_node, &svc_monitors) {
+        if (svc_mon->state == SVC_MON_S_WAIT_FLOWS && !svc_mon->flows_seqno) {
+            if (!requested) {
+                ofctrl_seqno_update_create(svc_mon_seqno_type,
+                                           ++svc_mon_seqno_requested);
+                requested = true;
+            }
+            svc_mon->flows_seqno = svc_mon_seqno_requested;
+        }
+    }
+
     if (changed) {
         notify_pinctrl_handler();
     }
 
+}
+
+/* Must be called by the main thread after ofctrl_seqno_run(). */
+void
+pinctrl_seqno_run(void)
+{
+    struct ofctrl_acked_seqnos *acked =
+        ofctrl_acked_seqnos_get(svc_mon_seqno_type);
+    uint64_t last_acked = acked->last_acked;
+    ofctrl_acked_seqnos_destroy(acked);
+
+    if (last_acked == svc_mon_seqno_acked) {
+        return;
+    }
+    svc_mon_seqno_acked = last_acked;
+
+    bool changed = false;
+    struct svc_monitor *svc_mon;
+    ovs_mutex_lock(&pinctrl_mutex);
+    LIST_FOR_EACH (svc_mon, list_node, &svc_monitors) {
+        if (svc_mon->state == SVC_MON_S_WAIT_FLOWS && svc_mon->flows_seqno
+            && svc_mon->flows_seqno <= last_acked) {
+            svc_mon->state = SVC_MON_S_INIT;
+            changed = true;
+        }
+    }
+    ovs_mutex_unlock(&pinctrl_mutex);
+
+    if (changed) {
+        notify_pinctrl_handler();
+    }
+}
+
+/* Must be called by the main thread after ofctrl_seqno_flush().  The flushed
+ * requests are never acked, so the waiting monitors request again. */
+void
+pinctrl_seqno_flush(void)
+{
+    struct svc_monitor *svc_mon;
+    ovs_mutex_lock(&pinctrl_mutex);
+    LIST_FOR_EACH (svc_mon, list_node, &svc_monitors) {
+        if (svc_mon->state == SVC_MON_S_WAIT_FLOWS) {
+            svc_mon->flows_seqno = 0;
+        }
+    }
+    ovs_mutex_unlock(&pinctrl_mutex);
 }
 
 enum bfd_state {
@@ -8452,6 +8531,10 @@ svc_monitors_run(struct rconn *swconn,
         long long int next_run_time = LLONG_MAX;
         enum svc_monitor_status old_status = svc_mon->status;
         switch (svc_mon->state) {
+        case SVC_MON_S_WAIT_FLOWS:
+            /* pinctrl_seqno_run() moves it to SVC_MON_S_INIT. */
+            break;
+
         case SVC_MON_S_INIT:
             svc_monitor_send_health_check(swconn, svc_mon);
             next_run_time = svc_mon->wait_time;
@@ -8534,6 +8617,11 @@ static void
 pinctrl_handle_icmp_svc_check(struct dp_packet *pkt_in,
                               struct svc_monitor *svc_mon)
 {
+    if (svc_mon->state == SVC_MON_S_WAIT_FLOWS) {
+        /* No probe sent yet. */
+        return;
+    }
+
     if (!svc_mon->is_ip6) {
         /* IPv4 ICMP echo reply */
         struct icmp_header *ih = dp_packet_l4(pkt_in);
@@ -8573,7 +8661,7 @@ pinctrl_handle_tcp_svc_check(struct rconn *swconn,
 {
     struct tcp_header *th = dp_packet_l4(pkt_in);
 
-    if (!th) {
+    if (!th || svc_mon->state == SVC_MON_S_WAIT_FLOWS) {
         return false;
     }
 
@@ -8814,6 +8902,10 @@ pinctrl_handle_svc_check(struct rconn *swconn, const struct flow *ip_flow,
             static struct vlog_rate_limit rl = VLOG_RATE_LIMIT_INIT(1, 5);
             VLOG_WARN_RL(&rl, "handle service check: Service monitor not "
                          "found for ICMP packet");
+            return;
+        }
+
+        if (svc_mon->state == SVC_MON_S_WAIT_FLOWS) {
             return;
         }
 

controller/pinctrl.h
@@ -63,6 +63,8 @@ void pinctrl_run(struct ovsdb_idl_txn *ovnsb_idl_txn,
                  int64_t cur_cfg);
 void pinctrl_wait(struct ovsdb_idl_txn *ovnsb_idl_txn);
 void pinctrl_destroy(void);
+void pinctrl_seqno_run(void);
+void pinctrl_seqno_flush(void);
 
 void pinctrl_update_swconn(const char *target, int probe_interval);
 